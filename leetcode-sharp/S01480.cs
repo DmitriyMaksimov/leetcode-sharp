228bfcdba1b5616bf07fd4889c824b08ccfb0c04
Dmitriy <dmitriy.maksimov@workday.com>
2022-11-15 21:00:59 +1000

    1480. Running Sum of 1d Array

---
+++ leetcode-sharp/S01480.cs
@@ -0,0 +1,14 @@
+﻿namespace leetcode_sharp;
+
+public class S01480
+{
+    public int[] RunningSum(int[] nums)
+    {
+        for (var i = 1; i < nums.Length; i++)
+        {
+            nums[i] += nums[i - 1];
+        }
+
+        return nums;
+    }
+}
