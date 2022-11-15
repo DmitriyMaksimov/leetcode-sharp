a6a68db1cc777139a46f0f557c553ffb4b25db28
Dmitriy <dmitriy.maksimov@workday.com>
2022-11-15 20:26:24 +1000

    1929. Concatenation of Array

---
+++ leetcode-sharp/S01929.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 1929. Concatenation of Array
+// https://leetcode.com/problems/concatenation-of-array/
+public class S01929
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+        return nums.Concat(nums).ToArray();
+    }
+}
