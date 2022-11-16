f5a1e5244d7f24902698bf6636a3e1b22081c9c3
Dmitriy <dmitriy.maksimov@workday.com>
2022-11-16 21:40:50 +1000

    1920. Build Array from Permutation

---
+++ leetcode-sharp/S01920.cs
@@ -0,0 +1,16 @@
+﻿namespace leetcode_sharp;
+
+// 1920. Build Array from Permutation
+// https://leetcode.com/problems/build-array-from-permutation/
+public class S01920
+{
+    public int[] BuildArray(int[] nums)
+    {
+        var retVal = new int[nums.Length];
+        for (var i = 0; i < nums.Length; i++)
+        {
+            retVal[i] = nums[nums[i]];
+        }
+        return retVal;
+    }
+}
