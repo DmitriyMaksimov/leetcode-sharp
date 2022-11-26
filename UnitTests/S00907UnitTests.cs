6cf608c7a0786649711321249d87f7fcb73e4c8e
Dmitriy <dmitriy.maksimov@workday.com>
2022-11-26 12:44:56 +1000

    907. Sum of Subarray Minimums

---
+++ UnitTests/S00907UnitTests.cs
@@ -0,0 +1,14 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00907UnitTests
+{
+    [Test]
+    public void Test1()
+    {
+        var sut = new S00907();
+        sut.SumSubarrayMins(new[] {3, 1, 2, 4}).Should().Be(17);
+    }
+}
