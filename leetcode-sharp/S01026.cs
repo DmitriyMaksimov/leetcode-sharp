56d4637786cc2330a0068ef4a404c25149efaaa4
Dmitriy <dmitriy.maksimov@workday.com>
2022-12-12 18:08:06 +1000

    1026. Maximum Difference Between Node and Ancestor

---
+++ leetcode-sharp/S01026.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 1026. Maximum Difference Between Node and Ancestor
+// https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
+public class S01026
+{
+    public int MaxAncestorDiff(TreeNode root)
+    {
+        var min = int.MaxValue;
+        var max = int.MinValue;
+
+        return findMaxDiff(root, min, max);
+    }
+
+    private static int findMaxDiff(TreeNode node, int min, int max)
+    {
+        if (node == null) return max - min;
+
+        max = Math.Max(max, node.val);
+        min = Math.Min(min, node.val);
+        return Math.Max(findMaxDiff(node.left, min, max), findMaxDiff(node.right, min, max));
+    }
+}
