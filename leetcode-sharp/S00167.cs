78f249c18c2b3bafc5c84b2dbd02a57e05649020
Dmitriy <dmitriy.maksimov@workday.com>
2022-12-19 22:11:59 +1000

    167. Two Sum II - Input Array Is Sorted

---
+++ leetcode-sharp/S00167.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 167. Two Sum II - Input Array Is Sorted
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+public class S00167
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            var index = Array.BinarySearch(numbers, i + 1, numbers.Length - i - 1, target - numbers[i]);
+            if (index >= 0)
+            {
+                return new[] {i + 1, index + 1};
+            }
+        }
+
+        return new[] {-1, -1};
+    }
+}
