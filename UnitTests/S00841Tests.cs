4d33ea8fb7cf5e823cad5437d2c1b3189257a38f
Dmitriy <dmitriy.maksimov@workday.com>
2022-12-20 23:25:45 +1000

    841. Keys and Rooms

---
+++ UnitTests/S00841Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00841Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00841();
+        sut.CanVisitAllRooms(new List<IList<int>> {new List<int> {1}, new List<int> {2}, new List<int> {3}, new List<int>()}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00841();
+        sut.CanVisitAllRooms(new List<IList<int>> {new List<int> {1, 3}, new List<int> {3, 0, 1}, new List<int> {2}, new List<int> {0}}).Should().BeFalse();
+    }
+}
