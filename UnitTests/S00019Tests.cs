1b2af9a8e01c22d65623cbc3042f742d28ba6b5a
Dmitriy <dmitriy.maksimov@workday.com>
2022-12-26 12:05:18 +1000

    19. Remove Nth Node From End of List

---
+++ UnitTests/S00019Tests.cs
@@ -0,0 +1,35 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00019Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00019();
+        ListNode.ToString(sut.RemoveNthFromEnd(ListNode.Parse("[1,2,3,4,5]"), 2)).Should().Be("[1,2,3,5]");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00019();
+        ListNode.ToString(sut.RemoveNthFromEnd(ListNode.Parse("[1]"), 1)).Should().Be("[]");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00019();
+        ListNode.ToString(sut.RemoveNthFromEnd(ListNode.Parse("[1, 2]"), 1)).Should().Be("[1]");
+    }
+    
+    [Test]
+    public void T4()
+    {
+        var sut = new S00019();
+        ListNode.ToString(sut.RemoveNthFromEnd(ListNode.Parse("[1, 2]"), 2)).Should().Be("[2]");
+    }
+}
