64c226b0c05baa78ea4d7f5f890ea4bcc0d00e59
Dmitriy <dmitriy.maksimov@workday.com>
2022-12-28 11:49:56 +1000

    1962. Remove Stones to Minimize the Total

---
+++ leetcode-sharp/S01962.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 1962. Remove Stones to Minimize the Total
+// https://leetcode.com/problems/remove-stones-to-minimize-the-total/
+public class S01962
+{
+    public int MinStoneSum(int[] piles, int k)
+    {
+        var sum = 0;
+        var queue = new PriorityQueue<int, int>();
+        foreach (var pile in piles)
+        {
+            queue.Enqueue(pile, int.MaxValue - pile);
+            sum += pile;
+        }
+
+        while (k-- > 0)
+        {
+            var n = queue.Dequeue();
+            var decrement = n / 2;
+            sum -= decrement;
+            n -= decrement;
+            queue.Enqueue(n, int.MaxValue - n);
+        }
+        
+        return sum;
+    }
+}
