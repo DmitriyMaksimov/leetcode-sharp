5239c4f2e9078814746f9b335ddb7d3f68fa13c2
Dmitriy <dmitriy.maksimov@workday.com>
2022-12-29 13:02:34 +1000

    1834. Single-Threaded CPU

---
+++ leetcode-sharp/S01834.cs
@@ -0,0 +1,74 @@
+﻿namespace leetcode_sharp;
+
+// 1834. Single-Threaded CPU
+// https://leetcode.com/problems/single-threaded-cpu/
+public class S01834
+{
+    private PriorityQueue<int, (int enqueueTime, int processingTime, int index)> _tasksByEnqueueTime = new();
+    private PriorityQueue<int, (int processingTime, int index)> _readyForExecution = new();
+    private int[][] _tasks;
+
+    public int[] GetOrder(int[][] tasks)
+    {
+        _tasks = tasks;
+
+        for (var i = 0; i < tasks.Length; ++i)
+        {
+            var task = tasks[i];
+            var (enqueueTime, processingTime) = (task[0], task[1]);
+            var priority = (enqueueTime, processingTime, i);
+            _tasksByEnqueueTime.Enqueue(i, priority);
+        }
+
+        var currentCpuCycle = 1;
+
+        var result = new List<int>(tasks.Length);
+
+        while (true)
+        {
+            findReadyToExecuteTasks(currentCpuCycle);
+
+            if (_readyForExecution.Count > 0)
+            {
+                // We have tasks => execute first one
+                var index = _readyForExecution.Dequeue();
+                var task = tasks[index];
+                var (enqueueTime, processingTime) = (task[0], task[1]);
+                currentCpuCycle += processingTime;
+                result.Add(index);
+            }
+            else
+            {
+                // No tasks to execute => idle until first task
+                if (_tasksByEnqueueTime.Count == 0)
+                {
+                    // All tasks processed
+                    break;
+                }
+                var index = _tasksByEnqueueTime.Peek();
+                var task = tasks[index];
+                var (enqueueTime, processingTime) = (task[0], task[1]);
+                currentCpuCycle = enqueueTime;
+            }
+        }
+
+        return result.ToArray();
+    }
+
+    private void findReadyToExecuteTasks(int currentCpuCycle)
+    {
+        while (_tasksByEnqueueTime.Count > 0)
+        {
+            var index = _tasksByEnqueueTime.Peek();
+            var task = _tasks[index];
+            var (enqueueTime, processingTime) = (task[0], task[1]);
+            if (enqueueTime > currentCpuCycle)
+            {
+                break;
+            }
+
+            _readyForExecution.Enqueue(index, (processingTime, index));
+            _tasksByEnqueueTime.Dequeue();
+        }
+    }
+}
