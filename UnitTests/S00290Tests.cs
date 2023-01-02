f4819c1e49cf0c49255336f04bfed9cd4f823eb4
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-02 11:05:31 +1000

    290. Word Pattern

---
+++ UnitTests/S00290Tests.cs
@@ -0,0 +1,42 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00290Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00290();
+        sut.WordPattern("abba", "dog cat cat dog").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00290();
+        sut.WordPattern("abba", "dog cat cat fish").Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00290();
+        sut.WordPattern("aaaa", "dog cat cat dog").Should().BeFalse();
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S00290();
+        sut.WordPattern("abba", "dog dog dog dog").Should().BeFalse();
+    }
+
+    [Test]
+    public void T5()
+    {
+        var sut = new S00290();
+        sut.WordPattern("aaa", "aa aa aa aa").Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/S00290.cs
@@ -0,0 +1,43 @@
+﻿namespace leetcode_sharp;
+
+// 290. Word Pattern
+// https://leetcode.com/problems/word-pattern/
+public class S00290
+{
+    public bool WordPattern(string pattern, string s)
+    {
+        var matches = new Dictionary<char, string>();
+        var strings = s.Split(' ').ToArray();
+        var words = new HashSet<string>();
+
+        if (strings.Length != pattern.Length)
+        {
+            return false;
+        }
+        
+        for (var i = 0; i < pattern.Length; i++)
+        {
+            var ch = pattern[i];
+            var word = strings[i];
+            
+            if (matches.ContainsKey(ch))
+            {
+                if (matches[ch] != word)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (words.Contains(word))
+                {
+                    return false;
+                }
+                matches[ch] = word;
+                words.Add(word);
+            }
+        }
+
+        return true;
+    }
+}
