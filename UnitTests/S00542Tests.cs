0c336703b67f5606e0082cded6ba9e7e0d674d26
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-02 22:14:57 +1000

    542. 01 Matrix

---
+++ UnitTests/S00542Tests.cs
@@ -0,0 +1,31 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00542Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00542();
+        sut.UpdateMatrix(new[] {new[] {0, 0, 0}, new[] {0, 1, 0}, new[] {0, 0, 0}})
+            .Should().BeEquivalentTo(new[] {new[] {0, 0, 0}, new[] {0, 1, 0}, new[] {0, 0, 0}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00542();
+        sut.UpdateMatrix(new[] {new[] {0, 0, 0}, new[] {0, 1, 0}, new[] {1,1,1}})
+            .Should().BeEquivalentTo(new[] {new[] {0, 0, 0}, new[] {0, 1, 0}, new[] {1, 2, 1}});
+    }
+    
+    [Test]
+    public void T3()
+    {
+        var sut = new S00542();
+        sut.UpdateMatrix(new[] {new[] {0, 0, 0}, new[] {0, 1, 0}})
+            .Should().BeEquivalentTo(new[] {new[] {0, 0, 0}, new[] {0, 1, 0}});
+    }
+}
