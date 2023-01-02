9e90062b2257c6d3cf3f621f1bed591ece4b16bc
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-02 11:43:34 +1000

    520. Detect Capital

---
+++ UnitTests/S00520Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00520Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00520();
+        sut.DetectCapitalUse("USA").Should().BeTrue();
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00520();
+        sut.DetectCapitalUse("FlaG").Should().BeFalse();
+    }    
+}
---
+++ leetcode-sharp/S00520.cs
@@ -0,0 +1,13 @@
+﻿namespace leetcode_sharp;
+
+// 520. Detect Capital
+// https://leetcode.com/problems/detect-capital/
+public class S00520
+{
+    public bool DetectCapitalUse(string word)
+    {
+        var restOfTheWord = word[1..];
+        
+        return char.IsUpper(word[0]) ? restOfTheWord.All(char.IsLower) || restOfTheWord.All(char.IsUpper) : restOfTheWord.All(char.IsLower);
+    }
+}
