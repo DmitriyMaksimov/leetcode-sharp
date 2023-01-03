fc27cea0b89edfac1106d2e82b53e72c5bc26dcb
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-03 20:09:30 +1000

    944. Delete Columns to Make Sorted

---
+++ UnitTests/S00944Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00944Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00944();
+        sut.MinDeletionSize(new[] {"cba","daf","ghi"}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00944();
+        sut.MinDeletionSize(new[] {"a","b"}).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00944();
+        sut.MinDeletionSize(new[] {"zyx","wvu","tsr"}).Should().Be(3);
+    }
+}
