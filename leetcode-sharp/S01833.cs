2d7f2d31b122d8b6e31a478f6e7486914fd8188c
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-06 14:56:33 +1000

    1833. Maximum Ice Cream Bars

---
+++ leetcode-sharp/S01833.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 1833. Maximum Ice Cream Bars
+// https://leetcode.com/problems/maximum-ice-cream-bars/
+public class S01833
+{
+    public int MaxIceCream(int[] costs, int coins)
+    {
+        Array.Sort(costs);
+        for (int i = 0; i < costs.Length; i++)
+        {
+            if (costs[i] > coins)
+            {
+                return i;
+            }
+
+            coins -= costs[i];
+        }
+        
+        return costs.Length;
+    }
+}
