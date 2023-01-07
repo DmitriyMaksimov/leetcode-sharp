29e28515412f04e670fe63fe3421044e3f78d0f7
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-07 12:35:20 +1000

    617. Merge Two Binary Trees

---
+++ UnitTests/S00617Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00617Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00617();
+        sut.MergeTrees(TreeNode.Parse("[1,3,2,5]"), TreeNode.Parse("[2,1,3,null,4,null,7]")).Should().BeEquivalentTo(TreeNode.Parse("[3,4,5,5,4,null,7]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00617();
+        sut.MergeTrees(TreeNode.Parse("[1]"), TreeNode.Parse("[1,2]")).Should().BeEquivalentTo(TreeNode.Parse("[2,2]"));
+    }
+}
