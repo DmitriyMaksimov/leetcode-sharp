f9a37a1f6f0c3babb58bd3679f018b7f076ccc1f
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-07 11:46:22 +1000

    120. Triangle

---
+++ UnitTests/S00120Tests.cs
@@ -0,0 +1,30 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00120Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00120();
+        sut.MinimumTotal(new List<IList<int>>
+        {
+            new List<int> {2},
+            new List<int> {3, 4},
+            new List<int> {6, 5, 7},
+            new List<int> {4, 1, 8, 3}
+        }).Should().Be(11);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00120();
+        sut.MinimumTotal(new List<IList<int>>
+        {
+            new List<int> {-10},
+        }).Should().Be(-10);
+    }
+}
