4bf2f7b43a22dd574b0abad1750fde04e8e026c9
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-08 18:47:55 +1000

    149. Max Points on a Line

---
+++ UnitTests/S00149Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00149Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00149();
+        sut.MaxPoints(new[] {new[] {1, 1}, new[] {2, 2}, new[] {3, 3}}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00149();
+        sut.MaxPoints(new[] {new[] {1, 1}, new[] {3, 2}, new[] {5, 3}, new[] {4, 1}, new[] {2, 3}, new[] {1, 4}}).Should().Be(4);
+    }
+    
+    [Test]
+    public void T3()
+    {
+        var sut = new S00149();
+        sut.MaxPoints(new[] {new[] {0, 0}}).Should().Be(1);
+    }
+}
