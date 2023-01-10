adaa0f0b0710530183de532f0445e170f36f8ea4
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-10 19:19:58 +1000

    206. Reverse Linked List

---
+++ UnitTests/S00206Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00206Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00206();
+        sut.ReverseList(ListNode.Parse("[1,2,3,4,5]")).Should().BeEquivalentTo(ListNode.Parse("[5,4,3,2,1]"));
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00206();
+        sut.ReverseList(ListNode.Parse("[1,2]")).Should().BeEquivalentTo(ListNode.Parse("[2,1]"));
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00206();
+        sut.ReverseList(ListNode.Parse("[]")).Should().BeEquivalentTo(ListNode.Parse("[]"));
+    }
+}
