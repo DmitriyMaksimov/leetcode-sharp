3faf84bad21fc09cb9d3d0b9f1fd4c1f37044f93
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-13 15:23:13 +1000

    46. Permutations

---
+++ UnitTests/S00046Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00046Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00046();
+        sut.Permute(new[] {1, 2, 3}).Should().BeEquivalentTo(new[] {new[] {1, 2, 3}, new[] {1, 3, 2}, new[] {2, 1, 3}, new[] {2, 3, 1}, new[] {3, 1, 2}, new[] {3, 2, 1}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00046();
+        sut.Permute(new[] {0, 1}).Should().BeEquivalentTo(new[] {new[] {0, 1}, new[] {1, 0}});
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00046();
+        sut.Permute(new[] {1}).Should().BeEquivalentTo(new[] {new[] {1}});
+    }
+}
