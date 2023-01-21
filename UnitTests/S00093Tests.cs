8218fc40e46d2e8539646677060e6e641aaf1cc4
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-21 13:44:10 +1000

    93. Restore IP Addresses

---
+++ UnitTests/S00093Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00093Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00093();
+        sut.RestoreIpAddresses("25525511135").Should().BeEquivalentTo("255.255.11.135", "255.255.111.35");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00093();
+        sut.RestoreIpAddresses("0000").Should().BeEquivalentTo("0.0.0.0");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00093();
+        sut.RestoreIpAddresses("101023").Should().BeEquivalentTo("1.0.10.23", "1.0.102.3", "10.1.0.23", "10.10.2.3", "101.0.2.3");
+    }
+}
---
+++ leetcode-sharp/S00093.cs
@@ -0,0 +1,54 @@
+﻿namespace leetcode_sharp;
+
+// 93. Restore IP Addresses
+// https://leetcode.com/problems/restore-ip-addresses/
+public class S00093
+{
+    public IList<string> RestoreIpAddresses(string s)
+    {
+        var result = new List<string>();
+
+        if (s.Length > 12)
+        {
+            return result;
+        }
+
+        for (var i1 = 1; i1 <= 3; i1++)
+        {
+            for (var i2 = 1; i2 <= 3; i2++)
+            {
+                for (var i3 = 1; i3 <= 3; i3++)
+                {
+                    if (i1 + i2 + i3 >= s.Length)
+                    {
+                        continue;
+                    }
+                    
+                    var s1 = s.Substring(0, i1);
+                    var s2 = s.Substring(i1, i2);
+                    var s3 = s.Substring(i1 + i2, i3);
+                    var s4 = s.Substring(i1 + i2 + i3);
+
+                    if (isValidOctet(s1) && isValidOctet(s2) && isValidOctet(s3) && isValidOctet(s4))
+                    {
+                        result.Add($"{s1}.{s2}.{s3}.{s4}");
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+
+    private static bool isValidOctet(string s)
+    {
+        switch (s.Length)
+        {
+            case > 3:
+            case > 1 when s[0] == '0':
+                return false;
+        }
+
+        return int.Parse(s) <= 255;
+    }
+}
