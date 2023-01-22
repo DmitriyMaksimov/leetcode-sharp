dcf98f2e93f530188da5fdc4603c7a427b227a58
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-22 12:47:56 +1000

    131. Palindrome Partitioning

---
+++ UnitTests/S00131Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00131Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00131();
+        sut.Partition("aab").Should().BeEquivalentTo(new[] {new[] {"a", "a", "b"}, new[] {"aa", "b"}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00131();
+        sut.Partition("a").Should().BeEquivalentTo(new[] {new[] {"a"}});
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00131();
+        sut.Partition("bb").Should().BeEquivalentTo(new[] {new[] {"bb"}, new []{"b", "b"}});
+    }
+}
---
+++ leetcode-sharp/S00131.cs
@@ -0,0 +1,54 @@
+﻿namespace leetcode_sharp;
+
+// 131. Palindrome Partitioning
+// https://leetcode.com/problems/palindrome-partitioning/
+public class S00131
+{
+    public IList<IList<string>> Partition(string s)
+    {
+        if (!s.Any())
+        {
+            return new List<IList<string>>();
+        }
+
+        if (s.Length == 1)
+        {
+            return new List<IList<string>> {new List<string>{s}};
+        }
+        
+        var result = new List<IList<string>>();
+        
+        for (var i = 1; i <= s.Length; i++)
+        {
+            var current = s[..i];
+            
+            if (isPalindrome(current))
+            {
+                if (i == s.Length)
+                {
+                    var list = new List<string> {current};
+                    result.Add(list);
+                    continue;
+                }
+                foreach (var palindromes in Partition(s[i..]))
+                {
+                    var list = new List<string> {current};
+                    list.AddRange(palindromes);
+                    result.Add(list);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static bool isPalindrome(string s)
+    {
+        for (var i = 0; i < s.Length / 2; i++)
+        {
+            if (s[i] != s[s.Length - i - 1]) return false;
+        }
+
+        return true;
+    }
+}
