4b4e696d1d7e96e1cb994a21349cff1efd99d10f
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-27 18:21:53 +1000

    472. Concatenated Words

---
+++ UnitTests/S00472Tests.cs
@@ -0,0 +1,24 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00472Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00472();
+        sut.FindAllConcatenatedWordsInADict(new[] {"cat", "cats", "catsdogcats", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat"}).Should()
+            .Equal("catsdogcats", "dogcatsdog", "ratcatdogcat");
+        
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00472();
+        sut.FindAllConcatenatedWordsInADict(new[] {"cat","dog","catdog"}).Should().Equal("catdog");
+        
+    }
+}
---
+++ leetcode-sharp/S00472.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 472. Concatenated Words
+// https://leetcode.com/problems/concatenated-words/
+public class S00472
+{
+    public IList<string> FindAllConcatenatedWordsInADict(string[] words)
+    {
+        var result = new List<string>();
+        var dictionary = words.ToHashSet();
+
+        foreach (var word in words)
+        {
+            var length = word.Length;
+            var dp = new bool[length + 1];
+            dp[0] = true;
+            for (var i = 1; i <= length; ++i)
+            {
+                for (var j = i == length ? 1 : 0; !dp[i] && j < i; ++j)
+                {
+                    dp[i] = dp[j] && dictionary.Contains(word[j..i]);
+                }
+            }
+
+            if (dp[length])
+            {
+                result.Add(word);
+            }
+        }
+
+        return result;
+    }
+}
