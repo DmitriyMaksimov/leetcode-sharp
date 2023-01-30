94c5e6ba9ec5748db052b9f1fcaa1198353fec0d
Dmitriy <dmitriy.maksimov@workday.com>
2023-01-30 19:01:11 +1000

    1137. N-th Tribonacci Number

---
+++ UnitTests/S01137Tests.cs
@@ -0,0 +1,31 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01137Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01137();
+
+        sut.Tribonacci(4).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01137();
+
+        sut.Tribonacci(25).Should().Be(1389537);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01137();
+
+        sut.Tribonacci(37).Should().Be(2082876103);
+    }
+}
---
+++ leetcode-sharp/S01137.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode_sharp;
+
+// 1137. N-th Tribonacci Number
+// https://leetcode.com/problems/n-th-tribonacci-number/
+public class S01137
+{
+    private readonly Dictionary<int, int> _cache = new() {{0, 0}, {1, 1}, {2, 1}};
+
+    public int Tribonacci(int n)
+    {
+        if (_cache.TryGetValue(n, out var val))
+        {
+            return val;
+        }
+
+        return _cache[n] = Tribonacci(n - 3) + Tribonacci(n - 2) + Tribonacci(n - 1);
+    }
+}
