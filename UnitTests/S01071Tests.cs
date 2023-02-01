4c2a2364a7d850a8e18a1c44b0088f6eaf75335b
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-01 20:06:00 +1000

    1071. Greatest Common Divisor of Strings

---
+++ UnitTests/S01071Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01071Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01071();
+        sut.GcdOfStrings("ABCABC", "ABC").Should().Be("ABC");
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S01071();
+        sut.GcdOfStrings("ABABAB", "ABAB").Should().Be("AB");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01071();
+        sut.GcdOfStrings("LEET", "CODE").Should().BeEmpty();
+    }
+}
