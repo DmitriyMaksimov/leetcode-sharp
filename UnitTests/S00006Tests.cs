4e103b890c8dfe221a041c98aaf64fb81895296d
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-03 22:01:32 +1000

    6. Zigzag Conversion

---
+++ UnitTests/S00006Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00006Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00006();
+        sut.Convert("PAYPALISHIRING", 3).Should().Be("PAHNAPLSIIGYIR");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00006();
+        sut.Convert("PAYPALISHIRING", 4).Should().Be("PINALSIGYAHRPI");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00006();
+        sut.Convert("A", 1).Should().Be("A");
+    }
+}
---
+++ leetcode-sharp/S00006.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 6. Zigzag Conversion
+// https://leetcode.com/problems/zigzag-conversion/
+public class S00006
+{
+    public string Convert(string s, int numRows)
+    {
+        if (numRows == 1)
+        {
+            return s;
+        }
+        
+        var rows = new string[numRows];
+        var currentRow = 0;
+        var direction = 1;
+        
+        foreach (var ch in s)
+        {
+            rows[currentRow] += ch;
+            currentRow += direction;
+            if (currentRow == numRows || currentRow == -1)
+            {
+                direction = -direction;
+                currentRow += direction * 2;
+            }
+        }
+        
+        return string.Join("", rows);
+    }
+}
