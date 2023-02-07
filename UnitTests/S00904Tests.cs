5e3bc86cc7e1796237d64390faef26015ea3f7f0
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-07 22:47:00 +1000

    904. Fruit Into Baskets

---
+++ UnitTests/S00904Tests.cs
@@ -0,0 +1,35 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00904Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00904();
+        sut.TotalFruit(new[] {1, 2, 1}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00904();
+        sut.TotalFruit(new[] {0, 1, 2, 2}).Should().Be(3);
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S00904();
+        sut.TotalFruit(new[] {1, 2, 3, 2, 2}).Should().Be(4);
+    }
+
+    [Test]
+    public void T5()
+    {
+        var sut = new S00904();
+        sut.TotalFruit(new[] {0, 1, 6, 6, 4, 4, 6}).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/S00904.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 904. Fruit Into Baskets
+// https://leetcode.com/problems/fruit-into-baskets/
+public class S00904
+{
+    public int TotalFruit(int[] fruits)
+    {
+        var basket = new Dictionary<int, int>();
+        var left = 0;
+        int right;
+
+        for (right = 0; right < fruits.Length; ++right)
+        {
+            var rightFruit = fruits[right];
+            
+            basket[rightFruit] = basket.GetValueOrDefault(rightFruit) + 1;
+
+            if (basket.Count <= 2) continue;
+            
+            var leftFruit = fruits[left];
+            
+            basket[leftFruit] -= 1;
+
+            if (basket[leftFruit] == 0)
+            {
+                basket.Remove(leftFruit);
+            }
+
+            left++;
+        }
+
+        return right - left;
+    }
+}
