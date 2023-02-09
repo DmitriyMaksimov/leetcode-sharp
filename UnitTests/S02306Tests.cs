c161ac8be1bae8843abd70d121f285f3d2e161d4
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-09 19:54:23 +1000

    2306. Naming a Company

---
+++ UnitTests/S02306Tests.cs
@@ -0,0 +1,20 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+public class S02306Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02306();
+        sut.DistinctNames(new[] {"coffee", "donuts", "time", "toffee"}).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02306();
+        sut.DistinctNames(new[] {"lack", "back"}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/S02306.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode_sharp;
+
+// 2306. Naming a Company
+// https://leetcode.com/problems/naming-a-company/
+public class S02306
+{
+    private readonly HashSet<string>[] _groups = new HashSet<string>[26];    // first char => list of ideas' suffix (all characters except the first)
+    
+    public long DistinctNames(string[] ideas)
+    {
+        for (var index = 0; index < _groups.Length; index++)
+        {
+            _groups[index] = new HashSet<string>();
+        }
+
+        foreach (var idea in ideas)
+        {
+            var firstChar = idea[0] - 'a';
+
+            _groups[firstChar].Add(idea[1..]);
+        }
+
+        var result = 0L;
+
+        for (var i = 0; i < _groups.Length - 1; i++)
+        {
+            for (var j = i + 1; j < _groups.Length; j++)
+            {
+                var intersectCount = _groups[i].Intersect(_groups[j]).Count();
+                var count1 = _groups[i].Count - intersectCount;
+                var count2 = _groups[j].Count - intersectCount;
+                result += count1 * count2;
+            }
+        }
+
+        return result * 2;
+    }
+}
