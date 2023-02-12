f85b4541ca10fa1f214a3113001dacacbcca4e64
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-12 12:27:44 +1000

    145. Binary Tree Postorder Traversal

---
+++ leetcode-sharp/S00145.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 145. Binary Tree Postorder Traversal
+// https://leetcode.com/problems/binary-tree-postorder-traversal/
+public class S00145
+{
+    private readonly List<int> _result = new();
+
+    public IList<int>? PostorderTraversal(TreeNode? root)
+    {
+        postorderTraversal(root);
+        return _result;
+    }
+    
+    private void postorderTraversal(TreeNode? node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        postorderTraversal(node.left);
+        postorderTraversal(node.right);
+
+        _result.Add(node.val);
+    }
+}
