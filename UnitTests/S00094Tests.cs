20099e1c194b12f544f927ac38a78365fcd5f187
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-12 12:33:56 +1000

    94. Binary Tree Inorder Traversal

---
+++ UnitTests/S00094Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00094Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00094();
+        sut.InorderTraversal(TreeNode.Parse("[1,null,2,3]")).Should().Equal(1, 3, 2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00094();
+        sut.InorderTraversal(null).Should().BeEmpty();
+    }
+    
+    [Test]
+    public void T3()
+    {
+        var sut = new S00094();
+        sut.InorderTraversal(TreeNode.Parse("[1]")).Should().Equal(1);
+    }
+}
