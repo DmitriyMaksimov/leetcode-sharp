9acf9b90381485140973f0d42a265f118e9abbfd
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-13 18:07:08 +1000

    1523. Count Odd Numbers in an Interval Range

---
+++ UnitTests/S01523Tests.cs
@@ -0,0 +1,56 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01523Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01523();
+        sut.CountOdds(3, 7).Should().Be(3);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S01523();
+        sut.CountOdds(8, 10).Should().Be(1);
+    }
+    
+    [Test]
+    public void T3()
+    {
+        var sut = new S01523();
+        sut.CountOdds(8, 9).Should().Be(1);
+    }
+    
+    [Test]
+    public void T4()
+    {
+        var sut = new S01523();
+        sut.CountOdds(8, 11).Should().Be(2);
+    }
+    
+    [Test]
+    public void T5()
+    {
+        var sut = new S01523();
+        sut.CountOdds(6, 6).Should().Be(0);
+    }
+    
+    [Test]
+    public void T6()
+    {
+        var sut = new S01523();
+        sut.CountOdds(7, 7).Should().Be(1);
+    }
+    
+    [Test]
+    public void T7()
+    {
+        var sut = new S01523();
+        sut.CountOdds(0, 0).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/S01523.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode_sharp;
+
+// 1523. Count Odd Numbers in an Interval Range
+// https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+public class S01523
+{
+    public int CountOdds(int low, int high)
+    {
+        var interval = high - low + 1;
+        if (interval % 2 == 0 || low % 2 == 0 || high % 2 == 0)
+        {
+            return interval / 2;
+        }
+
+        return interval / 2 + 1;
+    }
+}
