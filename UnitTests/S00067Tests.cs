b0157220a9f74ed0c962f04ffe58ca4171bc6f0e
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-14 12:04:11 +1000

    67. Add Binary

---
+++ UnitTests/S00067Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00067Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00067();
+        sut.AddBinary("11", "1").Should().Be("100");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00067();
+        sut.AddBinary("1010", "1011").Should().Be("10101");
+    }
+    
+    [Test]
+    public void T3()
+    {
+        var sut = new S00067();
+        sut.AddBinary("1111", "1111").Should().Be("11110");
+    }
+}
