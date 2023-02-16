f527b8eba9f238eac2db08134f7d6b87eb60095e
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-16 13:17:42 +1000

    111. Minimum Depth of Binary Tree

---
+++ leetcode-sharp/S00111.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 111. Minimum Depth of Binary Tree
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/
+public class S00111
+{
+    public int MinDepth(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        if (root.left == null || root.right == null)
+        {
+            return Math.Max(MinDepth(root.left), MinDepth(root.right)) + 1;
+        }
+        
+        return Math.Min(MinDepth(root.left), MinDepth(root.right)) + 1;
+    }
+}
