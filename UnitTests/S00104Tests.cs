74faddcd25938974f5d67d201ed4d6bc6d618a42
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-16 13:01:14 +1000

    104. Maximum Depth of Binary Tree

---
+++ UnitTests/S00104Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00104Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00104();
+        sut.MaxDepth(TreeNode.Parse("[3,9,20,null,null,15,7]")).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00104();
+        sut.MaxDepth(TreeNode.Parse("[1,null,2]")).Should().Be(2);
+    }
+}
