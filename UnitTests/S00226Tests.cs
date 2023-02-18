dd2816ef3476e77d1334b3eb1b03bc05b0308133
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-18 12:22:25 +1000

    226. Invert Binary Tree

---
+++ UnitTests/S00226Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00226Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00226();
+        sut.InvertTree(TreeNode.Parse("[4,2,7,1,3,6,9]")).Should().BeEquivalentTo(TreeNode.Parse("[4,7,2,9,6,3,1]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00226();
+        sut.InvertTree(TreeNode.Parse("[2,1,3]")).Should().BeEquivalentTo(TreeNode.Parse("[2,3,1]"));
+    }
+}
---
+++ leetcode-sharp/S00226.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 226. Invert Binary Tree
+// https://leetcode.com/problems/invert-binary-tree/
+public class S00226
+{
+    public TreeNode? InvertTree(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+
+        InvertTree(root.left);
+        InvertTree(root.right);
+
+        (root.left, root.right) = (root.right, root.left);
+        
+        return root;
+    }
+}
