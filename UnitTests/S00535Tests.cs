b9c5a52c264c696f9d805dec5243d78f05340395
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-18 23:37:37 +1000

    535. Encode and Decode TinyURL

---
+++ UnitTests/S00535Tests.cs
@@ -0,0 +1,15 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00535Tests
+{
+    [TestCase("https://leetcode.com/problems/design-tinyurl")]
+    public void T1(string url)
+    {
+        var sut = new S00535();
+        var tiny = sut.encode(url);
+        sut.decode(tiny).Should().Be(url);
+    }
+}
---
+++ leetcode-sharp/S00535.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode_sharp;
+
+// 535. Encode and Decode TinyURL
+// https://leetcode.com/problems/encode-and-decode-tinyurl/
+public class S00535
+{
+    private const string baseUrl = "http://tinyurl.com/";
+    private Dictionary<string, string> _id2url = new();
+    private Dictionary<string, string> _url2id = new();
+    
+    // Encodes a URL to a shortened URL
+    public string encode(string longUrl)
+    {
+        if (_url2id.TryGetValue(longUrl, out var id))
+        {
+            return baseUrl + id;
+        }
+
+        var newId = Guid.NewGuid().ToString("N");
+        _url2id[longUrl] = newId;
+        _id2url[newId] = longUrl;
+        
+        return baseUrl + newId;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public string decode(string shortUrl)
+    {
+        var id = shortUrl[baseUrl.Length..];
+
+        if (_id2url.TryGetValue(id, out var url))
+        {
+            return url;
+        }
+        
+        return null;
+    }
+}
