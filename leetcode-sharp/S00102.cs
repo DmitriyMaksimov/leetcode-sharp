211e6560f6d21c166be17a6c1b1cfee9ab9e25ed
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-19 11:52:41 +1000

    102. Binary Tree Level Order Traversal

---
+++ leetcode-sharp/S00102.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcode_sharp;
+
+// 103. Binary Tree Zigzag Level Order Traversal
+// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+public class S00102
+{
+    public IList<IList<int>>? LevelOrder(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return new List<IList<int>>();
+        }
+
+        var currentLevel = new List<TreeNode?> {root};
+        var nextLevel = new List<TreeNode?>();
+        var levels = new List<List<TreeNode?>> {currentLevel};
+
+        while (true)
+        {
+            foreach (var node in currentLevel.Where(node => node != null))
+            {
+                nextLevel.Add(node.left);
+                nextLevel.Add(node.right);
+            }
+
+            if (nextLevel.Any(x => x != null))
+            {
+                levels.Add(nextLevel);
+                currentLevel = nextLevel;
+                nextLevel = new List<TreeNode?>();
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return levels.Select(nodes => nodes.Where(x => x != null).Select(x => x.val)).Select(values => values.ToList()).Cast<IList<int>>().ToList();
+    }
+}
