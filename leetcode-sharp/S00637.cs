164ae186404cb56ede2ca402f99a56ac84e73120
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-19 12:19:26 +1000

    637. Average of Levels in Binary Tree

---
+++ leetcode-sharp/S00637.cs
@@ -0,0 +1,48 @@
+﻿namespace leetcode_sharp;
+
+// 637. Average of Levels in Binary Tree
+// https://leetcode.com/problems/average-of-levels-in-binary-tree/
+public class S00637
+{
+    public IList<double> AverageOfLevels(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return new List<double>();
+        }
+
+        var currentLevel = new List<TreeNode?> {root};
+        var nextLevel = new List<TreeNode?>();
+        var levels = new List<List<TreeNode?>> {currentLevel};
+
+        while (true)
+        {
+            foreach (var node in currentLevel.Where(node => node != null))
+            {
+                nextLevel.Add(node.left);
+                nextLevel.Add(node.right);
+            }
+
+            if (nextLevel.Any(x => x != null))
+            {
+                levels.Add(nextLevel);
+                currentLevel = nextLevel;
+                nextLevel = new List<TreeNode?>();
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        var result = new List<double>();
+        
+        foreach (var nodes in levels)
+        {
+            var sum = nodes.Where(x => x != null).Select(x => (long)x.val).Sum();
+            result.Add((double)sum / nodes.Count(x => x != null));
+        }
+
+        return result;
+    }
+}
