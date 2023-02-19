6bbe97fd4b9b57953bbe3f6cd5b0de143d412456
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-19 11:48:08 +1000

    103. Binary Tree Zigzag Level Order Traversal

---
+++ UnitTests/S00103Tests.cs
@@ -0,0 +1,41 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00103Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00103();
+        var list = sut.ZigzagLevelOrder(TreeNode.Parse("[3,9,20,null,null,15,7]"));
+        list[0].Should().Equal(3);
+        list[1].Should().Equal(20, 9);
+        list[2].Should().Equal(15, 7);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00103();
+        sut.ZigzagLevelOrder(TreeNode.Parse("[1]")).Should().BeEquivalentTo(new[] {new[] {1}});
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00103();
+        sut.ZigzagLevelOrder(null).Should().BeEmpty();
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S00103();
+        var list = sut.ZigzagLevelOrder(TreeNode.Parse("[1,2,3,4,null,null,5]"));
+        list[0].Should().Equal(1);
+        list[1].Should().Equal(3, 2);
+        list[2].Should().Equal(4, 5);
+    }
+}
---
+++ leetcode-sharp/S00103.cs
@@ -0,0 +1,49 @@
+﻿namespace leetcode_sharp;
+
+// 103. Binary Tree Zigzag Level Order Traversal
+// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+public class S00103
+{
+    public IList<IList<int>>? ZigzagLevelOrder(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return new List<IList<int>>();
+        }
+
+        var currentLevel = new List<TreeNode?> {root};
+        var nextLevel = new List<TreeNode?>();
+        var levels = new List<List<TreeNode?>> {currentLevel};
+
+        while (true)
+        {
+            foreach (var node in currentLevel.Where(node => node != null))
+            {
+                nextLevel.Add(node.left);
+                nextLevel.Add(node.right);
+            }
+
+            if (nextLevel.Any(x => x != null))
+            {
+                levels.Add(nextLevel);
+                currentLevel = nextLevel;
+                nextLevel = new List<TreeNode?>();
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        var oddLevel = true;
+        var result = new List<IList<int>>();
+        
+        foreach (var values in levels.Select(nodes => nodes.Where(x => x != null).Select(x => x.val)))
+        {
+            result.Add(oddLevel ? values.ToList() : values.Reverse().ToList());
+            oddLevel = !oddLevel;
+        }
+
+        return result;
+    }
+}
