c008c7f4390ab817d780cd3a2307cc90e68da595
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-21 21:35:29 +1000

    540. Single Element in a Sorted Array

---
+++ UnitTests/S00540Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00540Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00540();
+        sut.SingleNonDuplicate(new[] {1, 1, 2, 3, 3, 4, 4, 8, 8}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00540();
+        sut.SingleNonDuplicate(new[] {3,3,7,7,10,11,11}).Should().Be(10);
+    }
+}
