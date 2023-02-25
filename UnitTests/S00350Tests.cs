f1ee7c8c6c458294d18a75d6d95828e36a2e4593
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-25 18:03:17 +1000

    350. Intersection of Two Arrays II

---
+++ UnitTests/S00350Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00350Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00350();
+        sut.Intersect(new[] {1, 2, 2, 1}, new[] {2, 2}).Should().Equal(2, 2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00350();
+        sut.Intersect(new[] {4, 9, 5}, new[] {9, 4, 9, 8, 4}).OrderBy(x => x).Should().Equal(4, 9);
+    }
+}
