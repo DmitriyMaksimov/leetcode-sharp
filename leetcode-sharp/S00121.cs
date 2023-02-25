a2df76f0eafc9ddee0ce0f0af88fddbb5ced7665
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-25 12:06:12 +1000

    121. Best Time to Buy and Sell Stock

---
+++ leetcode-sharp/S00121.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 121. Best Time to Buy and Sell Stock
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+public class S00121
+{
+    public int MaxProfit(int[] prices)
+    {
+        var cheapestPrice = int.MaxValue;
+        var maxProfit = 0;
+
+        foreach (var currentPrice in prices)
+        {
+            if (currentPrice < cheapestPrice)
+            {
+                cheapestPrice = currentPrice;
+            }
+            else
+            {
+                maxProfit = Math.Max(maxProfit, currentPrice - cheapestPrice);
+            }
+        }
+        
+        return maxProfit;
+    }
+}
