8d50427254341dde66fbe61b232cc50d1c622b29
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-25 12:36:53 +1000

    88. Merge Sorted Array

---
+++ UnitTests/S00088Tests.cs
@@ -0,0 +1,34 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00088Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00088();
+        var nums1 = new[] {1, 2, 3, 0, 0, 0};
+        sut.Merge(nums1, 3, new[] {2, 5, 6}, 3);
+        nums1.Should().Equal(1, 2, 2, 3, 5, 6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00088();
+        var nums1 = new[] {1};
+        sut.Merge(nums1, 1, Array.Empty<int>(), 0);
+        nums1.Should().Equal(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00088();
+        var nums1 = new[] {0};
+        sut.Merge(nums1, 0, new[] {1}, 1);
+        nums1.Should().Equal(1);
+    }
+}
