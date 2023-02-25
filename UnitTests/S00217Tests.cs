69b9c44efb20208a8208f385473479b59aeed61f
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-25 17:44:35 +1000

    217. Contains Duplicate

---
+++ UnitTests/S00217Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00217Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00217();
+        sut.ContainsDuplicate(new[] {1, 2, 3, 1}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00217();
+        sut.ContainsDuplicate(new[] {1, 2, 3, 4}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00217();
+        sut.ContainsDuplicate(new[] {1,1,1,3,3,4,3,2,4,2}).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/S00217.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 217. Contains Duplicate
+// https://leetcode.com/problems/contains-duplicate/
+public class S00217
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var hashSet = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            if (hashSet.Contains(num))
+            {
+                return true;
+            }
+
+            hashSet.Add(num);
+        }
+        
+        return false;
+    }
+}
