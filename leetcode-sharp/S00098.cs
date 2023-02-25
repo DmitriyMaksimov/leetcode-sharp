8da475cc63e33510b673c68e954f4fcba3ab5c66
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-25 21:23:25 +1000

    98. Validate Binary Search Tree

---
+++ leetcode-sharp/S00098.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 98. Validate Binary Search Tree
+// https://leetcode.com/problems/validate-binary-search-tree/
+public class S00098
+{
+    private TreeNode? _previous = null;
+    
+    public bool IsValidBST(TreeNode? root)
+    {
+        if (root == null) return true;
+        if (!IsValidBST(root.left))
+        {
+            return false;
+        }
+
+        if (_previous != null && root.val <= _previous.val)
+        {
+            return false;
+        }
+
+        _previous = root;
+
+        return IsValidBST(root.right);
+    }
+}
