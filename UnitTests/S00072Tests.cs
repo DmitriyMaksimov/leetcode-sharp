1a4544d1b0873c9ffd11d24a16f5fc9231c467e1
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-26 11:04:31 +1000

    72. Edit Distance

---
+++ UnitTests/S00072Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00072Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00072();
+        sut.MinDistance("horse", "ros").Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00072();
+        sut.MinDistance("intention", "execution").Should().Be(5);
+    }
+}
