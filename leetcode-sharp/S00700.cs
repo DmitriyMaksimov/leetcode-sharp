99b705d708c9dc1d1e17907d129b5f606135c32f
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-26 19:19:57 +1000

    700. Search in a Binary Search Tree

---
+++ leetcode-sharp/S00700.cs
@@ -0,0 +1,13 @@
+﻿namespace leetcode_sharp;
+
+// 700. Search in a Binary Search Tree
+// https://leetcode.com/problems/search-in-a-binary-search-tree/
+public class S00700
+{
+    public TreeNode? SearchBST(TreeNode? root, int val)
+    {
+        if (root == null) return null;
+
+        return root.val == val ? root : SearchBST(root.val > val ? root.left : root.right, val);
+    }
+}
