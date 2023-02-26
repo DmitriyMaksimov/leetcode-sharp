86aecb8507b197645d31749a991689cb6d02dc2b
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-26 13:23:32 +1000

    2248. Intersection of Multiple Arrays

---
+++ leetcode-sharp/S02248.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 2248. Intersection of Multiple Arrays
+// https://leetcode.com/problems/intersection-of-multiple-arrays/
+public class S02248
+{
+    public IList<int> Intersection(int[][] nums)
+    {
+        var counts = new Dictionary<int, int>();
+        
+        foreach (var arr in nums)
+        {
+            foreach (var n in arr)
+            {
+                counts[n] = counts.GetValueOrDefault(n) + 1;
+            }
+        }
+        
+        return counts.Where(x => x.Value == nums.Length).Select(x => x.Key).OrderBy(x => x).ToArray();
+    }
+}
