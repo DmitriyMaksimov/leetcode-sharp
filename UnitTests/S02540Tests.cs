a9a8ae61448b154b4290f4e83911a0f247cd7dc9
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-26 13:09:31 +1000

    2540. Minimum Common Value

---
+++ UnitTests/S02540Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02540Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02540();
+        sut.GetCommon(new[] {1, 2, 3}, new[] {2, 4}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02540();
+        sut.GetCommon(new[] {1, 2, 3, 6}, new[] {2, 3, 4, 5}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/S02540.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 2540. Minimum Common Value
+// https://leetcode.com/problems/minimum-common-value/
+public class S02540
+{
+    public int GetCommon(int[] nums1, int[] nums2)
+    {
+        foreach (var n in nums1)
+        {
+            if (Array.BinarySearch(nums2, n) >= 0)
+            {
+                return n;
+            }
+        }
+
+        return -1;
+    }
+}
