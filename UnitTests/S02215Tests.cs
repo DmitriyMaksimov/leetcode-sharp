ae3fbcab2a0fff0f5e1aaa4ff76f66b5426fa52d
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-26 13:33:36 +1000

    2215. Find the Difference of Two Arrays

---
+++ UnitTests/S02215Tests.cs
@@ -0,0 +1,27 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02215Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02215();
+        var list = sut.FindDifference(new[] {1, 2, 3}, new[] {2, 4, 6});
+        list.Should().HaveCount(2);
+        list[0].Should().Equal(1, 3);
+        list[1].Should().Equal(4, 6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02215();
+        var list = sut.FindDifference(new[] {1, 2, 3, 3}, new[] {1, 1, 2, 2});
+        list.Should().HaveCount(2);
+        list[0].Should().Equal(3);
+        list[1].Should().BeEmpty();
+    }
+}
