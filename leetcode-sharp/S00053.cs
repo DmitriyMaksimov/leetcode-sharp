afc0bf4c0690abe90835c68cdbc8d3f2039f2fbc
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-26 11:22:06 +1000

    53. Maximum Subarray

---
+++ leetcode-sharp/S00053.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 53. Maximum Subarray
+// https://leetcode.com/problems/maximum-subarray/
+public class S00053
+{
+    public int MaxSubArray(int[] nums)
+    {
+        var maxSum = int.MinValue;
+        var currentSum = 0;
+        
+        foreach (var num in nums)
+        {
+            currentSum = Math.Max(num, currentSum + num);
+            maxSum = Math.Max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
