96d0a22fe12978c995b68b1951bf1dab2aae18dd
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-26 14:44:43 +1000

    566. Reshape the Matrix

---
+++ UnitTests/S00566Tests.cs
@@ -0,0 +1,38 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00566Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00566();
+        var matrix = sut.MatrixReshape(new[] {new[] {1, 2}, new[] {3, 4}}, 1, 4);
+        matrix.Should().HaveCount(1);
+        matrix[0].Should().Equal(1, 2, 3, 4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00566();
+        var matrix = sut.MatrixReshape(new[] {new[] {1, 2}, new[] {3, 4}}, 2, 4);
+        matrix.Should().HaveCount(2);
+        matrix[0].Should().Equal(1, 2);
+        matrix[1].Should().Equal(3, 4);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00566();
+        var matrix = sut.MatrixReshape(new[] {new[] {1, 2}, new[] {3, 4}}, 4, 1);
+        matrix.Should().HaveCount(4);
+        matrix[0].Should().Equal(1);
+        matrix[1].Should().Equal(2);
+        matrix[2].Should().Equal(3);
+        matrix[3].Should().Equal(4);
+    }
+}
---
+++ leetcode-sharp/S00566.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 566. Reshape the Matrix
+// https://leetcode.com/problems/reshape-the-matrix/
+public class S00566
+{
+    public int[][] MatrixReshape(int[][] mat, int r, int c)
+    {
+        var m = mat.Length;
+        var n = mat[0].Length;
+
+        if (m * n != r * c)
+        {
+            return mat;
+        }
+        
+        var result = new int[r][];
+
+        for (var i = 0; i < r; i++)
+        {
+            result[i] = new int[c];
+            for (var j = 0; j < c; j++)
+            {
+                var globalIndex = i * c + j;
+                var row = globalIndex / n;
+                var col = globalIndex % n;
+                result[i][j] = mat[row][col];
+            }
+        }
+
+        return result;
+    }
+}
