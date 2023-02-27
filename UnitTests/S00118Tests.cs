64ca8fef743db47a7873ab9f7218aa9b58214294
Dmitriy <dmitriy.maksimov@workday.com>
2023-02-27 22:11:12 +1000

    118. Pascal's Triangle

---
+++ UnitTests/S00118Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00118Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00118();
+        var list = sut.Generate(5);
+        list.Should().HaveCount(5);
+        list[0].Should().Equal(1);
+        list[1].Should().Equal(1, 1);
+        list[2].Should().Equal(1, 2, 1);
+        list[3].Should().Equal(1, 3, 3, 1);
+        list[4].Should().Equal(1, 4, 6, 4, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00118();
+        var list = sut.Generate(1);
+        list.Should().HaveCount(1);
+        list[0].Should().Equal(1);
+    }
+}
---
+++ leetcode-sharp/S00118.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 118. Pascal's Triangle
+// https://leetcode.com/problems/pascals-triangle/
+public class S00118
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        var list = new List<IList<int>> {new List<int> {1}};
+
+        while (--numRows > 0)
+        {
+            var row = new List<int>();
+            var prevRow = list.Last();
+            
+            for (var i = 0; i < prevRow.Count + 1; i++)
+            {
+                if (i == 0 || prevRow.Count == 1 || i == prevRow.Count)
+                {
+                    row.Add(1);
+                }
+                else
+                {
+                    row.Add(prevRow[i - 1] + prevRow[i]);
+                }
+            }
+
+            list.Add(row);
+        }
+        
+        return list;
+    }
+}
