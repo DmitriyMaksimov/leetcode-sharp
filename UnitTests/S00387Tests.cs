5cd9cbd7e63311a8bbd2dea5301ece891806416c
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-02 19:17:07 +1000

    387. First Unique Character in a String

---
+++ UnitTests/S00387Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00387Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00387();
+        sut.FirstUniqChar("leetcode").Should().Be(0);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00387();
+        sut.FirstUniqChar("loveleetcode").Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00387();
+        sut.FirstUniqChar("aabb").Should().Be(-1);
+    }
+}
