9a149d76e96c5edfa3071bf959c1358c7d2a90d3
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-03 19:30:51 +1000

    203. Remove Linked List Elements

---
+++ leetcode-sharp/S00203.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode_sharp;
+
+// 203. Remove Linked List Elements
+// https://leetcode.com/problems/remove-linked-list-elements/
+public class S00203
+{
+    public ListNode? RemoveElements(ListNode? head, int val)
+    {
+        ListNode? newHead = null;
+        ListNode? currentNode = null;
+
+        while (head != null)
+        {
+            if (head.val != val)
+            {
+                if (newHead == null)
+                {
+                    newHead = head;
+                    currentNode = head;
+                }
+                else
+                {
+                    currentNode.next = head;
+                    currentNode = currentNode.next;
+                }
+            }
+
+            head = head.next;
+        }
+
+        if (currentNode != null)
+        {
+            currentNode.next = null;
+        }
+        
+        return newHead;
+    }
+}
