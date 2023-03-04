b3e68b8748f2b97e14bf8fcbb758416c57e24552
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-04 19:02:02 +1000

    141. Linked List Cycle

---
+++ leetcode-sharp/S00141.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 141. Linked List Cycle
+// https://leetcode.com/problems/linked-list-cycle/
+public class S00141
+{
+    public bool HasCycle(ListNode? head)
+    {
+        var fastLoop = head;
+
+        while (fastLoop?.next != null)
+        {
+            fastLoop = fastLoop.next.next;
+            head = head!.next;
+            if (fastLoop == head)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
