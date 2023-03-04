1384c92ccd8221939221d983e2640fa4f71f6263
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-04 13:17:14 +1000

    83. Remove Duplicates from Sorted List

---
+++ leetcode-sharp/S00083.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode_sharp;
+
+// 83. Remove Duplicates from Sorted List
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+public class S00083
+{
+    public ListNode? DeleteDuplicates(ListNode? head)
+    {
+        if (head == null)
+        {
+            return null;
+        }
+        
+        var newHead = head;
+        var currentNode = head;
+        var prevVal = head.val;
+        head = head.next;
+
+        while (head != null)
+        {
+            if (head.val != prevVal)
+            {
+                currentNode.next = head;
+                currentNode = currentNode.next;
+                prevVal = head.val;
+            }
+
+            head = head.next;
+        }
+
+        if (currentNode != null)
+        {
+            currentNode.next = null;
+        }
+        
+        return newHead;
+    }
+}
