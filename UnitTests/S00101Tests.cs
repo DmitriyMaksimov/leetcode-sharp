8c289ad76334de63747f46cf20d4c8ecb33a5abd
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-06 19:56:24 +1000

    101. Symmetric Tree

---
+++ UnitTests/S00101Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00101Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00101();
+        sut.IsSymmetric(TreeNode.Parse("[1,2,2,3,4,4,3]")).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00101();
+        sut.IsSymmetric(TreeNode.Parse("[1,2,2,null,3,null,3]")).Should().BeFalse();
+    }
+}
