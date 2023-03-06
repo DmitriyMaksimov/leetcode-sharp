37ac21266f3cf0673d03a2db47100fd8bb1c8871
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-06 17:37:40 +1000

    1539. Kth Missing Positive Number

---
+++ UnitTests/S01539Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01539Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01539();
+        sut.FindKthPositive(new[] {2, 3, 4, 7, 11}, 5).Should().Be(9);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01539();
+        sut.FindKthPositive(new[] {1,2,3,4}, 2).Should().Be(6);
+    }
+}
