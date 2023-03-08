1b16bb5d167831b371153319d589b6751414ace2
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-08 17:13:56 +1000

    509. Fibonacci Number

---
+++ UnitTests/S00509Tests.cs
@@ -0,0 +1,27 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00509Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00509();
+        sut.Fib(2).Should().Be(1);
+    }
+    [Test]
+    public void T2()
+    {
+        var sut = new S00509();
+        sut.Fib(3).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00509();
+        sut.Fib(4).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/S00509.cs
@@ -0,0 +1,16 @@
+﻿namespace leetcode_sharp;
+
+// 509. Fibonacci Number
+// https://leetcode.com/problems/fibonacci-number/
+public class S00509
+{
+    public int Fib(int n)
+    {
+        return n switch
+        {
+            0 => 0,
+            1 => 1,
+            _ => Fib(n - 1) + Fib(n - 2)
+        };
+    }
+}
