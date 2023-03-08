173b7f057d91cc3c75b9e4eb9514e7ea1b7ce320
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-08 17:07:54 +1000

    701. Insert into a Binary Search Tree

---
+++ leetcode-sharp/S00701.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 701. Insert into a Binary Search Tree
+// https://leetcode.com/problems/insert-into-a-binary-search-tree/
+public class S00701
+{
+    public TreeNode InsertIntoBST(TreeNode? root, int val)
+    {
+        if (root == null)
+        {
+            return new TreeNode(val);
+        }
+
+        if (root.val > val)
+        {
+            root.left = InsertIntoBST(root.left, val);
+        }
+        else
+        {
+            root.right = InsertIntoBST(root.right, val);
+        }
+        
+        return root;
+    }
+}
