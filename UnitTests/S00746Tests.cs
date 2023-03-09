664a37cfce41888d68b1834b6f06e4b0eb4ba55c
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-09 18:40:29 +1000

    746. Min Cost Climbing Stairs

---
+++ UnitTests/S00746Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00746Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00746();
+        sut.MinCostClimbingStairs(new[] {10, 15, 20}).Should().Be(15);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00746();
+        sut.MinCostClimbingStairs(new[] {1, 100, 1, 1, 1, 100, 1, 1, 100, 1}).Should().Be(6);
+    }
+}
---
+++ leetcode-sharp/S00746.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 746. Min Cost Climbing Stairs
+// https://leetcode.com/problems/min-cost-climbing-stairs/
+public class S00746
+{
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        var n = cost.Length;
+        var dp = new int[n + 1];
+        
+        dp[n] = 0;
+        dp[n - 1] = cost[n - 1];
+
+        for (var i = n - 2; i >= 0; i--)
+        {
+            dp[i] = cost[i] + Math.Min(dp[i + 1], dp[i + 2]);
+        }
+        
+        return Math.Min(dp[0], dp[1]);
+    }
+}
