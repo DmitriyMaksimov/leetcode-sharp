921d33362f366f6d0bde8a769dc4ad5820a72a6d
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-09 18:21:04 +1000

    142. Linked List Cycle II

---
+++ leetcode-sharp/S00142.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcode_sharp;
+
+// 142. Linked List Cycle II
+// https://leetcode.com/problems/linked-list-cycle-ii/
+public class S00142
+{
+    public ListNode? DetectCycle(ListNode? head)
+    {
+        var slowPointer = head;
+        var fastPointer = head;
+        var loopExist = false;
+
+        while (slowPointer != null && fastPointer?.next != null)
+        {
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+
+            if (fastPointer == slowPointer)
+            {
+                loopExist = true;
+                break;
+            }
+        }
+
+        if (!loopExist)
+        {
+            return null;
+        }
+        
+        // reset slow pointer to head and loop again
+        slowPointer = head;
+        while (slowPointer != fastPointer)
+        {
+            slowPointer = slowPointer!.next;
+            fastPointer = fastPointer!.next;
+        }
+ 
+        return slowPointer;
+    }
+}
