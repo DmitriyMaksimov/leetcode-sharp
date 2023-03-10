2166bc507c2267c8cd262256b6d67b1879d11627
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-10 18:40:43 +1000

    213. House Robber II

---
+++ UnitTests/S00213Tests.cs
@@ -0,0 +1,42 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00213Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00213();
+        sut.Rob(new[] {1, 2, 3, 1}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00213();
+        sut.Rob(new[] {2, 3, 2}).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00213();
+        sut.Rob(new[] {1, 2, 3}).Should().Be(3);
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S00213();
+        sut.Rob(new[] {1}).Should().Be(1);
+    }
+
+    [Test]
+    public void T5()
+    {
+        var sut = new S00213();
+        sut.Rob(new[] {1, 2, 1, 1}).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/S00213.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 213. House Robber II
+// https://leetcode.com/problems/house-robber-ii/
+public class S00213
+{
+    public int Rob(int[] nums)
+    {
+        if (nums.Length == 1)
+        {
+            return nums[0];
+        }
+
+        return Math.Max(findMax(nums, 0, nums.Length - 2), findMax(nums, 1, nums.Length - 1));
+    }
+
+    private static int findMax(IReadOnlyList<int> nums, int minIndex, int maxIndex)
+    {
+        var include = 0;
+        var exclude = 0;
+        
+        for (var j = minIndex; j <= maxIndex; j++)
+        {
+            var i = include;
+            var e = exclude;
+            include = e + nums[j];
+            exclude = Math.Max(e, i);
+        }
+
+        return Math.Max(include, exclude);
+    }
+}
