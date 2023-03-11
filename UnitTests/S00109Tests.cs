5719bffd92771be1b50d898a6891bd886b861cbc
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-11 12:53:20 +1000

    109. Convert Sorted List to Binary Search Tree

---
+++ UnitTests/S00109Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00109Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00109();
+        sut.SortedListToBST(ListNode.Parse("[-10,-3,0,5,9]")).Should().BeEquivalentTo(TreeNode.Parse("[0,-3,9,-10,null,5]"));
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00109();
+        sut.SortedListToBST(ListNode.Parse("[]")).Should().BeNull();
+    }
+}
---
+++ leetcode-sharp/S00109.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcode_sharp;
+
+// 109. Convert Sorted List to Binary Search Tree
+// https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+public class S00109
+{
+    public TreeNode? SortedListToBST(ListNode? head)
+    {
+        if (head == null)
+        {
+            return null;
+        }
+
+        var list = new List<int>();
+        while (head != null)
+        {
+            list.Add(head.val);
+            head = head.next;
+        }
+        
+        return arrayToBst(list, 0, list.Count - 1);
+    }
+
+    private static TreeNode? arrayToBst(IReadOnlyList<int> nums, int lo, int hi)
+    {
+        if (lo > hi)
+        {
+            return null;
+        }
+        
+        var mid = lo + (hi - lo) / 2;
+        var root = new TreeNode(nums[mid])
+        {
+            left = arrayToBst(nums, lo, mid - 1),
+            right = arrayToBst(nums, mid + 1, hi)
+        };
+
+        return root;
+    }
+}
