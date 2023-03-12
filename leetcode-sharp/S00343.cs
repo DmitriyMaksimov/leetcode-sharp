6c71653cbfd53e6330c6787d224da2c599ac4666
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-12 13:20:06 +1000

    343. Integer Break

---
+++ leetcode-sharp/S00343.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 343. Integer Break
+// https://leetcode.com/problems/integer-break/
+public class S00343
+{
+    public int IntegerBreak(int n)
+    {
+        if (n is 2 or 3)
+        {
+            return n - 1;
+        }
+
+        // Use as many 3 as we can
+        var res = 1;
+        while (n > 4)
+        {
+            n -= 3;
+            res *= 3;
+        }
+
+        return n * res;
+    }
+}
