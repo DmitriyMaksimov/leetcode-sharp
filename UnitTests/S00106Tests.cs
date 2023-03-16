1aa875a71a3a7ff4deb3eb6ba71d582a81b90606
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-16 20:35:55 +1000

    106. Construct Binary Tree from Inorder and Postorder Traversal

---
+++ UnitTests/S00106Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00106Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00106();
+        sut.BuildTree(new[] {9, 3, 15, 20, 7}, new[] {9, 15, 7, 20, 3}).Should().BeEquivalentTo(TreeNode.Parse("[3,9,20,null,null,15,7]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00106();
+        sut.BuildTree(new[] {-1}, new[] {-1}).Should().BeEquivalentTo(TreeNode.Parse("[-1]"));
+    }
+}
