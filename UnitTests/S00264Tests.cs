9655e2b604f05fce9dcdfa94448ffec4ef3eae7c
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-17 20:20:31 +1000

    264. Ugly Number II

---
+++ UnitTests/S00264Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00264Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00264();
+        sut.NthUglyNumber(10).Should().Be(12);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00264();
+        sut.NthUglyNumber(1).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/S00264.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 264. Ugly Number II
+// https://leetcode.com/problems/ugly-number-ii/
+public class S00264
+{
+    public int NthUglyNumber(int n)
+    {
+        var uglyNumbers = new List<int>(n) {1};
+        var index2 = 0;
+        var index3 = 0;
+        var index5 = 0;
+
+        for (var i = 1; i < n; i++)
+        {
+            var uglyNumber2 = uglyNumbers[index2] * 2;
+            var uglyNumber3 = uglyNumbers[index3] * 3;
+            var uglyNumber5 = uglyNumbers[index5] * 5;
+            
+            uglyNumbers.Add(min(uglyNumber2, uglyNumber3, uglyNumber5));
+            
+            if (uglyNumbers[i] == uglyNumber2) index2++;
+            if (uglyNumbers[i] == uglyNumber3) index3++;
+            if (uglyNumbers[i] == uglyNumber5) index5++;
+        }
+
+        return uglyNumbers[n - 1];
+    }
+
+    private static int min(params int[] numbers)
+    {
+        var result = numbers[0];
+
+        for (var i = 1; i < numbers.Length; i++)
+        {
+            result = Math.Min(result, numbers[i]);
+        }
+
+        return result;
+    }
+}
