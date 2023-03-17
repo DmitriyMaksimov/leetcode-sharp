e5935a13f0bef515092ba1100ab3531d1ada3685
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-17 19:06:13 +1000

    152. Maximum Product Subarray

---
+++ UnitTests/S00152Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00152Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00152();
+        sut.MaxProduct(new[] {2, 3, -2, 4}).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00152();
+        sut.MaxProduct(new[] {-2, 0, -1}).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00152();
+        sut.MaxProduct(new[] {-2, 3, -4}).Should().Be(24);
+    }
+}
