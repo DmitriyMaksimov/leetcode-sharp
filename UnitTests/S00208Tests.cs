ccb4d701013ac0ea2a0999c1180dd4dea8210eb1
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-17 18:52:31 +1000

    208. Implement Trie (Prefix Tree)

---
+++ UnitTests/S00208Tests.cs
@@ -0,0 +1,19 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00208Tests
+{
+    [Test]
+    public void T1()
+    {
+        var trie = new S00208.Trie();
+        trie.Insert("apple");
+        trie.Search("apple").Should().BeTrue();
+        trie.Search("app").Should().BeFalse();
+        trie.StartsWith("app").Should().BeTrue();
+        trie.Insert("app");
+        trie.Search("app").Should().BeTrue();
+    }
+}
