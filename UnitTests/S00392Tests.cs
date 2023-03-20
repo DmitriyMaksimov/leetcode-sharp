8f3099df0c557a07e4cd8428408d632524df70b0
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-20 16:07:44 +1000

    392. Is Subsequence

---
+++ UnitTests/S00392Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00392Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00392();
+        sut.IsSubsequence("abc", "ahbgdc").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00392();
+        sut.IsSubsequence("axc", "ahbgdc").Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/S00392.cs
@@ -0,0 +1,36 @@
+﻿namespace leetcode_sharp;
+
+// 392. Is Subsequence
+// https://leetcode.com/problems/is-subsequence/
+public class S00392
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (s.Length > t.Length)
+        {
+            return false;
+        }
+
+        var index = 0;
+        foreach (var ch in s)
+        {
+            var found = false;
+            for (var j = index; j < t.Length; j++)
+            {
+                if (ch == t[j])
+                {
+                    index = j + 1;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
