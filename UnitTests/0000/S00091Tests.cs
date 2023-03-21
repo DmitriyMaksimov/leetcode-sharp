eb39c1de0732808c5527138a3044c62acd022d5a
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-21 20:06:20 +1000

    91. Decode Ways

---
+++ UnitTests/0000/S00091Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00091Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00091();
+        sut.NumDecodings("12").Should().Be(2);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00091();
+        sut.NumDecodings("226").Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00091();
+        sut.NumDecodings("06").Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0000/S00091.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 91. Decode Ways
+// https://leetcode.com/problems/decode-ways/
+public class S00091
+{
+    public int NumDecodings(string s)
+    {
+        var n = s.Length;
+        if (n == 0)
+        {
+            return 0;
+        }
+
+        var dp = new int[n + 1];
+        dp[n] = 1;
+        dp[n - 1] = s[n - 1] != '0' ? 1 : 0;
+
+        for (var i = n - 2; i >= 0; i--)
+        {
+            if (s[i] != '0')
+            {
+                dp[i] = int.Parse(s.Substring(i, 2)) <= 26 ? dp[i + 1] + dp[i + 2] : dp[i + 1];
+            }
+        }
+
+        return dp[0];
+    }
+}
