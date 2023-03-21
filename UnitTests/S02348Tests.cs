6b0090491feca43d6f64525859d9db8523ef3c12
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-21 18:01:54 +1000

    2348. Number of Zero-Filled Subarrays

---
+++ UnitTests/S02348Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02348Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02348();
+        sut.ZeroFilledSubarray(new[] {1, 3, 0, 0, 2, 0, 0, 4}).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02348();
+        sut.ZeroFilledSubarray(new[] {0, 0, 0, 2, 0, 0}).Should().Be(9);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02348();
+        sut.ZeroFilledSubarray(new[] {2, 10, 2019}).Should().Be(0);
+    }
+}
