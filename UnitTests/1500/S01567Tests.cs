7a99d988484c7ab21f58c654ed53ae8887d6a6b6
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-21 19:00:55 +1000

    1567. Maximum Length of Subarray With Positive Product

---
+++ UnitTests/1500/S01567Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01567Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01567();
+        sut.GetMaxLen(new []{1,-2,-3,4}).Should().Be(4);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S01567();
+        sut.GetMaxLen(new []{0,1,-2,-3,-4}).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01567();
+        sut.GetMaxLen(new []{-1,-2,-3,0,1}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/1500/S01567.cs
@@ -0,0 +1,39 @@
+﻿namespace leetcode_sharp;
+
+// 1567. Maximum Length of Subarray With Positive Product
+// https://leetcode.com/problems/maximum-length-of-subarray-with-positive-product/
+public class S01567
+{
+    public int GetMaxLen(int[] nums)
+    {
+        var numberOfPositive = 0;
+        var numberOfNegative = 0;
+        var result = 0;
+        
+        foreach (var x in nums)
+        {
+            switch (x)
+            {
+                case 0:
+                    numberOfPositive = 0;
+                    numberOfNegative = 0;
+                    break;
+                case > 0:
+                    numberOfPositive++;
+                    numberOfNegative = numberOfNegative == 0 ? 0 : numberOfNegative + 1;
+                    break;
+                default:
+                {
+                    var temp = numberOfPositive;
+                    numberOfPositive = numberOfNegative == 0 ? 0 : numberOfNegative + 1;
+                    numberOfNegative = temp + 1;
+                    break;
+                }
+            }
+
+            result = Math.Max(result, numberOfPositive);
+        }
+
+        return result;
+    }
+}
