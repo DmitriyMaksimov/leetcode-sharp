7fed22aef0fac7c55a7548123df23d6b8bc601b1
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-22 19:19:29 +1000

    139. Word Break

---
+++ UnitTests/0100/S00139Tests.cs
@@ -0,0 +1,36 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00139Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00139();
+        sut.WordBreak("leetcode", new List<string> {"leet", "code"}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00139();
+        sut.WordBreak("applepenapple", new List<string> {"apple", "pen"}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00139();
+        sut.WordBreak("catsandog", new List<string> {"cats", "dog", "sand", "and", "cat"}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S00139();
+        sut.WordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
+            new List<string> {"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0100/S00139.cs
@@ -0,0 +1,39 @@
+﻿namespace leetcode_sharp;
+
+// 139. Word Break
+// https://leetcode.com/problems/word-break/
+public class S00139
+{
+    private HashSet<string> _hashSet;
+    private readonly Dictionary<int, bool> _memo = new();
+
+    public bool WordBreak(string s, IList<string> wordDict)
+    {
+        _hashSet = wordDict.ToHashSet();
+
+        return wordBreak(s, 0);
+    }
+
+    private bool wordBreak(string s, int startPosition)
+    {
+        if (startPosition >= s.Length)
+        {
+            return true;
+        }
+
+        if (_memo.TryGetValue(startPosition, out var result))
+        {
+            return result;
+        }
+        
+        for (var i = startPosition + 1; i <= s.Length; i++)
+        {
+            if (_hashSet.Contains(s[startPosition..i]) && wordBreak(s, i))
+            {
+                return _memo[startPosition] = true;
+            }
+        }
+
+        return _memo[startPosition] = false;
+    }
+}
