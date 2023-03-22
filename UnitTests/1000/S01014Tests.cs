5541041910f00a50980cc0462dbaec394fbbac87
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-22 20:28:23 +1000

    1014. Best Sightseeing Pair

---
+++ UnitTests/1000/S01014Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01014Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01014();
+        sut.MaxScoreSightseeingPair(new[] {8, 1, 5, 2, 6}).Should().Be(11);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01014();
+        sut.MaxScoreSightseeingPair(new[] {1, 2}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/1000/S01014.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 1014. Best Sightseeing Pair
+// https://leetcode.com/problems/best-sightseeing-pair/
+public class S01014
+{
+    public int MaxScoreSightseeingPair(int[] values)
+    {
+        var currentMax = 0;
+        var result = 0;
+        
+        foreach (var value in values)
+        {
+            result = Math.Max(result, currentMax + value);
+            currentMax = Math.Max(currentMax, value) - 1;
+        }
+        
+        return result;
+    }
+}
