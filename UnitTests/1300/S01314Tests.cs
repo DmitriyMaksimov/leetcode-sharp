a9a6ba4f3bce236ec8050e913337cff435da40d2
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-23 19:44:59 +1000

    1314. Matrix Block Sum

---
+++ UnitTests/1300/S01314Tests.cs
@@ -0,0 +1,23 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01314Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01314();
+        sut.MatrixBlockSum(new[] {new[] {1, 2, 3}, new[] {4, 5, 6}, new[] {7, 8, 9}}, 1).Should()
+            .BeEquivalentTo(new[] {new[] {12, 21, 16}, new[] {27, 45, 33}, new[] {24, 39, 28}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01314();
+        sut.MatrixBlockSum(new[] {new[] {1, 2, 3}, new[] {4, 5, 6}, new[] {7, 8, 9}}, 2).Should()
+            .BeEquivalentTo(new[] {new[] {45, 45, 45}, new[] {45, 45, 45}, new[] {45, 45, 45}});
+    }
+}
