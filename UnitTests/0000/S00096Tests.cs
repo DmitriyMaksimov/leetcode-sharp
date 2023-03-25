e31f60f9b9c48461c91d9b1d9a882b3a51ff070c
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-25 17:48:45 +1000

    96. Unique Binary Search Trees

---
+++ UnitTests/0000/S00096Tests.cs
@@ -0,0 +1,19 @@
+﻿namespace UnitTests;
+
+[TestFixture]
+public class S00096Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00096();
+        sut.NumTrees(3).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00096();
+        sut.NumTrees(1).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/0000/S00096.cs
@@ -0,0 +1,24 @@
+﻿namespace UnitTests;
+
+// 96. Unique Binary Search Trees
+// https://leetcode.com/problems/unique-binary-search-trees/
+public class S00096
+{
+    public int NumTrees(int n)
+    {
+        if (n is 0 or 1)
+        {
+            return 1;
+        }
+
+        // Catalan number
+        var sum = 0;
+        
+        for (var i = 1; i <= n; i++)
+        {
+            sum += NumTrees(i - 1) * NumTrees(n - i);
+        }
+
+        return sum;
+    }
+}
