be6a3c2fdfed91b19588e5bdd5b3a01a3b473f1e
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-25 13:49:12 +1000

    714. Best Time to Buy and Sell Stock with Transaction Fee

---
+++ UnitTests/0700/S00714Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00714Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00714();
+        sut.MaxProfit(new[] {1, 3, 2, 8, 4, 9}, 2).Should().Be(8);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00714();
+        sut.MaxProfit(new[] {1, 3, 7, 5, 10, 3}, 3).Should().Be(6);
+    }
+}
