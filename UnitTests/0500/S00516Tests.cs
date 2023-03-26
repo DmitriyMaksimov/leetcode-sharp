ebb1449e4d071fd9c096e5d7eac0ae5f272e2164
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-26 11:39:13 +1000

    516. Longest Palindromic Subsequence

---
+++ UnitTests/0500/S00516Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00516Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00516();
+        sut.LongestPalindromeSubseq("bbbab").Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00516();
+        sut.LongestPalindromeSubseq("cbbd").Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/0500/S00516.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 516. Longest Palindromic Subsequence
+// https://leetcode.com/problems/longest-palindromic-subsequence/
+public class S00516
+{
+    private readonly Dictionary<(int i, int j), int> _cache = new();
+
+    public int LongestPalindromeSubseq(string s)
+    {
+        var reversedString = string.Join("", s.Reverse());
+        return getLCS(s, 0, reversedString, 0);
+    }
+    
+    private int getLCS(string a, int i, string b, int j)
+    {
+        if (i >= a.Length || j >= b.Length)
+        {
+            return 0;
+        }
+
+        if (_cache.TryGetValue((i, j), out var value))
+        {
+            return value;
+        }
+
+        if (a[i] == b[j])
+        {
+            return _cache[(i, j)] = 1 + getLCS(a, i + 1, b, j + 1);
+        }
+
+        return _cache[(i, j)] = Math.Max(getLCS(a, i + 1, b, j), getLCS(a, i, b, j + 1));
+    }
+
+}
