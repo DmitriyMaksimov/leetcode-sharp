20204085570f414cde4087a0e8a9937fc2ec5361
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-26 11:25:26 +1000

    2360. Longest Cycle in a Graph

---
+++ UnitTests/2300/S02360Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02360Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02360();
+        sut.LongestCycle(new[] {3,3,4,2,3 }).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02360();
+        sut.LongestCycle(new[] {2,-1,3,1 }).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/2300/S02360.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode_sharp;
+
+// 2360. Longest Cycle in a Graph
+// https://leetcode.com/problems/longest-cycle-in-a-graph/
+public class S02360
+{
+    public int LongestCycle(int[] edges)
+    {
+        var result = -1;
+        var memo = new List<(int, int)>(Enumerable.Repeat((-1, -1), edges.Length));
+
+        for (var i = 0; i < edges.Length; ++i)
+        {
+            var currentDistance = 0;
+            
+            for (var j = i; j != -1; j = edges[j])
+            {
+                var (distance, startIndex) = memo[j];
+                if (distance == -1)
+                {
+                    memo[j] = (currentDistance++, i);
+                }
+                else
+                {
+                    if (startIndex == i)
+                    {
+                        // Found cycle => record the result
+                        result = Math.Max(result, currentDistance - distance);
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
