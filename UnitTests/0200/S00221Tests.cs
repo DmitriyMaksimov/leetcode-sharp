430b558432f82f9f4eeee083f02c2e81e4494348
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-27 19:17:34 +1000

    221. Maximal Square

---
+++ UnitTests/0200/S00221Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00221Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00221();
+        sut.MaximalSquare(new[] {new[] {'1', '0', '1', '0', '0'}, new[] {'1', '0', '1', '1', '1'}, new[] {'1', '1', '1', '1', '1'}, new[] {'1', '0', '0', '1', '0'}})
+            .Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00221();
+        sut.MaximalSquare(new[] {new[] {'0', '1'}, new[] {'1', '0'}}).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00221();
+        sut.MaximalSquare(new[] {new[] {'0'}}).Should().Be(0);
+    }
+}
