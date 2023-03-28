c149d9aae440439217a76c9c0f7307d0dc900877
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-28 12:37:55 +1000

    377. Combination Sum IV

---
+++ UnitTests/0300/S00377Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp._0300;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00377Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00377();
+        sut.CombinationSum4(new[] {1, 2, 3}, 4).Should().Be(7);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00377();
+        sut.CombinationSum4(new[] {9}, 3).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00377();
+        sut.CombinationSum4(new[] {4, 2, 1}, 32).Should().Be(39882198);
+    }
+}
---
+++ leetcode-sharp/0300/S00377.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp._0300;
+
+// 377. Combination Sum IV
+// https://leetcode.com/problems/combination-sum-iv/
+public class S00377
+{
+    private readonly Dictionary<int, int> _dp = new();
+    
+    public int CombinationSum4(int[] nums, int target)
+    {
+        _dp[0] = 1;
+        
+        return combinationSum4(nums, target);
+    }
+
+    private int combinationSum4(int[] nums, int target)
+    {
+        if (_dp.TryGetValue(target, out var val))
+        {
+            return val;
+        }
+        
+        return _dp[target] = nums.Where(num => target >= num).Sum(num => CombinationSum4(nums, target - num));
+    }
+}
