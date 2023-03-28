37c5766bfa1ee2febbbc41748d23865180f7fab9
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-28 12:13:31 +1000

    983. Minimum Cost For Tickets

---
+++ UnitTests/0900/S00983Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00983Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00983();
+        sut.MincostTickets(new[] {1, 4, 6, 7, 8, 20}, new[] {2, 7, 15}).Should().Be(11);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00983();
+        sut.MincostTickets(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31}, new[] {2, 7, 15}).Should().Be(17);
+    }
+}
---
+++ leetcode-sharp/0900/S00983.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 983. Minimum Cost For Tickets
+// https://leetcode.com/problems/minimum-cost-for-tickets/
+public class S00983
+{
+    public int MincostTickets(int[] days, int[] costs)
+    {
+        var travelDays = days.ToHashSet();
+        var dp = new int[366];
+
+        for (var i = 1; i < 366; ++i)
+        {
+            if (!travelDays.Contains(i))
+            {
+                // If we don't need to travel this day, set the cost the same as previous day
+                dp[i] = dp[i - 1];
+            }
+            else
+            {
+                // Take minimum of 1 day, 7 days and 30 days tickets
+                dp[i] = Math.Min(Math.Min(dp[i - 1] + costs[0], dp[Math.Max(0, i - 7)] + costs[1]), dp[Math.Max(0, i - 30)] + costs[2]);
+            }
+        }
+
+        return dp.Last();
+    }
+}
