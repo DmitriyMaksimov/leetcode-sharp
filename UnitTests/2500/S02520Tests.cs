cc23425e8346849a8e4fe44c770759b188852517
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-29 23:13:07 +1000

    2520. Count the Digits That Divide a Number

---
+++ UnitTests/2500/S02520Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02520Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02520();
+        sut.CountDigits(7).Should().Be(1);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S02520();
+        sut.CountDigits(121).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02520();
+        sut.CountDigits(1248).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/2500/S02520.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 2520. Count the Digits That Divide a Number
+// https://leetcode.com/problems/count-the-digits-that-divide-a-number/
+public class S02520
+{
+    public int CountDigits(int num)
+    {
+        var count = 0;
+        var n = num;
+
+        while (n > 0)
+        {
+            var lastDigit = n % 10;
+            n /= 10;
+            if (num % lastDigit == 0)
+            {
+                ++count;
+            }
+        }
+        
+        return count;
+    }
+}
