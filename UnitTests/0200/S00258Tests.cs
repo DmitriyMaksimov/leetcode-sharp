927091facea887b046ca3e5b68199bb06c39fb05
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-29 21:11:24 +1000

    258. Add Digits

---
+++ UnitTests/0200/S00258Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00258Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00258();
+        sut.AddDigits(38).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00258();
+        sut.AddDigits(0).Should().Be(0);
+    }
+}
