21ff1342f6e9f4019fb68ffc54e3dd3cffa394c9
Dmitriy <dmitriy.maksimov@workday.com>
2023-03-31 18:58:27 +1000

    1444. Number of Ways of Cutting a Pizza

---
+++ UnitTests/1400/S01444Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01444Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01444();
+        sut.Ways(new[] {"A..", "AAA", "..."}, 3).Should().Be(3);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S01444();
+        sut.Ways(new[] {"A..", "AA.", "..."}, 3).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01444();
+        sut.Ways(new[] {"A..", "A..", "..."}, 1).Should().Be(1);
+    }
+}
