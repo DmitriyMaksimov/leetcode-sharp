1ded5a68b2d2578b0ac066a5509a59a35d95be92
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-04 21:16:26 +1000

    2405. Optimal Partition of String

---
+++ UnitTests/2400/S02405Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02405Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02405();
+        sut.PartitionString("abacaba").Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02405();
+        sut.PartitionString("ssssss").Should().Be(6);
+    }
+}
