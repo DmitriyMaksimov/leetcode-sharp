ae8f0a28b34a304846accb9d97744a08e6b459d7
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-05 18:58:30 +1000

    2439. Minimize Maximum of Array

---
+++ UnitTests/2400/S02439Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02439Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02439();
+        sut.MinimizeArrayValue(new[] { 3,7,1,6}).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02439();
+        sut.MinimizeArrayValue(new[] {10,1 }).Should().Be(10);
+    }
+}
