5a4254301eb094d7576208713f7495443fc1c93b
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-07 13:34:29 +1000

    1020. Number of Enclaves

---
+++ UnitTests/1000/S01020Tests.cs
@@ -0,0 +1,35 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01020Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01020();
+        sut.NumEnclaves(
+            new[]
+            {
+                new[] {0, 0, 0, 0},
+                new[] {1, 0, 1, 0},
+                new[] {0, 1, 1, 0},
+                new[] {0, 0, 0, 0},
+            }).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01020();
+        sut.NumEnclaves(
+            new[]
+            {
+                new[] {0, 1, 1, 0},
+                new[] {0, 0, 1, 0},
+                new[] {0, 0, 1, 0},
+                new[] {0, 0, 0, 0},
+            }).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1000/S01020.cs
@@ -0,0 +1,46 @@
+﻿namespace leetcode_sharp;
+
+// 1020. Number of Enclaves
+// https://leetcode.com/problems/number-of-enclaves/
+public class S01020
+{
+    private const int Water = 0;
+    private const int Land = 1;
+    private readonly int[] _directions = {0, 1, 0, -1, 0};
+
+    public int NumEnclaves(int[][] grid)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+
+        for (var x = 0; x < m; x++)
+        {
+            for (var y = 0; y < n; y++)
+            {
+                if (x == 0 || y == 0 || x == m - 1 || y == n - 1)
+                {
+                    markWater(grid, x, y);
+                }
+            }
+        }
+
+        return grid.Sum(row => row.Count(cell => cell == Land));
+    }
+
+    private void markWater(IReadOnlyList<int[]> grid, int x, int y)
+    {
+        if (x < 0 || x >= grid.Count || y < 0 || y >= grid[0].Length || grid[x][y] == Water)
+        {
+            return;
+        }
+
+        grid[x][y] = Water;
+
+        for (var i = 0; i < 4; ++i)
+        {
+            var nextX = x + _directions[i];
+            var nextY = y + _directions[i + 1];
+            markWater(grid, nextX, nextY);
+        }
+    }
+}
