4b644982eb4bcdb43328940d90503ad1e8fcb59b
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-07 23:02:34 +1000

    69. Sqrt(x)

---
+++ UnitTests/0000/S00069Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00069Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00069();
+        sut.MySqrt(4).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00069();
+        sut.MySqrt(8).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/0000/S00069.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 69. Sqrt(x)
+// https://leetcode.com/problems/sqrtx/
+public class S00069
+{
+    public int MySqrt(int x)
+    {
+        var lo = 1;
+        var hi = x;
+        var result = 0;
+        
+        while (lo <= hi)
+        {
+            var mid = lo + (hi - lo) / 2;
+            var square = (long)mid * mid;
+            if (square == x)
+            {
+                return mid;
+            }
+
+            if (square > x)
+            {
+                hi = mid - 1;
+            }
+            else
+            {
+                lo = mid + 1;
+                result = mid;
+            }
+        }
+
+        return result;
+    }
+}
