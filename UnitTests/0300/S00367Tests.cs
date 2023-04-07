b541481c186487f08de05dadfcb03ae095e2a79b
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-07 20:33:07 +1000

    367. Valid Perfect Square

---
+++ UnitTests/0300/S00367Tests.cs
@@ -0,0 +1,42 @@
+﻿using leetcode_sharp._0300;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00367Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00367();
+        sut.IsPerfectSquare(16).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00367();
+        sut.IsPerfectSquare(14).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00367();
+        sut.IsPerfectSquare(1).Should().BeTrue();
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S00367();
+        sut.IsPerfectSquare(4).Should().BeTrue();
+    }
+
+    [Test]
+    public void T5()
+    {
+        var sut = new S00367();
+        sut.IsPerfectSquare(2147483647).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0300/S00367.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp._0300;
+
+// 367. Valid Perfect Square
+// https://leetcode.com/problems/valid-perfect-square/
+public class S00367
+{
+    public bool IsPerfectSquare(int num)
+    {
+        var lo = 1;
+        var hi = num;
+        
+        while (lo <= hi)
+        {
+            var mid = lo + (hi - lo) / 2;
+            var square = (long)mid * mid;
+            if (square == num)
+            {
+                return true;
+            }
+
+            if (square > num)
+            {
+                hi = mid - 1;
+            }
+            else
+            {
+                lo = mid + 1;
+            }
+        }
+
+        return false;
+    }
+}
