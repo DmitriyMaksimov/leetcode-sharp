a692907c4824d205bdee40f3485ef6307a384ea2
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-07 21:58:51 +1000

    1385. Find the Distance Value Between Two Arrays

---
+++ leetcode-sharp/1300/S01385.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 1385. Find the Distance Value Between Two Arrays
+// https://leetcode.com/problems/find-the-distance-value-between-two-arrays/
+public class S01385
+{
+    public int FindTheDistanceValue(int[] arr1, int[] arr2, int d)
+    {
+        return arr1.Select(a => arr2.All(b => Math.Abs(a - b) > d)).Count(meetCriteria => meetCriteria);
+    }
+}
