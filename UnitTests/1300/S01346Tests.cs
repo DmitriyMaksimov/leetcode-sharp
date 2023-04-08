607fa150e2ff07a3770a15e370bb26f7b824eb24
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-08 16:58:26 +1000

    1346. Check If N and Its Double Exist

---
+++ UnitTests/1300/S01346Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01346Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01346();
+        sut.CheckIfExist(new[] {10, 2, 5, 3}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01346();
+        sut.CheckIfExist(new[] {3, 1, 7, 11}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/1300/S01346.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 1346. Check If N and Its Double Exist
+// https://leetcode.com/problems/check-if-n-and-its-double-exist/
+public class S01346
+{
+    public bool CheckIfExist(int[] arr)
+    {
+        var hashSet = new HashSet<int>();
+        
+        foreach (var n in arr)
+        {
+            if (hashSet.Contains(n * 2) || ( (n % 2) == 0 && hashSet.Contains(n / 2)))
+            {
+                return true;
+            }
+
+            hashSet.Add(n);
+        }
+
+        return false;
+    }
+}
