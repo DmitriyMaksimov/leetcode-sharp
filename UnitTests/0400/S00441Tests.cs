7c0f6f3dd4b42f388af9e264e0fef400712d635e
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-08 22:49:41 +1000

    441. Arranging Coins

---
+++ UnitTests/0400/S00441Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00441Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00441();
+        sut.ArrangeCoins(5).Should().Be(2);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00441();
+        sut.ArrangeCoins(8).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/0400/S00441.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 441. Arranging Coins
+// https://leetcode.com/problems/arranging-coins/
+public class S00441
+{
+    public int ArrangeCoins(int n)
+    {
+        return (int)(Math.Sqrt(2 * (long)n + 0.25) - 0.5);
+    }
+}
