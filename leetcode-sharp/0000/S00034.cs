d822f52bd1e4ed92264472bb996e25d3cbc19b6d
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-10 19:46:37 +1000

    34. Find First and Last Position of Element in Sorted Array

---
+++ leetcode-sharp/0000/S00034.cs
@@ -0,0 +1,76 @@
+﻿namespace leetcode_sharp._0000;
+
+// 34. Find First and Last Position of Element in Sorted Array
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+public class S00034
+{
+    public int[] SearchRange(int[] nums, int target)
+    {
+        if (nums.Length == 0)
+        {
+            return new[] {-1, -1};
+        }
+
+        var first = findFirst(nums, target);
+        
+        if (first >= nums.Length || nums[first] != target)
+        {
+            first = -1;
+        }
+
+        var last = findLast(nums, target);
+        
+        if (last >= nums.Length || nums[last] != target)
+        {
+            last = -1;
+        }
+        
+        return new[] {first, last};
+    }
+    
+    private static int findFirst(IReadOnlyList<int> list, long key)
+    {
+        var low = 0;
+        var high = list.Count - 1;
+
+        while (low != high)
+        {
+            var mid = low + (high - low) / 2;
+            long midVal = list[mid];
+
+            if (midVal < key)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+    
+    private static int findLast(IReadOnlyList<int> list, long key)
+    {
+        var low = 0;
+        var high = list.Count - 1;
+
+        while (low != high)
+        {
+            var mid = low + (high - low + 1) / 2;
+            long midVal = list[mid];
+
+            if (midVal > key)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid;
+            }
+        }
+
+        return low;
+    }
+}
