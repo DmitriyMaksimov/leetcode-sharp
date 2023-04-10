e23ef28c94449fd5ff567c6e8dfc66039fc99c1d
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-10 10:39:21 +1000

    162. Find Peak Element

---
+++ UnitTests/0100/S00162Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00162Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00162();
+        sut.FindPeakElement(new[] { 1,2,3,1}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00162();
+        sut.FindPeakElement(new[] { 1,2,1,3,5,6,4}).Should().BeOneOf(1, 5);
+    }
+}
---
+++ leetcode-sharp/0100/S00162.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 162. Find Peak Element
+// https://leetcode.com/problems/find-peak-element/
+public class S00162
+{
+    public int FindPeakElement(int[] nums)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left < right)
+        {
+            var mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[mid + 1])
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
