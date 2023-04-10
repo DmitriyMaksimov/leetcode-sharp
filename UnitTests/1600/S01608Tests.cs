369cd19836b6981b2cf10b5f734664a21ea813eb
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-10 19:46:26 +1000

    1608. Special Array With X Elements Greater Than or Equal X

---
+++ UnitTests/1600/S01608Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01608Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01608();
+        sut.SpecialArray(new[] {3, 5}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01608();
+        sut.SpecialArray(new[] {0, 0}).Should().Be(-1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01608();
+        sut.SpecialArray(new[] {0, 4, 3, 0, 4}).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/1600/S01608.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 1608. Special Array With X Elements Greater Than or Equal X
+// https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/
+public class S01608
+{
+    public int SpecialArray(int[] nums)
+    {
+        var counts = new Dictionary<int, int>();
+
+        foreach (var n in nums)
+        {
+            counts[n] = counts.GetValueOrDefault(n) + 1;
+        }
+
+        var number = nums.Length;
+
+        // Size of nums array is 100 at most
+        for (var i = 0; i <= 100; i++)
+        {
+            if (number == i)
+            {
+                return i;
+            }
+
+            number -= counts.GetValueOrDefault(i);
+        }
+
+        return -1;
+    }
+}
