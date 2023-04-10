735a9674e73414250cf708d534d8c889a1bc3b5a
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-10 10:42:33 +1000

    Formatting

--- UnitTests/0100/S00162Tests.cs
+++ UnitTests/0100/S00162Tests.cs
@@ -9,13 +9,13 @@ public class S00162Tests
     public void T1()
     {
         var sut = new S00162();
-        sut.FindPeakElement(new[] { 1,2,3,1}).Should().Be(2);
+        sut.FindPeakElement(new[] {1, 2, 3, 1}).Should().Be(2);
     }
 
     [Test]
     public void T2()
     {
         var sut = new S00162();
-        sut.FindPeakElement(new[] { 1,2,1,3,5,6,4}).Should().BeOneOf(1, 5);
+        sut.FindPeakElement(new[] {1, 2, 1, 3, 5, 6, 4}).Should().BeOneOf(1, 5);
     }
 }
