368312d96eac8bbf0135655e8ce2d63356a82479
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-12 19:53:15 +1000

    71. Simplify Path

---
+++ leetcode-sharp/0000/S00071.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace leetcode_sharp._0000;
+
+// 71. Simplify Path
+// https://leetcode.com/problems/simplify-path/
+public class S00071
+{
+    public string SimplifyPath(string path)
+    {
+        var stack = new Stack<string>();
+
+        foreach (var part in path.Split("/", StringSplitOptions.RemoveEmptyEntries))
+        {
+            if (stack.Any() && part == "..")
+            {
+                stack.Pop();
+            }
+            else if (part != "." && part != "..")
+            {
+                stack.Push(part);
+            }
+        }
+
+        return "/" + string.Join("/", stack.Reverse());
+    }
+}
