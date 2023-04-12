2fba3f898ddad28ef6f3eaf9559f72eeda3f4ad7
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-12 19:53:27 +1000

    2390. Removing Stars From a String

---
+++ UnitTests/2300/S02390Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02390Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02390();
+        sut.RemoveStars("leet**cod*e").Should().Be("lecoe");
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S02390();
+        sut.RemoveStars("erase*****").Should().Be("");
+    }
+}
---
+++ leetcode-sharp/2300/S02390.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 2390. Removing Stars From a String
+// https://leetcode.com/problems/removing-stars-from-a-string/
+public class S02390
+{
+    public string RemoveStars(string s)
+    {
+        var charArray = s.ToCharArray();
+        var j = 0;
+        for (var i = 0; i < charArray.Length; ++i)
+        {
+            if (charArray[i] == '*')
+            {
+                j--;
+            }
+            else
+            {
+                charArray[j++] = charArray[i];
+            }
+        }
+
+        return string.Join("", charArray[..j]);
+    }
+}
