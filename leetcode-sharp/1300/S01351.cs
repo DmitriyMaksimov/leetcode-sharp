d54ce3a0864ffa8fc119ce4e4781109581965afd
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-14 18:29:56 +1000

    1351. Count Negative Numbers in a Sorted Matrix

---
+++ leetcode-sharp/1300/S01351.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 1351. Count Negative Numbers in a Sorted Matrix
+// https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+public class S01351
+{
+    public int CountNegatives(int[][] grid)
+    {
+        return grid.Sum(row => row.Count(n => n < 0));
+    }
+}
