fc3d0b1a597082a9f117eade2e92c759b0b642df
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-15 14:46:00 +1000

    2218. Maximum Value of K Coins From Piles

---
+++ UnitTests/2200/S02218Tests.cs
@@ -0,0 +1,25 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02218Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02218();
+        sut.MaxValueOfCoins(new List<IList<int>> {new List<int> {1, 100, 3}, new List<int> {7, 8, 9}}, 2).Should().Be(101);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02218();
+        sut.MaxValueOfCoins(
+            new List<IList<int>>
+            {
+                new List<int> {100}, new List<int> {100}, new List<int> {100}, new List<int> {100}, new List<int> {100}, new List<int> {100}, new List<int> {1, 1, 1, 1, 1, 1, 700},
+            }, 7).Should().Be(706);
+    }
+}
