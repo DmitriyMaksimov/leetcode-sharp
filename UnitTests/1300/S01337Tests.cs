8b6ebc1a747e8922d5acd32905bf525805e79d14
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-15 19:27:06 +1000

    1337. The K Weakest Rows in a Matrix

---
+++ UnitTests/1300/S01337Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01337Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01337();
+        sut.KWeakestRows(new[] {new[] {1, 1, 0, 0, 0}, new[] {1, 1, 1, 1, 0}, new[] {1, 0, 0, 0, 0}, new[] {1, 1, 0, 0, 0}, new[] {1, 1, 1, 1, 1}}, 3).Should().Equal(2, 0, 3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01337();
+        sut.KWeakestRows(new[] {new[] {1, 0, 0, 0}, new[] {1, 1, 1, 1}, new[] {1, 0, 0, 0}, new[] {1, 0, 0, 0}}, 2).Should().Equal(0, 2);
+    }
+}
---
+++ leetcode-sharp/1300/S01337.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 1337. The K Weakest Rows in a Matrix
+// https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+public class S01337
+{
+    public int[] KWeakestRows(int[][] mat, int k)
+    {
+        var data = new (int count, int index)[mat.Length];
+
+        for (var index = 0; index < mat.Length; index++)
+        {
+            var row = mat[index];
+            data[index] = (row.TakeWhile(x => x == 1).Count(), index);
+        }
+
+        return data.OrderBy(x => x.count).ThenBy(x => x.index).Take(k).Select(x => x.index).ToArray();
+    }
+}
