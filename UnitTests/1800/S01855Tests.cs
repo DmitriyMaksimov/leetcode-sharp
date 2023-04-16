ad5fd5469370fd3df5f665482283ebc7b87ae15f
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-16 12:58:52 +1000

    1855. Maximum Distance Between a Pair of Values

---
+++ UnitTests/1800/S01855Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01855Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01855();
+        sut.MaxDistance(new[] {55, 30, 5, 4, 2}, new[] {100, 20, 10, 10, 5}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01855();
+        sut.MaxDistance(new[] {2, 2, 2}, new[] {10, 10, 1}).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01855();
+        sut.MaxDistance(new[] {30, 29, 19, 5}, new[] {25, 25, 25, 25, 25}).Should().Be(2);
+    }
+}
