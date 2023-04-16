ef092572862de91627fd5ed2f0310374a27da5a1
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-16 13:32:58 +1000

    Formatting

--- UnitTests/0100/S00153Tests.cs
+++ UnitTests/0100/S00153Tests.cs
@@ -7,20 +7,20 @@ public class S00153Tests
     public void T1()
     {
         var sut = new S00153();
-        sut.FindMin(new[] {3,4,5,1,2 }).Should().Be(1);
+        sut.FindMin(new[] {3, 4, 5, 1, 2}).Should().Be(1);
     }
-    
+
     [Test]
     public void T2()
     {
         var sut = new S00153();
-        sut.FindMin(new[] {4,5,6,7,0,1,2 }).Should().Be(0);
+        sut.FindMin(new[] {4, 5, 6, 7, 0, 1, 2}).Should().Be(0);
     }
 
     [Test]
     public void T3()
     {
         var sut = new S00153();
-        sut.FindMin(new[] {11,13,15,17 }).Should().Be(11);
+        sut.FindMin(new[] {11, 13, 15, 17}).Should().Be(11);
     }
 }
