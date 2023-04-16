e59903b3f96644c580a8b574556bde013f880c28
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-16 12:00:00 +1000

    1639. Number of Ways to Form a Target String Given a Dictionary

---
+++ UnitTests/1600/S01639Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01639Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01639();
+        sut.NumWays(new[] {"acca", "bbbb", "caca"}, "aba").Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01639();
+        sut.NumWays(new[] {"abba", "baab"}, "bab").Should().Be(4);
+    }
+}
