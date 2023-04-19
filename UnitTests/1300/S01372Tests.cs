bd82b82f280ca825f6be1fdd7bc813e979cc6ccc
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-19 18:49:05 +1000

    1372. Longest ZigZag Path in a Binary Tree

---
+++ UnitTests/1300/S01372Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01372Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01372();
+        sut.LongestZigZag(TreeNode.Parse("[1,null,1,1,1,null,null,1,1,null,1,null,null,null,1,null,1]")).Should().Be(3);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S01372();
+        sut.LongestZigZag(TreeNode.Parse("[1,1,1,null,1,null,null,1,1,null,1]")).Should().Be(4);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01372();
+        sut.LongestZigZag(TreeNode.Parse("[1]")).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1300/S01372.cs
@@ -0,0 +1,45 @@
+﻿namespace leetcode_sharp;
+
+// 1372. Longest ZigZag Path in a Binary Tree
+// https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/
+public class S01372
+{
+    private enum Direction
+    {
+        Left,
+        Right
+    }
+
+    private int _result;
+
+    public int LongestZigZag(TreeNode? root)
+    {
+        maxZigZag(root, Direction.Left, 0);
+        maxZigZag(root, Direction.Right, 0);
+
+        return _result;
+    }
+
+    private void maxZigZag(TreeNode? node, Direction direction, int currentStep)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        _result = Math.Max(_result, currentStep);
+
+        if (direction == Direction.Left)
+        {
+            // We can continue current path
+            maxZigZag(node.left, Direction.Right, currentStep + 1);
+            // ...or start over
+            maxZigZag(node.right, Direction.Left, 1);
+        }
+        else
+        {
+            maxZigZag(node.right, Direction.Left, currentStep + 1);
+            maxZigZag(node.left, Direction.Right, 1);
+        }
+    }
+}
