a80c26fed98f262482fad87ee7da24353510b255
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-25 11:45:29 +1000

    1046. Last Stone Weight

---
+++ leetcode-sharp/1000/S01046.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 1046. Last Stone Weight
+// https://leetcode.com/problems/last-stone-weight/
+public class S01046
+{
+    public int LastStoneWeight(int[] stones)
+    {
+        var queue = new PriorityQueue<int, int>();
+        foreach (var stone in stones)
+        {
+            queue.Enqueue(stone, -stone);
+        }
+
+        while (queue.Count > 1)
+        {
+            // Choose the heaviest two stones and smash them together
+            // Suppose the heaviest two stones have weights x and y with x <= y
+            var y = queue.Dequeue();
+            var x = queue.Dequeue();
+
+            if (y != x)
+            {
+                // the stone of weight x is destroyed, and the stone of weight y has new weight y - x
+                var newWeight = y - x;
+                queue.Enqueue(newWeight, -newWeight);
+            }
+        }
+
+        return queue.Count > 0 ? queue.Dequeue() : 0;
+    }
+}
