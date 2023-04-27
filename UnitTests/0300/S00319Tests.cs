9d422882f09ef1686ddb528f9a29715520506daf
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-27 18:34:32 +1000

    319. Bulb Switcher

---
+++ UnitTests/0300/S00319Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp._0300;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00319Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00319();
+        sut.BulbSwitch(3).Should().Be(1);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00319();
+        sut.BulbSwitch(0).Should().Be(0);
+    }
+    
+    [Test]
+    public void T3()
+    {
+        var sut = new S00319();
+        sut.BulbSwitch(1).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/0300/S00319.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp._0300;
+
+// 319. Bulb Switcher
+// https://leetcode.com/problems/bulb-switcher/
+public class S00319
+{
+    public int BulbSwitch(int n)
+    {
+        return (int)Math.Sqrt(n);
+    }
+}
