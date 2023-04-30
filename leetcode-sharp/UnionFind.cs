a243dccf7229330d33d5fb89d33387d676d48c93
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-30 10:35:08 +1000

    Refactoring of UnionFind

--- leetcode-sharp/UnionFind.cs
+++ leetcode-sharp/UnionFind.cs
@@ -1,49 +1,46 @@
 ﻿namespace leetcode_sharp;
 
-    public class UnionFind
+public class UnionFind
+{
+    private readonly int[] _component;
+    private int _distinctComponents;
+
+    // Initially all nodes are in different components
+    public UnionFind(int n)
     {
-        int[] component;
-        int distinctComponents;
-
-        /*
-         *   Initially all 'n' nodes are in different components.
-         *   e.g. component[2] = 2 i.e. node 2 belong to component 2.
-         */
-        public UnionFind(int n)
-        {
-            distinctComponents = n;
-            component = Enumerable.Range(0, n + 1).ToArray();
-        }
-
-        // Unite components
-        // Returns true when nodes 'a' and 'b' are in different components
-        public bool Unite(int a, int b)
-        {
-            if (FindComponent(a) == FindComponent(b))
-            {
-                return false;
-            }
-
-            component[FindComponent(a)] = b;
-            distinctComponents--;
-
-            return true;
-        }
-
-        // Returns component the node 'a' belong to.
-        public int FindComponent(int a)
-        {
-            if (component[a] != a)
-            {
-                component[a] = FindComponent(component[a]);
-            }
-
-            return component[a];
-        }
-
-         // Are all nodes united into a single component?
-        public bool United()
-        {
-            return distinctComponents == 1;
-        }
+        _distinctComponents = n;
+        _component = Enumerable.Range(0, n + 1).ToArray();
     }
+
+    // Unite components
+    // Returns true when nodes 'a' and 'b' are in different components
+    public bool Unite(int a, int b)
+    {
+        if (FindComponent(a) == FindComponent(b))
+        {
+            return false;
+        }
+
+        _component[FindComponent(a)] = b;
+        _distinctComponents--;
+
+        return true;
+    }
+
+    // Returns component the node 'a' belongs to
+    public int FindComponent(int a)
+    {
+        if (_component[a] != a)
+        {
+            _component[a] = FindComponent(_component[a]);
+        }
+
+        return _component[a];
+    }
+
+     // Are all nodes united into a single component?
+    public bool United()
+    {
+        return _distinctComponents == 1;
+    }
+}
