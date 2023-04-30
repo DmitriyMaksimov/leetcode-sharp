5b8db83d6f1faf33bafb8129f65c3171504dfc0d
Dmitriy <dmitriy.maksimov@workday.com>
2023-04-30 10:29:52 +1000

    1579. Remove Max Number of Edges to Keep Graph Fully Traversable

---
+++ UnitTests/1500/S01579Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01579Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01579();
+        sut.MaxNumEdgesToRemove(4, new[] {new[] {3,1,2}, new[] {3,2,3}, new[] {1,1,3}, new[] {1,2,4}, new[] {1,1,2}, new[] {2,3,4}}).Should().Be(2);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S01579();
+        sut.MaxNumEdgesToRemove(4, new[] {new[] {3,1,2}, new[] {3,2,3}, new[] {1,1,4}, new[] {2,1,4}}).Should().Be(0);
+    }
+    
+    [Test]
+    public void T3()
+    {
+        var sut = new S01579();
+        sut.MaxNumEdgesToRemove(4, new[] {new[] {3,2,3}, new[] {1,1,2}, new[] {2,3,4}}).Should().Be(-1);
+    }
+}
