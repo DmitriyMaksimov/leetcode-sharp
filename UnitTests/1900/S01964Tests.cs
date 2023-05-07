b689ca0d9b3fb18314972772ab5d003898c3d80e
Dmitriy <dmitriy.maksimov@workday.com>
2023-05-07 12:18:42 +1000

    1964. Find the Longest Valid Obstacle Course at Each Position

---
+++ UnitTests/1900/S01964Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01964Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01964();
+        sut.LongestObstacleCourseAtEachPosition(new[] {1, 2, 3, 2}).Should().Equal(1, 2, 3, 3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01964();
+        sut.LongestObstacleCourseAtEachPosition(new[] {2, 2, 1}).Should().Equal(1, 2, 1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01964();
+        sut.LongestObstacleCourseAtEachPosition(new[] {3, 1, 5, 6, 4, 2}).Should().Equal(1, 1, 2, 3, 2, 2);
+    }
+}
---
+++ leetcode-sharp/1900/S01964.cs
@@ -0,0 +1,44 @@
+﻿namespace leetcode_sharp;
+
+// 1964. Find the Longest Valid Obstacle Course at Each Position
+// https://leetcode.com/problems/find-the-longest-valid-obstacle-course-at-each-position/
+public class S01964
+{
+    public int[] LongestObstacleCourseAtEachPosition(int[] obstacles)
+    {
+        var n = obstacles.Length;
+        var length = 0;
+        var result = new int[n];
+        var dp = new int[n];
+
+        for (var i = 0; i < n; ++i)
+        {
+            var left = 0;
+            var right = length;
+            
+            while (left < right)
+            {
+                var m = (left + right) / 2;
+                if (dp[m] <= obstacles[i])
+                {
+                    left = m + 1;
+                }
+                else
+                {
+                    right = m;
+                }
+            }
+
+            result[i] = left + 1;
+
+            if (length == left)
+            {
+                length++;
+            }
+
+            dp[left] = obstacles[i];
+        }
+
+        return result;
+    }
+}
