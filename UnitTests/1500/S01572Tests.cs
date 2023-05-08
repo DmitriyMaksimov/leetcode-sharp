0d603c2bf41a12bafa0ba539c8d1c3da616b4d68
Dmitriy <dmitriy.maksimov@workday.com>
2023-05-08 15:53:26 +1000

    1572. Matrix Diagonal Sum

---
+++ UnitTests/1500/S01572Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01572Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01572();
+        sut.DiagonalSum(new[] {new[] {1, 2, 3}, new[] {4, 5, 6}, new[] {7, 8, 9}}).Should().Be(25);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01572();
+        sut.DiagonalSum(new[] {new[] {1, 1, 1, 1}, new[] {1, 1, 1, 1}, new[] {1, 1, 1, 1}, new[] {1, 1, 1, 1}}).Should()
+            .Be(8);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01572();
+        sut.DiagonalSum(new[] {new[] {5}}).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/1500/S01572.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 1572. Matrix Diagonal Sum
+// https://leetcode.com/problems/matrix-diagonal-sum/
+public class S01572
+{
+    public int DiagonalSum(int[][] mat)
+    {
+        var n = mat.Length;
+
+        var sum = 0;
+        for (var i = 0; i < n; i++)
+        {
+            sum += mat[i][i];
+            sum += mat[n - i - 1][i];
+        }
+
+        if (n % 2 == 1)
+        {
+            var mid = n / 2;
+            sum -= mat[mid][mid];
+        }
+        
+        return sum;
+    }
+}
