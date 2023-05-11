b8dfdb869f6f34732a62003fee1dc583c8521c19
Dmitriy <dmitriy.maksimov@workday.com>
2023-05-11 13:28:51 +1000

    59. Spiral Matrix II

---
+++ UnitTests/0000/S00059Tests.cs
@@ -0,0 +1,17 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00059Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00059();
+        var matrix = sut.GenerateMatrix(3);
+        matrix[0].Should().Equal(1, 2, 3);
+        matrix[1].Should().Equal(8, 9, 4);
+        matrix[2].Should().Equal(7, 6, 5);
+    }
+}
