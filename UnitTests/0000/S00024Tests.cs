a4137cba2aaf94546349b26d2a9e41b545c93e69
Dmitriy <dmitriy.maksimov@workday.com>
2023-05-16 18:08:22 +1000

    24. Swap Nodes in Pairs

---
+++ UnitTests/0000/S00024Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00024Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00024();
+        ListNode.ToString(sut.SwapPairs(ListNode.Parse("[1,2,3,4]"))).Should().Be("[2,1,4,3]");
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00024();
+        ListNode.ToString(sut.SwapPairs(ListNode.Parse("[1]"))).Should().Be("[1]");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00024();
+        ListNode.ToString(sut.SwapPairs(null)).Should().Be("[]");
+    }
+}
---
+++ leetcode-sharp/0000/S00024.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 24. Swap Nodes in Pairs
+// https://leetcode.com/problems/swap-nodes-in-pairs/
+public class S00024
+{
+    public ListNode? SwapPairs(ListNode? head)
+    {
+        if (head?.next == null)
+        {
+            return head;
+        }
+
+        var dummyNode = new ListNode();
+        var prevNode = dummyNode;
+        var currNode = head;
+
+        while (currNode?.next != null)
+        {
+            prevNode.next = currNode.next;
+            currNode.next = prevNode.next.next;
+            prevNode.next.next = currNode;
+
+            prevNode = currNode;
+            currNode = currNode.next;
+        }
+
+        return dummyNode.next;
+    }
+}
