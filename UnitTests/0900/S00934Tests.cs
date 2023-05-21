afbe9d344ad1ce4fefc7fe8cba9bcd8b113b317c
Dmitriy <dmitriy.maksimov@workday.com>
2023-05-21 11:38:50 +1000

    934. Shortest Bridge

---
+++ UnitTests/0900/S00934Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00934Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00934();
+        sut.ShortestBridge(new[] {new[] {0, 1}, new[] {1, 0}}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00934();
+        sut.ShortestBridge(new[] {new[] {0, 1, 0}, new[] {0, 0, 0}, new[] {0, 0, 1}}).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00934();
+        sut.ShortestBridge(new[] {new[] {1, 1, 1, 1, 1}, new[] {1, 0, 0, 0, 1}, new[] {1, 0, 1, 0, 1}, new[] {1, 0, 0, 0, 1}, new[] {1, 1, 1, 1, 1}}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/0900/S00934.cs
@@ -0,0 +1,86 @@
+﻿namespace leetcode_sharp;
+
+// 934. Shortest Bridge
+// https://leetcode.com/problems/shortest-bridge/
+public class S00934
+{
+    private int[,] _visit;
+    private readonly int[] _directions = new[] {0, 1, 0, -1, 0};
+
+    public int ShortestBridge(int[][] grid)
+    {
+        var n = grid.Length;
+        var m = grid[0].Length;
+        _visit = new int[n, m];
+        var queue = new Queue<(int row, int col)>();
+
+        findIsland(grid, n, m, queue);
+
+        var count = 0;
+        while (queue.Any())
+        {
+            var size = queue.Count;
+            while (size-- > 0)
+            {
+                var (row, col) = queue.Dequeue();
+                _visit[row, col] = 1;
+
+                for (var i = 0; i < 4; i++)
+                {
+                    var nextRow = row + _directions[i];
+                    var nextCol = col + _directions[i + 1];
+
+                    if (nextRow < n && nextRow >= 0 && nextCol < m && nextCol >= 0 && _visit[nextRow, nextCol] == 0)
+                    {
+                        queue.Enqueue((nextRow, nextCol));
+                        _visit[nextRow, nextCol] = 1;
+                        if (grid[nextRow][nextCol] == 1)
+                        {
+                            return count;
+                        }
+                    }
+                }
+            }
+
+            count++;
+        }
+
+        return -1;
+    }
+
+    private void findIsland(IReadOnlyList<int[]> grid, int n, int m, Queue<(int row, int col)> queue)
+    {
+        var flag = false;
+
+        for (var i = 0; i < n && !flag; i++)
+        {
+            for (var j = 0; j < m && !flag; j++)
+            {
+                if (_visit[i, j] == 0 && grid[i][j] == 1)
+                {
+                    dfs(i, j, grid, queue);
+                    flag = true;
+                }
+            }
+        }
+    }
+
+    private void dfs(int row, int col, IReadOnlyList<int[]> grid, Queue<(int row, int col)> queue)
+    {
+        var n = grid.Count;
+        var m = grid[0].Length;
+        queue.Enqueue((row, col));
+        _visit[row, col] = 1;
+
+        for (var i = 0; i < 4; i++)
+        {
+            var nextRow = row + _directions[i];
+            var nextCol = col + _directions[i + 1];
+
+            if (nextRow < n && nextRow >= 0 && nextCol < m && nextCol >= 0 && _visit[nextRow, nextCol] == 0 && grid[nextRow][nextCol] == 1)
+            {
+                dfs(nextRow, nextCol, grid, queue);
+            }
+        }
+    }
+}
