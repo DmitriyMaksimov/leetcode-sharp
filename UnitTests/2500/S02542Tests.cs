ffb4b1f6756eb496f1230f32df2b19878b0eb5cd
Dmitriy <dmitriy.maksimov@workday.com>
2023-05-24 12:40:22 +1000

    2542. Maximum Subsequence Score

---
+++ UnitTests/2500/S02542Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02542Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02542();
+        sut.MaxScore(new[] {1, 3, 3, 2}, new[] {2, 1, 3, 4}, 3).Should().Be(12);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02542();
+        sut.MaxScore(new[] {4, 2, 3, 1, 1}, new[] {7, 5, 10, 9, 6}, 1).Should().Be(30);
+    }
+}
