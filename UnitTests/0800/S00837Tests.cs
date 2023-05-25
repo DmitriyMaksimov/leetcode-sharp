8846c6d907bdfdc3c161dd39c58f0a04cdca188f
Dmitriy <dmitriy.maksimov@workday.com>
2023-05-25 15:12:40 +1000

    837. New 21 Game

---
+++ UnitTests/0800/S00837Tests.cs
@@ -0,0 +1,30 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00837Tests
+{
+    private const double Precision = 0.00001;
+
+    [Test]
+    public void T1()
+    {
+        var sut = new S00837();
+        sut.New21Game(10, 1, 10).Should().BeApproximately(1, Precision);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00837();
+        sut.New21Game(6, 1, 10).Should().BeApproximately(0.6, Precision);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00837();
+        sut.New21Game(21, 17, 10).Should().BeApproximately(0.73278, Precision);
+    }
+}
---
+++ leetcode-sharp/0800/S00837.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 837. New 21 Game
+// https://leetcode.com/problems/new-21-game/
+public class S00837
+{
+    public double New21Game(int n, int k, int maxPts)
+    {
+        if (k == 0 || n >= k + maxPts)
+        {
+            return 1.0;
+        }
+
+        var windowSum = 1.0;
+        var probability = 0.0;
+
+        var dp = new double[n + 1];
+        dp[0] = 1.0;
+
+        for (var i = 1; i <= n; i++)
+        {
+            dp[i] = windowSum / maxPts;
+
+            if (i < k)
+            {
+                windowSum += dp[i];
+            }
+            else
+            {
+                probability += dp[i];
+            }
+
+            if (i >= maxPts)
+            {
+                windowSum -= dp[i - maxPts];
+            }
+        }
+
+        return probability;
+    }
+}
