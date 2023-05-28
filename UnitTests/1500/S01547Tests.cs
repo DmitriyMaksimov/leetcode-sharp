5725c2e30473abd19afcba692d843cdf4046c1e5
Dmitriy <dmitriy.maksimov@workday.com>
2023-05-28 11:53:49 +1000

    1547. Minimum Cost to Cut a Stick

---
+++ UnitTests/1500/S01547Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01547Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01547();
+        sut.MinCost(7, new[] {1, 3, 4, 5}).Should().Be(16);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01547();
+        sut.MinCost(9, new[] {5, 6, 1, 4, 2}).Should().Be(22);
+    }
+}
