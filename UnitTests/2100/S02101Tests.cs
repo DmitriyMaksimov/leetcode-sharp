6389d2cb0500cfafb62232f44dc7a00bbfbbc88d
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-02 14:07:54 +1000

    2101. Detonate the Maximum Bombs

---
+++ UnitTests/2100/S02101Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02101Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02101();
+        sut.MaximumDetonation(new[] {new[] {2, 1, 3}, new[] {6, 1, 4}}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02101();
+        sut.MaximumDetonation(new[] {new[] {1, 1, 5}, new[] {10, 10, 5}}).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02101();
+        sut.MaximumDetonation(new[] {new[] {1, 2, 3}, new[] {2, 3, 1}, new[] {3, 4, 2}, new[] {4, 5, 3}, new[] {5, 6, 4}}).Should().Be(5);
+    }
+}
