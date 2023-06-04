96a530cb29ced6caf96acd07851c8fc9afc40c26
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-04 23:00:51 +1000

    1689. Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ leetcode-sharp/1600/S01689.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers
+// https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+public class S01689
+{
+    public int MinPartitions(string n)
+    {
+        return n.Max() - '0';
+    }
+}
