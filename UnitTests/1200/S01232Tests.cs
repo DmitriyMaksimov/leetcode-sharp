1bc9ee8614d6c3e188f114441676e78bc22bbb0e
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-05 20:10:11 +1000

    1232. Check If It Is a Straight Line

---
+++ UnitTests/1200/S01232Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01232Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01232();
+        sut.CheckStraightLine(new[] {new[] {1, 2}, new[] {2, 3}, new[] {3, 4}, new[] {4, 5}, new[] {5, 6}, new[] {6, 7}}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01232();
+        sut.CheckStraightLine(new[] {new[] {1, 1}, new[] {2, 3}, new[] {3, 4}, new[] {4, 5}, new[] {5, 6}, new[] {7, 7}}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/1200/S01232.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 1232. Check If It Is a Straight Line
+// https://leetcode.com/problems/check-if-it-is-a-straight-line/
+public class S01232
+{
+    public bool CheckStraightLine(int[][] coordinates)
+    {
+        var n = coordinates.Length;
+        var x1 = coordinates[0][0];
+        var y1 = coordinates[0][1];
+        var x2 = coordinates[1][0];
+        var y2 = coordinates[1][1];
+
+        for (var i = 2; i < n; ++i)
+        {
+            var x = coordinates[i][0];
+            var y = coordinates[i][1];
+
+            if ((y - y1) * (x - x2) != (y - y2) * (x - x1))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
