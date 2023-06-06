704126d3e827a0bf6e4d0550a944c37a99c2d52e
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-06 18:33:41 +1000

    1502. Can Make Arithmetic Progression From Sequence

---
+++ leetcode-sharp/1500/S01502.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 1502. Can Make Arithmetic Progression From Sequence
+// https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
+public class S01502
+{
+    public bool CanMakeArithmeticProgression(int[] arr)
+    {
+        Array.Sort(arr);
+        var diff = arr[1] - arr[0];
+
+        for (var i = 2; i < arr.Length; i++)
+        {
+            if (arr[i] - arr[i - 1] != diff)
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
