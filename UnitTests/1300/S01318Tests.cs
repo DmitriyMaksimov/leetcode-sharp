a876c87d5a7f1c0d6ba027a8844d2b7ea9b5b4e1
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-07 12:38:08 +1000

    1318. Minimum Flips to Make a OR b Equal to c

---
+++ UnitTests/1300/S01318Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01318Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01318();
+        sut.MinFlips(2, 6, 5).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01318();
+        sut.MinFlips(4, 2, 7).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01318();
+        sut.MinFlips(1, 2, 3).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1300/S01318.cs
@@ -0,0 +1,37 @@
+﻿namespace leetcode_sharp;
+
+// 1318. Minimum Flips to Make a OR b Equal to c
+// https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c/
+public class S01318
+{
+    public int MinFlips(int a, int b, int c)
+    {
+        var count = 0;
+
+        while (a > 0 || b > 0 || c > 0)
+        {
+            if ((c & 1) == 0)
+            {
+                if ((a & 1) == 1)
+                {
+                    count++;
+                }
+
+                if ((b & 1) == 1)
+                {
+                    count++;
+                }
+            }
+            else if ((a & 1) == 0 && (b & 1) == 0)
+            {
+                count++;
+            }
+
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+
+        return count;
+    }
+}
