fdf2e647e7698d2be69915011e0f917e0d1c2fef
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-10 15:48:12 +1000

    2114. Maximum Number of Words Found in Sentences

---
+++ UnitTests/2100/S02114Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02114Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02114();
+        sut.MostWordsFound(new[] {"alice and bob love leetcode", "i think so too", "this is great thanks very much"}).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02114();
+        sut.MostWordsFound(new[] {"please wait", "continue to fight", "continue to win"}).Should().Be(3);
+    }
+}
