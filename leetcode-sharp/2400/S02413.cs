5110d08463a2609820a5f00f0c0aabb411ea7021
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-10 15:37:56 +1000

    2413. Smallest Even Multiple

---
+++ leetcode-sharp/2400/S02413.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2413. Smallest Even Multiple
+// https://leetcode.com/problems/smallest-even-multiple/
+public class S02413
+{
+    public int SmallestEvenMultiple(int n)
+    {
+        return n % 2 == 0 ? n : n * 2;
+    }
+}
