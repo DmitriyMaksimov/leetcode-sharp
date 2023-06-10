0c5a2f7d21f8a6e01d426c30d06046ee2a3e9c13
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-11 09:58:03 +1000

    1859. Sorting the Sentence

---
+++ leetcode-sharp/1800/S01859.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 1859. Sorting the Sentence
+// https://leetcode.com/problems/sorting-the-sentence/
+public class S01859
+{
+    public string SortSentence(string s)
+    {
+        var words = new string[9];
+
+        foreach (var wordAndIndex in s.Split(' '))
+        {
+            var index = wordAndIndex[^1] - '1';
+            words[index] = wordAndIndex[..^1];
+        }
+        
+        return string.Join(" ", words.Where(x => !string.IsNullOrEmpty(x)));
+    }
+}
