781b5d3050436c463d22ec09f964fb882d249aa3
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-10 22:17:28 +1000

    2325. Decode the Message

---
+++ UnitTests/2300/S02325Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02325Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02325();
+        sut.DecodeMessage("the quick brown fox jumps over the lazy dog", "vkbs bs t suepuv").Should().Be("this is a secret");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02325();
+        sut.DecodeMessage("eljuxhpwnyrdgtqkviszcfmabo", "zwx hnfx lqantp mnoeius ycgk vcnjrdb").Should().Be("the five boxing wizards jump quickly");
+    }
+}
