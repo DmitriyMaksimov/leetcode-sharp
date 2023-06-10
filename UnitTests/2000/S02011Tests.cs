cb6543d60678a332daf4450ad6f2d4d88ed1ecb7
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-10 15:33:10 +1000

    2011. Final Value of Variable After Performing Operations

---
+++ UnitTests/2000/S02011Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp._2000;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02011Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02011();
+        sut.FinalValueAfterOperations(new[] {"--X","X++","X++"}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02011();
+        sut.FinalValueAfterOperations(new[] {"++X","++X","X++"}).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02011();
+        sut.FinalValueAfterOperations(new[] {"X++","++X","--X","X--"}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2000/S02011.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp._2000;
+
+// 2011. Final Value of Variable After Performing Operations
+// https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+public class S02011
+{
+    public int FinalValueAfterOperations(string[] operations)
+    {
+        return operations.Sum(operation => operation[1] == '+' ? 1 : -1);
+    }
+}
