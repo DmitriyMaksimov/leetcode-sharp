7040570eaae04dd25f5c1438bfcaceea7f52a17d
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-10 17:39:59 +1000

    1720. Decode XORed Array

---
+++ UnitTests/1700/S01720Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01720Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01720();
+        sut.Decode(new[] {1, 2, 3}, 1).Should().Equal(1, 0, 2, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01720();
+        sut.Decode(new[] {6, 2, 7, 3}, 4).Should().Equal(4, 2, 0, 7, 4);
+    }
+}
---
+++ leetcode-sharp/1700/S01720.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 1720. Decode XORed Array
+// https://leetcode.com/problems/decode-xored-array/
+public class S01720
+{
+    public int[] Decode(int[] encoded, int first)
+    {
+        var decoded = new int[encoded.Length + 1];
+        decoded[0] = first;
+
+        for (var i = 0; i < encoded.Length; i++)
+        {
+            decoded[i + 1] = encoded[i] ^ decoded[i];
+        }
+        
+        return decoded;
+    }
+}
