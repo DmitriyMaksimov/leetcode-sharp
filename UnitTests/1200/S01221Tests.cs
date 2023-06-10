c85f3789e7bfbf1bff45e75ec87e05447fe9a5f3
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-10 17:31:52 +1000

    1221. Split a String in Balanced Strings

---
+++ UnitTests/1200/S01221Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01221Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01221();
+        sut.BalancedStringSplit("RLRRLLRLRL").Should().Be(4);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S01221();
+        sut.BalancedStringSplit("RLRRRLLRLL").Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01221();
+        sut.BalancedStringSplit("LLLLRRRR").Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1200/S01221.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 1221. Split a String in Balanced Strings
+// https://leetcode.com/problems/split-a-string-in-balanced-strings/
+public class S01221
+{
+    public int BalancedStringSplit(string s)
+    {
+        var result = 0;
+        var balance = 0;
+        
+        foreach (var c in s)
+        {
+            if (c == 'L')
+            {
+                ++balance;
+            }
+            else
+            {
+                --balance;
+            }
+
+            if (balance == 0)
+            {
+                ++result;
+            }
+        }
+        
+        return result;
+    }
+}
