c05e883ad6bd31fdd7bc2bf5e338cbdcb8b76531
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-12 21:04:38 +1000

    1773. Count Items Matching a Rule

---
+++ leetcode-sharp/1700/S01773.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode_sharp;
+
+// 1773. Count Items Matching a Rule
+// https://leetcode.com/problems/count-items-matching-a-rule/
+public class S01773
+{
+    public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+    {
+        var ruleIndex = ruleKey switch
+        {
+            "type" => 0,
+            "color" => 1,
+            _ => 2
+        };
+
+        return items.Count(item => item[ruleIndex] == ruleValue);
+    }
+}
