f21b74a8df37ecd8cd2cdcf90c25ed6037035a92
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-13 22:02:42 +1000

    1791. Find Center of Star Graph

---
+++ UnitTests/1700/S01791Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01791Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01791();
+        sut.FindCenter(new[] {new[] {1, 2}, new[] {2, 3}, new[] {4, 2}}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01791();
+        sut.FindCenter(new[] {new[] {1, 2}, new[] {5, 1}, new[] {1, 3}, new[] {1, 4}}).Should().Be(1);
+    }
+}
