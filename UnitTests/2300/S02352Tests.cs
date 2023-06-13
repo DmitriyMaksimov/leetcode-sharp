1774edfb3073ed415e1603a9e174af9f4ae613a4
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-13 21:19:01 +1000

    2352. Equal Row and Column Pairs

---
+++ UnitTests/2300/S02352Tests.cs
@@ -0,0 +1,34 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02352Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02352();
+        sut.EqualPairs(new[] {new[] {3, 2, 1}, new[] {1, 7, 6}, new[] {2, 7, 7}}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02352();
+        sut.EqualPairs(new[] {new[] {3, 1, 2, 2}, new[] {1, 4, 4, 5}, new[] {2, 4, 2, 2}, new[] {2, 4, 2, 2}}).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02352();
+        sut.EqualPairs(new[]
+        {
+            new[] {3, 1, 2, 2},
+            new[] {1, 4, 4, 4},
+            new[] {2, 4, 2, 2},
+            new[] {2, 5, 2, 2}
+        }).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/2300/S02352.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 2352. Equal Row and Column Pairs
+// https://leetcode.com/problems/equal-row-and-column-pairs/
+public class S02352
+{
+    public int EqualPairs(int[][] grid)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+
+        var hash = new Dictionary<string, int>();
+
+        for (var row = 0; row < n; ++row)
+        {
+            var str = "";
+            for (var col = 0; col < m; ++col)
+            {
+                str += $"{grid[col][row]}.";
+            }
+
+            hash[str] = hash.GetValueOrDefault(str, 0) + 1;
+        }
+
+        var result = 0;
+
+        for (var col = 0; col < m; ++col)
+        {
+            var str = "";
+
+            for (var row = 0; row < n; ++row)
+            {
+                str += $"{grid[col][row]}.";
+            }
+
+            result += hash.GetValueOrDefault(str, 0);
+        }
+
+        return result;
+    }
+}
