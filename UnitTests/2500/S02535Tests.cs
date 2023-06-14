f114b0bda681adf191ea2f674aaa0da6b58c4db9
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-14 20:57:14 +1000

    2535. Difference Between Element Sum and Digit Sum of an Array

---
+++ UnitTests/2500/S02535Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02535Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02535();
+        sut.DifferenceOfSum(new[] {1, 15, 6, 3}).Should().Be(9);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02535();
+        sut.DifferenceOfSum(new[] {1, 2, 3, 4}).Should().Be(0);
+    }
+}
