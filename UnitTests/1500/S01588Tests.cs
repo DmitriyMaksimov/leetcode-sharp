ce3215718f7ae6d45f0cff84326a7dab376e9830
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-15 22:07:32 +1000

    1588. Sum of All Odd Length Subarrays

---
+++ UnitTests/1500/S01588Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01588Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01588();
+        sut.SumOddLengthSubarrays(new[] {1, 4, 2, 5, 3}).Should().Be(58);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01588();
+        sut.SumOddLengthSubarrays(new[] {1, 2}).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01588();
+        sut.SumOddLengthSubarrays(new[] {10, 11, 12}).Should().Be(66);
+    }
+}
