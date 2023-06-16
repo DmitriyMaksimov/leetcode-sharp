67aab9c8f5709a9b48d00dcca444cf123a354e78
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-16 23:12:30 +1000

    1323. Maximum 69 Number

---
+++ UnitTests/1300/S01323Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01323Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01323();
+        sut.Maximum69Number(9669).Should().Be(9969);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S01323();
+        sut.Maximum69Number(9996).Should().Be(9999);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01323();
+        sut.Maximum69Number(9999).Should().Be(9999);
+    }
+}
---
+++ leetcode-sharp/1300/S01323.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 1323. Maximum 69 Number
+// https://leetcode.com/problems/maximum-69-number/
+public class S01323
+{
+    public int Maximum69Number(int num)
+    {
+        var n = 10000;
+
+        while (n > 0)
+        {
+            var currentDigit = (num / n) % 10; 
+
+            if (currentDigit == 6)
+            {
+                // replace first `6` to `9` and return
+                return num + (9 - 6) * n;
+            }
+
+            n /= 10;
+        }
+        
+        return num;
+    }
+}
