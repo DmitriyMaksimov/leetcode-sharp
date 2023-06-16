c03f9c877cba3ceaf995ea89ec468051e047e257
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-16 18:17:47 +1000

    1569. Number of Ways to Reorder Array to Get Same BST

---
+++ UnitTests/1500/S01569Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01569Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01569();
+        sut.NumOfWays(new[] {2, 1, 3}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01569();
+        sut.NumOfWays(new[] {3, 4, 5, 1, 2}).Should().Be(5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01569();
+        sut.NumOfWays(new[] {1, 2, 3}).Should().Be(0);
+    }
+}
