be025ff57fb5bd77d5a5391593732bae1dab9549
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-16 22:59:35 +1000

    2006. Count Number of Pairs With Absolute Difference K

---
+++ UnitTests/2000/S02006Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02006Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02006();
+        sut.CountKDifference(new[] {1, 2, 2, 1}, 1).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02006();
+        sut.CountKDifference(new[] {1, 3}, 3).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02006();
+        sut.CountKDifference(new[] {3, 2, 1, 5, 4}, 2).Should().Be(3);
+    }
+}
