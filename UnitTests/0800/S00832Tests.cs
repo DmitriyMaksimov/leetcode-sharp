26fee49f50c3cea50534431db7bb13d1b4a1bf3b
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-17 19:07:22 +1000

    832. Flipping an Image

---
+++ UnitTests/0800/S00832Tests.cs
@@ -0,0 +1,30 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00832Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00832();
+        var image = sut.FlipAndInvertImage(new[] {new[] {1, 1, 0}, new[] {1, 0, 1}, new[] {0, 0, 0}});
+        image.Should().HaveCount(3);
+        image[0].Should().Equal(1, 0, 0);
+        image[1].Should().Equal(0, 1, 0);
+        image[2].Should().Equal(1, 1, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00832();
+        var image = sut.FlipAndInvertImage(new[] {new[] {1, 1, 0, 0}, new[] {1, 0, 0, 1}, new[] {0, 1, 1, 1}, new[] {1, 0, 1, 0}});
+        image.Should().HaveCount(4);
+        image[0].Should().Equal(1, 1, 0, 0);
+        image[1].Should().Equal(0, 1, 1, 0);
+        image[2].Should().Equal(0, 0, 0, 1);
+        image[3].Should().Equal(1, 0, 1, 0);
+    }
+}
---
+++ leetcode-sharp/0800/S00832.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 832. Flipping an Image
+// https://leetcode.com/problems/flipping-an-image/
+public class S00832
+{
+    public int[][] FlipAndInvertImage(int[][] image)
+    {
+        var n = image.Length;
+        var m = image[0].Length;
+
+        for (var row = 0; row < n; row++)
+        {
+            for (var col = 0; col < m / 2; col++)
+            {
+                var rightIndex = ^(col + 1);
+                (image[row][col], image[row][rightIndex]) = (image[row][rightIndex], image[row][col]);
+            }
+        }
+
+        for (var row = 0; row < n; row++)
+        {
+            for (var col = 0; col < m; col++)
+            {
+                image[row][col] = image[row][col] == 1 ? 0 : 1;
+            }
+        }
+
+        return image;
+    }
+}
