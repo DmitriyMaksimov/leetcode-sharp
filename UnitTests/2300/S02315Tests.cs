22c32bb077c00e9e9f4ac1d045fbe8e544a5aab8
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-17 13:44:43 +1000

    2315. Count Asterisks

---
+++ UnitTests/2300/S02315Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02315Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02315();
+        sut.CountAsterisks("l|*e*et|c**o|*de|").Should().Be(2);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S02315();
+        sut.CountAsterisks("iamprogrammer").Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02315();
+        sut.CountAsterisks("yo|uar|e**|b|e***au|tifu|l").Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/2300/S02315.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 2315. Count Asterisks
+// https://leetcode.com/problems/count-asterisks/
+public class S02315
+{
+    public int CountAsterisks(string s)
+    {
+        var count = 0;
+        var insidePipes = false;
+
+        foreach (var ch in s)
+        {
+            switch (ch)
+            {
+                case '|':
+                    insidePipes = !insidePipes;
+                    break;
+                case '*' when !insidePipes:
+                    ++count;
+                    break;
+            }
+        }
+        
+        return count;
+    }
+}
