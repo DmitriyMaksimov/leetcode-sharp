77ee6e47759d3c850faf41fbe91ff4a77a548747
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-17 14:03:06 +1000

    1021. Remove Outermost Parentheses

---
+++ leetcode-sharp/1000/S01021.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1021. Remove Outermost Parentheses
+// https://leetcode.com/problems/remove-outermost-parentheses/
+public class S01021
+{
+    public string RemoveOuterParentheses(string s)
+    {
+        var sb = new StringBuilder();
+        var opened = 0;
+
+        foreach (var c in s)
+        {
+            if (c == '(' && opened++ > 0)
+            {
+                sb.Append(c);
+            }
+
+            if (c == ')' && opened-- > 1)
+            {
+                sb.Append(c);
+            }
+        }
+        
+        return sb.ToString();
+    }
+}
