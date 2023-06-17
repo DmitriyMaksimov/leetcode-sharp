906798d3b05fff22e9d1dc2c40d1a3089e06cd82
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-17 20:45:38 +1000

    1967. Number of Strings That Appear as Substrings in Word

---
+++ leetcode-sharp/1900/S01967.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 1967. Number of Strings That Appear as Substrings in Word
+// https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/
+public class S01967
+{
+    public int NumOfStrings(string[] patterns, string word)
+    {
+        return patterns.Count(word.Contains);
+    }
+}
