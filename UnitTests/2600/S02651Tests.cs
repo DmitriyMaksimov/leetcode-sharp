ff7c15b3951a726f4c0bb6501e287b8aa2c74bd1
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-17 14:12:07 +1000

    2651. Calculate Delayed Arrival Time

---
+++ UnitTests/2600/S02651Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02651Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02651();
+        sut.FindDelayedArrivalTime(15, 5).Should().Be(20);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02651();
+        sut.FindDelayedArrivalTime(13, 11).Should().Be(0);
+    }
+}
