2b8b193ed2bb5be3000db6e3fc668184e1867e2c
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-18 16:24:10 +1000

    2328. Number of Increasing Paths in a Grid

---
+++ UnitTests/2300/S02328Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02328Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02328();
+        sut.CountPaths(new[] {new[] {1, 1}, new[] {3, 4}}).Should().Be(8);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02328();
+        sut.CountPaths(new[] {new[] {1}, new[] {2}}).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/2300/S02328.cs
@@ -0,0 +1,56 @@
+﻿namespace leetcode_sharp;
+
+// 2328. Number of Increasing Paths in a Grid
+// https://leetcode.com/problems/number-of-increasing-paths-in-a-grid/
+public class S02328
+{
+    private const int Mod = 1_000_000_007;
+
+    public int CountPaths(int[][] grid)
+    {
+        var n = grid.Length;
+        var m = grid[0].Length;
+
+        var dp = new int[n, m];
+        for (var row = 0; row < n; row++)
+        {
+            for (var col = 0; col < m; col++)
+            {
+                dp[row, col] = -1;
+            }
+        }
+
+        var paths = 0;
+        for (var row = 0; row < n; row++)
+        {
+            for (var col = 0; col < m; col++)
+            {
+                paths = (paths + solve(grid, row, col, -1, dp)) % Mod;
+            }
+        }
+
+        return paths;
+    }
+
+    private static int solve(int[][] grid, int row, int col, int prevValue, int[,] dp)
+    {
+        if (row < 0 || col < 0 || row >= grid.Length || col >= grid[0].Length || grid[row][col] <= prevValue)
+        {
+            return 0;
+        }
+
+        if (dp[row, col] != -1)
+        {
+            return dp[row, col];
+        }
+
+        var value = grid[row][col];
+        
+        var left = solve(grid, row, col - 1, value, dp);
+        var right = solve(grid, row, col + 1, value, dp);
+        var up = solve(grid, row - 1, col, value, dp);
+        var down = solve(grid, row + 1, col, value, dp);
+
+        return dp[row, col] = (left + right + up + down + 1) % Mod;
+    }
+}
