edc092f64877465cb4fec818acf009f57224dd29
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-18 18:19:20 +1000

    1863. Sum of All Subset XOR Totals

---
+++ UnitTests/1800/S01863Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01863Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01863();
+        sut.SubsetXORSum(new[] {1, 3}).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01863();
+        sut.SubsetXORSum(new[] {5, 1, 6}).Should().Be(28);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01863();
+        sut.SubsetXORSum(new[] {3, 4, 5, 6, 7, 8}).Should().Be(480);
+    }
+}
---
+++ leetcode-sharp/1800/S01863.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 1863. Sum of All Subset XOR Totals
+// https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+public class S01863
+{
+    public int SubsetXORSum(int[] nums)
+    {
+        return getAns(nums, 0, 0);
+    }
+
+    private static int getAns(int[] arr, int i, int cur)
+    {
+        if (i == arr.Length)
+        {
+            return cur;
+        }
+
+        return getAns(arr, i + 1, cur ^ arr[i]) + getAns(arr, i + 1, cur);
+    }
+}
