218e0d5aceccfbdef28eb260183437dc4289a9f5
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-18 18:09:59 +1000

    1464. Maximum Product of Two Elements in an Array

---
+++ UnitTests/1400/S01464Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01464Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01464();
+        sut.MaxProduct(new[] {3, 4, 5, 2}).Should().Be(12);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01464();
+        sut.MaxProduct(new[] {1, 5, 4, 5}).Should().Be(16);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01464();
+        sut.MaxProduct(new[] {3, 7}).Should().Be(12);
+    }
+}
---
+++ leetcode-sharp/1400/S01464.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 1464. Maximum Product of Two Elements in an Array
+// https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+public class S01464
+{
+    public int MaxProduct(int[] nums)
+    {
+        var largestA = int.MinValue;
+        var largestB = int.MinValue;
+
+        foreach (var value in nums)
+        {
+            if (value > largestA)
+            {
+                largestB = largestA;
+                largestA = value;
+            }
+            else if (value > largestB)
+            {
+                largestB = value;
+            }
+        }
+
+        return (largestA - 1) * (largestB - 1);
+    }
+}
