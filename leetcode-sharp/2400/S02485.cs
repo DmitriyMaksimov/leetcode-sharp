58e6475a9a0e7ea81803bba4741870fadfc84430
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-18 18:51:59 +1000

    2485. Find the Pivot Integer

---
+++ leetcode-sharp/2400/S02485.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 2485. Find the Pivot Integer
+// https://leetcode.com/problems/find-the-pivot-integer/
+public class S02485
+{
+    public int PivotInteger(int n)
+    {
+        var totalSum = n * (n + 1) / 2;
+        var sumRight = 0;
+
+        for (var i = 1; i <= n; i++)
+        {
+            sumRight += i;
+            if (totalSum - sumRight + i == sumRight)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
