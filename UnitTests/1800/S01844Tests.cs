940c767626abd11b6e5334eb7d5035ad81199366
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-18 17:50:29 +1000

    1844. Replace All Digits with Characters

---
+++ UnitTests/1800/S01844Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01844Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01844();
+        sut.ReplaceDigits("a1c1e1").Should().Be("abcdef");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01844();
+        sut.ReplaceDigits("a1b2c3d4e").Should().Be("abbdcfdhe");
+    }
+}
