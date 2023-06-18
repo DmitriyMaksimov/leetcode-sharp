1741411274de237d32eb642c6b2567b8f1d1fd40
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-18 16:50:00 +1000

    1534. Count Good Triplets

---
+++ leetcode-sharp/1500/S01534.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 1534. Count Good Triplets
+// https://leetcode.com/problems/count-good-triplets/
+public class S01534
+{
+    public int CountGoodTriplets(int[] arr, int a, int b, int c)
+    {
+        var count = 0;
+
+        for (var i = 0; i < arr.Length; i++)
+        {
+            for (var j = i + 1; j < arr.Length; j++)
+            {
+                for (var k = j + 1; k < arr.Length; k++)
+                {
+                    if (Math.Abs(arr[i] - arr[j]) <= a && Math.Abs(arr[j] - arr[k]) <= b && Math.Abs(arr[i] - arr[k]) <= c)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
