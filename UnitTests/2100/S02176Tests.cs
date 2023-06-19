19cb394a62c0eb2cb382feea24d103344de0c2db
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-19 10:59:21 +1000

    2176. Count Equal and Divisible Pairs in an Array

---
+++ UnitTests/2100/S02176Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02176Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02176();
+        sut.CountPairs(new[] {3, 1, 2, 2, 2, 1, 3}, 2).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02176();
+        sut.CountPairs(new[] {1, 2, 3, 4}, 1).Should().Be(0);
+    }
+}
