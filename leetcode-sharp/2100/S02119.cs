4b46e6d735086fc43e1b9701f55b2fdd1ba88711
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-19 22:15:53 +1000

    2119. A Number After a Double Reversal

---
+++ leetcode-sharp/2100/S02119.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2119. A Number After a Double Reversal
+// https://leetcode.com/problems/a-number-after-a-double-reversal/
+public class S02119
+{
+    public bool IsSameAfterReversals(int num)
+    {
+        return num == 0 || num % 10 != 0;
+    }
+}
