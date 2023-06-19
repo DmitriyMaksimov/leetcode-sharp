e7a661a2f2bffc791d295b1b91ee7becd174cfa7
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-19 19:09:01 +1000

    2108. Find First Palindromic String in the Array

---
+++ UnitTests/2100/S02108Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02108Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02108();
+        sut.FirstPalindrome(new[] {"abc", "car", "ada", "racecar", "cool"}).Should().Be("ada");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02108();
+        sut.FirstPalindrome(new[] {"notapalindrome", "racecar"}).Should().Be("racecar");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02108();
+        sut.FirstPalindrome(new[] {"def", "ghi"}).Should().Be("");
+    }
+}
---
+++ leetcode-sharp/2100/S02108.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 2108. Find First Palindromic String in the Array
+// https://leetcode.com/problems/find-first-palindromic-string-in-the-array/
+public class S02108
+{
+    public string FirstPalindrome(string[] words)
+    {
+        return words.FirstOrDefault(isPalindrome, "");
+    }
+
+    private static bool isPalindrome(string s)
+    {
+        for (var i = 0; i < s.Length / 2; i++)
+        {
+            if (s[i] != s[s.Length - i - 1]) return false;
+        }
+
+        return true;
+    }
+}
