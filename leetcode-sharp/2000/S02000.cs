b3fc98e8622a26c648a9cf80ea502d67cc521b3c
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-19 16:29:09 +1000

    2000. Reverse Prefix of Word

---
+++ leetcode-sharp/2000/S02000.cs
@@ -0,0 +1,12 @@
+﻿namespace leetcode_sharp;
+
+// 2000. Reverse Prefix of Word
+// https://leetcode.com/problems/reverse-prefix-of-word/
+public class S02000
+{
+    public string ReversePrefix(string word, char ch)
+    {
+        var index = word.IndexOf(ch);
+        return index < 0 ? word : new string(word[..(index + 1)].Reverse().Concat(word[(index + 1)..]).ToArray());
+    }
+}
