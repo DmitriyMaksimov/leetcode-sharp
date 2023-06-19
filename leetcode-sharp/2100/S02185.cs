02295f8542b862add44dfcb896226d1051f5764a
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-19 22:28:00 +1000

    2185. Counting Words With a Given Prefix

---
+++ leetcode-sharp/2100/S02185.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2185. Counting Words With a Given Prefix
+// https://leetcode.com/problems/counting-words-with-a-given-prefix/
+public class S02185
+{
+    public int PrefixCount(string[] words, string pref)
+    {
+        return words.Count(x => x.StartsWith(pref));
+    }
+}
