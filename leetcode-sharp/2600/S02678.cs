72f2d921a8348a7ce45bc779ac4692227e9d72b1
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-19 22:02:04 +1000

    2678. Number of Senior Citizens

---
+++ leetcode-sharp/2600/S02678.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 2678. Number of Senior Citizens
+// https://leetcode.com/problems/number-of-senior-citizens/
+public class S02678
+{
+    public int CountSeniors(string[] details)
+    {
+        var result = 0;
+        
+        foreach (var detail in details)
+        {
+            var age = (detail[11] - '0') * 10 + (detail[12] - '0');
+            if (age > 60)
+            {
+                ++result;
+            }
+        }
+        
+        return result;
+    }
+}
