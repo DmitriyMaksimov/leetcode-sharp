3ca44bf731da873907609f911d088018126d3b74
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-19 22:22:19 +1000

    561. Array Partition

---
+++ leetcode-sharp/0500/S00561.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 561. Array Partition
+// https://leetcode.com/problems/array-partition/
+public class S00561
+{
+    public int ArrayPairSum(int[] nums)
+    {
+        Array.Sort(nums);
+
+        var result = 0;
+        
+        for (var i = 0; i < nums.Length; i += 2)
+        {
+            result += nums[i];
+        }
+        
+        return result;
+    }
+}
