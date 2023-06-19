fbf0d66c7fadc61bf9fec1c7d8f3039b6604c969
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-19 19:35:50 +1000

    728. Self Dividing Numbers

---
+++ UnitTests/0700/S00728Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00728Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00728();
+        sut.SelfDividingNumbers(1, 22).Should().Equal(1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00728();
+        sut.SelfDividingNumbers(47, 85).Should().Equal(48, 55, 66, 77);
+    }
+}
---
+++ leetcode-sharp/0700/S00728.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 728. Self Dividing Numbers
+// https://leetcode.com/problems/self-dividing-numbers/
+public class S00728
+{
+    public IList<int> SelfDividingNumbers(int left, int right)
+    {
+        return Enumerable.Range(left, right - left + 1).Where(isSelfDividing).ToArray();
+    }
+
+    private static bool isSelfDividing(int num)
+    {
+        var n = num;
+
+        while (n > 0)
+        {
+            var digit = n % 10;
+
+            if (digit == 0 || num % digit != 0)
+            {
+                return false;
+            }
+
+            n /= 10;
+        }
+
+        return true;
+    }
+}
