655e5a5bacffb65901c5d60ee5b15132623e1560
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-19 10:23:57 +1000

    1732. Find the Highest Altitude

---
+++ UnitTests/1700/S01732Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01732Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01732();
+        sut.LargestAltitude(new[] {-5, 1, 5, 0, -7}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01732();
+        sut.LargestAltitude(new[] {-4, -3, -2, -1, 4, 3, 2}).Should().Be(0);
+    }
+}
