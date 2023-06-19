d94aa3230abe759340dbe2e2d98843debf8a56e9
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-19 10:46:56 +1000

    2500. Delete Greatest Value in Each Row

---
+++ UnitTests/2500/S02500Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02500Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02500();
+        sut.DeleteGreatestValue(new[] {new[] {1, 2, 4}, new[] {3, 3, 1}}).Should().Be(8);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02500();
+        sut.DeleteGreatestValue(new[] {new[] {10}}).Should().Be(10);
+    }
+}
---
+++ leetcode-sharp/2500/S02500.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 2500. Delete Greatest Value in Each Row
+// https://leetcode.com/problems/delete-greatest-value-in-each-row/
+public class S02500
+{
+    public int DeleteGreatestValue(int[][] grid)
+    {
+        foreach (var row in grid)
+        {
+            Array.Sort(row);
+        }
+
+        var ans = 0;
+
+        for (var j = 0; j < grid[0].Length; j++)
+        {
+            ans += grid.Select(row => row[j]).Max();
+        }
+
+        return ans;
+    }
+}
