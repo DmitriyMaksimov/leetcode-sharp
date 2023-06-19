0359e76a3ea205566b90467f2a365beea4814c6d
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-19 16:36:55 +1000

    2427. Number of Common Factors

---
+++ UnitTests/2400/S02427Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02427Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02427();
+        sut.CommonFactors(12, 6).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02427();
+        sut.CommonFactors(25, 30).Should().Be(2);
+    }
+}
