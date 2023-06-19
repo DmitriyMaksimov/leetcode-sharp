9316b9e081b2dbff4bf089b7d90d1df4fd72119b
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-19 19:14:16 +1000

    1827. Minimum Operations to Make the Array Increasing

---
+++ UnitTests/1800/S01827Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01827Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01827();
+        sut.MinOperations(new[] {1, 1, 1}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01827();
+        sut.MinOperations(new[] {1, 5, 2, 4, 1}).Should().Be(14);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01827();
+        sut.MinOperations(new[] {8}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1800/S01827.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 1827. Minimum Operations to Make the Array Increasing
+// https://leetcode.com/problems/minimum-operations-to-make-the-array-increasing/
+public class S01827
+{
+    public int MinOperations(int[] nums)
+    {
+        if (nums.Length < 2)
+        {
+            return 0;
+        }
+
+        var result = 0;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            var operations = Math.Max(nums[i - 1] + 1, nums[i]);
+            result += operations - nums[i];  
+            nums[i] = operations;
+        }
+
+        return result;
+    }
+}
