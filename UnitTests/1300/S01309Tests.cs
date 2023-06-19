2d5b011c80adc0b465044e59ffbd2f44bfb8257d
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-19 18:33:06 +1000

    1309. Decrypt String from Alphabet to Integer Mapping

---
+++ UnitTests/1300/S01309Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01309Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01309();
+        sut.FreqAlphabets("10#11#12").Should().Be("jkab");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01309();
+        sut.FreqAlphabets("1326#").Should().Be("acz");
+    }
+}
---
+++ leetcode-sharp/1300/S01309.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1309. Decrypt String from Alphabet to Integer Mapping
+// https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/
+public class S01309
+{
+    public string FreqAlphabets(string s)
+    {
+        var result = new StringBuilder();
+        
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (i + 2 < s.Length && s[i + 2] == '#')
+            {
+                var n = (s[i] - '0') * 10 + (s[i + 1] - '0');
+                result.Append((char) ('j' + n - 10));
+                i += 2;
+            }
+            else
+            {
+                result.Append((char) ('a' + s[i] - '1'));
+            }
+        }
+
+        return result.ToString();
+    }
+}
