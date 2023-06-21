04edd4f46ae7f0b6523d6fdf4a4e73d25e654d21
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-21 22:23:52 +1000

    1941. Check if All Characters Have Equal Number of Occurrences

---
+++ UnitTests/1900/S01941Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01941Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01941();
+        sut.AreOccurrencesEqual("abacbc").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01941();
+        sut.AreOccurrencesEqual("aaabb").Should().BeFalse();
+    }
+}
