0d9c05d38c98433317a1a6b4c0d7bff02cf4332d
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-21 19:14:02 +1000

    2448. Minimum Cost to Make Array Equal

---
+++ UnitTests/2400/S02448Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02448Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02448();
+        sut.MinCost(new[] {1, 3, 5, 2}, new[] {2, 3, 1, 14}).Should().Be(8);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02448();
+        sut.MinCost(new[] {2, 2, 2, 2, 2}, new[] {4, 2, 8, 1, 3}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2400/S02448.cs
@@ -0,0 +1,46 @@
+﻿namespace leetcode_sharp;
+
+// 2448. Minimum Cost to Make Array Equal
+// https://leetcode.com/problems/minimum-cost-to-make-array-equal/
+public class S02448
+{
+    public long MinCost(int[] nums, int[] cost)
+    {
+        var n = nums.Length;
+
+        var numsAndCost = new int[n][];
+        for (var i = 0; i < n; ++i)
+        {
+            numsAndCost[i] = new [] {nums[i], cost[i]};
+        }
+
+        Array.Sort(numsAndCost, (a, b) => a[0] - b[0]);
+
+        // Get the prefix sum array of the costs
+        var prefixSumCost = new long[n];
+        prefixSumCost[0] = numsAndCost[0][1];
+
+        for (var i = 1; i < n; ++i)
+        {
+            prefixSumCost[i] = prefixSumCost[i - 1] + numsAndCost[i][1];
+        }
+
+        var totalCost = 0L;
+        for (var i = 1; i < n; ++i)
+        {
+            totalCost += (long)numsAndCost[i][1] * (numsAndCost[i][0] - numsAndCost[0][0]);
+        }
+
+        var result = totalCost;
+
+        for (var i = 1; i < n; ++i)
+        {
+            var gap = numsAndCost[i][0] - numsAndCost[i - 1][0];
+            totalCost += prefixSumCost[i - 1] * gap;
+            totalCost -= (prefixSumCost[n - 1] - prefixSumCost[i - 1]) * gap;
+            result = Math.Min(result, totalCost);
+        }
+
+        return result;
+    }
+}
