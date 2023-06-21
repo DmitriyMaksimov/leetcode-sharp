f035a47252547f9f9754a22ca8bd42b25d33e5a3
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-21 22:34:52 +1000

    1304. Find N Unique Integers Sum up to Zero

---
+++ UnitTests/1300/S01304Tests.cs
@@ -0,0 +1,37 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01304Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01304();
+        var sumZero = sut.SumZero(5);
+        sumZero.Should().HaveCount(5);
+        sumZero.Should().OnlyHaveUniqueItems();
+        sumZero.Sum().Should().Be(0);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S01304();
+        var sumZero = sut.SumZero(3);
+        sumZero.Should().HaveCount(3);
+        sumZero.Should().OnlyHaveUniqueItems();
+        sumZero.Sum().Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01304();
+        var sumZero = sut.SumZero(1);
+        sumZero.Should().HaveCount(1);
+        sumZero.Should().OnlyHaveUniqueItems();
+        sumZero.Sum().Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1300/S01304.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 1304. Find N Unique Integers Sum up to Zero
+// https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+public class S01304
+{
+    public int[] SumZero(int n)
+    {
+        var result = new int[n];
+
+        var i = 0;
+
+        if (n % 2 == 1)
+        {
+            result[i++] = 0;
+        }
+
+        while (i < n)
+        {
+            result[i] = i + 1;
+            result[i + 1] = -(i + 1);
+            i += 2;
+        }
+
+        return result;
+    }
+}
