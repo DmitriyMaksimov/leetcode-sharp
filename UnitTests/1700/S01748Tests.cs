067307afbefd474b3cf4924c3b08506fd4f8f8d1
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-22 22:45:57 +1000

    1748. Sum of Unique Elements

---
+++ UnitTests/1700/S01748Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01748Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01748();
+        sut.SumOfUnique(new[] {1, 2, 3, 2}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01748();
+        sut.SumOfUnique(new[] {1, 1, 1, 1, 1}).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01748();
+        sut.SumOfUnique(new[] {1, 2, 3, 4, 5}).Should().Be(15);
+    }
+}
---
+++ leetcode-sharp/1700/S01748.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode_sharp;
+
+// 1748. Sum of Unique Elements
+// https://leetcode.com/problems/sum-of-unique-elements/
+public class S01748
+{
+    public int SumOfUnique(int[] nums)
+    {
+        var dictionary = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            dictionary[num] = dictionary.GetValueOrDefault(num) + 1;
+        }
+
+        return dictionary.Where(x => x.Value == 1).Select(x => x.Key).Sum();
+    }
+}
