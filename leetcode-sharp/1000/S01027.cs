0d3857c0cd60b52629abb2aa7eebc224e4533c35
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-23 20:25:37 +1000

    1027. Longest Arithmetic Subsequence

---
+++ leetcode-sharp/1000/S01027.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 1027. Longest Arithmetic Subsequence
+// https://leetcode.com/problems/longest-arithmetic-subsequence/
+public class S01027
+{
+    public int LongestArithSeqLength(int[] nums)
+    {
+        var n = nums.Length;
+        if (n <= 2)
+        {
+            return n;
+        }
+
+        var longest = 2;
+        var dp = new Dictionary<int, int>[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            dp[i] = new Dictionary<int, int>();
+            for (var j = 0; j < i; j++)
+            {
+                var diff = nums[i] - nums[j];
+                dp[i][diff] = dp[j].GetValueOrDefault(diff, 1) + 1;
+                longest = Math.Max(longest, dp[i][diff]);
+            }
+        }
+
+        return longest;
+    }
+}
