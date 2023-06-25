3879de2f45beaf3045593b669c2702d041c3324e
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-25 18:55:12 +1000

    657. Robot Return to Origin

---
+++ UnitTests/0600/S00657Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00657Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00657();
+        sut.JudgeCircle("UD").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00657();
+        sut.JudgeCircle("LL").Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0600/S00657.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 657. Robot Return to Origin
+// https://leetcode.com/problems/robot-return-to-origin/
+public class S00657
+{
+    public bool JudgeCircle(string moves)
+    {
+        var x = 0;
+        var y = 0;
+
+        foreach (var move in moves)
+        {
+            switch (move)
+            {
+                case 'D':
+                    ++y;
+                    break;
+                case 'U':
+                    --y;
+                    break;
+                case 'L':
+                    --x;
+                    break;
+                case 'R':
+                    ++x;
+                    break;
+            }
+        }
+        
+        return x == 0 && y == 0;
+    }
+}
