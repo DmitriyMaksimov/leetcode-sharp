404ee686c15c914ad21f3bb392111e9919029aa2
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-25 18:45:07 +1000

    461. Hamming Distance

---
+++ UnitTests/0400/S00461Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00461Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00461();
+        sut.HammingDistance(1, 4).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00461();
+        sut.HammingDistance(3, 1).Should().Be(1);
+    }
+}
