b8a1c5d8089517e830eef8e509c7b5e53002e711
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-26 18:55:17 +1000

    2462. Total Cost to Hire K Workers

---
+++ UnitTests/2400/S02462Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02462Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02462();
+        sut.TotalCost(new[] {17, 12, 10, 2, 7, 2, 11, 20, 8}, 3, 4).Should().Be(11);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02462();
+        sut.TotalCost(new[] {1, 2, 4, 1}, 3, 3).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/2400/S02462.cs
@@ -0,0 +1,47 @@
+﻿namespace leetcode_sharp;
+
+// 2462. Total Cost to Hire K Workers
+// https://leetcode.com/problems/total-cost-to-hire-k-workers/
+public class S02462
+{
+    public long TotalCost(int[] costs, int k, int candidates)
+    {
+        var left = 0;
+        var right = costs.Length - 1;
+        var leftQueue = new PriorityQueue<int, int>();
+        var rightQueue = new PriorityQueue<int, int>();
+
+        var result = 0L;
+
+        while (k-- > 0)
+        {
+            while (leftQueue.Count < candidates && left <= right)
+            {
+                leftQueue.Enqueue(costs[left], costs[left]);
+                ++left;
+            }
+
+            while (rightQueue.Count < candidates && left <= right)
+            {
+                rightQueue.Enqueue(costs[right], costs[right]);
+                --right;
+            }
+
+            var leftCost = leftQueue.Count > 0 ? leftQueue.Peek() : int.MaxValue;
+            var rightCost = rightQueue.Count > 0 ? rightQueue.Peek() : int.MaxValue;
+
+            if (leftCost <= rightCost)
+            {
+                result += leftCost;
+                leftQueue.Dequeue();
+            }
+            else
+            {
+                result += rightCost;
+                rightQueue.Dequeue();
+            }
+        }
+
+        return result;
+    }
+}
