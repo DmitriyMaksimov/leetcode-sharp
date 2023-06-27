30a603101f38a1fd16f60814d5b3cabf13a2f11c
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-27 21:03:26 +1000

    2496. Maximum Value of a String in an Array

---
+++ leetcode-sharp/2400/S02496.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode_sharp;
+
+// 2496. Maximum Value of a String in an Array
+// https://leetcode.com/problems/maximum-value-of-a-string-in-an-array/
+public class S02496
+{
+    public int MaximumValue(string[] strs)
+    {
+        var result = 0;
+        
+        foreach (var str in strs)
+        {
+            result = Math.Max(result, str.All(char.IsDigit) ? int.Parse(str) : str.Length);
+        }
+
+        return result;
+    }
+}
