bff2c5024d510682c93be78d924d7ff94bd3b8a1
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-27 20:32:49 +1000

    1290. Convert Binary Number in a Linked List to Integer

---
+++ leetcode-sharp/1200/S01290.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 1290. Convert Binary Number in a Linked List to Integer
+// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/description/
+public class S01290
+{
+    public int GetDecimalValue(ListNode? head)
+    {
+        var result = 0;
+
+        while (head != null)
+        {
+            result *= 2;
+            result += head.val;
+            head = head.next;
+        }
+        
+        return result;
+    }
+}
