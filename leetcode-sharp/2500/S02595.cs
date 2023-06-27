0f0aa7b0a60ea27d4d14028f9fc8e1526b84e13e
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-27 20:45:44 +1000

    2595. Number of Even and Odd Bits

---
+++ leetcode-sharp/2500/S02595.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 2595. Number of Even and Odd Bits
+// https://leetcode.com/problems/number-of-even-and-odd-bits/
+public class S02595
+{
+    public int[] EvenOddBit(int n)
+    {
+        var even = 0;
+        var odd = 0;
+
+        while (n > 0)
+        {
+            if (n % 2 == 1)
+            {
+                ++even;
+            }
+
+            n /= 2;
+
+            if (n % 2 == 1)
+            {
+                ++odd;
+            }
+
+            n /= 2;
+        }
+
+        return new[] {even, odd};
+    }
+}
