c6437f1f4cdb1437d9800fbbddc5ba33422c622b
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-27 22:00:12 +1000

    559. Maximum Depth of N-ary Tree

---
+++ leetcode-sharp/0500/S00559.cs
@@ -0,0 +1,50 @@
+﻿namespace leetcode_sharp;
+
+// 559. Maximum Depth of N-ary Tree
+// https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+public class S00559
+{
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node()
+        {
+        }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    private int max;
+
+    public int MaxDepth(Node? root)
+    {
+        dfs(root, 0);
+        return max;
+    }
+
+    private void dfs(Node? node, int d)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        max = Math.Max(max, d + 1);
+
+        foreach (var child in node.children)
+        {
+            dfs(child, d + 1);
+        }
+    }
+}
