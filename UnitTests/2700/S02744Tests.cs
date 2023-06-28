79c7b5b4979d7a4a2f1de413c1ebb17693dba399
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-28 22:56:47 +1000

    2744. Find Maximum Number of String Pairs

---
+++ UnitTests/2700/S02744Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02744Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02744();
+        sut.MaximumNumberOfStringPairs(new[] {"cd", "ac", "dc", "ca", "zz"}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02744();
+        sut.MaximumNumberOfStringPairs(new[] {"ab", "ba", "cc"}).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02744();
+        sut.MaximumNumberOfStringPairs(new[] {"aa", "ab"}).Should().Be(0);
+    }
+}
