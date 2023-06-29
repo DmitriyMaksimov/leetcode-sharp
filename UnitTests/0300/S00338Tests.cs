08224b798ad09a476475eeaaf91afcc0b8ba7b3a
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-29 20:46:25 +1000

    338. Counting Bits

---
+++ UnitTests/0300/S00338Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00338Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00338();
+        sut.CountBits(2).Should().Equal(0, 1, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00338();
+        sut.CountBits(5).Should().Equal(0, 1, 1, 2, 1, 2);
+    }
+}
