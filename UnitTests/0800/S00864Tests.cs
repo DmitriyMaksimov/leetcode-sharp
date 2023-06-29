e96af9849fb5e08e8ec37f9c069c2598a1be9f1e
Dmitriy <dmitriy.maksimov@workday.com>
2023-06-29 13:57:53 +1000

    864. Shortest Path to Get All Keys

---
+++ UnitTests/0800/S00864Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00864Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00864();
+        sut.ShortestPathAllKeys(new[] {"@.a..", "###.#", "b.A.B"}).Should().Be(8);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00864();
+        sut.ShortestPathAllKeys(new[] {"@..aA", "..B#.", "....b"}).Should().Be(6);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00864();
+        sut.ShortestPathAllKeys(new[] {"@Aa"}).Should().Be(-1);
+    }
+}
