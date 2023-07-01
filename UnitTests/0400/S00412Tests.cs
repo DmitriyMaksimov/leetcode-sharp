7ddfd8fc13b3d63aa901cca7239c9547439b0543
Dmitriy <dmitriy.maksimov@workday.com>
2023-07-01 14:14:38 +1000

    412. Fizz Buzz

---
+++ UnitTests/0400/S00412Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00412Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00412();
+        sut.FizzBuzz(3).Should().Equal("1", "2", "Fizz");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00412();
+        sut.FizzBuzz(5).Should().Equal("1", "2", "Fizz", "4", "Buzz");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00412();
+        sut.FizzBuzz(15).Should().Equal("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz");
+    }
+}
---
+++ leetcode-sharp/0400/S00412.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 412. Fizz Buzz
+// https://leetcode.com/problems/fizz-buzz/
+public class S00412
+{
+    public IList<string> FizzBuzz(int n)
+    {
+        return Enumerable.Range(1, n).Select(fooBar).ToArray();
+    }
+
+    private static string fooBar(int n)
+    {
+        if (n % 3 == 0 && n % 5 == 0)
+        {
+            return "FizzBuzz";
+        }
+
+        if (n % 3 == 0)
+        {
+            return "Fizz";
+        }
+        
+        return n % 5 == 0 ? "Buzz" : n.ToString();
+    }
+}
