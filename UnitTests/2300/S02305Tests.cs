0ba176985bba471370d0d9ae359760ec1b1f8e71
Dmitriy <dmitriy.maksimov@workday.com>
2023-07-01 13:27:16 +1000

    2305. Fair Distribution of Cookies

---
+++ UnitTests/2300/S02305Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02305Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02305();
+        sut.DistributeCookies(new[] {8, 15, 10, 20, 8}, 2).Should().Be(31);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02305();
+        sut.DistributeCookies(new[] {6, 1, 3, 2, 2, 4, 1, 2}, 3).Should().Be(7);
+    }
+}
---
+++ leetcode-sharp/2300/S02305.cs
@@ -0,0 +1,37 @@
+﻿namespace leetcode_sharp;
+
+// 2305. Fair Distribution of Cookies
+// https://leetcode.com/problems/fair-distribution-of-cookies/
+public class S02305
+{
+    private int[] _cookiesOfChild;
+    private int _result = int.MaxValue;
+
+    public int DistributeCookies(int[] cookies, int k)
+    {
+        _cookiesOfChild = new int[k];
+        backTrack(cookies, 0);
+        return _result;
+    }
+
+    private void backTrack(IReadOnlyList<int> cookies, int i)
+    {
+        for (var j = 0; j < _cookiesOfChild.Length; j++)
+        {
+            // Temporary give `cookies[i]` to child `j`
+            _cookiesOfChild[j] += cookies[i];
+
+            if (i == cookies.Count - 1)
+            {
+                _result = Math.Min(_result, _cookiesOfChild.Max());
+            }
+            else
+            {
+                backTrack(cookies, i + 1);
+            }
+
+            // Subtract the cookies
+            _cookiesOfChild[j] -= cookies[i];
+        }
+    }
+}
