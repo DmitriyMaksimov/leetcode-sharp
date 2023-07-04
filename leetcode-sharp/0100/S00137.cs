1deadc297241bf20a38a56146ec49df1bc148daf
Dmitriy <dmitriy.maksimov@workday.com>
2023-07-04 16:39:36 +1000

    137. Single Number II

---
+++ leetcode-sharp/0100/S00137.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode_sharp;
+
+// 137. Single Number II
+// https://leetcode.com/problems/single-number-ii/
+public class S00137
+{
+    public int SingleNumber(int[] nums)
+    {
+        var occurrences = new Dictionary<int, int>();
+        
+        foreach (var n in nums)
+        {
+            occurrences[n] = occurrences.GetValueOrDefault(n) + 1;
+        }
+
+        return occurrences.First(x => x.Value == 1).Key;
+    }
+}
