a6bbfacda0285e1512cc6a35bdbf80b648a369ec
Dmitriy <dmitriy.maksimov@workday.com>
2023-07-06 17:06:45 +1000

    209. Minimum Size Subarray Sum

---
+++ UnitTests/0200/S00209Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00209Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00209();
+        sut.MinSubArrayLen(7, new[] {2, 3, 1, 2, 4, 3}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00209();
+        sut.MinSubArrayLen(4, new[] {1, 4, 4}).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00209();
+        sut.MinSubArrayLen(11, new[] {1, 1, 1, 1, 1, 1, 1, 1}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0200/S00209.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 209. Minimum Size Subarray Sum
+// https://leetcode.com/problems/minimum-size-subarray-sum/
+public class S00209
+{
+    public int MinSubArrayLen(int target, int[] nums)
+    {
+        var left = 0;
+        var right = 0;
+        var sum = 0;
+        var result = int.MaxValue;
+
+        while (right < nums.Length)
+        {
+            sum += nums[right];
+            while (sum >= target)
+            {
+                result = Math.Min(result, right - left + 1);
+                sum -= nums[left++];
+            }
+
+            ++right;
+        }
+
+        return result == int.MaxValue ? 0 : result;
+    }
+}
