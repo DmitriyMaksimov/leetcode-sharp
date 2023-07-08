eee3903ef4df2cfb0f4cc6e2b577fc2a6ff70dbf
Dmitriy <dmitriy.maksimov@workday.com>
2023-07-08 12:03:35 +1000

    2024. Maximize the Confusion of an Exam

---
+++ UnitTests/2000/S02024Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02024Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02024();
+        sut.MaxConsecutiveAnswers("TTFF", 2).Should().Be(4);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S02024();
+        sut.MaxConsecutiveAnswers("TFFT", 1).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02024();
+        sut.MaxConsecutiveAnswers("TTFTTFTT", 1).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/2000/S02024.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 2024. Maximize the Confusion of an Exam
+// https://leetcode.com/problems/maximize-the-confusion-of-an-exam/
+public class S02024
+{
+    public int MaxConsecutiveAnswers(string answerKey, int k)
+    {
+        var maxFreq = 0;
+        var left = 0;
+        var charCount = new Dictionary<char, int>();
+
+        for (var right = 0; right < answerKey.Length; right++)
+        {
+            var currentChar = answerKey[right];
+            charCount[currentChar] = charCount.GetValueOrDefault(currentChar) + 1;
+            maxFreq = Math.Max(maxFreq, charCount[currentChar]);
+
+            if (right - left + 1 > maxFreq + k)
+            {
+                charCount[answerKey[left]] = charCount.GetValueOrDefault(answerKey[left]) - 1;
+                left++;
+            }
+        }
+
+        return answerKey.Length - left;
+    }
+}
