37a443d93b4ef1931d2a6513b5175707fb8c7925
Dmitriy <dmitriy.maksimov@workday.com>
2023-07-08 12:03:18 +1000

    2551. Put Marbles in Bags

---
+++ UnitTests/2500/S02551Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02551Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02551();
+        sut.PutMarbles(new[] {1, 3, 5, 1}, 2).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02551();
+        sut.PutMarbles(new[] {1, 3}, 2).Should().Be(0);
+    }
+}
