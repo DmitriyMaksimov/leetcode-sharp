d2d811f9b4afabae7a6addc7d24f6ab2863a7b64
Dmitriy <dmitriy.maksimov@workday.com>
2023-07-10 21:03:51 +1000

    500. Keyboard Row

---
+++ UnitTests/0500/S00500Tests.cs
@@ -0,0 +1,42 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00500Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00500();
+        sut.FindWords(new[] {"Hello", "Alaska", "Dad", "Peace"}).Should().Equal("Alaska", "Dad");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00500();
+        sut.FindWords(new[] {"omk"}).Should().Equal();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00500();
+        sut.FindWords(new[] {"adsdf", "sfd"}).Should().Equal("adsdf", "sfd");
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S00500();
+        sut.FindWords(new[] {"a", "b"}).Should().Equal("a", "b");
+    }
+
+    [Test]
+    public void T5()
+    {
+        var sut = new S00500();
+        sut.FindWords(new[] {"qz", "wq", "asdddafadsfa", "adfadfadfdassfawde"}).Should().Equal("wq", "asdddafadsfa");
+    }
+}
