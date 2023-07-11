33f6ff8ba106adaf40ad3bcb8844bb68dce867ba
Dmitriy <dmitriy.maksimov@workday.com>
2023-07-11 19:16:39 +1000

    863. All Nodes Distance K in Binary Tree

---
+++ leetcode-sharp/0800/S00863.cs
@@ -0,0 +1,47 @@
+﻿namespace leetcode_sharp;
+
+// 863. All Nodes Distance K in Binary Tree
+// https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
+public class S00863
+{
+    private readonly Dictionary<TreeNode, TreeNode?> _parentMap = new();
+    private readonly HashSet<TreeNode?> _visited = new();
+
+    public IList<int> DistanceK(TreeNode root, TreeNode target, int k)
+    {
+        dfs(root, null);
+        return findNodes(target, k);
+    }
+
+    private void dfs(TreeNode? node, TreeNode? parent)
+    {
+        if (node == null) return;
+
+        _parentMap[node] = parent;
+        dfs(node.left, node);
+        dfs(node.right, node);
+    }
+
+    private List<int> findNodes(TreeNode? origin, int distance)
+    {
+        if (origin == null)
+        {
+            return new List<int>();
+        }
+
+        if (_visited.Contains(origin)) return new List<int>();
+
+        if (distance == 0)
+        {
+            return new List<int> {origin.val};
+        }
+
+        _visited.Add(origin);
+
+        var res1 = findNodes(origin.left, distance - 1);
+        var res2 = findNodes(origin.right, distance - 1);
+        var res3 = findNodes(_parentMap[origin], distance - 1);
+
+        return res1.Concat(res2).Concat(res3).ToList();
+    }
+}
