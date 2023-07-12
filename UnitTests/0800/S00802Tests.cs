86fd1c42d16f0578a2af4017c216aafc28789275
Dmitriy <dmitriy.maksimov@workday.com>
2023-07-12 21:09:36 +1000

    802. Find Eventual Safe States

---
+++ UnitTests/0800/S00802Tests.cs
@@ -0,0 +1,24 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00802Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00802();
+        sut.EventualSafeNodes(
+            new[] {new[] {1, 2}, new[] {2, 3}, new[] {5}, new[] {0}, new []{5}, Array.Empty<int>(), Array.Empty<int>()})
+            .Should().Equal(2, 4, 5, 6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00802();
+        sut.EventualSafeNodes(new[] {new[] {1, 2, 3, 4}, new[] {1, 2}, new[] {3, 4}, new[] {0, 4}, Array.Empty<int>()})
+            .Should().Equal(4);
+    }
+}
