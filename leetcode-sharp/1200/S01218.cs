7e7884759b1f95c2820bfe7e7e87b19eab3d5146
Dmitriy <dmitriy.maksimov@workday.com>
2023-07-14 17:59:08 +1000

    1218. Longest Arithmetic Subsequence of Given Difference

---
+++ leetcode-sharp/1200/S01218.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 1218. Longest Arithmetic Subsequence of Given Difference
+// https://leetcode.com/problems/longest-arithmetic-subsequence-of-given-difference/
+public class S01218
+{
+    public int LongestSubsequence(int[] arr, int difference)
+    {
+        var dp = new Dictionary<int, int>();
+        var result = 0;
+
+        foreach (var n in arr)
+        {
+            dp[n] = dp.GetValueOrDefault(n - difference) + 1;
+            result = Math.Max(dp[n], result);
+        }
+
+        return result;
+    }
+}
