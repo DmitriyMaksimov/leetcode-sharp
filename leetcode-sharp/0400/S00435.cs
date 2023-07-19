bd5737754fa20ef9dbdc381a57f056237e75e112
Dmitriy <dmitriy.maksimov@workday.com>
2023-07-19 15:12:49 +1000

    435. Non-overlapping Intervals

---
+++ leetcode-sharp/0400/S00435.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 435. Non-overlapping Intervals
+// https://leetcode.com/problems/non-overlapping-intervals/
+public class S00435
+{
+    public int EraseOverlapIntervals(int[][] intervals)
+    {
+        var n = intervals.Length;
+        Array.Sort(intervals, (a, b) => a[1] - b[1]);
+
+        var prev = 0;
+        var count = 1;
+
+        for (var i = 1; i < n; i++)
+        {
+            if (intervals[i][0] >= intervals[prev][1])
+            {
+                prev = i;
+                count++;
+            }
+        }
+
+        return n - count;
+    }
+}
