656558919f0e92d7831a578db7de641242c1f973
Dmitriy <dmitriy.maksimov@workday.com>
2023-07-22 12:35:46 +1000

    1028. Recover a Tree From Preorder Traversal

---
+++ UnitTests/1000/S01028Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01028Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01028();
+        sut.RecoverFromPreorder("1-2--3--4-5--6--7").Should().BeEquivalentTo(TreeNode.Parse("[1,2,5,3,4,6,7]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01028();
+        sut.RecoverFromPreorder("1-2--3---4-5--6---7").Should().BeEquivalentTo(TreeNode.Parse("[1,2,5,3,null,6,null,4,null,7]"));
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01028();
+        sut.RecoverFromPreorder("1-401--349---90--88").Should().BeEquivalentTo(TreeNode.Parse("[1,401,null,349,88,90]"));
+    }
+}
