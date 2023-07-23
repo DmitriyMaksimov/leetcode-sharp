9944d91ec336f5f6fdea27e471a37427b9950e09
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:25 +1000

    841. Keys and Rooms

---
+++ leetcode-sharp/S00841.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 841. Keys and Rooms
+// https://leetcode.com/problems/keys-and-rooms/
+public class S00841
+{
+    public bool CanVisitAllRooms(IList<IList<int>> rooms)
+    {
+        var visitedRooms = new HashSet<int>();
+        var roomsToVisit = new Queue<int>();
+        roomsToVisit.Enqueue(0);
+
+        while (roomsToVisit.Any())
+        {
+            var currentRoom = roomsToVisit.Dequeue();
+            visitedRooms.Add(currentRoom);
+            foreach (var key in rooms[currentRoom])
+            {
+                if (visitedRooms.Contains(key))
+                {
+                    // Already visited
+                    continue;
+                }
+                roomsToVisit.Enqueue(key);
+            }
+        }
+        
+        return visitedRooms.Count == rooms.Count;
+    }
+}
