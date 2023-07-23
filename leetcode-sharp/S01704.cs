4cd33860599cd451e62fae05b4b44164b35e6c91
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:17 +1000

    1704. Determine if String Halves Are Alike

---
+++ leetcode-sharp/S01704.cs
@@ -0,0 +1,15 @@
+﻿namespace leetcode_sharp;
+
+// 1704. Determine if String Halves Are Alike
+// https://leetcode.com/problems/determine-if-string-halves-are-alike/
+public class S01704
+{
+    public bool HalvesAreAlike(string s)
+    {
+        var vowels = new HashSet<char> {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+        var halfLength = s.Length / 2;
+
+        return s[..halfLength].Count(c => vowels.Contains(c)) == s[halfLength..].Count(c => vowels.Contains(c));
+    }
+}
