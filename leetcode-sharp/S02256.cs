fcfeff3bba8b3d715f5df24eb9db703e2cb41c8b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:17 +1000

    2256. Minimum Average Difference

---
+++ leetcode-sharp/S02256.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 2256. Minimum Average Difference
+// https://leetcode.com/problems/minimum-average-difference/ 
+public class S02256
+{
+    public int MinimumAverageDifference(int[] nums)
+    {
+        var longs = nums.Select(x => (long)x);
+        var rightSum = longs.Sum();
+        var numsLength = nums.Length;
+        var averages = new long[numsLength];
+        var leftSum = 0L;
+        
+        for (var i = 0; i < numsLength; i++)
+        {
+            leftSum += nums[i];
+            rightSum -= nums[i];
+            var numberOfElementsOnRight = numsLength - i - 1;
+            if (numberOfElementsOnRight == 0)
+            {
+                numberOfElementsOnRight = 1;
+            }
+            averages[i] = Math.Abs(leftSum / (i + 1) - rightSum / numberOfElementsOnRight);
+        }
+
+        var minAverage = averages[0];
+        var index = 0;
+        
+        for (var i = 1; i < averages.Length; i++)
+        {
+            if (averages[i] < minAverage)
+            {
+                minAverage = averages[i];
+                index = i;
+            }
+        }
+        
+        return index;
+    }
+}
