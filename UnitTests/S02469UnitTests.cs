8df8733f00e6c248c82ff76dc5b512efc64a20be
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:11 +1000

    2469. Convert the Temperature

---
+++ UnitTests/S02469UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02469UnitTests
+{
+    [Test]
+    public void Test1()
+    {
+        var sut = new S02469();
+        sut.ConvertTemperature(36.50).Should().Equal(309.65000, 97.70000);
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var sut = new S02469();
+        sut.ConvertTemperature(122.11).Should().Equal(395.26000, 251.79800);
+    }
+}
