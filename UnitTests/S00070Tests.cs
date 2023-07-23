130f28e56b1822a5216a25f73c08b4f5a0114639
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:19 +1000

    70. Climbing Stairs

---
+++ UnitTests/S00070Tests.cs
@@ -0,0 +1,20 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00070Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00070();
+        sut.ClimbStairs(2).Should().Be(2);
+    }
+    [Test]
+    public void T2()
+    {
+        var sut = new S00070();
+        sut.ClimbStairs(3).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/S00070.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 70. Climbing Stairs
+// https://leetcode.com/problems/climbing-stairs/
+public class S00070
+{
+    public int ClimbStairs(int n)
+    {
+        if (n == 0) return 0;
+
+        var prev = 0;
+        var next = 1;
+        for (var i = 1; i <= n; i++)
+        {
+            var sum = prev + next;
+            prev = next;
+            next = sum;
+        }
+
+        return next;
+    }
+}
