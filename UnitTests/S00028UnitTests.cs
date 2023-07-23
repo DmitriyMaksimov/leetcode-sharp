449f9beabeeef8951d517dc80b06bfd7c9693311
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:09 +1000

    28. Find the Index of the First Occurrence in a String

---
+++ UnitTests/S00028UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00028UnitTests
+{
+    [Test]
+    public void Test1()
+    {
+        var sut = new S00028();
+        sut.StrStr("sadbutsad", "sad").Should().Be(0);
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var sut = new S00028();
+        sut.StrStr("leetcode", "leeto").Should().Be(-1);
+    }
+}
