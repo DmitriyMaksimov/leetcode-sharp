b4be84624ccfc8002cd8f870e05e63485487368e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:16 +1000

    1207. Unique Number of Occurrences

---
+++ UnitTests/S01207UnitTests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01207UnitTests
+{
+    [Test]
+    public void Test1()
+    {
+        var sut = new S01207();
+        sut.UniqueOccurrences(new[] {1, 2, 2, 1, 1, 3}).Should().BeTrue();
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var sut = new S01207();
+        sut.UniqueOccurrences(new[] {1, 2}).Should().BeFalse();
+    }
+
+    [Test]
+    public void Test3()
+    {
+        var sut = new S01207();
+        sut.UniqueOccurrences(new[] {-3, 0, 1, -3, 1, 1, 1, -3, 10, 0}).Should().BeTrue();
+    }
+}
