6266a29d8ed1417c4d6a9156ccf4cd82e8a19842
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:21 +1000

    1143. Longest Common Subsequence

---
+++ leetcode-sharp/S01143.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 1143. Longest Common Subsequence
+// https://leetcode.com/problems/longest-common-subsequence/description/
+public class S01143
+{
+    private readonly Dictionary<(int i, int j), int> _cache = new();
+    
+    public int LongestCommonSubsequence(string text1, string text2)
+    {
+        return getLCS(text1, 0, text2, 0);
+    }
+
+    private int getLCS(string a, int i, string b, int j)
+    {
+        if (i >= a.Length || j >= b.Length)
+        {
+            return 0;
+        }
+
+        if (_cache.TryGetValue((i, j), out var value))
+        {
+            return value;
+        }
+
+        if (a[i] == b[j])
+        {
+            return _cache[(i, j)] = 1 + getLCS(a, i + 1, b, j + 1);
+        }
+
+        return _cache[(i, j)] = Math.Max(getLCS(a, i + 1, b, j), getLCS(a, i, b, j + 1));
+    }
+}
