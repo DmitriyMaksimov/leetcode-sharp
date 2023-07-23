3f1f8d7f7738f1eb0b5d1722c5b321c3241fb892
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:14 +1000

    279. Perfect Squares

---
+++ UnitTests/S00279UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00279UnitTests
+{
+    [Test]
+    public void Test1()
+    {
+        var sut = new S00279();
+        sut.NumSquares(12).Should().Be(3);
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var sut = new S00279();
+        sut.NumSquares(13).Should().Be(2);
+    }
+}
