bc5434af79b95ed84267902d030f87eed8f4afe8
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:18 +1000

    876. Middle of the Linked List

---
+++ leetcode-sharp/S00876.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 876. Middle of the Linked List
+// https://leetcode.com/problems/middle-of-the-linked-list/description/
+public class S00876
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode MiddleNode(ListNode head)
+    {
+        var fast = head;
+        var slow = head;
+
+        while (fast?.next != null)
+        {
+            fast = fast.next?.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
