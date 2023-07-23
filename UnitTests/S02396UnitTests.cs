629d37271168710f84508961428236e13213a7db
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:10 +1000

    2396. Strictly Palindromic Number

---
+++ UnitTests/S02396UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02396UnitTests
+{
+    [Test]
+    public void Test1()
+    {
+        var sut = new S02396();
+        sut.IsStrictlyPalindromic(4).Should().BeFalse();
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var sut = new S02396();
+        sut.IsStrictlyPalindromic(9).Should().BeFalse();
+    }
+}
