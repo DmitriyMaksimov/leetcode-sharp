d43f2e973e90072a2c1f6ea15c607fa2d5b3d1fa
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:27 +1000

    19. Remove Nth Node From End of List

---
+++ leetcode-sharp/S00019.cs
@@ -0,0 +1,43 @@
+﻿namespace leetcode_sharp;
+
+// 19. Remove Nth Node From End of List
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+public class S00019
+{
+    public ListNode? RemoveNthFromEnd(ListNode? head, int n)
+    {
+        if (head?.next == null)
+        {
+            return null;
+        }
+
+        var current = head;
+        var nodeToDelete = head;
+        var nodePrevToDelete = head;
+
+        while (current != null)
+        {
+            current = current.next;
+            if (n > 0)
+            {
+                --n;
+            }
+            else
+            {
+                nodePrevToDelete = nodeToDelete;
+                nodeToDelete = nodeToDelete?.next;
+            }
+        }
+
+        if (nodeToDelete != head)
+        {
+            nodePrevToDelete.next = nodeToDelete?.next;
+        }
+        else
+        {
+            return nodeToDelete.next;
+        }
+
+        return head;
+    }
+}
