2a3f2c7fdca2c8aff12794b07576e3f9fd439c76
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:31 +1000

    3. Longest Substring Without Repeating Characters

---
+++ UnitTests/S00003Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00003Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00003();
+        sut.LengthOfLongestSubstring("abcabcbb").Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00003();
+        sut.LengthOfLongestSubstring("bbbbb").Should().Be(1);
+    }
+    
+    [Test]
+    public void T3()
+    {
+        var sut = new S00003();
+        sut.LengthOfLongestSubstring("pwwkew").Should().Be(3);
+    }
+}
