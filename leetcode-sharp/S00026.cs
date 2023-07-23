b17287e360a439e6d666f0e3dd9860b11bb309b1
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:05 +1000

    Solution for 26 - Remove Duplicates from Sorted Array

---
+++ leetcode-sharp/S00026.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 26. Remove Duplicates from Sorted Array
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+public class S00026
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0) return 0;
+        
+        var newLength = 0;
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[newLength] != nums[i])
+            {
+                nums[++newLength] = nums[i];
+            }
+        }
+        
+        return newLength + 1;
+    }
+}
