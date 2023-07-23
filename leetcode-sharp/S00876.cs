1243f661e1a3cec11bf448a15d54f3be97927dd6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-23 12:20:27 +1000

    Extract ListNode

--- leetcode-sharp/S00876.cs
+++ leetcode-sharp/S00876.cs
@@ -4,18 +4,6 @@
 // https://leetcode.com/problems/middle-of-the-linked-list/description/
 public class S00876
 {
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
     public ListNode MiddleNode(ListNode head)
     {
         var fast = head;
