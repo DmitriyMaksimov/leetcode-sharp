e3db27b37f140cc82782cba1def111e055c42403
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-26 18:02:13 +1000

    1870. Minimum Speed to Arrive on Time

---
+++ UnitTests/1800/S01870Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01870Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01870();
+        sut.MinSpeedOnTime(new[] {1, 3, 2}, 6).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01870();
+        sut.MinSpeedOnTime(new[] {1, 3, 2}, 2.7).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01870();
+        sut.MinSpeedOnTime(new[] {1, 3, 2}, 1.9).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/1800/S01870.cs
@@ -0,0 +1,43 @@
+﻿namespace leetcode_sharp;
+
+// 1870. Minimum Speed to Arrive on Time
+// https://leetcode.com/problems/minimum-speed-to-arrive-on-time/
+public class S01870
+{
+    public int MinSpeedOnTime(int[] dist, double hour)
+    {
+        var n = dist.Length;
+        if (hour <= n - 1)
+        {
+            return -1;
+        }
+
+        var left = 1;
+        var right = 10_000_000;
+
+        while (left < right)
+        {
+            var mid = left + (right - left) / 2;
+        
+            double time = 0;
+            
+            for (var i = 0; i < n - 1; i++)
+            {
+                time += Math.Ceiling((double) dist[i] / mid);
+            }
+
+            time += (double) dist[n - 1] / mid;
+
+            if (time <= hour)
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
