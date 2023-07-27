c85b17a89d692f72a33d19ac36d87547c33b688a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-27 14:57:29 +1000

    2141. Maximum Running Time of N Computers

---
+++ UnitTests/2100/S02141Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02141Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02141();
+        sut.MaxRunTime(2, new[] {3, 3, 3}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02141();
+        sut.MaxRunTime(2, new[] {1, 1, 1, 1}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/2100/S02141.cs
@@ -0,0 +1,53 @@
+﻿namespace leetcode_sharp;
+
+// 2141. Maximum Running Time of N Computers
+// https://leetcode.com/problems/maximum-running-time-of-n-computers/
+public class S02141
+{
+    public long MaxRunTime(int n, int[] batteries)
+    {
+        var left = 1L;
+        var right = batteries.Select(x => (long)x).Sum() / n;
+
+        Array.Sort(batteries);
+
+        while (left < right)
+        {
+            var mid = left + (right - left + 1) / 2;
+
+            if (possible(n, batteries, mid))
+            {
+                left = mid;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+
+    private static bool possible(int n, int[] batteries, long hours)
+    {
+        long s = 0;
+
+        foreach (var time in batteries)
+        {
+            s += time;
+
+            if (s >= hours)
+            {
+                s -= hours;
+                n--;
+            }
+
+            if (n == 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
