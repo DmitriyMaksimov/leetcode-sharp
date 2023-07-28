68ff35fa5ae15dbf8aae78c3e9ea63d76a8e2b2d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-28 15:09:44 +1000

    486. Predict the Winner

---
+++ UnitTests/0400/S00486Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00486Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00486();
+        sut.PredictTheWinner(new[] {1, 5, 2}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00486();
+        sut.PredictTheWinner(new[] {1, 5, 233, 7}).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/0400/S00486.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 486. Predict the Winner
+// https://leetcode.com/problems/predict-the-winner/
+public class S00486
+{
+    public bool PredictTheWinner(int[] nums)
+    {
+        var n = nums.Length;
+        var arr = new int[n];
+
+        for (var i = n - 1; i >= 0; i--)
+        {
+            arr[i] = nums[i];
+
+            for (var j = i + 1; j < n; j++)
+            {
+                arr[j] = Math.Max(nums[i] - arr[j], nums[j] - arr[j - 1]);
+            }
+        }
+
+        return arr[n - 1] >= 0;
+    }
+}
