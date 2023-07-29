7945a51fc8b3e8e4be21b8d2a80202823d387d54
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-29 16:27:57 +1000

    463. Island Perimeter

---
+++ UnitTests/0400/S00463Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00463Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00463();
+        sut.IslandPerimeter(new[] {new[] {0, 1, 0, 0}, new[] {1, 1, 1, 0}, new[] {0, 1, 0, 0}, new[] {1, 1, 0, 0}})
+            .Should().Be(16);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00463();
+        sut.IslandPerimeter(new[] {new[] {1}}).Should().Be(4);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00463();
+        sut.IslandPerimeter(new[] {new[] {1, 0}}).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/0400/S00463.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 463. Island Perimeter
+// https://leetcode.com/problems/island-perimeter/
+public class S00463
+{
+    public int IslandPerimeter(int[][] grid)
+    {
+        var rows = grid.Length;
+        var cols = grid[0].Length;
+        var ans = 0;
+
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < cols; j++)
+            {
+                if (grid[i][j] != 1) continue;
+                
+                ans += 4;
+                if (i > 0 && grid[i - 1][j] == 1) ans -= 2;
+                if (j > 0 && grid[i][j - 1] == 1) ans -= 2;
+            }
+        }
+
+        return ans;
+    }
+}
