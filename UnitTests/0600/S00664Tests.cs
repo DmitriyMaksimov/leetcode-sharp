b905897c2215d69eab38ab5b4dca42b1bb57414e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-07-30 13:13:35 +1000

    664. Strange Printer

---
+++ UnitTests/0600/S00664Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00664Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00664();
+        sut.StrangePrinter("aaabbb").Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00664();
+        sut.StrangePrinter("aba").Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/0600/S00664.cs
@@ -0,0 +1,64 @@
+﻿namespace leetcode_sharp;
+
+// 664. Strange Printer
+// https://leetcode.com/problems/strange-printer/
+public class S00664
+{
+    public int StrangePrinter(string s)
+    {
+        var n = s.Length;
+
+        if (n == 0)
+        {
+            return 0;
+        }
+        
+        // Shorten the original string, like reduce aaabbb to ab
+        var tempStr = new string(s[0], 1);
+
+        for (var i = 1; i < n; i++)
+        {
+            if (s[i] != tempStr[^1])
+            {
+                tempStr += s[i];
+            }
+        }
+
+        s = tempStr;
+        n = s.Length;
+
+        var state = new int[n, n];
+
+        for (var i = 0; i < n; i++)
+        {
+            state[i, i] = 1;
+        }
+
+        for (var i = n - 1; i >= 0; i--)
+        {
+            for (var dist = 1; dist + i < n; dist++)
+            {
+                var j = dist + i;
+                if (dist == 1)
+                {
+                    state[i, j] = s[i] == s[j] ? 1 : 2;
+                    continue;
+                }
+
+                state[i, j] = int.MaxValue;
+                for (var k = i; k + 1 <= j; k++)
+                {
+                    var tmp = state[i, k] + state[k + 1, j];
+                    state[i, j] = Math.Min(state[i, j], tmp);
+                }
+
+                if (s[i] == s[j])
+                {
+                    state[i, j]--;
+                }
+            }
+        }
+
+        return state[0, n - 1];
+    }
+}
