5112481d8afa3f75d921230c1af02f9dcd193164
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-03 14:43:37 +1000

    17. Letter Combinations of a Phone Number

---
+++ UnitTests/0000/S00017Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00017Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00017();
+        sut.LetterCombinations("23").Should().Equal("ad","ae","af","bd","be","bf","cd","ce","cf");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00017();
+        sut.LetterCombinations("").Should().Equal();
+    }
+    
+    [Test]
+    public void T3()
+    {
+        var sut = new S00017();
+        sut.LetterCombinations("2").Should().Equal("a","b","c");
+    }
+
+}
---
+++ leetcode-sharp/0000/S00017.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 17. Letter Combinations of a Phone Number
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+public class S00017
+{
+    private readonly List<string> _result = new List<string>();
+    private readonly string[] _dial = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public IList<string> LetterCombinations(string digits)
+    {
+        if (digits.Length == 0)
+        {
+            return _result;
+        }
+
+        helper("", 0, digits);
+        return _result;
+    }
+
+    private void helper(string comb, int index, string digits)
+    {
+        if (index == digits.Length)
+        {
+            _result.Add(comb);
+            return;
+        }
+
+        var letters = _dial[digits[index] - '0'];
+        foreach (var letter in letters)
+        {
+            helper(comb + letter, index + 1, digits);
+        }
+    }
+}
