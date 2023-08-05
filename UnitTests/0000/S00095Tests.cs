7772d677c5eda15f92813689f334f9b1d8dd3ca4
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-05 15:47:25 +1000

    95. Unique Binary Search Trees II

---
+++ UnitTests/0000/S00095Tests.cs
@@ -0,0 +1,30 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00095Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00095();
+        var trees = sut.GenerateTrees(3);
+        trees.Should().HaveCount(5);
+        trees.Should().ContainEquivalentOf(TreeNode.Parse("[1,null,2,null,3]"));
+        trees.Should().ContainEquivalentOf(TreeNode.Parse("[1,null,3,2]"));
+        trees.Should().ContainEquivalentOf(TreeNode.Parse("[2,1,3]"));
+        trees.Should().ContainEquivalentOf(TreeNode.Parse("[3,1,null,null,2]"));
+        trees.Should().ContainEquivalentOf(TreeNode.Parse("[3,2,null,1]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00095();
+        var trees = sut.GenerateTrees(1);
+        trees.Should().HaveCount(1);
+        trees.Should().ContainEquivalentOf(TreeNode.Parse("[1]"));
+    }
+
+}
---
+++ leetcode-sharp/0000/S00095.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 95. Unique Binary Search Trees II
+// https://leetcode.com/problems/unique-binary-search-trees-ii/
+public class S00095
+{
+    public IList<TreeNode?> GenerateTrees(int n)
+    {
+        return generateTrees(1, n);
+    }
+
+    private static IList<TreeNode?> generateTrees(int start, int end)
+    {
+        if (start > end) return new List<TreeNode?> {null};
+
+        var result = new List<TreeNode?>();
+
+        for (var i = start; i <= end; i++)
+        {
+            var left = generateTrees(start, i - 1);
+            var right = generateTrees(i + 1, end);
+
+            result.AddRange(from l in left from r in right select new TreeNode(i, l, r));
+        }
+
+        return result;
+    }
+}
