e9d564318d03ecf0dc7cc428899c31ba80b7a027
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-06 17:02:55 +1000

    920. Number of Music Playlists

---
+++ UnitTests/0900/S00920Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00920Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00920();
+        sut.NumMusicPlaylists(3, 3, 1).Should().Be(6);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00920();
+        sut.NumMusicPlaylists(2, 3, 0).Should().Be(6);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00920();
+        sut.NumMusicPlaylists(2, 3, 1).Should().Be(2);
+    }
+
+}
---
+++ leetcode-sharp/0900/S00920.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 920. Number of Music Playlists
+// https://leetcode.com/problems/number-of-music-playlists/
+public class S00920
+{
+    public int NumMusicPlaylists(int n, int goal, int k)
+    {
+        const int mod = 1_000_000_007;
+        var dp = new long[2, n + 1];
+
+        dp[0, 0] = 1;
+
+        for (var i = 1; i <= goal; i++)
+        {
+            dp[i % 2, 0] = 0;
+            for (var j = 1; j <= Math.Min(i, n); j++)
+            {
+                dp[i % 2, j] = dp[(i - 1) % 2, j - 1] * (n - (j - 1)) % mod;
+                if (j > k)
+                {
+                    dp[i % 2, j] = (dp[i % 2, j] + dp[(i - 1) % 2, j] * (j - k)) % mod;
+                }
+            }
+        }
+
+        return (int) dp[goal % 2, n];
+    }
+}
