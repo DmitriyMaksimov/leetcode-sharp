6401cb6a5093578589db2fa50b3a6d4224594f34
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-07 21:51:54 +1000

    39. Combination Sum

---
+++ UnitTests/0000/S00039Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00039Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00039();
+        sut.CombinationSum(new[] {2, 3, 6, 7}, 7).Should().BeEquivalentTo(new[] {new[] {2, 2, 3}, new[] {7}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00039();
+        sut.CombinationSum(new[] {2, 3, 5}, 8).Should().BeEquivalentTo(new[] {new[] {2, 2, 2, 2}, new[] {2, 3, 3}, new[] {3, 5}});
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00039();
+        sut.CombinationSum(new[] {2}, 1).Should().Equal();
+    }
+}
---
+++ leetcode-sharp/0000/S00039.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode_sharp;
+
+// 39. Combination Sum
+// https://leetcode.com/problems/combination-sum/
+public class S00039
+{
+    private readonly IList<IList<int>> _list = new List<IList<int>>();
+
+    public IList<IList<int>> CombinationSum(int[] candidates, int target)
+    {
+        Array.Sort(candidates);
+        backtrack(new List<int>(), candidates, target, 0);
+        return _list;
+    }
+
+    private void backtrack(IList<int> tempList, IReadOnlyList<int> candidates, int remain, int start)
+    {
+        switch (remain)
+        {
+            case < 0:
+                return;
+            case 0:
+                _list.Add(new List<int>(tempList));
+                break;
+            default:
+            {
+                for (var i = start; i < candidates.Count; i++)
+                {
+                    tempList.Add(candidates[i]);
+                    backtrack(tempList, candidates, remain - candidates[i], i);
+                    tempList.RemoveAt(tempList.Count - 1);
+                }
+
+                break;
+            }
+        }
+    }
+}
