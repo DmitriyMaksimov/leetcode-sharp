29efb0c4d1a6f37213346b6ceeecdac7978545c5
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-07 21:33:01 +1000

    22. Generate Parentheses

---
+++ UnitTests/0000/S00022Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00022Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00022();
+        sut.GenerateParenthesis(3).Should().BeEquivalentTo("((()))", "(()())", "(())()", "()(())", "()()()");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00022();
+        sut.GenerateParenthesis(1).Should().BeEquivalentTo("()");
+    }
+}
---
+++ leetcode-sharp/0000/S00022.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 22. Generate Parentheses
+// https://leetcode.com/problems/generate-parentheses/
+public class S00022
+{
+    public IList<string> GenerateParenthesis(int n)
+    {
+        var res = new List<string>();
+        helper(res, 0, 0, "", n);
+        return res;
+    }
+
+    private static void helper(ICollection<string> res, int left, int right, string s, int n)
+    {
+        if (s.Length == n * 2)
+        {
+            res.Add(s);
+            return;
+        }
+
+        if (left < n)
+        {
+            helper(res, left + 1, right, s + "(", n);
+        }
+
+        if (right < left)
+        {
+            helper(res, left, right + 1, s + ")", n);
+        }
+    }
+}
