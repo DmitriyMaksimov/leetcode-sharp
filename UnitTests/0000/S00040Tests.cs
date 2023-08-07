fb4cb8080d2b0cc8fcc948c55dda4b35541e3bfb
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-07 21:58:37 +1000

    40. Combination Sum II

---
+++ UnitTests/0000/S00040Tests.cs
@@ -0,0 +1,26 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00040Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00040();
+        sut.CombinationSum2(new[] {10, 1, 2, 7, 6, 1, 5}, 8).Should().BeEquivalentTo(
+            new[]
+            {
+                new[] {1, 1, 6}, new[] {1, 2, 5}, new[] {1, 7}, new[] {2, 6}
+            });
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00040();
+        sut.CombinationSum2(new[] {2, 5, 2, 1, 2}, 5).Should()
+            .BeEquivalentTo(new[] {new[] {1, 2, 2}, new[] {5}});
+    }
+}
---
+++ leetcode-sharp/0000/S00040.cs
@@ -0,0 +1,43 @@
+﻿namespace leetcode_sharp;
+
+// 40. Combination Sum II
+// https://leetcode.com/problems/combination-sum-ii/
+public class S00040
+{
+    private readonly IList<IList<int>> _list = new List<IList<int>>();
+
+    public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+    {
+        Array.Sort(candidates);
+        backtrack(new List<int>(), candidates, target, 0);
+        return _list;
+    }
+
+    private void backtrack(IList<int> tempList, IReadOnlyList<int> candidates, int remain, int start)
+    {
+        switch (remain)
+        {
+            case < 0:
+                return;
+            case 0:
+                _list.Add(new List<int>(tempList));
+                break;
+            default:
+            {
+                for (var i = start; i < candidates.Count; i++)
+                {
+                    if (i > start && candidates[i] == candidates[i - 1])
+                    {
+                        continue;
+                    }
+
+                    tempList.Add(candidates[i]);
+                    backtrack(tempList, candidates, remain - candidates[i], i + 1);
+                    tempList.RemoveAt(tempList.Count - 1);
+                }
+
+                break;
+            }
+        }
+    }
+}
