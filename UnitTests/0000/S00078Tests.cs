ec723c76b92a89723912f2291b76a87f9462e443
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-07 21:24:36 +1000

    78. Subsets

---
+++ UnitTests/0000/S00078Tests.cs
@@ -0,0 +1,27 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00078Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00078();
+        sut.Subsets(new[] {1, 2, 3}).Should().BeEquivalentTo(new[]
+        {
+            Array.Empty<int>(),
+            new[] {1}, new[] {2}, new[] {3},
+            new[] {1, 2}, new[] {1, 3}, new[] {2, 3},
+            new[] {1, 2, 3}
+        });
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00078();
+        sut.Subsets(new[] {0}).Should().BeEquivalentTo(new[] {Array.Empty<int>(), new[] {0}});
+    }
+}
---
+++ leetcode-sharp/0000/S00078.cs
@@ -0,0 +1,44 @@
+﻿namespace leetcode_sharp;
+
+// 78. Subsets
+
+// https://leetcode.com/problems/subsets/?envType=study-plan-v2&envId=top-100-liked
+public class S00078
+{
+    private int[] _nums;
+    private int _totalElements;
+
+    public IList<IList<int>> Subsets(int[] nums)
+    {
+        _nums = nums;
+
+        IList<IList<int>> result = new List<IList<int>>();
+
+        _totalElements = 1 << nums.Length;
+
+        for (var i = 0; i < _totalElements; i++)
+        {
+            result.Add(bitmaskToList(i));
+        }
+        
+        return result;
+    }
+
+    private IList<int> bitmaskToList(int n)
+    {
+        var result = new List<int>();
+        var currentBit = 1;
+
+        for (var i = 0; i < _totalElements; i++)
+        {
+            if ((n & currentBit) != 0)
+            {
+                result.Add(_nums[i]);
+            }
+
+            currentBit <<= 1;
+        }
+
+        return result;
+    }
+}
