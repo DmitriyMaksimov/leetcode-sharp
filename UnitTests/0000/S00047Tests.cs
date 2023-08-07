42ba6b4979c2016f7c24989437ad47eebb52aa16
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-07 23:46:35 +1000

    47. Permutations II

---
+++ UnitTests/0000/S00047Tests.cs
@@ -0,0 +1,25 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00047Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00047();
+        sut.PermuteUnique(new[] {1, 1, 2}).Should()
+            .BeEquivalentTo(new[] {new[] {1, 1, 2}, new[] {1, 2, 1}, new[] {2, 1, 1}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00047();
+        sut.PermuteUnique(new[] {1, 2, 3}).Should().BeEquivalentTo(new[]
+        {
+            new[] {1, 2, 3}, new[] {1, 3, 2}, new[] {2, 1, 3}, new[] {2, 3, 1}, new[] {3, 1, 2}, new[] {3, 2, 1}
+        });
+    }
+}
---
+++ leetcode-sharp/0000/S00047.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcode_sharp;
+
+// 47. Permutations II
+// https://leetcode.com/problems/permutations-ii/
+public class S00047
+{
+    private readonly IList<IList<int>> _list = new List<IList<int>>();
+    private bool[] _used;
+
+    public IList<IList<int>> PermuteUnique(int[] nums)
+    {
+        Array.Sort(nums);
+        _used = new bool[nums.Length];
+        backtrack(new List<int>(), nums);
+        return _list;
+    }
+
+    private void backtrack(IList<int> tempList, IReadOnlyList<int> nums)
+    {
+        if (tempList.Count == nums.Count)
+        {
+            _list.Add(new List<int>(tempList));
+            return;
+        }
+
+        for (var i = 0; i < nums.Count; i++)
+        {
+            if (_used[i] || i > 0 && nums[i] == nums[i - 1] && !_used[i - 1])
+            {
+                continue;
+            }
+
+            _used[i] = true;
+            tempList.Add(nums[i]);
+            backtrack(tempList, nums);
+            _used[i] = false;
+            tempList.RemoveAt(tempList.Count - 1);
+        }
+    }
+}
