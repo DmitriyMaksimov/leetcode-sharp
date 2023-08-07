1e24f48619179a54bfd286227d2c0dd134042d75
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-07 23:22:43 +1000

    90. Subsets II

---
+++ UnitTests/0000/S00090Tests.cs
@@ -0,0 +1,26 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00090Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00090();
+        sut.SubsetsWithDup(new[] {1, 2, 2}).Should().BeEquivalentTo(
+            new[]
+            {
+                Array.Empty<int>(),
+                new[] {1}, new[] {2}, new[] {1, 2}, new[] {2, 2}, new[] {1, 2, 2}
+            });
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00090();
+        sut.SubsetsWithDup(new[] {0}).Should().BeEquivalentTo(new[] {Array.Empty<int>(), new[] {0}});
+    }
+}
---
+++ leetcode-sharp/0000/S00090.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 90. Subsets II
+// https://leetcode.com/problems/subsets-ii/
+public class S00090
+{
+    private readonly IList<IList<int>> _list = new List<IList<int>>();
+
+    public IList<IList<int>> SubsetsWithDup(int[] nums)
+    {
+        Array.Sort(nums);
+        backtrack(new List<int>(), nums, 0);
+        return _list;
+    }
+
+    private void backtrack(IList<int> tempList, IReadOnlyList<int> nums, int start)
+    {
+        _list.Add(new List<int>(tempList));
+        for (var i = start; i < nums.Count; i++)
+        {
+            if (i > start && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+
+            tempList.Add(nums[i]);
+            backtrack(tempList, nums, i + 1);
+            tempList.RemoveAt(tempList.Count - 1);
+        }
+    }
+}
