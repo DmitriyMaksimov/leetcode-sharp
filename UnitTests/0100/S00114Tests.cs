b33c2daa7388c7b93b3e0752c278338e0805124f
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-08 20:08:51 +1000

    114. Flatten Binary Tree to Linked List

---
+++ UnitTests/0100/S00114Tests.cs
@@ -0,0 +1,16 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00114Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00114();
+        var root = TreeNode.Parse("[1,2,5,3,4,null,6]");
+        sut.Flatten(root);
+        root.Should().BeEquivalentTo(TreeNode.Parse("[1,null,2,null,3,null,4,null,5,null,6]"));
+    }
+}
---
+++ leetcode-sharp/0100/S00114.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 114. Flatten Binary Tree to Linked List
+// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+public class S00114
+{
+    private TreeNode prev;
+
+    public void Flatten(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return;
+        }
+
+        Flatten(root.right);
+        Flatten(root.left);
+        prev = root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+}
