ab03ba560bfd94881f0599fb6fea5b1ab0814691
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-08 21:46:52 +1000

    199. Binary Tree Right Side View

---
+++ UnitTests/0100/S00199Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00199Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00199();
+        sut.RightSideView(TreeNode.Parse("[1,2,3,null,5,null,4]")).Should().Equal(1,3,4);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00199();
+        sut.RightSideView(TreeNode.Parse("[1,null,3]")).Should().Equal(1,3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00199();
+        sut.RightSideView(TreeNode.Parse("[]")).Should().Equal();
+    }
+
+}
---
+++ leetcode-sharp/0100/S00199.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 199. Binary Tree Right Side View
+// https://leetcode.com/problems/binary-tree-right-side-view/
+public class S00199
+{
+    public IList<int> RightSideView(TreeNode? root)
+    {
+        var result = new List<int>();
+        rightView(root, result, 0);
+        return result;
+    }
+
+    private static void rightView(TreeNode? curr, ICollection<int> result, int currDepth)
+    {
+        if (curr == null)
+        {
+            return;
+        }
+
+        if (currDepth == result.Count)
+        {
+            result.Add(curr.val);
+        }
+
+        rightView(curr.right, result, currDepth + 1);
+        rightView(curr.left, result, currDepth + 1);
+    }
+}
