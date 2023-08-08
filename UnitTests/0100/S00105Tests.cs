b946f9b0564aaf947a429a249d8622ceae6f3d10
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-08 22:05:09 +1000

    105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ UnitTests/0100/S00105Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00105Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00105();
+        sut.BuildTree(new[] {3, 9, 20, 15, 7}, new[] {9, 3, 15, 20, 7}).Should()
+            .BeEquivalentTo(TreeNode.Parse("[3,9,20,null,null,15,7]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00105();
+        sut.BuildTree(new[] {-1}, new[] {-1}).Should().BeEquivalentTo(TreeNode.Parse("[-1]"));
+    }
+}
---
+++ leetcode-sharp/0100/S00105.cs
@@ -0,0 +1,39 @@
+﻿namespace leetcode_sharp;
+
+// 105. Construct Binary Tree from Preorder and Inorder Traversal
+// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+public class S00105
+{
+    private int _preorderIdx;
+    private readonly Dictionary<int, int> _map = new();
+    private int[] _preorder;
+
+    public TreeNode? BuildTree(int[] preorder, int[] inorder)
+    {
+        _preorder = preorder;
+        var len = inorder.Length;
+        for (var i = 0; i < len; i++)
+        {
+            _map.Add(inorder[i], i);
+        }
+
+        return buildTree(0, len - 1);
+    }
+
+    private TreeNode? buildTree(int left, int right)
+    {
+        if (left > right)
+        {
+            return null;
+        }
+
+        var preorderValue = _preorder[_preorderIdx++];
+        var inorderIndex = _map[preorderValue];
+
+        return new TreeNode(preorderValue)
+        {
+            left = buildTree(left, inorderIndex - 1),
+            right = buildTree(inorderIndex + 1, right)
+        };
+    }
+}
