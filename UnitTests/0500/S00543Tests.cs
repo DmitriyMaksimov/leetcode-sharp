08579fb35b3859251bed67bc12f4146e953a8e2a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-08 20:27:03 +1000

    543. Diameter of Binary Tree

---
+++ UnitTests/0500/S00543Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00543Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00543();
+        sut.DiameterOfBinaryTree(TreeNode.Parse("[1,2,3,4,5]")).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00543();
+        sut.DiameterOfBinaryTree(TreeNode.Parse("[1,2]")).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/0500/S00543.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 543. Diameter of Binary Tree
+// https://leetcode.com/problems/diameter-of-binary-tree/
+public class S00543
+{
+    private int _answer;
+
+    public int DiameterOfBinaryTree(TreeNode? root)
+    {
+        maxDepth(root);
+        return _answer;
+    }
+
+    private int maxDepth(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        var leftHeight = maxDepth(root.left);
+        var rightHeight = maxDepth(root.right);
+        
+        _answer = Math.Max(_answer, leftHeight + rightHeight);
+
+        return Math.Max(leftHeight, rightHeight) + 1;
+    }
+}
