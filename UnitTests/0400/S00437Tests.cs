7708ca514a6e3f3dafdf430de047b3553371e8a4
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-08 23:17:56 +1000

    437. Path Sum III

---
+++ UnitTests/0400/S00437Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00437Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00437();
+        sut.PathSum(TreeNode.Parse("[10,5,-3,3,2,null,11,3,-2,null,1]"), 8).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00437();
+        sut.PathSum(TreeNode.Parse("[5,4,8,11,null,13,4,7,2,null,null,5,1]"), 22).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00437();
+        sut.PathSum(TreeNode.Parse("[1000000000,1000000000,null,294967296,null,1000000000,null,1000000000,null,1000000000]"), 0).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0400/S00437.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 437. Path Sum III
+// https://leetcode.com/problems/path-sum-iii/
+public class S00437
+{
+    private readonly Dictionary<long, int> _prefixSum = new() {{0, 1}};
+    private int _target;
+
+    public int PathSum(TreeNode? root, int targetSum)
+    {
+        _target = targetSum;
+        return helper(root, 0);
+    }
+
+    private int helper(TreeNode? root, long currentSum)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        currentSum += root.val;
+        
+        var result = _prefixSum.GetValueOrDefault(currentSum - _target);
+
+        _prefixSum[currentSum] = _prefixSum.GetValueOrDefault(currentSum) + 1;
+        result += helper(root.left, currentSum) + helper(root.right, currentSum);
+        _prefixSum[currentSum] -= 1;
+        
+        return result;
+    }
+}
