3a9f1cf47f0d86c6374414b5447636888f2b1fa4
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-09 23:05:28 +1000

    236. Lowest Common Ancestor of a Binary Tree

---
+++ UnitTests/0200/S00236Tests.cs
@@ -0,0 +1,37 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00236Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00236();
+        var root = TreeNode.Parse("[3,5,1,6,2,0,8,null,null,7,4]");
+        var node5 = root?.left;
+        var node1 = root?.right;
+        sut.LowestCommonAncestor(root, node5, node1).Should().Be(root);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00236();
+        var root = TreeNode.Parse("[3,5,1,6,2,0,8,null,null,7,4]");
+        var node5 = root?.left;
+        var node4 = root?.left?.right?.right;
+        sut.LowestCommonAncestor(root, node5, node4).Should().Be(node5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00236();
+        var root = TreeNode.Parse("[1,2]");
+        var node1 = root;
+        var node2 = root?.left;
+        sut.LowestCommonAncestor(root, node1, node2).Should().Be(node1);
+    }
+}
---
+++ leetcode-sharp/0200/S00236.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 236. Lowest Common Ancestor of a Binary Tree
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+public class S00236
+{
+    public TreeNode? LowestCommonAncestor(TreeNode? root, TreeNode? p, TreeNode? q)
+    {
+        if (root == null || root == p || root == q)
+        {
+            return root;
+        }
+
+        var left = LowestCommonAncestor(root.left, p, q);
+        var right = LowestCommonAncestor(root.right, p, q);
+        
+        return left == null ? right : right == null ? left : root;
+    }
+}
