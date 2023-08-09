eb300dac24f7ccc7628e09f8f05514f602bd8733
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-09 22:54:39 +1000

    230. Kth Smallest Element in a BST

---
+++ UnitTests/0200/S00230Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00230Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00230();
+        sut.KthSmallest(TreeNode.Parse("[3,1,4,null,2]"), 1).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00230();
+        sut.KthSmallest(TreeNode.Parse("[5,3,6,2,4,null,null,1]"), 3).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/0200/S00230.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 230. Kth Smallest Element in a BST
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+public class S00230
+{
+    private int _answer;
+    private int _count;
+
+    public int KthSmallest(TreeNode? root, int k)
+    {
+        inorder(root, k);
+        return _answer;
+    }
+
+    private void inorder(TreeNode? root, int k)
+    {
+        if (root == null)
+        {
+            return;
+        }
+
+        inorder(root.left, k);
+        _count++;
+        if (_count == k)
+        {
+            _answer = root.val;
+            return;
+        }
+
+        inorder(root.right, k);
+    }
+}
