4e60e1445bf21dfaeb183b8c088e7f6f44710def
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-09 19:23:09 +1000

    2616. Minimize the Maximum Difference of Pairs

---
+++ UnitTests/2600/S02616Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02616Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02616();
+        sut.MinimizeMax(new[] {10, 1, 2, 7, 1, 3}, 2).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02616();
+        sut.MinimizeMax(new[] {4, 2, 1, 2}, 1).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2600/S02616.cs
@@ -0,0 +1,48 @@
+﻿namespace leetcode_sharp;
+
+// 2616. Minimize the Maximum Difference of Pairs
+// https://leetcode.com/problems/minimize-the-maximum-difference-of-pairs/
+public class S02616
+{
+    public int MinimizeMax(int[] nums, int p)
+    {
+        Array.Sort(nums);
+
+        var left = 0;
+        var right = nums[^1] - nums[0];
+
+        while (left < right)
+        {
+            var mid = (left + right) / 2;
+            if (canFormPairs(nums, mid, p))
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private static bool canFormPairs(int[] nums, int mid, int p)
+    {
+        var count = 0;
+        for (var i = 0; i < nums.Length - 1 && count < p;)
+        {
+            if (nums[i + 1] - nums[i] <= mid)
+            {
+                count++;
+                i += 2;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        return count >= p;
+    }
+}
