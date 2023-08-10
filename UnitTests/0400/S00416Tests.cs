2c15cd045d3d898e98adfbe587e728d2ca5d0caa
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-10 18:34:20 +1000

    416. Partition Equal Subset Sum

---
+++ UnitTests/0400/S00416Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00416Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00416();
+        sut.CanPartition(new[] {1, 5, 11, 5}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00416();
+        sut.CanPartition(new[] {1, 2, 3, 5}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0400/S00416.cs
@@ -0,0 +1,46 @@
+﻿namespace leetcode_sharp;
+
+// 416. Partition Equal Subset Sum
+// https://leetcode.com/problems/partition-equal-subset-sum/
+public class S00416
+{
+    public bool CanPartition(int[] nums)
+    {
+        var sum = nums.Sum();
+        if (sum % 2 != 0)
+        {
+            return false;
+        }
+
+        var n = nums.Length;
+        sum /= 2;
+        var possible = new bool[n + 1, sum + 1];
+
+        for (var i = 1; i <= sum; i++)
+        {
+            possible[0, i] = false;
+        }
+
+        for (var i = 0; i <= n; i++)
+        {
+            possible[i, 0] = true;
+        }
+
+        for (var i = 1; i <= n; i++)
+        {
+            for (var j = 1; j <= sum; j++)
+            {
+                if (nums[i - 1] > j)
+                {
+                    possible[i, j] = possible[i - 1, j];
+                }
+                else
+                {
+                    possible[i, j] = possible[i - 1, j - nums[i - 1]] || possible[i - 1, j];
+                }
+            }
+        }
+
+        return possible[n, sum];
+    }
+}
