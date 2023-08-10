3351a2df79e0f792937d52963f81e3698988d874
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-10 22:17:15 +1000

    763. Partition Labels

---
+++ UnitTests/0700/S00763Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00763Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00763();
+        sut.PartitionLabels("ababcbacadefegdehijhklij").Should().Equal(9, 7, 8);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00763();
+        sut.PartitionLabels("eccbbbbdec").Should().Equal(10);
+    }
+}
---
+++ leetcode-sharp/0700/S00763.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 763. Partition Labels
+// https://leetcode.com/problems/partition-labels/
+public class S00763
+{
+    public IList<int> PartitionLabels(string s)
+    {
+        var map = new Dictionary<char, int>();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            map[s[i]] = i;
+        }
+
+        var result = new List<int>();
+        var prev = -1;
+        var max = 0;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            max = Math.Max(max, map[s[i]]);
+            if (max == i)
+            {
+                result.Add(max - prev);
+                prev = max;
+            }
+        }
+
+        return result;
+    }
+}
