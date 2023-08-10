9b0f007982d1d45e47849adc5ab4644712d10197
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-10 18:26:14 +1000

    81. Search in Rotated Sorted Array II

---
+++ UnitTests/0000/S00081Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00081Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00081();
+        sut.Search(new[] {2, 5, 6, 0, 0, 1, 2}, 0).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00081();
+        sut.Search(new[] {2, 5, 6, 0, 0, 1, 2}, 3).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0000/S00081.cs
@@ -0,0 +1,50 @@
+﻿namespace leetcode_sharp;
+
+// 81. Search in Rotated Sorted Array II
+// https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+public class S00081
+{
+    public bool Search(int[] nums, int target)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+            {
+                return true;
+            }
+
+            if (nums[mid] == nums[left] && nums[mid] == nums[right])
+            {
+                left++;
+                right--;
+            }
+            else if (nums[mid] >= nums[left])
+            {
+                if (target >= nums[left] && target < nums[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            else if (nums[mid] <= nums[right])
+            {
+                if (target > nums[mid] && target <= nums[right])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
