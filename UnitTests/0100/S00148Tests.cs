7454d747ec7594e7001a12b56cfe64604b2e2850
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-11 17:48:08 +1000

    148. Sort List

---
+++ UnitTests/0100/S00148Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+using leetcode_sharp._0100;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00148Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00148();
+        sut.SortList(ListNode.Parse("[4,2,1,3]")).Should().BeEquivalentTo(ListNode.Parse("[1,2,3,4]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00148();
+        sut.SortList(ListNode.Parse("[-1,5,3,4,0]")).Should().BeEquivalentTo(ListNode.Parse("[-1,0,3,4,5]"));
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00148();
+        sut.SortList(ListNode.Parse("[]")).Should().BeEquivalentTo(ListNode.Parse("[]"));
+    }
+}
---
+++ leetcode-sharp/0100/S00148.cs
@@ -0,0 +1,84 @@
+﻿namespace leetcode_sharp._0100;
+
+// 148. Sort List
+// https://leetcode.com/problems/sort-list/
+public class S00148
+{
+    public ListNode? SortList(ListNode? head)
+    {
+        if (head?.next == null)
+        {
+            return head;
+        }
+
+        var slow = head;
+        var fast = head;
+        ListNode? prev = null;
+
+        while (fast?.next != null)
+        {
+            prev = slow;
+            slow = slow?.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = null;
+        var l1 = SortList(head);
+        var l2 = SortList(slow);
+
+        return mergeSort(l1, l2);
+    }
+
+    private static ListNode? mergeSort(ListNode? l1, ListNode? l2)
+    {
+        ListNode? fh = null;
+        ListNode? ft = null;
+
+        while (l1 != null && l2 != null)
+        {
+            if (fh == null && ft == null)
+            {
+                if (l1.val > l2.val)
+                {
+                    fh = l2;
+                    ft = l2;
+                    l2 = l2.next;
+                }
+                else
+                {
+                    fh = l1;
+                    ft = l1;
+                    l1 = l1.next;
+                }
+            }
+
+            if (l1 != null && l2 != null)
+            {
+                if (l1.val < l2.val)
+                {
+                    ft.next = l1;
+                    ft = ft.next;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    ft.next = l2;
+                    ft = ft.next;
+                    l2 = l2.next;
+                }
+            }
+        }
+
+        if (l1 != null)
+        {
+            ft.next = l1;
+        }
+
+        if (l2 != null)
+        {
+            ft.next = l2;
+        }
+
+        return fh;
+    }
+}
