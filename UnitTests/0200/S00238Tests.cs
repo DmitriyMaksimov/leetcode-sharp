ba61c8c7f74a10371ecd1909a70cf8e3503bcaad
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-11 19:03:34 +1000

    238. Product of Array Except Self

---
+++ UnitTests/0200/S00238Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00238Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00238();
+        sut.ProductExceptSelf(new[] {1, 2, 3, 4}).Should().Equal(24, 12, 8, 6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00238();
+        sut.ProductExceptSelf(new[] {-1, 1, 0, -3, 3}).Should().Equal(0, 0, 9, 0, 0);
+    }
+}
---
+++ leetcode-sharp/0200/S00238.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 238. Product of Array Except Self
+// https://leetcode.com/problems/product-of-array-except-self/
+public class S00238
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        var n = nums.Length;
+        var ans = new int[n];
+        for (var i = 0; i < n; ++i)
+        {
+            ans[i] = 1;
+        }
+
+        int leftProduct = 1, rightProduct = 1;
+        for (int i = 1, j = n - 2; i < n; ++i, --j)
+        {
+            leftProduct *= nums[i - 1];
+            rightProduct *= nums[j + 1];
+
+            ans[i] *= leftProduct;
+            ans[j] *= rightProduct;
+        }
+
+        return ans;
+    }
+}
