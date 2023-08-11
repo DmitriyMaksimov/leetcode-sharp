018f25bc2be2f61fecdfecf0f5a2db04a83bc295
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-11 17:55:07 +1000

    160. Intersection of Two Linked Lists

---
+++ leetcode-sharp/0100/S00160.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp._0100;
+
+// 160. Intersection of Two Linked Lists
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
+public class S00160
+{
+    public ListNode? GetIntersectionNode(ListNode? headA, ListNode? headB)
+    {
+        var nodeA = headA;
+        var nodeB = headB;
+
+        while (nodeA != nodeB)
+        {
+            nodeA = (nodeA == null) ? headB : nodeA.next;
+            nodeB = (nodeB == null) ? headA : nodeB.next;
+        }
+
+        return nodeA;
+    }
+}
