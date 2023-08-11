2310362c6fbb368e22e693a579edf2fa17c356dc
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-11 18:25:37 +1000

    73. Set Matrix Zeroes

---
+++ UnitTests/0000/S00073Tests.cs
@@ -0,0 +1,41 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00073Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00073();
+        var matrix = new[]
+        {
+            new[] {1, 1, 1},
+            new[] {1, 0, 1},
+            new[] {1, 1, 1}
+        };
+
+        sut.SetZeroes(matrix);
+        matrix[0].Should().Equal(1, 0, 1);
+        matrix[1].Should().Equal(0, 0, 0);
+        matrix[2].Should().Equal(1, 0, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00073();
+        var matrix = new[]
+        {
+            new[] {0, 1, 2, 0},
+            new[] {3, 4, 5, 2},
+            new[] {1, 3, 1, 5}
+        };
+
+        sut.SetZeroes(matrix);
+        matrix[0].Should().Equal(0, 0, 0, 0);
+        matrix[1].Should().Equal(0, 4, 5, 0);
+        matrix[2].Should().Equal(0, 3, 1, 0);
+    }
+}
---
+++ leetcode-sharp/0000/S00073.cs
@@ -0,0 +1,37 @@
+﻿namespace leetcode_sharp;
+
+// 73. Set Matrix Zeroes
+// https://leetcode.com/problems/set-matrix-zeroes/
+public class S00073
+{
+    public void SetZeroes(int[][] matrix)
+    {
+        var rows = new HashSet<int>();
+        var columns = new HashSet<int>();
+        var m = matrix.GetLength(0);
+        var n = matrix[0].Length;
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    rows.Add(i);
+                    columns.Add(j);
+                }
+            }
+        }
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                if (rows.Contains(i) || columns.Contains(j))
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
