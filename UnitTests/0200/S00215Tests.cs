0fb653c2aff1639c01d3e01ea664a593d728c8f5
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-11 14:26:07 +1000

    215. Kth Largest Element in an Array

---
+++ UnitTests/0200/S00215Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00215Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00215();
+        sut.FindKthLargest(new[] {3, 2, 1, 5, 6, 4}, 2).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00215();
+        sut.FindKthLargest(new[] {3, 2, 3, 1, 2, 4, 5, 5, 6}, 4).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/0200/S00215.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 215. Kth Largest Element in an Array
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+public class S00215
+{
+    public int FindKthLargest(int[] nums, int k)
+    {
+        var pq = new PriorityQueue<int, int>();
+
+        foreach (var val in nums)
+        {
+            pq.Enqueue(val, val);
+
+            if (pq.Count > k)
+            {
+                pq.Dequeue();
+            }
+        }
+
+        return pq.Peek();
+    }
+}
