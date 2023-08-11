ee1d659021e4a77ef170fd7b81bbf8e9a7a9f1e8
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-11 18:35:33 +1000

    240. Search a 2D Matrix II

---
+++ UnitTests/0200/S00240Tests.cs
@@ -0,0 +1,37 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00240Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00240();
+        var matrix = new[]
+        {
+            new[] {1, 4, 7, 11, 15},
+            new[] {2, 5, 8, 12, 19},
+            new[] {3, 6, 9, 16, 22},
+            new[] {10, 13, 14, 17, 24},
+            new[] {18, 21, 23, 26, 30}
+        };
+        sut.SearchMatrix(matrix, 5).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00240();
+        var matrix = new[]
+        {
+            new[] {1, 4, 7, 11, 15},
+            new[] {2, 5, 8, 12, 19},
+            new[] {3, 6, 9, 16, 22},
+            new[] {10, 13, 14, 17, 24},
+            new[] {18, 21, 23, 26, 30}
+        };
+        sut.SearchMatrix(matrix, 20).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0200/S00240.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 240. Search a 2D Matrix II
+// https://leetcode.com/problems/search-a-2d-matrix-ii/
+public class S00240
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        return matrix.Any(row => Array.BinarySearch(row, target) >= 0);
+    }
+}
