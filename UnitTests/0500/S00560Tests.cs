70f3556b26708417e39618d31ddb082ad2dde74b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-11 14:16:34 +1000

    560. Subarray Sum Equals K

---
+++ UnitTests/0500/S00560Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00560Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00560();
+        sut.SubarraySum(new[] {1, 1, 1}, 2).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00560();
+        sut.SubarraySum(new[] {1, 2, 3}, 3).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/0500/S00560.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 560. Subarray Sum Equals K
+// https://leetcode.com/problems/subarray-sum-equals-k/
+public class S00560
+{
+    public int SubarraySum(int[] nums, int k)
+    {
+        var sum = 0;
+        var answer = 0;
+        var prefixSum = new Dictionary<int, int>
+        {
+            [sum] = 1
+        };
+
+        foreach (var num in nums)
+        {
+            sum += num;
+            var find = sum - k;
+            if (prefixSum.TryGetValue(find, out var value))
+            {
+                answer += value;
+            }
+
+            prefixSum[sum] = prefixSum.GetValueOrDefault(sum) + 1;
+        }
+
+        return answer;
+    }
+}
