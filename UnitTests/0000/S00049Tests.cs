7fe6d050f5dd751215846e477742fa8608a4c670
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-11 13:45:54 +1000

    49. Group Anagrams

---
+++ UnitTests/0000/S00049Tests.cs
@@ -0,0 +1,32 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00049Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00049();
+        sut.GroupAnagrams(new[] {"eat", "tea", "tan", "ate", "nat", "bat"}).Should().BeEquivalentTo(new[]
+        {
+            new List<string> {"bat"},
+            new List<string> {"nat", "tan"}, new List<string> {"ate", "eat", "tea"}
+        });
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00049();
+        sut.GroupAnagrams(new[] {""}).Should().BeEquivalentTo(new[] {new List<string> {""}});
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00049();
+        sut.GroupAnagrams(new[] {"a"}).Should().BeEquivalentTo(new[] {new List<string> {"a"}});
+    }
+}
---
+++ leetcode-sharp/0000/S00049.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 49. Group Anagrams
+// https://leetcode.com/problems/group-anagrams/
+public class S00049
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        var map = new Dictionary<string, List<string>>();
+
+        foreach (var word in strs)
+        {
+            var sortedWord = new string(word.OrderBy(ch => ch).ToArray());
+
+            if (!map.ContainsKey(sortedWord))
+            {
+                map.Add(sortedWord, new List<string>());
+            }
+
+            map[sortedWord].Add(word);
+        }
+
+        return new List<IList<string>>(map.Values);
+    }
+}
