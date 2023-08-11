576370544f132d804732c5d19a0f8ec98327e088
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-11 18:49:43 +1000

    287. Find the Duplicate Number

---
+++ UnitTests/0200/S00287Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00287Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00287();
+        sut.FindDuplicate(new[] {1, 3, 4, 2, 2}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00287();
+        sut.FindDuplicate(new[] {3, 1, 3, 4, 2}).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/0200/S00287.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 287. Find the Duplicate Number
+// https://leetcode.com/problems/find-the-duplicate-number/
+public class S00287
+{
+    public int FindDuplicate(int[] nums)
+    {
+        var nextPos = nums[0];
+        var pos = nums[0];
+        var visited = 0;
+
+        while (true)
+        {
+            if (nextPos < 0)
+            {
+                return Math.Abs(pos);
+            }
+
+            nums[visited] = -pos;
+            pos = nextPos;
+            visited = pos;
+            nextPos = nums[pos];
+        }
+    }
+}
