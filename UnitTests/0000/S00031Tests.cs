c4704ff68862668d91b69de30740398b976e26b2
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-11 20:44:51 +1000

    31. Next Permutation

---
+++ UnitTests/0000/S00031Tests.cs
@@ -0,0 +1,34 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00031Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00031();
+        var nums = new[] {1, 2, 3};
+        sut.NextPermutation(nums);
+        nums.Should().Equal(1, 3, 2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00031();
+        var nums = new[] {3, 2, 1};
+        sut.NextPermutation(nums);
+        nums.Should().Equal(1, 2, 3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00031();
+        var nums = new[] {1, 1, 5};
+        sut.NextPermutation(nums);
+        nums.Should().Equal(1, 5, 1);
+    }
+}
---
+++ leetcode-sharp/0000/S00031.cs
@@ -0,0 +1,48 @@
+﻿namespace leetcode_sharp;
+
+// 31. Next Permutation
+// https://leetcode.com/problems/next-permutation/
+public class S00031
+{
+    public void NextPermutation(int[] nums)
+    {
+        int i;
+        var min = int.MaxValue;
+        var pivot = 0;
+        var pos = 0;
+
+        for (i = nums.Length - 1; i >= 1; i--)
+        {
+            if (nums[i] <= nums[i - 1]) continue;
+            pivot = i;
+            break;
+        }
+
+        if (pivot > 0)
+        {
+            for (i = pivot; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[pivot - 1])
+                {
+                    min = Math.Min(min, nums[i]);
+                }
+
+                if (min == nums[i])
+                {
+                    pos = i;
+                }
+            }
+
+            (nums[pos], nums[pivot - 1]) = (nums[pivot - 1], nums[pos]);
+        }
+
+        //reverse from pivot to end
+        var high = nums.Length - 1;
+        while (pivot <= high)
+        {
+            (nums[high], nums[pivot]) = (nums[pivot], nums[high]);
+            high--;
+            pivot++;
+        }
+    }
+}
