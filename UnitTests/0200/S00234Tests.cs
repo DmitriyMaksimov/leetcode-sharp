0a5344cdbf74b60f47a3f120a65c8ec2d1bb244e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-11 18:44:34 +1000

    234. Palindrome Linked List

---
+++ UnitTests/0200/S00234Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00234Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00234();
+        sut.IsPalindrome(ListNode.Parse("[1,2,2,1]")).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00234();
+        sut.IsPalindrome(ListNode.Parse("[1,2]")).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0200/S00234.cs
@@ -0,0 +1,45 @@
+﻿namespace leetcode_sharp;
+
+// 234. Palindrome Linked List
+// https://leetcode.com/problems/palindrome-linked-list/
+public class S00234
+{
+    public bool IsPalindrome(ListNode? head)
+    {
+        var slow = head;
+        var fast = head;
+
+        while (fast?.next != null)
+        {
+            slow = slow?.next;
+            fast = fast.next.next;
+        }
+
+        var prev = slow;
+        slow = slow!.next;
+        prev!.next = null;
+
+        while (slow != null)
+        {
+            var temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+
+        fast = head;
+        slow = prev;
+        while (slow != null)
+        {
+            if (fast?.val != slow.val)
+            {
+                return false;
+            }
+
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return true;
+    }
+}
