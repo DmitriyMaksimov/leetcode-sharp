55a092c018fd35d5c1bcb4d57eeed6e973f754d5
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-11 19:25:48 +1000

    75. Sort Colors

---
+++ UnitTests/0000/S00075Tests.cs
@@ -0,0 +1,25 @@
+﻿using leetcode_sharp._0000;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00075Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00075();
+        var nums = new[] {2, 0, 2, 1, 1, 0};
+        sut.SortColors(nums);
+        nums.Should().Equal(0, 0, 1, 1, 2, 2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00075();
+        var nums = new[] {2, 0, 1};
+        sut.SortColors(nums);
+        nums.Should().Equal(0, 1, 2);
+    }
+}
---
+++ leetcode-sharp/0000/S00075.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcode_sharp._0000;
+
+// 75. Sort Colors
+// https://leetcode.com/problems/sort-colors/
+public class S00075
+{
+    public void SortColors(int[] nums)
+    {
+        var numberOfZeros = 0;
+        var numberOfOnes = 0;
+        foreach (var num in nums)
+        {
+            switch (num)
+            {
+                case 0:
+                    ++numberOfZeros;
+                    break;
+                case 1:
+                    ++numberOfOnes;
+                    break;
+            }
+        }
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (i > numberOfZeros + numberOfOnes - 1)
+            {
+                nums[i] = 2;
+            }
+            else if (i > numberOfZeros - 1)
+            {
+                nums[i] = 1;
+            }
+            else
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}
