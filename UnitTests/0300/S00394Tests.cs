464a2c7d914ea03111d84b149bab49fcde1bba53
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-12 11:26:25 +1000

    394. Decode String

---
+++ UnitTests/0300/S00394Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00394Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00394();
+        sut.DecodeString("3[a]2[bc]").Should().Be("aaabcbc");
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00394();
+        sut.DecodeString("3[a2[c]]").Should().Be("accaccacc");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00394();
+        sut.DecodeString("2[abc]3[cd]ef").Should().Be("abcabccdcdcdef");
+    }
+
+}
---
+++ leetcode-sharp/0300/S00394.cs
@@ -0,0 +1,54 @@
+﻿namespace leetcode_sharp;
+
+// 394. Decode String
+// https://leetcode.com/problems/decode-string/
+public class S00394
+{
+    public string DecodeString(string s)
+    {
+        var stack = new Stack<char>();
+
+        foreach (var ch in s)
+        {
+            var temp = "";
+            var temp2 = "";
+
+            if (ch != ']')
+            {
+                stack.Push(ch);
+                continue;
+            }
+
+            while (stack.Any() && char.IsLetter(stack.Peek()))
+            {
+                temp += stack.Pop();
+            }
+
+            stack.Pop();
+
+            temp = new string(temp.Reverse().ToArray());
+
+            while (stack.Any() && char.IsDigit(stack.Peek()))
+            {
+                temp2 += stack.Pop();
+            }
+
+            temp2 = new string(temp2.Reverse().ToArray());
+
+            var n = int.Parse(temp2);
+            temp2 = temp;
+
+            while (--n > 0)
+            {
+                temp += temp2;
+            }
+
+            foreach (var c in temp)
+            {
+                stack.Push(c);
+            }
+        }
+
+        return new string(stack.Reverse().ToArray());
+    }
+}
