4726455fc31d8ff4e12e790681db497d4f2d51a8
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-12 13:01:18 +1000

    41. First Missing Positive

---
+++ UnitTests/0000/S00041Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00041Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00041();
+        sut.FirstMissingPositive(new[] {1, 2, 0}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00041();
+        sut.FirstMissingPositive(new[] {3, 4, -1, 1}).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00041();
+        sut.FirstMissingPositive(new[] {7, 8, 9, 11, 12}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/0000/S00041.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 41. First Missing Positive
+// https://leetcode.com/problems/first-missing-positive/
+public class S00041
+{
+    public int FirstMissingPositive(int[] nums)
+    {
+        for (var i = 0; i < nums.Length;)
+        {
+            var correct = nums[i] - 1;
+
+            if (nums[i] <= nums.Length && nums[i] > 0 && nums[i] != nums[correct])
+            {
+                (nums[i], nums[correct]) = (nums[correct], nums[i]);
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        for (var j = 0; j < nums.Length; j++)
+        {
+            if (nums[j] != j + 1)
+            {
+                return j + 1;
+            }
+        }
+
+        return nums.Length + 1;
+    }
+}
