24b58a9decbb974c5e5efa2bc942f2a13d08df72
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-12 11:32:43 +1000

    11. Container With Most Water

---
+++ UnitTests/0000/S00011Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00011Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00011();
+        sut.MaxArea(new[] {1, 8, 6, 2, 5, 4, 8, 3, 7}).Should().Be(49);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00011();
+        sut.MaxArea(new[] {1, 1}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/0000/S00011.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 11. Container With Most Water
+// https://leetcode.com/problems/container-with-most-water/
+public class S00011
+{
+    public int MaxArea(int[] height)
+    {
+        var left = 0;
+        var right = height.Length - 1;
+        var result = 0;
+
+        while (left < right)
+        {
+            var currentArea = Math.Min(height[left], height[right]) * (right - left);
+            result = Math.Max(result, currentArea);
+
+            if (height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return result;
+    }
+}
