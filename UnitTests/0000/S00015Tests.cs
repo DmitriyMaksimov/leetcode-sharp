159aec78cfd4a85c5679bf13e5c496d66ed39c65
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-12 11:08:47 +1000

    15. 3Sum

---
+++ UnitTests/0000/S00015Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00015Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00015();
+        sut.ThreeSum(new[] {-1, 0, 1, 2, -1, -4}).Should().BeEquivalentTo(new[] {new[] {-1, -1, 2}, new[] {-1, 0, 1}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00015();
+        sut.ThreeSum(new[] {0, 1, 1}).Should().Equal();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00015();
+        sut.ThreeSum(new[] {0, 0, 0}).Should().BeEquivalentTo(new[] {new[] {0, 0, 0}});
+    }
+}
---
+++ leetcode-sharp/0000/S00015.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode_sharp;
+
+// 15. 3Sum
+// https://leetcode.com/problems/3sum/
+public class S00015
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        Array.Sort(nums);
+        var s = new HashSet<(int, int, int)>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var j = i + 1;
+            var k = nums.Length - 1;
+            while (j < k)
+            {
+                var sum = nums[i] + nums[j] + nums[k];
+                switch (sum)
+                {
+                    case 0:
+                        s.Add((nums[i], nums[j], nums[k]));
+                        j++;
+                        k--;
+                        break;
+                    case < 0:
+                        j++;
+                        break;
+                    default:
+                        k--;
+                        break;
+                }
+            }
+        }
+
+        return s.Select(x => new List<int>{x.Item1, x.Item2, x.Item3} as IList<int>).ToList();
+    }
+}
