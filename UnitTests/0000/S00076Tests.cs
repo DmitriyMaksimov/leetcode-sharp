4a675aa12c43174b232414124af69ea19827bd3a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-12 18:44:00 +1000

    76. Minimum Window Substring

---
+++ UnitTests/0000/S00076Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00076Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00076();
+        sut.MinWindow("ADOBECODEBANC", "ABC").Should().Be("BANC");
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00076();
+        sut.MinWindow("a", "a").Should().Be("a");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00076();
+        sut.MinWindow("a", "aa").Should().Be("");
+    }
+
+}
---
+++ leetcode-sharp/0000/S00076.cs
@@ -0,0 +1,62 @@
+﻿namespace leetcode_sharp;
+
+// 76. Minimum Window Substring
+// https://leetcode.com/problems/minimum-window-substring/
+public class S00076
+{
+    public string MinWindow(string s, string t)
+    {
+        if (s.Length == 0 || t.Length == 0)
+        {
+            return "";
+        }
+
+        var bank = new int[256];
+
+        var left = 0;
+        var right = 0;
+        var count = 0;
+
+        var min = int.MaxValue;
+        var minString = "";
+
+        var pLength = t.Length;
+        var length = s.Length;
+
+        for (var i = 0; i < pLength; i++)
+        {
+            bank[t[i]]++;
+        }
+
+        while (right < length)
+        {
+            var rightChar = s[right++];
+            if (bank[rightChar] > 0)
+            {
+                count++;
+            }
+
+            bank[rightChar]--;
+
+            while (count == pLength)
+            {
+                var size = right - left;
+                if (min > size)
+                {
+                    min = size;
+                    minString = s.Substring(left, right - left);
+                }
+
+                var leftChar = s[left++];
+                bank[leftChar]++;
+
+                if (bank[leftChar] > 0)
+                {
+                    count--;
+                }
+            }
+        }
+
+        return minString;
+    }
+}
