e33b9a88ae2d02028c2a96614273d6fa80a988fb
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-12 12:55:34 +1000

    25. Reverse Nodes in k-Group

---
+++ UnitTests/0000/S00025Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00025Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00025();
+        sut.ReverseKGroup(ListNode.Parse("[1,2,3,4,5]"), 2).Should().BeEquivalentTo(ListNode.Parse("[2,1,4,3,5]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00025();
+        sut.ReverseKGroup(ListNode.Parse("[1,2,3,4,5]"), 3).Should().BeEquivalentTo(ListNode.Parse("[3,2,1,4,5]"));
+    }
+}
---
+++ leetcode-sharp/0000/S00025.cs
@@ -0,0 +1,54 @@
+﻿namespace leetcode_sharp;
+
+// 25. Reverse Nodes in k-Group
+// https://leetcode.com/problems/reverse-nodes-in-k-group/
+public class S00025
+{
+    public ListNode? ReverseKGroup(ListNode? head, int k)
+    {
+        if (k == 1)
+        {
+            return head;
+        }
+
+        var tempList = new List<int>();
+        var finalList = new List<int>();
+        var newNode = new ListNode();
+        var finalNode = newNode;
+
+        while (head != null)
+        {
+            tempList.Add(head.val);
+            head = head.next;
+        }
+
+        var count = tempList.Count / k;
+        var j = 0;
+
+        for (var i = 0; i < count; i++)
+        {
+            var list = tempList.Skip(j).Take(k);
+            j = j + k;
+            list = list.Reverse();
+            finalList.AddRange(list);
+        }
+
+        if (tempList.Count - finalList.Count > 0)
+        {
+            finalList.AddRange(tempList.Skip(finalList.Count).Take(tempList.Count - finalList.Count));
+        }
+
+        for (var i = 0; i < finalList.Count; i++)
+        {
+            newNode.val = finalList[i];
+            if (i != finalList.Count - 1)
+            {
+                newNode.next = new ListNode();
+            }
+
+            newNode = newNode.next;
+        }
+
+        return finalNode;
+    }
+}
