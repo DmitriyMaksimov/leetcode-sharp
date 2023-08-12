8d223ca8b6b48b7245a77e959755679d679dd141
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-12 12:00:22 +1000

    56. Merge Intervals

---
+++ UnitTests/0000/S00056Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00056Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00056();
+        sut.Merge(new[] {new[] {1, 3}, new[] {2, 6}, new[] {8, 10}, new[] {15, 18}}).Should()
+            .BeEquivalentTo(new[] {new[] {1, 6}, new[] {8, 10}, new[] {15, 18}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00056();
+        sut.Merge(new[] {new[] {1, 4}, new[] {4, 5}}).Should().BeEquivalentTo(new[] {new[] {1, 5}});
+    }
+}
---
+++ leetcode-sharp/0000/S00056.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 56. Merge Intervals
+// https://leetcode.com/problems/merge-intervals/
+public class S00056
+{
+    public int[][] Merge(int[][] intervals)
+    {
+        var result = new List<int[]>();
+
+        Array.Sort(intervals, (a, b) => a[0] - b[0]);
+
+        result.Add(intervals[0]);
+
+        for (var i = 1; i < intervals.Length; i++)
+        {
+            if (result[^1][1] >= intervals[i][0])
+            {
+                if (result[^1][1] <= intervals[i][1])
+                {
+                    result[^1][1] = intervals[i][1];
+                }
+            }
+            else
+            {
+                result.Add(intervals[i]);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
