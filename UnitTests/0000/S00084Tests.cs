c7f63facb6cbf87ae411ec3be9809c3fe3172a30
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-12 17:00:45 +1000

    84. Largest Rectangle in Histogram

---
+++ UnitTests/0000/S00084Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00084Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00084();
+        sut.LargestRectangleArea(new[] {2, 1, 5, 6, 2, 3}).Should().Be(10);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00084();
+        sut.LargestRectangleArea(new[] {2, 4}).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/0000/S00084.cs
@@ -0,0 +1,48 @@
+﻿namespace leetcode_sharp;
+
+// 84. Largest Rectangle in Histogram
+// https://leetcode.com/problems/largest-rectangle-in-histogram/
+public class S00084
+{
+    public int LargestRectangleArea(int[] heights)
+    {
+        var n = heights.Length;
+        var stack = new Stack<int>();
+        var leftSmall = new int[n];
+        var rightSmall = new int[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            while (stack.Any() && heights[stack.Peek()] >= heights[i])
+            {
+                stack.Pop();
+            }
+
+            leftSmall[i] = stack.Any() ? stack.Peek() + 1 : 0;
+            stack.Push(i);
+        }
+
+        stack.Clear();
+
+        for (var i = n - 1; i >= 0; i--)
+        {
+            while (stack.Any() && heights[stack.Peek()] >= heights[i])
+            {
+                stack.Pop();
+            }
+
+            rightSmall[i] = stack.Any() ? stack.Peek() - 1 : n - 1;
+
+            stack.Push(i);
+        }
+
+        var maxA = 0;
+
+        for (var i = 0; i < n; i++)
+        {
+            maxA = Math.Max(maxA, heights[i] * (rightSmall[i] - leftSmall[i] + 1));
+        }
+
+        return maxA;
+    }
+}
