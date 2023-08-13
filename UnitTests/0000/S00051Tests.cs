3753ae1c66975ac03882ea4ecff308e1d3276560
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-13 14:43:08 +1000

    51. N-Queens

---
+++ UnitTests/0000/S00051Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp._0000;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00051Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00051();
+        sut.SolveNQueens(4).Should().BeEquivalentTo(new[]{new List<string> {".Q..", "...Q", "Q...", "..Q."},
+            new List<string> {"..Q.", "Q...", "...Q", ".Q.."}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00051();
+        sut.SolveNQueens(1).Should().BeEquivalentTo(new[]{new List<string> {"Q"}});
+    }
+}
---
+++ leetcode-sharp/0000/S00051.cs
@@ -0,0 +1,57 @@
+﻿namespace leetcode_sharp._0000;
+
+// 51. N-Queens
+// https://leetcode.com/problems/n-queens/
+public class S00051
+{
+    private readonly List<List<char>> _board = new();
+    private readonly IList<IList<string>> _ans = new List<IList<string>>();
+    private int[] _left;
+    private int[] _upperD;
+    private int[] _lowerD;
+    private int _n;
+
+    private void solve(int col)
+    {
+        if (col == _n)
+        {
+            _ans.Add(_board.Select(chars => string.Join("", chars)).ToList());
+            return;
+        }
+
+        for (var row = 0; row < _n; row++)
+        {
+            if (_left[row] == 0 && _lowerD[row + col] == 0 && _upperD[_n - 1 + col - row] == 0)
+            {
+                _board[row][col] = 'Q';
+                _left[row] = 1;
+                _lowerD[row + col] = 1;
+                _upperD[_n - 1 + col - row] = 1;
+
+                solve(col + 1);
+                
+                _board[row][col] = '.';
+                _left[row] = 0;
+                _lowerD[row + col] = 0;
+                _upperD[_n - 1 + col - row] = 0;
+            }
+        }
+    }
+
+    public IList<IList<string>> SolveNQueens(int n)
+    {
+        _n = n;
+        _left = new int[n];
+        _upperD = new int[2 * n - 1];
+        _lowerD = new int[2 * n - 1];
+        
+        for (var i = 0; i < n; i++)
+        {
+            _board.Add(new List<char>(Enumerable.Repeat('.', n)));
+        }
+
+        solve(0);
+
+        return _ans;
+    }
+}
