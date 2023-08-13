58522aeca552a88641ec59fac1920ce43f427744
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-13 15:03:55 +1000

    155. Min Stack

---
+++ UnitTests/0100/S00155Tests.cs
@@ -0,0 +1,20 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00155Tests
+{
+    [Test]
+    public void T1()
+    {
+        var minStack = new S00155.MinStack();
+        minStack.Push(-2);
+        minStack.Push(0);
+        minStack.Push(-3);
+        minStack.GetMin().Should().Be(-3);
+        minStack.Pop();
+        minStack.Top().Should().Be(0);
+        minStack.GetMin().Should().Be(-2);
+    }
+}
---
+++ leetcode-sharp/0100/S00155.cs
@@ -0,0 +1,36 @@
+﻿namespace leetcode_sharp;
+
+// 155. Min Stack
+// https://leetcode.com/problems/min-stack/
+public class S00155
+{
+public class MinStack
+{
+    private Stack<(int value, int min)> _stack = new();
+
+    public MinStack()
+    {
+    }
+
+    public void Push(int val)
+    {
+        var min = _stack.Any() ? GetMin() : val;
+        _stack.Push((val, Math.Min(val, min)));
+    }
+
+    public void Pop()
+    {
+        _stack.Pop();
+    }
+
+    public int Top()
+    {
+        return _stack.Peek().value;
+    }
+
+    public int GetMin()
+    {
+        return _stack.Peek().min;
+    }
+}
+}
