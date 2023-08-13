71f6a2a17406dfb8b59f86f19e76c1cdada6e48a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-13 22:33:19 +1000

    58. Length of Last Word

---
+++ UnitTests/0000/S00058Tests.cs
@@ -0,0 +1,35 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00058Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00058();
+        sut.LengthOfLastWord("Hello World").Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00058();
+        sut.LengthOfLastWord("   fly me   to   the moon  ").Should().Be(4);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00058();
+        sut.LengthOfLastWord("luffy is still joyboy").Should().Be(6);
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S00058();
+        sut.LengthOfLastWord("test  ").Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/0000/S00058.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 58. Length of Last Word
+// https://leetcode.com/problems/length-of-last-word/
+public class S00058
+{
+    public int LengthOfLastWord(string s)
+    {
+        var n = s.Length - 1;
+
+        while (s[n] == ' ')
+        {
+            --n;
+        }
+
+        var len = 0;
+
+        while (n >= 0 && s[n--] != ' ')
+        {
+            ++len;
+        }
+        
+        
+        return len;
+    }
+}
