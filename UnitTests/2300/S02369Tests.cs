3d6658b74c3457d56fd3bef81e25553a4ec8e912
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-13 13:35:53 +1000

    2369. Check if There is a Valid Partition For The Array

---
+++ UnitTests/2300/S02369Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02369Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02369();
+        sut.ValidPartition(new[] {4, 4, 4, 5, 6}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02369();
+        sut.ValidPartition(new[] {1, 1, 1, 2}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/2300/S02369.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode_sharp;
+
+// 2369. Check if There is a Valid Partition For The Array
+// https://leetcode.com/problems/check-if-there-is-a-valid-partition-for-the-array/
+public class S02369
+{
+    public bool ValidPartition(int[] nums)
+    {
+        var n = nums.Length;
+        var dp = new bool[n + 1];
+        dp[0] = true;
+
+        if (nums[1] == nums[0])
+        {
+            dp[2] = true;
+        }
+
+        for (var i = 2; i < n; i++)
+        {
+            if (nums[i] == nums[i - 1])
+            {
+                dp[i + 1] |= dp[i - 1];
+            }
+
+            if (nums[i] == nums[i - 1] && nums[i] == nums[i - 2])
+            {
+                dp[i + 1] |= dp[i - 2];
+            }
+
+            if (nums[i] == nums[i - 1] + 1 && nums[i] == nums[i - 2] + 2)
+            {
+                dp[i + 1] |= dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+}
