2a395f7471e7274ef120c82fe9498398ad4b6081
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-13 21:26:01 +1000

    135. Candy

---
+++ UnitTests/0100/S00135Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00135Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00135();
+        sut.Candy(new[] {1, 0, 2}).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00135();
+        sut.Candy(new[] {1, 2, 2}).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/0100/S00135.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 135. Candy
+// https://leetcode.com/problems/candy/
+public class S00135
+{
+    public int Candy(int[] ratings)
+    {
+        var n = ratings.Length;
+        if (n <= 1)
+        {
+            return n;
+        }
+
+        var num = Enumerable.Repeat(1, n).ToArray();
+
+        for (var i = 1; i < n; i++)
+        {
+            if (ratings[i] > ratings[i - 1])
+            {
+                num[i] = num[i - 1] + 1;
+            }
+        }
+
+        for (var i = n - 1; i > 0; i--)
+        {
+            if (ratings[i - 1] > ratings[i])
+            {
+                num[i - 1] = Math.Max(num[i] + 1, num[i - 1]);
+            }
+        }
+
+        return num.Sum();
+    }
+}
