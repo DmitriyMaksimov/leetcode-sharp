441be9b78c91470f821088033108bf6c2ca9d6b0
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-13 21:40:48 +1000

    52. N-Queens II

---
+++ UnitTests/0000/S00052Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00052Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00052();
+        sut.TotalNQueens(4).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00052();
+        sut.TotalNQueens(1).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/0000/S00052.cs
@@ -0,0 +1,57 @@
+﻿namespace leetcode_sharp;
+
+// 52. N-Queens II
+// https://leetcode.com/problems/n-queens-ii/
+public class S00052
+{
+    private readonly List<List<char>> _board = new();
+    private int _ans;
+    private int[] _left;
+    private int[] _upperD;
+    private int[] _lowerD;
+    private int _n;
+
+    public int TotalNQueens(int n)
+    {
+        _n = n;
+        _left = new int[n];
+        _upperD = new int[2 * n - 1];
+        _lowerD = new int[2 * n - 1];
+
+        for (var i = 0; i < n; i++)
+        {
+            _board.Add(new List<char>(Enumerable.Repeat('.', n)));
+        }
+
+        solve(0);
+
+        return _ans;
+    }
+
+    private void solve(int col)
+    {
+        if (col == _n)
+        {
+            ++_ans;
+            return;
+        }
+
+        for (var row = 0; row < _n; row++)
+        {
+            if (_left[row] == 0 && _lowerD[row + col] == 0 && _upperD[_n - 1 + col - row] == 0)
+            {
+                _board[row][col] = 'Q';
+                _left[row] = 1;
+                _lowerD[row + col] = 1;
+                _upperD[_n - 1 + col - row] = 1;
+
+                solve(col + 1);
+
+                _board[row][col] = '.';
+                _left[row] = 0;
+                _lowerD[row + col] = 0;
+                _upperD[_n - 1 + col - row] = 0;
+            }
+        }
+    }
+}
