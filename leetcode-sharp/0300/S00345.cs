6635ab1219de9b71b23161c89a8d83c6f7702aba
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-13 16:56:44 +1000

    345. Reverse Vowels of a String

---
+++ leetcode-sharp/0300/S00345.cs
@@ -0,0 +1,37 @@
+﻿namespace leetcode_sharp;
+
+// 345. Reverse Vowels of a String
+// https://leetcode.com/problems/reverse-vowels-of-a-string/
+public class S00345
+{
+    private readonly HashSet<char> _vowels = new() { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+    public string ReverseVowels(string s)
+    {
+        var chars = s.ToCharArray();
+        var left = 0;
+        var right = s.Length - 1;
+
+        while (left < right)
+        {
+            if (!_vowels.Contains(chars[left]))
+            {
+                ++left;
+            }
+
+            if (!_vowels.Contains(chars[right]))
+            {
+                --right;
+            }
+
+            if (_vowels.Contains(chars[left]) && _vowels.Contains(chars[right]))
+            {
+                (chars[left], chars[right]) = (chars[right], chars[left]);
+                ++left;
+                --right;
+            }
+        }
+
+        return new string(chars);
+    }
+}
