9f6e00642f202163727b04d4faa7c628da89836e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-13 21:50:30 +1000

    68. Text Justification

---
+++ UnitTests/0000/S00068Tests.cs
@@ -0,0 +1,45 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00068Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00068();
+        sut.FullJustify(new[] {"This", "is", "an", "example", "of", "text", "justification."}, 16).Should().Equal(
+            "This    is    an",
+            "example  of text",
+            "justification.  ");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00068();
+        sut.FullJustify(new[] {"What", "must", "be", "acknowledgment", "shall", "be"}, 16).Should().Equal(
+            "What   must   be",
+            "acknowledgment  ",
+            "shall be        ");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00068();
+        sut.FullJustify(
+            new[]
+            {
+                "Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.",
+                "Art", "is", "everything", "else", "we", "do"
+            }, 20).Should().Equal(
+            "Science  is  what we",
+            "understand      well",
+            "enough to explain to",
+            "a  computer.  Art is",
+            "everything  else  we",
+            "do                  ");
+    }
+}
---
+++ leetcode-sharp/0000/S00068.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 68. Text Justification
+// https://leetcode.com/problems/text-justification/
+public class S00068
+{
+    public IList<string> FullJustify(string[] words, int maxWidth)
+    {
+        var result = new List<string>();
+        var currentLine = new List<string>();
+        var currentLength = 0;
+
+        foreach (var word in words)
+        {
+            if (currentLength + currentLine.Count + word.Length > maxWidth)
+            {
+                var numWords = currentLine.Count;
+                var numSpaces = maxWidth - currentLength;
+                var justifiedLine = "";
+
+                if (numWords == 1)
+                {
+                    justifiedLine = currentLine[0] + new string(' ', numSpaces);
+                }
+                else
+                {
+                    var spacesBetweenWords = numSpaces / (numWords - 1);
+                    var extraSpaces = numSpaces % (numWords - 1);
+
+                    var sb = new StringBuilder();
+
+                    for (var i = 0; i < numWords - 1; i++)
+                    {
+                        sb.Append(currentLine[i]);
+                        sb.Append(new string(' ', spacesBetweenWords));
+
+                        if (i < extraSpaces)
+                        {
+                            sb.Append(' ');
+                        }
+                    }
+
+                    sb.Append(currentLine[numWords - 1]);
+                    justifiedLine = sb.ToString();
+                }
+
+                result.Add(justifiedLine);
+                currentLine.Clear();
+                currentLength = 0;
+            }
+
+            currentLine.Add(word);
+            currentLength += word.Length;
+        }
+
+        var lastLine = string.Join(" ", currentLine);
+        lastLine += new string(' ', maxWidth - lastLine.Length);
+        result.Add(lastLine);
+
+        return result;
+    }
+}
