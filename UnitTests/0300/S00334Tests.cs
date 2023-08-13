5899b1422d2691baa264f1f07a336ea25c89649b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-13 21:32:29 +1000

    334. Increasing Triplet Subsequence

---
+++ UnitTests/0300/S00334Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00334Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00334();
+        sut.IncreasingTriplet(new[] {1, 2, 3, 4, 5}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00334();
+        sut.IncreasingTriplet(new[] {5, 4, 3, 2, 1}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00334();
+        sut.IncreasingTriplet(new[] {2, 1, 5, 0, 4, 6}).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/0300/S00334.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 334. Increasing Triplet Subsequence
+// https://leetcode.com/problems/increasing-triplet-subsequence/
+public class S00334
+{
+    public bool IncreasingTriplet(int[] nums)
+    {
+        var max1 = int.MaxValue;
+        var max2 = int.MaxValue;
+
+        foreach (var n in nums)
+        {
+            if (n <= max1)
+            {
+                max1 = n;
+            }
+            else if (n <= max2)
+            {
+                max2 = n;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
