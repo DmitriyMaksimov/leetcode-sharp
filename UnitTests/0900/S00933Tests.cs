8304de41f7810da15946aafd4c1d17f8bdeb0ee7
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 11:31:36 +1000

    933. Number of Recent Calls

---
+++ UnitTests/0900/S00933Tests.cs
@@ -0,0 +1,17 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00933Tests
+{
+    [Test]
+    public void T1()
+    {
+        var recentCounter = new S00933.RecentCounter();
+        recentCounter.Ping(1).Should().Be(1); // requests = [1], range is [-2999,1], return 1
+        recentCounter.Ping(100).Should().Be(2); // requests = [1, 100], range is [-2900,100], return 2
+        recentCounter.Ping(3001).Should().Be(3); // requests = [1, 100, 3001], range is [1,3001], return 3
+        recentCounter.Ping(3002).Should().Be(3); // requests = [1, 100, 3001, 3002], range is [2,3002], return 3
+    }
+}
---
+++ leetcode-sharp/0900/S00933.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode_sharp;
+
+// 933. Number of Recent Calls
+// https://leetcode.com/problems/number-of-recent-calls/
+public static class S00933
+{
+    public class RecentCounter
+    {
+        private readonly List<int> _calls = new();
+
+        public int Ping(int t)
+        {
+            _calls.Add(t);
+            return _calls.Count(call => t - call <= 3000);
+        }
+    }
+}
