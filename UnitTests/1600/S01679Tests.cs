29e42374e77eae5c13d92c3f5093912f96ab4407
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 11:01:40 +1000

    1679. Max Number of K-Sum Pairs

---
+++ UnitTests/1600/S01679Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01679Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01679();
+        sut.MaxOperations(new[] {1, 2, 3, 4}, 5).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01679();
+        sut.MaxOperations(new[] {3, 1, 3, 4, 3}, 6).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1600/S01679.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 1679. Max Number of K-Sum Pairs
+// https://leetcode.com/problems/max-number-of-k-sum-pairs/
+public class S01679
+{
+    public int MaxOperations(int[] nums, int k)
+    {
+        Array.Sort(nums);
+        
+        var i = 0;
+        var j = nums.Length - 1;
+        var count = 0;
+
+        while (j > i)
+        {
+            if (nums[i] + nums[j] == k)
+            {
+                i++;
+                j--;
+                count++;
+            }
+            else if (nums[i] + nums[j] < k)
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+
+        return count;
+    }
+}
