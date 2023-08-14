511c3b8bda45490e15849b2f27e23f397ca41b72
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 12:05:46 +1000

    450. Delete Node in a BST

---
+++ UnitTests/0400/S00450Tests.cs
@@ -0,0 +1,30 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00450Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00450();
+        sut.DeleteNode(TreeNode.Parse("[5,3,6,2,4,null,7]"), 3).Should()
+            .BeEquivalentTo(TreeNode.Parse("[5,4,6,2,null,null,7]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00450();
+        sut.DeleteNode(TreeNode.Parse("[5,3,6,2,4,null,7]"), 0).Should()
+            .BeEquivalentTo(TreeNode.Parse("[5,3,6,2,4,null,7]"));
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00450();
+        sut.DeleteNode(null, 0).Should().BeNull();
+    }
+}
---
+++ leetcode-sharp/0400/S00450.cs
@@ -0,0 +1,51 @@
+﻿namespace leetcode_sharp;
+
+// 450. Delete Node in a BST
+// https://leetcode.com/problems/delete-node-in-a-bst/
+public class S00450
+{
+    public TreeNode? DeleteNode(TreeNode? root, int key)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+
+        if (key < root.val)
+        {
+            root.left = DeleteNode(root.left, key);
+        }
+        else if (key > root.val)
+        {
+            root.right = DeleteNode(root.right, key);
+        }
+        else
+        {
+            if (root.left == null)
+            {
+                return root.right;
+            }
+
+            if (root.right == null)
+            {
+                return root.left;
+            }
+
+            var minNode = findMin(root.right);
+            root.val = minNode.val;
+            root.right = DeleteNode(root.right, root.val);
+        }
+
+        return root;
+    }
+
+    private static TreeNode findMin(TreeNode node)
+    {
+        while (node.left != null)
+        {
+            node = node.left;
+        }
+
+        return node;
+    }
+}
