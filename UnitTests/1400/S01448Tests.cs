a5b1dce7d6eabcae38e6f4d627a4dc6d423e0189
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 11:58:20 +1000

    1448. Count Good Nodes in Binary Tree

---
+++ UnitTests/1400/S01448Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01448Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01448();
+        sut.GoodNodes(TreeNode.Parse("[3,1,4,3,null,1,5]")!).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01448();
+        sut.GoodNodes(TreeNode.Parse("[3,3,null,4,2]")!).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01448();
+        sut.GoodNodes(TreeNode.Parse("[1]")!).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1400/S01448.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 1448. Count Good Nodes in Binary Tree
+// https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+public class S01448
+{
+    public int GoodNodes(TreeNode root)
+    {
+        return dfs(root, int.MinValue);
+    }
+
+    private static int dfs(TreeNode? root, int maxVal)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        var res = root.val >= maxVal ? 1 : 0;
+
+        res += dfs(root.left, Math.Max(root.val, maxVal));
+        res += dfs(root.right, Math.Max(root.val, maxVal));
+
+        return res;
+    }
+}
