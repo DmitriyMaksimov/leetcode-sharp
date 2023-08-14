2c65c3a5b51be878c2ba4e082c411a8733754fac
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 16:48:37 +1000

    97. Interleaving String

---
+++ UnitTests/0000/S00097Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00097Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00097();
+        sut.IsInterleave("aabcc", "dbbca", "aadbbcbcac").Should().BeTrue();
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00097();
+        sut.IsInterleave("aabcc", "dbbca", "aadbbbaccc").Should().BeFalse();
+    }
+    
+    [Test]
+    public void T3()
+    {
+        var sut = new S00097();
+        sut.IsInterleave("", "", "").Should().BeTrue();
+    }
+
+}
---
+++ leetcode-sharp/0000/S00097.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 97. Interleaving String
+// https://leetcode.com/problems/interleaving-string/
+public class S00097
+{
+    public bool IsInterleave(string s1, string s2, string s3)
+    {
+        return s3.Length == s1.Length + s2.Length && helper(s1, s2, s3, s1.Length - 1, s2.Length - 1, s3.Length - 1);
+    }
+
+    private static bool helper(string s1, string s2, string s3, int l1, int l2, int l3)
+    {
+        switch (l1)
+        {
+            case < 0 when l2 < 0 && l3 < 0:
+                return true;
+
+            case >= 0 when s1[l1] == s3[l3] && l2 >= 0 && s2[l2] == s3[l3]:
+                return helper(s1, s2, s3, l1 - 1, l2, l3 - 1) || helper(s1, s2, s3, l1, l2 - 1, l3 - 1);
+            
+            case >= 0 when s1[l1] == s3[l3]:
+                return helper(s1, s2, s3, l1 - 1, l2, l3 - 1);
+        }
+
+        if (l2 >= 0 && s2[l2] == s3[l3])
+        {
+            return helper(s1, s2, s3, l1, l2 - 1, l3 - 1);
+        }
+        
+        return false;
+    }
+}
