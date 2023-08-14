df995aea96007772ca2d318bbe403e9d8894f729
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 16:53:58 +1000

    172. Factorial Trailing Zeroes

---
+++ UnitTests/0100/S00172Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00172Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00172();
+        sut.TrailingZeroes(3).Should().Be(0);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00172();
+        sut.TrailingZeroes(5).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00172();
+        sut.TrailingZeroes(0).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0100/S00172.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 172. Factorial Trailing Zeroes
+// https://leetcode.com/problems/factorial-trailing-zeroes/
+public class S00172
+{
+    public int TrailingZeroes(int n)
+    {
+        var result = 0;
+
+        while (n >= 5)
+        {
+            n /= 5;
+            result += n;
+        }
+
+        return result;
+    }
+}
