2fcb2d139b6e599bd7287a007a254eb3d8e5e517
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 13:12:27 +1000

    125. Valid Palindrome

---
+++ UnitTests/0100/S00125Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00125Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00125();
+        sut.IsPalindrome("A man, a plan, a canal: Panama").Should().BeTrue();
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00125();
+        sut.IsPalindrome("race a car").Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00125();
+        sut.IsPalindrome(" ").Should().BeTrue();
+    }
+
+}
---
+++ leetcode-sharp/0100/S00125.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 125. Valid Palindrome
+// https://leetcode.com/problems/valid-palindrome/
+public class S00125
+{
+    public bool IsPalindrome(string s)
+    {
+        return isPalindrome(new string(s.Where(c => char.IsLetter(c) || char.IsNumber(c)).Select(char.ToLower).ToArray()));
+    }
+
+    private static bool isPalindrome(string s)
+    {
+        for (var i = 0; i < s.Length / 2; i++)
+        {
+            if (s[i] != s[s.Length - i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
