b3bf018d782e3b2fabfd89ae43104034e803dbaf
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 13:04:07 +1000

    205. Isomorphic Strings

---
+++ UnitTests/0200/S00205Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00205Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00205();
+        sut.IsIsomorphic("egg", "add").Should().BeTrue();
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00205();
+        sut.IsIsomorphic("foo", "bar").Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00205();
+        sut.IsIsomorphic("paper", "title").Should().BeTrue();
+    }
+
+}
---
+++ leetcode-sharp/0200/S00205.cs
@@ -0,0 +1,36 @@
+﻿namespace leetcode_sharp;
+
+// 205. Isomorphic Strings
+// https://leetcode.com/problems/isomorphic-strings/
+public class S00205
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        var dictionary = new Dictionary<char, char>();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var charS = s[i];
+            var charT = t[i];
+ 
+            if (dictionary.TryGetValue(charS, out var value))
+            {
+                if (charT != value)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (dictionary.ContainsValue(charT))
+                {
+                    return false;
+                }
+
+                dictionary.Add(charS, charT);
+            }
+        }
+
+        return true;
+    }
+}
