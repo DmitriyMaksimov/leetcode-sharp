dc887cd4b47355677657c61c813816d5d1501884
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 12:28:03 +1000

    1268. Search Suggestions System

---
+++ UnitTests/1200/S01268Tests.cs
@@ -0,0 +1,38 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01268Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01268();
+        sut.SuggestedProducts(new[] {"mobile", "mouse", "moneypot", "monitor", "mousepad"}, "mouse").Should()
+            .BeEquivalentTo(new[]
+            {
+                new[] {"mobile", "moneypot", "monitor"},
+                new[] {"mobile", "moneypot", "monitor"},
+                new[] {"mouse", "mousepad"},
+                new[] {"mouse", "mousepad"},
+                new[] {"mouse", "mousepad"}
+            });
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01268();
+        sut.SuggestedProducts(new[] {"havana"}, "havana").Should()
+            .BeEquivalentTo(new[]
+            {
+                new[] {"havana"},
+                new[] {"havana"},
+                new[] {"havana"},
+                new[] {"havana"},
+                new[] {"havana"},
+                new[] {"havana"},
+            });
+    }
+}
---
+++ leetcode-sharp/1200/S01268.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 1268. Search Suggestions System
+// https://leetcode.com/problems/search-suggestions-system/
+public class S01268
+{
+    public IList<IList<string>> SuggestedProducts(string[] products, string searchWord)
+    {
+        var result = new List<IList<string>>();
+
+        for (var i = 1; i <= searchWord.Length; i++)
+        {
+            var curSearchSubstring = searchWord[..i];
+            result.Add(
+                products
+                    .Where(p => (p.Length > i ? p[..i] : p) == curSearchSubstring)
+                    .OrderBy(p => p)
+                    .Take(3)
+                    .ToList());
+        }
+
+        return result;
+    }
+}
