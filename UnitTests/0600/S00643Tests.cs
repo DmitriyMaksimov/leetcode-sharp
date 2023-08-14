e86d05a4346027f33c139b15885b335e80742db6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 11:09:01 +1000

    643. Maximum Average Subarray I

---
+++ UnitTests/0600/S00643Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00643Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00643();
+        sut.FindMaxAverage(new[] {1, 12, -5, -6, 50, 3}, 4).Should().Be(12.75);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00643();
+        sut.FindMaxAverage(new[] {5}, 1).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/0600/S00643.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 643. Maximum Average Subarray I
+// https://leetcode.com/problems/maximum-average-subarray-i/
+public class S00643
+{
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        var n = nums.Length;
+        var sum = new int[n + 1];
+        sum[0] = 0;
+
+        for (var i = 1; i <= n; i++)
+        {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+
+        var max = double.MinValue;
+
+        for (int i = k, j = 0; i <= n; i++, j++)
+        {
+            max = Math.Max(max, (double) (sum[i] - sum[j]) / k);
+        }
+
+        return max;
+    }
+}
