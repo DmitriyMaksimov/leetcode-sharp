d46d52e3ea6890334a75518fa7ffce6b212c9a45
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 11:24:33 +1000

    724. Find Pivot Index

---
+++ UnitTests/0700/S00724Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00724Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00724();
+        sut.PivotIndex(new[] {1, 7, 3, 6, 5, 6}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00724();
+        sut.PivotIndex(new[] {1, 2, 3}).Should().Be(-1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00724();
+        sut.PivotIndex(new[] {2, 1, -1}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0700/S00724.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 724. Find Pivot Index
+// https://leetcode.com/problems/find-pivot-index/
+public class S00724
+{
+    public int PivotIndex(int[] nums)
+    {
+        var totalSum = nums.Sum();
+
+        for (int i = 0, leftSum = 0; i < nums.Length; ++i)
+        {
+            if (leftSum * 2 == totalSum - nums[i])
+            {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
