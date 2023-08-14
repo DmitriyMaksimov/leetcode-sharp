9119873027710d4bf5452404906cb0689efdd324
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 13:19:55 +1000

    14. Longest Common Prefix

---
+++ UnitTests/0000/S00014Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00014Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00014();
+        sut.LongestCommonPrefix(new[] {"flower", "flow", "flight"}).Should().Be("fl");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00014();
+        sut.LongestCommonPrefix(new[] {"dog", "racecar", "car"}).Should().Be("");
+    }
+}
---
+++ leetcode-sharp/0000/S00014.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 14. Longest Common Prefix
+// https://leetcode.com/problems/longest-common-prefix/
+public class S00014
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        Array.Sort(strs);
+        
+        var s1 = strs[0];
+        var s2 = strs[^1];
+        var index = 0;
+
+        while (index < s1.Length && index < s2.Length)
+        {
+            if (s1[index] != s2[index])
+            {
+                break;
+            }
+
+            index++;
+        }
+
+        return s1[..index];
+    }
+}
