cb251c685ab492f02dcc61896415ce883cf7d51e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 12:57:42 +1000

    219. Contains Duplicate II

---
+++ UnitTests/0200/S00219Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00219Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00219();
+        sut.ContainsNearbyDuplicate(new[] {1, 2, 3, 1}, 3).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00219();
+        sut.ContainsNearbyDuplicate(new[] {1, 0, 1, 1}, 1).Should().BeTrue();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00219();
+        sut.ContainsNearbyDuplicate(new[] {1, 2, 3, 1, 2, 3}, 2).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0200/S00219.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 219. Contains Duplicate II
+// https://leetcode.com/problems/contains-duplicate-ii/
+public class S00219
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var j = i + 1;
+            while (j < nums.Length && Math.Abs(i - j) <= k)
+            {
+                if (nums[i] == nums[j])
+                {
+                    return true;
+                }
+
+                j++;
+            }
+        }
+
+        return false;
+    }
+}
