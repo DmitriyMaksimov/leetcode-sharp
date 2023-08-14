c05c98c8b5f4b3a0226ee96a8ff460ab1a45406b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 11:14:46 +1000

    1004. Max Consecutive Ones III

---
+++ leetcode-sharp/1000/S01004.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 1004. Max Consecutive Ones III
+// https://leetcode.com/problems/max-consecutive-ones-iii/
+public class S01004
+{
+    public int LongestOnes(int[] nums, int k)
+    {
+        var i = 0;
+        var j = 0;
+
+        for (; j < nums.Length; ++j)
+        {
+            if (nums[j] == 0)
+            {
+                k--;
+            }
+
+            if (k < 0 && nums[i++] == 0)
+            {
+                k++;
+            }
+        }
+
+        return j - i;
+    }
+}
