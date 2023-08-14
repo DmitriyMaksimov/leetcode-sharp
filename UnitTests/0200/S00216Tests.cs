688e82554828a1cf54bccce23de22c3f0b0675e6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 12:15:25 +1000

    216. Combination Sum III

---
+++ UnitTests/0200/S00216Tests.cs
@@ -0,0 +1,30 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00216Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00216();
+        sut.CombinationSum3(3, 7).Should()
+            .BeEquivalentTo(new[] {new List<int> {1, 2, 4}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00216();
+        sut.CombinationSum3(3, 9).Should()
+            .BeEquivalentTo(new[] {new List<int> {1,2,6 }, new List<int> {1,3,5 }, new List<int> {2,3,4 }});
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00216();
+        sut.CombinationSum3(4, 1).Should().BeEmpty();
+    }
+}
---
+++ leetcode-sharp/0200/S00216.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 216. Combination Sum III
+// https://leetcode.com/problems/combination-sum-iii/
+public class S00216
+{
+    private readonly int[] _arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private readonly List<IList<int>> _ans = new();
+    private readonly List<int> _ds = new();
+
+    public IList<IList<int>> CombinationSum3(int k, int n)
+    {
+        helper(0, k, n);
+        return _ans;
+    }
+
+    private void helper(int idx, int len, int target)
+    {
+        if (idx == _arr.Length)
+        {
+            if (_ds.Count == len && target == 0) _ans.Add(new List<int>(_ds));
+            return;
+        }
+
+        _ds.Add(_arr[idx]);
+        helper(idx + 1, len, target - _arr[idx]);
+        _ds.RemoveAt(_ds.Count - 1);
+        helper(idx + 1, len, target);
+    }
+}
