342157f5c85260052f92cc718a715ee12771b8fd
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 15:25:32 +1000

    80. Remove Duplicates from Sorted Array II

---
+++ UnitTests/0000/S00080Tests.cs
@@ -0,0 +1,34 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00080Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00080();
+        var nums = new[] {1, 1, 1, 2, 2, 3};
+        sut.RemoveDuplicates(nums).Should().Be(5);
+        nums[..5].Should().Equal(1, 1, 2, 2, 3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00080();
+        var nums = new[] {0, 0, 1, 1, 1, 1, 2, 3, 3};
+        sut.RemoveDuplicates(nums).Should().Be(7);
+        nums[..7].Should().Equal(0, 0, 1, 1, 2, 3, 3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00080();
+        var nums = new[] {1};
+        sut.RemoveDuplicates(nums).Should().Be(1);
+        nums.Should().Equal(1);
+    }
+}
---
+++ leetcode-sharp/0000/S00080.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 80. Remove Duplicates from Sorted Array II
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+public class S00080
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length <= 1)
+        {
+            return nums.Length;
+        }
+        
+        var j = 1;
+        
+        for (var i = 2; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[j - 1])
+            {
+                nums[++j] = nums[i];
+            }
+        }
+
+        return j + 1;
+    }
+}
