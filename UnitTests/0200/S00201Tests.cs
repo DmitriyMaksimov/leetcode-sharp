7326500bc4d84d3451252815360110e4505d0359
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 20:35:05 +1000

    201. Bitwise AND of Numbers Range

---
+++ UnitTests/0200/S00201Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00201Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00201();
+        sut.RangeBitwiseAnd(5, 7).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00201();
+        sut.RangeBitwiseAnd(0, 0).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00201();
+        sut.RangeBitwiseAnd(1, 2147483647).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0200/S00201.cs
@@ -0,0 +1,16 @@
+﻿namespace leetcode_sharp;
+
+// 201. Bitwise AND of Numbers Range
+// https://leetcode.com/problems/bitwise-and-of-numbers-range/
+public class S00201
+{
+    public int RangeBitwiseAnd(int left, int right)
+    {
+        while (right > left)
+        {
+            right &= right - 1;
+        }
+
+        return left & right;
+    }
+}
