546f5d3853730a16d406a34fc2447b91ac70bd17
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 12:36:33 +1000

    901. Online Stock Span

---
+++ UnitTests/0900/S00901Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00901Tests
+{
+    [Test]
+    public void T1()
+    {
+        var stockSpanner = new S00901.StockSpanner();
+        stockSpanner.Next(100).Should().Be(1); // return 1
+        stockSpanner.Next(80).Should().Be(1); // return 1
+        stockSpanner.Next(60).Should().Be(1); // return 1
+        stockSpanner.Next(70).Should().Be(2); // return 2
+        stockSpanner.Next(60).Should().Be(1); // return 1
+        stockSpanner.Next(75).Should().Be(4); // return 4, because the last 4 prices (including today's price of 75)
+                                              // were less than or equal to today's price.
+        stockSpanner.Next(85).Should().Be(6); // return 6
+    }
+}
---
+++ leetcode-sharp/0900/S00901.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode_sharp;
+
+// 901. Online Stock Span
+// https://leetcode.com/problems/online-stock-span/
+public static class S00901
+{
+public class StockSpanner
+{
+    private readonly List<int> _stockPrices = new();
+    private readonly List<int> _spans = new();
+
+    public int Next(int price)
+    {
+        var span = 1;
+        if (_stockPrices.Count > 0)
+        {
+            int i;
+
+            if (_stockPrices.Last() <= price)
+            {
+                span = _spans.Last() + 1;
+                i = _stockPrices.Count - span;
+            }
+            else
+            {
+                i = _stockPrices.Count - 1;
+            }
+
+            while (i >= 0 && _stockPrices[i] <= price)
+            {
+                span++;
+                i--;
+            }
+        }
+
+        _stockPrices.Add(price);
+        _spans.Add(span);
+
+        return span;
+    }
+}
+}
