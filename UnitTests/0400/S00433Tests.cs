e5bc99f0475559cedbcee47b8bbef250c205f869
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 20:35:21 +1000

    433. Minimum Genetic Mutation

---
+++ UnitTests/0400/S00433Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00433Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00433();
+        sut.MinMutation("AACCGGTT", "AACCGGTA", new[] {"AACCGGTA"}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00433();
+        sut.MinMutation("AACCGGTT", "AAACGGTA", new[] {"AACCGGTA", "AACCGCTA", "AAACGGTA"}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/0400/S00433.cs
@@ -0,0 +1,67 @@
+﻿namespace leetcode_sharp;
+
+// 433. Minimum Genetic Mutation
+// https://leetcode.com/problems/minimum-genetic-mutation/
+public class S00433
+{
+    public int MinMutation(string startGene, string endGene, string[] bank)
+    {
+        var q = new Queue<string>();
+        var visited = new HashSet<string>();
+
+        q.Enqueue(startGene);
+        visited.Add(startGene);
+
+        var mutations = 0;
+        var foundSolution = false;
+
+        while (q.Count > 0 && !foundSolution)
+        {
+            var iterationCount = q.Count;
+
+            for (var i = 0; i < iterationCount && !foundSolution; i++)
+            {
+                var c = q.Dequeue();
+
+                for (var j = 0; j < bank.Length && !foundSolution; j++)
+                {
+                    if (visited.Contains(bank[j]))
+                    {
+                        continue;
+                    }
+
+                    var diffs = 0;
+                    for (var k = 0; k < 8 && diffs <= 1; k++)
+                    {
+                        if (c[k] != bank[j][k])
+                        {
+                            diffs++;
+                        }
+                    }
+
+                    if (diffs == 1)
+                    {
+                        if (bank[j] == endGene)
+                        {
+                            foundSolution = true;
+                        }
+                        else
+                        {
+                            q.Enqueue(bank[j]);
+                            visited.Add(bank[j]);
+                        }
+                    }
+                }
+            }
+
+            mutations++;
+        }
+
+        if (!foundSolution)
+        {
+            return -1;
+        }
+
+        return mutations;
+    }
+}
