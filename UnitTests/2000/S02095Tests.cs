b62817eb54c4a203d6cd9ae359c25ff5c65aa747
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 11:41:27 +1000

    2095. Delete the Middle Node of a Linked List

---
+++ UnitTests/2000/S02095Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02095Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02095();
+        sut.DeleteMiddle(ListNode.Parse("[1,3,4,7,1,2,6]")).Should().BeEquivalentTo(ListNode.Parse("[1,3,4,1,2,6]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02095();
+        sut.DeleteMiddle(ListNode.Parse("[1,2,3,4]")).Should().BeEquivalentTo(ListNode.Parse("[1,2,4]"));
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02095();
+        sut.DeleteMiddle(ListNode.Parse("[2,1]")).Should().BeEquivalentTo(ListNode.Parse("[2]"));
+    }
+}
---
+++ leetcode-sharp/2000/S02095.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 2095. Delete the Middle Node of a Linked List
+// https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+public class S02095
+{
+    public ListNode? DeleteMiddle(ListNode? head)
+    {
+        if (head?.next == null)
+        {
+            return null;
+        }
+
+        var slowPointer = head;
+        var fastPointer = head.next;
+
+        while (fastPointer.next != null && fastPointer.next.next != null)
+        {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+
+        slowPointer.next = slowPointer.next.next;
+
+        return head;
+    }
+}
