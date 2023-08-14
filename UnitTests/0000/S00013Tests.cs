2dfdf411c6633347aa37c9676b5865857cc57e02
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-14 15:34:32 +1000

    13. Roman to Integer

---
+++ UnitTests/0000/S00013Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00013Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00013();
+        sut.RomanToInt("III").Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00013();
+        sut.RomanToInt("LVIII").Should().Be(58);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00013();
+        sut.RomanToInt("MCMXCIV").Should().Be(1994);
+    }
+}
---
+++ leetcode-sharp/0000/S00013.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 13. Roman to Integer
+// https://leetcode.com/problems/roman-to-integer/
+public class S00013
+{
+    private readonly Dictionary<char, int> _m = new() {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+
+    public int RomanToInt(string s)
+    {
+        var answer = 0;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (i < s.Length - 1 && _m[s[i]] < _m[s[i + 1]])
+            {
+                answer -= _m[s[i]];
+            }
+            else
+            {
+                answer += _m[s[i]];
+            }
+        }
+
+        return answer;
+    }
+}
