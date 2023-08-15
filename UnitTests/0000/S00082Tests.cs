354f90a8f7dc01a01cbde84f45921223ea4e69d6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-16 09:34:09 +1000

    82. Remove Duplicates from Sorted List II

---
+++ UnitTests/0000/S00082Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00082Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00082();
+        sut.DeleteDuplicates(ListNode.Parse("[1,2,3,3,4,4,5]")).Should().BeEquivalentTo(ListNode.Parse("[1,2,5]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00082();
+        sut.DeleteDuplicates(ListNode.Parse("[1,1,1,2,3]")).Should().BeEquivalentTo(ListNode.Parse("[2,3]"));
+    }
+}
---
+++ leetcode-sharp/0000/S00082.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 82. Remove Duplicates from Sorted List II
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+public class S00082
+{
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        var dummy = new ListNode(0, head);
+        var prev = dummy;
+
+        while (head != null)
+        {
+            if (head.next != null && head.val == head.next.val)
+            {
+                while (head.next != null && head.val == head.next.val)
+                {
+                    head = head.next;
+                }
+
+                prev.next = head.next;
+            }
+            else
+            {
+                prev = prev.next;
+            }
+
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+}
