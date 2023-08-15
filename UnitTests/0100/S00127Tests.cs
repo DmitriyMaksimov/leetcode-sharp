106e95ea3353df8613eb3f6a4f50913250788cce
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-16 09:33:47 +1000

    127. Word Ladder

---
+++ UnitTests/0100/S00127Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00127Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00127();
+        sut.LadderLength("hit", "cog", new List<string> {"hot", "dot", "dog", "lot", "log", "cog"}).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00127();
+        sut.LadderLength("hit", "cog", new List<string> {"hot", "dot", "dog", "lot", "log"}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0100/S00127.cs
@@ -0,0 +1,57 @@
+﻿namespace leetcode_sharp;
+
+// 127. Word Ladder
+// https://leetcode.com/problems/word-ladder/
+public class S00127
+{
+    public int LadderLength(string beginWord, string endWord, IList<string> wordList)
+    {
+        var set = new HashSet<string>(wordList);
+        if (!set.Contains(endWord))
+        {
+            return 0;
+        }
+
+        var queue = new Queue<string>();
+        queue.Enqueue(beginWord);
+        queue.Enqueue(beginWord);
+
+        var visited = new HashSet<string>();
+
+        var result = 1;
+
+        while (queue.Any())
+        {
+            var size = queue.Count;
+            for (var i = 0; i < size; i++)
+            {
+                var word = queue.Dequeue();
+                if (word == endWord)
+                {
+                    return result;
+                }
+
+                for (var j = 0; j < word.Length; j++)
+                {
+                    for (var k = 'a'; k <= 'z'; k++)
+                    {
+                        var arr = word.ToCharArray();
+                        arr[j] = k;
+
+                        var str = new string(arr);
+
+                        if (set.Contains(str) && !visited.Contains(str))
+                        {
+                            queue.Enqueue(str);
+                            visited.Add(str);
+                        }
+                    }
+                }
+            }
+
+            ++result;
+        }
+
+        return 0;
+    }
+}
