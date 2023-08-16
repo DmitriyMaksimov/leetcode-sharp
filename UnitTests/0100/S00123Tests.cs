bb123d2e6f6c757d9e888316473602e7b0db4f7a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-16 18:17:24 +1000

    123. Best Time to Buy and Sell Stock III

---
+++ UnitTests/0100/S00123Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00123Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00123();
+        sut.MaxProfit(new[] {3, 3, 5, 0, 0, 3, 1, 4}).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00123();
+        sut.MaxProfit(new[] {1, 2, 3, 4, 5}).Should().Be(4);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00123();
+        sut.MaxProfit(new[] {7, 6, 4, 3, 1}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0100/S00123.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 123. Best Time to Buy and Sell Stock III
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+public class S00123
+{
+    public int MaxProfit(int[] prices)
+    {
+        var buy1 = int.MaxValue;
+        var buy2 = int.MaxValue;
+        var sell1 = 0;
+        var sell2 = 0;
+
+        foreach (var price in prices)
+        {
+            buy1 = Math.Min(buy1, price);
+            sell1 = Math.Max(sell1, price - buy1);
+            buy2 = Math.Min(buy2, price - sell1);
+            sell2 = Math.Max(sell2, price - buy2);
+        }
+
+        return sell2;
+    }
+}
