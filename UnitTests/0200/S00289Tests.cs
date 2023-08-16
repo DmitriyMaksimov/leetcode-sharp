35fe3fb38a0b08d2dcebb4b8d2eb1c83a1679ce9
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-16 18:12:51 +1000

    289. Game of Life

---
+++ UnitTests/0200/S00289Tests.cs
@@ -0,0 +1,51 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00289Tests
+{
+    [Test]
+    public void T1()
+    {
+        var board = new[]
+        {
+            new[] {0, 1, 0},
+            new[] {0, 0, 1},
+            new[] {1, 1, 1},
+            new[] {0, 0, 0}
+        };
+
+        var sut = new S00289();
+        sut.GameOfLife(board);
+
+        board.Should().BeEquivalentTo(
+        new[]
+        {
+            new[] {0, 0, 0},
+            new[] {1, 0, 1},
+            new[] {0, 1, 1},
+            new[] {0, 1, 0}
+        });
+    }
+
+    [Test]
+    public void T2()
+    {
+        var board = new[]
+        {
+            new[] {1, 1},
+            new[] {1, 0},
+        };
+
+        var sut = new S00289();
+        sut.GameOfLife(board);
+
+        board.Should().BeEquivalentTo(
+        new[]
+        {
+            new[] {1, 1},
+            new[] {1, 1}
+        });
+    }
+}
---
+++ leetcode-sharp/0200/S00289.cs
@@ -0,0 +1,60 @@
+﻿namespace leetcode_sharp;
+
+// 289. Game of Life
+// https://leetcode.com/problems/game-of-life/
+public class S00289
+{
+    public void GameOfLife(int[][] board)
+    {
+        if (board.Length == 0)
+        {
+            return;
+        }
+
+        var m = board.Length;
+        var n = board[0].Length;
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                var lives = liveNeighbors(board, m, n, i, j);
+
+                if (board[i][j] == 1 && lives is >= 2 and <= 3)
+                {
+                    board[i][j] = 0b11;
+                }
+
+                if (board[i][j] == 0 && lives == 3)
+                {
+                    board[i][j] = 0b10;
+                }
+            }
+        }
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                board[i][j] >>= 1;
+            }
+        }
+    }
+
+    private static int liveNeighbors(int[][] board, int m, int n, int i, int j)
+    {
+        var lives = 0;
+        
+        for (var x = Math.Max(i - 1, 0); x <= Math.Min(i + 1, m - 1); x++)
+        {
+            for (var y = Math.Max(j - 1, 0); y <= Math.Min(j + 1, n - 1); y++)
+            {
+                lives += board[x][y] & 1;
+            }
+        }
+
+        lives -= board[i][j] & 1;
+
+        return lives;
+    }
+}
