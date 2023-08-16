725af13e20f3d928257c9edfe6eaf4f411403793
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-16 21:16:01 +1000

    274. H-Index

---
+++ UnitTests/0200/S00274Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00274Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00274();
+        sut.HIndex(new[] {3, 0, 6, 1, 5}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00274();
+        sut.HIndex(new[] {1, 3, 1}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/0200/S00274.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 274. H-Index
+// https://leetcode.com/problems/h-index/
+public class S00274
+{
+    public int HIndex(int[] citations)
+    {
+        Array.Sort(citations);
+        var result = 0;
+        var n = citations.Length;
+
+        for (var i = 0; i < n; i++)
+        {
+            result = Math.Max(result, Math.Min(citations[i], n - i));
+        }
+
+        return result;
+    }
+}
