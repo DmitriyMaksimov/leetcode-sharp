f1c137afee775ea4598d53a30e6770289b7b52c9
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-16 14:31:40 +1000

    30. Substring with Concatenation of All Words

---
+++ UnitTests/0000/S00030Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00030Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00030();
+        sut.FindSubstring("barfoothefoobarman", new[] {"foo", "bar"}).Should().Equal(0, 9);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00030();
+        sut.FindSubstring("wordgoodgoodgoodbestword", new[] {"word", "good", "best", "word"}).Should().BeEmpty();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00030();
+        sut.FindSubstring("barfoofoobarthefoobarman", new[] {"bar", "foo", "the"}).Should().Equal(6, 9, 12);
+    }
+}
---
+++ leetcode-sharp/0000/S00030.cs
@@ -0,0 +1,44 @@
+﻿namespace leetcode_sharp;
+
+// 30. Substring with Concatenation of All Words
+// https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+public class S00030
+{
+    private static bool ContentEquals<TKey, TValue>(Dictionary<TKey, TValue> dictionary, Dictionary<TKey, TValue> otherDictionary) where TKey : notnull
+    {
+        return otherDictionary
+            .OrderBy(kvp => kvp.Key)
+            .SequenceEqual(dictionary.OrderBy(kvp => kvp.Key));
+    }
+
+    public IList<int> FindSubstring(string s, string[] words)
+    {
+        var n = words.Length;
+        var len = words[0].Length;
+        var result = new List<int>();
+
+        var mp = new Dictionary<string, int>();
+
+        foreach (var str in words)
+        {
+            mp[str] = mp.GetValueOrDefault(str) + 1;
+        }
+
+        for (var i = 0; i < s.Length - (n * len) + 1; i++)
+        {
+            var m = new Dictionary<string, int>();
+            for (var j = i; j < i + n * len; j += len)
+            {
+                var str = s.Substring(j, len);
+                m[str] = m.GetValueOrDefault(str) + 1;
+            }
+
+            if (ContentEquals(mp, m))
+            {
+                result.Add(i);
+            }
+        }
+
+        return result;
+    }
+}
