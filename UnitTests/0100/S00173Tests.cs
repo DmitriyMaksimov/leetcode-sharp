820431fe2d53911a45325651eaaa8f0bb8d57aa9
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-16 16:51:43 +1000

    173. Binary Search Tree Iterator

---
+++ UnitTests/0100/S00173Tests.cs
@@ -0,0 +1,23 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00173Tests
+{
+    [Test]
+    public void T1()
+    {
+        var bstIterator = new S00173.BSTIterator(TreeNode.Parse("[7, 3, 15, null, null, 9, 20]"));
+        bstIterator.Next().Should().Be(3);
+        bstIterator.Next().Should().Be(7);
+        bstIterator.HasNext().Should().BeTrue();
+        bstIterator.Next().Should().Be(9);
+        bstIterator.HasNext().Should().BeTrue();
+        bstIterator.Next().Should().Be(15);
+        bstIterator.HasNext().Should().BeTrue();
+        bstIterator.Next().Should().Be(20);
+        bstIterator.HasNext().Should().BeFalse();
+    }
+
+}
---
+++ leetcode-sharp/0100/S00173.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcode_sharp;
+
+// 173. Binary Search Tree Iterator
+// https://leetcode.com/problems/binary-search-tree-iterator/
+public static class S00173
+{
+    public class BSTIterator
+    {
+        private readonly Stack<TreeNode> _stack = new();
+
+        public BSTIterator(TreeNode? root)
+        {
+            populateStack(root);
+        }
+
+        public int Next()
+        {
+            var root = _stack.Pop();
+            var val = root.val;
+
+            root = root.right;
+            populateStack(root);
+            return val;
+        }
+
+        public bool HasNext()
+        {
+            return _stack.Count > 0;
+        }
+
+        private void populateStack(TreeNode? root)
+        {
+            while (root != null)
+            {
+                _stack.Push(root);
+                root = root.left;
+            }
+        }
+    }
+}
