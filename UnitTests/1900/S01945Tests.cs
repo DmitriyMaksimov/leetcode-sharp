58d0d74957a5dbb35c5bde5a693f48fd2679bed6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-16 21:39:41 +1000

    1945. Sum of Digits of String After Convert

---
+++ UnitTests/1900/S01945Tests.cs
@@ -0,0 +1,42 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01945Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01945();
+        sut.GetLucky("iiii", 1).Should().Be(36);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01945();
+        sut.GetLucky("leetcode", 2).Should().Be(6);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01945();
+        sut.GetLucky("zbax", 2).Should().Be(8);
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S01945();
+        sut.GetLucky("iaozzbyqzwbpurzze", 2).Should().Be(5);
+    }
+
+    [Test]
+    public void T5()
+    {
+        var sut = new S01945();
+        sut.GetLucky("ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss", 2).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1900/S01945.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 1945. Sum of Digits of String After Convert
+// https://leetcode.com/problems/sum-of-digits-of-string-after-convert/
+public class S01945
+{
+    public int GetLucky(string s, int k)
+    {
+        var n = s.Select(ch => ch - 'a' + 1).Select(i => i / 10 + i % 10).Sum();
+
+        while (k-- > 1)
+        {
+            n = n / 1000 + n % 1000 / 100 + n % 100 / 10 + n % 10;
+        }
+        
+        
+        return (int) n;
+    }
+}
