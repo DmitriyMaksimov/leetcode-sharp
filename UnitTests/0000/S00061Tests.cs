28ad484dd4bb928bad4c36cf1d34228e057e5aea
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-16 10:39:31 +1000

    61. Rotate List

---
+++ UnitTests/0000/S00061Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00061Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00061();
+        sut.RotateRight(ListNode.Parse("[1,2,3,4,5]"), 2).Should().BeEquivalentTo(ListNode.Parse("[4,5,1,2,3]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00061();
+        sut.RotateRight(ListNode.Parse("[0,1,2]"), 4).Should().BeEquivalentTo(ListNode.Parse("[2,0,1]"));
+    }
+}
---
+++ leetcode-sharp/0000/S00061.cs
@@ -0,0 +1,44 @@
+﻿namespace leetcode_sharp;
+
+// 61. Rotate List
+// https://leetcode.com/problems/rotate-list/
+public class S00061
+{
+    public ListNode? RotateRight(ListNode? head, int k)
+    {
+        if (head == null)
+        {
+            return head;
+        }
+
+        var len = 1;
+        var curr = head;
+
+        while (curr.next != null)
+        {
+            curr = curr.next;
+            len++;
+        }
+
+        k %= len;
+        
+        if (k == 0)
+        {
+            return head;
+        }
+
+        curr.next = head;
+        
+        var newHead = head;
+        
+        for (var i = 0; i < len - k - 1; i++)
+        {
+            newHead = newHead.next;
+        }
+
+        var temp = newHead.next;
+        newHead.next = null;
+        
+        return temp;
+    }
+}
