74a0ed19d8eeedc68f85997241273bfe9a6a46fa
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-16 13:52:43 +1000

    210. Course Schedule II

---
+++ UnitTests/0200/S00210Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00210Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00210();
+        sut.FindOrder(2, new[] {new[] {1, 0}}).Should().Equal(0, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00210();
+        sut.FindOrder(4, new[] {new[] {1, 0}, new[] {2, 0}, new[] {3, 1}, new[] {3, 2}}).Should().Equal(0, 2, 1, 3);
+    }
+}
---
+++ leetcode-sharp/0200/S00210.cs
@@ -0,0 +1,64 @@
+﻿namespace leetcode_sharp;
+
+// 210. Course Schedule II
+// https://leetcode.com/problems/course-schedule-ii/
+public class S00210
+{
+    public int[] FindOrder(int numCourses, int[][] prerequisites)
+    {
+        var n = prerequisites.Length;
+
+        var arr = new List<List<int>>();
+        for (var i = 0; i < numCourses; i++)
+        {
+            arr.Add(new List<int>());
+        }
+
+        for (var i = 0; i < n; i++)
+        {
+            arr[prerequisites[i][0]].Add(prerequisites[i][1]);
+        }
+
+        var inp = new int[numCourses];
+
+        for (var i = 0; i < numCourses; i++)
+        {
+            foreach (var it in arr[i])
+            {
+                inp[it]++;
+            }
+        }
+
+        var q = new Queue<int>();
+        for (var i = 0; i < numCourses; i++)
+        {
+            if (inp[i] == 0)
+            {
+                q.Enqueue(i);
+            }
+        }
+
+        var res = new List<int>();
+        while (q.Any())
+        {
+            var node = q.Dequeue();
+
+            res.Add(node);
+            foreach (var i in arr[node])
+            {
+                if (--inp[i] == 0)
+                {
+                    q.Enqueue(i);
+                }
+            }
+        }
+
+        var ans = new int[res.Count];
+        for (var i = res.Count - 1; i >= 0; i--)
+        {
+            ans[ans.Length - i - 1] = res[i];
+        }
+
+        return res.Count != numCourses ? Array.Empty<int>() : ans;
+    }
+}
