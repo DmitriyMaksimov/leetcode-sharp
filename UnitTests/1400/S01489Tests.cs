e871cffb0522768f2346f92730874c54d862e6dd
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-19 16:47:36 +1000

    1489. Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree

---
+++ UnitTests/1400/S01489Tests.cs
@@ -0,0 +1,30 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01489Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01489();
+        sut.FindCriticalAndPseudoCriticalEdges(5,
+            new[]
+            {
+                new[] {0, 1, 1}, new[] {1, 2, 1}, new[] {2, 3, 2}, new[] {0, 3, 2}, new[] {0, 4, 3}, new[] {3, 4, 3},
+                new[] {1, 4, 6}
+            }).Should().BeEquivalentTo(new[] {new []{0, 1}, new[] {2, 3, 4, 5}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01489();
+        sut.FindCriticalAndPseudoCriticalEdges(4,
+            new[]
+            {
+                new[] {0, 1, 1}, new[] {1, 2, 1}, new[] {2, 3, 1}, new[] {0, 3, 1}
+            }).Should().BeEquivalentTo(new[] {Array.Empty<int>(), new[] {0, 1, 2, 3}});
+    }
+}
---
+++ leetcode-sharp/1400/S01489.cs
@@ -0,0 +1,58 @@
+﻿namespace leetcode_sharp;
+
+// 1489. Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree
+// https://leetcode.com/problems/find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/
+public class S01489
+{
+    public IList<IList<int>> FindCriticalAndPseudoCriticalEdges(int n, int[][] edges)
+    {
+        var critical = new List<int>();
+        var pseudo = new List<int>();
+
+        var map = new Dictionary<int[], int>();
+        for (var i = 0; i < edges.Length; i++)
+        {
+            map.Add(edges[i], i);
+        }
+
+        Array.Sort(edges, (a, b) => a[2] - b[2]);
+
+        var minCost = buildMST(n, edges, null, null);
+
+        foreach (var edge in edges)
+        {
+            var index = map[edge];
+            var costWithout = buildMST(n, edges, null, edge);
+            if (costWithout > minCost)
+            {
+                critical.Add(index);
+            }
+            else
+            {
+                var costWith = buildMST(n, edges, edge, null);
+                if (costWith == minCost)
+                {
+                    pseudo.Add(index);
+                }
+            }
+        }
+
+        return new List<IList<int>> {critical, pseudo};
+    }
+
+    private static int buildMST(int n, int[][] edges, int[] pick, int[] skip)
+    {
+        var unionFind = new UnionFind(n);
+        var cost = 0;
+
+        if (pick != null)
+        {
+            unionFind.Unite(pick[0], pick[1]);
+            cost += pick[2];
+        }
+
+        cost += edges.Where(edge => edge != skip && unionFind.Unite(edge[0], edge[1])).Sum(edge => edge[2]);
+
+        return unionFind.United() ? cost : int.MaxValue;
+    }
+}
