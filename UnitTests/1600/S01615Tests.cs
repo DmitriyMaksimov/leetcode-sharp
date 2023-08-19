f10377a3a74986e9f96e9fd0a12df90839bc65bf
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-19 11:44:55 +1000

    1615. Maximal Network Rank

---
+++ UnitTests/1600/S01615Tests.cs
@@ -0,0 +1,30 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01615Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01615();
+        sut.MaximalNetworkRank(4, new[] {new[] {0, 1}, new[] {0, 3}, new[] {1, 2}, new[] {1, 3}}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01615();
+        sut.MaximalNetworkRank(5,
+            new[] {new[] {0, 1}, new[] {0, 3}, new[] {1, 2}, new[] {1, 3}, new[] {2, 3}, new[] {2, 4}}).Should().Be(5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01615();
+        sut.MaximalNetworkRank(8,
+            new[] {new[] {0, 1}, new[] {1, 2}, new[] {2, 3}, new[] {2, 4}, new[] {5, 6}, new[] {5, 7}}).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/1600/S01615.cs
@@ -0,0 +1,37 @@
+﻿namespace leetcode_sharp;
+
+// 1615. Maximal Network Rank
+// https://leetcode.com/problems/maximal-network-rank/
+public class S01615
+{
+    public int MaximalNetworkRank(int n, int[][] roads)
+    {
+        var degree = new int[n];
+        var roadSet = new HashSet<string>();
+
+        foreach (var road in roads)
+        {
+            degree[road[0]]++;
+            degree[road[1]]++;
+            roadSet.Add($"{road[0]}_{road[1]}");
+            roadSet.Add($"{road[1]}_{road[0]}");
+        }
+
+        var maxRank = 0;
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = i + 1; j < n; j++)
+            {
+                var rank = degree[i] + degree[j];
+                if (roadSet.Contains($"{i}_{j}"))
+                {
+                    rank--;
+                }
+
+                maxRank = Math.Max(maxRank, rank);
+            }
+        }
+
+        return maxRank;
+    }
+}
