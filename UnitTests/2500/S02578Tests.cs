580ab734c078b37d0d834a1fb287e032311bbb5c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-20 23:10:38 +1000

    2578. Split With Minimum Sum

---
+++ UnitTests/2500/S02578Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02578Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02578();
+        sut.SplitNum(4325).Should().Be(59);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02578();
+        sut.SplitNum(687).Should().Be(75);
+    }
+}
---
+++ leetcode-sharp/2500/S02578.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 2578. Split With Minimum Sum
+// https://leetcode.com/problems/split-with-minimum-sum/
+public class S02578
+{
+    public int SplitNum(int num)
+    {
+        var chars = num.ToString().OrderBy(x => x).ToArray();
+
+        var n1 = 0;
+        var n2 = 0;
+
+        for (var i = 0; i < chars.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                n1 = n1 * 10 + chars[i] - '0';
+            }
+            else
+            {
+                n2 = n2 * 10 + chars[i] - '0';
+            }
+        }
+        
+        return n1 + n2;
+    }
+}
