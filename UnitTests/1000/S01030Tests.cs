d0e4cea552034bebb14e8ee300b6981378fb2e76
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-20 23:22:48 +1000

    1030. Matrix Cells in Distance Order

---
+++ UnitTests/1000/S01030Tests.cs
@@ -0,0 +1,30 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01030Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01030();
+        sut.AllCellsDistOrder(1, 2, 0, 0).Should().BeEquivalentTo(new[] {new[] {0, 0}, new[] {0, 1}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01030();
+        sut.AllCellsDistOrder(2, 2, 0, 1).Should()
+            .BeEquivalentTo(new[] {new[] {0, 1}, new[] {0, 0}, new[] {1, 1}, new[] {1, 0}});
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01030();
+        sut.AllCellsDistOrder(2, 3, 1, 2).Should().BeEquivalentTo(new[]
+            {new[] {1, 2}, new[] {0, 2}, new[] {1, 1}, new[] {0, 1}, new[] {1, 0}, new[] {0, 0}});
+    }
+}
---
+++ leetcode-sharp/1000/S01030.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 1030. Matrix Cells in Distance Order
+// https://leetcode.com/problems/matrix-cells-in-distance-order/
+public class S01030
+{
+    public int[][] AllCellsDistOrder(int rows, int cols, int rCenter, int cCenter)
+    {
+        var visited = new bool[rows, cols];
+        var result = new int[rows * cols][];
+        var i = 0;
+        var queue = new Queue<int[]>();
+        queue.Enqueue(new[] {rCenter, cCenter});
+        while (queue.Any())
+        {
+            var cell = queue.Dequeue();
+            var r = cell[0];
+            var c = cell[1];
+
+            if (r < 0 || r >= rows || c < 0 || c >= cols)
+            {
+                continue;
+            }
+
+            if (visited[r, c])
+            {
+                continue;
+            }
+
+            result[i++] = cell;
+            visited[r, c] = true;
+
+            queue.Enqueue(new[] {r, c - 1});
+            queue.Enqueue(new[] {r, c + 1});
+            queue.Enqueue(new[] {r - 1, c});
+            queue.Enqueue(new[] {r + 1, c});
+        }
+
+        return result;
+    }
+}
