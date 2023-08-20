1e135f4758f7a421fbd4cd815068a5cfee28f3ba
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-20 23:42:02 +1000

    2085. Count Common Words With One Occurrence

---
+++ UnitTests/2000/S02085Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02085Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02085();
+        sut.CountWords(new[] {"leetcode", "is", "amazing", "as", "is"}, new[] {"amazing", "leetcode", "is"}).Should()
+            .Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02085();
+        sut.CountWords(new[] {"b", "bb", "bbb"}, new[] {"a", "aa", "aaa"}).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02085();
+        sut.CountWords(new[] {"a", "ab"}, new[] {"a", "a", "a", "ab"}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/2000/S02085.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 2085. Count Common Words With One Occurrence
+// https://leetcode.com/problems/count-common-words-with-one-occurrence/
+public class S02085
+{
+    public int CountWords(string[] words1, string[] words2)
+    {
+        var dictionary = new Dictionary<string, int>();
+        foreach (var word in words1)
+        {
+            dictionary[word] = dictionary.GetValueOrDefault(word) + 1;
+        }
+
+        foreach (var word in words2)
+        {
+            if (dictionary.TryGetValue(word, out var count) && count < 2)
+            {
+                dictionary[word]--;
+            }
+        }
+        
+        return dictionary.Count(p => p.Value == 0);
+    }
+}
