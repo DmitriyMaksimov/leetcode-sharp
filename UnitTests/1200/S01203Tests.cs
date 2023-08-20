be0c4e960e4817d8f63d729f02e64694e60aea97
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-20 13:41:47 +1000

    1203. Sort Items by Groups Respecting Dependencies

---
+++ UnitTests/1200/S01203Tests.cs
@@ -0,0 +1,31 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01203Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01203();
+        sut.SortItems(8, 2, new[] {-1, -1, 1, 0, 0, 1, 0, -1},
+            new IList<int>[]
+            {
+                Array.Empty<int>(), new[] {6}, new[] {5}, new[] {6}, new[] {3, 6},
+                Array.Empty<int>(), Array.Empty<int>(), Array.Empty<int>()
+            }).Should().Equal(7, 0, 5, 2, 6, 3, 4, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01203();
+        sut.SortItems(8, 2, new[] {-1, -1, 1, 0, 0, 1, 0, -1},
+            new IList<int>[]
+            {
+                Array.Empty<int>(), new[] {6}, new[] {5}, new[] {6}, new[] {3}, Array.Empty<int>(),
+                new[] {4}, Array.Empty<int>()
+            }).Should().BeEmpty();
+    }
+}
---
+++ leetcode-sharp/1200/S01203.cs
@@ -0,0 +1,99 @@
+﻿namespace leetcode_sharp;
+
+// 1203. Sort Items by Groups Respecting Dependencies
+// https://leetcode.com/problems/sort-items-by-groups-respecting-dependencies/
+public class S01203
+{
+    public int[] SortItems(int n, int m, int[] group, IList<IList<int>> beforeItems)
+    {
+        var groupId = m;
+        for (var i = 0; i < n; i++)
+        {
+            if (group[i] == -1)
+            {
+                group[i] = groupId;
+                groupId++;
+            }
+        }
+
+        var itemGraph = new Dictionary<int, List<int>>();
+        var itemIndegree = new int[n];
+        for (var i = 0; i < n; ++i)
+        {
+            itemGraph.Add(i, new List<int>());
+        }
+
+        var groupGraph = new Dictionary<int, List<int>>();
+        var groupIndegree = new int[groupId];
+        for (var i = 0; i < groupId; ++i)
+        {
+            groupGraph.Add(i, new List<int>());
+        }
+
+        for (var curr = 0; curr < n; curr++)
+        {
+            foreach (var prev in beforeItems[curr])
+            {
+                itemGraph[prev].Add(curr);
+                itemIndegree[curr]++;
+
+                if (group[curr] != group[prev])
+                {
+                    groupGraph[group[prev]].Add(group[curr]);
+                    groupIndegree[group[curr]]++;
+                }
+            }
+        }
+
+        var itemOrder = topologicalSort(itemGraph, itemIndegree);
+        var groupOrder = topologicalSort(groupGraph, groupIndegree);
+
+        if (!itemOrder.Any() || !groupOrder.Any())
+        {
+            return Array.Empty<int>();
+        }
+
+        var orderedGroups = new Dictionary<int, List<int>>();
+        
+        foreach (var item in itemOrder)
+        {
+            orderedGroups[group[item]] = orderedGroups.GetValueOrDefault(group[item], new List<int>());
+            orderedGroups[group[item]].Add(item);
+        }
+
+        var answerList = new List<int>();
+        foreach (var groupIndex in groupOrder)
+        {
+            answerList.AddRange(orderedGroups.GetValueOrDefault(groupIndex, new List<int>()));
+        }
+
+        return answerList.ToArray();
+    }
+
+    private List<int> topologicalSort(Dictionary<int, List<int>> graph, int[] indegree)
+    {
+        var visited = new List<int>();
+        var stack = new Stack<int>();
+        foreach (var key in graph.Keys.Where(key => indegree[key] == 0))
+        {
+            stack.Push(key);
+        }
+
+        while (stack.Any())
+        {
+            var curr = stack.Pop();
+            visited.Add(curr);
+            
+            foreach (var prev in graph[curr])
+            {
+                indegree[prev]--;
+                if (indegree[prev] == 0)
+                {
+                    stack.Push(prev);
+                }
+            }
+        }
+
+        return visited.Count == graph.Count ? visited : new List<int>();
+    }
+}
