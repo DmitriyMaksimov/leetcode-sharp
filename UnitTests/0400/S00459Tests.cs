8ac9ed8378ec27eb898af0230d471f76a4cfdd47
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-21 20:19:58 +1000

    459. Repeated Substring Pattern

---
+++ UnitTests/0400/S00459Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00459Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00459();
+        sut.RepeatedSubstringPattern("abab").Should().BeTrue();
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00459();
+        sut.RepeatedSubstringPattern("aba").Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00459();
+        sut.RepeatedSubstringPattern("abcabcabcabc").Should().BeTrue();
+    }
+
+}
---
+++ leetcode-sharp/0400/S00459.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 459. Repeated Substring Pattern
+// https://leetcode.com/problems/repeated-substring-pattern/
+public class S00459
+{
+    public bool RepeatedSubstringPattern(string s)
+    {
+        var n = s.Length;
+        for (var i = 1; i <= n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                var found = true;
+                var substring = s.AsSpan(0, i);
+                for (var j = 1; j < n / i; j++)
+                {
+                    if (!substring.SequenceEqual(s.AsSpan(j * i, i)))
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+
+                if (found) return true;
+            }
+        }
+
+        return false;
+    }
+}
