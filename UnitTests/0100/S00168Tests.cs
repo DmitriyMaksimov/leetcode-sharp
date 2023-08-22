4ae4a8792e3abd188005add65f6283ec1e9ccf67
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-22 18:07:06 +1000

    168. Excel Sheet Column Title

---
+++ UnitTests/0100/S00168Tests.cs
@@ -0,0 +1,36 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00168Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00168();
+        sut.ConvertToTitle(1).Should().Be("A");
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00168();
+        sut.ConvertToTitle(28).Should().Be("AB");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00168();
+        sut.ConvertToTitle(701).Should().Be("ZY");
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S00168();
+        sut.ConvertToTitle(26).Should().Be("Z");
+    }
+
+}
---
+++ leetcode-sharp/0100/S00168.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 168. Excel Sheet Column Title
+// https://leetcode.com/problems/excel-sheet-column-title/
+public class S00168
+{
+    public string ConvertToTitle(int columnNumber)
+    {
+        var result = "";
+
+        while (columnNumber > 0)
+        {
+            result = (char)('A' + (columnNumber - 1) % 26) + result;
+            columnNumber = (columnNumber - 1) / 26;
+        }
+
+        return result;
+    }
+}
