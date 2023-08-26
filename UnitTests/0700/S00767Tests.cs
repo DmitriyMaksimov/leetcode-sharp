f2917c5c341016a660e2b40673b7275be797679a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-26 11:26:53 +1000

    767. Reorganize String

---
+++ UnitTests/0700/S00767Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00767Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00767();
+        sut.ReorganizeString("aab").Should().Be("aba");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00767();
+        sut.ReorganizeString("aaab").Should().Be("");
+    }
+}
---
+++ leetcode-sharp/0700/S00767.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode_sharp;
+
+// 767. Reorganize String
+// https://leetcode.com/problems/reorganize-string/
+public class S00767
+{
+    public string ReorganizeString(string s)
+    {
+        var freqMap = new Dictionary<char, int>();
+        foreach (var c in s)
+        {
+            freqMap.TryAdd(c, 0);
+            freqMap[c]++;
+        }
+
+        var sortedChars = new List<char>(freqMap.Keys);
+        sortedChars.Sort((a, b) => freqMap[b].CompareTo(freqMap[a]));
+
+        if (freqMap[sortedChars[0]] > (s.Length + 1) / 2)
+        {
+            return "";
+        }
+
+        var res = new char[s.Length];
+        var i = 0;
+        foreach (var c in sortedChars)
+        {
+            for (var j = 0; j < freqMap[c]; j++)
+            {
+                if (i >= s.Length)
+                {
+                    i = 1;
+                }
+
+                res[i] = c;
+                i += 2;
+            }
+        }
+
+        return new string(res);
+    }
+}
