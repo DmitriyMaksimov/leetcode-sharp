3efa8dc8be1e1388137762455587e01e5379581c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-26 11:27:11 +1000

    646. Maximum Length of Pair Chain

---
+++ UnitTests/0600/S00646Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00646Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00646();
+        sut.FindLongestChain(new[] {new[] {1, 2}, new[] {2, 3}, new[] {3, 4}}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00646();
+        sut.FindLongestChain(new[] {new[] {1, 2}, new[] {7, 8}, new[] {4, 5}}).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/0600/S00646.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 646. Maximum Length of Pair Chain
+// https://leetcode.com/problems/maximum-length-of-pair-chain/
+public class S00646
+{
+    public int FindLongestChain(int[][] pairs)
+    {
+        Array.Sort(pairs, (a, b) => a[1] - b[1]);
+
+        var cur = int.MinValue;
+        var ans = 0;
+
+        foreach (var pair in pairs)
+        {
+            if (cur < pair[0])
+            {
+                cur = pair[1];
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
