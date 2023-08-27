17c2e77341683217dd690969048ceba55bcaa136
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-27 11:41:28 +1000

    403. Frog Jump

---
+++ UnitTests/0400/S00403Tests.cs
@@ -0,0 +1,23 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00403Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00403();
+        var stones = new[] {0, 1, 3, 5, 6, 8, 12, 17};
+        sut.CanCross(stones).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00403();
+        var stones = new[] {0, 1, 2, 3, 4, 8, 9, 11};
+        sut.CanCross(stones).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0400/S00403.cs
@@ -0,0 +1,36 @@
+﻿namespace leetcode_sharp;
+
+// 403. Frog Jump
+// https://leetcode.com/problems/frog-jump/
+public class S00403
+{
+    public bool CanCross(int[] stones)
+    {
+        var dp = new HashSet<int>[stones.Length];
+
+        for (var i = 0; i < dp.Length; i++)
+        {
+            dp[i] = new HashSet<int>();
+        }
+
+        dp[0].Add(0);
+
+        for (var i = 0; i < stones.Length; i++)
+        {
+            var lst = dp[i];
+
+            foreach (var jump in lst)
+            {
+                for (var j = i + 1; j < stones.Length && stones[j] - stones[i] - jump <= 1; ++j)
+                {
+                    if (Math.Abs(stones[j] - stones[i] - jump) <= 1)
+                    {
+                        dp[j].Add(stones[j] - stones[i]);
+                    }
+                }
+            }
+        }
+
+        return dp[stones.Length - 1].Count > 0;
+    }
+}
