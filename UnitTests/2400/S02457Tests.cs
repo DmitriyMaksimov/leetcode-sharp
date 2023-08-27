e951675585270d511e4c4c42dfd26eb17b9c62ec
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-27 18:31:55 +1000

    2457. Minimum Addition to Make Integer Beautiful

---
+++ UnitTests/2400/S02457Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02457Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02457();
+        sut.MakeIntegerBeautiful(16, 6).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02457();
+        sut.MakeIntegerBeautiful(467, 6).Should().Be(33);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02457();
+        sut.MakeIntegerBeautiful(1, 1).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2400/S02457.cs
@@ -0,0 +1,44 @@
+﻿namespace leetcode_sharp;
+
+// 2457. Minimum Addition to Make Integer Beautiful
+// https://leetcode.com/problems/minimum-addition-to-make-integer-beautiful/
+public class S02457
+{
+    public long MakeIntegerBeautiful(long n, int target)
+    {
+        var digits = new List<long>();
+        var sum = 0L;
+
+        for (var l = n; l > 0; l /= 10)
+        {
+            var digit = l % 10;
+            digits.Add(digit);
+            sum += digit;
+        }
+
+        for (var i = 0; i < digits.Count; i++)
+        {
+            if (sum <= target)
+            {
+                break;
+            }
+
+            sum -= digits[i];
+
+            digits[i] = 0;
+            
+            if (i + 1 < digits.Count)
+            {
+                digits[i + 1]++;
+            }
+            else
+            {
+                digits.Add(1);
+            }
+
+            sum++;
+        }
+
+        return digits.AsEnumerable().Reverse().Aggregate<long, long>(0, (current, t) => current * 10 + t) - n;
+    }
+}
