2221c724e28cbc831e539484b7f7723858a57daa
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-28 18:31:28 +1000

    342. Power of Four

---
+++ UnitTests/0300/S00342Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00342Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00342();
+        sut.IsPowerOfFour(16).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00342();
+        sut.IsPowerOfFour(5).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00342();
+        sut.IsPowerOfFour(1).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/0300/S00342.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 342. Power of Four
+// https://leetcode.com/problems/power-of-four/
+public class S00342
+{
+    public bool IsPowerOfFour(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+
+        while (n % 4 == 0)
+        {
+            n /= 4;
+        }
+
+        return n == 1;
+    }
+}
