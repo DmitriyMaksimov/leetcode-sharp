d8dcbd0a56afc819489d9855d38d61a08aae0b48
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-28 19:24:21 +1000

    405. Convert a Number to Hexadecimal

---
+++ UnitTests/0400/S00405Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00405Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00405();
+        sut.ToHex(26).Should().Be("1a");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00405();
+        sut.ToHex(-1).Should().Be("ffffffff");
+    }
+}
---
+++ leetcode-sharp/0400/S00405.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 405. Convert a Number to Hexadecimal
+// https://leetcode.com/problems/convert-a-number-to-hexadecimal/
+public class S00405
+{
+    private char[] _hexDigits = "0123456789abcdef".ToCharArray();
+
+    public string ToHex(int num)
+    {
+        if (num == 0)
+        {
+            return "0";
+        }
+
+        var sb = new StringBuilder();
+        const int mask = 0xF;
+        for (var i = 0; i < 8; i++)
+        {
+            var digit = num & mask;
+            sb.Insert(0, _hexDigits[digit]);
+            num >>= 4;
+        }
+
+        return sb.ToString().TrimStart('0');
+    }
+}
