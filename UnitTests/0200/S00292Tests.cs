e9ff0ec459ee207a52d19ee1e34e05435a6ac5b2
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-28 18:38:33 +1000

    292. Nim Game

---
+++ UnitTests/0200/S00292Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00292Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00292();
+        sut.CanWinNim(4).Should().BeFalse();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00292();
+        sut.CanWinNim(1).Should().BeTrue();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00292();
+        sut.CanWinNim(2).Should().BeTrue();
+    }
+
+}
---
+++ leetcode-sharp/0200/S00292.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 292. Nim Game
+// https://leetcode.com/problems/nim-game/
+public class S00292
+{
+    public bool CanWinNim(int n)
+    {
+        return n % 4 != 0;
+    }
+}
