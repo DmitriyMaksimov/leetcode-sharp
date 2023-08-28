3ccd3df3ff1def0f52f5102c7abf3403443774e6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-28 18:27:24 +1000

    225. Implement Stack using Queues

---
+++ UnitTests/0200/S00225Tests.cs
@@ -0,0 +1,18 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00225Tests
+{
+    [Test]
+    public void T1()
+    {
+        var myStack = new S00225.MyStack();
+        myStack.Push(1);
+        myStack.Push(2);
+        myStack.Top().Should().Be(2);
+        myStack.Pop().Should().Be(2);
+        myStack.Empty().Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0200/S00225.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+public static class S00225
+{
+    public class MyStack
+    {
+        private readonly Queue<int> _queue = new();
+
+        public void Push(int x)
+        {
+            _queue.Enqueue(x);
+            for (var i = 0; i < _queue.Count - 1; i++)
+            {
+                _queue.Enqueue(_queue.Dequeue());
+            }
+        }
+
+        public int Pop()
+        {
+            return _queue.Dequeue();
+        }
+
+        public int Top()
+        {
+            return _queue.Peek();
+        }
+
+        public bool Empty()
+        {
+            return !_queue.Any();
+        }
+    }
+}
