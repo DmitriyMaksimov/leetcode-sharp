d8dea4e4ad6996d62b972a0571b2290acb780a0c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-28 18:35:18 +1000

    326. Power of Three

---
+++ UnitTests/0300/S00326Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00326Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00326();
+        sut.IsPowerOfThree(27).Should().BeTrue();
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00326();
+        sut.IsPowerOfThree(0).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00326();
+        sut.IsPowerOfThree(-1).Should().BeFalse();
+    }
+
+}
---
+++ leetcode-sharp/0300/S00326.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode_sharp;
+
+// 326. Power of Three
+// https://leetcode.com/problems/power-of-three/
+public class S00326
+{
+    public bool IsPowerOfThree(int n)
+    {
+        if (n < 1) return false;
+
+        while (n % 3 == 0)
+        {
+            n /= 3;
+        }
+
+        return n == 1;
+    }
+}
