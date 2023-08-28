81935fe98007d8feedd158ea0a333cd3ccbc3a87
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-28 19:16:21 +1000

    389. Find the Difference

---
+++ UnitTests/0300/S00389Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00389Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00389();
+        sut.FindTheDifference("abcd", "abcde").Should().Be('e');
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00389();
+        sut.FindTheDifference("", "y").Should().Be('y');
+    }
+}
---
+++ leetcode-sharp/0300/S00389.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 389. Find the Difference
+// https://leetcode.com/problems/find-the-difference/
+public class S00389
+{
+    public char FindTheDifference(string s, string t)
+    {
+        var s1 = s.ToCharArray();
+        var t1 = t.ToCharArray();
+        
+        Array.Sort(s1);
+        Array.Sort(t1);
+
+        for (var i = 0; i < s1.Length; i++)
+        {
+            if (s1[i] != t1[i])
+            {
+                return t1[i];
+            }
+        }
+
+        return t1[^1];
+    }
+}
