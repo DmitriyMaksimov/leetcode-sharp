20caf1d528b0e1a8a8ae1043943f2c81888bb21e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-28 18:58:43 +1000

    110. Balanced Binary Tree

---
+++ UnitTests/0100/S00110Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00110Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00110();
+        sut.IsBalanced(TreeNode.Parse("[3,9,20,null,null,15,7]")).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00110();
+        sut.IsBalanced(TreeNode.Parse("[1,2,2,3,3,null,null,4,4]")).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00110();
+        sut.IsBalanced(null).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/0100/S00110.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 110. Balanced Binary Tree
+// https://leetcode.com/problems/balanced-binary-tree/
+public class S00110
+{
+    public bool IsBalanced(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return true;
+        }
+
+        return Math.Abs(GetHeight(root.left) - GetHeight(root.right)) <= 1 && IsBalanced(root.left) && IsBalanced(root.right);
+    }
+
+    private static int GetHeight(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        return Math.Max(GetHeight(root.left), GetHeight(root.right)) + 1;
+    }
+}
