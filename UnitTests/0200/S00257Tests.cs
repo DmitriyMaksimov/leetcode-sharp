ad7e8a8259d90e547214ada27f10315b2fccb7aa
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-28 18:52:40 +1000

    257. Binary Tree Paths

---
+++ UnitTests/0200/S00257Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00257Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00257();
+        sut.BinaryTreePaths(TreeNode.Parse("[1,2,3,null,5]")).Should().Equal("1->2->5", "1->3");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00257();
+        sut.BinaryTreePaths(TreeNode.Parse("[1]")).Should().Equal("1");
+    }
+}
---
+++ leetcode-sharp/0200/S00257.cs
@@ -0,0 +1,57 @@
+﻿namespace leetcode_sharp;
+
+// 257. Binary Tree Paths
+// https://leetcode.com/problems/binary-tree-paths/
+public class S00257
+{
+    public IList<string> BinaryTreePaths(TreeNode? root)
+    {
+        var result = new List<string>();
+        if (root == null)
+        {
+            return result;
+        }
+
+        var path = new List<int> {root.val};
+        if (root.left == null && root.right == null)
+        {
+            result.Add(string.Join("->", path));
+            return result;
+        }
+
+        if (root.left != null)
+        {
+            dfs(root.left, path, result);
+        }
+
+        if (root.right != null)
+        {
+            dfs(root.right, path, result);
+        }
+
+        return result;
+    }
+
+    private static void dfs(TreeNode rootRight, IList<int> path, ICollection<string> result)
+    {
+        path.Add(rootRight.val);
+        if (rootRight.left == null && rootRight.right == null)
+        {
+            result.Add(string.Join("->", path));
+            path.RemoveAt(path.Count - 1);
+            return;
+        }
+
+        if (rootRight.left != null)
+        {
+            dfs(rootRight.left, path, result);
+        }
+
+        if (rootRight.right != null)
+        {
+            dfs(rootRight.right, path, result);
+        }
+
+        path.RemoveAt(path.Count - 1);
+    }
+}
