d84d6dc21592ec350d314928f834c323e1343ec6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-28 18:45:10 +1000

    268. Missing Number

---
+++ UnitTests/0200/S00268Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00268Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00268();
+        sut.MissingNumber(new[] {3, 0, 1}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00268();
+        sut.MissingNumber(new[] {0, 1}).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00268();
+        sut.MissingNumber(new[] {9, 6, 4, 2, 3, 5, 7, 0, 1}).Should().Be(8);
+    }
+}
---
+++ leetcode-sharp/0200/S00268.cs
@@ -0,0 +1,14 @@
+﻿namespace leetcode_sharp;
+
+// 268. Missing Number
+// https://leetcode.com/problems/missing-number/
+public class S00268
+{
+    public int MissingNumber(int[] nums)
+    {
+        var n = nums.Length;
+        var sum = (n + 1) * n / 2;
+
+        return nums.Aggregate(sum, (current, num) => current - num);
+    }
+}
