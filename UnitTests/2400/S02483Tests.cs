8bd4cb1629866ea41a74937861a411f593536eae
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-29 10:13:31 +1000

    2483. Minimum Penalty for a Shop

---
+++ UnitTests/2400/S02483Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02483Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02483();
+        sut.BestClosingTime("YYNY").Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02483();
+        sut.BestClosingTime("NNNNN").Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02483();
+        sut.BestClosingTime("YYYY").Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/2400/S02483.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 2483. Minimum Penalty for a Shop
+// https://leetcode.com/problems/minimum-penalty-for-a-shop/
+public class S02483
+{
+    public int BestClosingTime(string customers)
+    {
+        (int hour, int penalty) best = (customers.Length, customers.Count(c => c == 'N'));
+        var penalty = best.penalty;
+        
+        for (var hour = customers.Length - 1; hour >= 0; hour--)
+        {
+            penalty += customers[hour] == 'Y' ? 1 : -1;
+            if (penalty <= best.penalty)
+            {
+                best = (hour, penalty);
+            }
+        }
+
+        return best.hour;
+    }
+}
