133d4caf429900369d4fa1b440623cdc990352f3
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-29 18:27:57 +1000

    434. Number of Segments in a String

---
+++ UnitTests/0400/S00434Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00434Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00434();
+        sut.CountSegments("Hello, my name is John").Should().Be(5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00434();
+        sut.CountSegments("Hello").Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/0400/S00434.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 434. Number of Segments in a String
+// https://leetcode.com/problems/number-of-segments-in-a-string/
+public class S00434
+{
+    public int CountSegments(string s)
+    {
+        return s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+    }
+}
