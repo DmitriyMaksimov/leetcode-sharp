b2915e9dd3059d8a16fa553de4595dce916f8b4e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-29 18:46:52 +1000

    482. License Key Formatting

---
+++ UnitTests/0400/S00482Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00482Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00482();
+        sut.LicenseKeyFormatting("5F3Z-2e-9-w", 4).Should().Be("5F3Z-2E9W");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00482();
+        sut.LicenseKeyFormatting("2-5g-3-J", 2).Should().Be("2-5G-3J");
+    }
+}
---
+++ leetcode-sharp/0400/S00482.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 482. License Key Formatting
+// https://leetcode.com/problems/license-key-formatting/
+public class S00482
+{
+    public string LicenseKeyFormatting(string s, int k)
+    {
+        var sb = new StringBuilder();
+        var count = 0;
+        for (var i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] == '-')
+            {
+                continue;
+            }
+
+            if (count == k)
+            {
+                sb.Append('-');
+                count = 0;
+            }
+
+            sb.Append(char.ToUpper(s[i]));
+            count++;
+        }
+
+        return new string(sb.ToString().Reverse().ToArray());
+    }
+}
