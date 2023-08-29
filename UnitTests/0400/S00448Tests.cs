76fbed08384741b2d2ced3a4f8e82b49a94cff7c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-29 18:38:30 +1000

    448. Find All Numbers Disappeared in an Array

---
+++ UnitTests/0400/S00448Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00448Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00448();
+        sut.FindDisappearedNumbers(new[] {4, 3, 2, 7, 8, 2, 3, 1}).Should().Equal(5, 6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00448();
+        sut.FindDisappearedNumbers(new[] {1, 1}).Should().Equal(2);
+    }
+}
---
+++ leetcode-sharp/0400/S00448.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 448. Find All Numbers Disappeared in an Array
+// https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+public class S00448
+{
+    public IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        foreach (var num in nums)
+        {
+            var idx = Math.Abs(num) - 1;
+
+            if (nums[idx] > 0)
+            {
+                nums[idx] = -nums[idx];
+            }
+        }
+
+        var list = new List<int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0)
+            {
+                list.Add(i + 1);
+            }
+        }
+
+        return list;
+    }
+}
