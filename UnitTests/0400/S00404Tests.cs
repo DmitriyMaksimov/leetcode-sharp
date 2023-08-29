ec829640a9ae92a9e90a2ca8a4afd6b45d788f16
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-29 10:12:39 +1000

    404. Sum of Left Leaves

---
+++ UnitTests/0400/S00404Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00404Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00404();
+        sut.SumOfLeftLeaves(null).Should().Be(0);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00404();
+        sut.SumOfLeftLeaves(TreeNode.Parse("[3,9,20,null,null,15,7]")).Should().Be(24);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00404();
+        sut.SumOfLeftLeaves(TreeNode.Parse("[1]")).Should().Be(0);
+    }
+
+}
---
+++ leetcode-sharp/0400/S00404.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 404. Sum of Left Leaves
+// https://leetcode.com/problems/sum-of-left-leaves/
+public class S00404
+{
+    public int SumOfLeftLeaves(TreeNode? root)
+    {
+        return sum(root);
+    }
+
+    private static int sum(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        var leftSum = 0;
+        if (root.left is {left: null} && root.left.right == null)
+        {
+            leftSum += root.left.val;
+        }
+
+        var leftSubtreeSum = sum(root.left);
+        var rightSubtreeSum = sum(root.right);
+        return leftSum + leftSubtreeSum + rightSubtreeSum;
+    }
+}
