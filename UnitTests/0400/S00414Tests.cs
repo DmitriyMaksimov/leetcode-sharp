28c68c91f74e5a5b29dd79eea725b3c64bfe9fb3
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-29 18:23:10 +1000

    414. Third Maximum Number

---
+++ UnitTests/0400/S00414Tests.cs
@@ -0,0 +1,35 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00414Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00414();
+        sut.ThirdMax(new[] {3, 2, 1}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00414();
+        sut.ThirdMax(new[] {1, 2}).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00414();
+        sut.ThirdMax(new[] {2, 2, 3, 1}).Should().Be(1);
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S00414();
+        sut.ThirdMax(new[] {1, 2, 2, 5, 3, 5}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/0400/S00414.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 414. Third Maximum Number
+// https://leetcode.com/problems/third-maximum-number/
+public class S00414
+{
+    public int ThirdMax(int[] nums)
+    {
+        return nums.Distinct().OrderByDescending(x => x).Cast<int?>().ElementAtOrDefault(2) ?? nums.Max();
+    }
+}
