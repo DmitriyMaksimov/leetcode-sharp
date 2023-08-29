4655e46318165f9fdc268a14c02bcb5605806dff
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-29 16:54:52 +1000

    409. Longest Palindrome

---
+++ UnitTests/0400/S00409Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00409Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00409();
+        sut.LongestPalindrome("abccccdd").Should().Be(7);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00409();
+        sut.LongestPalindrome("a").Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/0400/S00409.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 409. Longest Palindrome
+// https://leetcode.com/problems/longest-palindrome/
+public class S00409
+{
+    public int LongestPalindrome(string s)
+    {
+        var dict = new Dictionary<char, int>();
+
+        foreach (var c in s)
+        {
+            dict[c] = dict.GetValueOrDefault(c) + 1;
+        }
+
+        var result = 0;
+        var hasOdd = false;
+        foreach (var (_, value) in dict)
+        {
+            if (value % 2 == 0)
+            {
+                result += value;
+            }
+            else
+            {
+                result += value - 1;
+                hasOdd = true;
+            }
+        }
+
+        return hasOdd ? result + 1 : result;
+    }
+}
