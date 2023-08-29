5059c0cda761da6c94cdc6c48e552d7eb0be7f6e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-29 18:44:09 +1000

    476. Number Complement

---
+++ UnitTests/0400/S00476Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00476Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00476();
+        sut.FindComplement(5).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00476();
+        sut.FindComplement(1).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0400/S00476.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode_sharp;
+
+// 476. Number Complement
+// https://leetcode.com/problems/number-complement/
+public class S00476
+{
+    public int FindComplement(int num)
+    {
+        var mask = 1;
+        while (mask < num)
+        {
+            mask = (mask << 1) + 1;
+        }
+
+        return num ^ mask;
+    }
+}
