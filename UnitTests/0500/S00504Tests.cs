e40de04a08669d35daf354c7dc1ecc1432890a41
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-30 14:25:59 +1000

    504. Base 7

---
+++ UnitTests/0500/S00504Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00504Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00504();
+        sut.ConvertToBase7(100).Should().Be("202");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00504();
+        sut.ConvertToBase7(-7).Should().Be("-10");
+    }
+}
---
+++ leetcode-sharp/0500/S00504.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 504. Base 7
+// https://leetcode.com/problems/base-7/
+public class S00504
+{
+    public string ConvertToBase7(int num)
+    {
+        if (num == 0)
+        {
+            return "0";
+        }
+
+        var isNegative = num < 0;
+        num = Math.Abs(num);
+        var sb = new StringBuilder();
+        while (num > 0)
+        {
+            sb.Append(num % 7);
+            num /= 7;
+        }
+
+        if (isNegative)
+        {
+            sb.Append('-');
+        }
+
+        return new string(sb.ToString().Reverse().ToArray());
+    }
+}
