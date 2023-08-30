81bb989e765cb11cf9c387c6c5e18a25ee86ed67
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-30 14:13:30 +1000

    507. Perfect Number

---
+++ UnitTests/0500/S00507Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00507Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00507();
+        sut.CheckPerfectNumber(28).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00507();
+        sut.CheckPerfectNumber(7).Should().BeFalse();
+    }
+
+}
---
+++ leetcode-sharp/0500/S00507.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 507. Perfect Number
+// https://leetcode.com/problems/perfect-number/
+public class S00507
+{
+    public bool CheckPerfectNumber(int num)
+    {
+        if (num <= 1)
+        {
+            return false;
+        }
+
+        var sum = 1;
+        var sqrt = (int) Math.Sqrt(num);
+
+        for (var i = 2; i <= sqrt; i++)
+        {
+            if (num % i != 0) continue;
+            
+            sum += i + num / i;
+        }
+
+        return sum == num;
+    }
+}
