4fddf08e7722177418152fc42d910cb41c5758aa
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-30 11:35:23 +1000

    2366. Minimum Replacements to Sort the Array

---
+++ UnitTests/2300/S02366Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02366Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02366();
+        sut.MinimumReplacement(new[] {1, 2, 3, 4, 5}).Should().Be(0);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02366();
+        sut.MinimumReplacement(new[] {3, 9, 3}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/2300/S02366.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 2366. Minimum Replacements to Sort the Array
+// https://leetcode.com/problems/minimum-replacements-to-sort-the-array/
+public class S02366
+{
+    public long MinimumReplacement(int[] nums)
+    {
+        var n = nums.Length;
+        var prev = nums[n - 1];
+        var ans = 0L;
+        for (var i = n - 2; i >= 0; i--)
+        {
+            var noOfTime = nums[i] / prev;
+
+            if (nums[i] % prev != 0)
+            {
+                noOfTime++;
+                prev = nums[i] / noOfTime;
+            }
+
+            ans += noOfTime - 1;
+        }
+
+        return ans;
+    }
+}
