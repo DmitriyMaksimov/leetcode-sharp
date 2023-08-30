e4f90c1cc102af28ac36bc4c21876869a916d5eb
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-30 14:31:31 +1000

    501. Find Mode in Binary Search Tree

---
+++ UnitTests/0500/S00501Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00501Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00501();
+        sut.FindMode(TreeNode.Parse("[1,null,2,2]")).Should().BeEquivalentTo(new[] {2});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00501();
+        sut.FindMode(TreeNode.Parse("[0]")).Should().BeEquivalentTo(new[] {0});
+    }
+}
---
+++ leetcode-sharp/0500/S00501.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode_sharp;
+
+// 501. Find Mode in Binary Search Tree
+// https://leetcode.com/problems/find-mode-in-binary-search-tree/
+public class S00501
+{
+    public int[] FindMode(TreeNode? root)
+    {
+        var dict = new Dictionary<int, int>();
+        var queue = new Queue<TreeNode?>();
+        queue.Enqueue(root);
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            if (dict.ContainsKey(node.val))
+            {
+                dict[node.val]++;
+            }
+            else
+            {
+                dict.Add(node.val, 1);
+            }
+
+            if (node.left != null)
+            {
+                queue.Enqueue(node.left);
+            }
+
+            if (node.right != null)
+            {
+                queue.Enqueue(node.right);
+            }
+        }
+
+        var max = dict.Values.Max();
+        return dict.Where(x => x.Value == max).Select(x => x.Key).ToArray();
+    }
+}
