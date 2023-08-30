3b72de57532687e88408e50a4c4f0a758e86892a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-30 11:26:34 +1000

    2828. Check if a String Is an Acronym of Words

---
+++ UnitTests/2800/S02828Tests.cs
@@ -0,0 +1,35 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02828Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02828();
+        sut.IsAcronym(new List<string> {"alice", "bob", "charlie"}, "abc").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02828();
+        sut.IsAcronym(new List<string> {"an", "apple"}, "a").Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02828();
+        sut.IsAcronym(new List<string> {"never", "gonna", "give", "up", "on", "you"}, "ngguoy").Should().BeTrue();
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S02828();
+        sut.IsAcronym(new List<string> {"a", "b", "c"}, "abcd").Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/2800/S02828.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 2828. Check if a String Is an Acronym of Words
+// https://leetcode.com/problems/check-if-a-string-is-an-acronym-of-words/
+public class S02828
+{
+    public bool IsAcronym(IList<string> words, string s)
+    {
+        if (s.Length != words.Count)
+        {
+            return false;
+        }
+
+        var i = 0;
+        foreach (var word in words)
+        {
+            if (word.Length == 0)
+            {
+                continue;
+            }
+
+            if (word[0] != s[i])
+            {
+                return false;
+            }
+
+            i++;
+        }
+
+        return true;
+    }
+}
