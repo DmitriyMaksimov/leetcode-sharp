487ec08316791b3b24c66f46859cb3522a0a6a9a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-30 14:21:46 +1000

    506. Relative Ranks

---
+++ UnitTests/0500/S00506Tests.cs
@@ -0,0 +1,23 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00506Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00506();
+        sut.FindRelativeRanks(new[] {5, 4, 3, 2, 1}).Should()
+            .Equal("Gold Medal", "Silver Medal", "Bronze Medal", "4", "5");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00506();
+        sut.FindRelativeRanks(new[] {10, 3, 8, 9, 4}).Should()
+            .Equal("Gold Medal", "5", "Bronze Medal", "Silver Medal", "4");
+    }
+}
---
+++ leetcode-sharp/0500/S00506.cs
@@ -0,0 +1,39 @@
+﻿namespace leetcode_sharp;
+
+// 506. Relative Ranks
+// https://leetcode.com/problems/relative-ranks/
+public class S00506
+{
+    public string[] FindRelativeRanks(int[] score)
+    {
+        var res = new string[score.Length];
+        var pq = new PriorityQueue<int, int>();
+
+        for (var i = 0; i < score.Length; i++)
+        {
+            pq.Enqueue(i, -score[i]);
+        }
+
+        if (pq.Count > 0)
+        {
+            res[pq.Dequeue()] = "Gold Medal";
+        }
+
+        if (pq.Count > 0)
+        {
+            res[pq.Dequeue()] = "Silver Medal";
+        }
+
+        if (pq.Count > 0)
+        {
+            res[pq.Dequeue()] = "Bronze Medal";
+        }
+
+        for (var i = 4; pq.Count > 0; i++)
+        {
+            res[pq.Dequeue()] = i.ToString();
+        }
+
+        return res;
+    }
+}
