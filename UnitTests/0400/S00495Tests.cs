3065315e497c27955f2d136af5e1cb709f91a2d0
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-30 14:44:08 +1000

    495. Teemo Attacking

---
+++ UnitTests/0400/S00495Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00495Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00495();
+        sut.FindPoisonedDuration(new[] {1, 4}, 2).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00495();
+        sut.FindPoisonedDuration(new[] {1, 2}, 2).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/0400/S00495.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 495. Teemo Attacking
+// https://leetcode.com/problems/teemo-attacking/
+public class S00495
+{
+    public int FindPoisonedDuration(int[] timeSeries, int duration)
+    {
+        var result = 0;
+        for (var i = 0; i < timeSeries.Length; i++)
+        {
+            if (i == timeSeries.Length - 1)
+            {
+                result += duration;
+            }
+            else
+            {
+                result += Math.Min(timeSeries[i + 1] - timeSeries[i], duration);
+            }
+        }
+
+        return result;
+    }
+}
