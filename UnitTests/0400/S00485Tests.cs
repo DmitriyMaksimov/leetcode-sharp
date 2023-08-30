f291de059e510f44f1643e210221e46610f58c3f
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-30 14:53:20 +1000

    485. Max Consecutive Ones

---
+++ UnitTests/0400/S00485Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00485Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00485();
+        sut.FindMaxConsecutiveOnes(new[] {1, 1, 0, 1, 1, 1}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00485();
+        sut.FindMaxConsecutiveOnes(new[] {1, 0, 1, 1, 0, 1}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/0400/S00485.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 485. Max Consecutive Ones
+// https://leetcode.com/problems/max-consecutive-ones/
+public class S00485
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        var max = 0;
+        var current = 0;
+        foreach (var num in nums)
+        {
+            if (num == 1)
+            {
+                current++;
+            }
+            else
+            {
+                max = Math.Max(max, current);
+                current = 0;
+            }
+        }
+
+        return Math.Max(max, current);
+    }
+}
