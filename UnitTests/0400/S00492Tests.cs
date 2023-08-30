547461df2f16b812cf2e54a3a020a437e49389e5
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-30 14:49:10 +1000

    492. Construct the Rectangle

---
+++ UnitTests/0400/S00492Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00492Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00492();
+        sut.ConstructRectangle(4).Should().Equal(2, 2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00492();
+        sut.ConstructRectangle(37).Should().Equal(37, 1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00492();
+        sut.ConstructRectangle(122122).Should().Equal(427, 286);
+    }
+}
---
+++ leetcode-sharp/0400/S00492.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode_sharp;
+
+// 492. Construct the Rectangle
+// https://leetcode.com/problems/construct-the-rectangle/
+public class S00492
+{
+    public int[] ConstructRectangle(int area)
+    {
+        var w = (int) Math.Sqrt(area);
+        while (area % w != 0)
+        {
+            w--;
+        }
+
+        return new[] {area / w, w};
+    }
+}
