054c0eb34ed4a45e71843844140d0fcae36b0f33
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-31 15:10:22 +1000

    521. Longest Uncommon Subsequence I

---
+++ UnitTests/0500/S00521Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00521Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00521();
+        sut.FindLUSlength("aba", "cdc").Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00521();
+        sut.FindLUSlength("aaa", "bbb").Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00521();
+        sut.FindLUSlength("aaa", "aaa").Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/0500/S00521.cs
@@ -0,0 +1,16 @@
+﻿namespace leetcode_sharp;
+
+// 521. Longest Uncommon Subsequence I
+// https://leetcode.com/problems/longest-uncommon-subsequence-i/
+public class S00521
+{
+    public int FindLUSlength(string a, string b)
+    {
+        if (a == b)
+        {
+            return -1;
+        }
+
+        return Math.Max(a.Length, b.Length);
+    }
+}
