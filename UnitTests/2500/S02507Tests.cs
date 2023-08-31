b3ae2620d9c4549169bd1daf9551238196b4e053
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-31 15:01:09 +1000

    2507. Smallest Value After Replacing With Sum of Prime Factors

---
+++ UnitTests/2500/S02507Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02507Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02507();
+        sut.SmallestValue(15).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02507();
+        sut.SmallestValue(3).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/2500/S02507.cs
@@ -0,0 +1,34 @@
+﻿namespace leetcode_sharp;
+
+// 2507. Smallest Value Represented As Sum Of Two Distinct Positive Integers
+// https://leetcode.com/problems/smallest-value-after-replacing-with-sum-of-prime-factors/
+public class S02507
+{
+    public int SmallestValue(int n)
+    {
+        if (n == 4)
+        {
+            return 4;
+        }
+
+        var list = new List<int>();
+
+        for (var i = 2; i <= n; i++)
+        {
+            while (n % i == 0)
+            {
+                n /= i;
+                list.Add(i);
+            }
+        }
+
+        if (list.Count == 1)
+        {
+            return list[0];
+        }
+
+        var result = list.Sum();
+
+        return SmallestValue(result);
+    }
+}
