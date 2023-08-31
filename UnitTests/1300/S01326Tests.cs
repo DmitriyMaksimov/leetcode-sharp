807b86b677af01c7d64eb2df59658cc57af0083c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-08-31 15:01:28 +1000

    1326. Minimum Number of Taps to Open to Water a Garden

---
+++ UnitTests/1300/S01326Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01326Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01326();
+        sut.MinTaps(5, new[] {3, 4, 1, 1, 0, 0}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01326();
+        sut.MinTaps(3, new[] {0, 0, 0, 0}).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/1300/S01326.cs
@@ -0,0 +1,44 @@
+﻿namespace leetcode_sharp;
+
+// 1326. Minimum Number of Taps to Open to Water a Garden
+// https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/ 
+public class S01326
+{
+    public int MinTaps(int n, int[] ranges)
+    {
+        var arr = new int[n + 1];
+
+        for (var i = 0; i < ranges.Length; i++)
+        {
+            if (ranges[i] == 0)
+            {
+                continue;
+            }
+
+            var left = Math.Max(0, i - ranges[i]);
+            arr[left] = Math.Max(arr[left], i + ranges[i]);
+        }
+
+        var end = 0;
+        var farCanReach = 0;
+        var cnt = 0;
+        
+        for (var i = 0; i <= n; i++)
+        {
+            if (i > end)
+            {
+                if (farCanReach <= end)
+                {
+                    return -1;
+                }
+
+                end = farCanReach;
+                cnt++;
+            }
+
+            farCanReach = Math.Max(farCanReach, arr[i]);
+        }
+
+        return cnt + (end < n ? 1 : 0);
+    }
+}
