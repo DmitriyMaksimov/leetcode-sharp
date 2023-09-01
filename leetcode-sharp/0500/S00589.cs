735731a034afca29dd1d39b55b3308bc5b228795
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 09:13:02 +1000

    589. N-ary Tree Preorder Traversal

---
+++ leetcode-sharp/0500/S00589.cs
@@ -0,0 +1,51 @@
+﻿namespace leetcode_sharp;
+
+// 589. N-ary Tree Preorder Traversal
+// https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+public class S00589
+{
+    public class Node
+    {
+        public int val;
+        public IList<Node?>? children;
+
+        public Node()
+        {
+        }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node?>? _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    private readonly List<int> _result = new();
+
+    public IList<int> Preorder(Node? root)
+    {
+        preorderTraversal(root);
+        
+        return _result;
+    }
+
+    private void preorderTraversal(Node? node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+        
+        _result.Add(node.val);
+
+        foreach (var childNode in node.children ?? new List<Node?>())
+        {
+            preorderTraversal(childNode);
+        }
+    }
+}
