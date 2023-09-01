1235268f3e38eb8387ce8d07ed00fb2d8602f020
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 09:52:29 +1000

    Use Equal instead of BeEquivalentTo

--- UnitTests/0500/S00589Tests.cs
+++ UnitTests/0500/S00589Tests.cs
@@ -16,7 +16,7 @@ public class S00589Tests
     public void T2()
     {
         var sut = new S00589();
-        sut.Preorder(new S00589.Node()).Should().BeEquivalentTo(new List<int> {0});
+        sut.Preorder(new S00589.Node()).Should().Equal(0);
     }
 
     [Test]
@@ -24,7 +24,7 @@ public class S00589Tests
     {
         var sut = new S00589();
         sut.Preorder(new S00589.Node(1, new List<S00589.Node?> {new(3, new List<S00589.Node?> {new(5), new(6)}), new(2), new(4)}))
-            .Should().BeEquivalentTo(new List<int> {1, 3, 5, 6, 2, 4});
+            .Should().Equal(1, 3, 5, 6, 2, 4);
     }
 
     [Test]
@@ -32,7 +32,7 @@ public class S00589Tests
     {
         var sut = new S00589();
         sut.Preorder(new S00589.Node(1, new List<S00589.Node?> {new(2), new(3, new List<S00589.Node?> {new(6), new(7, new List<S00589.Node?> {new(11, new List<S00589.Node?> {new(14)})})}), new(4, new List<S00589.Node?> {new(8, new List<S00589.Node?> {new(12)})}), new(5, new List<S00589.Node?> {new(9, new List<S00589.Node?> {new(13)}), new(10)})}))
-            .Should().BeEquivalentTo(new List<int> {1, 2, 3, 6, 7, 11, 14, 4, 8, 12, 5, 9, 13, 10});
+            .Should().Equal(1, 2, 3, 6, 7, 11, 14, 4, 8, 12, 5, 9, 13, 10);
     }
 
 }
