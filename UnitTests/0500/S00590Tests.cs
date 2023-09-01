eda496921717817b5645681a7130a6354d053313
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 09:51:58 +1000

    590. N-ary Tree Postorder Traversal

---
+++ UnitTests/0500/S00590Tests.cs
@@ -0,0 +1,47 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00590Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00590();
+        sut.Postorder(null).Should().BeEmpty();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00590();
+        sut.Postorder(new S00590.Node()).Should().BeEquivalentTo(new List<int> {0});
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00590();
+        sut.Postorder(new S00590.Node(1,
+                new List<S00590.Node?> {new(3, new List<S00590.Node?> {new(5), new(6)}), new(2), new(4)}))
+            .Should().Equal(5, 6, 3, 2, 4, 1);
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S00590();
+        sut.Postorder(new S00590.Node(1,
+                new List<S00590.Node?>
+                {
+                    new(2),
+                    new(3,
+                        new List<S00590.Node?>
+                            {new(6), new(7, new List<S00590.Node?> {new(11, new List<S00590.Node?> {new(14)})})}),
+                    new(4, new List<S00590.Node?> {new(8, new List<S00590.Node?> {new(12)})}),
+                    new(5, new List<S00590.Node?> {new(9, new List<S00590.Node?> {new(13)}), new(10)})
+                }))
+            .Should().Equal(2, 6, 14, 11, 7, 3, 12, 8, 4, 13, 9, 10, 5, 1);
+    }
+}
---
+++ leetcode-sharp/0500/S00590.cs
@@ -0,0 +1,51 @@
+﻿namespace leetcode_sharp;
+
+// 590. N-ary Tree Postorder Traversal
+// https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+public class S00590
+{
+    public class Node
+    {
+        public int val;
+        public IList<Node?>? children;
+
+        public Node()
+        {
+        }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node?>? _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    private readonly List<int> _result = new();
+
+    public IList<int> Postorder(Node? root)
+    {
+        postorderTraversal(root);
+        
+        return _result;
+    }
+
+    private void postorderTraversal(Node? node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        foreach (var childNode in node.children ?? new List<Node?>())
+        {
+            postorderTraversal(childNode);
+        }
+
+        _result.Add(node.val);
+    }
+}
