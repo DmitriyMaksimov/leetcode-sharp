79382c05612b0b19cc4a13092fc1ed0afa25d6da
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-01 23:15:23 +1000

    572. Subtree of Another Tree

---
+++ UnitTests/0500/S00572Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00572Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00572();
+        sut.IsSubtree(TreeNode.Parse("[3,4,5,1,2]"), TreeNode.Parse("[4,1,2]")).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00572();
+        sut.IsSubtree(TreeNode.Parse("[3,4,5,1,2,null,null,null,null,0]"), TreeNode.Parse("[4,1,2]")).Should()
+            .BeFalse();
+    }
+}
---
+++ leetcode-sharp/0500/S00572.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 572. Subtree of Another Tree
+// https://leetcode.com/problems/subtree-of-another-tree/
+public class S00572
+{
+    public bool IsSubtree(TreeNode? root, TreeNode? subRoot)
+    {
+        if (root == null || subRoot == null) return false;
+        if (isSameTree(root, subRoot)) return true;
+        return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+    }
+
+    private static bool isSameTree(TreeNode? root, TreeNode? subRoot)
+    {
+        if (root == null && subRoot == null) return true;
+        if (root == null || subRoot == null) return false;
+        if (root.val != subRoot.val) return false;
+        return isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
+    }
+}
