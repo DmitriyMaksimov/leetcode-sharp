426a9d4a89019e9f4854709be7d6c223f2e9807b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-01 18:59:47 +1000

    541. Reverse String II

---
+++ UnitTests/0500/S00541Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00541Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00541();
+        sut.ReverseStr("abcdefg", 2).Should().Be("bacdfeg");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00541();
+        sut.ReverseStr("abcd", 2).Should().Be("bacd");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00541();
+        sut.ReverseStr("abcd", 4).Should().Be("dcba");
+    }
+}
---
+++ leetcode-sharp/0500/S00541.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 541. Reverse String II
+// https://leetcode.com/problems/reverse-string-ii/
+public class S00541
+{
+    public string ReverseStr(string s, int k)
+    {
+        var n = s.Length;
+        var a = s.ToCharArray();
+
+        for (var i = 0; i < n; i += 2 * k)
+        {
+            var start = i;
+            var end = Math.Min(i + k - 1, n - 1);
+            while (start < end)
+            {
+                (a[start], a[end]) = (a[end], a[start]);
+                ++start;
+                --end;
+            }
+        }
+
+        return new string(a);
+    }
+}
