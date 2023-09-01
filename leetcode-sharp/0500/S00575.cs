e84f962062c4ccfcba6f2000f7fb0e0812da90d3
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 09:03:17 +1000

    Change the solution to 1 line

--- leetcode-sharp/0500/S00575.cs
+++ leetcode-sharp/0500/S00575.cs
@@ -6,12 +6,6 @@ public class S00575
 {
     public int DistributeCandies(int[] candyType)
     {
-        var set = new HashSet<int>();
-        foreach (var candy in candyType)
-        {
-            set.Add(candy);
-        }
-
-        return Math.Min(set.Count, candyType.Length / 2);
+        return Math.Min(candyType.Distinct().Count(), candyType.Length / 2);
     }
 }
