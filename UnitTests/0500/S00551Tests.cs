b2e6119f09fe924efb25ae37fff862b1b368fa27
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-01 19:55:06 +1000

    551. Student Attendance Record I

---
+++ UnitTests/0500/S00551Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00551Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00551();
+        sut.CheckRecord("PPALLP").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00551();
+        sut.CheckRecord("PPALLL").Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0500/S00551.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 551. Student Attendance Record I
+// https://leetcode.com/problems/student-attendance-record-i/
+public class S00551
+{
+    public bool CheckRecord(string s)
+    {
+        var absent = 0;
+        var late = 0;
+        foreach (var c in s)
+        {
+            switch (c)
+            {
+                case 'A':
+                    absent++;
+                    late = 0;
+                    break;
+                case 'L':
+                    late++;
+                    break;
+                default:
+                    late = 0;
+                    break;
+            }
+
+            if (absent > 1 || late > 2)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
