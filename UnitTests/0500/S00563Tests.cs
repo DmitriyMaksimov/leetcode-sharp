d1429e49e720cc31c6b59f9bb7f28e5ae74590a1
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-01 23:09:36 +1000

    563. Binary Tree Tilt

---
+++ UnitTests/0500/S00563Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00563Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00563();
+        sut.FindTilt(TreeNode.Parse("[1,2,3]")).Should().Be(1);
+    }
+    
+    [Test]
+    public void T2()
+    {
+        var sut = new S00563();
+        sut.FindTilt(TreeNode.Parse("[4,2,9,3,5,null,7]")).Should().Be(15);
+    }
+    
+    [Test]
+    public void T3()
+    {
+        var sut = new S00563();
+        sut.FindTilt(TreeNode.Parse("[21,7,14,1,1,2,2,3,3]")).Should().Be(9);
+    }
+
+}
---
+++ leetcode-sharp/0500/S00563.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 563. Binary Tree Tilt
+// https://leetcode.com/problems/binary-tree-tilt/
+public class S00563
+{
+    private int _answer;
+
+    public int FindTilt(TreeNode? root)
+    {
+        helper(root);
+        return _answer;
+    }
+
+    private int helper(TreeNode? root)
+    {
+        if (root == null) return 0;
+
+        var leftSum = helper(root.left);
+        var rightSum = helper(root.right);
+
+        _answer += Math.Abs(leftSum - rightSum);
+
+        return root.val + leftSum + rightSum;
+    }
+}
