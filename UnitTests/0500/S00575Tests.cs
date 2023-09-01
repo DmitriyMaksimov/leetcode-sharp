7de6410a7edd5a8e3b9039c477fb66b97dbd8998
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 08:59:32 +1000

    575. Distribute Candies

---
+++ UnitTests/0500/S00575Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00575Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00575();
+        sut.DistributeCandies(new[] {1, 1, 2, 2, 3, 3}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00575();
+        sut.DistributeCandies(new[] {1, 1, 2, 3}).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00575();
+        sut.DistributeCandies(new[] {6, 6, 6, 6}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/0500/S00575.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode_sharp;
+
+// 575. Distribute Candies
+// https://leetcode.com/problems/distribute-candies/
+public class S00575
+{
+    public int DistributeCandies(int[] candyType)
+    {
+        var set = new HashSet<int>();
+        foreach (var candy in candyType)
+        {
+            set.Add(candy);
+        }
+
+        return Math.Min(set.Count, candyType.Length / 2);
+    }
+}
