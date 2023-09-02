72d1813c4d43bce73b586dc1ee43a71b049e0704
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 15:11:34 +1000

    671. Second Minimum Node In a Binary Tree

---
+++ UnitTests/0600/S00671Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00671Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00671();
+        sut.FindSecondMinimumValue(TreeNode.Parse("[2,2,5,null,null,5,7]")).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00671();
+        sut.FindSecondMinimumValue(TreeNode.Parse("[2,2,2]")).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/0600/S00671.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 671. Second Minimum Node In a Binary Tree
+// https://leetcode.com/problems/second-minimum-node-in-a-binary-tree/
+public class S00671
+{
+    public int FindSecondMinimumValue(TreeNode? root)
+    {
+        if (root?.left == null)
+        {
+            return -1;
+        }
+
+        var left = root.left.val == root.val ? FindSecondMinimumValue(root.left) : root.left.val;
+        var right = root.right!.val == root.val ? FindSecondMinimumValue(root.right) : root.right.val;
+
+        return left == -1 || right == -1 ? Math.Max(left, right) : Math.Min(left, right);
+    }
+}
