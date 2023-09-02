4700958b6b76603f65efa1176e78d61a10b54c61
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 15:25:54 +1000

    682. Baseball Game

---
+++ UnitTests/0600/S00682Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00682Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00682();
+        sut.CalPoints(new[] {"5", "2", "C", "D", "+"}).Should().Be(30);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00682();
+        sut.CalPoints(new[] {"5", "-2", "4", "C", "D", "9", "+", "+"}).Should().Be(27);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00682();
+        sut.CalPoints(new[] {"1", "C"}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0600/S00682.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 682. Baseball Game
+// https://leetcode.com/problems/baseball-game/
+public class S00682
+{
+    public int CalPoints(string[] operations)
+    {
+        var scores = new List<int>();
+
+        foreach (var operation in operations)
+        {
+            switch (operation)
+            {
+                case "+":
+                    scores.Add(scores[^1] + scores[^2]);
+                    break;
+                case "D":
+                    scores.Add(scores[^1] * 2);
+                    break;
+                case "C":
+                    scores.RemoveAt(scores.Count - 1);
+                    break;
+                default:
+                    scores.Add(int.Parse(operation));
+                    break;
+            }
+        }
+
+        return scores.Sum();
+    }
+}
