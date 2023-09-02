ac5e16997473fe53717541b7624c0364b663b1e8
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 13:41:23 +1000

    628. Maximum Product of Three Numbers

---
+++ UnitTests/0600/S00628Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00628Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00628();
+        sut.MaximumProduct(new[] {1, 2, 3}).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00628();
+        sut.MaximumProduct(new[] {1, 2, 3, 4}).Should().Be(24);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00628();
+        sut.MaximumProduct(new[] {-1, -2, -3}).Should().Be(-6);
+    }
+}
---
+++ leetcode-sharp/0600/S00628.cs
@@ -0,0 +1,13 @@
+﻿namespace leetcode_sharp;
+
+// 628. Maximum Product of Three Numbers
+// https://leetcode.com/problems/maximum-product-of-three-numbers/
+public class S00628
+{
+    public int MaximumProduct(int[] nums)
+    {
+        Array.Sort(nums);
+        var n = nums.Length;
+        return Math.Max(nums[0] * nums[1] * nums[n - 1], nums[n - 1] * nums[n - 2] * nums[n - 3]);
+    }
+}
