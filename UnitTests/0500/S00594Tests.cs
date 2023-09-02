d4c6fe4c82fde31a1cd9a74b3df12e5ddcc546b1
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 13:02:01 +1000

    594. Longest Harmonious Subsequence

---
+++ UnitTests/0500/S00594Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00594Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00594();
+        sut.FindLHS(new[] {1, 3, 2, 2, 5, 2, 3, 7}).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00594();
+        sut.FindLHS(new[] {1, 2, 3, 4}).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00594();
+        sut.FindLHS(new[] {1, 1, 1, 1}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0500/S00594.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 594. Longest Harmonious Subsequence
+// https://leetcode.com/problems/longest-harmonious-subsequence/
+public class S00594
+{
+    public int FindLHS(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            dict[num] = dict.GetValueOrDefault(num, 0) + 1;
+        }
+
+        var max = 0;
+
+        foreach (var key in dict.Keys)
+        {
+            if (dict.ContainsKey(key + 1))
+            {
+                max = Math.Max(max, dict[key] + dict[key + 1]);
+            }
+        }
+
+        return max;
+    }
+}
