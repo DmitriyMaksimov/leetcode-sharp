67dab75f917110a07b4554609c207909c1e3a57a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 15:14:54 +1000

    674. Longest Continuous Increasing Subsequence

---
+++ UnitTests/0600/S00674Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00674Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00674();
+        sut.FindLengthOfLCIS(new[] {1, 3, 5, 4, 7}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00674();
+        sut.FindLengthOfLCIS(new[] {2, 2, 2, 2, 2}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/0600/S00674.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 674. Longest Continuous Increasing Subsequence
+// https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+public class S00674
+{
+    public int FindLengthOfLCIS(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }
+
+        var max = 1;
+        var cur = 1;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                cur++;
+            }
+            else
+            {
+                max = Math.Max(max, cur);
+                cur = 1;
+            }
+        }
+
+        return Math.Max(max, cur);
+    }
+}
