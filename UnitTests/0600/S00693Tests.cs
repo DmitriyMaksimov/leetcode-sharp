15ac3b3c5228584b4a11f97173e02c375e97ec84
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 15:29:32 +1000

    693. Binary Number with Alternating Bits

---
+++ UnitTests/0600/S00693Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00693Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00693();
+        sut.HasAlternatingBits(5).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00693();
+        sut.HasAlternatingBits(7).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00693();
+        sut.HasAlternatingBits(11).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0600/S00693.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 693. Binary Number with Alternating Bits
+// https://leetcode.com/problems/binary-number-with-alternating-bits/
+public class S00693
+{
+    public bool HasAlternatingBits(int n)
+    {
+        var s = Convert.ToString(n, 2);
+
+        for (var i = 1; i < s.Length; i++)
+        {
+            if (s[i] == s[i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
