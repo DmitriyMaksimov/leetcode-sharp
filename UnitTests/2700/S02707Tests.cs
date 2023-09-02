d557db3c2967d70eefb82cddcf978ed0b7825605
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 12:57:41 +1000

    2707. Extra Characters in a String

---
+++ UnitTests/2700/S02707Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02707Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02707();
+        sut.MinExtraChar("leetscode", new[] {"leet", "code", "leetcode"}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02707();
+        sut.MinExtraChar("sayhelloworld", new[] {"hello", "world"}).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/2700/S02707.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 2707. Extra Characters in a String
+// https://leetcode.com/problems/extra-characters-in-a-string/ 
+public class S02707
+{
+    public int MinExtraChar(string s, string[] dictionary)
+    {
+        var maxVal = s.Length + 1;
+        var dp = new int[s.Length + 1];
+        Array.Fill(dp, maxVal);
+        dp[0] = 0;
+
+        var dictionarySet = new HashSet<string>(dictionary);
+
+        for (var i = 1; i <= s.Length; ++i)
+        {
+            dp[i] = dp[i - 1] + 1;
+            for (var j = 1; j <= i; ++j)
+            {
+                if (dictionarySet.Contains(s.Substring(i - j, j)))
+                {
+                    dp[i] = Math.Min(dp[i], dp[i - j]);
+                }
+            }
+        }
+
+        return dp[s.Length];
+    }
+}
