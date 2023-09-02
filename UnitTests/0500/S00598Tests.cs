58609e97e30e1fb4c67c368dfe5ec4b4bd884f7a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 13:17:15 +1000

    598. Range Addition II

---
+++ UnitTests/0500/S00598Tests.cs
@@ -0,0 +1,33 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00598Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00598();
+        sut.MaxCount(3, 3, new[] {new[] {2, 2}, new[] {3, 3}}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00598();
+        sut.MaxCount(3, 3,
+            new[]
+            {
+                new[] {2, 2}, new[] {3, 3}, new[] {3, 3}, new[] {2, 2}, new[] {3, 3}, new[] {3, 3}, new[] {3, 3},
+                new[] {2, 2}, new[] {3, 3}, new[] {3, 3}, new[] {3, 3}
+            }).Should().Be(4);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00598();
+        sut.MaxCount(3, 3, Array.Empty<int[]>()).Should().Be(9);
+    }
+}
---
+++ leetcode-sharp/0500/S00598.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 598. Range Addition II
+// https://leetcode.com/problems/range-addition-ii
+public class S00598
+{
+    public int MaxCount(int m, int n, int[][] ops)
+    {
+        var minM = m;
+        var minN = n;
+
+        foreach (var op in ops)
+        {
+            minM = Math.Min(minM, op[0]);
+            minN = Math.Min(minN, op[1]);
+        }
+
+        return minM * minN;
+    }
+}
