e29ca59235ba3f62f4ff632103bd207661c7c211
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 15:20:45 +1000

    680. Valid Palindrome II

---
+++ leetcode-sharp/0600/S00680.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 680. Valid Palindrome II
+// https://leetcode.com/problems/valid-palindrome-ii/
+public class S00680
+{
+    public bool ValidPalindrome(string s)
+    {
+        var i = 0;
+        var j = s.Length - 1;
+
+        while (i < j)
+        {
+            if (s[i] != s[j])
+            {
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    private static bool isPalindrome(string s, int i, int j)
+    {
+        while (i < j)
+        {
+            if (s[i] != s[j])
+            {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
