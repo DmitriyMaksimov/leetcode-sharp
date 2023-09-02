ff45821d029e7775a9760569697ded8481517efb
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 13:29:58 +1000

    599. Minimum Index Sum of Two Lists

---
+++ UnitTests/0500/S00599Tests.cs
@@ -0,0 +1,33 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00599Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00599();
+        sut.FindRestaurant(new[] {"Shogun", "Tapioca Express", "Burger King", "KFC"},
+                new[] {"Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"}).Should()
+            .Equal("Shogun");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00599();
+        sut.FindRestaurant(new[] {"Shogun", "Tapioca Express", "Burger King", "KFC"},
+                new[] {"KFC", "Shogun", "Burger King"}).Should()
+            .Equal("Shogun");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00599();
+        sut.FindRestaurant(new[] {"happy", "sad", "good"}, new[] {"sad", "happy", "good"}).Should()
+            .Equal("happy", "sad");
+    }
+}
---
+++ leetcode-sharp/0500/S00599.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 599. Minimum Index Sum of Two Lists
+// https://leetcode.com/problems/minimum-index-sum-of-two-lists/
+public class S00599
+{
+    public string[] FindRestaurant(string[] list1, string[] list2)
+    {
+        var commonStrings = list1.Intersect(list2);
+        var indexSums = commonStrings.Select(x =>
+            new {name = x, sum = Array.IndexOf(list1, x) + Array.IndexOf(list2, x)}
+        );
+        var minIndexSum = indexSums.Min(x => x.sum);
+
+        return indexSums
+            .Where(x => x.sum == minIndexSum)
+            .Select(x => x.name)
+            .ToArray();
+    }
+}
