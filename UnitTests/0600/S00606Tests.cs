323e470bd7b87d7a03f3362efa9164d0ac4ab9ab
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 13:33:54 +1000

    606. Construct String from Binary Tree

---
+++ UnitTests/0600/S00606Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00606Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00606();
+        sut.Tree2str(TreeNode.Parse("[1,2,3,4]")).Should().Be("1(2(4))(3)");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00606();
+        sut.Tree2str(TreeNode.Parse("[1,2,3,null,4]")).Should().Be("1(2()(4))(3)");
+    }
+}
---
+++ leetcode-sharp/0600/S00606.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 606. Construct String from Binary Tree
+// https://leetcode.com/problems/construct-string-from-binary-tree/
+public class S00606
+{
+    public string Tree2str(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return string.Empty;
+        }
+
+        var left = Tree2str(root.left);
+        var right = Tree2str(root.right);
+        if (string.IsNullOrEmpty(left) && string.IsNullOrEmpty(right))
+        {
+            return root.val.ToString();
+        }
+
+        return string.IsNullOrEmpty(right) ? $"{root.val}({left})" : $"{root.val}({left})({right})";
+    }
+}
