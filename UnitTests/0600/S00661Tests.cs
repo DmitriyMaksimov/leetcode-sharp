3031ff008b667537f3d7329b9174029b4a12c2ab
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 15:07:32 +1000

    661. Image Smoother

---
+++ UnitTests/0600/S00661Tests.cs
@@ -0,0 +1,23 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00661Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00661();
+        sut.ImageSmoother(new[] {new[] {1, 1, 1}, new[] {1, 0, 1}, new[] {1, 1, 1}}).Should()
+            .BeEquivalentTo(new[] {new[] {0, 0, 0}, new[] {0, 0, 0}, new[] {0, 0, 0}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00661();
+        sut.ImageSmoother(new[] {new[] {100, 200, 100}, new[] {200, 50, 200}, new[] {100, 200, 100}}).Should()
+            .BeEquivalentTo(new[] {new[] {137, 141, 137}, new[] {141, 138, 141}, new[] {137, 141, 137}});
+    }
+}
---
+++ leetcode-sharp/0600/S00661.cs
@@ -0,0 +1,36 @@
+﻿namespace leetcode_sharp;
+
+// 661. Image Smoother
+// https://leetcode.com/problems/image-smoother/
+public class S00661
+{
+    public int[][] ImageSmoother(int[][] img)
+    {
+        var n = img.Length;
+        var m = img[0].Length;
+        var result = new int[n][];
+
+        for (var i = 0; i < n; i++)
+        {
+            result[i] = new int[m];
+
+            for (var j = 0; j < m; j++)
+            {
+                var amount = 0;
+                var count = 0;
+                for (var k = Math.Max(i - 1, 0); k <= Math.Min(i + 1, n - 1); k++)
+                {
+                    for (var l = Math.Max(j - 1, 0); l <= Math.Min(j + 1, m - 1); l++)
+                    {
+                        count++;
+                        amount += img[k][l];
+                    }
+                }
+
+                result[i][j] = amount / count;
+            }
+        }
+
+        return result;
+    }
+}
