80dce2b974bfd9dfac96b87fffdceaeb7aff7c6b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-02 14:58:29 +1000

    645. Set Mismatch

---
+++ UnitTests/0600/S00645Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00645Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00645();
+        sut.FindErrorNums(new[] {1, 2, 2, 4}).Should().Equal(2, 3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00645();
+        sut.FindErrorNums(new[] {1, 1}).Should().Equal(1, 2);
+    }
+
+}
---
+++ leetcode-sharp/0600/S00645.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 645. Set Mismatch
+// https://leetcode.com/problems/set-mismatch/
+public class S00645
+{
+    public int[] FindErrorNums(int[] nums)
+    {
+        var set = new HashSet<int>();
+        var result = new int[2];
+
+        foreach (var num in nums)
+        {
+            if (set.Contains(num))
+            {
+                result[0] = num;
+            }
+            else
+            {
+                set.Add(num);
+            }
+        }
+
+        for (var i = 1; i <= nums.Length; i++)
+        {
+            if (!set.Contains(i))
+            {
+                result[1] = i;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
