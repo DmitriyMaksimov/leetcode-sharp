589b3c1f08454db795e8748e4062cbf1f91b7384
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-03 13:40:59 +1000

    762. Prime Number of Set Bits in Binary Representation

---
+++ UnitTests/0700/S00762Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00762Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00762();
+        sut.CountPrimeSetBits(6, 10).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00762();
+        sut.CountPrimeSetBits(10, 15).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/0700/S00762.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace leetcode_sharp;
+
+// 762. Prime Number of Set Bits in Binary Representation
+// https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation/
+public class S00762
+{
+    public int CountPrimeSetBits(int left, int right)
+    {
+        var primes = new HashSet<int> {2, 3, 5, 7, 11, 13, 17, 19};
+        var count = 0;
+
+        for (var i = left; i <= right; i++)
+        {
+            if (primes.Contains(BitOperations.PopCount((uint) i)))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
