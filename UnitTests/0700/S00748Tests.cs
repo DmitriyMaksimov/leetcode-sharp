bd060dd8d4ece8bbaad21ca29e2c9fe3bc03de5d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-03 13:34:22 +1000

    748. Shortest Completing Word

---
+++ UnitTests/0700/S00748Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00748Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00748();
+        sut.ShortestCompletingWord("1s3 PSt", new[] {"step", "steps", "stripe", "stepple"}).Should().Be("steps");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00748();
+        sut.ShortestCompletingWord("1s3 456", new[] {"looks", "pest", "stew", "show"}).Should().Be("pest");
+    }
+}
---
+++ leetcode-sharp/0700/S00748.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcode_sharp;
+
+// 748. Shortest Completing Word
+// https://leetcode.com/problems/shortest-completing-word/
+public class S00748
+{
+    public string ShortestCompletingWord(string licensePlate, string[] words)
+    {
+        var dict = new Dictionary<char, int>();
+
+        foreach (var key in licensePlate.Where(char.IsLetter).Select(char.ToLower))
+        {
+            dict[key] = dict.GetValueOrDefault(key) + 1;
+        }
+
+        var result = string.Empty;
+
+        foreach (var word in words)
+        {
+            var temp = new Dictionary<char, int>(dict);
+            foreach (var c in word)
+            {
+                if (temp.ContainsKey(c))
+                {
+                    temp[c]--;
+                }
+            }
+
+            if (temp.Values.All(v => v <= 0))
+            {
+                if (result == string.Empty || word.Length < result.Length)
+                {
+                    result = word;
+                }
+            }
+        }
+
+        return result;
+    }
+}
