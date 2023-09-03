c16579c6f26f8134ddf34dfb234200f5b35d4f0c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-03 13:52:40 +1000

    796. Rotate String

---
+++ UnitTests/0700/S00796Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00796Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00796();
+        sut.RotateString("abcde", "cdeab").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00796();
+        sut.RotateString("abcde", "abced").Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0700/S00796.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcode_sharp;
+
+// 796. Rotate String
+// https://leetcode.com/problems/rotate-string/
+public class S00796
+{
+    public bool RotateString(string s, string goal)
+    {
+        if (s.Length != goal.Length)
+        {
+            return false;
+        }
+
+        if (s.Length == 0)
+        {
+            return true;
+        }
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] != goal[0]) continue;
+            
+            var j = 0;
+            for (; j < goal.Length; j++)
+            {
+                if (s[(i + j) % s.Length] != goal[j])
+                {
+                    break;
+                }
+            }
+
+            if (j == goal.Length)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
