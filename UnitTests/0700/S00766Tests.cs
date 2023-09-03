722c54a766fcd7ba41cd444e5b7365dce0461e8d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-03 13:47:40 +1000

    766. Toeplitz Matrix

---
+++ UnitTests/0700/S00766Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00766Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00766();
+        sut.IsToeplitzMatrix(new[] {new[] {1, 2, 3, 4}, new[] {5, 1, 2, 3}, new[] {9, 5, 1, 2}}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00766();
+        sut.IsToeplitzMatrix(new[] {new[] {1, 2}, new[] {2, 2}}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0700/S00766.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 766. Toeplitz Matrix
+// https://leetcode.com/problems/toeplitz-matrix/
+public class S00766
+{
+    public bool IsToeplitzMatrix(int[][] matrix)
+    {
+        var n = matrix.Length;
+        var m = matrix[0].Length;
+        
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < m; j++)
+            {
+                if (j - i >= 0 && matrix[i][j] != matrix[0][j - i] || i - j >= 0 && matrix[i][j] != matrix[i - j][0])
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+}
