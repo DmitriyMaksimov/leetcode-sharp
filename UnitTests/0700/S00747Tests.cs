0332ee3d59c2cf96b58bd60507b9de8c2e688056
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-03 13:27:30 +1000

    747. Largest Number At Least Twice of Others

---
+++ UnitTests/0700/S00747Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00747Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00747();
+        sut.DominantIndex(new[] {3, 6, 1, 0}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00747();
+        sut.DominantIndex(new[] {1, 2, 3, 4}).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/0700/S00747.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 747. Largest Number At Least Twice of Others
+// https://leetcode.com/problems/largest-number-at-least-twice-of-others/
+public class S00747
+{
+    public int DominantIndex(int[] nums)
+    {
+        var max = 0;
+        var maxIndex = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > max)
+            {
+                max = nums[i];
+                maxIndex = i;
+            }
+        }
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (i != maxIndex && nums[i] * 2 > max)
+            {
+                return -1;
+            }
+        }
+
+        return maxIndex;
+    }
+}
