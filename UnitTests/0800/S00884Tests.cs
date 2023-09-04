0a4fa6949a3d50f92726c84d7b4505ac962d501d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-04 18:46:19 +1000

    884. Uncommon Words from Two Sentences

---
+++ UnitTests/0800/S00884Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00884Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00884();
+        sut.UncommonFromSentences("this apple is sweet", "this apple is sour").Should().Equal("sweet", "sour");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00884();
+        sut.UncommonFromSentences("apple apple", "banana").Should().Equal("banana");
+    }
+}
---
+++ leetcode-sharp/0800/S00884.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 884. Uncommon Words from Two Sentences
+// https://leetcode.com/problems/uncommon-words-from-two-sentences/
+public class S00884
+{
+    public string[] UncommonFromSentences(string s1, string s2)
+    {
+        var dict = new Dictionary<string, int>();
+        foreach (var s in s1.Split(' '))
+        {
+            dict[s] = dict.GetValueOrDefault(s) + 1;
+        }
+
+        foreach (var s in s2.Split(' '))
+        {
+            dict[s] = dict.GetValueOrDefault(s) + 1;
+        }
+
+        return dict.Where(kv => kv.Value == 1).Select(kv => kv.Key).ToArray();
+    }
+}
