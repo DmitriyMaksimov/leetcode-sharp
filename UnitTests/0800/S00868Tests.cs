31bcf60f68820c3850bac7988629fe372bbbbf37
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-04 17:52:00 +1000

    868. Binary Gap

---
+++ UnitTests/0800/S00868Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00868Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00868();
+        sut.BinaryGap(22).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00868();
+        sut.BinaryGap(8).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00868();
+        sut.BinaryGap(5).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/0800/S00868.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 868. Binary Gap
+// https://leetcode.com/problems/binary-gap/
+public class S00868
+{
+    public int BinaryGap(int n)
+    {
+        var max = 0;
+        var last = -1;
+        var i = 0;
+
+        while (n > 0)
+        {
+            if ((n & 1) == 1)
+            {
+                if (last >= 0)
+                {
+                    max = Math.Max(max, i - last);
+                }
+
+                last = i;
+            }
+
+            n >>= 1;
+            i++;
+        }
+
+        return max;
+    }
+}
