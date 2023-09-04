ac28dd8d4690806af22a943759df5ac08ad92116
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-05 09:00:17 +1000

    897. Increasing Order Search Tree

---
+++ UnitTests/0800/S00897Tests.cs
@@ -0,0 +1,23 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00897Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00897();
+        sut.IncreasingBST(TreeNode.Parse("[5,3,6,2,4,null,8,1,null,null,null,7,9]"))
+            .Should().BeEquivalentTo(TreeNode.Parse("[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00897();
+        sut.IncreasingBST(TreeNode.Parse("[5,1,7]"))
+            .Should().BeEquivalentTo(TreeNode.Parse("[1,null,5,null,7]"));
+    }
+}
---
+++ leetcode-sharp/0800/S00897.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 897. Increasing Order Search Tree
+// https://leetcode.com/problems/increasing-order-search-tree/
+public class S00897
+{
+    public TreeNode IncreasingBST(TreeNode? root)
+    {
+        var list = new List<int>();
+
+        traverse(root, list);
+        var result = new TreeNode(list[0]);
+        var current = result;
+
+        for (var i = 1; i < list.Count; i++)
+        {
+            current.right = new TreeNode(list[i]);
+            current = current.right;
+        }
+
+        return result;
+    }
+
+    private static void traverse(TreeNode? node, ICollection<int> list)
+    {
+        if (node == null) return;
+        traverse(node.left, list);
+        list.Add(node.val);
+        traverse(node.right, list);
+    }
+}
