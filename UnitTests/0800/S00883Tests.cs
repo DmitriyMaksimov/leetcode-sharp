5fd94aab897f84612734021c02d265d3f33c16b7
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-04 18:36:49 +1000

    883. Projection Area of 3D Shapes

---
+++ UnitTests/0800/S00883Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00883Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00883();
+        sut.ProjectionArea(new[] {new[] {1, 2}, new[] {3, 4}}).Should().Be(17);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00883();
+        sut.ProjectionArea(new[] {new[] {2}}).Should().Be(5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00883();
+        sut.ProjectionArea(new[] {new[] {1, 0}, new[] {0, 2}}).Should().Be(8);
+    }
+}
---
+++ leetcode-sharp/0800/S00883.cs
@@ -0,0 +1,34 @@
+﻿namespace leetcode_sharp;
+
+// 883. Projection Area of 3D Shapes
+// https://leetcode.com/problems/projection-area-of-3d-shapes/
+public class S00883
+{
+    public int ProjectionArea(int[][] grid)
+    {
+        var xy = 0;
+        var xz = 0;
+        var yz = 0;
+
+        for (var i = 0; i < grid.Length; i++)
+        {
+            var maxRow = 0;
+            var maxCol = 0;
+            for (var j = 0; j < grid[i].Length; j++)
+            {
+                if (grid[i][j] > 0)
+                {
+                    xy++;
+                }
+
+                maxRow = Math.Max(maxRow, grid[i][j]);
+                maxCol = Math.Max(maxCol, grid[j][i]);
+            }
+
+            xz += maxRow;
+            yz += maxCol;
+        }
+
+        return xy + xz + yz;
+    }
+}
