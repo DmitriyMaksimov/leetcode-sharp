985ead7dd19fb71b4628204a17ee0e1c88437c05
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-04 19:26:03 +1000

    892. Surface Area of 3D Shapes

---
+++ UnitTests/0800/S00892Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00892Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00892();
+        sut.SurfaceArea(new[] {new[] {1, 2}, new[] {3, 4}}).Should().Be(34);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00892();
+        sut.SurfaceArea(new[] {new[] {1, 1, 1}, new[] {1, 0, 1}, new[] {1, 1, 1}}).Should().Be(32);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00892();
+        sut.SurfaceArea(new[] {new[] {2, 2, 2}, new[] {2, 1, 2}, new[] {2, 2, 2}}).Should().Be(46);
+    }
+}
---
+++ leetcode-sharp/0800/S00892.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 892. Surface Area of 3D Shapes
+// https://leetcode.com/problems/surface-area-of-3d-shapes/
+public class S00892
+{
+    public int SurfaceArea(int[][] grid)
+    {
+        var result = 0;
+        var n = grid.Length;
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                var v = grid[i][j];
+                if (v <= 0) continue;
+                
+                result += 2;
+                result += i == 0 ? v : Math.Max(0, v - grid[i - 1][j]);
+                result += i == n - 1 ? v : Math.Max(0, v - grid[i + 1][j]);
+                result += j == 0 ? v : Math.Max(0, v - grid[i][j - 1]);
+                result += j == n - 1 ? v : Math.Max(0, v - grid[i][j + 1]);
+            }
+        }
+
+        return result;
+    }
+}
