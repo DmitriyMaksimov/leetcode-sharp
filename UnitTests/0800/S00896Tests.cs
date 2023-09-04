a1fdae10e72db32e41610f3e2c61462d309fdbb8
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-04 19:32:40 +1000

    896. Monotonic Array

---
+++ UnitTests/0800/S00896Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00896Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00896();
+        sut.IsMonotonic(new[] {1, 2, 2, 3}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00896();
+        sut.IsMonotonic(new[] {6, 5, 4, 4}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00896();
+        sut.IsMonotonic(new[] {1, 3, 2}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0800/S00896.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 896. Monotonic Array
+// https://leetcode.com/problems/monotonic-array/
+public class S00896
+{
+    public bool IsMonotonic(int[] nums)
+    {
+        var isIncreasing = true;
+        var isDecreasing = true;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                isDecreasing = false;
+            }
+
+            if (nums[i] < nums[i - 1])
+            {
+                isIncreasing = false;
+            }
+        }
+
+        return isIncreasing || isDecreasing;
+    }
+}
