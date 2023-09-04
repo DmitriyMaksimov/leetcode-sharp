33a0db831a44b1b50a04fd4a4c57b03649f125ab
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-04 19:16:56 +1000

    888. Fair Candy Swap

---
+++ UnitTests/0800/S00888Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00888Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00888();
+        sut.FairCandySwap(new[] {1, 1}, new[] {2, 2}).Should().Equal(1, 2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00888();
+        sut.FairCandySwap(new[] {1, 2}, new[] {2, 3}).Should().Equal(1, 2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00888();
+        sut.FairCandySwap(new[] {2}, new[] {1, 3}).Should().Equal(2, 3);
+    }
+}
---
+++ leetcode-sharp/0800/S00888.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 888. Fair Candy Swap
+// https://leetcode.com/problems/fair-candy-swap/
+public class S00888
+{
+    public int[] FairCandySwap(int[] aliceSizes, int[] bobSizes)
+    {
+        var aliceSum = aliceSizes.Sum();
+        var bobSum = bobSizes.Sum();
+        var diff = (aliceSum - bobSum) / 2;
+        var bobSet = new HashSet<int>(bobSizes);
+
+        foreach (var a in aliceSizes)
+        {
+            var b = a - diff;
+            if (bobSet.Contains(b))
+            {
+                return new[] {a, b};
+            }
+        }
+
+        return Array.Empty<int>();
+    }
+}
