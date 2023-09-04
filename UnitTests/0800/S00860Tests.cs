05066db1ea846e87db24c011c943640cab56c2e7
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-04 17:44:39 +1000

    860. Lemonade Change

---
+++ UnitTests/0800/S00860Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00860Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00860();
+        sut.LemonadeChange(new[] {5, 5, 5, 10, 20}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00860();
+        sut.LemonadeChange(new[] {5, 5, 10, 10, 20}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0800/S00860.cs
@@ -0,0 +1,54 @@
+﻿namespace leetcode_sharp;
+
+// 860. Lemonade Change
+// https://leetcode.com/problems/lemonade-change/
+public class S00860
+{
+    public bool LemonadeChange(int[] bills)
+    {
+        var five = 0;
+        var ten = 0;
+
+        foreach (var bill in bills)
+        {
+            switch (bill)
+            {
+                case 5:
+                    five++;
+                    break;
+                case 10:
+                    if (five == 0)
+                    {
+                        return false;
+                    }
+
+                    five--;
+                    ten++;
+                    break;
+                case 20:
+                    if (five == 0)
+                    {
+                        return false;
+                    }
+
+                    if (ten > 0)
+                    {
+                        ten--;
+                        five--;
+                    }
+                    else if (five >= 3)
+                    {
+                        five -= 3;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                    break;
+            }
+        }
+
+        return true;
+    }
+}
