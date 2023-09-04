e1bbdc314be3c422d712a2ef7dda2c4633ef0d00
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-04 17:49:37 +1000

    867. Transpose Matrix

---
+++ UnitTests/0800/S00867Tests.cs
@@ -0,0 +1,24 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00867Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00867();
+        sut.Transpose(new[] {new[] {1, 2, 3}, new[] {4, 5, 6}, new[] {7, 8, 9}})
+            .Should().BeEquivalentTo(new[]{new[] {1, 4, 7}, new[] {2, 5, 8}, new[] {3, 6, 9}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00867();
+        sut.Transpose(new[] {new[] {1, 2, 3}, new[] {4, 5, 6}})
+            .Should().BeEquivalentTo(new[]{new[] {1, 4}, new[] {2, 5}, new[] {3, 6}});
+    }
+
+}
---
+++ leetcode-sharp/0800/S00867.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 867. Transpose Matrix
+// https://leetcode.com/problems/transpose-matrix/
+public class S00867
+{
+    public int[][] Transpose(int[][] matrix)
+    {
+        var n = matrix.Length;
+        var m = matrix[0].Length;
+        var result = new int[m][];
+
+        for (var i = 0; i < m; i++)
+        {
+            result[i] = new int[n];
+
+            for (var j = 0; j < n; j++)
+            {
+                result[i][j] = matrix[j][i];
+            }
+        }
+
+        return result;
+    }
+}
