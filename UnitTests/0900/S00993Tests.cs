afb61e9de596fc9c9a8096a19abfa20b3d613e5d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 00:27:53 +1000

    993. Cousins in Binary Tree

---
+++ UnitTests/0900/S00993Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00993Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00993();
+        sut.IsCousins(TreeNode.Parse("[1,2,3,4]"), 4, 3).Should().BeFalse();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00993();
+        sut.IsCousins(TreeNode.Parse("[1,2,3,null,4,null,5]"), 5, 4).Should().BeTrue();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00993();
+        sut.IsCousins(TreeNode.Parse("[1,2,3,null,4]"), 2, 3).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0900/S00993.cs
@@ -0,0 +1,44 @@
+﻿namespace leetcode_sharp;
+
+// 993. Cousins in Binary Tree
+// https://leetcode.com/problems/cousins-in-binary-tree/
+public class S00993
+{
+    public bool IsCousins(TreeNode? root, int x, int y)
+    {
+        var xParent = -1;
+        var xDepth = -1;
+        var yParent = -1;
+        var yDepth = -1;
+        
+        var queue = new Queue<(TreeNode?, int, int)>();
+        queue.Enqueue((root, 0, -1));
+
+        while (queue.Count > 0)
+        {
+            var (node, depth, parent) = queue.Dequeue();
+            if (node.val == x)
+            {
+                xParent = parent;
+                xDepth = depth;
+            }
+            else if (node.val == y)
+            {
+                yParent = parent;
+                yDepth = depth;
+            }
+
+            if (node.left != null)
+            {
+                queue.Enqueue((node.left, depth + 1, node.val));
+            }
+
+            if (node.right != null)
+            {
+                queue.Enqueue((node.right, depth + 1, node.val));
+            }
+        }
+
+        return xParent != yParent && xDepth == yDepth;
+    }
+}
