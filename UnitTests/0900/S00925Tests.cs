d67e8aa5dc07d76c00eb14d833cc79b5571bab20
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-05 21:10:25 +1000

    925. Long Pressed Name

---
+++ UnitTests/0900/S00925Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00925Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00925();
+        sut.IsLongPressedName("alex", "aaleex").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00925();
+        sut.IsLongPressedName("saeed", "ssaaedd").Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0900/S00925.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 925. Long Pressed Name
+// https://leetcode.com/problems/long-pressed-name/
+public class S00925
+{
+    public bool IsLongPressedName(string name, string typed)
+    {
+        var i = 0;
+        var j = 0;
+        
+        while (i < name.Length && j < typed.Length)
+        {
+            if (name[i] == typed[j])
+            {
+                i++;
+                j++;
+            }
+            else if (j > 0 && typed[j] == typed[j - 1])
+            {
+                j++;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
