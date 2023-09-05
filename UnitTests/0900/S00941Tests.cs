eb10f7d090ffe5c5302811c1b02ac4b8e5b67d37
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-05 21:25:18 +1000

    941. Valid Mountain Array

---
+++ UnitTests/0900/S00941Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00941Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00941();
+        sut.ValidMountainArray(new[] {2, 1}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00941();
+        sut.ValidMountainArray(new[] {3, 5, 5}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00941();
+        sut.ValidMountainArray(new[] {0, 3, 2, 1}).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/0900/S00941.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 
+// https://leetcode.com/problems/valid-mountain-array/
+public class S00941
+{
+    public bool ValidMountainArray(int[] arr)
+    {
+        if (arr.Length < 3)
+        {
+            return false;
+        }
+
+        var i = 0;
+
+        while (i < arr.Length - 1 && arr[i] < arr[i + 1])
+        {
+            i++;
+        }
+
+        if (i == 0 || i == arr.Length - 1)
+        {
+            return false;
+        }
+
+        while (i < arr.Length - 1 && arr[i] > arr[i + 1])
+        {
+            i++;
+        }
+
+        return i == arr.Length - 1;
+    }
+}
