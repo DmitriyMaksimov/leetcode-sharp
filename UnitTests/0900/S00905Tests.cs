f56d2fc495c22d82b60acdf77d8a5951b42563ae
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-05 20:27:40 +1000

    905. Sort Array By Parity

---
+++ UnitTests/0900/S00905Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00905Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00905();
+        sut.SortArrayByParity(new[] {3, 1, 2, 4}).Should().Equal(2, 4, 1, 3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00905();
+        sut.SortArrayByParity(new[] {0}).Should().Equal(0);
+    }
+}
---
+++ leetcode-sharp/0900/S00905.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 905. Sort Array By Parity
+// https://leetcode.com/problems/sort-array-by-parity/
+public class S00905
+{
+    public int[] SortArrayByParity(int[] nums)
+    {
+        var result = new int[nums.Length];
+        var evenIndex = 0;
+        var oddIndex = nums.Length - 1;
+        foreach (var num in nums)
+        {
+            if (num % 2 == 0)
+            {
+                result[evenIndex++] = num;
+            }
+            else
+            {
+                result[oddIndex--] = num;
+            }
+        }
+
+        return result;
+    }
+}
