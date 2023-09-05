5ea88ce4bf4b4553f963880717d469e305f6ce14
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-05 21:31:20 +1000

    942. DI String Match

---
+++ UnitTests/0900/S00942Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00942Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00942();
+        sut.DiStringMatch("IDID").Should().Equal(0, 4, 1, 3, 2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00942();
+        sut.DiStringMatch("III").Should().Equal(0, 1, 2, 3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00942();
+        sut.DiStringMatch("DDI").Should().Equal(3, 2, 0, 1);
+    }
+}
---
+++ leetcode-sharp/0900/S00942.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 942. DI String Match
+// https://leetcode.com/problems/di-string-match/
+public class S00942
+{
+    public int[] DiStringMatch(string s)
+    {
+        var result = new int[s.Length + 1];
+        var min = 0;
+        var max = s.Length;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == 'I')
+            {
+                result[i] = min++;
+            }
+            else
+            {
+                result[i] = max--;
+            }
+        }
+        
+        result[^1] = min;
+
+        return result;
+    }
+}
