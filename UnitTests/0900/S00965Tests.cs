0e8bd12907699d63c2157a69ec03e587802bb0bb
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-05 21:42:38 +1000

    965. Univalued Binary Tree

---
+++ UnitTests/0900/S00965Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00965Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00965();
+        sut.IsUnivalTree(TreeNode.Parse("[1,1,1,1,1,null,1]")).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00965();
+        sut.IsUnivalTree(TreeNode.Parse("[2,2,2,5,2]")).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0900/S00965.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 965. Univalued Binary Tree
+// https://leetcode.com/problems/univalued-binary-tree/
+public class S00965
+{
+    public bool IsUnivalTree(TreeNode? root)
+    {
+        return helper(root, root!.val);
+    }
+
+    private static bool helper(TreeNode? root, int x)
+    {
+        if (root == null)
+        {
+            return true;
+        }
+
+        if (root.val != x)
+        {
+            return false;
+        }
+
+        return helper(root.left, x) && helper(root.right, x);
+    }
+}
