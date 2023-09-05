892b479b5fd4277792e0e0cd235440389032d429
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 00:03:17 +1000

    976. Largest Perimeter Triangle

---
+++ UnitTests/0900/S00976Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00976Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00976();
+        sut.LargestPerimeter(new[] {2, 1, 2}).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00976();
+        sut.LargestPerimeter(new[] {1, 2, 1, 10}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0900/S00976.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 976. Largest Perimeter Triangle
+// https://leetcode.com/problems/largest-perimeter-triangle/
+public class S00976
+{
+    public int LargestPerimeter(int[] nums)
+    {
+        Array.Sort(nums);
+
+        for (var i = nums.Length - 1; i >= 2; i--)
+        {
+            if (nums[i - 2] + nums[i - 1] > nums[i])
+            {
+                return nums[i - 2] + nums[i - 1] + nums[i];
+            }
+        }
+
+        return 0;
+    }
+}
