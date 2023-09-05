b6eb221c0963bf11d1176899aa130a9f2f675748
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-05 20:31:45 +1000

    908. Smallest Range I

---
+++ UnitTests/0900/S00908Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00908Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00908();
+        sut.SmallestRangeI(new[] {1}, 0).Should().Be(0);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00908();
+        sut.SmallestRangeI(new[] {0, 10}, 2).Should().Be(6);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00908();
+        sut.SmallestRangeI(new[] {1, 3, 6}, 3).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0900/S00908.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 908. Smallest Range I
+// https://leetcode.com/problems/smallest-range-i
+public class S00908
+{
+    public int SmallestRangeI(int[] nums, int k)
+    {
+        var min = nums[0];
+        var max = nums[0];
+
+        foreach (var num in nums)
+        {
+            if (num < min)
+            {
+                min = num;
+            }
+
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        return Math.Max(0, max - min - 2 * k);
+    }
+}
