f1eeca1ccfdd054295f264373c3e5f66548ba707
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-05 21:03:42 +1000

    914. X of a Kind in a Deck of Cards

---
+++ UnitTests/0900/S00914Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00914Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00914();
+        sut.HasGroupsSizeX(new[] {1, 2, 3, 4, 4, 3, 2, 1}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00914();
+        sut.HasGroupsSizeX(new[] {1, 1, 1, 2, 2, 2, 3, 3}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/0900/S00914.cs
@@ -0,0 +1,34 @@
+﻿namespace leetcode_sharp;
+
+// 914. X of a Kind in a Deck of Cards
+// https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/
+public class S00914
+{
+    public bool HasGroupsSizeX(int[] deck)
+    {
+        var dict = new Dictionary<int, int>();
+        foreach (var i in deck)
+        {
+            dict[i] = dict.GetValueOrDefault(i) + 1;
+        }
+
+        var min = dict.Values.Min();
+
+        if (min < 2)
+        {
+            return false;
+        }
+
+        for (var i = 2; i <= min; i++)
+        {
+            var flag = dict.Values.All(j => j % i == 0);
+
+            if (flag)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
