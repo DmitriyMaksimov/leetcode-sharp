5a77acf64394cd4f20b1764e04889c57965f78ab
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-05 21:06:34 +1000

    917. Reverse Only Letters

---
+++ UnitTests/0900/S00917Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00917Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00917();
+        sut.ReverseOnlyLetters("ab-cd").Should().Be("dc-ba");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00917();
+        sut.ReverseOnlyLetters("a-bC-dEf-ghIj").Should().Be("j-Ih-gfE-dCba");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00917();
+        sut.ReverseOnlyLetters("Test1ng-Leet=code-Q!").Should().Be("Qedo1ct-eeLg=ntse-T!");
+    }
+}
---
+++ leetcode-sharp/0900/S00917.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 917. Reverse Only Letters
+// https://leetcode.com/problems/reverse-only-letters/
+public class S00917
+{
+    public string ReverseOnlyLetters(string s)
+    {
+        var chars = s.ToCharArray();
+        var left = 0;
+        var right = chars.Length - 1;
+
+        while (left < right)
+        {
+            while (left < right && !char.IsLetter(chars[left]))
+            {
+                left++;
+            }
+
+            while (left < right && !char.IsLetter(chars[right]))
+            {
+                right--;
+            }
+
+            if (left < right)
+            {
+                (chars[left], chars[right]) = (chars[right], chars[left]);
+                left++;
+                right--;
+            }
+        }
+
+        return new string(chars);
+    }
+}
