1cb9f0a753b6d1c0eeb305750fed389386be5df0
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-05 21:21:47 +1000

    929. Unique Email Addresses

---
+++ UnitTests/0900/S00929Tests.cs
@@ -0,0 +1,25 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00929Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00929();
+        sut.NumUniqueEmails(new[]
+                {"test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"})
+            .Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00929();
+        sut.NumUniqueEmails(new[]
+                {"a@leetcode.com", "b@leetcode.com", "c@leetcode.com"})
+            .Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/0900/S00929.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 929. Unique Email Addresses
+// https://leetcode.com/problems/unique-email-addresses/
+public class S00929
+{
+    public int NumUniqueEmails(string[] emails)
+    {
+        var set = new HashSet<string>();
+
+        foreach (var email in emails)
+        {
+            var parts = email.Split('@');
+            var localName = parts[0].Split('+')[0].Replace(".", "");
+            set.Add($"{localName}@{parts[1]}");
+        }
+
+        return set.Count;
+    }
+}
