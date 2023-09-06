d5bef7c274b7a858ad95119f487a1bfacb51019b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 12:10:56 +1000

    1037. Valid Boomerang

---
+++ UnitTests/1000/S01037Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01037Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01037();
+        sut.IsBoomerang(new[] {new[] {1, 1}, new[] {2, 3}, new[] {3, 2}}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01037();
+        sut.IsBoomerang(new[] {new[] {1, 1}, new[] {2, 2}, new[] {3, 3}}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/1000/S01037.cs
@@ -0,0 +1,15 @@
+﻿namespace leetcode_sharp;
+
+// 1037. Valid Boomerang
+// https://leetcode.com/problems/valid-boomerang/
+public class S01037
+{
+    public bool IsBoomerang(int[][] points)
+    {
+        var p1 = points[0];
+        var p2 = points[1];
+        var p3 = points[2];
+        
+        return (p1[0] - p2[0]) * (p1[1] - p3[1]) != (p1[0] - p3[0]) * (p1[1] - p2[1]);
+    }
+}
