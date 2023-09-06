069a2e07cb82d05b28e81e7aac85158663701d9e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 11:58:58 +1000

    1018. Binary Prefix Divisible By 5

---
+++ UnitTests/1000/S01018Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01018Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01018();
+        sut.PrefixesDivBy5(new[] {0, 1, 1}).Should().Equal(true, false, false);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01018();
+        sut.PrefixesDivBy5(new[] {1, 1, 1}).Should().Equal(false, false, false);
+    }
+}
---
+++ leetcode-sharp/1000/S01018.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 1018. Binary Prefix Divisible By 5
+// https://leetcode.com/problems/binary-prefix-divisible-by-5/
+public class S01018
+{
+    public IList<bool> PrefixesDivBy5(int[] nums)
+    {
+        var result = new List<bool>();
+        var num = 0;
+        foreach (var n in nums)
+        {
+            num = (num * 2 + n) % 5;
+            result.Add(num == 0);
+        }
+
+        return result;
+    }
+}
