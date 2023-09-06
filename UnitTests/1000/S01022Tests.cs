b6a54bb7c2122769f09da2fb91f9b3a8df8a4fbd
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 20:12:54 +1000

    1022. Sum of Root To Leaf Binary Numbers

---
+++ UnitTests/1000/S01022Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01022Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01022();
+        sut.SumRootToLeaf(TreeNode.Parse("[1,0,1,0,1,0,1]")).Should().Be(22);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01022();
+        sut.SumRootToLeaf(TreeNode.Parse("[0]")).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1000/S01022.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 1022. Sum of Root To Leaf Binary Numbers
+// https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
+public class S01022
+{
+    public int SumRootToLeaf(TreeNode? root)
+    {
+        return sumRootToLeaf(root, 0);
+    }
+    
+    private static int sumRootToLeaf(TreeNode? root, int sum)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+        
+        sum = sum * 2 + root.val;
+ 
+        if (root.left == null && root.right == null)
+        {
+            return sum;
+        }
+        
+        return sumRootToLeaf(root.left, sum) + sumRootToLeaf(root.right, sum);
+    }
+}
