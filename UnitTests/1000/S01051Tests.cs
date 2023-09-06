dd338face61af0d8de9b51cf85270171cb7c7afd
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 17:26:20 +1000

    1051. Height Checker

---
+++ UnitTests/1000/S01051Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01051Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01051();
+        sut.HeightChecker(new[] {1, 1, 4, 2, 1, 3}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01051();
+        sut.HeightChecker(new[] {5, 1, 2, 3, 4}).Should().Be(5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01051();
+        sut.HeightChecker(new[] {1, 2, 3, 4, 5}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1000/S01051.cs
@@ -0,0 +1,15 @@
+﻿namespace leetcode_sharp;
+
+// 1051. Height Checker
+// https://leetcode.com/problems/height-checker/
+public class S01051
+{
+    public int HeightChecker(int[] heights)
+    {
+        var sorted = new int[heights.Length];
+        Array.Copy(heights, sorted, heights.Length);
+        Array.Sort(sorted);
+
+        return heights.Where((height, i) => height != sorted[i]).Count();
+    }
+}
