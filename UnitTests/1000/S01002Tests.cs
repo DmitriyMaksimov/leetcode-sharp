e4bfa2e304e6d8ffc2e9d4e9450cec0ed0dc822f
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 11:24:53 +1000

    1002. Find Common Characters

---
+++ UnitTests/1000/S01002Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01002Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01002();
+        sut.CommonChars(new[] {"bella", "label", "roller"}).Should().Equal("e", "l", "l");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01002();
+        sut.CommonChars(new[] {"cool", "lock", "cook"}).Should().Equal("c", "o");
+    }
+}
---
+++ leetcode-sharp/1000/S01002.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcode_sharp;
+
+// 1002. Find Common Characters
+// https://leetcode.com/problems/find-common-characters/
+public class S01002
+{
+    private const int MaxLetters = 26;
+
+    public IList<string> CommonChars(string[] words)
+    {
+        var count = new int[MaxLetters];
+        Array.Fill(count, int.MaxValue);
+
+        foreach (var word in words)
+        {
+            var count2 = new int[MaxLetters];
+            foreach (var c in word)
+            {
+                ++count2[c - 'a'];
+            }
+
+            for (var i = 0; i < MaxLetters; ++i)
+            {
+                count[i] = Math.Min(count[i], count2[i]);
+            }
+        }
+
+        var res = new List<string>();
+
+        for (var i = 0; i < MaxLetters; ++i)
+        {
+            for (var j = 0; j < count[i]; ++j)
+            {
+                res.Add(new string((char) (i + 'a'), 1));
+            }
+        }
+
+        return res;
+    }
+}
