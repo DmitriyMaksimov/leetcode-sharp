bceb929da53351821a4dd55dba66c95853d4954c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 10:12:02 +1000

    725. Split Linked List in Parts

---
+++ UnitTests/0700/S00725Tests.cs
@@ -0,0 +1,35 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00725Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00725();
+        sut.SplitListToParts(ListNode.Parse("[1,2,3]"), 5).Should().BeEquivalentTo(new[]
+            {
+                ListNode.Parse("[1]"),
+                ListNode.Parse("[2]"),
+                ListNode.Parse("[3]"),
+                null,
+                null
+            }
+        );
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00725();
+        sut.SplitListToParts(ListNode.Parse("[1,2,3,4,5,6,7,8,9,10]"), 3).Should().BeEquivalentTo(new[]
+            {
+                ListNode.Parse("[1,2,3,4]"),
+                ListNode.Parse("[5,6,7]"),
+                ListNode.Parse("[8,9,10]"),
+            }
+        );
+    }
+}
---
+++ leetcode-sharp/0700/S00725.cs
@@ -0,0 +1,48 @@
+﻿namespace leetcode_sharp;
+
+// 725. Split Linked List in Parts
+// https://leetcode.com/problems/split-linked-list-in-parts/
+public class S00725
+{
+    public ListNode?[] SplitListToParts(ListNode? head, int k)
+    {
+        var result = new ListNode?[k];
+        var length = 0;
+        var current = head;
+        while (current != null)
+        {
+            length++;
+            current = current.next;
+        }
+
+        var partLength = length / k;
+        var remainder = length % k;
+        current = head;
+        for (var i = 0; i < k; i++)
+        {
+            var part = current;
+            var partLengthTemp = partLength;
+            if (remainder > 0)
+            {
+                partLengthTemp++;
+                remainder--;
+            }
+
+            for (var j = 0; j < partLengthTemp - 1; j++)
+            {
+                current = current?.next;
+            }
+
+            if (current != null)
+            {
+                var temp = current.next;
+                current.next = null;
+                current = temp;
+            }
+
+            result[i] = part;
+        }
+
+        return result;
+    }
+}
