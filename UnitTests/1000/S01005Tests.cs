6919baf8a6a93e547325ae5f6439a2b9b29b0384
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 11:29:50 +1000

    1005. Maximize Sum Of Array After K Negations

---
+++ UnitTests/1000/S01005Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01005Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01005();
+        sut.LargestSumAfterKNegations(new[] {4, 2, 3}, 1).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01005();
+        sut.LargestSumAfterKNegations(new[] {3, -1, 0, 2}, 3).Should().Be(6);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01005();
+        sut.LargestSumAfterKNegations(new[] {2, -3, -1, 5, -4}, 2).Should().Be(13);
+    }
+}
---
+++ leetcode-sharp/1000/S01005.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 1005. Maximize Sum Of Array After K Negations
+// https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
+public class S01005
+{
+    public int LargestSumAfterKNegations(int[] nums, int k)
+    {
+        Array.Sort(nums);
+        var i = 0;
+        while (k > 0 && i < nums.Length && nums[i] < 0)
+        {
+            nums[i] = -nums[i];
+            i++;
+            k--;
+        }
+
+        Array.Sort(nums);
+
+        if (k % 2 == 1)
+        {
+            nums[0] = -nums[0];
+        }
+
+        return nums.Sum();
+    }
+}
