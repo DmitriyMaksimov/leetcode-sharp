155d00cad7578001547e9033b35f17fa56489b7b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 19:39:14 +1000

    1089. Duplicate Zeros

---
+++ UnitTests/1000/S01089Tests.cs
@@ -0,0 +1,25 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01089Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01089();
+        var arr = new[] {1, 0, 2, 3, 0, 4, 5, 0};
+        sut.DuplicateZeros(arr);
+        arr.Should().Equal(1, 0, 0, 2, 3, 0, 0, 4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01089();
+        var arr = new[] {1, 2, 3};
+        sut.DuplicateZeros(arr);
+        arr.Should().Equal(1, 2, 3);
+    }
+}
---
+++ leetcode-sharp/1000/S01089.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 1089. Duplicate Zeros
+// https://leetcode.com/problems/duplicate-zeros/
+public class S01089
+{
+    public void DuplicateZeros(int[] arr)
+    {
+        var len = arr.Length;
+        var i = 0;
+        while (i < len)
+        {
+            if (arr[i] == 0)
+            {
+                for (var j = len - 1; j > i; j--)
+                {
+                    arr[j] = arr[j - 1];
+                }
+
+                i++;
+            }
+
+            i++;
+        }
+    }
+}
