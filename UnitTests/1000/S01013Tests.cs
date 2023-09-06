5381900d93b76d8dcbf101696ec7980869de3219
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 11:48:40 +1000

    1013. Partition Array Into Three Parts With Equal Sum

---
+++ UnitTests/1000/S01013Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01013Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01013();
+        sut.CanThreePartsEqualSum(new[] {0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01013();
+        sut.CanThreePartsEqualSum(new[] {0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01013();
+        sut.CanThreePartsEqualSum(new[] {3, 3, 6, 5, -2, 2, 5, 1, -9, 4}).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/1000/S01013.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 1013. Partition Array Into Three Parts With Equal Sum
+// https://leetcode.com/problems/partition-array-into-three-parts-with-equal-sum/
+public class S01013
+{
+    public bool CanThreePartsEqualSum(int[] arr)
+    {
+        var sum = arr.Sum();
+        if (sum % 3 != 0)
+        {
+            return false;
+        }
+
+        var partSum = sum / 3;
+        var partCount = 0;
+        var currentSum = 0;
+        
+        foreach (var num in arr)
+        {
+            currentSum += num;
+            if (currentSum == partSum)
+            {
+                partCount++;
+                currentSum = 0;
+            }
+        }
+
+        return partCount >= 3;
+    }
+}
