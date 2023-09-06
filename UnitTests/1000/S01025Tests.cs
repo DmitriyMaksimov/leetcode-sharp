9b5673f27e6c4904796f8929773452d803339c29
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 12:04:02 +1000

    1025. Divisor Game

---
+++ UnitTests/1000/S01025Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01025Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01025();
+        sut.DivisorGame(2).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01025();
+        sut.DivisorGame(3).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/1000/S01025.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 1025. Divisor Game
+// https://leetcode.com/problems/divisor-game/
+public class S01025
+{
+    public bool DivisorGame(int n)
+    {
+        return n % 2 == 0;
+    }
+}
