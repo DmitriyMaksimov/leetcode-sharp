53af60540a8a914f2945fe5308d6dfd162ec83d6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 11:39:28 +1000

    1009. Complement of Base 10 Integer

---
+++ UnitTests/1000/S01009Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01009Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01009();
+        sut.BitwiseComplement(5).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01009();
+        sut.BitwiseComplement(7).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01009();
+        sut.BitwiseComplement(10).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/1000/S01009.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 1009. Complement of Base 10 Integer
+// https://leetcode.com/problems/complement-of-base-10-integer/
+public class S01009
+{
+    public int BitwiseComplement(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+
+        var mask = 1;
+        while (mask <= n)
+        {
+            mask <<= 1;
+        }
+
+        return (mask - 1) ^ n;
+    }
+}
