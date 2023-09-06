1f6a7a6672a7d13314f864a4c847426aabebfd70
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 14:39:46 +1000

    1047. Remove All Adjacent Duplicates In String

---
+++ UnitTests/1000/S01047Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01047Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01047();
+        sut.RemoveDuplicates("abbaca").Should().Be("ca");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01047();
+        sut.RemoveDuplicates("azxxzy").Should().Be("ay");
+    }
+}
---
+++ leetcode-sharp/1000/S01047.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1047. Remove All Adjacent Duplicates In String
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+public class S01047
+{
+    public string RemoveDuplicates(string s)
+    {
+        var stack = new Stack<char>();
+        foreach (var c in s)
+        {
+            if (stack.Count > 0 && stack.Peek() == c)
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(c);
+            }
+        }
+
+        var sb = new StringBuilder();
+        while (stack.Count > 0)
+        {
+            sb.Append(stack.Pop());
+        }
+
+        return new string(sb.ToString().Reverse().ToArray());
+    }
+}
