26b48b0aecbb737b850089c871c8a210d5f5cac3
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-06 19:44:25 +1000

    1078. Occurrences After Bigram

---
+++ UnitTests/1000/S01078Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01078Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01078();
+        sut.FindOcurrences("alice is a good girl she is a good student", "a", "good").Should().Equal("girl", "student");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01078();
+        sut.FindOcurrences("we will we will rock you", "we", "will").Should().Equal("we", "rock");
+    }
+}
---
+++ leetcode-sharp/1000/S01078.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 1078. Occurrences After Bigram
+// https://leetcode.com/problems/occurrences-after-bigram/
+public class S01078
+{
+    public string[] FindOcurrences(string text, string first, string second)
+    {
+        var words = text.Split(' ');
+        var result = new List<string>();
+
+        for (var i = 0; i < words.Length - 2; i++)
+        {
+            if (words[i] == first && words[i + 1] == second)
+            {
+                result.Add(words[i + 2]);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
