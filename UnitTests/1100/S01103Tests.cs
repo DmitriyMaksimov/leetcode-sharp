2541c04e2a4406f76138df68ade28250ecef0746
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-07 18:45:56 +1000

    1103. Distribute Candies to People

---
+++ UnitTests/1100/S01103Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01103Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01103();
+        sut.DistributeCandies(7, 4).Should().Equal(1, 2, 3, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01103();
+        sut.DistributeCandies(10, 3).Should().Equal(5, 2, 3);
+    }
+}
---
+++ leetcode-sharp/1100/S01103.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 1103. Distribute Candies to People
+// https://leetcode.com/problems/distribute-candies-to-people/
+public class S01103
+{
+    public int[] DistributeCandies(int candies, int num_people)
+    {
+        var result = new int[num_people];
+
+        for (var i = 0; candies > 0; i++)
+        {
+            result[i % num_people] += Math.Min(candies, i + 1);
+            candies -= i + 1;
+        }
+
+        return result;
+    }
+}
