1b9fb33b34a3e79af69458275eb74f2e8fca3729
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-07 18:50:54 +1000

    1122. Relative Sort Array

---
+++ UnitTests/1100/S01122Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01122Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01122();
+        sut.RelativeSortArray(new[] {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}, new[] {2, 1, 4, 3, 9, 6}).Should()
+            .Equal(2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01122();
+        sut.RelativeSortArray(new[] {28, 6, 22, 8, 44, 17}, new[] {22, 28, 8, 6}).Should().Equal(22, 28, 8, 6, 17, 44);
+    }
+}
---
+++ leetcode-sharp/1100/S01122.cs
@@ -0,0 +1,36 @@
+﻿namespace leetcode_sharp;
+
+// 1122. Relative Sort Array
+// https://leetcode.com/problems/relative-sort-array/
+public class S01122
+{
+    public int[] RelativeSortArray(int[] arr1, int[] arr2)
+    {
+        var dict = new Dictionary<int, int>();
+        foreach (var i in arr1)
+        {
+            dict[i] = dict.GetValueOrDefault(i) + 1;
+        }
+
+        var result = new List<int>();
+        foreach (var i in arr2)
+        {
+            for (var j = 0; j < dict[i]; j++)
+            {
+                result.Add(i);
+            }
+
+            dict.Remove(i);
+        }
+
+        foreach (var i in dict.Keys.OrderBy(x => x))
+        {
+            for (var j = 0; j < dict[i]; j++)
+            {
+                result.Add(i);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
