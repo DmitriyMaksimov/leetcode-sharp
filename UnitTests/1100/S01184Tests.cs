f65d104f58a2f15fc7277df30ed37023e7288915
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-07 21:29:28 +1000

    1184. Distance Between Bus Stops

---
+++ UnitTests/1100/S01184Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01184Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01184();
+        sut.DistanceBetweenBusStops(new[] {1, 2, 3, 4}, 0, 1).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01184();
+        sut.DistanceBetweenBusStops(new[] {1, 2, 3, 4}, 0, 2).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01184();
+        sut.DistanceBetweenBusStops(new[] {1, 2, 3, 4}, 0, 3).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/1100/S01184.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 1184. Distance Between Bus Stops
+// https://leetcode.com/problems/distance-between-bus-stops/
+public class S01184
+{
+    public int DistanceBetweenBusStops(int[] distance, int start, int destination)
+    {
+        var totalDistance = 0;
+        var distanceFromStart = 0;
+
+        for (var i = 0; i < distance.Length; i++)
+        {
+            totalDistance += distance[i];
+
+            if (i >= start && i < destination || i >= destination && i < start)
+            {
+                distanceFromStart += distance[i];
+            }
+        }
+
+        return Math.Min(distanceFromStart, totalDistance - distanceFromStart);
+    }
+}
