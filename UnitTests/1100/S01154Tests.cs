96e46a9a40d9a1295ce75f4cd5e2b8f14eefd3f2
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-07 19:04:04 +1000

    1154. Day of the Year

---
+++ UnitTests/1100/S01154Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01154Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01154();
+        sut.DayOfYear("2019-01-09").Should().Be(9);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01154();
+        sut.DayOfYear("2019-02-10").Should().Be(41);
+    }
+}
---
+++ leetcode-sharp/1100/S01154.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace leetcode_sharp;
+
+// 1154. Day of the Year
+// https://leetcode.com/problems/day-of-year/
+public class S01154
+{
+    public int DayOfYear(string date)
+    {
+        return DateOnly.ParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture).DayOfYear;
+    }
+}
