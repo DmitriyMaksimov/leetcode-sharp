1fd329afa8bd7d57ce30e336ad78a494d30f2c15
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-07 18:41:48 +1000

    1189. Maximum Number of Balloons

---
+++ UnitTests/1100/S01189Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01189Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01189();
+        sut.MaxNumberOfBalloons("nlaebolko").Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01189();
+        sut.MaxNumberOfBalloons("loonbalxballpoon").Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01189();
+        sut.MaxNumberOfBalloons("leetcode").Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1100/S01189.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 1189. Maximum Number of Balloons
+// https://leetcode.com/problems/maximum-number-of-balloons/
+public class S01189
+{
+    public int MaxNumberOfBalloons(string text)
+    {
+        var dict = new Dictionary<char, int>();
+        foreach (var c in text)
+        {
+            dict[c] = dict.GetValueOrDefault(c) + 1;
+        }
+
+        var min = int.MaxValue;
+
+        foreach (var c in "balloon")
+        {
+            if (!dict.ContainsKey(c))
+            {
+                return 0;
+            }
+
+            min = c is 'l' or 'o' ? Math.Min(min, dict[c] / 2) : Math.Min(min, dict[c]);
+        }
+
+        return min;
+    }
+}
