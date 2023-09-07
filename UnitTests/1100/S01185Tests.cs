b0b2837be0e567718af9b7e0eb3631a9360382c2
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-07 19:09:32 +1000

    1185. Day of the Week

---
+++ UnitTests/1100/S01185Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01185Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01185();
+        sut.DayOfTheWeek(31, 8, 2019).Should().Be("Saturday");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01185();
+        sut.DayOfTheWeek(18, 7, 1999).Should().Be("Sunday");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01185();
+        sut.DayOfTheWeek(15, 8, 1993).Should().Be("Sunday");
+    }
+}
---
+++ leetcode-sharp/1100/S01185.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace leetcode_sharp;
+
+// 1185. Day of the Week
+// https://leetcode.com/problems/day-of-the-week/
+public class S01185
+{
+    public string DayOfTheWeek(int day, int month, int year)
+    {
+        return new DateOnly(year, month, day).ToString("dddd", CultureInfo.InvariantCulture);
+    }
+}
