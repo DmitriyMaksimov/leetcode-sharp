e8cb9ef3b7001d42a3060449df031b234e6f049d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-07 20:57:26 +1000

    1175. Prime Arrangements

---
+++ UnitTests/1100/S01175Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01175Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01175();
+        sut.NumPrimeArrangements(5).Should().Be(12);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01175();
+        sut.NumPrimeArrangements(100).Should().Be(682289015);
+    }
+}
---
+++ leetcode-sharp/1100/S01175.cs
@@ -0,0 +1,61 @@
+﻿namespace leetcode_sharp;
+
+// 1175. Prime Arrangements
+// https://leetcode.com/problems/prime-arrangements/
+public class S01175
+{
+    public int NumPrimeArrangements(int n)
+    {
+        const int modulo = 1000000007;
+
+        var primeCount = 0;
+
+        for (var i = 2; i <= n; i++)
+        {
+            if (isPrime(i))
+            {
+                primeCount++;
+            }
+        }
+
+        var nonPrimeCount = n - primeCount;
+        var result = 1L;
+
+        for (var i = 1; i <= primeCount; i++)
+        {
+            result *= i;
+            result %= modulo;
+        }
+
+        for (var i = 1; i <= nonPrimeCount; i++)
+        {
+            result *= i;
+            result %= modulo;
+        }
+
+        return (int) result;
+    }
+
+    private static bool isPrime(int n)
+    {
+        if (n == 2)
+        {
+            return true;
+        }
+
+        if (n % 2 == 0)
+        {
+            return false;
+        }
+
+        for (var i = 3; i <= Math.Sqrt(n); i += 2)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
