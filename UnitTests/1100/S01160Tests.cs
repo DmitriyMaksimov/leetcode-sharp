a0bc68e82690b1e355809285ef5fabfe3fed4485
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-07 20:38:45 +1000

    1160. Find Words That Can Be Formed by Characters

---
+++ UnitTests/1100/S01160Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01160Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01160();
+        sut.CountCharacters(new[] {"cat", "bt", "hat", "tree"}, "atach").Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01160();
+        sut.CountCharacters(new[] {"hello", "world", "leetcode"}, "welldonehoneyr").Should().Be(10);
+    }
+}
---
+++ leetcode-sharp/1100/S01160.cs
@@ -0,0 +1,44 @@
+﻿namespace leetcode_sharp;
+
+// 1160. Find Words That Can Be Formed by Characters
+// https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
+public class S01160
+{
+    public int CountCharacters(string[] words, string chars)
+    {
+        const int maxChars = 26;
+
+        var charCounts = new int[maxChars];
+        foreach (var c in chars)
+        {
+            charCounts[c - 'a']++;
+        }
+
+        var result = 0;
+        foreach (var word in words)
+        {
+            var wordCounts = new int[maxChars];
+            foreach (var c in word)
+            {
+                wordCounts[c - 'a']++;
+            }
+
+            var isGood = true;
+            for (var i = 0; i < maxChars; i++)
+            {
+                if (wordCounts[i] > charCounts[i])
+                {
+                    isGood = false;
+                    break;
+                }
+            }
+
+            if (isGood)
+            {
+                result += word.Length;
+            }
+        }
+
+        return result;
+    }
+}
