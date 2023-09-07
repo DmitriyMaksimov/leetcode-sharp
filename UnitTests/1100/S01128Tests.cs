9d7d50ce3d78c0cd1838ea94a28d321d96924d99
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-07 18:58:20 +1000

    1128. Number of Equivalent Domino Pairs

---
+++ UnitTests/1100/S01128Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01128Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01128();
+        sut.NumEquivDominoPairs(new[] {new[] {1, 2}, new[] {2, 1}, new[] {3, 4}, new[] {5, 6}}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01128();
+        sut.NumEquivDominoPairs(new[] {new[] {1, 2}, new[] {1, 2}, new[] {1, 1}, new[] {1, 2}, new[] {2, 2}}).Should()
+            .Be(3);
+    }
+}
---
+++ leetcode-sharp/1100/S01128.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 1128. Number of Equivalent Domino Pairs
+// https://leetcode.com/problems/number-of-equivalent-domino-pairs/
+public class S01128
+{
+    public int NumEquivDominoPairs(int[][] dominoes)
+    {
+        var dict = new Dictionary<int, int>();
+        var result = 0;
+        foreach (var domino in dominoes)
+        {
+            var key = Math.Min(domino[0], domino[1]) * 10 + Math.Max(domino[0], domino[1]);
+
+            if (dict.ContainsKey(key))
+            {
+                result += dict[key];
+                dict[key]++;
+            }
+            else
+            {
+                dict.Add(key, 1);
+            }
+        }
+
+        return result;
+    }
+}
