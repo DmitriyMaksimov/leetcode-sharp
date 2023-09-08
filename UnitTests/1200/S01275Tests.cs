7beb724655396a1a339ebaa15212de6184009912
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-08 20:25:11 +1000

    1275. Find Winner on a Tic Tac Toe Game

---
+++ UnitTests/1200/S01275Tests.cs
@@ -0,0 +1,54 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01275Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01275();
+        sut.Tictactoe(new[]
+        {
+            new[] {0, 0},
+            new[] {2, 0},
+            new[] {1, 1},
+            new[] {2, 1},
+            new[] {2, 2}
+        }).Should().Be("A");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01275();
+        sut.Tictactoe(new[]
+        {
+            new[] {0, 0},
+            new[] {1, 1},
+            new[] {0, 1},
+            new[] {0, 2},
+            new[] {1, 0},
+            new[] {2, 0}
+        }).Should().Be("B");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01275();
+        sut.Tictactoe(new[]
+        {
+            new[] {0, 0},
+            new[] {1, 1},
+            new[] {2, 0},
+            new[] {1, 0},
+            new[] {1, 2},
+            new[] {2, 1},
+            new[] {0, 1},
+            new[] {0, 2},
+            new[] {2, 2}
+        }).Should().Be("Draw");
+    }
+}
---
+++ leetcode-sharp/1200/S01275.cs
@@ -0,0 +1,58 @@
+﻿namespace leetcode_sharp;
+
+// 1275. Find Winner on a Tic Tac Toe Game
+// https://leetcode.com/problems/find-winner-on-a-tic-tac-toe-game/
+public class S01275
+{
+    public string Tictactoe(int[][] moves)
+    {
+        var board = new int[3, 3];
+        var player = 1;
+
+        foreach (var move in moves)
+        {
+            board[move[0], move[1]] = player;
+            player = -player;
+        }
+
+        if (isWinner(board, 1))
+        {
+            return "A";
+        }
+
+        if (isWinner(board, -1))
+        {
+            return "B";
+        }
+
+        return moves.Length == 9 ? "Draw" : "Pending";
+    }
+
+    private static bool isWinner(int[,] board, int player)
+    {
+        for (var i = 0; i < 3; i++)
+        {
+            if (board[i, 0] == player && board[i, 1] == player && board[i, 2] == player)
+            {
+                return true;
+            }
+
+            if (board[0, i] == player && board[1, i] == player && board[2, i] == player)
+            {
+                return true;
+            }
+        }
+
+        if (board[0, 0] == player && board[1, 1] == player && board[2, 2] == player)
+        {
+            return true;
+        }
+
+        if (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
