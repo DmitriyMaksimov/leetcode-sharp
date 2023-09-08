1d82c557a503234a2f51fa74e72814e7ecaf25c4
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-08 21:22:07 +1000

    1299. Replace Elements with Greatest Element on Right Side

---
+++ UnitTests/1200/S01299Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01299Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01299();
+        sut.ReplaceElements(new[] {17, 18, 5, 4, 6, 1}).Should().Equal(18, 6, 6, 6, 1, -1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01299();
+        sut.ReplaceElements(new[] {400}).Should().Equal(-1);
+    }
+}
---
+++ leetcode-sharp/1200/S01299.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 1299. Replace Elements with Greatest Element on Right Side
+// https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+public class S01299
+{
+    public int[] ReplaceElements(int[] arr)
+    {
+        var max = -1;
+        for (var i = arr.Length - 1; i >= 0; i--)
+        {
+            var temp = arr[i];
+            arr[i] = max;
+            max = Math.Max(max, temp);
+        }
+
+        return arr;
+    }
+}
