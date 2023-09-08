6c3b4fd293423e2fac8eb2ce0479537ead4f605b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-08 20:48:49 +1000

    1287. Element Appearing More Than 25% In Sorted Array

---
+++ UnitTests/1200/S01287Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01287Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01287();
+        sut.FindSpecialInteger(new[] {1, 2, 2, 6, 6, 6, 6, 7, 10}).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01287();
+        sut.FindSpecialInteger(new[] {1, 1}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1200/S01287.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 1287. Element Appearing More Than 25% In Sorted Array
+// https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/
+public class S01287
+{
+    public int FindSpecialInteger(int[] arr)
+    {
+        var count = arr.Length / 4;
+        var current = arr[0];
+        var currentCount = 1;
+
+        for (var i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] != current)
+            {
+                current = arr[i];
+                currentCount = 1;
+            }
+            else
+            {
+                currentCount++;
+                if (currentCount > count)
+                {
+                    return current;
+                }
+            }
+        }
+
+        return current;
+    }
+}
