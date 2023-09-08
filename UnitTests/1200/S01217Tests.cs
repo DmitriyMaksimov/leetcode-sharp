165fcfa7d2f7d75e2b8d0360fd3c41ecf596ecc5
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-08 19:30:59 +1000

    1217. Minimum Cost to Move Chips to The Same Position

---
+++ UnitTests/1200/S01217Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01217Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01217();
+        sut.MinCostToMoveChips(new[] {1, 2, 3}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01217();
+        sut.MinCostToMoveChips(new[] {2, 2, 2, 3, 3}).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01217();
+        sut.MinCostToMoveChips(new[] {1, 1000000000}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1200/S01217.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 1217. Minimum Cost to Move Chips to The Same Position
+// https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/
+public class S01217
+{
+    public int MinCostToMoveChips(int[] position)
+    {
+        var odd = 0;
+        var even = 0;
+
+        foreach (var p in position)
+        {
+            if (p % 2 == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+
+        return Math.Min(odd, even);
+    }
+}
