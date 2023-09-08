c1a308cd2922b710e7a102789870cba20d7079c1
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-08 19:33:39 +1000

    1252. Cells with Odd Values in a Matrix

---
+++ UnitTests/1200/S01252Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01252Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01252();
+        sut.OddCells(2, 3, new[] {new[] {0, 1}, new[] {1, 1}}).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01252();
+        sut.OddCells(2, 2, new[] {new[] {1, 1}, new[] {0, 0}}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1200/S01252.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 1252. Cells with Odd Values in a Matrix
+// https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/
+public class S01252
+{
+    public int OddCells(int m, int n, int[][] indices)
+    {
+        var matrix = new int[m][];
+        for (var i = 0; i < m; i++)
+        {
+            matrix[i] = new int[n];
+        }
+
+        foreach (var index in indices)
+        {
+            var row = index[0];
+            var col = index[1];
+            for (var i = 0; i < n; i++)
+            {
+                matrix[row][i]++;
+            }
+
+            for (var i = 0; i < m; i++)
+            {
+                matrix[i][col]++;
+            }
+        }
+
+        return matrix.Sum(row => row.Count(col => col % 2 == 1));
+    }
+}
