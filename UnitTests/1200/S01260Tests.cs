962bf3f9b7197eb87606261da854c408cb238661
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-08 19:42:39 +1000

    1260. Shift 2D Grid

---
+++ UnitTests/1200/S01260Tests.cs
@@ -0,0 +1,33 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01260Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01260();
+        sut.ShiftGrid(new[] {new[] {1, 2, 3}, new[] {4, 5, 6}, new[] {7, 8, 9}}, 1).Should()
+            .BeEquivalentTo(new[] {new[] {9, 1, 2}, new[] {3, 4, 5}, new[] {6, 7, 8}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01260();
+        sut.ShiftGrid(new[] {new[] {3, 8, 1, 9}, new[] {19, 7, 2, 5}, new[] {4, 6, 11, 10}, new[] {12, 0, 21, 13}}, 4)
+            .Should()
+            .BeEquivalentTo(
+                new[] {new[] {12, 0, 21, 13}, new[] {3, 8, 1, 9}, new[] {19, 7, 2, 5}, new[] {4, 6, 11, 10}});
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01260();
+        sut.ShiftGrid(new[] {new[] {1, 2, 3}, new[] {4, 5, 6}, new[] {7, 8, 9}}, 9).Should()
+            .BeEquivalentTo(new[] {new[] {1, 2, 3}, new[] {4, 5, 6}, new[] {7, 8, 9}});
+    }
+}
---
+++ leetcode-sharp/1200/S01260.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 1260. Shift 2D Grid
+// https://leetcode.com/problems/shift-2d-grid/
+public class S01260
+{
+    public IList<IList<int>> ShiftGrid(int[][] grid, int k)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+
+        IList<IList<int>> result = new List<IList<int>>();
+        for (var i = 0; i < m; i++)
+        {
+            result.Add(new int[n]);
+        }
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                var index = (i * n + j + k) % (m * n);
+                var row = index / n;
+                var col = index % n;
+                result[row][col] = grid[i][j];
+            }
+        }
+
+        return result;
+    }
+}
