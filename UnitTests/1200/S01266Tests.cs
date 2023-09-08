620c0ed80b3c12249f9c4bd68ba2c37d387e8533
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-08 19:53:19 +1000

    1266. Minimum Time Visiting All Points

---
+++ UnitTests/1200/S01266Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01266Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01266();
+        sut.MinTimeToVisitAllPoints(new[] {new[] {1, 1}, new[] {3, 4}, new[] {-1, 0}}).Should().Be(7);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01266();
+        sut.MinTimeToVisitAllPoints(new[] {new[] {3, 2}, new[] {-2, 2}}).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/1200/S01266.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 1266. Minimum Time Visiting All Points
+// https://leetcode.com/problems/minimum-time-visiting-all-points/
+public class S01266
+{
+    public int MinTimeToVisitAllPoints(int[][] points)
+    {
+        var ans = 0;
+        
+        for (var i = 1; i < points.Length; ++i)
+        {
+            var cur = points[i];
+            var prev = points[i - 1];
+
+            ans += Math.Max(Math.Abs(cur[0] - prev[0]), Math.Abs(cur[1] - prev[1]));
+        }
+
+        return ans;
+    }
+}
