3c0ade9873ee0e1b65903df572e24eb486fcf449
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-08 18:35:18 +1000

    1200. Minimum Absolute Difference

---
+++ UnitTests/1200/S01200Tests.cs
@@ -0,0 +1,30 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01200Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01200();
+        sut.MinimumAbsDifference(new[] {4, 2, 1, 3}).Should()
+            .BeEquivalentTo(new[] {new[] {1, 2}, new[] {2, 3}, new[] {3, 4}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01200();
+        sut.MinimumAbsDifference(new[] {1, 3, 6, 10, 15}).Should().BeEquivalentTo(new[] {new[] {1, 3}});
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01200();
+        sut.MinimumAbsDifference(new[] {3, 8, -10, 23, 19, -4, -14, 27}).Should()
+            .BeEquivalentTo(new[] {new[] {-14, -10}, new[] {19, 23}, new[] {23, 27}});
+    }
+}
---
+++ leetcode-sharp/1200/S01200.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 1200. Minimum Absolute Difference
+// https://leetcode.com/problems/minimum-absolute-difference/
+public class S01200
+{
+    public IList<IList<int>> MinimumAbsDifference(int[] arr)
+    {
+        Array.Sort(arr);
+
+        var min = int.MaxValue;
+        
+        for (var i = 0; i < arr.Length - 1; i++)
+        {
+            var diff = Math.Abs(arr[i] - arr[i + 1]);
+            min = Math.Min(min, diff);
+        }
+
+        var ans = new List<IList<int>>();
+
+        for (var i = 0; i < arr.Length - 1; i++)
+        {
+            var diff = Math.Abs(arr[i] - arr[i + 1]);
+        
+            if (diff == min)
+            {
+                ans.Add(new List<int> {arr[i], arr[i + 1]});
+            }
+        }
+
+        return ans;
+    }
+}
