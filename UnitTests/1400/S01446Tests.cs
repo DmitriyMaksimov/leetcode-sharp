9749edd7fd7450dc9e4fbad14a169be378d94725
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 18:51:37 +1000

    1446. Consecutive Characters

---
+++ UnitTests/1400/S01446Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01446Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01446();
+        sut.MaxPower("leetcode").Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01446();
+        sut.MaxPower("abbcccddddeeeeedcba").Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/1400/S01446.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 1446. Consecutive Characters
+// https://leetcode.com/problems/consecutive-characters/
+public class S01446
+{
+    public int MaxPower(string s)
+    {
+        var max = 0;
+        var count = 0;
+        var prev = '\0';
+
+        foreach (var c in s)
+        {
+            if (c == prev)
+            {
+                count++;
+            }
+            else
+            {
+                max = Math.Max(max, count);
+                count = 1;
+                prev = c;
+            }
+        }
+
+        return Math.Max(max, count);
+    }
+}
