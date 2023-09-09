d32d44f4c25e9ae215540ee8c5ea11aa77956511
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-10 09:21:25 +1000

    1582. Special Positions in a Binary Matrix

---
+++ UnitTests/1500/S01582Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01582Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01582();
+        sut.NumSpecial(new[] {new[] {1, 0, 0}, new[] {0, 0, 1}, new[] {1, 0, 0}}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01582();
+        sut.NumSpecial(new[] {new[] {1, 0, 0}, new[] {0, 1, 0}, new[] {0, 0, 1}}).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/1500/S01582.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode_sharp;
+
+// 1582. Special Positions in a Binary Matrix
+// https://leetcode.com/problems/special-positions-in-a-binary-matrix/
+public class S01582
+{
+    public int NumSpecial(int[][] mat)
+    {
+        var rows = mat.Length;
+        var cols = mat[0].Length;
+        var rowSum = new int[rows];
+        var colSum = new int[cols];
+        var res = 0;
+
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < cols; j++)
+            {
+                rowSum[i] += mat[i][j];
+                colSum[j] += mat[i][j];
+            }
+        }
+
+        for (var i = 0; i < rows; i++)
+        {
+            if (rowSum[i] != 1)
+            {
+                continue;
+            }
+
+            for (var j = 0; j < cols; j++)
+            {
+                if (mat[i][j] == 1 && colSum[j] == 1)
+                {
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
