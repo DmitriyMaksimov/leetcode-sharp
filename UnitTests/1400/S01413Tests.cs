6b052a488172a668b3888e473b77ca00d438c26e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 17:06:48 +1000

    1413. Minimum Value to Get Positive Step by Step Sum

---
+++ UnitTests/1400/S01413Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01413Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01413();
+        sut.MinStartValue(new[] {-3, 2, -3, 4, 2}).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01413();
+        sut.MinStartValue(new[] {1, 2}).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01413();
+        sut.MinStartValue(new[] {1, -2, -3}).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/1400/S01413.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 1413. Minimum Value to Get Positive Step by Step Sum
+// https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum/
+public class S01413
+{
+    public int MinStartValue(int[] nums)
+    {
+        var min = 0;
+        var sum = 0;
+
+        foreach (var num in nums)
+        {
+            sum += num;
+            min = Math.Min(min, sum);
+        }
+
+        return 1 - min;
+    }
+}
