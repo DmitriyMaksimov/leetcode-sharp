95bbf213d820dfc82c1c4677ba6234d3fa17c5a2
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 12:04:41 +1000

    1317. Convert Integer to the Sum of Two No-Zero Integers

---
+++ UnitTests/1300/S01317Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01317Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01317();
+        sut.GetNoZeroIntegers(2).Should().Equal(1, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01317();
+        sut.GetNoZeroIntegers(11).Should().Equal(2, 9);
+    }
+}
---
+++ leetcode-sharp/1300/S01317.cs
@@ -0,0 +1,34 @@
+﻿namespace leetcode_sharp;
+
+// 1317. Convert Integer to the Sum of Two No-Zero Integers
+// https://leetcode.com/problems/convert-integer-to-the-sum-of-two-no-zero-integers/
+public class S01317
+{
+    public int[] GetNoZeroIntegers(int n)
+    {
+        for (var i = 1; i < n; i++)
+        {
+            if (hasNoZero(i) && hasNoZero(n - i))
+            {
+                return new[] {i, n - i};
+            }
+        }
+
+        return Array.Empty<int>();
+    }
+
+    private static bool hasNoZero(int n)
+    {
+        while (n > 0)
+        {
+            if (n % 10 == 0)
+            {
+                return false;
+            }
+
+            n /= 10;
+        }
+
+        return true;
+    }
+}
