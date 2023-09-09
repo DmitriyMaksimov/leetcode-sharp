33fa5eb88f85684a307a3ec1bdbd753e49a98e3c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 14:22:27 +1000

    1356. Sort Integers by The Number of 1 Bits

---
+++ UnitTests/1300/S01356Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01356Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01356();
+        sut.SortByBits(new[] {0, 1, 2, 3, 4, 5, 6, 7, 8}).Should().Equal(new[] {0, 1, 2, 4, 8, 3, 5, 6, 7});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01356();
+        sut.SortByBits(new[] {1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1}).Should()
+            .Equal(new[] {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024});
+    }
+}
---
+++ leetcode-sharp/1300/S01356.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace leetcode_sharp;
+
+// 1356. Sort Integers by The Number of 1 Bits
+// https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/
+public class S01356
+{
+    public int[] SortByBits(int[] arr)
+    {
+        Array.Sort(arr, (a, b) =>
+        {
+            var aBits = BitOperations.PopCount((uint) a);
+            var bBits = BitOperations.PopCount((uint) b);
+            return aBits == bBits ? a - b : aBits - bBits;
+        });
+        return arr;
+    }
+}
