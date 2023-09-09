72641d50d54eb6e0666e1ae2b11f82630215e468
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 19:35:01 +1000

    1450. Number of Students Doing Homework at a Given Time

---
+++ UnitTests/1400/S01450Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01450Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01450();
+        sut.BusyStudent(new[] {1, 2, 3}, new[] {3, 2, 7}, 4).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01450();
+        sut.BusyStudent(new[] {4}, new[] {4}, 4).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1400/S01450.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 1450. Number of Students Doing Homework at a Given Time
+// https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/
+public class S01450
+{
+    public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+    {
+        return startTime.Where((t, i) => t <= queryTime && queryTime <= endTime[i]).Count();
+    }
+}
