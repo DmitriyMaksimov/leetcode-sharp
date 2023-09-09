1d509ab37eff454a0eb29647af9ce19ef4d10298
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-10 09:10:16 +1000

    1576. Replace All ?'s to Avoid Consecutive Repeating Characters

---
+++ UnitTests/1500/S01576Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01576Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01576();
+        sut.ModifyString("?zs").Should().Be("azs");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01576();
+        sut.ModifyString("ubv?w").Should().Be("ubvaw");
+    }
+}
---
+++ leetcode-sharp/1500/S01576.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 1576. Replace All ?'s to Avoid Consecutive Repeating Characters
+// https://leetcode.com/problems/replace-all-s-to-avoid-consecutive-repeating-characters/
+public class S01576
+{
+    public string ModifyString(string s)
+    {
+        var chars = s.ToCharArray();
+
+        for (var i = 0; i < chars.Length; i++)
+        {
+            if (chars[i] != '?') continue;
+            
+            var left = i == 0 ? ' ' : chars[i - 1];
+            var right = i == chars.Length - 1 ? ' ' : chars[i + 1];
+            var c = 'a';
+            while (c == left || c == right)
+            {
+                c++;
+            }
+
+            chars[i] = c;
+        }
+
+        return new string(chars);
+    }
+}
