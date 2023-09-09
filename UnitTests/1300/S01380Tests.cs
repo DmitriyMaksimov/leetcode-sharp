098c4302d097587c348ad98a03c5155985fe00dd
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 14:47:32 +1000

    1380. Lucky Numbers in a Matrix

---
+++ UnitTests/1300/S01380Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01380Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01380();
+        sut.LuckyNumbers(new[] {new[] {3, 7, 8}, new[] {9, 11, 13}, new[] {15, 16, 17}}).Should().Equal(15);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01380();
+        sut.LuckyNumbers(new[] {new[] {1, 10, 4, 2}, new[] {9, 3, 8, 7}, new[] {15, 16, 17, 12}}).Should().Equal(12);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01380();
+        sut.LuckyNumbers(new[] {new[] {7, 8}, new[] {1, 2}}).Should().Equal(7);
+    }
+}
---
+++ leetcode-sharp/1300/S01380.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 1380. Lucky Numbers in a Matrix
+// https://leetcode.com/problems/lucky-numbers-in-a-matrix/
+public class S01380
+{
+    public IList<int> LuckyNumbers(int[][] matrix)
+    {
+        var rows = matrix.Length;
+        var cols = matrix[0].Length;
+
+        var minInRow = new int[rows];
+        var maxInCol = new int[cols];
+        Array.Fill(minInRow, int.MaxValue);
+        Array.Fill(maxInCol, int.MinValue);
+
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < cols; j++)
+            {
+                minInRow[i] = Math.Min(minInRow[i], matrix[i][j]);
+                maxInCol[j] = Math.Max(maxInCol[j], matrix[i][j]);
+            }
+        }
+
+        var result = new List<int>();
+
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < cols; j++)
+            {
+                if (matrix[i][j] == minInRow[i] && matrix[i][j] == maxInCol[j])
+                {
+                    result.Add(matrix[i][j]);
+                }
+            }
+        }
+
+        return result;
+    }
+}
