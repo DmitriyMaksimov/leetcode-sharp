2a0ca92c13362e6f8a90882fe8d6bd52a0b59780
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 21:40:01 +1000

    1544. Make The String Great

---
+++ UnitTests/1500/S01544Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01544Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01544();
+        sut.MakeGood("leEeetcode").Should().Be("leetcode");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01544();
+        sut.MakeGood("abBAcC").Should().Be("");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01544();
+        sut.MakeGood("s").Should().Be("s");
+    }
+}
---
+++ leetcode-sharp/1500/S01544.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1544. Make The String Great
+// https://leetcode.com/problems/make-the-string-great/
+public class S01544
+{
+    public string MakeGood(string s)
+    {
+        var stack = new Stack<char>();
+        foreach (var c in s)
+        {
+            if (stack.Count > 0 && stack.Peek() != c && char.ToLower(stack.Peek()) == char.ToLower(c))
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(c);
+            }
+        }
+
+        var sb = new StringBuilder();
+        while (stack.Count > 0)
+        {
+            sb.Append(stack.Pop());
+        }
+
+        return new string(sb.ToString().Reverse().ToArray());
+    }
+}
