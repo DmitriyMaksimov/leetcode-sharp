5b4820eba26130fe7e9cb2e1a3046112d95f4087
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 17:40:42 +1000

    1422. Maximum Score After Splitting a String

---
+++ UnitTests/1400/S01422Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01422Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01422();
+        sut.MaxScore("011101").Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01422();
+        sut.MaxScore("00111").Should().Be(5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01422();
+        sut.MaxScore("1111").Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/1400/S01422.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 1422. Maximum Score After Splitting a String
+// https://leetcode.com/problems/maximum-score-after-splitting-a-string/
+public class S01422
+{
+    public int MaxScore(string s)
+    {
+        var max = 0;
+        var left = 0;
+        var right = s.Count(t => t == '1');
+
+        foreach (var c in s)
+        {
+            if (c == '0')
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+
+            max = Math.Max(max, left + right);
+        }
+
+        return max;
+    }
+}
