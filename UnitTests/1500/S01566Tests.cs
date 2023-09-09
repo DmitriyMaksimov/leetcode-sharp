3c5aaf9199dbd6e4ae89338fb524507a3074d1e3
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-10 09:01:41 +1000

    1566. Detect Pattern of Length M Repeated K or More Times

---
+++ UnitTests/1500/S01566Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01566Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01566();
+        sut.ContainsPattern(new[] {1, 2, 4, 4, 4, 4}, 1, 3).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01566();
+        sut.ContainsPattern(new[] {1, 2, 1, 2, 1, 1, 1, 3}, 2, 2).Should().BeTrue();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01566();
+        sut.ContainsPattern(new[] {1, 2, 1, 2, 1, 3}, 2, 3).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/1500/S01566.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 1566. Detect Pattern of Length M Repeated K or More Times
+// https://leetcode.com/problems/detect-pattern-of-length-m-repeated-k-or-more-times/
+public class S01566
+{
+    public bool ContainsPattern(int[] arr, int m, int k)
+    {
+        var count = 0;
+
+        for (var i = 0; i + m < arr.Length; i++)
+        {
+            if (arr[i] != arr[i + m])
+            {
+                count = 0;
+                continue;
+            }
+
+            ++count;
+
+            if (count == (k - 1) * m)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
