4923a2709a4c9581cc236b812fd0baf7743ac767
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-10 09:33:51 +1000

    1592. Rearrange Spaces Between Words

---
+++ UnitTests/1500/S01592Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01592Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01592();
+        sut.ReorderSpaces("  this   is  a sentence ").Should().Be("this   is   a   sentence");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01592();
+        sut.ReorderSpaces(" practice   makes   perfect").Should().Be("practice   makes   perfect ");
+    }
+}
---
+++ leetcode-sharp/1500/S01592.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1592. Rearrange Spaces Between Words
+// https://leetcode.com/problems/rearrange-spaces-between-words/
+public class S01592
+{
+    public string ReorderSpaces(string text)
+    {
+        var spaces = 0;
+        var words = new List<string>();
+        var word = new StringBuilder();
+        foreach (var c in text)
+        {
+            if (c == ' ')
+            {
+                spaces++;
+                if (word.Length <= 0) continue;
+                words.Add(word.ToString());
+                word.Clear();
+            }
+            else
+            {
+                word.Append(c);
+            }
+        }
+
+        if (word.Length > 0)
+        {
+            words.Add(word.ToString());
+        }
+
+        var result = new StringBuilder();
+        if (words.Count == 1)
+        {
+            result.Append(words[0]);
+            for (var i = 0; i < spaces; i++)
+            {
+                result.Append(' ');
+            }
+        }
+        else
+        {
+            var space = spaces / (words.Count - 1);
+            var remain = spaces % (words.Count - 1);
+            for (var i = 0; i < words.Count; i++)
+            {
+                result.Append(words[i]);
+                if (i >= words.Count - 1) continue;
+                for (var j = 0; j < space; j++)
+                {
+                    result.Append(' ');
+                }
+            }
+
+            for (var i = 0; i < remain; i++)
+            {
+                result.Append(' ');
+            }
+        }
+
+        return result.ToString();
+    }
+}
