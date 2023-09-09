89bbf2753c63f8b1d06707a56ef66a69d2ca857a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 14:38:16 +1000

    1370. Increasing Decreasing String

---
+++ UnitTests/1300/S01370Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01370Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01370();
+        sut.SortString("aaaabbbbcccc").Should().Be("abccbaabccba");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01370();
+        sut.SortString("rat").Should().Be("art");
+    }
+}
---
+++ leetcode-sharp/1300/S01370.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1370. Increasing Decreasing String
+// https://leetcode.com/problems/increasing-decreasing-string/
+public class S01370
+{
+    public string SortString(string s)
+    {
+        const int maxChars = 26;
+
+        var charCount = new int[maxChars];
+        foreach (var c in s)
+        {
+            charCount[c - 'a']++;
+        }
+
+        var result = new StringBuilder();
+
+        while (result.Length < s.Length)
+        {
+            for (var i = 0; i < maxChars; i++)
+            {
+                if (charCount[i] > 0)
+                {
+                    result.Append((char)('a' + i));
+                    charCount[i]--;
+                }
+            }
+
+            for (var i = maxChars - 1; i >= 0; i--)
+            {
+                if (charCount[i] > 0)
+                {
+                    result.Append((char)('a' + i));
+                    charCount[i]--;
+                }
+            }
+        }
+
+        return result.ToString();
+    }
+}
