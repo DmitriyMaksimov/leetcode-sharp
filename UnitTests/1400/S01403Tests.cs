9d4e7d1383f19aa8a645477122c1d508939c9371
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 16:00:37 +1000

    1403. Minimum Subsequence in Non-Increasing Order

---
+++ UnitTests/1400/S01403Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01403Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01403();
+        sut.MinSubsequence(new[] {4, 3, 10, 9, 8}).Should().Equal(10, 9);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01403();
+        sut.MinSubsequence(new[] {4, 4, 7, 6, 7}).Should().Equal(7, 7, 6);
+    }
+}
---
+++ leetcode-sharp/1400/S01403.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 1403. Minimum Subsequence in Non-Increasing Order
+// https://leetcode.com/problems/minimum-subsequence-in-non-increasing-order/
+public class S01403
+{
+    public IList<int> MinSubsequence(int[] nums)
+    {
+        Array.Sort(nums);
+
+        var result = new List<int>();
+        var sum = nums.Sum();
+        var subSum = 0;
+        
+        for (var i = nums.Length - 1; i >= 0; i--)
+        {
+            subSum += nums[i];
+            result.Add(nums[i]);
+            if (subSum > sum - subSum)
+            {
+                break;
+            }
+        }
+
+        return result;
+    }
+}
