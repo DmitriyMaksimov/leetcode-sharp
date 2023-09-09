6c690dad02823ed8904fbd314531aa4f442824f6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 15:13:55 +1000

    1394. Find Lucky Integer in an Array

---
+++ UnitTests/1300/S01394Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01394Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01394();
+        sut.FindLucky(new[] {2, 2, 3, 4}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01394();
+        sut.FindLucky(new[] {1, 2, 2, 3, 3, 3}).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01394();
+        sut.FindLucky(new[] {2, 2, 2, 3, 3}).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/1300/S01394.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 
+// https://leetcode.com/problems/find-lucky-integer-in-an-array/
+public class S01394
+{
+    public int FindLucky(int[] arr)
+    {
+        var dict = new Dictionary<int, int>();
+
+        foreach (var i in arr)
+        {
+            dict[i] = dict.GetValueOrDefault(i) + 1;
+        }
+
+        var max = -1;
+        foreach (var (key, value) in dict)
+        {
+            if (key == value && key > max)
+            {
+                max = key;
+            }
+        }
+
+        return max;
+    }
+}
