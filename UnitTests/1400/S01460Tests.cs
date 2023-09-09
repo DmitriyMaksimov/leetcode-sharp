bf69f8af8cf1a8ed258cb3c05cb68e054911f4eb
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 20:06:16 +1000

    1460. Make Two Arrays Equal by Reversing Sub-arrays

---
+++ UnitTests/1400/S01460Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01460Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01460();
+        sut.CanBeEqual(new[] {1, 2, 3, 4}, new[] {2, 4, 1, 3}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01460();
+        sut.CanBeEqual(new[] {7}, new[] {7}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01460();
+        sut.CanBeEqual(new[] {3, 7, 9}, new[] {3, 7, 11}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/1400/S01460.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 1460. Make Two Arrays Equal by Reversing Sub-arrays
+// https://leetcode.com/problems/make-two-arrays-equal-by-reversing-sub-arrays/
+public class S01460
+{
+    public bool CanBeEqual(int[] target, int[] arr)
+    {
+        var targetMap = new Dictionary<int, int>();
+        var arrMap = new Dictionary<int, int>();
+
+        foreach (var i in target)
+        {
+            targetMap[i] = targetMap.GetValueOrDefault(i) + 1;
+        }
+
+        foreach (var i in arr)
+        {
+            arrMap[i] = arrMap.GetValueOrDefault(i) + 1;
+        }
+
+        return targetMap.All(i => arrMap.ContainsKey(i.Key) && arrMap[i.Key] == i.Value);
+    }
+}
