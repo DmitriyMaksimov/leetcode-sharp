0610ad14ccf5140f19ab54d7e9d4b3e1c4db1fe4
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 19:41:26 +1000

    1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ UnitTests/1400/S01455Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01455Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01455();
+        sut.IsPrefixOfWord("i love eating burger", "burg").Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01455();
+        sut.IsPrefixOfWord("this problem is an easy problem", "pro").Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01455();
+        sut.IsPrefixOfWord("i am tired", "you").Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/1400/S01455.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence
+// https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/
+public class S01455
+{
+    public int IsPrefixOfWord(string sentence, string searchWord)
+    {
+        var words = sentence.Split(' ');
+
+        for (var i = 0; i < words.Length; i++)
+        {
+            if (words[i].StartsWith(searchWord))
+            {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+}
