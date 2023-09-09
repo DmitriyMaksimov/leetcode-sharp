dd23d85b4b597451cb3292dbbb93618714dfe996
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 18:48:59 +1000

    1437. Check If All 1's Are at Least Length K Places Away

---
+++ UnitTests/1400/S01437Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01437Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01437();
+        sut.KLengthApart(new[] {1, 0, 0, 0, 1, 0, 0, 1}, 2).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01437();
+        sut.KLengthApart(new[] {1, 0, 0, 1, 0, 1}, 2).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/1400/S01437.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 1437. Check If All 1's Are at Least Length K Places Away
+// https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/
+public class S01437
+{
+    public bool KLengthApart(int[] nums, int k)
+    {
+        var last = -1;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+            {
+                if (last != -1 && i - last - 1 < k)
+                {
+                    return false;
+                }
+
+                last = i;
+            }
+        }
+
+        return true;
+    }
+}
