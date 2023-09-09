670e3f8141b7e2fbaaa7cbe3794eb69d66eaaa52
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 20:23:03 +1000

    1475. Final Prices With a Special Discount in a Shop

---
+++ UnitTests/1400/S01475Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01475Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01475();
+        sut.FinalPrices(new[] {8, 4, 6, 2, 3}).Should().Equal(4, 2, 4, 2, 3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01475();
+        sut.FinalPrices(new[] {1, 2, 3, 4, 5}).Should().Equal(1, 2, 3, 4, 5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01475();
+        sut.FinalPrices(new[] {10, 1, 1, 6}).Should().Equal(9, 0, 1, 6);
+    }
+}
---
+++ leetcode-sharp/1400/S01475.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 1475. Final Prices With a Special Discount in a Shop
+// https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+public class S01475
+{
+    public int[] FinalPrices(int[] prices)
+    {
+        var result = new int[prices.Length];
+
+        for (var i = 0; i < prices.Length; i++)
+        {
+            var discount = 0;
+            for (var j = i + 1; j < prices.Length; j++)
+            {
+                if (prices[j] <= prices[i])
+                {
+                    discount = prices[j];
+                    break;
+                }
+            }
+
+            result[i] = prices[i] - discount;
+        }
+
+        return result;
+    }
+}
