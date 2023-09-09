79b090ecc89318a47591a1ba5fdacd6b6d63ba4c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 17:03:30 +1000

    1408. String Matching in an Array

---
+++ UnitTests/1400/S01408Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01408Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01408();
+        sut.StringMatching(new[] {"mass", "as", "hero", "superhero"}).Should().Equal("as", "hero");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01408();
+        sut.StringMatching(new[] {"leetcode", "et", "code"}).Should().Equal("et", "code");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01408();
+        sut.StringMatching(new[] {"blue", "green", "bu"}).Should().BeEmpty();
+    }
+}
---
+++ leetcode-sharp/1400/S01408.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 1408. String Matching in an Array
+// https://leetcode.com/problems/string-matching-in-an-array/
+public class S01408
+{
+    public IList<string> StringMatching(string[] words)
+    {
+        var result = new HashSet<string>();
+
+        for (var i = 0; i < words.Length; i++)
+        {
+            for (var j = 0; j < words.Length; j++)
+            {
+                if (i != j && words[i].Contains(words[j]))
+                {
+                    result.Add(words[j]);
+                }
+            }
+        }
+
+        return result.ToList();
+    }
+}
