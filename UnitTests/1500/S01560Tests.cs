0547c72236800568a9dcfc3847489c08b891b670
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-10 08:51:40 +1000

    1560. Most Visited Sector in a Circular Track

---
+++ UnitTests/1500/S01560Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01560Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01560();
+        sut.MostVisited(4, new[] {1, 3, 1, 2}).Should().Equal(1, 2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01560();
+        sut.MostVisited(2, new[] {2, 1, 2, 1, 2, 1, 2, 1, 2}).Should().Equal(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01560();
+        sut.MostVisited(7, new[] {1, 3, 5, 7}).Should().Equal(1, 2, 3, 4, 5, 6, 7);
+    }
+}
---
+++ leetcode-sharp/1500/S01560.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 1560. Most Visited Sector in a Circular Track
+// https://leetcode.com/problems/most-visited-sector-in-a-circular-track/
+public class S01560
+{
+    public IList<int> MostVisited(int n, int[] rounds)
+    {
+        var result = new List<int>();
+        var start = rounds[0];
+        var end = rounds[^1];
+
+        if (start <= end)
+        {
+            for (var i = start; i <= end; i++)
+            {
+                result.Add(i);
+            }
+        }
+        else
+        {
+            for (var i = 1; i <= end; i++)
+            {
+                result.Add(i);
+            }
+
+            for (var i = start; i <= n; i++)
+            {
+                result.Add(i);
+            }
+        }
+
+        return result;
+    }
+}
