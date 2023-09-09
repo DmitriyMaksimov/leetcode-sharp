150af35b2510358b0aaa3152db7b4327670f141a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 14:17:31 +1000

    1332. Remove Palindromic Subsequences

---
+++ leetcode-sharp/1300/S01332.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 1332. Remove Palindromic Subsequences
+// https://leetcode.com/problems/remove-palindromic-subsequences/
+public class S01332
+{
+    public int RemovePalindromeSub(string s)
+    {
+        if (s.Length == 0)
+        {
+            return 0;
+        }
+
+        return isPalindrome(s) ? 1 : 2;
+    }
+    
+    private static bool isPalindrome(string s)
+    {
+        for (var i = 0; i < s.Length / 2; i++)
+        {
+            if (s[i] != s[s.Length - i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
