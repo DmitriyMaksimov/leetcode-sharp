c64c75c6262ec9997e8d1640668145a99b69e77e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 20:44:57 +1000

    1496. Path Crossing

---
+++ UnitTests/1400/S01496Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01496Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01496();
+        sut.IsPathCrossing("NES").Should().BeFalse();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01496();
+        sut.IsPathCrossing("NESWW").Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/1400/S01496.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 1496. Path Crossing
+// https://leetcode.com/problems/path-crossing/
+public class S01496
+{
+    public bool IsPathCrossing(string path)
+    {
+        var visited = new HashSet<(int x, int y)>();
+
+        (int x, int y) current = (0, 0);
+        visited.Add(current);
+
+        foreach (var c in path)
+        {
+            current = c switch
+            {
+                'N' => (current.x, current.y + 1),
+                'S' => (current.x, current.y - 1),
+                'E' => (current.x + 1, current.y),
+                'W' => (current.x - 1, current.y),
+                _ => current
+            };
+
+            if (visited.Contains(current))
+            {
+                return true;
+            }
+
+            visited.Add(current);
+        }
+
+        return false;
+    }
+}
