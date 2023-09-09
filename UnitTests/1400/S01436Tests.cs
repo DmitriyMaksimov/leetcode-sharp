a8a9c8c364b93a11e2ebfd22df383170d7869610
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 17:49:07 +1000

    1436. Destination City

---
+++ UnitTests/1400/S01436Tests.cs
@@ -0,0 +1,41 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01436Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01436();
+        sut.DestCity(new List<IList<string>>
+        {
+            new List<string> {"London", "New York"},
+            new List<string> {"New York", "Lima"},
+            new List<string> {"Lima", "Sao Paulo"}
+        }).Should().Be("Sao Paulo");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01436();
+        sut.DestCity(new List<IList<string>>
+        {
+            new List<string> {"B", "C"},
+            new List<string> {"D", "B"},
+            new List<string> {"C", "A"}
+        }).Should().Be("A");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01436();
+        sut.DestCity(new List<IList<string>>
+        {
+            new List<string> {"A", "Z"}
+        }).Should().Be("Z");
+    }
+}
---
+++ leetcode-sharp/1400/S01436.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 1436. Destination City
+// https://leetcode.com/problems/destination-city/
+public class S01436
+{
+    public string DestCity(IList<IList<string>> paths)
+    {
+        var from = new HashSet<string>();
+        var to = new HashSet<string>();
+
+        foreach (var path in paths)
+        {
+            from.Add(path[0]);
+            to.Add(path[1]);
+        }
+
+        return to.FirstOrDefault(city => !from.Contains(city), string.Empty);
+    }
+}
