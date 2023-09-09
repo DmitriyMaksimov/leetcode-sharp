5f64dd4ab496d5b676b7d2c623ab4a12fba5266d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 17:31:26 +1000

    1417. Reformat The String

---
+++ UnitTests/1400/S01417Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01417Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01417();
+        sut.Reformat("a0b1c2").Should().Be("0a1b2c");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01417();
+        sut.Reformat("leetcode").Should().Be("");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01417();
+        sut.Reformat("1229857369").Should().Be("");
+    }
+}
---
+++ leetcode-sharp/1400/S01417.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1417. Reformat The String
+// https://leetcode.com/problems/reformat-the-string/
+public class S01417
+{
+    public string Reformat(string s)
+    {
+        var digits = new List<char>();
+        var letters = new List<char>();
+
+        foreach (var c in s)
+        {
+            if (char.IsDigit(c))
+            {
+                digits.Add(c);
+            }
+            else
+            {
+                letters.Add(c);
+            }
+        }
+
+        if (Math.Abs(digits.Count - letters.Count) > 1)
+        {
+            return string.Empty;
+        }
+
+        var result = new StringBuilder();
+        var i = 0;
+        var j = 0;
+        var isDigit = digits.Count >= letters.Count;
+
+        while (i < digits.Count || j < letters.Count)
+        {
+            result.Append(isDigit ? digits[i++] : letters[j++]);
+            isDigit = !isDigit;
+        }
+
+        return result.ToString();
+    }
+}
