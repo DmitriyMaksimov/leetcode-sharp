e5a7ce5969861ecc1745448055d3dc0b7156b4da
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 21:51:30 +1000

    1550. Three Consecutive Odds

---
+++ UnitTests/1500/S01550Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01550Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01550();
+        sut.ThreeConsecutiveOdds(new[] {2, 6, 4, 1}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01550();
+        sut.ThreeConsecutiveOdds(new[] {1, 2, 34, 3, 4, 5, 7, 23, 12}).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/1500/S01550.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 1550. Three Consecutive Odds
+// https://leetcode.com/problems/three-consecutive-odds/
+public class S01550
+{
+    public bool ThreeConsecutiveOdds(int[] arr)
+    {
+        for (var i = 0; i < arr.Length - 2; i++)
+        {
+            if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
