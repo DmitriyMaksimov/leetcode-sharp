1c8c76258e72bd88d5ca773d6c718c28db6a0b3d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 21:11:14 +1000

    1518. Water Bottles

---
+++ UnitTests/1500/S01518Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01518Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01518();
+        sut.NumWaterBottles(9, 3).Should().Be(13);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01518();
+        sut.NumWaterBottles(15, 4).Should().Be(19);
+    }
+}
---
+++ leetcode-sharp/1500/S01518.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 1518. Water Bottles
+// https://leetcode.com/problems/water-bottles/
+public class S01518
+{
+    public int NumWaterBottles(int numBottles, int numExchange)
+    {
+        var result = numBottles;
+        var emptyBottles = numBottles;
+        
+        while (emptyBottles >= numExchange)
+        {
+            var newBottles = emptyBottles / numExchange;
+            result += newBottles;
+            emptyBottles = emptyBottles % numExchange + newBottles;
+        }
+
+        return result;
+    }
+}
