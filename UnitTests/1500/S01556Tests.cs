0677f935269215c4dc07993691b6454fa27d5f68
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 22:00:11 +1000

    1556. Thousand Separator

---
+++ UnitTests/1500/S01556Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01556Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01556();
+        sut.ThousandSeparator(987).Should().Be("987");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01556();
+        sut.ThousandSeparator(1234).Should().Be("1.234");
+    }
+}
---
+++ leetcode-sharp/1500/S01556.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1556. Thousand Separator
+// https://leetcode.com/problems/thousand-separator/
+public class S01556
+{
+    public string ThousandSeparator(int n)
+    {
+        var s = n.ToString();
+        var sb = new StringBuilder();
+        var count = 0;
+
+        for (var i = s.Length - 1; i >= 0; i--)
+        {
+            sb.Append(s[i]);
+            if (++count != 3 || i == 0) continue;
+            
+            sb.Append('.');
+            count = 0;
+        }
+
+        return new string(sb.ToString().ToCharArray().Reverse().ToArray());
+    }
+}
