fb42545fc27d3de8efc7a9b2548bb6c101750a85
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 14:32:46 +1000

    1360. Number of Days Between Two Dates

---
+++ UnitTests/1300/S01360Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01360Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01360();
+        sut.DaysBetweenDates("2019-06-29", "2019-06-30").Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01360();
+        sut.DaysBetweenDates("2020-01-15", "2019-12-31").Should().Be(15);
+    }
+}
---
+++ leetcode-sharp/1300/S01360.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 1360. Number of Days Between Two Dates
+// https://leetcode.com/problems/number-of-days-between-two-dates/
+public class S01360
+{
+    public int DaysBetweenDates(string date1, string date2)
+    {
+        return Math.Abs((DateTime.Parse(date1) - DateTime.Parse(date2)).Days);
+    }
+}
