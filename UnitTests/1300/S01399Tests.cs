6999761c5db892d6e1aa2a5448ac40e1ac3dbdd9
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 15:33:28 +1000

    1399. Count Largest Group

---
+++ UnitTests/1300/S01399Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01399Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01399();
+        sut.CountLargestGroup(13).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01399();
+        sut.CountLargestGroup(2).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/1300/S01399.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 1399. Count Largest Group
+// https://leetcode.com/problems/count-largest-group/
+public class S01399
+{
+    public int CountLargestGroup(int n)
+    {
+        var dict = new Dictionary<int, int>();
+
+        for (var i = 1; i <= n; i++)
+        {
+            var sum = sumOfDigits(i);
+            dict[sum] = dict.GetValueOrDefault(sum, 0) + 1;
+        }
+
+        var max = dict.Values.Max();
+        return dict.Values.Count(x => x == max);
+    }
+
+    private static int sumOfDigits(int n)
+    {
+        var sum = 0;
+        while (n > 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
