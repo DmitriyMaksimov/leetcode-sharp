1c2214e035a9c1355fac0f3a4955056797c03dc1
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-09 20:58:42 +1000

    1507. Reformat Date

---
+++ UnitTests/1500/S01507Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01507Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01507();
+        sut.ReformatDate("20th Oct 2052").Should().Be("2052-10-20");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01507();
+        sut.ReformatDate("6th Jun 1933").Should().Be("1933-06-06");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01507();
+        sut.ReformatDate("26th May 1960").Should().Be("1960-05-26");
+    }
+}
---
+++ leetcode-sharp/1500/S01507.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 1507. Reformat Date
+// https://leetcode.com/problems/reformat-date/
+public class S01507
+{
+    private readonly string[] _months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+    public string ReformatDate(string date)
+    {
+        var parts = date.Split(' ');
+
+        var day = int.Parse(parts[0][..^2]);
+        var month = Array.IndexOf(_months, parts[1]) + 1;
+        var year = parts[2];
+
+        return $"{year}-{month:D2}-{day:D2}";
+    }
+}
