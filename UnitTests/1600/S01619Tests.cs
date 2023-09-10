21b86bb1c3c5fc01f67b3ca0cb913b3f3dbeb944
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-10 10:46:07 +1000

    1619. Mean of Array After Removing Some Elements

---
+++ UnitTests/1600/S01619Tests.cs
@@ -0,0 +1,32 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01619Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01619();
+        sut.TrimMean(new[] {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01619();
+        sut.TrimMean(new[] {6, 2, 7, 5, 1, 2, 0, 3, 10, 2, 5, 0, 5, 5, 0, 8, 7, 6, 8, 0}).Should().Be(4);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01619();
+        sut.TrimMean(new[]
+        {
+            6, 0, 7, 0, 7, 5, 7, 8, 3, 4, 0, 7, 8, 1, 6, 8, 1, 1, 2, 4, 8, 1, 9, 5, 4, 3, 8, 5, 10, 8, 6, 6, 1, 0, 6,
+            10, 8, 2, 3, 4
+        }).Should().Be(4.777777777777778);
+    }
+}
---
+++ leetcode-sharp/1600/S01619.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 1619. Mean of Array After Removing Some Elements
+// https://leetcode.com/problems/mean-of-array-after-removing-some-elements/
+public class S01619
+{
+    public double TrimMean(int[] arr)
+    {
+        Array.Sort(arr);
+        var n = arr.Length;
+        
+        var start = n / 20;
+        var end = n - start;
+        var sum = 0;
+        
+        for (var i = start; i < end; i++)
+        {
+            sum += arr[i];
+        }
+        return (double)sum / (end - start);
+    }
+}
