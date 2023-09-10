b6500401dcfe9f25ad3ef12ed7c09393c3c4c03a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-10 10:01:59 +1000

    1598. Crawler Log Folder

---
+++ UnitTests/1500/S01598Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01598Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01598();
+        sut.MinOperations(new[] {"d1/", "d2/", "../", "d21/", "./"}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01598();
+        sut.MinOperations(new[] {"d1/", "d2/", "./", "d3/", "../", "d31/"}).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01598();
+        sut.MinOperations(new[] {"d1/", "../", "../", "../"}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1500/S01598.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 1598. Crawler Log Folder
+// https://leetcode.com/problems/crawler-log-folder/
+public class S01598
+{
+    public int MinOperations(string[] logs)
+    {
+        var depth = 0;
+        foreach (var log in logs)
+        {
+            if (log == "../")
+            {
+                if (depth > 0)
+                {
+                    depth--;
+                }
+            }
+            else if (log != "./")
+            {
+                depth++;
+            }
+        }
+
+        return depth;
+    }
+}
