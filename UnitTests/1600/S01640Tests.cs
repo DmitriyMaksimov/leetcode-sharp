23d26fe434e068e720d534245f20864b9a8286a8
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-10 11:05:29 +1000

    1640. Check Array Formation Through Concatenation

---
+++ UnitTests/1600/S01640Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01640Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01640();
+        sut.CanFormArray(new[] {15, 88}, new[] {new[] {88}, new[] {15}}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01640();
+        sut.CanFormArray(new[] {49, 18, 16}, new[] {new[] {16, 18, 49}}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01640();
+        sut.CanFormArray(new[] {91, 4, 64, 78}, new[] {new[] {78}, new[] {4, 64}, new[] {91}}).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/1600/S01640.cs
@@ -0,0 +1,37 @@
+﻿namespace leetcode_sharp;
+
+// 1640. Check Array Formation Through Concatenation
+// https://leetcode.com/problems/check-array-formation-through-concatenation/
+public class S01640
+{
+    public bool CanFormArray(int[] arr, int[][] pieces)
+    {
+        var map = new Dictionary<int, int[]>();
+
+        foreach (var piece in pieces)
+        {
+            map[piece[0]] = piece;
+        }
+
+        for (var i = 0; i < arr.Length;)
+        {
+            if (!map.ContainsKey(arr[i]))
+            {
+                return false;
+            }
+
+            var piece = map[arr[i]];
+            foreach (var num in piece)
+            {
+                if (num != arr[i])
+                {
+                    return false;
+                }
+
+                i++;
+            }
+        }
+
+        return true;
+    }
+}
