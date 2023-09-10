5f04aad22464650671389d8b467804954259f081
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-10 10:07:08 +1000

    1359. Count All Valid Pickup and Delivery Options

---
+++ UnitTests/1300/S01359Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01359Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01359();
+        sut.CountOrders(1).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01359();
+        sut.CountOrders(2).Should().Be(6);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01359();
+        sut.CountOrders(3).Should().Be(90);
+    }
+}
---
+++ leetcode-sharp/1300/S01359.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 1359. Count All Valid Pickup and Delivery Options
+// https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/
+public class S01359
+{
+    public int CountOrders(int n)
+    {
+        const int modulo = 1_000_000_007;
+        var res = 1L;
+
+        for (var i = 1; i <= n; ++i)
+        {
+            res = res * i * (2 * i - 1) % modulo;
+        }
+        
+        return (int)res;
+    }
+}
