78731f98a6b40087d37fe1db1f8527df306ee504
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-10 11:12:38 +1000

    1646. Get Maximum in Generated Array

---
+++ UnitTests/1600/S01646Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01646Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01646();
+        sut.GetMaximumGenerated(7).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01646();
+        sut.GetMaximumGenerated(2).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01646();
+        sut.GetMaximumGenerated(3).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/1600/S01646.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 1646. Get Maximum in Generated Array
+// https://leetcode.com/problems/get-maximum-in-generated-array/
+public class S01646
+{
+    public int GetMaximumGenerated(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+
+        var nums = new int[n + 1];
+        nums[1] = 1;
+        var max = 1;
+
+        for (var i = 2; i <= n; i++)
+        {
+            nums[i] = nums[i / 2] + i % 2 * nums[i / 2 + 1];
+            max = Math.Max(max, nums[i]);
+        }
+
+        return max;
+    }
+}
