345df748fd06bcd7e8de0ecb1f904ee68431dbe2
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-10 10:49:13 +1000

    1624. Largest Substring Between Two Equal Characters

---
+++ UnitTests/1600/S01624Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01624Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01624();
+        sut.MaxLengthBetweenEqualCharacters("aa").Should().Be(0);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01624();
+        sut.MaxLengthBetweenEqualCharacters("abca").Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01624();
+        sut.MaxLengthBetweenEqualCharacters("cbzxy").Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/1600/S01624.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 1624. Largest Substring Between Two Equal Characters
+// https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+public class S01624
+{
+    public int MaxLengthBetweenEqualCharacters(string s)
+    {
+        var max = -1;
+        
+        for (var i = 0; i < s.Length; i++)
+        {
+            var j = s.LastIndexOf(s[i]);
+            if (j > i)
+            {
+                max = Math.Max(max, j - i - 1);
+            }
+        }
+
+        return max;
+    }
+}
