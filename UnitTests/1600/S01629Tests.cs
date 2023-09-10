b496fa526062173198ec1672281fdc17599da668
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-10 11:05:44 +1000

    1629. Slowest Key

---
+++ UnitTests/1600/S01629Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01629Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01629();
+        sut.SlowestKey(new[] {9, 29, 49, 50}, "cbcd").Should().Be('c');
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01629();
+        sut.SlowestKey(new[] {12, 23, 36, 46, 62}, "spuda").Should().Be('a');
+    }
+}
---
+++ leetcode-sharp/1600/S01629.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 1629. Slowest Key
+// https://leetcode.com/problems/slowest-key/
+public class S01629
+{
+    public char SlowestKey(int[] releaseTimes, string keysPressed)
+    {
+        var maxDuration = releaseTimes[0];
+        var maxKey = keysPressed[0];
+
+        for (var i = 1; i < releaseTimes.Length; i++)
+        {
+            var duration = releaseTimes[i] - releaseTimes[i - 1];
+        
+            if (duration > maxDuration)
+            {
+                maxDuration = duration;
+                maxKey = keysPressed[i];
+            }
+            else if (duration == maxDuration)
+            {
+                maxKey = maxKey > keysPressed[i] ? maxKey : keysPressed[i];
+            }
+        }
+
+        return maxKey;
+    }
+}
