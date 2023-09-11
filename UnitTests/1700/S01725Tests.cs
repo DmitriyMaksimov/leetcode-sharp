385b44c5ab1e466e3dc660ae49d7be14870dd328
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 15:50:55 +1000

    1725. Number Of Rectangles That Can Form The Largest Square

---
+++ UnitTests/1700/S01725Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01725Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01725();
+        sut.CountGoodRectangles(new[] {new[] {5, 8}, new[] {3, 9}, new[] {5, 12}, new[] {16, 5}}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01725();
+        sut.CountGoodRectangles(new[] {new[] {2, 3}, new[] {3, 7}, new[] {4, 3}, new[] {3, 7}}).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/1700/S01725.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 1725. Number Of Rectangles That Can Form The Largest Square
+// https://leetcode.com/problems/number-of-rectangles-that-can-form-the-largest-square/
+public class S01725
+{
+    public int CountGoodRectangles(int[][] rectangles)
+    {
+        var maxLen = 0;
+        var maxLenCount = 0;
+
+        foreach (var rectangle in rectangles)
+        {
+            var len = Math.Min(rectangle[0], rectangle[1]);
+
+            if (len > maxLen)
+            {
+                maxLen = len;
+                maxLenCount = 1;
+            }
+            else if (len == maxLen)
+            {
+                maxLenCount++;
+            }
+        }
+
+        return maxLenCount;
+    }
+}
