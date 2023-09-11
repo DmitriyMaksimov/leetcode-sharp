dcab9abf129e38044ace5badea6d5323c683c587
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 15:30:43 +1000

    1710. Maximum Units on a Truck

---
+++ UnitTests/1700/S01710Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01710Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01710();
+        sut.MaximumUnits(new[] {new[] {1, 3}, new[] {2, 2}, new[] {3, 1}}, 4).Should().Be(8);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01710();
+        sut.MaximumUnits(new[] {new[] {5, 10}, new[] {2, 5}, new[] {4, 7}, new[] {3, 9}}, 10).Should().Be(91);
+    }
+}
---
+++ leetcode-sharp/1700/S01710.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 1710. Maximum Units on a Truck
+// https://leetcode.com/problems/maximum-units-on-a-truck/
+public class S01710
+{
+    public int MaximumUnits(int[][] boxTypes, int truckSize)
+    {
+        Array.Sort(boxTypes, (a, b) => b[1] - a[1]);
+        var res = 0;
+        
+        foreach (var box in boxTypes)
+        {
+            if (truckSize >= box[0])
+            {
+                res += box[0] * box[1];
+                truckSize -= box[0];
+            }
+            else
+            {
+                res += truckSize * box[1];
+                break;
+            }
+        }
+
+        return res;
+    }
+}
