7ed958ae62fc85aaf5b6b655076c8080a42f298b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 19:00:47 +1000

    1796. Second Largest Digit in a String

---
+++ UnitTests/1700/S01796Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01796Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01796();
+        sut.SecondHighest("dfa12321afd").Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01796();
+        sut.SecondHighest("abc1111").Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/1700/S01796.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 1796. Second Largest Digit in a String
+// https://leetcode.com/problems/second-largest-digit-in-a-string/
+public class S01796
+{
+    public int SecondHighest(string s)
+    {
+        var first = -1;
+        var second = -1;
+        foreach (var digit in s.Where(char.IsDigit).Select(c => c - '0'))
+        {
+            if (digit > first)
+            {
+                second = first;
+                first = digit;
+            }
+            else if (digit < first && digit > second)
+            {
+                second = digit;
+            }
+        }
+
+        return second;
+    }
+}
