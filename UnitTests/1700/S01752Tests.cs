b199c9a00f261502636c9519eb77547c7f2879d8
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 16:21:18 +1000

    1752. Check if Array Is Sorted and Rotated

---
+++ UnitTests/1700/S01752Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01752Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01752();
+        sut.Check(new[] {3, 4, 5, 1, 2}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01752();
+        sut.Check(new[] {2, 1, 3, 4}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01752();
+        sut.Check(new[] {1, 2, 3}).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/1700/S01752.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 1752. Check if Array Is Sorted and Rotated
+// https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+public class S01752
+{
+    public bool Check(int[] nums)
+    {
+        return nums.Where((t, i) => t > nums[(i + 1) % nums.Length]).Count() <= 1;
+    }
+}
