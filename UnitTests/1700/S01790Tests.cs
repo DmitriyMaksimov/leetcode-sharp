8ce5bbcab21fb08186066a44f39ca45af7cb820a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 18:27:57 +1000

    1790. Check if One String Swap Can Make Strings Equal

---
+++ UnitTests/1700/S01790Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01790Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01790();
+        sut.AreAlmostEqual("bank", "kanb").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01790();
+        sut.AreAlmostEqual("attack", "defend").Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01790();
+        sut.AreAlmostEqual("kelb", "kelb").Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/1700/S01790.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode_sharp;
+
+// 1790. Check if One String Swap Can Make Strings Equal
+// https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/
+public class S01790
+{
+    public bool AreAlmostEqual(string s1, string s2)
+    {
+        if (s1.Length != s2.Length)
+        {
+            return false;
+        }
+
+        var diff = new List<int>();
+
+        for (var i = 0; i < s1.Length; i++)
+        {
+            if (s1[i] == s2[i]) continue;
+            
+            diff.Add(i);
+            if (diff.Count > 2)
+            {
+                return false;
+            }
+        }
+
+        if (diff.Count == 0)
+        {
+            return true;
+        }
+
+        if (diff.Count != 2)
+        {
+            return false;
+        }
+
+        var i1 = diff[0];
+        var i2 = diff[1];
+
+        return s1[i1] == s2[i2] && s1[i2] == s2[i1];
+    }
+}
