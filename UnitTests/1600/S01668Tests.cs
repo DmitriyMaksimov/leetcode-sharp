c8b3399498ef7efc1c626b5a6b2f606f2428595c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 14:50:43 +1000

    1668. Maximum Repeating Substring

---
+++ UnitTests/1600/S01668Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01668Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01668();
+        sut.MaxRepeating("ababc", "ab").Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01668();
+        sut.MaxRepeating("ababc", "ba").Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01668();
+        sut.MaxRepeating("ababc", "ac").Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1600/S01668.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1668. Maximum Repeating Substring
+// https://leetcode.com/problems/maximum-repeating-substring/
+public class S01668
+{
+    public int MaxRepeating(string sequence, string word)
+    {
+        var count = 0;
+        var sb = new StringBuilder(word);
+
+        while (sequence.Contains(sb.ToString()))
+        {
+            sb.Append(word);
+            count++;
+        }
+
+        return count;
+    }
+}
