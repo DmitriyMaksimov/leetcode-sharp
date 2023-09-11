b1e873964fbdeb6250c6c5990cbc9a1940982a1a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 14:46:17 +1000

    1652. Defuse the Bomb

---
+++ UnitTests/1600/S01652Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01652Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01652();
+        sut.Decrypt(new[] {5, 7, 1, 4}, 3).Should().Equal(12, 10, 16, 13);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01652();
+        sut.Decrypt(new[] {1, 2, 3, 4}, 0).Should().Equal(0, 0, 0, 0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01652();
+        sut.Decrypt(new[] {2, 4, 9, 3}, -2).Should().Equal(12, 5, 6, 13);
+    }
+}
---
+++ leetcode-sharp/1600/S01652.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcode_sharp;
+
+// 1652. Defuse the Bomb
+// https://leetcode.com/problems/defuse-the-bomb/
+public class S01652
+{
+    public int[] Decrypt(int[] code, int k)
+    {
+        var result = new int[code.Length];
+
+        if (k == 0)
+        {
+            return result;
+        }
+
+        var sum = 0;
+        var start = 1;
+        var end = k;
+
+        if (k < 0)
+        {
+            start = code.Length + k;
+            end = code.Length - 1;
+        }
+
+        for (var i = start; i <= end; i++)
+        {
+            sum += code[i];
+        }
+
+        for (var i = 0; i < code.Length; i++)
+        {
+            result[i] = sum;
+            sum -= code[(start + i) % code.Length];
+            sum += code[(end + 1 + i) % code.Length];
+        }
+
+        return result;
+    }
+}
