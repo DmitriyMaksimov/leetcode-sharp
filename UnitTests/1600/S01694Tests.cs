cddedd158df255cff31f7ba597d393722e2c6193
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 15:02:53 +1000

    1694. Reformat Phone Number

---
+++ UnitTests/1600/S01694Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01694Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01694();
+        sut.ReformatNumber("1-23-45 6").Should().Be("123-456");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01694();
+        sut.ReformatNumber("123 4-567").Should().Be("123-45-67");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01694();
+        sut.ReformatNumber("123 4-5678").Should().Be("123-456-78");
+    }
+}
---
+++ leetcode-sharp/1600/S01694.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1694. Reformat Phone Number
+// https://leetcode.com/problems/reformat-phone-number/
+public class S01694
+{
+    public string ReformatNumber(string number)
+    {
+        var s = number.Replace(" ", "").Replace("-", "");
+        var sb = new StringBuilder();
+
+        for (var i = 0; i < s.Length;)
+        {
+            if (sb.Length > 0)
+            {
+                sb.Append('-');
+            }
+
+            if (s.Length - i == 4)
+            {
+                sb.Append(s.AsSpan(i, 2));
+                sb.Append('-');
+                sb.Append(s.AsSpan(i + 2, 2));
+                break;
+            }
+
+            if (s.Length - i == 2)
+            {
+                sb.Append(s.AsSpan(i, 2));
+                break;
+            }
+
+            sb.Append(s.AsSpan(i, 3));
+            i += 3;
+        }
+
+        return sb.ToString();
+    }
+}
