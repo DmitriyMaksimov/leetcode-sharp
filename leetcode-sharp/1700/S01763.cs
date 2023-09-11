a2e9ef72bfb12beb1a0d59e2642c921a098cb8d0
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 17:21:29 +1000

    1763. Longest Nice Substring

---
+++ leetcode-sharp/1700/S01763.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 1763. Longest Nice Substring
+// https://leetcode.com/problems/longest-nice-substring/
+public class S01763
+{
+    public string LongestNiceSubstring(string s)
+    {
+        var n = s.Length;
+        if (n == 1) return "";
+
+        var hashSet = new HashSet<char>(s.ToCharArray());
+
+        for (var i = 0; i < n; i++)
+        {
+            if (hashSet.Contains(char.ToLower(s[i])) && hashSet.Contains(char.ToUpper(s[i]))) continue;
+            var prev = LongestNiceSubstring(s[..i]); 
+            var next = LongestNiceSubstring(s[(i + 1)..]);
+
+            return prev.Length >= next.Length ? prev : next;
+        }
+
+        return s;
+    }
+}
