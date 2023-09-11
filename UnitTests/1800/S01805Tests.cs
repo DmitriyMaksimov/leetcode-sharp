996ab46a8cf32fd070da611d9c1cf5a9e5334737
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 19:10:57 +1000

    1805. Number of Different Integers in a String

---
+++ UnitTests/1800/S01805Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01805Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01805();
+        sut.NumDifferentIntegers("a123bc34d8ef34").Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01805();
+        sut.NumDifferentIntegers("leet1234code234").Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01805();
+        sut.NumDifferentIntegers("a1b01c001").Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1800/S01805.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1805. Number of Different Integers in a String
+// https://leetcode.com/problems/number-of-different-integers-in-a-string/
+public class S01805
+{
+    public int NumDifferentIntegers(string word)
+    {
+        var hashSet = new HashSet<string>();
+        var sb = new StringBuilder();
+
+        foreach (var c in word)
+        {
+            if (char.IsDigit(c))
+            {
+                sb.Append(c);
+            }
+            else
+            {
+                if (sb.Length > 0)
+                {
+                    hashSet.Add(sb.ToString().TrimStart('0'));
+                    sb.Clear();
+                }
+            }
+        }
+
+        if (sb.Length > 0)
+        {
+            hashSet.Add(sb.ToString().TrimStart('0'));
+        }
+
+        return hashSet.Count;
+    }
+}
