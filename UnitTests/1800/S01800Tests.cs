7282f48fab6439fb5320f9466324eb51170648fa
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 19:03:27 +1000

    1800. Maximum Ascending Subarray Sum

---
+++ UnitTests/1800/S01800Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01800Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01800();
+        sut.MaxAscendingSum(new[] {10, 20, 30, 5, 10, 50}).Should().Be(65);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01800();
+        sut.MaxAscendingSum(new[] {10, 20, 30, 40, 50}).Should().Be(150);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01800();
+        sut.MaxAscendingSum(new[] {12, 17, 15, 13, 10, 11, 12}).Should().Be(33);
+    }
+}
---
+++ leetcode-sharp/1800/S01800.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 1800. Maximum Ascending Subarray Sum
+// https://leetcode.com/problems/maximum-ascending-subarray-sum/
+public class S01800
+{
+    public int MaxAscendingSum(int[] nums)
+    {
+        var max = nums[0];
+        var sum = nums[0];
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                sum += nums[i];
+            }
+            else
+            {
+                sum = nums[i];
+            }
+
+            max = Math.Max(max, sum);
+        }
+
+        return max;
+    }
+}
