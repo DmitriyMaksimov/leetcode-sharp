02792d04b41c2ac081ac3a70b69b809ac05998f9
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 17:23:59 +1000

    1779. Find Nearest Point That Has the Same X or Y Coordinate

---
+++ UnitTests/1700/S01779Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01779Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01779();
+        sut.NearestValidPoint(3, 4, new[] {new[] {1, 2}, new[] {3, 1}, new[] {2, 4}, new[] {2, 3}, new[] {4, 4}})
+            .Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01779();
+        sut.NearestValidPoint(3, 4, new[] {new[] {3, 4}}).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01779();
+        sut.NearestValidPoint(3, 4, new[] {new[] {2, 3}}).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/1700/S01779.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 1779. Find Nearest Point That Has the Same X or Y Coordinate
+// https://leetcode.com/problems/find-nearest-point-that-has-the-same-x-or-y-coordinate/
+public class S01779
+{
+    public int NearestValidPoint(int x, int y, int[][] points)
+    {
+        var min = int.MaxValue;
+        var index = -1;
+
+        for (var i = 0; i < points.Length; i++)
+        {
+            var point = points[i];
+            if (point[0] == x || point[1] == y)
+            {
+                var distance = Math.Abs(point[0] - x) + Math.Abs(point[1] - y);
+                if (distance < min)
+                {
+                    min = distance;
+                    index = i;
+                }
+            }
+        }
+
+        return index;
+    }
+}
