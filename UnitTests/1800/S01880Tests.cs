9e21392fbf3f59456ef26ec1a52f312c90f06ce0
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 22:26:38 +1000

    1880. Check if Word Equals Summation of Two Words

---
+++ UnitTests/1800/S01880Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01880Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01880();
+        sut.IsSumEqual("acb", "cba", "cdb").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01880();
+        sut.IsSumEqual("aaa", "a", "aab").Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01880();
+        sut.IsSumEqual("aaa", "a", "aaaa").Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/1800/S01880.cs
@@ -0,0 +1,16 @@
+﻿namespace leetcode_sharp;
+
+// 1880. Check if Word Equals Summation of Two Words
+// https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/
+public class S01880
+{
+    public bool IsSumEqual(string firstWord, string secondWord, string targetWord)
+    {
+        return convertToInt(firstWord) + convertToInt(secondWord) == convertToInt(targetWord);
+    }
+
+    private static int convertToInt(string targetWord)
+    {
+        return targetWord.Aggregate(0, (current, c) => current * 10 + (c - 'a'));
+    }
+}
