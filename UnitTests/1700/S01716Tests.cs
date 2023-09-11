4b22d2d9d069e69663b09e8f772abe87d7821e3d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 15:41:22 +1000

    1716. Calculate Money in Leetcode Bank

---
+++ UnitTests/1700/S01716Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01716Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01716();
+        sut.TotalMoney(4).Should().Be(10);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01716();
+        sut.TotalMoney(10).Should().Be(37);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01716();
+        sut.TotalMoney(20).Should().Be(96);
+    }
+}
---
+++ leetcode-sharp/1700/S01716.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 1716. Calculate Money in Leetcode Bank
+// https://leetcode.com/problems/calculate-money-in-leetcode-bank/
+public class S01716
+{
+    public int TotalMoney(int n)
+    {
+        var weeks = n / 7;
+        var days = n % 7;
+        var sum = 0;
+
+        for (var i = 0; i < weeks; i++)
+        {
+            sum += 28 + i * 7;
+        }
+
+        for (var i = 0; i < days; i++)
+        {
+            sum += weeks + i + 1;
+        }
+
+        return sum;
+    }
+}
