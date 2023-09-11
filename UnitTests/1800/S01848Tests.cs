27ce6c930b6a3cbd0cb63a577892296b94b259c4
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 21:33:21 +1000

    1848. Minimum Distance to the Target Element

---
+++ UnitTests/1800/S01848Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01848Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01848();
+        sut.GetMinDistance(new[] {1, 2, 3, 4, 5}, 5, 3).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01848();
+        sut.GetMinDistance(new[] {1}, 1, 0).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01848();
+        sut.GetMinDistance(new[] {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 1, 0).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1800/S01848.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 1848. Minimum Distance to the Target Element
+// https://leetcode.com/problems/minimum-distance-to-the-target-element/
+public class S01848
+{
+    public int GetMinDistance(int[] nums, int target, int start)
+    {
+        var min = int.MaxValue;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                min = Math.Min(min, Math.Abs(i - start));
+            }
+        }
+
+        return min;
+    }
+}
