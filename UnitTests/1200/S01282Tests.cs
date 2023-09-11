e18c90fb7f942fd4ab4efd16611cc70d473c2733
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 10:07:22 +1000

    1282. Group the People Given the Group Size They Belong To

---
+++ UnitTests/1200/S01282Tests.cs
@@ -0,0 +1,31 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01282Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01282();
+        sut.GroupThePeople(new[] {3, 3, 3, 3, 3, 1, 3}).Should().BeEquivalentTo(new []
+        {
+            new [] {5},
+            new [] {0, 1, 2},
+            new [] {3, 4, 6},
+        });
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01282();
+        sut.GroupThePeople(new[] {2, 1, 3, 3, 3, 2}).Should().BeEquivalentTo(new []
+        {
+            new [] {1},
+            new [] {0, 5},
+            new [] {2, 3, 4}
+        });
+    }
+}
---
+++ leetcode-sharp/1200/S01282.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 1282. Group the People Given the Group Size They Belong To
+// https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to/
+public class S01282
+{
+    public IList<IList<int>> GroupThePeople(int[] groupSizes)
+    {
+        var result = new List<IList<int>>();
+        var dict = new Dictionary<int, List<int>>();
+
+        for (var i = 0; i < groupSizes.Length; i++)
+        {
+            var size = groupSizes[i];
+            if (!dict.ContainsKey(size))
+            {
+                dict.Add(size, new List<int>());
+            }
+
+            dict[size].Add(i);
+
+            if (dict[size].Count == size)
+            {
+                result.Add(dict[size]);
+                dict.Remove(size);
+            }
+        }
+
+        return result;
+    }
+}
