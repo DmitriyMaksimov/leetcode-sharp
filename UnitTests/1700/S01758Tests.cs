ca2650bdf719ff4eddfb6f9103f3c8d0d7d148ee
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 16:32:54 +1000

    1758. Minimum Changes To Make Alternating Binary String

---
+++ UnitTests/1700/S01758Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01758Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01758();
+        sut.MinOperations("0100").Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01758();
+        sut.MinOperations("10").Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01758();
+        sut.MinOperations("1111").Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/1700/S01758.cs
@@ -0,0 +1,13 @@
+﻿namespace leetcode_sharp;
+
+// 1758. Minimum Changes To Make Alternating Binary String
+// https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/
+public class S01758
+{
+    public int MinOperations(string s)
+    {
+        var count = s.Where((t, i) => t - '0' != i % 2).Count();
+
+        return Math.Min(count, s.Length - count);
+    }
+}
