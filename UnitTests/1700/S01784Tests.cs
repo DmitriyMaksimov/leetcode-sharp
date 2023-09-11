6288310140c29a8d37a261e1968b836a87ef16d4
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 18:12:29 +1000

    1784. Check if Binary String Has at Most One Segment of Ones

---
+++ UnitTests/1700/S01784Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01784Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01784();
+        sut.CheckOnesSegment("1001").Should().BeFalse();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01784();
+        sut.CheckOnesSegment("110").Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/1700/S01784.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 1784. Check if Binary String Has at Most One Segment of Ones
+// https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+public class S01784
+{
+    public bool CheckOnesSegment(string s)
+    {
+        var ones = 0;
+        var zeros = 0;
+
+        foreach (var c in s)
+        {
+            if (c == '1')
+            {
+                if (zeros > 0)
+                {
+                    return false;
+                }
+
+                ones++;
+            }
+            else
+            {
+                zeros++;
+            }
+        }
+
+        return ones > 0;
+    }
+}
