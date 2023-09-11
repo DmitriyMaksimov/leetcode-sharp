85792b6ceaae15a7dafe1e3b8506cf0b92d3c4b8
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 15:57:14 +1000

    1736. Latest chars by Replacing Hidden Digits

---
+++ UnitTests/1700/S01736Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01736Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01736();
+        sut.MaximumTime("2?:?0").Should().Be("23:50");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01736();
+        sut.MaximumTime("0?:3?").Should().Be("09:39");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01736();
+        sut.MaximumTime("1?:22").Should().Be("19:22");
+    }
+}
---
+++ leetcode-sharp/1700/S01736.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode_sharp;
+
+// 1736. Latest chars by Replacing Hidden Digits
+// https://leetcode.com/problems/latest-chars-by-replacing-hidden-digits/
+public class S01736
+{
+    public string MaximumTime(string time)
+    {
+        var chars = time.ToCharArray();
+
+        chars[0] = chars[0] != '?' ? chars[0] : (chars[1] == '?' || chars[1] <= '3') ? '2' : '1';
+        chars[1] = chars[1] != '?' ? chars[1] : chars[0] == '2' ? '3' : '9';
+        chars[3] = chars[3] != '?' ? chars[3] : '5';
+        chars[4] = chars[4] != '?' ? chars[4] : '9';
+
+        return new string(chars);
+    }
+}
