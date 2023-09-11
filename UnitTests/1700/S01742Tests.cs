6baac7779f06ea30647763b3d371abc5c141d8fa
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 16:12:47 +1000

    1742. Maximum Number of Balls in a Box

---
+++ UnitTests/1700/S01742Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01742Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01742();
+        sut.CountBalls(1, 10).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01742();
+        sut.CountBalls(5, 15).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01742();
+        sut.CountBalls(19, 28).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/1700/S01742.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 1742. Maximum Number of Balls in a Box
+// https://leetcode.com/problems/maximum-number-of-balls-in-a-box/
+public class S01742
+{
+    public int CountBalls(int lowLimit, int highLimit)
+    {
+        var dict = new Dictionary<int, int>();
+
+        for (var i = lowLimit; i <= highLimit; i++)
+        {
+            var sum = 0;
+            var n = i;
+        
+            while (n > 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            
+            dict[sum] = dict.GetValueOrDefault(sum) + 1;
+        }
+
+        return dict.Values.Max();
+    }
+}
