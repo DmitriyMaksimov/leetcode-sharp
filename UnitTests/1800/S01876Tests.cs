4e1a55df49ad44b1e84c26b8c0e9c5f59b6219d6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-11 22:22:43 +1000

    1876. Substrings of Size Three with Distinct Characters

---
+++ UnitTests/1800/S01876Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01876Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01876();
+        sut.CountGoodSubstrings("xyzzaz").Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01876();
+        sut.CountGoodSubstrings("aababcabc").Should().Be(4);
+    }
+}
