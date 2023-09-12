8322c93317be8ca777b521b6cca1264d6a49b19b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-12 15:39:16 +1000

    1952. Three Divisors

---
+++ UnitTests/1900/S01952Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01952Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01952();
+        sut.IsThree(2).Should().BeFalse();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01952();
+        sut.IsThree(4).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/1900/S01952.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 1952. Three Divisors
+// https://leetcode.com/problems/three-divisors/
+public class S01952
+{
+    public bool IsThree(int n)
+    {
+        if (n <= 2) return false; 
+
+        var count = 1;
+
+        for (var i = 2; i <= n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                count++;
+            }
+        }
+
+        return count == 2;
+    }
+}
