74d5632eebf106ee393890145c68730c6c3c8b3d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-12 17:10:57 +1000

    1957. Delete Characters to Make Fancy String

---
+++ UnitTests/1900/S01957Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01957Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01957();
+        sut.MakeFancyString("leeetcode").Should().Be("leetcode");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01957();
+        sut.MakeFancyString("aaabaaaa").Should().Be("aabaa");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01957();
+        sut.MakeFancyString("aab").Should().Be("aab");
+    }
+}
---
+++ leetcode-sharp/1900/S01957.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1957. Delete Characters to Make Fancy String
+// https://leetcode.com/problems/delete-characters-to-make-fancy-string/
+public class S01957
+{
+    public string MakeFancyString(string s)
+    {
+        var sb = new StringBuilder();
+        var count = 0;
+        var prev = ' ';
+        foreach (var c in s)
+        {
+            if (c == prev)
+            {
+                count++;
+            }
+            else
+            {
+                count = 1;
+                prev = c;
+            }
+
+            if (count < 3)
+            {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
+}
