03b709ffb0e5d89b110f22d832343ed15f4f9a7a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-12 19:39:32 +1000

    1984. Minimum Difference Between Highest and Lowest of K Scores

---
+++ UnitTests/1900/S01984Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01984Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01984();
+        sut.MinimumDifference(new[] {90}, 1).Should().Be(0);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01984();
+        sut.MinimumDifference(new[] {9, 4, 1, 7}, 2).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/1900/S01984.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 1984. Minimum Difference Between Highest and Lowest of K Scores
+// https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/
+public class S01984
+{
+    public int MinimumDifference(int[] nums, int k)
+    {
+        Array.Sort(nums);
+        
+        var min = int.MaxValue;
+
+        for (var i = 0; i <= nums.Length - k; i++)
+        {
+            min = Math.Min(min, nums[i + k - 1] - nums[i]);
+        }
+
+        return min;
+    }
+}
