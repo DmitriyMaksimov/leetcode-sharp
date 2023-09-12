4ea28191df9ee8f8ba98c2687bec992c56c452b8
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-12 17:58:33 +1000

    1961. Check If String Is a Prefix of Array

---
+++ UnitTests/1900/S01961Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01961Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01961();
+        sut.IsPrefixString("iloveleetcode", new[] {"i", "love", "leetcode", "apples"}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01961();
+        sut.IsPrefixString("iloveleetcode", new[] {"apples", "i", "love", "leetcode"}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/1900/S01961.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1961. Check If String Is a Prefix of Array
+// https://leetcode.com/problems/check-if-string-is-a-prefix-of-array/
+public class S01961
+{
+    public bool IsPrefixString(string s, string[] words)
+    {
+        var sb = new StringBuilder();
+
+        foreach (var word in words)
+        {
+            sb.Append(word);
+
+            if (sb.ToString() == s)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
