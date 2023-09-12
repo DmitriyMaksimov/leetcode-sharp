295ea2bb93f54cca64d3f3327fb8517eb29f7a7d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-12 22:19:50 +1000

    1974. Minimum Time to Type Word Using Special Typewriter

---
+++ UnitTests/1900/S01974Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01974Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01974();
+        sut.MinTimeToType("abc").Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01974();
+        sut.MinTimeToType("bza").Should().Be(7);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01974();
+        sut.MinTimeToType("zjpc").Should().Be(34);
+    }
+}
---
+++ leetcode-sharp/1900/S01974.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 1974. Minimum Time to Type Word Using Special Typewriter
+// https://leetcode.com/problems/minimum-time-to-type-word-using-special-typewriter/
+public class S01974
+{
+    public int MinTimeToType(string word)
+    {
+        const int totalChars = 26;
+        var result = 0;
+        var current = 'a';
+
+        foreach (var c in word)
+        {
+            var diff = Math.Abs(c - current);
+            result += Math.Min(diff, totalChars - diff) + 1;
+            current = c;
+        }
+
+        return result;
+    }
+}
