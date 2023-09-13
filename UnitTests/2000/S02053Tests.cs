79717d72f0ebbeb95a33116fc4f26851d9c95d5c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-13 15:32:35 +1000

    2053. Kth Distinct String

---
+++ UnitTests/2000/S02053Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02053Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02053();
+        sut.KthDistinct(new[] {"d", "b", "c", "b", "c", "a"}, 2).Should().Be("a");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02053();
+        sut.KthDistinct(new[] {"aaa", "aa", "a"}, 1).Should().Be("aaa");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02053();
+        sut.KthDistinct(new[] {"a", "b", "a"}, 3).Should().Be("");
+    }
+}
---
+++ leetcode-sharp/2000/S02053.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_sharp;
+
+// 2053. Kth Distinct String
+// https://leetcode.com/problems/kth-distinct-string/
+public class S02053
+{
+    public string KthDistinct(string[] arr, int k)
+    {
+        var dict = new Dictionary<string, int>();
+
+        foreach (var s in arr)
+        {
+            dict[s] = dict.GetValueOrDefault(s) + 1;
+        }
+
+        var distinct = dict.Where(x => x.Value == 1).Select(x => x.Key).ToArray();
+        return k > distinct.Length ? "" : distinct[k - 1];
+    }
+}
