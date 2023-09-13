d544d42bdb8a07f6660326529bb64e2fb6be8f3b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-13 18:12:27 +1000

    2068. Check if Two String Arrays are Equivalent

---
+++ UnitTests/2000/S02068Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02068Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02068();
+        sut.CheckAlmostEquivalent("aaaa", "bccb").Should().BeFalse();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02068();
+        sut.CheckAlmostEquivalent("abcdeef", "abaaacc").Should().BeTrue();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02068();
+        sut.CheckAlmostEquivalent("cccddabba", "babababab").Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/2000/S02068.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 2068. Check if Two String Arrays are Equivalent
+// https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+public class S02068
+{
+    public bool CheckAlmostEquivalent(string word1, string word2)
+    {
+        var a1 = new int[26];
+        var a2 = new int[26];
+
+        foreach (var n in word1.ToCharArray())
+        {
+            a1[n - 'a']++;
+        }
+
+        foreach (var n in word2.ToCharArray())
+        {
+            a2[n - 'a']++;
+        }
+
+        return !a1.Where((t, i) => Math.Abs(t - a2[i]) > 3).Any();
+    }
+}
