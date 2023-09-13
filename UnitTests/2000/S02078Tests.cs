de6b65f6b7934c9d0985b05d713c6b571c37d662
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-13 18:47:35 +1000

    2078. Two Furthest Houses With Different Colors

---
+++ UnitTests/2000/S02078Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02078Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02078();
+        sut.MaxDistance(new[] {1, 1, 1, 6, 1, 1, 1}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02078();
+        sut.MaxDistance(new[] {1, 8, 3, 8, 3}).Should().Be(4);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02078();
+        sut.MaxDistance(new[] {0, 1}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/2000/S02078.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 2078. Two Furthest Houses With Different Colors
+// https://leetcode.com/problems/two-furthest-houses-with-different-colors/
+public class S02078
+{
+    public int MaxDistance(int[] colors)
+    {
+        var n = colors.Length;
+        var i = 0;
+        var j = n - 1;
+
+        while (colors[0] == colors[j])
+        {
+            j--;
+        }
+
+        while (colors[n - 1] == colors[i])
+        {
+            i++;
+        }
+
+        return Math.Max(n - 1 - i, j);
+    }
+}
