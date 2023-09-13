109398b780d498a075b7b605f10e7e2e527cee1c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-13 11:40:38 +1000

    2016. Maximum Difference Between Increasing Elements

---
+++ UnitTests/2000/S02016Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02016Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02016();
+        sut.MaximumDifference(new[] {7, 1, 5, 4}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02016();
+        sut.MaximumDifference(new[] {9, 4, 3, 2}).Should().Be(-1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02016();
+        sut.MaximumDifference(new[] {1, 5, 2, 10}).Should().Be(9);
+    }
+}
---
+++ leetcode-sharp/2000/S02016.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 2016. Maximum Difference Between Increasing Elements
+// https://leetcode.com/problems/maximum-difference-between-increasing-elements/
+public class S02016
+{
+    public int MaximumDifference(int[] nums)
+    {
+        var max = -1;
+        var min = nums[0];
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > min)
+            {
+                max = Math.Max(max, nums[i] - min);
+            }
+            else
+            {
+                min = nums[i];
+            }
+        }
+
+        return max;
+    }
+}
