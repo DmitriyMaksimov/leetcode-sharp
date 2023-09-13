7769168019fca7fe72f6d8462d38c68d53b93397
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-13 11:49:46 +1000

    2022. Convert 1D Array Into 2D Array

---
+++ UnitTests/2000/S02022Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02022Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02022();
+        sut.Construct2DArray(new[] {1, 2, 3, 4}, 2, 2).Should().BeEquivalentTo(new[] {new[] {1, 2}, new[] {3, 4}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02022();
+        sut.Construct2DArray(new[] {1, 2, 3}, 1, 3).Should().BeEquivalentTo(new[] {new[] {1, 2, 3}});
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02022();
+        sut.Construct2DArray(new[] {1, 2}, 1, 1).Should().BeEquivalentTo(Array.Empty<int[]>());
+    }
+}
---
+++ leetcode-sharp/2000/S02022.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 2022. Convert 1D Array Into 2D Array
+// https://leetcode.com/problems/convert-1d-array-into-2d-array/
+public class S02022
+{
+    public int[][] Construct2DArray(int[] original, int m, int n)
+    {
+        if (original.Length != m * n)
+        {
+            return Array.Empty<int[]>();
+        }
+
+        var result = new int[m][];
+        for (var i = 0; i < m; i++)
+        {
+            result[i] = new int[n];
+            for (var j = 0; j < n; j++)
+            {
+                result[i][j] = original[i * n + j];
+            }
+        }
+
+        return result;
+    }
+}
