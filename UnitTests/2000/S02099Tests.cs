1a0f36958c13406dcc3bba08f6eace9ba56729d3
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-13 19:22:04 +1000

    2099. Find Subsequence of Length K With the Largest Sum

---
+++ UnitTests/2000/S02099Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02099Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02099();
+        sut.MaxSubsequence(new[] {2, 1, 3, 3}, 2).Should().Equal(3, 3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02099();
+        sut.MaxSubsequence(new[] {-1, -2, 3, 4}, 3).Should().Equal(-1, 3, 4);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02099();
+        sut.MaxSubsequence(new[] {3, 4, 3, 3}, 2).Should().Equal(3, 4);
+    }
+}
---
+++ leetcode-sharp/2000/S02099.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode_sharp;
+
+// 2099. Find Subsequence of Length K With the Largest Sum
+// https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+public class S02099
+{
+    public int[] MaxSubsequence(int[] nums, int k)
+    {
+        return nums
+            .Select((n, i) => new {n, i})
+            .OrderByDescending(x => x.n)
+            .Take(k)
+            .OrderBy(x => x.i)
+            .Select(x => x.n)
+            .ToArray();
+    }
+}
