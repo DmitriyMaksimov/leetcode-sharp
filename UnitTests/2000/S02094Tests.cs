a097487fb189e9eea7790bd4dca9981158841d36
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-13 19:14:22 +1000

    2094. Finding 3-Digit Even Numbers

---
+++ UnitTests/2000/S02094Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02094Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02094();
+        sut.FindEvenNumbers(new[] {2, 1, 3, 0}).Should().Equal(102, 120, 130, 132, 210, 230, 302, 310, 312, 320);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02094();
+        sut.FindEvenNumbers(new[] {2, 2, 8, 8, 2}).Should().Equal(222, 228, 282, 288, 822, 828, 882);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02094();
+        sut.FindEvenNumbers(new[] {3, 7, 5}).Should().BeEmpty();
+    }
+}
---
+++ leetcode-sharp/2000/S02094.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+
+namespace leetcode_sharp;
+
+// 2094. Finding 3-Digit Even Numbers
+// https://leetcode.com/problems/finding-3-digit-even-numbers/
+public class S02094
+{
+    public int[] FindEvenNumbers(int[] digits)
+    {
+        var result = new List<int>();
+
+        for (var num = 100; num < 999; num += 2)
+        {
+            if (canBuildNumber(num, digits))
+            {
+                result.Add(num);
+            }
+        }
+
+        return result.ToArray();
+    }
+
+    private static bool canBuildNumber(int num, IEnumerable<int> digits)
+    {
+        var ones = num % 10;
+        var tens = num / 10 % 10;
+        var hundreds = num / 100;
+
+        var onesFound = false;
+        var tensFound = false;
+        var hundredsFound = false;
+
+        foreach (var digit in digits)
+        {
+            if (!onesFound && ones == digit)
+            {
+                onesFound = true;
+            }
+            else if (!tensFound && tens == digit)
+            {
+                tensFound = true;
+            }
+            else if (!hundredsFound && hundreds == digit)
+            {
+                hundredsFound = true;
+            }
+                
+            if (onesFound && tensFound && hundredsFound)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
