2c0de78bcee0f1beaf76fd7815a04cc5dfd763af
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-13 11:58:58 +1000

    2027. Minimum Moves to Convert String

---
+++ UnitTests/2000/S02027Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02027Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02027();
+        sut.MinimumMoves("XXX").Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02027();
+        sut.MinimumMoves("XXOX").Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02027();
+        sut.MinimumMoves("OOOO").Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2000/S02027.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 2027. Minimum Moves to Convert String
+// https://leetcode.com/problems/minimum-moves-to-convert-string/
+public class S02027
+{
+    public int MinimumMoves(string s)
+    {
+        var count = 0;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] != 'X') continue;
+            
+            count++;
+            i += 2;
+        }
+
+        return count;
+    }
+}
