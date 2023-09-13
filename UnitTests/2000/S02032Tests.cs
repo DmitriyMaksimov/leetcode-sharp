3e215e869aa77f49ab29bf35e119596715856513
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-13 13:02:05 +1000

    2032. Two Out of Three

---
+++ UnitTests/2000/S02032Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02032Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02032();
+        sut.TwoOutOfThree(new[] {1, 1, 3, 2}, new[] {2, 3}, new[] {3}).Should().BeEquivalentTo(new[] {3, 2});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02032();
+        sut.TwoOutOfThree(new[] {3, 1}, new[] {2, 3}, new[] {1, 2}).Should().BeEquivalentTo(new[] {2, 3, 1});
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02032();
+        sut.TwoOutOfThree(new[] {1, 2, 2}, new[] {4, 3, 3}, new[] {5}).Should().BeEquivalentTo(Array.Empty<int>());
+    }
+}
---
+++ leetcode-sharp/2000/S02032.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 2032. Two Out of Three
+// https://leetcode.com/problems/two-out-of-three/
+public class S02032
+{
+    public IList<int> TwoOutOfThree(int[] nums1, int[] nums2, int[] nums3)
+    {
+        var result = new HashSet<int>();
+        var set1 = new HashSet<int>(nums1);
+        var set2 = new HashSet<int>(nums2);
+        var set3 = new HashSet<int>(nums3);
+
+        foreach (var num in set1.Where(num => set2.Contains(num) || set3.Contains(num)))
+        {
+            result.Add(num);
+        }
+
+        foreach (var num in set2.Where(num => set1.Contains(num) || set3.Contains(num)))
+        {
+            result.Add(num);
+        }
+
+        foreach (var num in set3.Where(num => set1.Contains(num) || set2.Contains(num)))
+        {
+            result.Add(num);
+        }
+
+        return result.ToArray();
+    }
+}
