c4947e13fbb42fbfead7e9d22c6c7eadf846c065
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-13 18:39:46 +1000

    2073. Time Needed to Buy Tickets

---
+++ UnitTests/2000/S02073Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02073Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02073();
+        sut.TimeRequiredToBuy(new[] {2, 3, 2}, 2).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02073();
+        sut.TimeRequiredToBuy(new[] {5, 1, 1, 1}, 0).Should().Be(8);
+    }
+}
---
+++ leetcode-sharp/2000/S02073.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 2073. Time Needed to Buy Tickets
+// https://leetcode.com/problems/time-needed-to-buy-tickets/
+public class S02073
+{
+    public int TimeRequiredToBuy(int[] tickets, int k)
+    {
+        var amount = 0;
+        var amountToRemove = 0;
+        
+        for (var i = 0; i < tickets.Length; i++)
+        {
+            if (tickets[k] <= tickets[i])
+            {
+                if (i > k)
+                {
+                    amountToRemove++;
+                }
+
+                amount += tickets[k];
+            }
+            else
+            {
+                amount += tickets[i];
+            }
+        }
+
+        return amount - amountToRemove;
+    }
+}
