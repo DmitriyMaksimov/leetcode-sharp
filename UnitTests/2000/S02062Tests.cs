97f240a96dba1bfb17b0120a6eba5411003a4383
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-13 15:43:59 +1000

    2062. Count Vowel Substrings of a String

---
+++ UnitTests/2000/S02062Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02062Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02062();
+        sut.CountVowelSubstrings("aeiouu").Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02062();
+        sut.CountVowelSubstrings("unicornarihan").Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02062();
+        sut.CountVowelSubstrings("cuaieuouac").Should().Be(7);
+    }
+}
---
+++ leetcode-sharp/2000/S02062.cs
@@ -0,0 +1,34 @@
+﻿namespace leetcode_sharp;
+
+// 2062. Count Vowel Substrings of a String
+// https://leetcode.com/problems/count-vowel-substrings-of-a-string/
+public class S02062
+{
+    public int CountVowelSubstrings(string word)
+    {
+        if (word.Length < 5 || word.GroupBy(c => c).Select(g => g.Key).Distinct().Count() < 5)
+        {
+            return 0;
+        }
+
+        var cnt = 0;
+
+        for (var i = 0; i < word.Length - 4; i++)
+        {
+            for (var j = 0; j < word.Length - i - 4; j++)
+            {
+                if (isValidSubstring(word[i..(i + j + 5)]))
+                {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+
+    private static bool isValidSubstring(string w)
+    {
+        return w.Contains('a') && w.Contains('e') && w.Contains('i') && w.Contains('o') && w.Contains('u') && w.Distinct().Count() == 5;
+    }
+}
