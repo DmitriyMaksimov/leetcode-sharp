395d0e3c80a1fc72604f5237f3332279f49f5095
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-14 17:30:19 +1000

    2154. Keep Multiplying Found Values by Two

---
+++ UnitTests/2100/S02154Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02154Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02154();
+        sut.FindFinalValue(new[] {5, 3, 6, 1, 12}, 3).Should().Be(24);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02154();
+        sut.FindFinalValue(new[] {2, 7, 9}, 4).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/2100/S02154.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 2154. Keep Multiplying Found Values by Two
+// https://leetcode.com/problems/keep-multiplying-found-values-by-two/
+public class S02154
+{
+    public int FindFinalValue(int[] nums, int original)
+    {
+        var hashSet = new HashSet<int>(nums);
+
+        var valid = true;
+
+        while (valid)
+        {
+            if (hashSet.Contains(original))
+            {
+                valid = true;
+                original *= 2;
+            }
+            else
+            {
+                valid = false;
+            }
+        }
+
+        return original;
+    }
+}
