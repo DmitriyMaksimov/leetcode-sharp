c7fd9aebb46ed1402d5ef375d788c8a5443182f2
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-14 18:59:46 +1000

    2190. Most Frequent Number Following Key In an Array

---
+++ UnitTests/2100/S02190Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02190Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02190();
+        sut.MostFrequent(new[] {1, 100, 200, 1, 100}, 1).Should().Be(100);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02190();
+        sut.MostFrequent(new[] {2, 2, 2, 2, 3}, 2).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/2100/S02190.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 2190. Most Frequent Number Following Key In an Array
+// https://leetcode.com/problems/most-frequent-number-following-key-in-an-array/
+public class S02190
+{
+    public int MostFrequent(int[] nums, int key)
+    {
+        var dict = new Dictionary<int, int>();
+        var max = int.MinValue;
+
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] != key) continue;
+            
+            var target = nums[i + 1];
+            dict[target] = dict.GetValueOrDefault(target) + 1;
+            max = Math.Max(max, dict[target]);
+        }
+
+        return dict.First(kvp => kvp.Value == max).Key;
+    }
+}
