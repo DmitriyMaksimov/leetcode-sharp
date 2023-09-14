2801315c8b4acf367e153071b4d2dd25f7b04010
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-14 15:06:08 +1000

    2144. Minimum Cost of Buying Candies With Discount

---
+++ UnitTests/2100/S02144Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02144Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02144();
+        sut.MinimumCost(new[] {1, 2, 3}).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02144();
+        sut.MinimumCost(new[] {6, 5, 7, 9, 2, 2}).Should().Be(23);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02144();
+        sut.MinimumCost(new[] {5, 5}).Should().Be(10);
+    }
+}
---
+++ leetcode-sharp/2100/S02144.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 2144. Minimum Cost of Buying Candies With Discount
+// https://leetcode.com/problems/minimum-cost-of-buying-candies-with-discount/
+public class S02144
+{
+    public int MinimumCost(int[] cost)
+    {
+        Array.Sort(cost);
+
+        var sum = 0;
+ 
+        for (int i = cost.Length - 1, count = 1; i >= 0; i--, ++count)
+        {
+            if (count % 3 != 0)
+            {
+                sum += cost[i];
+            }
+        }
+
+        return sum;
+    }
+}
