d1ec94be4091e628396575169dc673a030d6087e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-14 18:43:45 +1000

    2180. Count Integers With Even Digit Sum

---
+++ UnitTests/2100/S02180Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02180Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02180();
+        sut.CountEven(4).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02180();
+        sut.CountEven(30).Should().Be(14);
+    }
+}
---
+++ leetcode-sharp/2100/S02180.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2180. Count Integers With Even Digit Sum
+// https://leetcode.com/problems/count-integers-with-even-digit-sum/
+public class S02180
+{
+    public int CountEven(int num)
+    {
+        return Enumerable.Range(1, num).Count(i => i.ToString().Sum(c => c - '0') % 2 == 0);
+    }
+}
