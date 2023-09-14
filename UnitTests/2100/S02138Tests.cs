7366fb0c248f8272851af750246cd0d8bb0cc085
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-14 12:32:10 +1000

    2138. Divide a String Into Groups of Size k

---
+++ UnitTests/2100/S02138Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02138Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02138();
+        sut.DivideString("abcdefghi", 3, 'x').Should().Equal("abc", "def", "ghi");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02138();
+        sut.DivideString("abcdefghij", 3, 'x').Should().Equal("abc", "def", "ghi", "jxx");
+    }
+    
+    [Test]
+    public void T3()
+    {
+        var sut = new S02138();
+        sut.DivideString("ctoyjrwtngqwt", 8, 'n').Should().Equal("ctoyjrwt", "ngqwtnnn");
+    }
+}
---
+++ leetcode-sharp/2100/S02138.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2138. Divide a String Into Groups of Size k
+// https://leetcode.com/problems/divide-a-string-into-groups-of-size-k/
+public class S02138
+{
+    public string[] DivideString(string s, int k, char fill)
+    {
+        return s.Chunk(k).Select(x => new string(x) + new string(fill, k - x.Length)).ToArray();
+    }
+}
