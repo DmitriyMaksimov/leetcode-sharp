53cb8ba09b2108b5ee885f41442a342f61b82274
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-14 15:20:44 +1000

    2148. Count Elements With Strictly Smaller and Greater Elements

---
+++ UnitTests/2100/S02148Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02148Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02148();
+        sut.CountElements(new[] {11, 7, 2, 15}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02148();
+        sut.CountElements(new[] {-3, 3, 3, 90}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/2100/S02148.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 2148. Count Elements With Strictly Smaller and Greater Elements
+// https://leetcode.com/problems/count-elements-with-strictly-smaller-and-greater-elements/
+public class S02148
+{
+    public int CountElements(int[] nums)
+    {
+        if (nums.Length < 3)
+        {
+            return 0;
+        }
+
+        var arr = minMax(nums);
+
+        return nums.Count(num => num != arr.min && num != arr.max);
+    }
+
+    private static (int min, int max) minMax(IReadOnlyList<int> arr)
+    {
+        var max = arr[0];
+        var min = arr[0];
+
+        foreach (var n in arr)
+        {
+            max = Math.Max(max, n);
+            min = Math.Min(min, n);
+        }
+
+        return (min, max);
+    }
+}
