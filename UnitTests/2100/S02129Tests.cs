bce6fe015cc7880cabf79fe59a6b944216397e35
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-14 12:11:01 +1000

    2129. Capitalize the Title

---
+++ UnitTests/2100/S02129Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02129Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02129();
+        sut.CapitalizeTitle("capiTalIze tHe titLe").Should().Be("Capitalize The Title");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02129();
+        sut.CapitalizeTitle("First leTTeR of EACH Word").Should().Be("First Letter of Each Word");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02129();
+        sut.CapitalizeTitle("i lOve leetcode").Should().Be("i Love Leetcode");
+    }
+}
---
+++ leetcode-sharp/2100/S02129.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 2129. Capitalize the Title
+// https://leetcode.com/problems/capitalize-the-title/
+public class S02129
+{
+    public string CapitalizeTitle(string title)
+    {
+        var sb = new StringBuilder();
+
+        foreach (var word in title.Split(' '))
+        {
+            if (word.Length < 3)
+            {
+                sb.Append(word.ToLower());
+            }
+            else
+            {
+                sb.Append(char.ToUpper(word[0]));
+                sb.Append(word[1..].ToLower());
+            }
+
+            sb.Append(' ');
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
