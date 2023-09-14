cd3438738bb07ccc75c760611fe93aaaba144435
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-14 18:39:58 +1000

    2169. Count Operations to Obtain Zero

---
+++ UnitTests/2100/S02169Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02169Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02169();
+        sut.CountOperations(1, 3).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02169();
+        sut.CountOperations(10, 10).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/2100/S02169.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 2169. Count Operations to Obtain Zero
+// https://leetcode.com/problems/count-operations-to-obtain-zero/
+public class S02169
+{
+    public int CountOperations(int num1, int num2)
+    {
+        var count = 0;
+
+        while (num1 > 0 && num2 > 0)
+        {
+            if (num1 >= num2)
+            {
+                num1 -= num2;
+            }
+            else
+            {
+                num2 -= num1;
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+}
