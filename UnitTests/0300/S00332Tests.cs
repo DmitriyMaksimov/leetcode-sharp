1322c7c36a73e4c515ff97697fca73a99a2ca7cd
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-14 11:55:16 +1000

    332. Reconstruct Itinerary

---
+++ UnitTests/0300/S00332Tests.cs
@@ -0,0 +1,30 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00332Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00332();
+        sut.FindItinerary(
+                new IList<string>[]
+                    {new[] {"MUC", "LHR"}, new[] {"JFK", "MUC"}, new[] {"SFO", "SJC"}, new[] {"LHR", "SFO"}}).Should()
+            .Equal("JFK", "MUC", "LHR", "SFO", "SJC");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00332();
+        sut.FindItinerary(
+                new IList<string>[]
+                {
+                    new[] {"JFK", "SFO"}, new[] {"JFK", "ATL"}, new[] {"SFO", "ATL"}, new[] {"ATL", "JFK"},
+                    new[] {"ATL", "SFO"}
+                }).Should()
+            .Equal("JFK", "ATL", "JFK", "SFO", "ATL", "SFO");
+    }
+}
---
+++ leetcode-sharp/0300/S00332.cs
@@ -0,0 +1,45 @@
+﻿namespace leetcode_sharp;
+
+// 332. Reconstruct Itinerary
+// https://leetcode.com/problems/reconstruct-itinerary/
+public class S00332
+{
+    public IList<string> FindItinerary(IList<IList<string>> tickets)
+    {
+        var graph = new Dictionary<string, List<string>>();
+
+        foreach (var ticket in tickets)
+        {
+            if (!graph.ContainsKey(ticket[0]))
+            {
+                graph[ticket[0]] = new List<string>();
+            }
+
+            graph[ticket[0]].Add(ticket[1]);
+        }
+
+        foreach (var destinations in graph.Values)
+        {
+            destinations.Sort((a, b) => b.CompareTo(a));
+        }
+
+        var itinerary = new List<string>();
+
+        dfs("JFK", graph, itinerary);
+        itinerary.Reverse();
+
+        return itinerary;
+    }
+
+    private static void dfs(string airport, Dictionary<string, List<string>> graph, ICollection<string> itinerary)
+    {
+        while (graph.ContainsKey(airport) && graph[airport].Count > 0)
+        {
+            var next = graph[airport][^1];
+            graph[airport].RemoveAt(graph[airport].Count - 1);
+            dfs(next, graph, itinerary);
+        }
+
+        itinerary.Add(airport);
+    }
+}
