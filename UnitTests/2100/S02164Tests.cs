7060a7022bcee1d05f981e68827c9b642c8fa559
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-14 17:55:33 +1000

    2164. Sort Even and Odd Indices Independently

---
+++ UnitTests/2100/S02164Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02164Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02164();
+        sut.SortEvenOdd(new[] {4, 1, 2, 3}).Should().Equal(2, 3, 4, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02164();
+        sut.SortEvenOdd(new[] {2, 1}).Should().Equal(2, 1);
+    }
+}
