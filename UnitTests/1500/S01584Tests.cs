5e956e2897b6cc346df0ded92e90209fc9f0da0f
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-15 11:35:05 +1000

    1584. Min Cost to Connect All Points

---
+++ UnitTests/1500/S01584Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01584Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01584();
+        sut.MinCostConnectPoints(new[] {new[] {0, 0}, new[] {2, 2}, new[] {3, 10}, new[] {5, 2}, new[] {7, 0}}).Should()
+            .Be(20);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01584();
+        sut.MinCostConnectPoints(new[] {new[] {3, 12}, new[] {-2, 5}, new[] {-4, 1}}).Should().Be(18);
+    }
+}
---
+++ leetcode-sharp/1500/S01584.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode_sharp;
+
+// 1584. Min Cost to Connect All Points
+// https://leetcode.com/problems/min-cost-to-connect-all-points/
+public class S01584
+{
+    private readonly HashSet<int[]> _visited = new();
+    private readonly PriorityQueue<(int[] point, int wt), int> _heap = new();
+
+    public int MinCostConnectPoints(int[][] points)
+    {
+        _heap.Enqueue((points[0], 0), 0);
+
+        var total = 0;
+        while (_heap.Count > 0 && _visited.Count < points.Length)
+        {
+            var tuple = _heap.Dequeue();
+            if (_visited.Contains(tuple.point)) continue;
+
+            total += tuple.wt;
+            _visited.Add(tuple.point);
+
+            foreach (var point in points)
+            {
+                if (_visited.Contains(point)) continue;
+                var distance = getDistance(point, tuple.point);
+                _heap.Enqueue((point, distance), distance);
+            }
+        }
+
+        return total;
+    }
+
+    private static int getDistance(int[] a, int[] b)
+    {
+        return Math.Abs(a[0] - b[0]) + Math.Abs(a[1] - b[1]);
+    }
+}
