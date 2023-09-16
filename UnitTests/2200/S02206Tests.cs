89b0e310edde1e4cf7c86512de5297cb5f5a7e3a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-16 11:08:39 +1000

    2206. Divide Array Into Equal Pairs

---
+++ UnitTests/2200/S02206Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02206Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02206();
+        sut.DivideArray(new[] {3, 2, 3, 2, 2, 2}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02206();
+        sut.DivideArray(new[] {1, 2, 3, 4}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/2200/S02206.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode_sharp;
+
+// 2206. Divide Array Into Equal Pairs
+// https://leetcode.com/problems/divide-array-into-equal-pairs/
+public class S02206
+{
+    public bool DivideArray(int[] nums)
+    {
+        var map = new Dictionary<int, int>();
+        foreach (var num in nums)
+        {
+            map[num] = map.GetValueOrDefault(num) + 1;
+        }
+
+        return map.Values.All(value => value % 2 == 0);
+    }
+}
