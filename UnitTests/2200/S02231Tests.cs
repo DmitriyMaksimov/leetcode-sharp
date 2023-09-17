11cbe78f55a93c9f0ed4e8a27d339eacceef31a0
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-17 17:54:30 +1000

    2231. Largest Number After Digit Swaps by Parity

---
+++ UnitTests/2200/S02231Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02231Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02231();
+        sut.LargestInteger(1234).Should().Be(3412);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02231();
+        sut.LargestInteger(65875).Should().Be(87655);
+    }
+}
---
+++ leetcode-sharp/2200/S02231.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 2231. Largest Number After Digit Swaps by Parity
+// https://leetcode.com/problems/largest-number-after-digit-swaps-by-parity/
+public class S02231
+{
+    public int LargestInteger(int num)
+    {
+        var digits = num.ToString().Select(x => x - '0').ToArray();
+
+        for (var i = 0; i < digits.Length - 1; i++)
+        {
+            for (var j = i + 1; j < digits.Length; j++)
+            {
+                if (digits[i] % 2 == digits[j] % 2 && digits[i] < digits[j])
+                {
+                    (digits[i], digits[j]) = (digits[j], digits[i]);
+                }
+            }
+        }
+
+        return int.Parse(new string(digits.Select(x => (char) (x + '0')).ToArray()));
+    }
+}
