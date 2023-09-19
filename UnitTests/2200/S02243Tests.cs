5fdf8b51924566576987aed3f17cb0ca069f22f1
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-19 18:46:15 +1000

    2243. Calculate Digit Sum of a String

---
+++ UnitTests/2200/S02243Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02243Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02243();
+        sut.DigitSum("11111222223", 3).Should().Be("135");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02243();
+        sut.DigitSum("00000000", 3).Should().Be("000");
+    }
+}
---
+++ leetcode-sharp/2200/S02243.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 2243. Calculate Digit Sum of a String
+// https://leetcode.com/problems/calculate-digit-sum-of-a-string/
+public class S02243
+{
+    public string DigitSum(string s, int k)
+    {
+        for (; s.Length > k;)
+        {
+            var res = "";
+
+            for (var i = 0; i < s.Length;)
+            {
+                var sum = 0;
+                var j = i;
+
+                for (; j < i + k && j < s.Length; j++)
+                {
+                    sum += s[j] - '0';
+                }
+
+                res += sum.ToString();
+                i = j;
+            }
+
+            s = res;
+        }
+
+        return s;
+    }
+}
