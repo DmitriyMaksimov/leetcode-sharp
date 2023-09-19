dea93f71e0c5c7e25ee8db557ec7fd21a5c0986b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-19 18:39:22 +1000

    2239. Find Closest Number to Zero

---
+++ UnitTests/2200/S02239Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02239Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02239();
+        sut.FindClosestNumber(new[] {-4, -2, 1, 4, 8}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02239();
+        sut.FindClosestNumber(new[] {2, -1, 1}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/2200/S02239.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 2239. Find Closest Number to Zero
+// https://leetcode.com/problems/find-closest-number-to-zero/
+public class S02239
+{
+    public int FindClosestNumber(int[] nums)
+    {
+        var min = int.MaxValue;
+        var minAbs = int.MaxValue;
+
+        foreach (var num in nums)
+        {
+            var abs = Math.Abs(num);
+            if (abs < minAbs)
+            {
+                minAbs = abs;
+                min = num;
+            }
+            else if (abs == minAbs)
+            {
+                min = Math.Max(min, num);
+            }
+        }
+
+        return min;
+    }
+}
