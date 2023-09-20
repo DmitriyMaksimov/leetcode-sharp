67377284c46e2faaf20340e8892445424a15143a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-20 19:40:41 +1000

    2255. Count Prefixes of a Given String

---
+++ UnitTests/2200/S02255Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02255Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02255();
+        sut.CountPrefixes(new[] {"a", "b", "c", "ab", "bc", "abc"}, "abc").Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02255();
+        sut.CountPrefixes(new[] {"a", "a"}, "aa").Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/2200/S02255.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2255. Count Prefixes of a Given String
+// https://leetcode.com/problems/count-prefixes-of-a-given-string/
+public class S02255
+{
+    public int CountPrefixes(string[] words, string s)
+    {
+        return words.Count(s.StartsWith);
+    }
+}
