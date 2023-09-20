26b75c1395aa34629a0ac52079ad4c94215bf812
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-20 19:47:55 +1000

    2259. Remove Digit From Number to Maximize Result

---
+++ UnitTests/2200/S02259Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02259Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02259();
+        sut.RemoveDigit("123", '3').Should().Be("12");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02259();
+        sut.RemoveDigit("1231", '1').Should().Be("231");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02259();
+        sut.RemoveDigit("551", '5').Should().Be("51");
+    }
+}
---
+++ leetcode-sharp/2200/S02259.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 2259. Remove Digit From Number to Maximize Result
+// https://leetcode.com/problems/remove-digit-from-number-to-maximize-result
+public class S02259
+{
+    public string RemoveDigit(string number, char digit)
+    {
+        for (var i = 0; i < number.Length - 1; ++i)
+        {
+            if (number[i] == digit && number[i + 1] > digit)
+            {
+                return number[..i] + number[(i + 1)..];
+            }
+        }
+
+        var lastDigitPosition = number.LastIndexOf(digit);
+        return number[..lastDigitPosition] + number[(lastDigitPosition + 1)..];
+    }
+}
