a257826ddc0b01b6b97c5bf28148026bebea12d2
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-20 22:21:33 +1000

    2269. Find the K-Beauty of a Number

---
+++ UnitTests/2200/S02269Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02269Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02269();
+        sut.DivisorSubstrings(240, 2).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02269();
+        sut.DivisorSubstrings(430043, 2).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/2200/S02269.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 2269. Find the K-Beauty of a Number
+// https://leetcode.com/problems/find-the-k-beauty-of-a-number/
+public class S02269
+{
+    public int DivisorSubstrings(int num, int k)
+    {
+        var s = num.ToString();
+        var n = s.Length;
+        var ans = 0;
+
+        for (var i = 0; i < n - k + 1; i++)
+        {
+            var subNum = int.Parse(s.AsSpan(i, k));
+
+            if (subNum != 0 && num % subNum == 0)
+            {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
