688d8378b2768894cb604a6f02548ff5664fd4c2
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-20 22:28:50 +1000

    2273. Find Resultant Array After Removing Anagrams

---
+++ leetcode-sharp/2200/S02273.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 2273. Find Resultant Array After Removing Anagrams
+// https://leetcode.com/problems/find-resultant-array-after-removing-anagrams/
+public class S02273
+{
+    public IList<string> RemoveAnagrams(string[] words)
+    {
+        IList<string> list = new List<string>();
+        var stack = new Stack<string>();
+
+        foreach (var word in words)
+        {
+            var sorted = string.Concat(word.OrderBy(c => c));
+            
+            if (stack.Count == 0)
+            {
+                stack.Push(sorted);
+                list.Add(word);
+            }
+            else
+            {
+                if (!stack.Peek().Equals(sorted))
+                {
+                    list.Add(word);
+                    stack.Push(sorted);
+                }
+            }
+        }
+
+        return list;
+    }
+}
