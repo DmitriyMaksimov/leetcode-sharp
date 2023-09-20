245d2cdc94bc17b3c4e907d1ce5c02711bd72092
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-20 18:40:24 +1000

    1658. Minimum Operations to Reduce X to Zero

---
+++ UnitTests/1600/S01658Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01658Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01658();
+        sut.MinOperations(new[] {1, 1, 4, 2, 3}, 5).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01658();
+        sut.MinOperations(new[] {5, 6, 7, 8, 9}, 4).Should().Be(-1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01658();
+        sut.MinOperations(new[] {3, 2, 20, 1, 1, 3}, 10).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/1600/S01658.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode_sharp;
+
+// 1658. Minimum Operations to Reduce X to Zero
+// https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
+public class S01658
+{
+    public int MinOperations(int[] nums, int x)
+    {
+        var sum = nums.Sum();
+
+        var target = sum - x;
+        if (target == 0)
+        {
+            return nums.Length;
+        }
+
+        var left = 0;
+        var right = 0;
+        var current = 0;
+        var max = -1;
+
+        while (right < nums.Length)
+        {
+            current += nums[right];
+        
+            while (current > target && left <= right)
+            {
+                current -= nums[left];
+                left++;
+            }
+
+            if (current == target)
+            {
+                max = Math.Max(max, right - left + 1);
+            }
+
+            right++;
+        }
+
+        return max == -1 ? -1 : nums.Length - max;
+    }
+}
