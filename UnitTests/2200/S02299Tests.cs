2af90ffcfd819cd700792721b373d30acf376f9b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-21 18:11:16 +1000

    2299. Strong Password Checker II

---
+++ UnitTests/2200/S02299Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02299Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02299();
+        sut.StrongPasswordCheckerII("IloveLe3tcode!").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02299();
+        sut.StrongPasswordCheckerII("Me+You--IsMyDream").Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02299();
+        sut.StrongPasswordCheckerII("1aB!").Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/2200/S02299.cs
@@ -0,0 +1,48 @@
+﻿namespace leetcode_sharp;
+
+// 2299. Strong Password Checker II
+// https://leetcode.com/problems/strong-password-checker-ii/
+public class S02299
+{
+    public bool StrongPasswordCheckerII(string password)
+    {
+        if (password.Length < 8)
+        {
+            return false;
+        }
+
+        var hasLower = false;
+        var hasUpper = false;
+        var hasDigit = false;
+        var hasSpecial = false;
+        var prev = ' ';
+
+        foreach (var c in password)
+        {
+            if (!hasLower && char.IsLower(c))
+            {
+                hasLower = true;
+            }
+            else if (!hasUpper && char.IsUpper(c))
+            {
+                hasUpper = true;
+            }
+            else if (prev != ' ' && prev == c)
+            {
+                return false;
+            }
+            else if (!hasDigit && char.IsDigit(c))
+            {
+                hasDigit = true;
+            }
+            else if (!hasSpecial && "!@#$%^&*()-+".Contains(c))
+            {
+                hasSpecial = true;
+            }
+
+            prev = c;
+        }
+
+        return hasLower && hasUpper && hasDigit && hasSpecial;
+    }
+}
