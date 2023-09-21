ee71c6e3047ab8c1454241d6f3b9404d2aff9f9b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-21 15:13:04 +1000

    2278. Percentage of Letter in String

---
+++ UnitTests/2200/S02278Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02278Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02278();
+        sut.PercentageLetter("foobar", 'o').Should().Be(33);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02278();
+        sut.PercentageLetter("jjjj", 'k').Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2200/S02278.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2278. Percentage of Letter in String
+// https://leetcode.com/problems/percentage-of-letter-in-string
+public class S02278
+{
+    public int PercentageLetter(string s, char letter)
+    {
+        return s.Count(c => c == letter) * 100 / s.Length;
+    }
+}
