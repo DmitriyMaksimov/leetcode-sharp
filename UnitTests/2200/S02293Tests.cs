f54fcb9d7f8fb77a8a400ab1e264288ce8c32f19
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-21 18:05:32 +1000

    2293. Min Max Game

---
+++ UnitTests/2200/S02293Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02293Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02293();
+        sut.MinMaxGame(new[] {1, 3, 5, 2, 4, 8, 2, 2}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02293();
+        sut.MinMaxGame(new[] {3}).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/2200/S02293.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 
+// https://leetcode.com/problems/min-max-game
+public class S02293
+{
+    public int MinMaxGame(int[] nums)
+    {
+        for (var n = nums.Length; n > 1; n -= n / 2)
+        {
+            for (var i = 0; i < n / 2; i++)
+            {
+                var val1 = nums[2 * i];
+                var val2 = nums[2 * i + 1];
+
+                nums[i] = i % 2 == 0 ? Math.Min(val1, val2) : Math.Max(val1, val2);
+            }
+        }
+
+        return nums[0];
+    }
+}
