cf7c532ddcfb0072cc035b4e9424b3cb608103c8
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-21 19:36:33 +1000

    2319. Check if Matrix Is X-Matrix

---
+++ UnitTests/2300/S02319Tests.cs
@@ -0,0 +1,30 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02319Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02319();
+        sut.CheckXMatrix(new[] {new[] {2, 0, 0, 1}, new[] {0, 3, 1, 0}, new[] {0, 5, 2, 0}, new[] {4, 0, 0, 2}})
+            .Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02319();
+        sut.CheckXMatrix(new[] {new[] {5, 7, 0}, new[] {0, 3, 1}, new[] {0, 5, 0}}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02319();
+        sut.CheckXMatrix(new[] {new[] {0, 2, 1, 0}, new[] {1, 0, 0, 3}, new[] {4, 0, 0, 5}, new[] {0, 3, 3, 0}})
+            .Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/2300/S02319.cs
@@ -0,0 +1,34 @@
+﻿namespace leetcode_sharp;
+
+// 2319. Check if Matrix Is X-Matrix
+// https://leetcode.com/problems/check-if-matrix-is-x-matrix/
+public class S02319
+{
+    public bool CheckXMatrix(int[][] grid)
+    {
+        var n = grid.Length;
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                if (i == j || i == n - j - 1)
+                {
+                    if (grid[i][j] == 0)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (grid[i][j] != 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
