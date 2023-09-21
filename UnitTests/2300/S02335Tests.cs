c39de560fbb5fa04b243e48b96644ad756bf9202
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-21 22:15:56 +1000

    2335. Minimum Amount of Time to Fill Cups

---
+++ UnitTests/2300/S02335Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02335Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02335();
+        sut.FillCups(new[] {1, 4, 2}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02335();
+        sut.FillCups(new[] {5, 4, 4}).Should().Be(7);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02335();
+        sut.FillCups(new[] {5, 0, 0}).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/2300/S02335.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 2335. Minimum Amount of Time to Fill Cups
+// https://leetcode.com/problems/minimum-amount-of-time-to-fill-cups/
+public class S02335
+{
+    public int FillCups(int[] amount)
+    {
+        var seconds = 0;
+
+        while (amount.Sum() > 0)
+        {
+            // Sort the amounts in decreasing order
+            Array.Sort(amount, (a, b) => b - a);
+
+            // If we have at least two types of cups to fill, fill two cups of different types
+            if (amount[1] > 0)
+            {
+                amount[0]--;
+                amount[1]--;
+                seconds++;
+            }
+            else
+            {
+                // If we only have one type of cup to fill, fill one cup of any type for each remaining second
+                seconds += amount[0];
+                break;
+            }
+        }
+
+        return seconds;
+    }
+}
