7776991123c10f6d786055c9efda9b3fa5ba2b46
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-21 19:49:26 +1000

    2331. Evaluate Boolean Binary Tree

---
+++ UnitTests/2300/S02331Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02331Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02331();
+        sut.EvaluateTree(TreeNode.Parse("[2,1,3,null,null,0,1]")!).Should().BeTrue();
+    }
+    [Test]
+    public void T3()
+    {
+        var sut = new S02331();
+        sut.EvaluateTree(TreeNode.Parse("[0]")!).Should().BeFalse();
+    }
+
+}
---
+++ leetcode-sharp/2300/S02331.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode_sharp;
+
+// 2331. Evaluate Boolean Binary Tree
+// https://leetcode.com/problems/evaluate-boolean-binary-tree/
+public class S02331
+{
+    public bool EvaluateTree(TreeNode root)
+    {
+        return root.val switch
+        {
+            1 => true,
+            0 => false,
+            2 => EvaluateTree(root.left!) || EvaluateTree(root.right!),
+            _ => EvaluateTree(root.left!) && EvaluateTree(root.right!)
+        };
+    }
+}
