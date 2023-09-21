4ece22fa0b2f0a05b949163a7b60c248834474b7
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-21 19:23:47 +1000

    2309. Greatest English Letter in Upper and Lower Case

---
+++ leetcode-sharp/2300/S02309.cs
@@ -0,0 +1,34 @@
+﻿namespace leetcode_sharp;
+
+// 2309. Greatest English Letter in Upper and Lower Case
+// https://leetcode.com/problems/greatest-english-letter-in-upper-and-lower-case
+public class S02309
+{
+    public string GreatestLetter(string s)
+    {
+        var lowercaseLetters = new HashSet<char>();
+        var uppercaseLetters = new HashSet<char>();
+
+        foreach (var c in s)
+        {
+            if (char.IsLower(c))
+            {
+                lowercaseLetters.Add(c);
+            }
+            else if (char.IsUpper(c))
+            {
+                uppercaseLetters.Add(c);
+            }
+        }
+
+        for (var c = 'Z'; c >= 'A'; c--)
+        {
+            if (uppercaseLetters.Contains(c) && lowercaseLetters.Contains(char.ToLower(c)))
+            {
+                return c.ToString();
+            }
+        }
+
+        return string.Empty;
+    }
+}
