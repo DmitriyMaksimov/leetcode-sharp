06316581229f5ce2f37f0ada417a492892c84b07
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-21 19:15:09 +1000

    2303. Calculate Amount Paid in Taxes

---
+++ UnitTests/2300/S02303Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02303Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02303();
+        sut.CalculateTax(new[] {new[] {3, 50}, new[] {7, 10}, new[] {12, 25}}, 10).Should().Be(2.65);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02303();
+        sut.CalculateTax(new[] {new[] {1, 0}, new[] {4, 25}, new[] {5, 50}}, 2).Should().Be(0.25);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02303();
+        sut.CalculateTax(new[] {new[] {2, 50}}, 0).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2300/S02303.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 2303. Calculate Amount Paid in Taxes
+// https://leetcode.com/problems/calculate-amount-paid-in-taxes/
+public class S02303
+{
+    public double CalculateTax(int[][] brackets, int income)
+    {
+        var prev = 0;
+        var taxes = 0.0;
+
+        foreach (var bracket in brackets)
+        {
+            if (income <= 0)
+            {
+                break;
+            }
+
+            var diff = Math.Min(bracket[0] - prev, income);
+
+            taxes += diff * (bracket[1] / 100.0);
+
+            income -= diff;
+            prev = bracket[0];
+        }
+
+        return taxes;
+    }
+}
