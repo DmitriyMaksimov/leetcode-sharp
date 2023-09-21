db4913c081a21b21746e532702d786af93ec33f0
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-21 17:58:14 +1000

    2287. Rearrange Characters to Make Target String

---
+++ UnitTests/2200/S02287Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02287Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02287();
+        sut.RearrangeCharacters("ilovecodingonleetcode", "code").Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02287();
+        sut.RearrangeCharacters("abcba", "abc").Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02287();
+        sut.RearrangeCharacters("abbaccaddaeea", "aaaaa").Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/2200/S02287.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 2287. Rearrange Characters to Make Target String
+// https://leetcode.com/problems/rearrange-characters-to-make-target-string/
+public class S02287
+{
+    public int RearrangeCharacters(string s, string target)
+    {
+        var freq = new int[26];
+        var freq2 = new int[26];
+
+        foreach (var ch in s.ToCharArray())
+        {
+            freq[ch - 'a']++;
+        }
+
+        foreach (var ch in target.ToCharArray())
+        {
+            freq2[ch - 'a']++;
+        }
+
+        return target.ToCharArray().Select(ch => freq[ch - 'a'] / freq2[ch - 'a']).Min();
+    }
+}
