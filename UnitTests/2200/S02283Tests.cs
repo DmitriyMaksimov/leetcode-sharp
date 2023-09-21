b0897bab5b223069e923a1e05032e82aa782693e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-21 15:23:59 +1000

    2283. Check if Number Has Equal Digit Count and Digit Value

---
+++ UnitTests/2200/S02283Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02283Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02283();
+        sut.DigitCount("1210").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02283();
+        sut.DigitCount("030").Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/2200/S02283.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode_sharp;
+
+// 2283. Check if Number Has Equal Digit Count and Digit Value
+// https://leetcode.com/problems/check-if-number-has-equal-digit-count-and-digit-value/
+public class S02283
+{
+    public bool DigitCount(string num)
+    {
+        var dictionary = new Dictionary<char, int>();
+        foreach (var c in num)
+        {
+            dictionary[c] = dictionary.GetValueOrDefault(c) + 1;
+        }
+
+        return !num.Where((t, i) => dictionary.GetValueOrDefault((char) ('0' + i)) != t - '0').Any();
+    }
+}
