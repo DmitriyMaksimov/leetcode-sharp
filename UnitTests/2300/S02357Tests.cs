761fb0d0e6d8de1b0aac81b9b84b5650a40d0284
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-22 18:49:28 +1000

    2357. Make Array Zero by Subtracting Equal Amounts

---
+++ UnitTests/2300/S02357Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02357Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02357();
+        sut.MinimumOperations(new[] {1, 5, 0, 3, 5}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02357();
+        sut.MinimumOperations(new[] {0}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2300/S02357.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2357. Make Array Zero by Subtracting Equal Amounts
+// https://leetcode.com/problems/make-array-zero-by-subtracting-equal-amounts/
+public class S02357
+{
+    public int MinimumOperations(int[] nums)
+    {
+        return nums.Distinct().Count(x => x != 0);
+    }
+}
