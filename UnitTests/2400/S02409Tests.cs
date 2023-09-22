7e0a8004a94e876ef97e4d9050b148bcd0534aa4
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-23 08:51:52 +1000

    2409. Count Days Spent Together

---
+++ UnitTests/2400/S02409Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02409Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02409();
+        sut.CountDaysTogether("08-15", "08-18", "08-16", "08-19").Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02409();
+        sut.CountDaysTogether("10-01", "10-31", "11-01", "12-31").Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2400/S02409.cs
@@ -0,0 +1,16 @@
+﻿namespace leetcode_sharp;
+
+// 2409. Count Days Spent Together
+// https://leetcode.com/problems/count-days-spent-together/
+public class S02409
+{
+    public int CountDaysTogether(string arriveAlice, string leaveAlice, string arriveBob, string leaveBob)
+    {
+        var arrive_alice = DateTime.ParseExact(arriveAlice, "MM-dd", null).DayOfYear;
+        var leave_alice = DateTime.ParseExact(leaveAlice, "MM-dd", null).DayOfYear;
+        var arrive_bob = DateTime.ParseExact(arriveBob, "MM-dd", null).DayOfYear;
+        var leave_bob = DateTime.ParseExact(leaveBob, "MM-dd", null).DayOfYear;
+
+        return Math.Max(0, Math.Min(leave_alice, leave_bob) - Math.Max(arrive_alice, arrive_bob) + 1);
+    }
+}
