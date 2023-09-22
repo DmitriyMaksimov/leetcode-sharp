984816d57996cc0712dcacb9d7c32ab0b9365b36
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-22 20:01:06 +1000

    2363. Merge Similar Items

---
+++ UnitTests/2300/S02363Tests.cs
@@ -0,0 +1,32 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02363Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02363();
+        sut.MergeSimilarItems(new[] {new[] {1, 1}, new[] {4, 5}, new[] {3, 8}}, new[] {new[] {3, 1}, new[] {1, 5}})
+            .Should().BeEquivalentTo(new[] {new[] {1, 6}, new[] {3, 9}, new[] {4, 5}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02363();
+        sut.MergeSimilarItems(new[] {new[] {1, 1}, new[] {3, 2}, new[] {2, 3}},
+                new[] {new[] {2, 1}, new[] {3, 2}, new[] {1, 3}}).Should()
+            .BeEquivalentTo(new[] {new[] {1, 4}, new[] {2, 4}, new[] {3, 4}});
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02363();
+        sut.MergeSimilarItems(new[] {new[] {1, 3}, new[] {2, 2}}, new[] {new[] {7, 1}, new[] {2, 2}, new[] {1, 4}})
+            .Should().BeEquivalentTo(new[] {new[] {1, 7}, new[] {2, 4}, new[] {7, 1}});
+    }
+}
---
+++ leetcode-sharp/2300/S02363.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 2363. Merge Similar Items
+// https://leetcode.com/problems/merge-similar-items/
+public class S02363
+{
+    public IList<IList<int>> MergeSimilarItems(int[][] items1, int[][] items2)
+    {
+        // Create a dictionary to keep track of values and summed weights
+        var valueWeights = new Dictionary<int, int>();
+
+        AddToDictionary(items1);
+        AddToDictionary(items2);
+
+        return valueWeights.OrderBy(x => x.Key).Select(pair => new[] {pair.Key, pair.Value}).Cast<IList<int>>().ToList();
+
+        void AddToDictionary(int[][] items)
+        {
+            foreach (var item in items)
+            {
+                valueWeights.TryAdd(item[0], 0);
+                valueWeights[item[0]] += item[1];
+            }
+        }
+    }
+}
