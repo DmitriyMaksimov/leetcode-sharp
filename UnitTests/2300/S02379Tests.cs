4aa5782def1958b01f1e00ff5ff1bc2de7dd68c4
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-22 20:05:21 +1000

    2379. Minimum Recolors to Get K Consecutive Black Blocks

---
+++ UnitTests/2300/S02379Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02379Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02379();
+        sut.MinimumRecolors("WBBWWBBWBW", 7).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02379();
+        sut.MinimumRecolors("WBWBBBW", 2).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2300/S02379.cs
@@ -0,0 +1,45 @@
+﻿namespace leetcode_sharp;
+
+// 
+// https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/
+public class S02379
+{
+    public int MinimumRecolors(string blocks, int k)
+    {
+        var minOps = k;
+        var whiteBlocks = new int[blocks.Length];
+
+        // Initialise the counts of 'W's
+        for (var i = 0; i < blocks.Length; i++)
+        {
+            if (i > 0)
+            {
+                whiteBlocks[i] = whiteBlocks[i - 1];
+            }
+
+            if (blocks[i] == 'W')
+            {
+                whiteBlocks[i]++;
+            }
+        }
+
+        // We will slide our window of size 'k' from start to end
+        for (var i = 0; i <= blocks.Length - k; i++)
+        {
+            int ops;
+
+            if (i == 0)
+            {
+                ops = whiteBlocks[i + k - 1];
+            }
+            else
+            {
+                ops = whiteBlocks[i + k - 1] - whiteBlocks[i - 1];
+            }
+
+            minOps = Math.Min(minOps, ops);
+        }
+
+        return minOps;
+    }
+}
