1f2c9af286d01195bd8950c7d344a8dde80e03a5
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-22 18:43:07 +1000

    2351. First Letter to Appear Twice

---
+++ UnitTests/2300/S02351Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02351Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02351();
+        sut.RepeatedCharacter("abccbaacz").Should().Be('c');
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02351();
+        sut.RepeatedCharacter("abcdd").Should().Be('d');
+    }
+}
---
+++ leetcode-sharp/2300/S02351.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 2351. First Letter to Appear Twice
+// https://leetcode.com/problems/first-letter-to-appear-twice/
+public class S02351
+{
+    public char RepeatedCharacter(string s)
+    {
+        var set = new HashSet<char>();
+
+        foreach (var c in s)
+        {
+            if (set.Contains(c))
+            {
+                return c;
+            }
+
+            set.Add(c);
+        }
+
+        return ' ';
+    }
+}
