31b47143d51b08913e4b8b1b75dae03ffc165627
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-22 18:28:37 +1000

    2341. Maximum Number of Pairs in Array

---
+++ UnitTests/2300/S02341Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02341Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02341();
+        sut.NumberOfPairs(new[] {1, 3, 2, 1, 3, 2, 2}).Should().Equal(3, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02341();
+        sut.NumberOfPairs(new[] {1, 1}).Should().Equal(1, 0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02341();
+        sut.NumberOfPairs(new[] {0}).Should().Equal(0, 1);
+    }
+}
---
+++ leetcode-sharp/2300/S02341.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 2341. Maximum Number of Pairs in Array
+// https://leetcode.com/problems/maximum-number-of-pairs-in-array/
+public class S02341
+{
+    public int[] NumberOfPairs(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+        foreach (var num in nums)
+        {
+            dict[num] = dict.GetValueOrDefault(num) + 1;
+        }
+
+        var result = new int[2];
+
+        foreach (var value in dict.Values)
+        {
+            result[0] += value / 2;
+            result[1] += value % 2;
+        }
+
+        return result;
+    }
+}
