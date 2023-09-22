01a8cd4774955206d30fd98e8bdba1445c65842e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-23 08:00:55 +1000

    2395. Find Subarrays With Equal Sum

---
+++ UnitTests/2300/S02395Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02395Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02395();
+        sut.FindSubarrays(new[] {4, 2, 4}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02395();
+        sut.FindSubarrays(new[] {1, 2, 3, 4, 5}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02395();
+        sut.FindSubarrays(new[] {0, 0, 0}).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/2300/S02395.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 2395. Find Subarrays With Equal Sum
+// https://leetcode.com/problems/find-subarrays-with-equal-sum/
+public class S02395
+{
+    public bool FindSubarrays(int[] nums)
+    {
+        var hashSet = new HashSet<int>();
+        
+        for (var i = 1; i < nums.Length; i++)
+        {
+            var sum = nums[i- 1] + nums[i];
+
+            if (hashSet.Contains(sum))
+            {
+                return true;
+            }
+            
+            hashSet.Add(sum);
+        }
+
+        return false;
+    }
+}
