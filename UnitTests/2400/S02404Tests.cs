76bff50a134b584b1370364cfa8f970b03910b65
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-23 08:14:11 +1000

    2404. Most Frequent Even Element

---
+++ UnitTests/2400/S02404Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02404Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02404();
+        sut.MostFrequentEven(new[] {0, 1, 2, 2, 4, 4, 1}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02404();
+        sut.MostFrequentEven(new[] {4, 4, 4, 9, 2, 4}).Should().Be(4);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02404();
+        sut.MostFrequentEven(new[] {29, 47, 21, 41, 13, 37, 25, 7}).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/2400/S02404.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode_sharp;
+
+// 
+// https://leetcode.com/problems/most-frequent-even-element/
+public class S02404
+{
+    public int MostFrequentEven(int[] nums)
+    {
+        var dictionary = new Dictionary<int, int>();
+        foreach (var num in nums.Where(n => n % 2 == 0))
+        {
+            dictionary[num] = dictionary.GetValueOrDefault(num) + 1;
+        }
+        
+        return dictionary.OrderByDescending(x => x.Value).ThenBy(x => x.Key).FirstOrDefault(new KeyValuePair<int, int>(-1,0)).Key; 
+    }
+}
