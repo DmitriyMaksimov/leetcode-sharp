b076af101307c16c356100ddca1b639852605ed9
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-23 16:55:20 +1000

    2451. Odd String Difference

---
+++ UnitTests/2400/S02451Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02451Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02451();
+        sut.OddString(new[] {"adc", "wzy", "abc"}).Should().Be("abc");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02451();
+        sut.OddString(new[] {"aaa", "bob", "ccc", "ddd"}).Should().Be("bob");
+    }
+}
---
+++ leetcode-sharp/2400/S02451.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 2451. Odd String Difference
+// https://leetcode.com/problems/odd-string-difference/
+public class S02451
+{
+    public string OddString(string[] words)
+    {
+        var differenceCounts = new Dictionary<string, int>();
+
+        foreach (var word in words)
+        {
+            var difference = getDifference(word);
+            differenceCounts[difference] = differenceCounts.GetValueOrDefault(difference) + 1;
+        }
+
+        foreach (var word in words)
+        {
+            if (differenceCounts[getDifference(word)] == 1)
+            {
+                return word;
+            }
+        }
+
+        return string.Empty;
+    }
+
+    private static string getDifference(string word)
+    {
+        var difference = new StringBuilder();
+        for (var i = 0; i < word.Length - 1; i++)
+        {
+            difference.Append(word[i + 1] - word[i]);
+            difference.Append(',');
+        }
+
+        return difference.ToString();
+    }
+}
