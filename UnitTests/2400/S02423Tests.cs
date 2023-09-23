d8860c2b49cd5bd8ec3831ac4dc95e93790b7e94
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-23 10:45:11 +1000

    2423. Remove Letter To Equalize Frequency

---
+++ UnitTests/2400/S02423Tests.cs
@@ -0,0 +1,56 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02423Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02423();
+        sut.EqualFrequency("abcc").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02423();
+        sut.EqualFrequency("aazz").Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02423();
+        sut.EqualFrequency("bac").Should().BeTrue();
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S02423();
+        sut.EqualFrequency("cccaa").Should().BeTrue();
+    }
+
+    [Test]
+    public void T5()
+    {
+        var sut = new S02423();
+        sut.EqualFrequency("ddaccb").Should().BeFalse();
+    }
+
+    [Test]
+    public void T6()
+    {
+        var sut = new S02423();
+        sut.EqualFrequency("abbcc").Should().BeTrue();
+    }
+
+    [Test]
+    public void T7()
+    {
+        var sut = new S02423();
+        sut.EqualFrequency("babbdd").Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/2400/S02423.cs
@@ -0,0 +1,64 @@
+﻿namespace leetcode_sharp;
+
+// 2423. Remove Letter To Equalize Frequency
+// https://leetcode.com/problems/remove-letter-to-equalize-frequency/
+public class S02423
+{
+    public bool EqualFrequency(string word)
+    {
+        var arr = new int[26];
+        foreach (var c in word)
+        {
+            arr[c - 'a']++;
+        }
+
+        var freq = new Dictionary<int, List<char>>();
+        for (var i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == 0)
+            {
+                continue;
+            }
+
+            var letter = (char) (i + 'a');
+            if (freq.TryGetValue(arr[i], out var list))
+            {
+                list.Add(letter);
+            }
+            else
+            {
+                freq[arr[i]] = new List<char>() {letter};
+            }
+        }
+
+        if (freq.Count > 2)
+        {
+            return false;
+        }
+
+        var min = freq.Keys.Min();
+        var max = freq.Keys.Max();
+
+        if (freq.Count == 1 && freq[min].Count == 1)
+        {
+            return true;
+        }
+
+        if (min == 1 && freq[min].Count == 1)
+        {
+            return true;
+        }
+
+        if (min == 1 && max == 1)
+        {
+            return true;
+        }
+
+        if (max - min == 1 && freq[max].Count == 1)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
