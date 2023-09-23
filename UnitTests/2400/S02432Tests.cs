a32f9e7532794341096188cdba9a9f1ae897c8d6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-23 14:56:29 +1000

    2432. The Employee That Worked on the Longest Task

---
+++ UnitTests/2400/S02432Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02432Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02432();
+        sut.HardestWorker(10, new[] {new[] {0, 3}, new[] {2, 5}, new[] {0, 9}, new[] {1, 15}}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02432();
+        sut.HardestWorker(26, new[] {new[] {1, 1}, new[] {3, 7}, new[] {2, 12}, new[] {7, 17}}).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02432();
+        sut.HardestWorker(2, new[] {new[] {0, 10}, new[] {1, 20}}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2400/S02432.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 2432. The Employee That Worked on the Longest Task
+// https://leetcode.com/problems/the-employee-that-worked-on-the-longest-task/
+public class S02432
+{
+    public int HardestWorker(int n, int[][] logs)
+    {
+        var prev = 0;
+        var max = 0;
+        var result = 0;
+
+        foreach (var item in logs)
+        {
+            var len = item[1] - prev;
+
+            if (len > max || len == max && result > item[0])
+            {
+                max = len;
+                result = item[0];
+            }
+
+            prev = item[1];
+        }
+
+        return result;
+    }
+}
