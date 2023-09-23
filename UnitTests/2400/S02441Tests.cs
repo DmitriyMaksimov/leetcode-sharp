e2339d21b651cef4f0af2d697c310889f10e9ea9
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-23 16:36:11 +1000

    2441. Largest Positive Integer That Exists With Its Negative

---
+++ UnitTests/2400/S02441Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02441Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02441();
+        sut.FindMaxK(new[] {-1, 2, -3, 3}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02441();
+        sut.FindMaxK(new[] {-1, 10, 6, 7, -7, 1}).Should().Be(7);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02441();
+        sut.FindMaxK(new[] {-10, 8, 6, 7, -2, -3}).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/2400/S02441.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 2441. Largest Positive Integer That Exists With Its Negative
+// https://leetcode.com/problems/largest-positive-integer-that-exists-with-its-negative/
+public class S02441
+{
+    public int FindMaxK(int[] nums)
+    {
+        var max = -1;
+        var set = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            if (set.Contains(-num))
+            {
+                max = Math.Max(max, Math.Abs(num));
+            }
+
+            set.Add(num);
+        }
+
+        return max;
+    }
+}
