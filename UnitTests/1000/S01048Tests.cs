721c9641f4c9ff65ec5c8ca420405127b7ca4b6c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-23 10:48:40 +1000

    1048. Longest String Chain

---
+++ UnitTests/1000/S01048Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01048Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01048();
+        sut.LongestStrChain(new[] {"a", "b", "ba", "bca", "bda", "bdca"}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01048();
+        sut.LongestStrChain(new[] {"xbc", "pcxbcf", "xb", "cxbc", "pcxbc"}).Should().Be(5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01048();
+        sut.LongestStrChain(new[] {"abcd", "dbqca"}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1000/S01048.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1048. Longest String Chain
+// https://leetcode.com/problems/longest-string-chain/
+public class S01048
+{
+    public int LongestStrChain(string[] words)
+    {
+        var dp = new Dictionary<string, int>();
+        var res = 0;
+
+        Array.Sort(words, (a, b) => a.Length - b.Length);
+        
+        foreach (var word in words)
+        {
+            var max = 0;
+
+            for (var i = 0; i < word.Length; i++)
+            {
+                var sb = new StringBuilder(word);
+                sb.Remove(i, 1);
+                var prev = sb.ToString();
+                if (dp.TryGetValue(prev, out var value))
+                {
+                    max = Math.Max(max, value);
+                }
+            }
+
+            dp[word] = max + 1;
+
+            res = Math.Max(res, dp[word]);
+        }
+
+        return res;
+    }
+}
