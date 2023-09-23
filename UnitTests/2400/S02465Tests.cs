dabd488145563e4a3a7e36f19fe4244da17b3a2d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-23 19:45:28 +1000

    2465. Number of Distinct Averages

---
+++ UnitTests/2400/S02465Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02465Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02465();
+        sut.DistinctAverages(new[] {4, 1, 4, 0, 3, 5}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02465();
+        sut.DistinctAverages(new[] {1, 100}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/2400/S02465.cs
@@ -0,0 +1,51 @@
+﻿namespace leetcode_sharp;
+
+// 2465. Number of Distinct Averages
+// https://leetcode.com/problems/number-of-distinct-averages
+public class S02465
+{
+    public int DistinctAverages(int[] nums)
+    {
+        var counts = new Dictionary<int, int>();
+        var minHeap = new SortedSet<(int val, int index)>();
+        var maxHeap = new SortedSet<(int val, int index)>();
+        var averages = new HashSet<double>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (!counts.ContainsKey(nums[i]))
+            {
+                counts[nums[i]] = 0;
+            }
+
+            counts[nums[i]]++;
+            minHeap.Add((nums[i], i));
+            maxHeap.Add((-nums[i], i));
+        }
+
+        while (counts.Count != 0)
+        {
+            var min = minHeap.Min;
+            var max = maxHeap.Min;
+            minHeap.Remove(min);
+            maxHeap.Remove(max);
+
+            var average = (min.val - max.val) / 2.0;
+            averages.Add(average);
+
+            counts[min.val]--;
+            if (counts[min.val] == 0)
+            {
+                counts.Remove(min.val);
+            }
+
+            counts[-max.val]--;
+            if (counts[-max.val] == 0)
+            {
+                counts.Remove(-max.val);
+            }
+        }
+
+        return averages.Count;
+    }
+}
