23a595aac1a632a4b295aa06358c9d9889f31ba5
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-23 15:01:48 +1000

    2437. Number of Valid Clock Times

---
+++ UnitTests/2400/S02437Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02437Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02437();
+        sut.CountTime("?5:00").Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02437();
+        sut.CountTime("0?:0?").Should().Be(100);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02437();
+        sut.CountTime("??:??").Should().Be(1440);
+    }
+}
---
+++ leetcode-sharp/2400/S02437.cs
@@ -0,0 +1,45 @@
+﻿namespace leetcode_sharp;
+
+// 2437. Number of Valid Clock Times
+// https://leetcode.com/problems/number-of-valid-clock-times/
+public class S02437
+{
+    public int CountTime(string time)
+    {
+        var count = 0;
+        var hour = time[0] == '?' ? -1 : time[0] - '0';
+        var hour2 = time[1] == '?' ? -1 : time[1] - '0';
+        var minute = time[3] == '?' ? -1 : time[3] - '0';
+        var minute2 = time[4] == '?' ? -1 : time[4] - '0';
+
+        for (var i = 0; i < 24; i++)
+        {
+            if (hour != -1 && hour != i / 10)
+            {
+                continue;
+            }
+
+            for (var j = 0; j < 60; j++)
+            {
+                if (hour2 != -1 && hour2 != i % 10)
+                {
+                    continue;
+                }
+
+                if (minute != -1 && minute != j / 10)
+                {
+                    continue;
+                }
+
+                if (minute2 != -1 && minute2 != j % 10)
+                {
+                    continue;
+                }
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
