3723d03a91ed09176d43d2ca7d725849d702cd84
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-23 19:40:13 +1000

    2460. Apply Operations to an Array

---
+++ UnitTests/2400/S02460Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02460Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02460();
+        sut.ApplyOperations(new[] {1, 2, 2, 1, 1, 0}).Should().Equal(1, 4, 2, 0, 0, 0);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02460();
+        sut.ApplyOperations(new[] {0, 1}).Should().Equal(1, 0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02460();
+        sut.ApplyOperations(new[] {847, 847, 0, 0, 0, 399, 416, 416, 879, 879, 206, 206, 206, 272}).Should()
+            .Equal(1694, 399, 832, 1758, 412, 206, 272, 0, 0, 0, 0, 0, 0, 0);
+    }
+}
---
+++ leetcode-sharp/2400/S02460.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 2460. Apply Operations to an Array
+// https://leetcode.com/problems/apply-operations-to-an-array/
+public class S02460
+{
+    public int[] ApplyOperations(int[] nums)
+    {
+        var n = nums.Length;
+        var lastNonZeroFoundAt = 0;
+
+        // Check each element in array
+        for (var i = 0; i < n - 1; i++)
+        {
+            // If it's not zero and equal to next, double the value and zero the next
+            if (nums[i] != 0 && nums[i] == nums[i + 1])
+            {
+                nums[lastNonZeroFoundAt++] = nums[i] * 2;
+                nums[i + 1] = 0;
+            }
+            else if (nums[i] != 0)
+            {
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+
+        // If last element is not zero
+        if (nums[n - 1] != 0)
+        {
+            nums[lastNonZeroFoundAt++] = nums[n - 1];
+        }
+
+        // Move all 0's to the end
+        while (lastNonZeroFoundAt < n)
+        {
+            nums[lastNonZeroFoundAt++] = 0;
+        }
+
+        return nums;
+    }
+}
