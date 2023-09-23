13b526f9e55cd0509070d4cab01182cd287820fb
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-23 17:03:07 +1000

    2455. Average Value of Even Numbers That Are Divisible by Three

---
+++ UnitTests/2400/S02455Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02455Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02455();
+        sut.AverageValue(new[] {1, 3, 6, 10, 12, 15}).Should().Be(9);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02455();
+        sut.AverageValue(new[] {1, 2, 4, 7, 10}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2400/S02455.cs
@@ -0,0 +1,12 @@
+﻿namespace leetcode_sharp;
+
+// 2455. Average Value of Even Numbers That Are Divisible by Three
+// https://leetcode.com/problems/average-value-of-even-numbers-that-are-divisible-by-three/
+public class S02455
+{
+    public int AverageValue(int[] nums)
+    {
+        var ints = nums.Where(x => x % 2 == 0 && x % 3 == 0);
+        return ints.Any() ? (int) ints.Average() : 0;
+    }
+}
