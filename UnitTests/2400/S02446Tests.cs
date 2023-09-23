7129dd884349da0bd50a9a585871f485f4e8e5fe
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-23 16:46:38 +1000

    2446. Determine if Two Events Have Conflict

---
+++ UnitTests/2400/S02446Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02446Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02446();
+        sut.HaveConflict(new[] {"01:15", "02:00"}, new[] {"02:00", "03:00"}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02446();
+        sut.HaveConflict(new[] {"01:00", "02:00"}, new[] {"01:20", "03:00"}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02446();
+        sut.HaveConflict(new[] {"10:00", "11:00"}, new[] {"14:00", "15:00"}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/2400/S02446.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 2446. Determine if Two Events Have Conflict
+// https://leetcode.com/problems/determine-if-two-events-have-conflict/
+public class S02446
+{
+    public bool HaveConflict(string[] event1, string[] event2)
+    {
+        var start1 = convertTimeToInt(event1[0]);
+        var end1 = convertTimeToInt(event1[1]);
+        var start2 = convertTimeToInt(event2[0]);
+        var end2 = convertTimeToInt(event2[1]);
+
+        return (start1 >= start2 && start1 <= end2) || (start2 >= start1 && start2 <= end1);
+    }
+
+    private static int convertTimeToInt(string time)
+    {
+        var splitTime = time.Split(':');
+        return int.Parse(splitTime[0]) * 60 + int.Parse(splitTime[1]);
+    }
+}
