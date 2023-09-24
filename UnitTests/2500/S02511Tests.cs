38cdbaa7c879c1ce2297eb03f649b35d5f139502
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-24 19:05:29 +1000

    2511. Maximum Enemy Forts That Can Be Captured

---
+++ UnitTests/2500/S02511Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02511Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02511();
+        sut.CaptureForts(new[] {1, 0, 0, -1, 0, 0, 0, 0, 1}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02511();
+        sut.CaptureForts(new[] {0, 0, 1, -1}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2500/S02511.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 2511. Maximum Enemy Forts That Can Be Captured
+// https://leetcode.com/problems/maximum-enemy-forts-that-can-be-captured/
+public class S02511
+{
+    public int CaptureForts(int[] forts)
+    {
+        var res = 0;
+
+        for (int i = 0, j = 0; i < forts.Length; ++i)
+        {
+            if (forts[i] == 0) continue;
+            
+            if (forts[j] == -forts[i])
+            {
+                res = Math.Max(res, i - j - 1);
+            }
+
+            j = i;
+        }
+
+        return res;
+    }
+}
