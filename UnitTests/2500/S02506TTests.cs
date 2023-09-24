120c7fa39db815c400b320c3a919e154b2092902
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-24 19:00:12 +1000

    2506. Count Pairs Of Similar Strings

---
+++ UnitTests/2500/S02506TTests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02506TTests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02506();
+        sut.SimilarPairs(new[] {"aba", "aabb", "abcd", "bac", "aabc"}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02506();
+        sut.SimilarPairs(new[] {"aabb", "ab", "ba"}).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02506();
+        sut.SimilarPairs(new[] {"nba", "cba", "dba"}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2500/S02506.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 2506. Count Pairs Of Similar Strings
+// https://leetcode.com/problems/count-pairs-of-similar-strings/
+public class S02506
+{
+    public int SimilarPairs(string[] words)
+    {
+        var result = 0;
+
+        for (var i = 0; i < words.Length; i++)
+        {
+            for (var j = i + 1; j < words.Length; j++)
+            {
+                if (isSimilar(words[i], words[j]))
+                {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+    
+    private static bool isSimilar(string a, string b)
+    {
+        var hashSet = new HashSet<char>(a);
+        var hashSet2 = new HashSet<char>(b);
+        
+        return hashSet.SetEquals(hashSet2);
+    }
+}
