80898406edc6e8bae8ed9c4cbd337281d1306c21
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-24 16:44:26 +1000

    2475. Number of Unequal Triplets in Array

---
+++ UnitTests/2400/S02475Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02475Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02475();
+        sut.UnequalTriplets(new[] {4, 4, 2, 4, 3}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02475();
+        sut.UnequalTriplets(new[] {1, 1, 1, 1, 1}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2400/S02475.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 2475. Number of Unequal Triplets in Array
+// https://leetcode.com/problems/number-of-unequal-triplets-in-array/
+public class S02475
+{
+    public int UnequalTriplets(int[] nums)
+    {
+        var count = 0;
+        
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                for (var k = j + 1; k < nums.Length; k++)
+                {
+                    if (nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k])
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
