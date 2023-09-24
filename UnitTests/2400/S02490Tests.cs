1fa831cce071fcabec3dff3010324bd954ab400d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-24 17:50:56 +1000

    2490. Circular Sentence

---
+++ UnitTests/2400/S02490Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02490Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02490();
+        sut.IsCircularSentence("leetcode exercises sound delightful").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02490();
+        sut.IsCircularSentence("eetcode").Should().BeTrue();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02490();
+        sut.IsCircularSentence("Leetcode is cool").Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/2400/S02490.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 
+// https://leetcode.com/problems/circular-sentence/
+public class S02490
+{
+    public bool IsCircularSentence(string sentence)
+    {
+        var words = sentence.Split(' ').ToArray();
+
+        for (var i = 1; i < words.Length; i++)
+        {
+            if (words[i - 1][^1] != words[i][0])
+            {
+                return false;
+            }
+        }
+
+        return words[^1][^1] == words[0][0];
+    }
+}
