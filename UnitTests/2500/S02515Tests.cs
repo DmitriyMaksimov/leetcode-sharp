96e8bc60daa5139cee95c8e5e7cce4a8aaf1dfb0
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-25 21:02:36 +1000

    2515. Shortest Distance to Target String in a Circular Array

---
+++ UnitTests/2500/S02515Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02515Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02515();
+        sut.ClosetTarget(new[] {"hello", "i", "am", "leetcode", "hello"}, "hello", 1).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02515();
+        sut.ClosetTarget(new[] {"a", "b", "leetcode"}, "leetcode", 0).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02515();
+        sut.ClosetTarget(new[] {"i", "eat", "leetcode"}, "ate", 0).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/2500/S02515.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 2515. Shortest Distance to Target String in a Circular Array
+// https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/
+public class S02515
+{
+    public int ClosetTarget(string[] words, string target, int startIndex)
+    {
+        var n = words.Length;
+        var ans = int.MaxValue;
+
+        for (var i = 0; i < n; i++)
+        {
+            if (words[i] == target)
+            {
+                ans = Math.Min(ans, Math.Min(Math.Abs(i - startIndex), n - Math.Abs(i - startIndex)));
+            }
+        }
+
+        return ans == int.MaxValue ? -1 : ans;
+    }
+}
