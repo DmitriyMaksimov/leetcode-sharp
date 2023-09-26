01ecc83ddea870d4e59faa0109a87e907a14e6b5
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-26 21:09:09 +1000

    2525. Categorize Box According to Criteria

---
+++ UnitTests/2500/S02525Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02525Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02525();
+        sut.CategorizeBox(1000, 35, 700, 300).Should().Be("Heavy");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02525();
+        sut.CategorizeBox(200, 50, 800, 50).Should().Be("Neither");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02525();
+        sut.CategorizeBox(3223, 1271, 2418, 749).Should().Be("Both");
+    }
+}
---
+++ leetcode-sharp/2500/S02525.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode_sharp;
+
+// 2525. Categorize Box According to Criteria
+// https://leetcode.com/problems/categorize-box-according-to-criteria/
+public class S02525
+{
+    public string CategorizeBox(int length, int width, int height, int mass)
+    {
+        var isBulky = length >= 10_000 || width >= 10_000 || height >= 10_000 || (long)length * width * height >= 1_000_000_000;
+        var isHeavy = mass >= 100;
+
+        if (isBulky && isHeavy) return "Both";
+        if (isBulky) return "Bulky";
+
+        return isHeavy ? "Heavy" : "Neither";
+    }
+}
