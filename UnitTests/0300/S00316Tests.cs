c9fd9508e3d773e6d58a25f3fcef6e037d1f569e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-26 12:49:34 +1000

    316. Remove Duplicate Letters

---
+++ UnitTests/0300/S00316Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00316Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00316();
+        sut.RemoveDuplicateLetters("bcabc").Should().Be("abc");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00316();
+        sut.RemoveDuplicateLetters("cbacdcbc").Should().Be("acdb");
+    }
+}
---
+++ leetcode-sharp/0300/S00316.cs
@@ -0,0 +1,34 @@
+﻿namespace leetcode_sharp;
+
+// 316. Remove Duplicate Letters
+// https://leetcode.com/problems/remove-duplicate-letters
+public class S00316
+{
+    public string RemoveDuplicateLetters(string s)
+    {
+        var stack = new Stack<char>();
+        var seen = new HashSet<char>();
+        var lastOcc = new Dictionary<char, int>();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            lastOcc[s[i]] = i;
+        }
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var c = s[i];
+            if (seen.Contains(c)) continue;
+
+            while (stack.Count > 0 && c < stack.Peek() && i < lastOcc[stack.Peek()])
+            {
+                seen.Remove(stack.Pop());
+            }
+
+            seen.Add(c);
+            stack.Push(c);
+        }
+
+        return new string(stack.Reverse().ToArray());
+    }
+}
