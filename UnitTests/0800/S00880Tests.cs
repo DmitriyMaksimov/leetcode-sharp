8b63aca571a71bd465ec02854037fccad6130ea3
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-27 20:09:39 +1000

    880. Decoded String at Index

---
+++ UnitTests/0800/S00880Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00880Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00880();
+        sut.DecodeAtIndex("leet2code3", 10).Should().Be("o");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00880();
+        sut.DecodeAtIndex("ha22", 5).Should().Be("h");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00880();
+        sut.DecodeAtIndex("a2345678999999999999999", 1).Should().Be("a");
+    }
+}
---
+++ leetcode-sharp/0800/S00880.cs
@@ -0,0 +1,45 @@
+﻿namespace leetcode_sharp;
+
+// 880. Decoded String at Index
+// https://leetcode.com/problems/decoded-string-at-index
+public class S00880
+{
+    public string DecodeAtIndex(string s, int k)
+    {
+        var size = 0L;
+        var n = s.Length;
+
+        for (var i = 0; i < n; i++)
+        {
+            if (char.IsDigit(s[i]))
+            {
+                size *= s[i] - '0';
+            }
+            else
+            {
+                size++;
+            }
+        }
+
+        for (var i = n - 1; i >= 0; i--)
+        {
+            k = (int) (k % size);
+
+            if (k == 0 && char.IsLetter(s[i]))
+            {
+                return s[i].ToString();
+            }
+
+            if (char.IsDigit(s[i]))
+            {
+                size /= s[i] - '0';
+            }
+            else
+            {
+                size--;
+            }
+        }
+
+        return "";
+    }
+}
