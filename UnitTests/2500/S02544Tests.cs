6dbb84a05dee42fd3326b06da736da397d89d751
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-30 16:30:37 +1000

    2544. Alternating Digit Sum

---
+++ UnitTests/2500/S02544Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02544Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02544();
+        sut.AlternateDigitSum(521).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02544();
+        sut.AlternateDigitSum(111).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02544();
+        sut.AlternateDigitSum(886996).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2500/S02544.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 2544. Alternating Digit Sum
+// https://leetcode.com/problems/alternating-digit-sum
+public class S02544
+{
+    public int AlternateDigitSum(int n)
+    {
+        var sum = 0;
+        var shouldNegate = false;
+        var div = (int)Math.Pow(10, n.ToString().Length - 1);
+
+        while (n > 0)
+        {
+            var digit = n / div;
+            n %= div;
+            div /= 10;
+            sum += shouldNegate ? -digit : digit;
+            shouldNegate = !shouldNegate;
+        }
+        
+        return sum;
+    }
+}
