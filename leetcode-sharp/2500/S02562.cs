3420de984bf63b3db8fbbce77db8fc59b2c5a171
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-30 18:51:01 +1000

    2562. Find the Array Concatenation Value

---
+++ leetcode-sharp/2500/S02562.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 2562. Find the Array Concatenation Value
+// https://leetcode.com/problems/find-the-array-concatenation-value
+public class S02562
+{
+    public long FindTheArrayConcVal(int[] nums)
+    {
+        var result = 0L;
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            if (left == right)
+            {
+                result += nums[left];
+            }
+            else
+            {
+                var concatValue = int.Parse(nums[left].ToString() + nums[right]);
+                result += concatValue;
+            }
+
+            ++left;
+            --right;
+        }
+        
+        
+        return result;
+    }
+}
