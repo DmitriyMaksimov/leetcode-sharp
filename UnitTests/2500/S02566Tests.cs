223f6d21264f796e6eb7cbe36e3b94e7a4c28d1a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-30 19:17:47 +1000

    2566. Maximum Difference by Remapping a Digit

---
+++ UnitTests/2500/S02566Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02566Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02566();
+        sut.MinMaxDifference(11891).Should().Be(99009);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02566();
+        sut.MinMaxDifference(90).Should().Be(99);
+    }
+}
---
+++ leetcode-sharp/2500/S02566.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 2566. Maximum Difference by Remapping a Digit
+// https://leetcode.com/problems/maximum-difference-by-remapping-a-digit
+public class S02566
+{
+    public int MinMaxDifference(int num)
+    {
+        var chars = num.ToString().ToCharArray();
+        var idx = -1;
+
+        for (var i = 0; i < chars.Length; i++)
+        {
+            if (chars[i] != '9')
+            {
+                idx = i;
+                break;
+            }
+        }
+
+        if (idx == -1) return num;
+        
+        var max = int.Parse(num.ToString().Replace("" + chars[idx], "9"));
+        var min = int.Parse(num.ToString().Replace("" + chars[0], "0"));
+        
+        return max - min;
+    }
+}
