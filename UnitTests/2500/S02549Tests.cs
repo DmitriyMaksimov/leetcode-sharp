77e9e9739c9bfc1299ac7b3c612b5cbcbfac207e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-30 16:35:37 +1000

    2549. Count Distinct Numbers on Board

---
+++ UnitTests/2500/S02549Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02549Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02549();
+        sut.DistinctIntegers(5).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02549();
+        sut.DistinctIntegers(3).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02549();
+        sut.DistinctIntegers(1).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/2500/S02549.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2549. Count Distinct Numbers on Board
+// https://leetcode.com/problems/count-distinct-numbers-on-board
+public class S02549
+{
+    public int DistinctIntegers(int n)
+    {
+        return n == 1 ? 1 : n - 1;
+    }
+}
