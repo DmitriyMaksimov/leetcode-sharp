4680bab03724740c2c7c42955521cd53ac417958
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-30 18:42:11 +1000

    2586. Count the Number of Vowel Strings in Range

---
+++ UnitTests/2500/S02586Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02586Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02586();
+        sut.VowelStrings(new[] {"are", "amy", "u"}, 0, 2).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02586();
+        sut.VowelStrings(new[] {"hey", "aeo", "mu", "ooo", "artro"}, 1, 4).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/2500/S02586.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode_sharp;
+
+// 2586. Count the Number of Vowel Strings in Range
+// https://leetcode.com/problems/count-the-number-of-vowel-strings-in-range
+public class S02586
+{
+    private readonly HashSet<char> _vowels = new() {'a', 'e', 'i', 'o', 'u'};
+    
+    public int VowelStrings(string[] words, int left, int right)
+    {
+        return words.Skip(left).Take(right - left + 1).Count(isVowelString);
+    }
+
+    private bool isVowelString(string s)
+    {
+        return _vowels.Contains(s[0]) && _vowels.Contains(s[^1]);
+    }
+}
