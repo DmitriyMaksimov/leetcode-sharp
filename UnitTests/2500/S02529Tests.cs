776423f098f9fe2e3e34a799e1131af47803532b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-09-30 16:22:09 +1000

    2529. Maximum Count of Positive Integer and Negative Integer

---
+++ UnitTests/2500/S02529Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02529Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02529();
+        sut.MaximumCount(new[] {-2, -1, -1, 1, 2, 3}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02529();
+        sut.MaximumCount(new[] {-3, -2, -1, 0, 0, 1, 2}).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02529();
+        sut.MaximumCount(new[] {5, 20, 66, 1314}).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/2500/S02529.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 2529. Maximum Count of Positive Integer and Negative Integer
+// https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer
+public class S02529
+{
+    public int MaximumCount(int[] nums)
+    {
+        var negativeCount = 0;
+        var positiveCount = 0;
+        
+        foreach (var num in nums)
+        {
+            if (num > 0)
+            {
+                ++positiveCount;
+            }
+            else if (num < 0)
+            {
+                ++negativeCount;
+            }
+        }
+
+        return Math.Max(positiveCount, negativeCount);
+    }
+}
