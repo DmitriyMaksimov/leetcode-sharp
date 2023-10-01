a4ee8c878af01944b7420dd04e720d2fff355a0c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-01 13:37:34 +1000

    2558. Take Gifts From the Richest Pile

---
+++ UnitTests/2500/S02558Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02558Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02558();
+        sut.PickGifts(new[] {25, 64, 9, 4, 100}, 4).Should().Be(29);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02558();
+        sut.PickGifts(new[] {1, 1, 1, 1}, 4).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/2500/S02558.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 
+// https://leetcode.com/problems/take-gifts-from-the-richest-pile
+public class S02558
+{
+    public long PickGifts(int[] gifts, int k)
+    {
+        var ans = 0L;
+        var queue = new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => y - x));
+
+        for (var i = 0; i < gifts.Length; i++)
+        {
+            queue.Enqueue(i, gifts[i]);
+        }
+
+        while (k-- > 0)
+        {
+            if (queue.TryDequeue(out var element, out var priority))
+            {
+                queue.Enqueue(element, (int)Math.Floor(Math.Sqrt(priority)));
+            }
+        }
+
+        while (queue.Count > 0)
+        {
+            if (queue.TryDequeue(out _, out var priority))
+            {
+                ans += priority;
+            }
+        }
+
+        return ans;
+    }
+}
