447ad78712bfc038e87908746faa1029d7828047
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-01 14:40:55 +1000

    2570. Merge Two 2D Arrays by Summing Values

---
+++ UnitTests/2500/S02570Tests.cs
@@ -0,0 +1,27 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02570Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02570();
+        sut.MergeArrays(
+            new[] {new[] {1, 2}, new[] {2, 3}, new[] {4, 5}},
+            new[] {new[] {1, 4}, new[] {3, 2}, new[] {4, 1}}
+        ).Should().BeEquivalentTo(new[] {new[] {1, 6}, new[] {2, 3}, new[] {3, 2}, new[] {4, 6}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02570();
+        sut.MergeArrays(
+            new[] {new[] {2, 4}, new[] {3, 6}, new[] {5, 5}},
+            new[] {new[] {1, 3}, new[] {4, 3}}
+        ).Should().BeEquivalentTo(new[] {new[] {1, 3}, new[] {2, 4}, new[] {3, 6}, new[] {4, 3}, new[] {5, 5}});
+    }
+}
---
+++ leetcode-sharp/2500/S02570.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 2570. Merge Two 2D Arrays by Summing Values
+// https://leetcode.com/problems/merge-two-2d-arrays-by-summing-values
+public class S02570
+{
+    public int[][] MergeArrays(int[][] nums1, int[][] nums2)
+    {
+        var result = new List<int[]>();
+        var p1 = 0;
+        var p2 = 0;
+
+        while (p1 < nums1.Length && p2 < nums2.Length)
+        {
+            if (nums1[p1][0] == nums2[p2][0])
+            {
+                result.Add(new[] {nums1[p1][0], nums1[p1++][1] + nums2[p2++][1]});
+            }
+            else if (nums1[p1][0] < nums2[p2][0])
+            {
+                result.Add(nums1[p1++]);
+            }
+            else
+            {
+                result.Add(nums2[p2++]);
+            }
+        }
+
+        while (p1 < nums1.Length)
+        {
+            result.Add(nums1[p1++]);
+        }
+
+        while (p2 < nums2.Length)
+        {
+            result.Add(nums2[p2++]);
+        }
+
+        return result.ToArray();
+    }
+}
