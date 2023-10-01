df573fe2ca4471e9a8766146f5abb819724c8bef
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-01 17:46:55 +1000

    2609. Find the Longest Balanced Substring of a Binary String

---
+++ UnitTests/2600/S02609Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02609Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02609();
+        sut.FindTheLongestBalancedSubstring("01000111").Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02609();
+        sut.FindTheLongestBalancedSubstring("00111").Should().Be(4);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02609();
+        sut.FindTheLongestBalancedSubstring("111").Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2600/S02609.cs
@@ -0,0 +1,37 @@
+﻿namespace leetcode_sharp;
+
+// 2609. Find the Longest Balanced Substring of a Binary String
+// https://leetcode.com/problems/find-the-longest-balanced-substring-of-a-binary-string
+public class S02609
+{
+    public int FindTheLongestBalancedSubstring(string s)
+    {
+        var max = 0;
+        
+        for (var i = 0; i < s.Length;)
+        {
+            var zeros = 0;
+            var ones = 0;
+
+            while (i < s.Length && s[i] == '0')
+            {
+                zeros++;
+                i++;
+            }
+
+            while (i < s.Length && s[i] == '1' && zeros > ones)
+            {
+                ones++;
+                i++;
+                max = Math.Max(max, ones * 2);
+            }
+
+            while (i < s.Length && s[i] == '1')
+            {
+                i++;
+            }
+        }
+
+        return max;
+    }
+}
