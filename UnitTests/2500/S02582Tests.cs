a8ad21b44d20da291c9a6e71c26925113af97a11
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-01 16:46:20 +1000

    2582. Pass the Pillow

---
+++ UnitTests/2500/S02582Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02582Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02582();
+        sut.PassThePillow(4, 5).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02582();
+        sut.PassThePillow(3, 2).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/2500/S02582.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 2582. Pass the Pillow
+// https://leetcode.com/problems/pass-the-pillow
+public class S02582
+{
+    public int PassThePillow(int n, int time)
+    {
+        var turns = time / (n - 1);
+        
+        time %= n - 1;
+
+        if (turns % 2 == 0)
+        {
+            return 1 + time;
+        }
+
+        return n - time;
+    }
+}
