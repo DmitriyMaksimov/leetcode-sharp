7ddf958416b687adbd3a501f5735b41999e40154
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-01 16:58:34 +1000

    2591. Distribute Money to Maximum Children

---
+++ UnitTests/2500/S02591Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02591Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02591();
+        sut.DistMoney(20, 3).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02591();
+        sut.DistMoney(16, 2).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/2500/S02591.cs
@@ -0,0 +1,45 @@
+﻿namespace leetcode_sharp;
+
+// 2591. Distribute Money to Maximum Children
+// https://leetcode.com/problems/distribute-money-to-maximum-children
+public class S02591
+{
+    public int DistMoney(int money, int children)
+    {
+        if (money < children)
+        {
+            return -1;
+        }
+
+        var ans = 0;
+
+        for (var j = 1; j <= children; j++)
+        {
+            var leftMoney = money - 8;
+            var leftChildren = children - j;
+            if (leftMoney >= leftChildren)
+            {
+                money = leftMoney;
+                ans += 1;
+            }
+            else
+            {
+                leftChildren = children - j + 1;
+                if (leftChildren == 1 && money == 4)
+                {
+                    ans -= 1;
+                }
+
+                money = 0;
+                break;
+            }
+        }
+
+        if (money > 0)
+        {
+            ans -= 1;
+        }
+
+        return ans;
+    }
+}
