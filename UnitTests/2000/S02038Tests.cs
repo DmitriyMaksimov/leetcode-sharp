ce6ea694bae51b641437c5c6e0cce52597417570
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-02 11:41:08 +1000

    2038. Remove Colored Pieces if Both Neighbors are the Same Color

---
+++ UnitTests/2000/S02038Tests.cs
@@ -0,0 +1,35 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02038Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02038();
+        sut.WinnerOfGame("AAABABB").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02038();
+        sut.WinnerOfGame("AA").Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02038();
+        sut.WinnerOfGame("ABBBBBBBAAA").Should().BeFalse();
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S02038();
+        sut.WinnerOfGame("AAAAABBB").Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/2000/S02038.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode_sharp;
+
+// 2038. Remove Colored Pieces if Both Neighbors are the Same Color
+// https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color
+public class S02038
+{
+    public bool WinnerOfGame(string colors)
+    {
+        var countA = 0;
+        var countB = 0;
+
+        for (var i = 0; i < colors.Length; i++)
+        {
+            var color = colors[i];
+            var count = 0;
+
+            while (i < colors.Length && colors[i] == color)
+            {
+                i++;
+                count++;
+            }
+
+            switch (color)
+            {
+                case 'A':
+                    countA += Math.Max(count - 2, 0);
+                    break;
+                case 'B':
+                    countB += Math.Max(count - 2, 0);
+                    break;
+            }
+
+            i--;
+        }
+
+        return countA > countB;
+    }
+}
