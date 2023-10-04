21170f8751abf4508a5bdcdfdbdc190ba89fab50
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-04 17:43:33 +1000

    706. Design HashMap

---
+++ UnitTests/0700/S00706Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00706Tests
+{
+    [Test]
+    public void T1()
+    {
+        var myHashMap = new S00706.MyHashMap();
+        myHashMap.Put(1, 1); // The map is now [[1,1]]
+        myHashMap.Put(2, 2); // The map is now [[1,1], [2,2]]
+        myHashMap.Get(1).Should().Be(1); // return 1, The map is now [[1,1], [2,2]]
+        myHashMap.Get(3).Should().Be(-1); // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+        myHashMap.Put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+        myHashMap.Get(2).Should().Be(1); // return 1, The map is now [[1,1], [2,1]]
+        myHashMap.Remove(2); // remove the mapping for 2, The map is now [[1,1]]
+        myHashMap.Get(2).Should().Be(-1); // return -1 (i.e., not found), The map is now [[1,1]]   
+    }
+}
---
+++ leetcode-sharp/0700/S00706.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 706. Design HashMap
+// https://leetcode.com/problems/design-hashmap
+public static class S00706
+{
+    public class MyHashMap
+    {
+        private readonly Dictionary<int, int> _dictionary = new();
+
+        public void Put(int key, int value)
+        {
+            _dictionary[key] = value;
+        }
+
+        public int Get(int key)
+        {
+            return _dictionary.GetValueOrDefault(key, -1);
+        }
+
+        public void Remove(int key)
+        {
+            _dictionary.Remove(key);
+        }
+    }
+}
