8169d185cfb7ed95be8907c83b228d1264adfd79
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-05 15:34:38 +1000

    229. Majority Element II

---
+++ UnitTests/0200/S00229Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00229Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00229();
+        sut.MajorityElement(new[] {3, 2, 3}).Should().Equal(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00229();
+        sut.MajorityElement(new[] {1}).Should().Equal(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00229();
+        sut.MajorityElement(new[] {1, 2}).Should().Equal(1, 2);
+    }
+}
---
+++ leetcode-sharp/0200/S00229.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 229. Majority Element II
+// https://leetcode.com/problems/majority-element-ii
+public class S00229
+{
+    public IList<int> MajorityElement(int[] nums)
+    {
+        return nums.GroupBy(n => n).Where(g => g.Count() > nums.Length / 3).Select(x => x.Key).ToArray();
+    }
+}
