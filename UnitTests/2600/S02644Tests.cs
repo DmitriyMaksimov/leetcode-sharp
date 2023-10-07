ca99180dfe7bf6394a6d7c5480c8def532cef3ab
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-07 14:12:32 +1000

    2644. Find the Maximum Divisibility Score

---
+++ UnitTests/2600/S02644Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02644Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02644();
+        sut.MaxDivScore(new[] {4, 7, 9, 3, 9}, new[] {5, 2, 3}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02644();
+        sut.MaxDivScore(new[] {20, 14, 21, 10}, new[] {5, 7, 5}).Should().Be(5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02644();
+        sut.MaxDivScore(new[] {12}, new[] {10, 16}).Should().Be(10);
+    }
+}
---
+++ leetcode-sharp/2600/S02644.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 2644. Find the Maximum Divisibility Score
+// https://leetcode.com/problems/find-the-maximum-divisibility-score
+public class S02644
+{
+    public int MaxDivScore(int[] nums, int[] divisors)
+    {
+        var result = 0;
+        var current = -1;
+
+        foreach (var divisor in divisors)
+        {
+            var score = nums.Sum(num => num % divisor == 0 ? 1 : 0);
+
+            if (score >= current)
+            {
+                result = score == current ? Math.Min(result, divisor) : divisor;
+                current = score;
+            }
+        }
+
+        return result;
+    }
+}
