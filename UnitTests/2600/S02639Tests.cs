daa17a7a39ad0e0ddaf6d3bd746f98140cd51501
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-07 14:04:18 +1000

    2639. Find the Width of Columns of a Grid

---
+++ UnitTests/2600/S02639Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02639Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02639();
+        sut.FindColumnWidth(new[] {new[] {1}, new[] {22}, new[] {333}}).Should().Equal(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02639();
+        sut.FindColumnWidth(new[] {new[] {-15, 1, 3}, new[] {15, 7, 12}, new[] {5, 6, -2}}).Should().Equal(3, 1, 2);
+    }
+}
---
+++ leetcode-sharp/2600/S02639.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 2639. Find the Width of Columns of a Grid
+// https://leetcode.com/problems/find-the-width-of-columns-of-a-grid
+public class S02639
+{
+    public int[] FindColumnWidth(int[][] grid)
+    {
+        var ans = new int[grid[0].Length];
+        for (var i = 0; i < grid[0].Length; ++i)
+        {
+            foreach (var row in grid)
+            {
+                ans[i] = Math.Max(ans[i], row[i].ToString().Length);
+            }
+        }
+
+        return ans;
+    }
+}
