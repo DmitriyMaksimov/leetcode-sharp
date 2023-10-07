3b9fb8b8b75d678ecd92ac53c45ffe261d8fc049
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-07 13:42:09 +1000

    2614. Prime In Diagonal

---
+++ UnitTests/2600/S02614Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02614Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02614();
+        sut.DiagonalPrime(new[] {new[] {1, 2, 3}, new[] {5, 6, 7}, new[] {9, 10, 11}}).Should().Be(11);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02614();
+        sut.DiagonalPrime(new[] {new[] {1, 2, 3}, new[] {5, 17, 7}, new[] {9, 11, 10}}).Should().Be(17);
+    }
+}
---
+++ leetcode-sharp/2600/S02614.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode_sharp;
+
+// 2614. Prime In Diagonal
+// https://leetcode.com/problems/prime-in-diagonal
+public class S02614
+{
+    public int DiagonalPrime(int[][] nums)
+    {
+        var result = 0;
+        var n = nums.Length;
+
+        for (var i = 0; i < n; i++)
+        {
+            if (isPrime(nums[i][i]))
+            {
+                result = Math.Max(result, nums[i][i]);
+            }
+
+            if (isPrime(nums[i][n - 1 - i]))
+            {
+                result = Math.Max(result, nums[i][n - 1 - i]);
+            }
+        }
+
+        return result;
+    }
+
+    private static bool isPrime(int a)
+    {
+        if (a <= 1) return false;
+
+        for (var i = 2; i <= Math.Sqrt(a); i++)
+        {
+            if (a % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
