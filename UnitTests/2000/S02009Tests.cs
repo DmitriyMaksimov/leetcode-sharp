12988acec25ecc0acc13ad33f1940c8c0f84a5aa
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-10 12:40:02 +1000

    2009. Minimum Number of Operations to Make Array Continuous

---
+++ UnitTests/2000/S02009Tests.cs
@@ -0,0 +1,35 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02009Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02009();
+        sut.MinOperations(new[] {4, 2, 5, 3}).Should().Be(0);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02009();
+        sut.MinOperations(new[] {1, 2, 3, 5, 6}).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02009();
+        sut.MinOperations(new[] {1, 10, 100, 1000}).Should().Be(3);
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S02009();
+        sut.MinOperations(new[] {8, 10, 16, 18, 10, 10, 16, 13, 13, 16}).Should().Be(6);
+    }
+}
---
+++ leetcode-sharp/2000/S02009.cs
@@ -0,0 +1,37 @@
+﻿namespace leetcode_sharp;
+
+// 2009. Minimum Number of Operations to Make Array Continuous
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-array-continuous
+public class S02009
+{
+    public int MinOperations(int[] nums)
+    {
+        var n = nums.Length;
+        Array.Sort(nums);
+        var numberOfUniqElements = 1;
+
+        // Remove duplicate elements in the sorted array
+        for (var i = 1; i < n; ++i)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                nums[numberOfUniqElements++] = nums[i];
+            }
+        }
+
+        var ans = n;
+
+        // Use a sliding window
+        for (int i = 0, j = 0; i < numberOfUniqElements; ++i)
+        {
+            while (j < numberOfUniqElements && nums[j] - nums[i] <= n - 1)
+            {
+                ++j;
+            }
+
+            ans = Math.Min(ans, n - (j - i));
+        }
+
+        return ans;
+    }
+}
