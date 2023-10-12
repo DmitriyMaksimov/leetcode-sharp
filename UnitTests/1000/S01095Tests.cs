96176014e3d89d5746a02e1ce4bfd4b56725d8e3
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-13 07:36:35 +1000

    1095. Find in Mountain Array

---
+++ UnitTests/1000/S01095Tests.cs
@@ -0,0 +1,23 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01095Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01095();
+        var mountainArray = new S01095.MountainArray(new[] {1, 2, 3, 4, 5, 3, 1});
+        sut.FindInMountainArray(3, mountainArray).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01095();
+        var mountainArray = new S01095.MountainArray(new[] {0, 1, 2, 4, 2, 1});
+        sut.FindInMountainArray(3, mountainArray).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/1000/S01095.cs
@@ -0,0 +1,92 @@
+﻿namespace leetcode_sharp;
+
+// 1095. Find in Mountain Array
+// https://leetcode.com/problems/find-in-mountain-array
+public class S01095
+{
+    public class MountainArray
+    {
+        private readonly IList<int> _list;
+
+        public MountainArray(IList<int> list)
+        {
+            _list = list;
+        }
+        
+        public int Get(int index)
+        {
+            return _list[index];
+        }
+
+        public int Length()
+        {
+            return _list.Count;
+        }
+    }
+
+    public int FindInMountainArray(int target, MountainArray mountainArr)
+    {
+        var peak = peakIndex(mountainArr);
+        var index = binarySearch(mountainArr, target, 0, peak);
+
+        return index != -1 ? index : binarySearch(mountainArr, target, peak + 1, mountainArr.Length() - 1);
+    }
+
+    private static int peakIndex(MountainArray mountainArray)
+    {
+        var si = 0;
+        var ei = mountainArray.Length() - 1;
+        while (si < ei)
+        {
+            var mid = si + (ei - si) / 2;
+            if (mountainArray.Get(mid) > mountainArray.Get(mid + 1))
+            {
+                ei = mid;
+            }
+            else
+            {
+                si = mid + 1;
+            }
+        }
+
+        return si;
+    }
+
+    private static int binarySearch(MountainArray mountainArray, int target, int si, int ei)
+    {
+        var isAsc = mountainArray.Get(si) < mountainArray.Get(ei);
+        while (si <= ei)
+        {
+            var mid = si + (ei - si) / 2;
+            if (target == mountainArray.Get(mid))
+            {
+                return mid;
+            }
+
+            if (isAsc)
+            {
+                if (target > mountainArray.Get(mid))
+                {
+                    si = mid + 1;
+                }
+                else
+                {
+                    ei = mid - 1;
+                }
+            }
+            else
+            {
+                if (target > mountainArray.Get(mid))
+                {
+                    ei = mid - 1;
+                }
+                else
+                {
+                    si = mid + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
