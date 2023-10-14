89614f9b8f326e4b652aae9dbbfc2b4cc7d8ddd9
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-14 11:04:24 +1000

    2742. Painting the Walls

---
+++ UnitTests/2700/S02742Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02742Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02742();
+        sut.PaintWalls(new[] {1, 2, 3, 2}, new[] {1, 2, 3, 2}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02742();
+        sut.PaintWalls(new[] {2, 3, 4, 2}, new[] {1, 1, 1, 1}).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/2700/S02742.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 2742. Painting the Walls
+// https://leetcode.com/problems/painting-the-walls
+public class S02742
+{
+    private long[,] _dp = null!;
+
+    public int PaintWalls(int[] cost, int[] time)
+    {
+        _dp = new long[cost.Length + 1, cost.Length + 1];
+
+        return (int) helper(cost, time, 0, 0);
+    }
+
+    private long helper(IReadOnlyList<int> cost, IReadOnlyList<int> time, int i, int t)
+    {
+        if (t >= cost.Count)
+        {
+            return 0;
+        }
+
+        if (i == cost.Count)
+        {
+            return int.MaxValue;
+        }
+
+        if (_dp[i, t] != 0)
+        {
+            return _dp[i, t];
+        }
+
+        return _dp[i, t] = Math.Min(cost[i] + helper(cost, time, i + 1, t + 1 + time[i]), helper(cost, time, i + 1, t));
+    }
+}
