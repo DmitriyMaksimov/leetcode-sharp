4af64536f421f461ac25199c779bfc7e5c21d05e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-15 11:55:05 +1000

    1269. Number of Ways to Stay in the Same Place After Some Steps

---
+++ UnitTests/1200/S01269Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01269Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01269();
+        sut.NumWays(3, 2).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01269();
+        sut.NumWays(2, 4).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01269();
+        sut.NumWays(4, 2).Should().Be(8);
+    }
+}
---
+++ leetcode-sharp/1200/S01269.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 1269. Number of Ways to Stay in the Same Place After Some Steps
+// https://leetcode.com/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps
+public class S01269
+{
+    public int NumWays(int steps, int arrLen)
+    {
+        const int kMod = 1_000_000_007;
+        var n = Math.Min(arrLen, steps / 2 + 1);
+        var dp = new long[n];
+        dp[0] = 1;
+
+        while (steps-- > 0)
+        {
+            var newDp = new long[n];
+
+            for (var i = 0; i < n; ++i)
+            {
+                newDp[i] = dp[i];
+
+                if (i - 1 >= 0)
+                {
+                    newDp[i] += dp[i - 1];
+                }
+
+                if (i + 1 < n)
+                {
+                    newDp[i] += dp[i + 1];
+                }
+
+                // Take the modulo to avoid integer overflow.
+                newDp[i] %= kMod;
+            }
+
+            dp = newDp;
+        }
+
+        return (int) dp[0];
+    }
+}
