4920652cf8047f072f57c5c26c03b91b9272b877
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-17 12:18:38 +1000

    1361. Validate Binary Tree Nodes

---
+++ UnitTests/1300/S01361Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01361Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01361();
+        sut.ValidateBinaryTreeNodes(4, new[] {1, -1, 3, -1}, new[] {2, -1, -1, -1}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01361();
+        sut.ValidateBinaryTreeNodes(4, new[] {1, -1, 3, -1}, new[] {2, 3, -1, -1}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01361();
+        sut.ValidateBinaryTreeNodes(2, new[] {1, 0}, new[] {-1, -1}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/1300/S01361.cs
@@ -0,0 +1,80 @@
+﻿namespace leetcode_sharp;
+
+// 1361. Validate Binary Tree Nodes
+// https://leetcode.com/problems/validate-binary-tree-nodes
+public class S01361
+{
+    public bool ValidateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild)
+    {
+        var graph = new Dictionary<int, List<int>>();
+        var inDegree = new int[n];
+
+        for (var node = 0; node < n; node++)
+        {
+            var left = leftChild[node];
+            var right = rightChild[node];
+            if (left != -1)
+            {
+                if (!graph.ContainsKey(node))
+                {
+                    graph[node] = new List<int>();
+                }
+
+                graph[node].Add(left);
+                inDegree[left]++;
+            }
+
+            if (right != -1)
+            {
+                if (!graph.ContainsKey(node))
+                {
+                    graph[node] = new List<int>();
+                }
+
+                graph[node].Add(right);
+                inDegree[right]++;
+            }
+        }
+
+        var rootCandidates = new List<int>();
+        for (var node = 0; node < n; node++)
+        {
+            if (inDegree[node] == 0)
+            {
+                rootCandidates.Add(node);
+            }
+        }
+
+        if (rootCandidates.Count != 1)
+        {
+            return false;
+        }
+
+        var root = rootCandidates[0];
+
+        var queue = new Queue<int>();
+        var seen = new HashSet<int>();
+        queue.Enqueue(root);
+        seen.Add(root);
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            if (graph.TryGetValue(node, out var value))
+            {
+                foreach (var child in value)
+                {
+                    if (seen.Contains(child))
+                    {
+                        return false;
+                    }
+
+                    seen.Add(child);
+                    queue.Enqueue(child);
+                }
+            }
+        }
+
+        return seen.Count == n;
+    }
+}
