f8768df6f60211b980dac259ea6e2c5363ad52d4
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-18 13:25:51 +1000

    2050. Parallel Courses III

---
+++ UnitTests/2000/S02050Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02050Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02050();
+        sut.MinimumTime(3, new[] {new[] {1, 3}, new[] {2, 3}}, new[] {3, 2, 5}).Should().Be(8);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02050();
+        sut.MinimumTime(5, new[] {new[] {1, 5}, new[] {2, 5}, new[] {3, 5}, new[] {3, 4}, new[] {4, 5}},
+            new[] {1, 2, 3, 4, 5}).Should().Be(12);
+    }
+}
---
+++ leetcode-sharp/2000/S02050.cs
@@ -0,0 +1,49 @@
+﻿namespace leetcode_sharp;
+
+// 2050. Parallel Courses III
+// https://leetcode.com/problems/parallel-courses-iii
+public class S02050
+{
+    public int MinimumTime(int n, int[][] relations, int[] time)
+    {
+        var graph = new List<List<int>>();
+        for (var i = 0; i < n; i++)
+        {
+            graph.Add(new List<int>());
+        }
+
+        foreach (var relation in relations)
+        {
+            var prev = relation[0] - 1;
+            var next = relation[1] - 1;
+            graph[next].Add(prev);
+        }
+
+        var memo = new int[n];
+        var overallMinTime = 0;
+
+        for (var course = 0; course < n; course++)
+        {
+            overallMinTime = Math.Max(overallMinTime, calculateTime(course, graph, time, memo));
+        }
+
+        return overallMinTime;
+    }
+
+    private static int calculateTime(int course, List<List<int>> graph, IReadOnlyList<int> time, IList<int> memo)
+    {
+        if (memo[course] != 0)
+        {
+            return memo[course];
+        }
+
+        var maxPrerequisiteTime = 0;
+        foreach (var prerequisite in graph[course])
+        {
+            maxPrerequisiteTime = Math.Max(maxPrerequisiteTime, calculateTime(prerequisite, graph, time, memo));
+        }
+
+        memo[course] = maxPrerequisiteTime + time[course];
+        return memo[course];
+    }
+}
