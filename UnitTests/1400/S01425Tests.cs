04d023884fc142c2a6d69613657f5d6915b22f1c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-21 10:58:20 +1000

    1425. Constrained Subsequence Sum

---
+++ UnitTests/1400/S01425Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01425Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01425();
+        sut.ConstrainedSubsetSum(new[] {10, 2, -10, 5, 20}, 2).Should().Be(37);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01425();
+        sut.ConstrainedSubsetSum(new[] {-1, -2, -3}, 1).Should().Be(-1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01425();
+        sut.ConstrainedSubsetSum(new[] {10, -2, -10, -5, 20}, 2).Should().Be(23);
+    }
+}
---
+++ leetcode-sharp/1400/S01425.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_sharp;
+
+// 1425. Constrained Subsequence Sum
+// https://leetcode.com/problems/constrained-subsequence-sum
+public class S01425
+{
+    public int ConstrainedSubsetSum(int[] nums, int k)
+    {
+        var linkedList = new LinkedList<int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            nums[i] += linkedList.Count > 0 ? nums[linkedList.First!.Value] : 0;
+
+            while (linkedList.Count > 0 && (i - linkedList.First!.Value >= k || nums[i] >= nums[linkedList.Last!.Value]))
+            {
+                if (nums[i] >= nums[linkedList.Last!.Value])
+                {
+                    linkedList.RemoveLast();
+                }
+                else
+                {
+                    linkedList.RemoveFirst();
+                }
+            }
+
+            if (nums[i] > 0)
+            {
+                linkedList.AddLast(i);
+            }
+        }
+
+        return nums.Max();
+    }
+}
