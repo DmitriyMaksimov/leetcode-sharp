3084d051886488f7b22d932b507d1779dad20232
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-22 12:41:07 +1000

    1793. Maximum Score of a Good Subarray

---
+++ UnitTests/1700/S01793Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01793Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01793();
+        sut.MaximumScore(new[] {1, 4, 3, 7, 4, 5}, 3).Should().Be(15);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01793();
+        sut.MaximumScore(new[] {5, 5, 4, 5, 4, 1, 1, 1}, 0).Should().Be(20);
+    }
+}
---
+++ leetcode-sharp/1700/S01793.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 1793. Maximum Score of a Good Subarray
+// https://leetcode.com/problems/maximum-score-of-a-good-subarray
+public class S01793
+{
+    public int MaximumScore(int[] nums, int k)
+    {
+        var left = k;
+        var right = k;
+        var minVal = nums[k];
+        var maxScore = minVal;
+
+        while (left > 0 || right < nums.Length - 1)
+        {
+            if (left == 0 || (right < nums.Length - 1 && nums[right + 1] > nums[left - 1]))
+            {
+                right++;
+            }
+            else
+            {
+                left--;
+            }
+
+            minVal = Math.Min(minVal, Math.Min(nums[left], nums[right]));
+            maxScore = Math.Max(maxScore, minVal * (right - left + 1));
+        }
+
+        return maxScore;
+    }
+}
