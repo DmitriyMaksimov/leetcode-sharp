ba176d635fab0acd0f44f15590d4a979a79fd1bb
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-24 15:36:07 +1000

    515. Find Largest Value in Each Tree Row

---
+++ UnitTests/0500/S00515Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00515Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00515();
+        sut.LargestValues(TreeNode.Parse("[1,3,2,5,3,null,9]")).Should().Equal(1, 3, 9);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00515();
+        sut.LargestValues(TreeNode.Parse("[1,2,3]")).Should().Equal(1, 3);
+    }
+}
---
+++ leetcode-sharp/0500/S00515.cs
@@ -0,0 +1,34 @@
+﻿namespace leetcode_sharp;
+
+// 515. Find Largest Value in Each Tree Row
+// https://leetcode.com/problems/find-largest-value-in-each-tree-row
+public class S00515
+{
+    public IList<int> LargestValues(TreeNode? root)
+    {
+        if (root == null) return new List<int>();
+
+        var result = new List<int>();
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            var currLevelSize = queue.Count;
+            var maxVal = Int32.MinValue;
+
+            for (var i = 0; i < currLevelSize; i++)
+            {
+                var node = queue.Dequeue();
+                maxVal = Math.Max(maxVal, node.val);
+
+                if (node.left != null) queue.Enqueue(node.left);
+                if (node.right != null) queue.Enqueue(node.right);
+            }
+
+            result.Add(maxVal);
+        }
+
+        return result;
+    }
+}
