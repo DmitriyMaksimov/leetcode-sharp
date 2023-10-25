db8e0bb6dc7f5d6ee0b9512eed7838a5e28efc5e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-25 14:56:01 +1000

    779. K-th Symbol in Grammar

---
+++ UnitTests/0700/S00779Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00779Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00779();
+        sut.KthGrammar(1, 1).Should().Be(0);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00779();
+        sut.KthGrammar(2, 1).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00779();
+        sut.KthGrammar(2, 2).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/0700/S00779.cs
@@ -0,0 +1,15 @@
+﻿namespace leetcode_sharp;
+
+// 779. K-th Symbol in Grammar
+// https://leetcode.com/problems/k-th-symbol-in-grammar
+public class S00779
+{
+    public int KthGrammar(int n, int k)
+    {
+        if (n == 1) return 0;
+        
+        var length = 1 << (n - 2);
+
+        return k <= length ? KthGrammar(n - 1, k) : 1 - KthGrammar(n - 1, k - length);
+    }
+}
