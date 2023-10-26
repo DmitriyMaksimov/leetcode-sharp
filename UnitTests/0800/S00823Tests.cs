0f80fd41884bac9b845a79a7afae2853517c77fc
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-26 14:31:18 +1000

    823. Binary Trees With Factors

---
+++ UnitTests/0800/S00823Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00823Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00823();
+        sut.NumFactoredBinaryTrees(new[] {2, 4}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00823();
+        sut.NumFactoredBinaryTrees(new[] {2, 4, 5, 10}).Should().Be(7);
+    }
+}
---
+++ leetcode-sharp/0800/S00823.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 823. Binary Trees With Factors
+// https://leetcode.com/problems/binary-trees-with-factors
+public class S00823
+{
+    private const int Mod = 1_000_000_007;
+
+    public int NumFactoredBinaryTrees(int[] arr)
+    {
+        Array.Sort(arr);
+        var s = new HashSet<int>(arr);
+        var dp = new Dictionary<int, long>();
+        foreach (var x in arr)
+        {
+            dp[x] = 1;
+        }
+
+        foreach (var i in arr)
+        {
+            foreach (var j in arr)
+            {
+                if (j > Math.Sqrt(i)) break;
+                if (i % j == 0 && s.Contains(i / j))
+                {
+                    var temp = dp[j] * dp[i / j];
+                    dp[i] = (dp[i] + (i / j == j ? temp : temp * 2)) % Mod;
+                }
+            }
+        }
+
+        var result = 0L;
+
+        foreach (var val in dp.Values)
+        {
+            result = (result + val) % Mod;
+        }
+
+        return (int) result;
+    }
+}
