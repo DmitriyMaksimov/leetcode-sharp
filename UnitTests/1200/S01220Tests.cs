67be4d47e9ff58c03ecf9aac12e6d0549b09297f
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-28 11:25:10 +1000

    1220. Count Vowels Permutation

---
+++ UnitTests/1200/S01220Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01220Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01220();
+        sut.CountVowelPermutation(1).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01220();
+        sut.CountVowelPermutation(2).Should().Be(10);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01220();
+        sut.CountVowelPermutation(5).Should().Be(68);
+    }
+}
---
+++ leetcode-sharp/1200/S01220.cs
@@ -0,0 +1,34 @@
+﻿namespace leetcode_sharp;
+
+// 1220. Count Vowels Permutation
+// https://leetcode.com/problems/count-vowels-permutation
+public class S01220
+{
+    private const int Mod = 1_000_000_007;
+
+    public int CountVowelPermutation(int n)
+    {
+        var a = 1L;
+        var e = 1L;
+        var i = 1L;
+        var o = 1L;
+        var u = 1L;
+
+        for (var j = 1; j < n; j++)
+        {
+            var aNext = e;
+            var eNext = (a + i) % Mod;
+            var iNext = (a + e + o + u) % Mod;
+            var oNext = (i + u) % Mod;
+            var uNext = a;
+
+            a = aNext;
+            e = eNext;
+            i = iNext;
+            o = oNext;
+            u = uNext;
+        }
+
+        return (int) ((a + e + i + o + u) % Mod);
+    }
+}
