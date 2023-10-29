08d48a9afbe2d0b770860aeda42e3d130e5c5a53
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-29 11:34:19 +1000

    458. Poor Pigs

---
+++ UnitTests/0400/S00458Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00458Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00458();
+        sut.PoorPigs(4, 15, 15).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00458();
+        sut.PoorPigs(4, 15, 30).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S00458();
+        sut.PoorPigs(125, 1, 4).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/0400/S00458.cs
@@ -0,0 +1,12 @@
+﻿namespace leetcode_sharp;
+
+// 458. Poor Pigs
+// https://leetcode.com/problems/poor-pigs
+public class S00458
+{
+    public int PoorPigs(int buckets, int minutesToDie, int minutesToTest)
+    {
+        // ReSharper disable once PossibleLossOfFraction
+        return (int)Math.Ceiling(Math.Log2(buckets) / Math.Log2(minutesToTest / minutesToDie + 1));
+    }
+}
