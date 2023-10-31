84d67dbe32f794b7e967a04a6ae87965113d7d38
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-10-31 13:50:52 +1000

    2433. Find The Original Array of Prefix Xor

---
+++ UnitTests/2400/S02433Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02433Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02433();
+        sut.FindArray(new[] {5, 2, 0, 3, 1}).Should().Equal(5, 7, 2, 3, 2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02433();
+        sut.FindArray(new[] {13}).Should().Equal(13);
+    }
+}
