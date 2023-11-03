d3068e6a524a88e312ea5a1cbfc94df0a5727122
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-03 11:16:06 +1000

    1441. Build an Array With Stack Operations

---
+++ UnitTests/1400/S01441Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01441Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01441();
+        sut.BuildArray(new[] {1, 3}, 3).Should().Equal("Push", "Push", "Pop", "Push");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01441();
+        sut.BuildArray(new[] {1, 2, 3}, 3).Should().Equal("Push", "Push", "Push");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01441();
+        sut.BuildArray(new[] {1, 2}, 4).Should().Equal("Push", "Push");
+    }
+}
---
+++ leetcode-sharp/1400/S01441.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 1441. Build an Array With Stack Operations
+// https://leetcode.com/problems/build-an-array-with-stack-operations
+public class S01441
+{
+    public IList<string> BuildArray(int[] target, int n)
+    {
+        var targetSet = new HashSet<int>(target);
+        var result = new List<string>();
+
+        for (var i = 1; i <= target[^1]; i++)
+        {
+            if (targetSet.Contains(i))
+            {
+                result.Add("Push");
+            }
+            else
+            {
+                result.Add("Push");
+                result.Add("Pop");
+            }
+        }
+
+        return result;
+    }
+}
