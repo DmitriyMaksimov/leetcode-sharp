295a23bd16e8b00d167d10690a6d762cb622cfab
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-04 10:30:38 +1000

    1503. Last Moment Before All Ants Fall Out of a Plank

---
+++ UnitTests/1500/S01503Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01503Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01503();
+        sut.GetLastMoment(4, new int[] {4, 3}, new[] {0, 1}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01503();
+        sut.GetLastMoment(7, Array.Empty<int>(), new[] {0, 1, 2, 3, 4, 5, 6, 7}).Should().Be(7);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01503();
+        sut.GetLastMoment(7, new int[] {0, 1, 2, 3, 4, 5, 6, 7}, Array.Empty<int>()).Should().Be(7);
+    }
+}
---
+++ leetcode-sharp/1500/S01503.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 1503. Last Moment Before All Ants Fall Out of a Plank
+// https://leetcode.com/problems/last-moment-before-all-ants-fall-out-of-a-plank
+public class S01503
+{
+    public int GetLastMoment(int n, int[] left, int[] right)
+    {
+        return right.Select(i => n - i).Prepend(left.Prepend(0).Max()).Max();
+    }
+}
