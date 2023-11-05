57755e9d6a53f4538f82e19530f6f0ae96deabaa
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-05 10:49:12 +1000

    1535. Find the Winner of an Array Game

---
+++ UnitTests/1500/S01535Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01535Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01535();
+        sut.GetWinner(new[] {2, 1, 3, 5, 4, 6, 7}, 2).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01535();
+        sut.GetWinner(new[] {3, 2, 1}, 10).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/1500/S01535.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode_sharp;
+
+// 1535. Find the Winner of an Array Game
+// https://leetcode.com/problems/find-the-winner-of-an-array-game
+public class S01535
+{
+    public int GetWinner(int[] arr, int k)
+    {
+        if (k == 1)
+        {
+            return Math.Max(arr[0], arr[1]);
+        }
+
+        if (k >= arr.Length)
+        {
+            return arr.Max();
+        }
+
+        var currentWinner = arr[0];
+        var consecutiveWins = 0;
+
+        for (var i = 1; i < arr.Length; i++)
+        {
+            if (currentWinner > arr[i])
+            {
+                consecutiveWins++;
+            }
+            else
+            {
+                currentWinner = arr[i];
+                consecutiveWins = 1;
+            }
+
+            if (consecutiveWins == k)
+            {
+                return currentWinner;
+            }
+        }
+
+        return currentWinner;
+    }
+}
