c1ee2ab3ebe6a9a6cbe4ee9a9f0e5fb81adf0159
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-06 10:57:48 +1000

    1845. Seat Reservation Manager

---
+++ UnitTests/1800/S01845Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01845Tests
+{
+    [Test]
+    public void T1()
+    {
+        var seatManager = new S01845.SeatManager(5);
+        seatManager.Reserve().Should().Be(1);   // All seats are available, so return the lowest numbered seat, which is 1.
+        seatManager.Reserve().Should().Be(2);   // The available seats are [2,3,4,5], so return the lowest of them, which is 2.
+        seatManager.Unreserve(2);               // Unreserve seat 2, so now the available seats are [2,3,4,5].
+        seatManager.Reserve().Should().Be(2);   // The available seats are [2,3,4,5], so return the lowest of them, which is 2.
+        seatManager.Reserve().Should().Be(3);   // The available seats are [3,4,5], so return the lowest of them, which is 3.
+        seatManager.Reserve().Should().Be(4);   // The available seats are [4,5], so return the lowest of them, which is 4.
+        seatManager.Reserve().Should().Be(5);   // The only available seat is seat 5, so return 5.
+        seatManager.Unreserve(5);               // Unreserve seat 5, so now the available seats are [5].
+    }
+}
---
+++ leetcode-sharp/1800/S01845.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcode_sharp;
+
+// 1845. Seat Reservation Manager
+// https://leetcode.com/problems/seat-reservation-manager
+public static class S01845
+{
+    public class SeatManager
+    {
+        private int _largestReserved = 0;
+        private readonly SortedSet<int> _sortedSet = new();
+        
+        public SeatManager(int n)
+        {
+        }
+
+        public int Reserve()
+        {
+            if (!_sortedSet.Any())
+            {
+                return ++_largestReserved;
+            }
+
+            var seat = _sortedSet.Min;
+            _sortedSet.Remove(seat);
+            return seat;
+        }
+
+        public void Unreserve(int seatNumber)
+        {
+            if (seatNumber == _largestReserved)
+            {
+                --_largestReserved;
+            }
+            else
+            {
+                _sortedSet.Add(seatNumber);
+            }
+        }
+    }
+}
