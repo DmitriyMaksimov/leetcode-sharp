ad35a6a23d81c7c41ca0029b7bd70404ecfd0215
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-07 10:59:39 +1000

    1921. Eliminate Maximum Number of Monsters

---
+++ UnitTests/1900/S01921Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01921Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01921();
+        sut.EliminateMaximum(new[] {1, 3, 4}, new[] {1, 1, 1}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01921();
+        sut.EliminateMaximum(new[] {1, 1, 2, 3}, new[] {1, 1, 1, 1}).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01921();
+        sut.EliminateMaximum(new[] {3, 2, 4}, new[] {5, 3, 2}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1900/S01921.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 1921. Eliminate Maximum Number of Monsters
+// https://leetcode.com/problems/eliminate-maximum-number-of-monsters
+public class S01921
+{
+    public int EliminateMaximum(int[] dist, int[] speed)
+    {
+        var n = dist.Length;
+        var timeToCity = new double[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            timeToCity[i] = (double) dist[i] / speed[i];
+        }
+
+        Array.Sort(timeToCity);
+
+        for (var i = 0; i < n; i++)
+        {
+            if (timeToCity[i] <= i)
+            {
+                return i;
+            }
+        }
+
+        return n;
+    }
+}
