5de443dcb1e42a4b7606a343f0f0cf4f353cafb7
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-08 10:42:27 +1000

    2849. Determine if a Cell Is Reachable at a Given Time

---
+++ UnitTests/2800/S02849Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02849Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02849();
+        sut.IsReachableAtTime(2, 4, 7, 7, 6).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02849();
+        sut.IsReachableAtTime(3, 1, 7, 3, 3).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/2800/S02849.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 2849. Determine if a Cell Is Reachable at a Given Time
+// https://leetcode.com/problems/determine-if-a-cell-is-reachable-at-a-given-time
+public class S02849
+{
+    public bool IsReachableAtTime(int sx, int sy, int fx, int fy, int t)
+    {
+        var xDistance = Math.Abs(sx - fx);
+        var yDistance = Math.Abs(sy - fy);
+        var minDist = Math.Min(xDistance, yDistance) + Math.Abs(yDistance - xDistance);
+
+        if (minDist == 0)
+        {
+            return t != 1;
+        }
+
+        return t >= minDist;
+    }
+}
