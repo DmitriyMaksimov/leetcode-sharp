b575b34af7587d7dfd252c91c96309543353c132
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-09 10:41:00 +1000

    1759. Count Number of Homogenous Substrings

---
+++ UnitTests/1700/S01759Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01759Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01759();
+        sut.CountHomogenous("abbcccaa").Should().Be(13);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01759();
+        sut.CountHomogenous("xy").Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01759();
+        sut.CountHomogenous("zzzzz").Should().Be(15);
+    }
+}
---
+++ leetcode-sharp/1700/S01759.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 1759. Count Number of Homogenous Substrings
+// https://leetcode.com/problems/count-number-of-homogenous-substrings
+public class S01759
+{
+    private const int Mod = 1_000_000_007;
+
+    public int CountHomogenous(string s)
+    {
+        var answer = 0;
+        var currFreq = 0;
+        var lastChar = '\0';
+
+        foreach (var c in s)
+        {
+            if (c == lastChar)
+            {
+                currFreq++;
+            }
+            else
+            {
+                currFreq = 1;
+                lastChar = c;
+            }
+
+            answer = (answer + currFreq) % Mod;
+        }
+
+        return answer;
+    }
+}
