bf93eaadc37b4a26addd3c1f73143df54f1ac92b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-10 10:33:07 +1000

    1743. Restore the Array From Adjacent Pairs

---
+++ UnitTests/1700/S01743Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01743Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01743();
+        sut.RestoreArray(new[] {new[] {2, 1}, new[] {3, 4}, new[] {3, 2}}).Should().Equal(1, 2, 3, 4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01743();
+        sut.RestoreArray(new[] {new[] {4, -2}, new[] {1, 4}, new[] {-3, 1}}).Should().Equal(-2, 4, 1, -3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01743();
+        sut.RestoreArray(new[] {new[] {100000, -100000}}).Should().Equal(100000, -100000);
+    }
+}
---
+++ leetcode-sharp/1700/S01743.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode_sharp;
+
+// 1743. Restore the Array From Adjacent Pairs
+// https://leetcode.com/problems/restore-the-array-from-adjacent-pairs
+public class S01743
+{
+    public int[] RestoreArray(int[][] adjacentPairs)
+    {
+        var ps = new Dictionary<int, List<int>>();
+
+        foreach (var adjacentPair in adjacentPairs)
+        {
+            ps.TryAdd(adjacentPair[0], new List<int>());
+            ps.TryAdd(adjacentPair[1], new List<int>());
+            ps[adjacentPair[0]].Add(adjacentPair[1]);
+            ps[adjacentPair[1]].Add(adjacentPair[0]);
+        }
+
+
+        var res = new List<int>();
+        foreach (var (key, value) in ps)
+        {
+            if (value.Count == 1)
+            {
+                res.Add(key);
+                res.Add(value[0]);
+                break;
+            }
+        }
+
+        while (res.Count < adjacentPairs.Length + 1)
+        {
+            var tail = res[^1];
+            var prev = res[^2];
+            var next = ps[tail];
+
+            res.Add(next[0] != prev ? next[0] : next[1]);
+        }
+
+        return res.ToArray();
+    }
+}
