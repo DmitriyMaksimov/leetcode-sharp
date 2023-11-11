36387dbd4fe109f8a2676242c24dcfce853e9992
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-11 17:49:18 +1000

    2670. Find the Distinct Difference Array

---
+++ UnitTests/2600/S02670Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02670Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02670();
+        sut.DistinctDifferenceArray(new[] {1, 2, 3, 4, 5}).Should().Equal(-3, -1, 1, 3, 5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02670();
+        sut.DistinctDifferenceArray(new[] {3, 2, 3, 4, 2}).Should().Equal(-2, -1, 0, 2, 3);
+    }
+}
---
+++ leetcode-sharp/2600/S02670.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 2670. Find the Distinct Difference Array
+// https://leetcode.com/problems/find-the-distinct-difference-array
+public class S02670
+{
+    public int[] DistinctDifferenceArray(int[] nums)
+    {
+        var n = nums.Length;
+        var result = new int[n];
+        var distinctElements = new HashSet<int> {nums[n - 1]};
+
+        for (var i = n - 2; i >= 0; i--)
+        {
+            result[i] = -distinctElements.Count;
+            distinctElements.Add(nums[i]);
+        }
+
+        distinctElements.Clear();
+
+        for (var i = 0; i <= n - 1; i++)
+        {
+            distinctElements.Add(nums[i]);
+            result[i] += distinctElements.Count;
+        }
+
+        return result;
+    }
+}
