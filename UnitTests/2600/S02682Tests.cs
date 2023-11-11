3c45b1a37b6758737c03d67da0f83b2aaaa556e6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-11 17:55:19 +1000

    2682. Find the Losers of the Circular Game

---
+++ UnitTests/2600/S02682Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02682Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02682();
+        sut.CircularGameLosers(5, 2).Should().Equal(4, 5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02682();
+        sut.CircularGameLosers(4, 4).Should().Equal(2, 3, 4);
+    }
+}
---
+++ leetcode-sharp/2600/S02682.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 2682. Find the Losers of the Circular Game
+// https://leetcode.com/problems/find-the-losers-of-the-circular-game
+public class S02682
+{
+    public int[] CircularGameLosers(int n, int k)
+    {
+        var visited = new bool[n];
+        var current = 0;
+        var step = 1;
+
+        do
+        {
+            visited[current] = true;
+            current = (current + step * k) % n;
+            step++;
+        } while (!visited[current]);
+
+        var result = new List<int>(n);
+
+        for (var i = 0; i < n; i++)
+        {
+            if (!visited[i]) result.Add(i + 1);
+        }
+
+        return result.ToArray();
+    }
+}
