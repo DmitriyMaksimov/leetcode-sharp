7ee4cd30c13008109f49e4857cb159b9ed4af718
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-11 13:09:42 +1000

    2660. Determine the Winner of a Bowling Game

---
+++ UnitTests/2600/S02660Tests.cs
@@ -0,0 +1,35 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02660Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02660();
+        sut.IsWinner(new[] {4, 10, 7, 9}, new[] {6, 5, 2, 3}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02660();
+        sut.IsWinner(new[] {3, 5, 7, 6}, new[] {8, 10, 10, 2}).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02660();
+        sut.IsWinner(new[] {2, 3}, new[] {4, 1}).Should().Be(0);
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S02660();
+        sut.IsWinner(new[] {10, 2, 2, 3}, new[] {3, 8, 4, 5}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/2600/S02660.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode_sharp;
+
+// 2660. Determine the Winner of a Bowling Game
+// https://leetcode.com/problems/determine-the-winner-of-a-bowling-game
+public class S02660
+{
+    public int IsWinner(int[] player1, int[] player2)
+    {
+        var score1 = calculateScore(player1);
+        var score2 = calculateScore(player2);
+
+        if (score1 == score2)
+        {
+            return 0;
+        }
+        
+        return score1 > score2 ? 1 : 2;
+    }
+
+    private static int calculateScore(int[] pins)
+    {
+        var result = 0;
+
+        for (var i = 0; i < pins.Length; i++)
+        {
+            var multiplier = getMultiplier(pins, i);
+            result += multiplier * pins[i];
+        }
+
+        return result;
+    }
+
+    private static int getMultiplier(int[] pins, int index)
+    {
+        return index switch
+        {
+            0 => 1,
+            1 => pins[index - 1] == 10 ? 2 : 1,
+            _ => pins[index - 1] == 10 || pins[index - 2] == 10 ? 2 : 1
+        };
+    }
+}
