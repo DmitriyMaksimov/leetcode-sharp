4b962c2a2d7f7b29c1915e52eedaecc93f0220ac
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 16:27:31 +1000

    2706. Buy Two Chocolates

---
+++ UnitTests/2700/S02706Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02706Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02706();
+        sut.BuyChoco(new[] {1, 2, 2}, 3).Should().Be(0);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02706();
+        sut.BuyChoco(new[] {3, 2, 3}, 3).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/2700/S02706.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 2706. Buy Two Chocolates
+// https://leetcode.com/problems/buy-two-chocolates
+public class S02706
+{
+    public int BuyChoco(int[] prices, int money)
+    {
+        var smallestPrice = int.MaxValue;
+        var secondSmallestPrice = int.MaxValue;
+
+        foreach (var price in prices)
+        {
+            if (smallestPrice > price)
+            {
+                secondSmallestPrice = smallestPrice;
+                smallestPrice = price;
+            }
+            else if (secondSmallestPrice > price)
+            {
+                secondSmallestPrice = price;
+            }
+        }
+
+        var minMoneyForTwoChocolates = smallestPrice + secondSmallestPrice;
+        
+        return minMoneyForTwoChocolates <= money ? money - minMoneyForTwoChocolates : money;
+    }
+}
