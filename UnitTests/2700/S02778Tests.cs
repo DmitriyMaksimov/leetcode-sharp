86e67f4d08db8a6184d3879d5c49947560b9e0ac
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 18:38:20 +1000

    2778. Sum of Squares of Special Elements

---
+++ UnitTests/2700/S02778Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02778Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02778();
+        sut.SumOfSquares(new[] {1, 2, 3, 4}).Should().Be(21);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02778();
+        sut.SumOfSquares(new[] {2, 7, 1, 19, 18, 3}).Should().Be(63);
+    }
+}
---
+++ leetcode-sharp/2700/S02778.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2778. Sum of Squares of Special Elements
+// https://leetcode.com/problems/sum-of-squares-of-special-elements
+public class S02778
+{
+    public int SumOfSquares(int[] nums)
+    {
+        return nums.Where((_, i) => nums.Length % (i + 1) == 0).Sum(x => x * x);
+    }
+}
