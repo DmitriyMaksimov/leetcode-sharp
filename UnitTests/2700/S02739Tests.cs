3521d9d5c27730aa3ebfda95e72fa4908338fe21
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 16:53:05 +1000

    2739. Total Distance Traveled

---
+++ UnitTests/2700/S02739Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02739Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02739();
+        sut.DistanceTraveled(5, 10).Should().Be(60);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02739();
+        sut.DistanceTraveled(1, 2).Should().Be(10);
+    }
+}
---
+++ leetcode-sharp/2700/S02739.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 2739. Total Distance Traveled
+// https://leetcode.com/problems/total-distance-traveled
+public class S02739
+{
+    public int DistanceTraveled(int mainTank, int additionalTank)
+    {
+        var currTank = mainTank;
+        var totalFuelBurnt = 0;
+
+        while (currTank >= 5 && additionalTank > 0)
+        {
+            currTank -= 4;
+            --additionalTank;
+            totalFuelBurnt += 5;
+        }
+
+        totalFuelBurnt += currTank;
+        
+        return totalFuelBurnt * 10;
+    }
+}
