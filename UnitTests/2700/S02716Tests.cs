21268306bdc7feee35697c2a3b11f3cf1903a8a2
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 16:34:21 +1000

    2716. Minimize String Length

---
+++ UnitTests/2700/S02716Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02716Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02716();
+        sut.MinimizedStringLength("aaabc").Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02716();
+        sut.MinimizedStringLength("cbbd").Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02716();
+        sut.MinimizedStringLength("dddaaa").Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/2700/S02716.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2716. Minimize String Length
+// https://leetcode.com/problems/minimize-string-length
+public class S02716
+{
+    public int MinimizedStringLength(string s)
+    {
+        return s.ToCharArray().Distinct().Count();
+    }
+}
