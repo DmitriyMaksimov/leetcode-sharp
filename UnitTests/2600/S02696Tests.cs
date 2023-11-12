85b6ad85b5e7a5b96b1c6231507243fbb63ba981
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 11:18:11 +1000

    2696. Minimum String Length After Removing Substrings

---
+++ UnitTests/2600/S02696Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02696Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02696();
+        sut.MinLength("ABFCACDB").Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02696();
+        sut.MinLength("ACBBD").Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/2600/S02696.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 2696. Minimum String Length After Removing Substrings
+// https://leetcode.com/problems/minimum-string-length-after-removing-substrings
+public class S02696
+{
+    public int MinLength(string s)
+    {
+        var match = new Dictionary<char, char>
+        {
+            {'B', 'A'}, {'D', 'C'}
+        };
+
+        var stack = new Stack<char>();
+
+        foreach (var ch in s)
+        {
+            if (stack.Count > 0 && match.TryGetValue(ch, out var value))
+            {
+                if (value == stack.Peek())
+                {
+                    stack.Pop();
+                    continue;
+                }
+            }
+
+            stack.Push(ch);
+        }
+
+        return stack.Count;
+    }
+}
