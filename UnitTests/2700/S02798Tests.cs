a78add6308ce26fa8e78c3109408c28df89b5fa6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 19:35:58 +1000

    2798. Number of Employees Who Met the Target

---
+++ UnitTests/2700/S02798Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02798Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02798();
+        sut.NumberOfEmployeesWhoMetTarget(new[] {0, 1, 2, 3, 4}, 2).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02798();
+        sut.NumberOfEmployeesWhoMetTarget(new[] {5, 1, 4, 2, 2}, 6).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2700/S02798.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2798. Number of Employees Who Met the Target
+// https://leetcode.com/problems/number-of-employees-who-met-the-target
+public class S02798
+{
+    public int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+    {
+        return hours.Count(x => x >= target);
+    }
+}
