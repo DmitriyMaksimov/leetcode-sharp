3602c9e824d399d108916c578af6d31bc651c822
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 20:50:51 +1000

    2810. Faulty Keyboard

---
+++ UnitTests/2800/S02810Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02810Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02810();
+        sut.FinalString("string").Should().Be("rtsng");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02810();
+        sut.FinalString("poiinter").Should().Be("ponter");
+    }
+}
---
+++ leetcode-sharp/2800/S02810.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 2810. Faulty Keyboard
+// https://leetcode.com/problems/faulty-keyboard
+public class S02810
+{
+    public string FinalString(string s)
+    {
+        var result = "";
+        
+        foreach (var letter in s)
+        {
+            if (letter == 'i')
+            {
+                var reversedString = new string(result.ToCharArray().Reverse().ToArray());
+                result = reversedString;
+            }
+            else
+            {
+                result += letter;
+            }
+        }
+
+        return result;
+    }
+}
