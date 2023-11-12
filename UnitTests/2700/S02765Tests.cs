570cd7a9c83b6e28a81d2b67a65d25e434d30978
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 17:12:44 +1000

    2765. Longest Alternating Subarray

---
+++ UnitTests/2700/S02765Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02765Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02765();
+        sut.AlternatingSubarray(new[] {2, 3, 4, 3, 4}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02765();
+        sut.AlternatingSubarray(new[] {4, 5, 6}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/2700/S02765.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 2765. Longest Alternating Subarray
+// https://leetcode.com/problems/longest-alternating-subarray
+public class S02765
+{
+    public int AlternatingSubarray(int[] nums)
+    {
+        var n = nums.Length;
+        var res = 0;
+
+        for (var i = 0; i < n; ++i)
+        {
+            for (var j = i + 1; j < n && nums[j] == nums[i] + (j - i) % 2; ++j)
+            {
+                res = Math.Max(res, j - i + 1);
+            }
+        }
+
+        return res > 1 ? res : -1;
+    }
+}
