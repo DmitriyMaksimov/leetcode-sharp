da7668e8cc3aa6406e275fcbeae7979593daefe5
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 17:07:51 +1000

    2760. Longest Even Odd Subarray With Threshold

---
+++ UnitTests/2700/S02760Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02760Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02760();
+        sut.LongestAlternatingSubarray(new[] {3, 2, 5, 4}, 5).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02760();
+        sut.LongestAlternatingSubarray(new[] {1, 2}, 2).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02760();
+        sut.LongestAlternatingSubarray(new[] {2, 3, 4, 5}, 4).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/2700/S02760.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 2760. Longest Even Odd Subarray With Threshold
+// https://leetcode.com/problems/longest-even-odd-subarray-with-threshold
+public class S02760
+{
+    public int LongestAlternatingSubarray(int[] nums, int threshold)
+    {
+        var result = 0;
+        
+        for (var left = 0; left < nums.Length; left++)
+        {
+            if (nums[left] % 2 != 0 || nums[left] > threshold) continue;
+            
+            var len = 1;
+
+            for (var right = left + 1; right < nums.Length; right++)
+            {
+                if (nums[right] > threshold || nums[right] % 2 == nums[right - 1] % 2)
+                {
+                    left = right - 1;
+                    break;
+                }
+
+                len++;
+            }
+
+            result = Math.Max(result, len);
+        }
+
+        return result;
+    }
+}
