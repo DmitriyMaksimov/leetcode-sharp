43bf727e5daa3931e7a08f6d5c93020a818ee89b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 19:33:33 +1000

    2788. Split Strings by Separator

---
+++ UnitTests/2700/S02788Tests.cs
@@ -0,0 +1,29 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02788Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02788();
+        sut.SplitWordsBySeparator(new[] {"one.two.three", "four.five", "six"}, '.').Should()
+            .Equal("one", "two", "three", "four", "five", "six");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02788();
+        sut.SplitWordsBySeparator(new[] {"$easy$", "$problem$"}, '$').Should().Equal("easy", "problem");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02788();
+        sut.SplitWordsBySeparator(new[] {"|||"}, '|').Should().BeEmpty();
+    }
+}
---
+++ leetcode-sharp/2700/S02788.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2788. Split Strings by Separator
+// https://leetcode.com/problems/split-strings-by-separator
+public class S02788
+{
+    public IList<string> SplitWordsBySeparator(IList<string> words, char separator)
+    {
+        return words.SelectMany(word => word.Split(separator, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)).ToList();
+    }
+}
