a9655755d600c17e535702a9960435bf7ff34bd8
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 11:45:41 +1000

    815. Bus Routes

---
+++ UnitTests/0800/S00815Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00815Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00815();
+        sut.NumBusesToDestination(new[] {new[] {1, 2, 7}, new[] {3, 6, 7}}, 1, 6).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00815();
+        sut.NumBusesToDestination(new[] {new[] {7, 12}, new[] {4, 5, 15}, new[] {6}, new[] {15, 19}, new[] {9, 12, 13}}, 15, 12).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/0800/S00815.cs
@@ -0,0 +1,55 @@
+﻿namespace leetcode_sharp;
+
+// 815. Bus Routes
+// https://leetcode.com/problems/bus-routes
+public class S00815
+{
+    public int NumBusesToDestination(int[][] routes, int source, int target)
+    {
+        var routeDict = new Dictionary<int, List<int>>();
+        var queue = new Queue<(int stop, int routeLength)>();
+
+        for (var i = 0; i < routes.Length; i++)
+        {
+            for (var j = 0; j < routes[i].Length; j++)
+            {
+                var stop = routes[i][j];
+                if (!routeDict.ContainsKey(stop))
+                {
+                    routeDict.Add(stop, new List<int>());
+                }
+
+                routeDict[stop].Add(i);
+            }
+        }
+
+        var visitedBuses = new HashSet<int>();
+        var visitedStops = new HashSet<int>();
+        queue.Enqueue((source, 0));
+
+        while (queue.Any())
+        {
+            var item = queue.Dequeue();
+            if (item.stop == target)
+            {
+                return item.routeLength;
+            }
+
+            foreach (var bus in routeDict[item.stop].Where(bus => !visitedBuses.Contains(bus)))
+            {
+                visitedBuses.Add(bus);
+
+                for (var i = 0; i < routes[bus].Length; i++)
+                {
+                    if (!visitedStops.Contains(routes[bus][i]))
+                    {
+                        visitedStops.Add(routes[bus][i]);
+                        queue.Enqueue((routes[bus][i], item.routeLength + 1));
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
