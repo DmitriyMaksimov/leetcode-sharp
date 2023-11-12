c2c1f232520556473a0c264154fd3406df17c49d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 17:03:41 +1000

    2748. Number of Beautiful Pairs

---
+++ UnitTests/2700/S02748Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02748Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02748();
+        sut.CountBeautifulPairs(new[] {2, 5, 1, 4}).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02748();
+        sut.CountBeautifulPairs(new[] {11, 21, 12}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/2700/S02748.cs
@@ -0,0 +1,43 @@
+﻿namespace leetcode_sharp;
+
+// 2748. Number of Beautiful Pairs
+// https://leetcode.com/problems/number-of-beautiful-pairs
+public class S02748
+{
+    public int CountBeautifulPairs(int[] nums)
+    {
+        var firstAndLasts = nums.Select(getFirstAndLast).ToArray();
+
+        var result = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                if (gcd(firstAndLasts[i].first, firstAndLasts[j].last) == 1)
+                {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static (int first, int last) getFirstAndLast(int n)
+    {
+        var l = n % 10;
+
+        while (n > 9)
+        {
+            n /= 10;
+        }
+
+        return (n, l);
+    }
+
+    static int gcd(int a, int b)
+    {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
