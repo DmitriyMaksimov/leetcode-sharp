e008593947a06a6b1e727c9aa60da6db6f353490
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 16:57:05 +1000

    2729. Check if The Number is Fascinating

---
+++ UnitTests/2700/S02729Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02729Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02729();
+        sut.IsFascinating(192).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02729();
+        sut.IsFascinating(100).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/2700/S02729.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 2729. Check if The Number is Fascinating
+// https://leetcode.com/problems/check-if-the-number-is-fascinating
+public class S02729
+{
+    public bool IsFascinating(int n)
+    {
+        var set = new HashSet<int>();
+        
+        for (var i = 1; i <= 3; i++)
+        {
+            var num = n * i;
+            
+            if (num > 999) return false;
+        
+            while (num != 0)
+            {
+                set.Add(num % 10);
+                num /= 10;
+            }
+        }
+
+        return set.Count == 9 && !set.Contains(0);
+    }
+}
