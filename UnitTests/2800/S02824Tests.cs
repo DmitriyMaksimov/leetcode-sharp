7049d4478833e80dde7ba4565d61b8c0ba8792b9
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 20:56:43 +1000

    2824. Count Pairs Whose Sum is Less than Target

---
+++ UnitTests/2800/S02824Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02824Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02824();
+        sut.CountPairs(new[] {-1, 1, 2, 3, 1}, 2).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02824();
+        sut.CountPairs(new[] {-6, 2, 5, -2, -7, -1, 3}, -2).Should().Be(10);
+    }
+}
---
+++ leetcode-sharp/2800/S02824.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 2824. Count Pairs Whose Sum is Less than Target
+// https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target
+public class S02824
+{
+    public int CountPairs(IList<int> nums, int target)
+    {
+        var count = 0;
+        var left = 0;
+        var right = nums.Count - 1;
+
+        nums = nums.OrderBy(x => x).ToList();
+
+        while (left < right)
+        {
+            if (nums[left] + nums[right] < target)
+            {
+                count += right - left;
+                ++left;
+            }
+            else
+            {
+                --right;
+            }
+        }
+
+        return count;
+    }
+}
