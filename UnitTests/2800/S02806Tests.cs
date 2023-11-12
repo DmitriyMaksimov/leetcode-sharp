b24ddbefd3a7eda87ba6beaec0b53903712c2ddb
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 20:47:06 +1000

    2806. Account Balance After Rounded Purchase

---
+++ UnitTests/2800/S02806Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02806Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02806();
+        sut.AccountBalanceAfterPurchase(9).Should().Be(90);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02806();
+        sut.AccountBalanceAfterPurchase(15).Should().Be(80);
+    }
+}
---
+++ leetcode-sharp/2800/S02806.cs
@@ -0,0 +1,15 @@
+﻿namespace leetcode_sharp;
+
+// 2806. Account Balance After Rounded Purchase
+// https://leetcode.com/problems/account-balance-after-rounded-purchase
+public class S02806
+{
+    public int AccountBalanceAfterPurchase(int purchaseAmount)
+    {
+        var reminder = purchaseAmount % 10;
+        
+        purchaseAmount = reminder is >= 0 and < 5 ? purchaseAmount - reminder : purchaseAmount + 10 - reminder;
+
+        return 100 - purchaseAmount;
+    }
+}
