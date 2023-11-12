6c212b66bbef44c399437dea9df2805089a6b0bc
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 20:11:28 +1000

    2815. Max Pair Sum in an Array

---
+++ UnitTests/2800/S02815Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02815Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02815();
+        sut.MaxSum(new[] {51, 71, 17, 24, 42}).Should().Be(88);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02815();
+        sut.MaxSum(new[] {1, 2, 3, 4}).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/2800/S02815.cs
@@ -0,0 +1,43 @@
+﻿namespace leetcode_sharp;
+
+// 2815. Max Pair Sum in an Array
+// https://leetcode.com/problems/max-pair-sum-in-an-array
+public class S02815
+{
+    public int MaxSum(int[] nums)
+    {
+        var result = -1;
+
+        var dictionary = new Dictionary<int, int>();
+
+        foreach (var n in nums)
+        {
+            var maxDigit = getMaxDigit(n);
+
+            if (dictionary.TryGetValue(maxDigit, out var value))
+            {
+                result = Math.Max(result, value + n);
+                dictionary[maxDigit] = Math.Max(value, n);
+            }
+            else
+            {
+                dictionary[maxDigit] = n;
+            }
+        }
+
+        return result;
+    }
+
+    private static int getMaxDigit(int num)
+    {
+        var maxDigit = 0;
+        
+        while (num != 0)
+        {
+            maxDigit = Math.Max(maxDigit, num % 10);
+            num /= 10;
+        }
+
+        return maxDigit;
+    }
+}
