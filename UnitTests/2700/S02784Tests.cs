f6da88ff3ca4abcdb38d7dcbf7f93fbccbaa209b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 20:42:51 +1000

    2784. Check if Array is Good

---
+++ UnitTests/2700/S02784Tests.cs
@@ -0,0 +1,35 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02784Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02784();
+        sut.IsGood(new[] {2, 1, 3}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02784();
+        sut.IsGood(new[] {1, 3, 3, 2}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02784();
+        sut.IsGood(new[] {1, 1}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S02784();
+        sut.IsGood(new[] {3, 4, 4, 1, 2, 1}).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/2700/S02784.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 2784. Check if Array is Good
+// https://leetcode.com/problems/check-if-array-is-good
+public class S02784
+{
+    public bool IsGood(int[] nums)
+    {
+        Array.Sort(nums);
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (i < nums.Length - 1)
+            {
+                if (nums[i] != i + 1)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (nums[i] != i)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
