4e58c90b119fe42016d4a6d44a7a7f2fb91a3565
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 17:15:25 +1000

    2769. Find the Maximum Achievable Number

---
+++ UnitTests/2700/S02769Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02769Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02769();
+        sut.TheMaximumAchievableX(4, 1).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02769();
+        sut.TheMaximumAchievableX(3, 2).Should().Be(7);
+    }
+}
---
+++ leetcode-sharp/2700/S02769.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2769. Find the Maximum Achievable Number
+// https://leetcode.com/problems/find-the-maximum-achievable-number/
+public class S02769
+{
+    public int TheMaximumAchievableX(int num, int t)
+    {
+        return num + 2 * t;
+    }
+}
