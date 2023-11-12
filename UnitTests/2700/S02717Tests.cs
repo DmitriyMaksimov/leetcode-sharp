d060711ba8090b834ef02ccf1695ac53eb518b3e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-12 16:40:09 +1000

    2717. Semi-Ordered Permutation

---
+++ UnitTests/2700/S02717Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02717Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02717();
+        sut.SemiOrderedPermutation(new[] {2, 1, 4, 3}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02717();
+        sut.SemiOrderedPermutation(new[] {2, 4, 1, 3}).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02717();
+        sut.SemiOrderedPermutation(new[] {1, 3, 4, 2, 5}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2700/S02717.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 2717. Semi-Ordered Permutation
+// https://leetcode.com/problems/semi-ordered-permutation
+public class S02717
+{
+    public int SemiOrderedPermutation(int[] nums)
+    {
+        var n = nums.Length;
+        var index = Array.IndexOf(nums, 1);
+        var count = index;
+
+        while (nums[0] != 1)
+        {
+            nums[index] = nums[index - 1];
+            nums[index - 1] = 1;
+            index--;
+        }
+
+        index = Array.IndexOf(nums, n);
+
+        return count + n - index - 1;
+    }
+}
