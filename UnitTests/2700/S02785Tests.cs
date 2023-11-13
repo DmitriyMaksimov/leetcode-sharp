c3bd5461ed6a68c1834959666a5c68f812ec417a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-13 19:52:58 +1000

    2785. Sort Vowels in a String

---
+++ UnitTests/2700/S02785Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02785Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02785();
+        sut.SortVowels("lEetcOde").Should().Be("lEOtcede");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02785();
+        sut.SortVowels("lYmpH").Should().Be("lYmpH");
+    }
+
+}
---
+++ leetcode-sharp/2700/S02785.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 2785. Sort Vowels in a String
+// https://leetcode.com/problems/sort-vowels-in-a-string
+public class S02785
+{
+    public string SortVowels(string s)
+    {
+        var vowels = new HashSet<char> {'a', 'e', 'i', 'o', 'u','A', 'E', 'I', 'O', 'U' };
+        var charArray = s.ToCharArray();
+        var vowelsFromString = new List<char>();
+        var vowelIndices = new List<int>();
+        
+        for (var i = 0; i < charArray.Length; i++)
+        {
+            if (vowels.Contains(charArray[i]))
+            {
+                vowelsFromString.Add(charArray[i]);
+                vowelIndices.Add(i);
+            }
+        }
+
+        vowelsFromString = vowelsFromString.OrderBy(x => x).ToList();
+        
+        for (var i = 0; i < vowelIndices.Count; i++)
+        {
+            charArray[vowelIndices[i]] = vowelsFromString[i];
+        }
+        
+        return new string(charArray);
+    }
+}
