62ae2393a746ce5e8c61d94ffce49c41e3322ff6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-14 12:01:04 +1000

    1930. Unique Length-3 Palindromic Subsequences

---
+++ UnitTests/1900/S01930Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01930Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01930();
+        sut.CountPalindromicSubsequence("aabca").Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01930();
+        sut.CountPalindromicSubsequence("adc").Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01930();
+        sut.CountPalindromicSubsequence("bbcbaba").Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/1900/S01930.cs
@@ -0,0 +1,59 @@
+﻿namespace leetcode_sharp;
+
+// 1930. Unique Length-3 Palindromic Subsequences
+// https://leetcode.com/problems/unique-length-3-palindromic-subsequences
+public class S01930
+{
+    public int CountPalindromicSubsequence(string s)
+    {
+        var count = 0;
+        var dictionary = new bool[26];
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var c = s[i] - 'a';
+
+            if (dictionary[c])
+            {
+                continue;
+            }
+
+            var j = s.Length - 1;
+
+            while (j > i && s[i] != s[j])
+            {
+                j--;
+            }
+
+            count += countDistinctCharacters(s, i + 1, j - 1);
+            dictionary[c] = true;
+        }
+
+        return count;
+    }
+
+    private static int countDistinctCharacters(string s, int x, int y)
+    {
+        if (x > y)
+        {
+            return 0;
+        }
+
+        var count = 0;
+        var dict = new bool[26];
+
+        for (var i = x; i <= y; i++)
+        {
+            var c = s[i] - 'a';
+        
+            if (!dict[c])
+            {
+                count++;
+            }
+
+            dict[c] = true;
+        }
+
+        return count;
+    }
+}
