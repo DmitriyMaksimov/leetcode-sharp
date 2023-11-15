18d1dd5b1274b2117a088fff5f72e569ec2b2b4e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-15 16:28:34 +1000

    1846. Maximum Element After Decreasing and Rearranging

---
+++ UnitTests/1800/S01846Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01846Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01846();
+        sut.MaximumElementAfterDecrementingAndRearranging(new[] {2, 2, 1, 2, 1}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01846();
+        sut.MaximumElementAfterDecrementingAndRearranging(new[] {100, 1, 1000}).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01846();
+        sut.MaximumElementAfterDecrementingAndRearranging(new[] {1, 2, 3, 4, 5}).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/1800/S01846.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 1846. Maximum Element After Decreasing and Rearranging
+// https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging
+public class S01846
+{
+    public int MaximumElementAfterDecrementingAndRearranging(int[] arr)
+    {
+        Array.Sort(arr);
+
+        arr[0] = 1;
+
+        for (var i = 1; i < arr.Length; ++i)
+        {
+            if (Math.Abs(arr[i] - arr[i - 1]) > 1)
+            {
+                arr[i] = arr[i - 1] + 1;
+            }
+        }
+
+        return arr[^1];
+    }
+}
