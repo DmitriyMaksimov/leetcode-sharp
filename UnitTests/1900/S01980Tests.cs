381816bf811951feb2d806ea0fcba3d2f9960885
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-16 10:28:24 +1000

    1980. Find Unique Binary String

---
+++ UnitTests/1900/S01980Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01980Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01980();
+        sut.FindDifferentBinaryString(new[] {"01", "10"}).Should().Be("11");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01980();
+        sut.FindDifferentBinaryString(new[] {"00", "01"}).Should().Be("10");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01980();
+        sut.FindDifferentBinaryString(new[] {"111", "011", "001"}).Should().Be("000");
+    }
+}
---
+++ leetcode-sharp/1900/S01980.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 
+// https://leetcode.com/problems/find-unique-binary-string
+public class S01980
+{
+    public string FindDifferentBinaryString(string[] nums)
+    {
+        var sb = new StringBuilder();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            sb.Append(nums[i][i] == '0' ? '1' : '0');
+        }
+
+        return sb.ToString();
+    }
+}
