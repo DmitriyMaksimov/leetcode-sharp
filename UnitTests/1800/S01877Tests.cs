aea35f23cf9f16fc43a120d37cc1859a25f91514
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-17 11:01:06 +1000

    1877. Minimize Maximum Pair Sum in Array

---
+++ UnitTests/1800/S01877Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01877Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01877();
+        sut.MinPairSum(new[] {3, 5, 2, 3}).Should().Be(7);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01877();
+        sut.MinPairSum(new[] {3, 5, 4, 2, 4, 6}).Should().Be(8);
+    }
+}
---
+++ leetcode-sharp/1800/S01877.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 1877. Minimize Maximum Pair Sum in Array
+// https://leetcode.com/problems/minimize-maximum-pair-sum-in-array
+public class S01877
+{
+    public int MinPairSum(int[] nums)
+    {
+        var result = int.MinValue;
+        Array.Sort(nums);
+
+        var start = 0;
+        var end = nums.Length - 1;
+
+        while (start < end)
+        {
+            result = Math.Max(result, nums[start++] + nums[end--]);
+        }
+
+        return result;
+    }
+}
