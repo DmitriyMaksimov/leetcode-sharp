c9f8aece0300dca527e5ec40869248959b1a8e7a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-18 12:20:40 +1000

    2839. Check if Strings Can be Made Equal With Operations I

---
+++ UnitTests/2800/S02839Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02839Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02839();
+        sut.CanBeEqual("abcd", "cdab").Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02839();
+        sut.CanBeEqual("abcd", "dacb").Should().BeFalse();
+    }
+}
