aec4ae9262eee69beee2ed2776dd04993aa9c5c0
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-18 11:21:16 +1000

    1838. Frequency of the Most Frequent Element

---
+++ leetcode-sharp/1800/S01838.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 1838. Frequency of the Most Frequent Element
+// https://leetcode.com/problems/frequency-of-the-most-frequent-element/
+public class S01838
+{
+    public int MaxFrequency(int[] nums, int k)
+    {
+        var maxFrequency = 0;
+        var currentSum = 0L;
+
+        Array.Sort(nums);
+
+        for (int left = 0, right = 0; right < nums.Length; ++right)
+        {
+            currentSum += nums[right];
+
+            while (currentSum + k < (long) nums[right] * (right - left + 1))
+            {
+                currentSum -= nums[left];
+                left++;
+            }
+
+            maxFrequency = Math.Max(maxFrequency, right - left + 1);
+        }
+
+        return maxFrequency;
+    }
+}
