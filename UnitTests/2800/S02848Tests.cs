2cbf529aeb4f70b2f6fb52eacfdbbcb9f01c8422
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-18 12:35:55 +1000

    2848. Points That Intersect With Cars

---
+++ UnitTests/2800/S02848Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02848Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02848();
+        sut.NumberOfPoints(new[] {new[] {3, 6}, new[] {1, 5}, new[] {4, 7}}).Should().Be(7);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02848();
+        sut.NumberOfPoints(new[] {new[] {1, 3}, new[] {5, 8}}).Should().Be(7);
+    }
+}
---
+++ leetcode-sharp/2800/S02848.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 2848. Points That Intersect With Cars
+// https://leetcode.com/problems/points-that-intersect-with-cars
+public class S02848
+{
+    public int NumberOfPoints(IList<IList<int>> nums)
+    {
+        var pointsWithinCar = new int[101];
+
+        foreach (var coordinates in nums)
+        {
+            var start = coordinates[0];
+            var end = coordinates[1];
+
+            for (var i = start; i <= end; i++)
+            {
+                ++pointsWithinCar[i];
+            }
+        }
+
+        return pointsWithinCar.Count(x => x > 0);
+    }
+}
