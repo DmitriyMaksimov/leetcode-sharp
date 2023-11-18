28c639bc62c8b52b5da91c8c8ffa86b16d20a89d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-18 18:44:41 +1000

    2873. Maximum Value of an Ordered Triplet I

---
+++ UnitTests/2800/S02873Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02873Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02873();
+        sut.MaximumTripletValue(new[] { 12,6,1,2,7}).Should().Be(77);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02873();
+        sut.MaximumTripletValue(new[] { 1,10,3,4,19}).Should().Be(133);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02873();
+        sut.MaximumTripletValue(new[] {1,2,3 }).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2800/S02873.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 2873. Maximum Value of an Ordered Triplet I
+// https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i
+public class S02873
+{
+    public long MaximumTripletValue(int[] nums)
+    {
+        var result = 0L;
+        var maxI = 0L;
+        var maxIj = 0L;
+
+        foreach (var num in nums)
+        {
+            result = Math.Max(result, 1L * maxIj * num);
+            maxIj = Math.Max(maxIj, maxI - num);
+            maxI = Math.Max(maxI, num);
+        }
+
+        return result;
+    }
+}
