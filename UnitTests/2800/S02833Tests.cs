86211e3de07134e2bbcf2d7e8f21b1163271891d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-18 12:13:33 +1000

    2833. Furthest Point From Origin

---
+++ UnitTests/2800/S02833Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02833Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02833();
+        sut.FurthestDistanceFromOrigin("L_RL__R").Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02833();
+        sut.FurthestDistanceFromOrigin("_R__LL_").Should().Be(5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02833();
+        sut.FurthestDistanceFromOrigin("_______").Should().Be(7);
+    }
+}
---
+++ leetcode-sharp/2800/S02833.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 2833. Furthest Point From Origin
+// https://leetcode.com/problems/furthest-point-from-origin
+public class S02833
+{
+    public int FurthestDistanceFromOrigin(string moves)
+    {
+        var leftCount = 0;
+        var rightCount = 0;
+        var bothCount = 0;
+
+        foreach (var move in moves)
+        {
+            switch (move)
+            {
+                case 'L':
+                    leftCount++;
+                    break;
+                case 'R':
+                    rightCount++;
+                    break;
+                default:
+                    bothCount++;
+                    break;
+            }
+        }
+
+        return Math.Abs(leftCount - rightCount) + bothCount;
+    }
+}
