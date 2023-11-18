c58af51ce299cfdd4be721a46735d2c523c8a8cd
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-18 13:19:57 +1000

    2855. Minimum Right Shifts to Sort the Array

---
+++ UnitTests/2800/S02855Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02855Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02855();
+        sut.MinimumRightShifts(new[] {3, 4, 5, 1, 2}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02855();
+        sut.MinimumRightShifts(new[] {1, 3, 5}).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02855();
+        sut.MinimumRightShifts(new[] {2, 1, 4}).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/2800/S02855.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 2855. Minimum Right Shifts to Sort the Array
+// https: //leetcode.com/problems/minimum-right-shifts-to-sort-the-array
+public class S02855
+{
+    public int MinimumRightShifts(IList<int> nums)
+    {
+        var i = 0;
+        while (i < nums.Count - 1 && nums[i] < nums[i + 1])
+        {
+            i++;
+        }
+
+        var j = i;
+
+        while (i < nums.Count - 1 && nums[j] > nums[i + 1])
+        {
+            i++;
+            if (i < nums.Count - 1 && (nums[i] > nums[i + 1] || nums[i + 1] > nums[0]))
+            {
+                return -1;
+            }
+        }
+
+        return i != nums.Count - 1 ? -1 : (nums.Count - 1) - j;
+    }
+}
