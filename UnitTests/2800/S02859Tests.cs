93f8a7daa2dc4e7ec6c3b038da35c96535c98308
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-18 17:12:48 +1000

    2859. Sum of Values at Indices With K Set Bits

---
+++ UnitTests/2800/S02859Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02859Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02859();
+        sut.SumIndicesWithKSetBits(new[] {5, 10, 1, 5, 2}, 1).Should().Be(13);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02859();
+        sut.SumIndicesWithKSetBits(new[] {4, 3, 2, 1}, 2).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/2800/S02859.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 2859. Sum of Values at Indices With K Set Bits
+// https://leetcode.com/problems/sum-of-values-at-indices-with-k-set-bits
+public class S02859
+{
+    public int SumIndicesWithKSetBits(IList<int> nums, int k)
+    {
+        var res = 0;
+
+        for (var i = 0; i < nums.Count; i++)
+        {
+            var c = 0;
+            var copy = i;
+
+            while (copy != 0)
+            {
+                c += copy & 1;
+                copy >>= 1;
+            }
+
+            if (c == k)
+            {
+                res += nums[i];
+            }
+        }
+
+        return res;
+    }
+}
