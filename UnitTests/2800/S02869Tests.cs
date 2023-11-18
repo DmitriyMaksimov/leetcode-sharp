fadc755f586a5d174b180ec0ca5b32369b7c06d7
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-18 18:34:03 +1000

    2869. Minimum Operations to Collect Elements

---
+++ UnitTests/2800/S02869Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02869Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02869();
+        sut.MinOperations(new[] {3, 1, 5, 4, 2}, 2).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02869();
+        sut.MinOperations(new[] {3, 1, 5, 4, 2}, 5).Should().Be(5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02869();
+        sut.MinOperations(new[] {3, 2, 5, 3, 1}, 3).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/2800/S02869.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 2869. Minimum Operations to Collect Elements
+// https://leetcode.com/problems/minimum-operations-to-collect-elements
+public class S02869
+{
+    public int MinOperations(IList<int> nums, int k)
+    {
+        var hashSet = new HashSet<int>();
+
+        for (var i = nums.Count - 1; i >= 0; i--)
+        {
+            if (nums[i] <= k)
+            {
+                hashSet.Add(nums[i]);
+
+                if (hashSet.Count == k)
+                {
+                    return nums.Count - i;
+                }
+            }
+        }
+
+        return nums.Count;
+    }
+}
