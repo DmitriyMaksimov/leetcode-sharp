aa1a64cdf47be9643bb68040f9d6a1732688fcec
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-18 19:17:44 +1000

    2899. Last Visited Integers

---
+++ UnitTests/2800/S02899Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02899Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02899();
+        sut.LastVisitedIntegers(new[] {"1", "2", "prev", "prev", "prev"}).Should().Equal(2, 1, -1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02899();
+        sut.LastVisitedIntegers(new[] {"1", "prev", "2", "prev", "prev"}).Should().Equal(1, 2, 1);
+    }
+}
---
+++ leetcode-sharp/2800/S02899.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 2899. Last Visited Integers
+// https://leetcode.com/problems/last-visited-integers
+public class S02899
+{
+    public IList<int> LastVisitedIntegers(IList<string> words)
+    {
+        var numbers = new List<int> {-1};
+        var idx = 0;
+        var result = new List<int>();
+
+        foreach (var w in words)
+        {
+            if (int.TryParse(w, out var val))
+            {
+                numbers.Add(val);
+                idx = numbers.Count - 1;
+            }
+            else
+            {
+                result.Add(numbers[idx]);
+                if (idx > 0) idx--;
+            }
+        }
+
+        return result;
+    }
+}
