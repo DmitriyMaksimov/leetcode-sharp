027273734c6e4d68cd79a343293689ace30d9791
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-18 19:04:06 +1000

    2894. Divisible and Non-divisible Sums Difference

---
+++ UnitTests/2800/S02894Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02894Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02894();
+        sut.DifferenceOfSums(10, 3).Should().Be(19);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02894();
+        sut.DifferenceOfSums(5, 6).Should().Be(15);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02894();
+        sut.DifferenceOfSums(5, 1).Should().Be(-15);
+    }
+}
---
+++ leetcode-sharp/2800/S02894.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 2894. Divisible and Non-divisible Sums Difference
+// https://leetcode.com/problems/divisible-and-non-divisible-sums-difference
+public class S02894
+{
+    public int DifferenceOfSums(int n, int m)
+    {
+        var num1 = 0;
+        var num2 = 0;
+        
+        for (var i = 1; i <= n; i++)
+        {
+            if (i % m != 0)
+            {
+                num1 += i;
+            }
+            else
+            {
+                num2 += i;
+            }
+        }
+
+        return num1 - num2;
+    }
+}
