7319863daee33542896531c596a0d0daea091161
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-18 12:28:43 +1000

    2843. Count Symmetric Integers

---
+++ UnitTests/2800/S02843Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02843Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02843();
+        sut.CountSymmetricIntegers(1, 100).Should().Be(9);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02843();
+        sut.CountSymmetricIntegers(1200, 1230).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/2800/S02843.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 2843. Count Symmetric Integers
+// https://leetcode.com/problems/count-symmetric-integers
+public class S02843
+{
+    public int CountSymmetricIntegers(int low, int high)
+    {
+        var result = 0;
+
+        for (var i = low; i <= high; ++i)
+        {
+            var s = i.ToString();
+            var n = s.Length;
+
+            if (n % 2 != 0) continue;
+
+            var diff = 0;
+
+            for (var j = 0; j < n / 2; ++j)
+            {
+                diff += s[j] - s[n - j - 1];
+            }
+
+            if (diff == 0) result++;
+        }
+
+        return result;
+    }
+}
