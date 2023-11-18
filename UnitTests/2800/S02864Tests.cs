aecf8f4d16c5d17c4b7a7a5d47acc591f5d0440b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-18 18:21:36 +1000

    2864. Maximum Odd Binary Number

---
+++ UnitTests/2800/S02864Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02864Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02864();
+        sut.MaximumOddBinaryNumber("010").Should().Be("001");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02864();
+        sut.MaximumOddBinaryNumber("0101").Should().Be("1001");
+    }
+}
---
+++ leetcode-sharp/2800/S02864.cs
@@ -0,0 +1,12 @@
+﻿namespace leetcode_sharp;
+
+// 2864. Maximum Odd Binary Number
+// https://leetcode.com/problems/maximum-odd-binary-number
+public class S02864
+{
+    public string MaximumOddBinaryNumber(string s)
+    {
+        var ones = s.Count(c => c == '1');
+        return new string(Enumerable.Repeat('1', ones - 1).Concat(Enumerable.Repeat('0', s.Length - ones)).Append('1').ToArray()); 
+    }
+}
