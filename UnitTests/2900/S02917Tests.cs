1da0750c6f1181b1039b7fb95999db76aadb2599
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-19 16:09:57 +1000

    2917. Find the K-or of an Array

---
+++ UnitTests/2900/S02917Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02917Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02917();
+        sut.FindKOr(new[] {7, 12, 9, 8, 9, 15}, 4).Should().Be(9);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02917();
+        sut.FindKOr(new[] {2, 12, 1, 11, 4, 5}, 6).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02917();
+        sut.FindKOr(new[] {10, 8, 5, 9, 11, 6, 8}, 1).Should().Be(15);
+    }
+}
---
+++ leetcode-sharp/2900/S02917.cs
@@ -0,0 +1,39 @@
+﻿namespace leetcode_sharp;
+
+// 2917. Find the K-or of an Array
+// https://leetcode.com/problems/find-the-k-or-of-an-array
+public class S02917
+{
+    public int FindKOr(int[] nums, int k)
+    {
+        var dp = new int[31];
+
+        foreach (var n in nums)
+        {
+            var num = n;
+            var i = 0;
+            while (num > 0)
+            {
+                if ((num & 1) == 1)
+                {
+                    dp[i] += 1;
+                }
+
+                i += 1;
+                num >>= 1;
+            }
+        }
+        
+        var answer = 0;
+
+        for (var i = 0; i < 31; i++)
+        {
+            if (dp[i] >= k)
+            {
+                answer += 1 << i;
+            }
+        }
+
+        return answer;
+    }
+}
