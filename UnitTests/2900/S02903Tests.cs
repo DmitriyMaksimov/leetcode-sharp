3c345ea313ebf9c8c9a41251e8f1860bb49bac3e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-19 15:24:22 +1000

    2903. Find Indices With Index and Value Difference I

---
+++ UnitTests/2900/S02903Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02903Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02903();
+        sut.FindIndices(new[] {5, 1, 4, 1}, 2, 4).Should().Equal(0, 3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02903();
+        sut.FindIndices(new[] {2, 1}, 0, 0).Should().Equal(0, 0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02903();
+        sut.FindIndices(new[] {1, 2, 3}, 2, 4).Should().Equal(-1, -1);
+    }
+}
---
+++ leetcode-sharp/2900/S02903.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 2903. Find Indices With Index and Value Difference I
+// https://leetcode.com/problems/find-indices-with-index-and-value-difference-i
+public class S02903
+{
+    public int[] FindIndices(int[] nums, int indexDifference, int valueDifference)
+    {
+        var minI = 0;
+        var maxI = 0;
+        var n = nums.Length;
+
+        for (var i = indexDifference; i < n; i++)
+        {
+            if (nums[i - indexDifference] < nums[minI]) minI = i - indexDifference;
+            if (nums[i - indexDifference] > nums[maxI]) maxI = i - indexDifference;
+            if (nums[i] - nums[minI] >= valueDifference) return new[] {minI, i};
+            if (nums[maxI] - nums[i] >= valueDifference) return new[] {maxI, i};
+        }
+
+        return new[] {-1, -1};
+    }
+}
