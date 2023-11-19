aaf424406f4b8f4888a1ea4c44e7d1455f563124
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-19 11:15:52 +1000

    1887. Reduction Operations to Make the Array Elements Equal

---
+++ UnitTests/1800/S01887Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01887Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01887();
+        sut.ReductionOperations(new[] {5, 1, 3}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01887();
+        sut.ReductionOperations(new[] {1, 1, 1}).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01887();
+        sut.ReductionOperations(new[] {1, 1, 2, 2, 3}).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/1800/S01887.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 1887. Reduction Operations to Make the Array Elements Equal
+// https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal
+public class S01887
+{
+    public int ReductionOperations(int[] nums)
+    {
+        Array.Sort(nums);
+
+        var result = 0;
+
+        for (var i = nums.Length - 1; i > 0; i--)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                result += nums.Length - i;
+            }
+        }
+
+        return result;
+    }
+}
