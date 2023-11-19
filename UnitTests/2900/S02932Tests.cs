0be4a9e597e94e0dc65c64c085706a1366dc7e42
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-19 15:43:59 +1000

    2932. Maximum Strong Pair XOR I

---
+++ UnitTests/2900/S02932Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02932Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02932();
+        sut.MaximumStrongPairXor(new[] {1, 2, 3, 4, 5}).Should().Be(7);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02932();
+        sut.MaximumStrongPairXor(new[] {10, 100}).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02932();
+        sut.MaximumStrongPairXor(new[] {5, 6, 25, 30}).Should().Be(7);
+    }
+}
---
+++ leetcode-sharp/2900/S02932.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 2932. Maximum Strong Pair XOR I
+// https://leetcode.com/problems/maximum-strong-pair-xor-i
+public class S02932
+{
+    public int MaximumStrongPairXor(int[] nums)
+    {
+        var result = 0;
+        
+        foreach (var x in nums)
+        {
+            foreach (var y in nums)
+            {
+                if (Math.Abs(x - y) <= Math.Min(x, y))
+                {
+                    result = Math.Max(result, x ^ y);
+                }
+            }
+        }
+
+        return result;
+    }
+}
