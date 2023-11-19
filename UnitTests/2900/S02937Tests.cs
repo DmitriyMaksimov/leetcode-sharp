1345c4b1b8ceec4308eb39d33533b300a83d3884
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-19 16:11:00 +1000

    2937. Make Three Strings Equal

---
+++ UnitTests/2900/S02937Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02937Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02937();
+        sut.FindMinimumOperations("abc", "abb", "ab").Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02937();
+        sut.FindMinimumOperations("dac", "bac", "cac").Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/2900/S02937.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 2937. Make Three Strings Equal
+// https://leetcode.com/problems/make-three-strings-equal
+public class S02937
+{
+    public int FindMinimumOperations(string s1, string s2, string s3)
+    {
+        var minLen = Math.Min(s1.Length, Math.Min(s2.Length, s3.Length));
+        var totalLen = s1.Length + s2.Length + s3.Length;
+
+        if (s1[0] != s2[0] || s1[0] != s3[0] || s2[0] != s3[0])
+        {
+            return -1;
+        }
+
+        for (var i = 0; i < minLen; i++)
+        {
+            if (s1[i] == s2[i] && s2[i] == s3[i])
+            {
+                totalLen -= 3;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return totalLen;
+    }
+}
