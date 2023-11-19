9d294517200792763612803c7769e6f6c9012c40
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-19 15:58:36 +1000

    2923. Find Champion I

---
+++ UnitTests/2900/S02923Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02923Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02923();
+        sut.FindChampion(new[] {new[] {0, 1}, new[] {0, 0}}).Should().Be(0);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02923();
+        sut.FindChampion(new[] {new[] {0, 0, 1}, new[] {1, 0, 1}, new[] {0, 0, 0}}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/2900/S02923.cs
@@ -0,0 +1,36 @@
+﻿namespace leetcode_sharp;
+
+// 2923. Find Champion I
+// https://leetcode.com/problems/find-champion-i
+public class S02923
+{
+    public int FindChampion(int[][] grid)
+    {
+        var n = grid.Length;
+
+        for (var i = 0; i < n; i++)
+        {
+            var found = true;
+
+            for (var j = 0; j < n; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+
+                if (grid[i][j] != 1)
+                {
+                    found = false;
+                }
+            }
+
+            if (found)
+            {
+                return i;
+            }
+        }
+
+        return n - 1;
+    }
+}
