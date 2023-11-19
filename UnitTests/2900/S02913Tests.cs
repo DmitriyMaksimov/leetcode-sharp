9baf8a7d865204edf62875eead1a8d0b97f36c23
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-19 15:35:51 +1000

    2913. Subarrays Distinct Element Sum of Squares I

---
+++ UnitTests/2900/S02913Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02913Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02913();
+        sut.SumCounts(new[] {1, 2, 1}).Should().Be(15);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02913();
+        sut.SumCounts(new[] {1, 1}).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/2900/S02913.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 2913. Subarrays Distinct Element Sum of Squares I
+// https://leetcode.com/problems/subarrays-distinct-element-sum-of-squares-i
+public class S02913
+{
+    public int SumCounts(IList<int> nums)
+    {
+        var result = 0;
+        
+        for (var i = 0; i < nums.Count; i++)
+        {
+            var hs = new HashSet<int>();
+            
+            for (var j = i; j < nums.Count; j++)
+            {
+                hs.Add(nums[j]);
+                result += hs.Count * hs.Count;
+            }
+        }
+
+        return result;
+    }
+}
