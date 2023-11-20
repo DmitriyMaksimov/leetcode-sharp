868d654140d833bc6646606fd780b89dd3371287
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-20 10:35:30 +1000

    2391. Minimum Amount of Time to Collect Garbage

---
+++ UnitTests/2900/S02391Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02391Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02391();
+        sut.GarbageCollection(new[] {"G", "P", "GP", "GG"}, new[] {2, 4, 3}).Should().Be(21);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02391();
+        sut.GarbageCollection(new[] {"MMM", "PGM", "GP"}, new[] {3, 10}).Should().Be(37);
+    }
+}
---
+++ leetcode-sharp/2300/S02391.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 2391. Minimum Amount of Time to Collect Garbage
+// https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage
+public class S02391
+{
+    public int GarbageCollection(string[] garbage, int[] travel)
+    {
+        var last = new int[128];
+        var res = 0;
+
+        for (var i = 0; i < garbage.Length; ++i)
+        {
+            res += garbage[i].Length;
+            for (var j = 0; j < garbage[i].Length; ++j)
+            {
+                last[garbage[i][j]] = i;
+            }
+        }
+
+        for (var j = 1; j < travel.Length; ++j)
+        {
+            travel[j] += travel[j - 1];
+        }
+
+        res += "PGM".ToCharArray().Where(c => last[c] > 0).Sum(c => travel[last[c] - 1]);
+
+        return res;
+    }
+}
