39fb2bf6c41d81691372a239065e6be1b03865bd
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-22 10:30:45 +1000

    1424. Diagonal Traverse II

---
+++ UnitTests/1400/S01424Tests.cs
@@ -0,0 +1,25 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01424Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01424();
+        sut.FindDiagonalOrder(new[] {new[] {1, 2, 3}, new[] {4, 5, 6}, new[] {7, 8, 9}}).Should()
+            .Equal(1, 4, 2, 7, 5, 3, 8, 6, 9);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01424();
+        sut.FindDiagonalOrder(new[]
+                {new[] {1, 2, 3, 4, 5}, new[] {6, 7}, new[] {8}, new[] {9, 10, 11}, new[] {12, 13, 14, 15, 16}})
+            .Should()
+            .Equal(1, 6, 2, 8, 7, 3, 9, 4, 12, 10, 5, 13, 11, 14, 15, 16);
+    }
+}
---
+++ leetcode-sharp/1400/S01424.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 1424. Diagonal Traverse II
+// https://leetcode.com/problems/diagonal-traverse-ii
+public class S01424
+{
+    public int[] FindDiagonalOrder(IList<IList<int>> nums)
+    {
+        var result = new List<int>();
+        var dict = new Dictionary<int, List<int>>();
+        
+        for (var i = 0; i < nums.Count; i++)
+        {
+            for (var j = 0; j < nums[i].Count; j++)
+            {
+                dict.TryAdd(i + j, new List<int>());
+                dict[i + j].Add(nums[i][j]);
+            }
+        }
+
+        var k = 0;
+
+        while (dict.ContainsKey(k))
+        {
+            var cur = dict[k];
+            cur.Reverse();
+            result.AddRange(cur);
+            k++;
+        }
+
+        return result.ToArray();
+    }
+}
