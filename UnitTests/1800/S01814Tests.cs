cbbaef92434aaf00ac8c3199539478e7c15fa685
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-22 10:24:24 +1000

    1814. Count Nice Pairs in an Array

---
+++ UnitTests/1800/S01814Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01814Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01814();
+        sut.CountNicePairs(new[] {42, 11, 1, 97}).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01814();
+        sut.CountNicePairs(new[] {13, 10, 35, 24, 76}).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/1800/S01814.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode_sharp;
+
+// 1814. Count Nice Pairs in an Array
+// https://leetcode.com/problems/count-nice-pairs-in-an-array
+public class S01814
+{
+    public int CountNicePairs(int[] nums)
+    {
+        const int mod = 1_000_000_007;
+        var count = 0;
+
+        var map = new Dictionary<long, int>();
+
+        foreach (var num in nums)
+        {
+            var rev = reverse(num);
+
+            var diff = num - rev;
+            if (map.TryGetValue(diff, out var value))
+            {
+                count = (count % mod + value % mod) % mod;
+            }
+
+            map[diff] = map.GetValueOrDefault(diff) + 1;
+        }
+
+        return count;
+    }
+
+    private static long reverse(int num)
+    {
+        var rev = 0L;
+        
+        while (num != 0)
+        {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        return rev;
+    }
+}
