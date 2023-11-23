ded3b87c62015b87a3ac94306e78254f256826b9
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-23 14:27:23 +1000

    1630. Arithmetic Subarrays

---
+++ UnitTests/1600/S01630Tests.cs
@@ -0,0 +1,23 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01630Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01630();
+        sut.CheckArithmeticSubarrays(new[] {4, 6, 5, 9, 3, 7}, new[] {0, 0, 2}, new[] {2, 3, 5}).Should()
+            .Equal(true, false, true);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01630();
+        sut.CheckArithmeticSubarrays(new[] {-12, -9, -3, -12, -6, 15, 20, -25, -20, -15, -10}, new[] {0, 1, 6, 4, 8, 7},
+            new[] {4, 4, 9, 7, 9, 10}).Should().Equal(false, true, false, false, true, true);
+    }
+}
---
+++ leetcode-sharp/1600/S01630.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 1630. Arithmetic Subarrays
+// https://leetcode.com/problems/arithmetic-subarrays
+public class S01630
+{
+    public IList<bool> CheckArithmeticSubarrays(int[] nums, int[] l, int[] r)
+    {
+        var result = new List<bool>();
+
+        for (var i = 0; i < l.Length; i++)
+        {
+            var left = l[i];
+            var right = r[i];
+
+            var subarray = new int[right - left + 1];
+            Array.Copy(nums, left, subarray, 0, right - left + 1);
+
+            result.Add(isArithmeticSequence(subarray));
+        }
+
+        return result;
+    }
+
+    private static bool isArithmeticSequence(int[] arr)
+    {
+        Array.Sort(arr);
+
+        var diff = arr[1] - arr[0];
+
+        for (var i = 2; i < arr.Length; i++)
+        {
+            if (arr[i] - arr[i - 1] != diff)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
