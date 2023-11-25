9f7d0ff2e29646d39f8ed05048eff1c3cd82f39e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-25 11:14:05 +1000

    1561. Maximum Number of Coins You Can Get

---
+++ UnitTests/1500/S01561Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01561Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01561();
+        sut.MaxCoins(new[] {2, 4, 1, 2, 7, 8}).Should().Be(9);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01561();
+        sut.MaxCoins(new[] {2, 4, 5}).Should().Be(4);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01561();
+        sut.MaxCoins(new[] {9, 8, 7, 6, 5, 1, 2, 3, 4}).Should().Be(18);
+    }
+}
---
+++ leetcode-sharp/1500/S01561.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 1561. Maximum Number of Coins You Can Get
+// https://leetcode.com/problems/maximum-number-of-coins-you-can-get
+public class S01561
+{
+    public int MaxCoins(int[] piles)
+    {
+        Array.Sort(piles);
+        
+        var ans = 0;
+        
+        for (int i = 0, j = piles.Length - 2; i < j; i++, j -= 2)
+        {
+            ans += piles[j];
+        }
+
+        return ans;
+    }
+}
