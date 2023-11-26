bcc4b0ffeeb3f2e28f12bc4d90951500365afe41
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-26 11:11:18 +1000

    1727. Largest Submatrix With Rearrangements

---
+++ UnitTests/1700/S01727Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01727Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01727();
+        sut.LargestSubmatrix(new[] {new[] {0, 0, 1}, new[] {1, 1, 1}, new[] {1, 0, 1}}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01727();
+        sut.LargestSubmatrix(new[] {new[] {1, 0, 1, 0, 1}}).Should().Be(3);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01727();
+        sut.LargestSubmatrix(new[] {new[] {1, 1, 0}, new[] {1, 0, 1}}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/1700/S01727.cs
@@ -0,0 +1,43 @@
+﻿namespace leetcode_sharp;
+
+// 1727. Largest Submatrix With Rearrangements
+// https://leetcode.com/problems/largest-submatrix-with-rearrangements
+public class S01727
+{
+    public int LargestSubmatrix(int[][] matrix)
+    {
+        for (var i = 1; i < matrix.Length; i++) //Start from row 1 because row 0 will be unaffected as no row above it.
+        {
+            for (var j = 0; j < matrix[i].Length; j++)
+            {
+                if (matrix[i][j] != 0)
+                {
+                    matrix[i][j] = matrix[i - 1][j] + 1;
+                }
+            }
+        }
+
+        var maxSize = 0;
+
+        foreach (var col in matrix)
+        {
+            var pq = new PriorityQueue<int, int>();
+            foreach (var row in col)
+            {
+                if (row > 0)
+                {
+                    pq.Enqueue(row, row);
+                }
+            }
+
+            while (pq.Count > 0)
+            {
+                var curr = pq.Dequeue();
+                var size = curr * (pq.Count + 1);
+                maxSize = Math.Max(maxSize, size);
+            }
+        }
+
+        return maxSize;
+    }
+}
