21b08799bdfd7a68efdf9fb487bb62adc0873cea
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-28 19:03:08 +1000

    2147. Number of Ways to Divide a Long Corridor

---
+++ UnitTests/2100/S02147Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02147Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02147();
+        sut.NumberOfWays("SSPPSPS").Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02147();
+        sut.NumberOfWays("PPSPSP").Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02147();
+        sut.NumberOfWays("S").Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2100/S02147.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 2147. Number of Ways to Divide a Long Corridor
+// https://leetcode.com/problems/number-of-ways-to-divide-a-long-corridor
+public class S02147
+{
+    private const int Mod = 1_000_000_007;
+
+    public int NumberOfWays(string corridor)
+    {
+        var n1 = 1;
+        var n2 = 0;
+        var n3 = 0;
+
+        foreach (var ch in corridor)
+        {
+            n1 = (n1 + n3) % Mod;
+
+            if (ch != 'S') continue;
+            
+            n3 = n2;
+            n2 = n1;
+            n1 = 0;
+        }
+
+        return n3;
+    }
+}
