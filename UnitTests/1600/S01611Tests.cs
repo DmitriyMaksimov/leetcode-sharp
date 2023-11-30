512527e3ed07d6366f180480593134c569ed333b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-11-30 18:50:50 +1000

    1611. Minimum One Bit Operations to Make Integers Zero

---
+++ UnitTests/1600/S01611Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01611Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01611();
+        sut.MinimumOneBitOperations(3).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01611();
+        sut.MinimumOneBitOperations(6).Should().Be(4);
+    }
+}
---
+++ leetcode-sharp/1600/S01611.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 1611. Minimum One Bit Operations to Make Integers Zero
+// https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero
+public class S01611
+{
+    public int MinimumOneBitOperations(int n)
+    {
+        var sign = 1;
+        var result = 0;
+        
+        while (n > 0)
+        {
+            result += n ^ (n - 1) * sign;
+            n &= n - 1;
+            sign = -sign;
+        }
+
+        return Math.Abs(result);
+    }
+}
