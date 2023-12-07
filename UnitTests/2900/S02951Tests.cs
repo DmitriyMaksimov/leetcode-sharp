82c273e42dc3bac64797ebb79dab0ea066c85b74
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-08 07:26:47 +1000

    2951. Find the Peaks

---
+++ UnitTests/2900/S02951Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02951Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02951();
+        sut.FindPeaks(new[] {2, 4, 4}).Should().Equal();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02951();
+        sut.FindPeaks(new[] {1, 4, 3, 8, 5}).Should().Equal(1, 3);
+    }
+}
---
+++ leetcode-sharp/2900/S02951.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 2951. Find the Peaks
+// https://leetcode.com/problems/find-the-peaks
+public class S02951
+{
+    public IList<int> FindPeaks(int[] mountain)
+    {
+        var peaks = new List<int>();
+        var n = mountain.Length;
+
+        for (var i = 1; i < n - 1; i++)
+        {
+            if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1])
+            {
+                peaks.Add(i);
+            }
+        }
+
+        return peaks;
+    }
+}
