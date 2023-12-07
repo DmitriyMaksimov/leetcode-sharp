d6fea98a8077ea5507510311a0f3f7c28728335e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-08 07:20:43 +1000

    2942. Find Words Containing Character

---
+++ UnitTests/2900/S02942Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02942Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02942();
+        sut.FindWordsContaining(new[] {"leet", "code"}, 'e').Should().Equal(0, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02942();
+        sut.FindWordsContaining(new[] {"abc", "bcd", "aaaa", "cbc"}, 'a').Should().Equal(0, 2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02942();
+        sut.FindWordsContaining(new[] {"abc", "bcd", "aaaa", "cbc"}, 'z').Should().Equal();
+    }
+}
---
+++ leetcode-sharp/2900/S02942.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 2942. Find Words Containing Character
+// https://leetcode.com/problems/find-words-containing-character
+public class S02942
+{
+    public IList<int> FindWordsContaining(string[] words, char x)
+    {
+        var result = new List<int>();
+        
+        for (var i = 0; i < words.Length; i++)
+        {
+            if (words[i].Contains(x))
+            {
+                result.Add(i);
+            }
+        }
+
+        return result;
+    }
+}
