d971273bd6ebac0a900f19401fb53b84cc7cace0
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-08 18:36:41 +1000

    2946. Matrix Similarity After Cyclic Shifts

---
+++ UnitTests/2900/S02946Tests.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+[TestFixture]
+public class S02946Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02946();
+        sut.AreSimilar(new[] {new[] {1, 2, 1, 2}, new[] {5, 5, 5, 5}, new[] {6, 3, 6, 3}}, 2).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02946();
+        sut.AreSimilar(new[] {new[] {2, 2}, new[] {2, 2}}, 3).Should().BeTrue();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02946();
+        sut.AreSimilar(new[] {new[] {1, 2}}, 1).Should().BeFalse();
+    }
+}
---
+++ leetcode-sharp/2900/S02946.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 2946. Matrix Similarity After Cyclic Shifts
+// https://leetcode.com/problems/matrix-similarity-after-cyclic-shifts
+public class S02946
+{
+    public bool AreSimilar(int[][] mat, int k)
+    {
+        var (rows, cols) = (mat.Length, mat[0].Length);
+
+        k %= cols;
+
+        if (k == 0)
+        {
+            return true;
+        }
+
+        for (var row = 0; row < rows; row++)
+        {
+            var c1 = row % 2 == 1 ? cols - k : k;
+
+            for (var col = 0; col < cols; col++, c1 = (c1 + 1) % cols)
+            {
+                if (mat[row][col] != mat[row][c1])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
