c828affb120fd41257178f7577c4d5099ffe0ebf
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-10 08:17:18 +1000

    2610. Convert an Array Into a 2D Array With Conditions

---
+++ UnitTests/2600/S02610Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02610Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02610();
+        sut.FindMatrix(new[] {1, 3, 4, 1, 2, 3, 1}).Should()
+            .BeEquivalentTo(new[] {new[] {1, 3, 4, 2}, new[] {1, 3}, new[] {1}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02610();
+        sut.FindMatrix(new[] {1, 2, 3, 4}).Should().BeEquivalentTo(new[] {new[] {1, 2, 3, 4}});
+    }
+}
---
+++ leetcode-sharp/2600/S02610.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 2610. Convert an Array Into a 2D Array With Conditions
+// https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions
+public class S02610
+{
+    public IList<IList<int>> FindMatrix(int[] nums)
+    {
+        var unique = new List<HashSet<int>>();
+
+        foreach (var item in nums)
+        {
+            if (!unique.Any(x => x.Add(item)))
+            {
+                unique.Add(new HashSet<int> {item});
+            }
+        }
+
+        return unique.Select(hs => hs.ToList() as IList<int>).ToList();
+    }
+}
