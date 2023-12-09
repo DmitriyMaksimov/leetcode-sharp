318738a812095596ae789037db327298dd60e536
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-10 08:01:25 +1000

    1637. Widest Vertical Area Between Two Points Containing No Points

---
+++ UnitTests/1600/S01637Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01637Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01637();
+        sut.MaxWidthOfVerticalArea(new[] {new[] {8, 7}, new[] {9, 9}, new[] {7, 4}, new[] {9, 7}}).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01637();
+        sut.MaxWidthOfVerticalArea(new[]
+            {new[] {3, 1}, new[] {9, 0}, new[] {1, 0}, new[] {1, 4}, new[] {5, 3}, new[] {8, 8}}).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/1600/S01637.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_sharp;
+
+// 1637. Widest Vertical Area Between Two Points Containing No Points
+// https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points
+public class S01637
+{
+    public int MaxWidthOfVerticalArea(int[][] points)
+    {
+        var xCoords = points.Select(p => p[0]).OrderBy(x => x).ToArray();
+
+        var res = 0;
+
+        for (var i = 1; i < xCoords.Length; i++)
+        {
+            res = Math.Max(res, xCoords[i] - xCoords[i - 1]);
+        }
+
+        return res;
+    }
+}
