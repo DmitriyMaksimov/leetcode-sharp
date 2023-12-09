be4e76deb5281ab4c780f1201dee23c9c331f7fe
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-09 20:59:44 +1000

    2161. Partition Array According to Given Pivot

---
+++ UnitTests/2100/S02161Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02161Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02161();
+        sut.PivotArray(new[] {9, 12, 5, 10, 14, 3, 10}, 10).Should().Equal(9, 5, 3, 10, 10, 12, 14);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02161();
+        sut.PivotArray(new[] {-3, 4, 3, 2}, 2).Should().Equal(-3, 2, 4, 3);
+    }
+}
---
+++ leetcode-sharp/2100/S02161.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 2161. Partition Array According to Given Pivot
+// https://leetcode.com/problems/partition-array-according-to-given-pivot
+public class S02161
+{
+    public int[] PivotArray(int[] nums, int pivot)
+    {
+        var less = new List<int>();
+        var equal = new List<int>();
+        var greater = new List<int>();
+
+        foreach (var n in nums)
+        {
+            if (n < pivot) less.Add(n);
+            else if (n == pivot) equal.Add(n);
+            else  greater.Add(n);
+        }
+
+        return less.Concat(equal).Concat(greater).ToArray();
+    }
+}
