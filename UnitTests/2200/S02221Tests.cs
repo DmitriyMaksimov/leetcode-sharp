f047add2bd2feb8695243e26aa81f65c1e0cb8dd
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-10 08:38:03 +1000

    2221. Find Triangular Sum of an Array

---
+++ UnitTests/2200/S02221Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02221Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02221();
+        sut.TriangularSum(new[] {1, 2, 3, 4, 5}).Should().Be(8);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02221();
+        sut.TriangularSum(new[] {5}).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/2200/S02221.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 2221. Find Triangular Sum of an Array
+// https://leetcode.com/problems/find-triangular-sum-of-an-array
+public class S02221
+{
+    public int TriangularSum(int[] nums)
+    {
+        var n = nums.Length;
+        
+        for (var i = n - 1; i >= 1; i--)
+        {
+            for (var j = 0; j < i; j++)
+            {
+                nums[j] = (nums[j] + nums[j + 1]) % 10;
+            }
+        }
+
+        return nums[0];
+    }
+}
