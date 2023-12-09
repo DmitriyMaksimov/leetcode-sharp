bfa3196d70055f3ab9a550d64e01942bc3ed48be
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-09 13:34:03 +1000

    1769. Minimum Number of Operations to Move All Balls to Each Box

---
+++ UnitTests/1700/S01769Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01769Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01769();
+        sut.MinOperations("110").Should().Equal(1, 1, 3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01769();
+        sut.MinOperations("001011").Should().Equal(11, 8, 5, 4, 3, 4);
+    }
+}
---
+++ leetcode-sharp/1700/S01769.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 1769. Minimum Number of Operations to Move All Balls to Each Box
+// https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box
+public class S01769
+{
+    public int[] MinOperations(string boxes)
+    {
+        var res = new int[boxes.Length];
+
+        for (var i = 0; i < boxes.Length; i++)
+        {
+            var sum = 0;
+        
+            for (var j = 0; j < boxes.Length; j++)
+            {
+                if (i == j) continue;
+                if (boxes[j] == '1')
+                    sum += Math.Abs(i - j);
+            }
+
+            res[i] = sum;
+        }
+
+        return res;
+    }
+}
