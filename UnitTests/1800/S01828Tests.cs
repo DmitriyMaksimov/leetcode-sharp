34a372452196e07b81a6f86d10fc1618f922c7b5
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-09 11:51:03 +1000

    1828. Queries on Number of Points Inside a Circle

---
+++ UnitTests/1800/S01828Tests.cs
@@ -0,0 +1,25 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01828Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01828();
+        sut.CountPoints(new[] {new[] {1, 3}, new[] {3, 3}, new[] {5, 3}, new[] {2, 2}},
+                new[] {new[] {2, 3, 1}, new[] {4, 3, 1}, new[] {1, 1, 2}})
+            .Should().Equal(3, 2, 2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01828();
+        sut.CountPoints(new[] {new[] {1, 1}, new[] {2, 2}, new[] {3, 3}, new[] {4, 4}, new[] {5, 5}},
+                new[] {new[] {1, 2, 2}, new[] {2, 2, 2}, new[] {4, 3, 2}, new[] {4, 3, 3}})
+            .Should().Equal(2, 3, 2, 4);
+    }
+}
---
+++ leetcode-sharp/1800/S01828.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 1828. Queries on Number of Points Inside a Circle
+// https://leetcode.com/problems/queries-on-number-of-points-inside-a-circle
+public class S01828
+{
+    public int[] CountPoints(int[][] points, int[][] queries)
+    {
+        var countPoints = new int[queries.Length];
+
+        foreach (var point in points)
+        {
+            for (var j = 0; j < queries.Length; j++)
+            {
+                if (Math.Sqrt((point[0] - queries[j][0]) * (point[0] - queries[j][0]) + (point[1] - queries[j][1]) * (point[1] - queries[j][1])) <= queries[j][2])
+                {
+                    ++countPoints[j];
+                }
+            }
+        }
+
+        return countPoints;
+    }
+}
