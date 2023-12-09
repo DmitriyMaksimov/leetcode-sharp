b5847269f450b5e42373c93c80519c58b525f6f7
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-09 11:26:09 +1000

    1476. Subrectangle Queries

---
+++ UnitTests/1400/S01476Tests.cs
@@ -0,0 +1,35 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01476Tests
+{
+    [Test]
+    public void T1()
+    {
+        var subrectangleQueries = new S01476.SubrectangleQueries(new[] {new[] {1, 2, 1}, new[] {4, 3, 4}, new[] {3, 2, 1}, new[] {1, 1, 1}});
+        // The initial rectangle (4x3) looks like:
+        // 1 2 1
+        // 4 3 4
+        // 3 2 1
+        // 1 1 1
+        subrectangleQueries.GetValue(0, 2).Should().Be(1);
+        subrectangleQueries.UpdateSubrectangle(0, 0, 3, 2, 5);
+        // After this update the rectangle looks like:
+        // 5 5 5
+        // 5 5 5
+        // 5 5 5
+        // 5 5 5 
+        subrectangleQueries.GetValue(0, 2).Should().Be(5);
+        subrectangleQueries.GetValue(3, 1).Should().Be(5);
+        subrectangleQueries.UpdateSubrectangle(3, 0, 3, 2, 10);
+        // After this update the rectangle looks like:
+        // 5   5   5
+        // 5   5   5
+        // 5   5   5
+        // 10  10  10 
+        subrectangleQueries.GetValue(3, 1).Should().Be(10); // return 10
+        subrectangleQueries.GetValue(0, 2).Should().Be(5); // return 5
+    }
+}
---
+++ leetcode-sharp/1400/S01476.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 1476. Subrectangle Queries
+// https://leetcode.com/problems/subrectangle-queries
+public static class S01476
+{
+    public class SubrectangleQueries
+    {
+        private readonly int[][] _rectangle;
+
+        public SubrectangleQueries(int[][] rectangle)
+        {
+            _rectangle = rectangle;
+        }
+
+        public void UpdateSubrectangle(int row1, int col1, int row2, int col2, int newValue)
+        {
+            for (var i = row1; i <= row2; i++)
+            {
+                for (var j = col1; j <= col2; j++)
+                {
+                    _rectangle[i][j] = newValue;
+                }
+            }
+        }
+
+        public int GetValue(int row, int col)
+        {
+            return _rectangle[row][col];
+        }
+    }
+}
