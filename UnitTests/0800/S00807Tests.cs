39be7d26ea4bad44e730f2cc1f4be72108a4f566
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-09 17:52:35 +1000

    807. Max Increase to Keep City Skyline

---
+++ UnitTests/0800/S00807Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00807Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00807();
+        sut.MaxIncreaseKeepingSkyline(new[]
+            {new[] {3, 0, 8, 4}, new[] {2, 4, 5, 7}, new[] {9, 2, 6, 3}, new[] {0, 3, 1, 0}}).Should().Be(35);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00807();
+        sut.MaxIncreaseKeepingSkyline(new[] {new[] {0, 0, 0}, new[] {0, 0, 0}, new[] {0, 0, 0}}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0800/S00807.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode_sharp;
+
+// 807. Max Increase to Keep City Skyline
+// https://leetcode.com/problems/max-increase-to-keep-city-skyline
+public class S00807
+{
+    public int MaxIncreaseKeepingSkyline(int[][] grid)
+    {
+        var n = grid.Length;
+        var rowMax = new int[n];
+        var colMax = new int[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                rowMax[i] = Math.Max(rowMax[i], grid[i][j]);
+            }
+        }
+
+        for (var j = 0; j < n; j++)
+        {
+            for (var i = 0; i < n; i++)
+            {
+                colMax[j] = Math.Max(colMax[j], grid[i][j]);
+            }
+        }
+
+        var result = 0;
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                var maxAllowedHeight = Math.Min(rowMax[i], colMax[j]);
+                result += maxAllowedHeight - grid[i][j];
+            }
+        }
+
+        return result;
+    }
+}
