2d9f7aba39bdde3647cbd113ccfb81e3ac37ca6d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-10 08:07:57 +1000

    1409. Queries on a Permutation With Key

---
+++ UnitTests/1400/S01409Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01409Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01409();
+        sut.ProcessQueries(new[] {3, 1, 2, 1}, 5).Should().Equal(2, 1, 2, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01409();
+        sut.ProcessQueries(new[] {4, 1, 2, 2}, 4).Should().Equal(3, 1, 2, 0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01409();
+        sut.ProcessQueries(new[] {7, 5, 5, 8, 3}, 8).Should().Equal(6, 5, 0, 7, 5);
+    }
+}
---
+++ leetcode-sharp/1400/S01409.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 1409. Queries on a Permutation With Key
+// https://leetcode.com/problems/queries-on-a-permutation-with-key
+public class S01409
+{
+    public int[] ProcessQueries(int[] queries, int m)
+    {
+        var arr = new List<int>(Enumerable.Range(1, m));
+        var answer = new int[queries.Length];
+
+        for (var i = 0; i < queries.Length; i++)
+        {
+            for (var j = 0; j < arr.Count; j++)
+            {
+                if (queries[i] == arr[j])
+                {
+                    answer[i] = j;
+                    arr.Insert(0, arr[j]);
+                    arr.RemoveAt(j + 1);
+                }
+            }
+        }
+
+        return answer;
+    }
+}
