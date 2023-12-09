fc5af19a46ca6fcc5fcdcd2d1ab1c27bb25be600
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-09 11:16:23 +1000

    2807. Insert Greatest Common Divisors in Linked List

---
+++ UnitTests/2800/S02807Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02807Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02807();
+        sut.InsertGreatestCommonDivisors(ListNode.Parse("[18,6,10,3]")).Should()
+            .BeEquivalentTo(ListNode.Parse("[18,6,6,2,10,1,3]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02807();
+        sut.InsertGreatestCommonDivisors(ListNode.Parse("[7]")).Should().BeEquivalentTo(ListNode.Parse("[7]"));
+    }
+}
---
+++ leetcode-sharp/2800/S02807.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 2807. Insert Greatest Common Divisors in Linked List
+// https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list
+public class S02807
+{
+    public ListNode? InsertGreatestCommonDivisors(ListNode? head)
+    {
+        if (head?.next is null) return head;
+        
+        var temp = head;
+
+        while (temp?.next?.next is not null)
+        {
+            var newNode = new ListNode(gcd(temp.val, temp.next.val), temp.next);
+            temp.next = newNode;
+            temp = temp.next.next;
+        }
+
+        temp.next = new ListNode(gcd(temp.val, temp.next.val), temp.next);
+
+        return head;
+    }
+
+    private static int gcd(int p, int q)
+    {
+        return q == 0 ? p : gcd(q, p % q);
+    }
+}
