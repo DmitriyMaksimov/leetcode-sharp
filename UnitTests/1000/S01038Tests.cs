d485a2ef86471c82c691cd94ce34298ea6def2a3
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-09 20:16:13 +1000

    1038. Binary Search Tree to Greater Sum Tree

---
+++ UnitTests/1000/S01038Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01038Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01038();
+        sut.BstToGst(TreeNode.Parse("[4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]")).Should()
+            .BeEquivalentTo(TreeNode.Parse("[30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01038();
+        sut.BstToGst(TreeNode.Parse("[0,null,1]")).Should().BeEquivalentTo(TreeNode.Parse("[1,null,1]"));
+    }
+}
---
+++ leetcode-sharp/1000/S01038.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 1038. Binary Search Tree to Greater Sum Tree
+// https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree
+public class S01038
+{
+    public TreeNode? BstToGst(TreeNode? root)
+    {
+        var sum = 0;
+
+        var stack = new Stack<TreeNode>();
+        var current = root;
+
+        while (stack.Any() || current != null)
+        {
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.right;
+            }
+
+            current = stack.Pop();
+            sum += current.val;
+            current.val = sum;
+            current = current.left;
+        }
+
+        return root;
+    }
+}
