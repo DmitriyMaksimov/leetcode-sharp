0f225c75c6fc2ee9835083e3f5d84aab13c3b603
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-09 11:40:53 +1000

    1302. Deepest Leaves Sum

---
+++ UnitTests/1300/S01302Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01302Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01302();
+        sut.DeepestLeavesSum(TreeNode.Parse("[1,2,3,4,5,null,6,7,null,null,null,null,8]")).Should().Be(15);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01302();
+        sut.DeepestLeavesSum(TreeNode.Parse("[6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]")).Should().Be(19);
+    }
+}
---
+++ leetcode-sharp/1300/S01302.cs
@@ -0,0 +1,36 @@
+﻿namespace leetcode_sharp;
+
+// 1302. Deepest Leaves Sum
+// https://leetcode.com/problems/deepest-leaves-sum
+public class S01302
+{
+    private int _maxDepth;
+    private int _sum;
+
+    public int DeepestLeavesSum(TreeNode? root)
+    {
+        Traverse(root, 0);
+        return _sum;
+    }
+
+    private void Traverse(TreeNode? node, int depth)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        if (depth > _maxDepth)
+        {
+            _sum = node.val;
+            _maxDepth = depth;
+        }
+        else if (depth == _maxDepth)
+        {
+            _sum += node.val;
+        }
+
+        Traverse(node.left, depth + 1);
+        Traverse(node.right, depth + 1);
+    }
+}
