bca1f6d10e177ac693ea297ad73016e510c20693
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-09 20:21:51 +1000

    654. Maximum Binary Tree

---
+++ UnitTests/0600/S00654Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00654Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00654();
+        sut.ConstructMaximumBinaryTree(new[] { 3,2,1,6,0,5}).Should().BeEquivalentTo(TreeNode.Parse("[6,3,5,null,2,0,null,null,1]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00654();
+        sut.ConstructMaximumBinaryTree(new[] {3,2,1 }).Should().BeEquivalentTo(TreeNode.Parse("[3,null,2,null,1]"));
+    }
+}
---
+++ leetcode-sharp/0600/S00654.cs
@@ -0,0 +1,42 @@
+﻿namespace leetcode_sharp;
+
+// 654. Maximum Binary Tree
+// https://leetcode.com/problems/maximum-binary-tree
+public class S00654
+{
+    public TreeNode? ConstructMaximumBinaryTree(int[] nums)
+    {
+        return constructMaximumBinaryTree(nums, 0, nums.Length - 1);
+    }
+
+    private static TreeNode? constructMaximumBinaryTree(IReadOnlyList<int> nums, int i, int j)
+    {
+        if (j < i)
+        {
+            return null;
+        }
+
+        if (i == j)
+        {
+            return new TreeNode(nums[i]);
+        }
+
+        var max = nums[i];
+        var index = i;
+
+        for (var k = i + 1; k <= j; k++)
+        {
+            if (nums[k] > max)
+            {
+                index = k;
+                max = nums[k];
+            }
+        }
+
+        return new TreeNode(max)
+        {
+            left = constructMaximumBinaryTree(nums, i, index - 1),
+            right = constructMaximumBinaryTree(nums, index + 1, j)
+        };
+    }
+}
