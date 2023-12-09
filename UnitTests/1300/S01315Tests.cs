a0de6c3b01da736022494ef537f670f386d20d2d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-09 13:41:35 +1000

    1315. Sum of Nodes with Even-Valued Grandparent

---
+++ UnitTests/1300/S01315Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01315Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01315();
+        sut.SumEvenGrandparent(TreeNode.Parse("[6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]")).Should().Be(18);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01315();
+        sut.SumEvenGrandparent(TreeNode.Parse("[1]")).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1300/S01315.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode_sharp;
+
+// 1315. Sum of Nodes with Even-Valued Grandparent
+// https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent
+public class S01315
+{
+    private int _result;
+
+    public int SumEvenGrandparent(TreeNode? root)
+    {
+        dfs(root, null, null);
+        return _result;
+    }
+
+    private void dfs(TreeNode? node, TreeNode? parent, TreeNode? grandp)
+    {
+        if (node == null) return;
+        
+        if (grandp != null && grandp.val % 2 == 0)
+        {
+            _result += node.val;
+        }
+
+        dfs(node.left, node, parent);
+        dfs(node.right, node, parent);
+    }
+}
