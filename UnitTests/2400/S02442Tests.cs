95fb442372b9c8b0ef4221a83dcc5f534d56fc38
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-10 08:33:47 +1000

    2442. Count Number of Distinct Integers After Reverse Operations

---
+++ UnitTests/2400/S02442Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02442Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02442();
+        sut.CountDistinctIntegers(new[] {1, 13, 10, 12, 31}).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02442();
+        sut.CountDistinctIntegers(new[] {2, 2, 2}).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/2400/S02442.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 2442. Count Number of Distinct Integers After Reverse Operations
+// https://leetcode.com/problems/count-number-of-distinct-integers-after-reverse-operations
+public class S02442
+{
+    public int CountDistinctIntegers(int[] nums)
+    {
+        var set = new HashSet<int>(nums);
+        var reverseSet = new HashSet<int>(set.Select(reverse));
+
+        set.UnionWith(reverseSet);
+
+        return set.Count;
+    }
+
+    private static int reverse(int num)
+    {
+        var rev = 0;
+
+        while (num != 0)
+        {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        return rev;
+    }
+}
