3be4bf471ce125bb2c538db8c5b5e4ec9182f137
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-09 20:30:53 +1000

    2545. Sort the Students by Their Kth Score

---
+++ UnitTests/2500/S02545Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02545Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02545();
+        sut.SortTheStudents(new[] {new[] {10, 6, 9, 1}, new[] {7, 5, 11, 2}, new[] {4, 8, 3, 15}}, 2).Should().BeEquivalentTo(new[]{new[] {7, 5, 11, 2}, new[] {10, 6, 9, 1}, new[] {4, 8, 3, 15}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02545();
+        sut.SortTheStudents(new[] {new[] {3, 4}, new[] {5, 6}}, 0).Should().BeEquivalentTo(new []{new[] {5, 6}, new[] {3, 4}});
+    }
+}
---
+++ leetcode-sharp/2500/S02545.cs
@@ -0,0 +1,13 @@
+﻿namespace leetcode_sharp;
+
+// 2545. Sort the Students by Their Kth Score
+// https://leetcode.com/problems/sort-the-students-by-their-kth-score
+public class S02545
+{
+    public int[][] SortTheStudents(int[][] score, int k)
+    {
+        var list = score.ToList();
+        list.Sort((x, y) => y[k] - x[k]);
+        return list.ToArray();
+    }
+}
