1131ed2aa2299fb25dd937eb9988439b4bbbb07c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-09 20:11:09 +1000

    2181. Merge Nodes in Between Zeros

---
+++ UnitTests/2100/S02181Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02181Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02181();
+        sut.MergeNodes(ListNode.Parse("[0,3,1,0,4,5,2,0]")).Should().BeEquivalentTo(ListNode.Parse("[4,11]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02181();
+        sut.MergeNodes(ListNode.Parse("[0,1,0,3,0,2,2,0]")).Should().BeEquivalentTo(ListNode.Parse("[1,3,4]"));
+    }
+}
---
+++ leetcode-sharp/2100/S02181.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 2181. Merge Nodes in Between Zeros
+// https://leetcode.com/problems/merge-nodes-in-between-zeros
+public class S02181
+{
+    public ListNode? MergeNodes(ListNode? head)
+    {
+        var current = head;
+
+        while (current.next != null)
+        {
+            var nextNode = current.next;
+
+            var nextNext = nextNode.next;
+            nextNode.next = null;
+
+            if (nextNode.val != 0)
+            {
+                current.val += nextNode.val;
+            }
+            else if (nextNode.val == 0 && nextNext != null)
+            {
+                current = nextNode;
+            }
+
+            current.next = nextNext;
+        }
+
+        return head;
+    }
+}
