718c2bc2991834b6d84fd0f4ac54c720476f2313
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-16 11:40:15 +1000

    1329. Sort the Matrix Diagonally

---
+++ UnitTests/1300/S01329Tests.cs
@@ -0,0 +1,31 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01329Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01329();
+        sut.DiagonalSort(new[] {new[] {3, 3, 1, 1}, new[] {2, 2, 1, 2}, new[] {1, 1, 1, 2}}).Should()
+            .BeEquivalentTo(new[]{new[] {1, 1, 1, 1}, new[] {1, 2, 2, 2}, new[] {1, 2, 3, 3}});
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01329();
+        sut.DiagonalSort(new[]
+            {
+                new[] {11, 25, 66, 1, 69, 7}, new[] {23, 55, 17, 45, 15, 52}, new[] {75, 31, 36, 44, 58, 8},
+                new[] {22, 27, 33, 25, 68, 4}, new[] {84, 28, 14, 11, 5, 50}
+            }).Should()
+            .BeEquivalentTo(new[]
+            {
+                new[] {5, 17, 4, 1, 52, 7}, new[] {11, 11, 25, 45, 8, 69}, new[] {14, 23, 25, 44, 58, 15},
+                new[] {22, 27, 31, 36, 50, 66}, new[] {84, 28, 75, 33, 55, 68}
+            });
+    }
+}
---
+++ leetcode-sharp/1300/S01329.cs
@@ -0,0 +1,54 @@
+﻿namespace leetcode_sharp;
+
+// 1329. Sort the Matrix Diagonally
+// https://leetcode.com/problems/sort-the-matrix-diagonally
+public class S01329
+{
+    private readonly HashSet<(int row, int cell)> _visited = new();
+
+    public int[][] DiagonalSort(int[][] mat)
+    {
+        for (var r = 0; r < mat.Length; r++)
+        {
+            for (var c = 0; c < mat[r].Length; c++)
+            {
+                if (_visited.Contains((r, c)))
+                {
+                    break;
+                }
+
+                var diag = dfs(mat, r, c);
+                diag.Sort(); // sort diagonal
+                fill(mat, r, c, diag, 0);
+            }
+        }
+
+        return mat;
+    }
+
+    private List<int> dfs(IReadOnlyList<int[]> mat, int r, int c)
+    {
+        if (r >= mat.Count || c >= mat[0].Length)
+        {
+            return new List<int>();
+        }
+
+        _visited.Add((r, c));
+        
+        List<int> result = new() {mat[r][c]};
+        result.AddRange(dfs(mat, r + 1, c + 1));
+
+        return result;
+    }
+
+    private static void fill(int[][] mat, int r, int c, IReadOnlyList<int> diag, int i)
+    {
+        if (r >= mat.Length || c >= mat[0].Length)
+        {
+            return;
+        }
+
+        mat[r][c] = diag[i];
+        fill(mat, r + 1, c + 1, diag, i + 1);
+    }
+}
