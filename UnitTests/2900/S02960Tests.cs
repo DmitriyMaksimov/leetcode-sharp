98c35055e19b9c8cc1aab7343f5f8ac7dc7f7548
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-16 14:00:59 +1000

    2960. Count Tested Devices After Test Operations

---
+++ UnitTests/2900/S02960Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02960Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02960();
+        sut.CountTestedDevices(new[] {1, 1, 2, 1, 3}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02960();
+        sut.CountTestedDevices(new[] {0, 1, 2}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/2900/S02960.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2960. Count Tested Devices After Test Operations
+// https://leetcode.com/problems/count-tested-devices-after-test-operations
+public class S02960
+{
+    public int CountTestedDevices(int[] batteryPercentages)
+    {
+        return batteryPercentages.Aggregate(0, (current, batteryPercentage) => current + (batteryPercentage > current ? 1 : 0));
+    }
+}
