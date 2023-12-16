19f28200fa424a0cca95af67a041b7f50606d638
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-16 18:22:32 +1000

    2120. Execution of All Suffix Instructions Staying in a Grid

---
+++ UnitTests/2100/S02120Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02120Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02120();
+        sut.ExecuteInstructions(3, new[] {0, 1}, "RRDDLU").Should().Equal(1, 5, 4, 3, 1, 0);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02120();
+        sut.ExecuteInstructions(2, new[] {1, 1}, "LURD").Should().Equal(4, 1, 0, 0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02120();
+        sut.ExecuteInstructions(1, new[] {0, 0}, "LRUD").Should().Equal(0, 0, 0, 0);
+    }
+}
---
+++ leetcode-sharp/2100/S02120.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 2120. Execution of All Suffix Instructions Staying in a Grid
+// https://leetcode.com/problems/execution-of-all-suffix-instructions-staying-in-a-grid
+public class S02120
+{
+    public int[] ExecuteInstructions(int n, int[] startPos, string s)
+    {
+        var ans = new int[s.Length];
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            ans[i] = canMove(startPos[0], startPos[1], i, s, n);
+        }
+
+        return ans;
+    }
+
+    private static int canMove(int row, int col, int index, string s, int n)
+    {
+        if (index >= s.Length) return 0;
+
+        return s[index] switch
+        {
+            'D' => row + 1 >= n ? 0 : canMove(row + 1, col, index + 1, s, n) + 1,
+            'U' => row - 1 < 0 ? 0 : canMove(row - 1, col, index + 1, s, n) + 1,
+            'L' => col - 1 < 0 ? 0 : canMove(row, col - 1, index + 1, s, n) + 1,
+            'R' => col + 1 >= n ? 0 : canMove(row, col + 1, index + 1, s, n) + 1,
+            _ => 0
+        };
+    }
+}
