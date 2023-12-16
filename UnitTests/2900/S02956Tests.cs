dcb3bc3be10592c2b4de76ceecd4c7fe2a44ff59
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-16 13:22:08 +1000

    2956. Find Common Elements Between Two Arrays

---
+++ UnitTests/2900/S02956Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02956Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02956();
+        sut.FindIntersectionValues(new[] {4, 3, 2, 3, 1}, new[] {2, 2, 5, 2, 3, 6}).Should().Equal(3, 4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02956();
+        sut.FindIntersectionValues(new[] {3, 4, 2, 3}, new[] {1, 5}).Should().Equal(0, 0);
+    }
+}
---
+++ leetcode-sharp/2900/S02956.cs
@@ -0,0 +1,13 @@
+﻿namespace leetcode_sharp;
+
+// 2956. Find Common Elements Between Two Arrays
+// https://leetcode.com/problems/find-common-elements-between-two-arrays
+public class S02956
+{
+    public int[] FindIntersectionValues(int[] nums1, int[] nums2)
+    {
+        var set1 = new HashSet<int>(nums1);
+        var set2 = new HashSet<int>(nums2);
+        return new[] {nums1.Count(x => set2.Contains(x)), nums2.Count(x => set1.Contains(x))};
+    }
+}
