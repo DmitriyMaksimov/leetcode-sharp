8dd5b1d5baca2765bd5510734d36739d055a2d30
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-18 01:26:18 +1000

    2125. Number of Laser Beams in a Bank

---
+++ UnitTests/2100/S02125Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02125Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02125();
+        sut.NumberOfBeams(new[] {"011001", "000000", "010100", "001000"}).Should().Be(8);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02125();
+        sut.NumberOfBeams(new[] {"000", "111", "000"}).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2100/S02125.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 2125. Number of Laser Beams in a Bank
+// https://leetcode.com/problems/number-of-laser-beams-in-a-bank
+public class S02125
+{
+    public int NumberOfBeams(string[] bank)
+    {
+        var count = 0;
+        var prev = 0;
+        
+        foreach (var floor in bank)
+        {
+            var ones = floor.Count(c => c == '1');
+            if (ones != 0)
+            {
+                count += ones * prev;
+                prev = ones;
+            }
+        }
+
+        return count;
+    }
+}
