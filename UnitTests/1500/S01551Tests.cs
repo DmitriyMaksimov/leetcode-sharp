bc3a201349ba3295cd8e6d8233211a8f8e36af9c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-17 19:29:36 +1000

    1551. Minimum Operations to Make Array Equal

---
+++ UnitTests/1500/S01551Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01551Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01551();
+        sut.MinOperations(3).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01551();
+        sut.MinOperations(6).Should().Be(9);
+    }
+}
---
+++ leetcode-sharp/1500/S01551.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 1551. Minimum Operations to Make Array Equal
+// https://leetcode.com/problems/minimum-operations-to-make-array-equal
+public class S01551
+{
+    public int MinOperations(int n)
+    {
+        return n * n / 4;
+    }
+}
