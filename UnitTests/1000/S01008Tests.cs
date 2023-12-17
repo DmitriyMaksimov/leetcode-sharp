850d4b1c87c4e5df7896ec7455be549e8cd8266d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-17 19:20:10 +1000

    1008. Construct Binary Search Tree from Preorder Traversal

---
+++ UnitTests/1000/S01008Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01008Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01008();
+        sut.BstFromPreorder(new[] {8,5,1,7,10,12 }).Should().BeEquivalentTo(TreeNode.Parse("[8,5,10,1,7,null,12]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01008();
+        sut.BstFromPreorder(new[] {1,3 }).Should().BeEquivalentTo(TreeNode.Parse("[1,null,3]"));
+    }
+}
---
+++ leetcode-sharp/1000/S01008.cs
@@ -0,0 +1,39 @@
+﻿namespace leetcode_sharp;
+
+// 1008. Construct Binary Search Tree from Preorder Traversal
+// https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal
+public class S01008
+{
+    public TreeNode BstFromPreorder(int[] preorder)
+    {
+        var len = preorder.Length;
+        var root = new TreeNode(preorder[0]);
+        var stack = new Stack<TreeNode>();
+
+        stack.Push(root);
+
+        for (var i = 1; i < len; ++i)
+        {
+            TreeNode? poppedNode = null;
+            var node = new TreeNode(preorder[i]);
+
+            while (stack.Count > 0 && stack.Peek().val < preorder[i])
+            {
+                poppedNode = stack.Pop();
+            }
+
+            if (poppedNode != null)
+            {
+                poppedNode.right = node;
+            }
+            else
+            {
+                stack.Peek().left = node;
+            }
+
+            stack.Push(node);
+        }
+
+        return root;
+    }
+}
