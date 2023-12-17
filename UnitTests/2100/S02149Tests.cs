6d3282903272fec9bafce6838dd7c6c8c5200dd1
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-17 19:08:40 +1000

    2149. Rearrange Array Elements by Sign

---
+++ UnitTests/2100/S02149Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02149Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02149();
+        sut.RearrangeArray(new[] {3, 1, -2, -5, 2, -4}).Should().Equal(3, -2, 1, -5, 2, -4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02149();
+        sut.RearrangeArray(new[] {-1, 1}).Should().Equal(1, -1);
+    }
+}
---
+++ leetcode-sharp/2100/S02149.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 2149. Rearrange Array Elements by Sign
+// https://leetcode.com/problems/rearrange-array-elements-by-sign
+public class S02149
+{
+    public int[] RearrangeArray(int[] nums)
+    {
+        var result = new int[nums.Length];
+        var positiveIndex = 0;
+        var negativeIndex = 1;
+
+        foreach (var num in nums)
+        {
+            switch (num)
+            {
+                case > 0:
+                    result[positiveIndex] = num;
+                    positiveIndex += 2;
+                    break;
+                case < 0:
+                    result[negativeIndex] = num;
+                    negativeIndex += 2;
+                    break;
+            }
+        }
+        
+        return result;
+    }
+}
