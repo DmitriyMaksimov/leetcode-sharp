3b4d40c6b47040edcce844dd40399ddd699d0667
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-18 22:12:57 +1000

    2965. Find Missing and Repeated Values

---
+++ UnitTests/2900/S02965Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02965Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02965();
+        sut.FindMissingAndRepeatedValues(new[] {new[] {1, 3}, new[] {2, 2}}).Should().Equal(2, 4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02965();
+        sut.FindMissingAndRepeatedValues(new[] {new[] {9, 1, 7}, new[] {8, 9, 2}, new[] {3, 4, 6}}).Should().Equal(9, 5);
+    }
+}
---
+++ leetcode-sharp/2900/S02965.cs
@@ -0,0 +1,36 @@
+﻿namespace leetcode_sharp;
+
+// 2965. Find Missing and Repeated Values
+// https://leetcode.com/problems/find-missing-and-repeated-values
+public class S02965
+{
+    public int[] FindMissingAndRepeatedValues(int[][] grid)
+    {
+        var n = grid.Length;
+        var a = 0;
+        var b = 0;
+        var dictionary = new Dictionary<int, int>();
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                dictionary[grid[i][j]] = dictionary.GetValueOrDefault(grid[i][j]) + 1;
+            }
+        }
+
+        for (var i = 1; i <= n * n; i++)
+        {
+            if (!dictionary.ContainsKey(i))
+            {
+                b = i;
+            }
+            else if (dictionary[i] == 2)
+            {
+                a = i;
+            }
+        }
+
+        return new[] {a, b};
+    }
+}
