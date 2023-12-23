1425bf096abcaaf788b69082b6f252e47d802393
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-23 12:19:35 +1000

    1382. Balance a Binary Search Tree

---
+++ UnitTests/1300/S01382Tests.cs
@@ -0,0 +1,22 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01382Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01382();
+        sut.BalanceBST(TreeNode.Parse("[1,null,2,null,3,null,4,null,null]")).Should()
+            .BeEquivalentTo(TreeNode.Parse("[2,1,3,null,null,null,4]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01382();
+        sut.BalanceBST(TreeNode.Parse("[2,1,3]")).Should().BeEquivalentTo(TreeNode.Parse("[2,1,3]"));
+    }
+}
---
+++ leetcode-sharp/1300/S01382.cs
@@ -0,0 +1,43 @@
+﻿namespace leetcode_sharp;
+
+// 1382. Balance a Binary Search Tree
+// https://leetcode.com/problems/balance-a-binary-search-tree
+public class S01382
+{
+    private readonly List<int> _list = new();
+
+    public TreeNode? BalanceBST(TreeNode? root)
+    {
+        inorder(root);
+        return balance(_list, 0, _list.Count - 1);
+    }
+
+    private void inorder(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return;
+        }
+
+        inorder(root.left);
+        _list.Add(root.val);
+        inorder(root.right);
+    }
+
+    private static TreeNode? balance(IReadOnlyList<int> list, int start, int end)
+    {
+        if (start > end)
+        {
+            return null;
+        }
+
+        var mid = (start + end) / 2;
+
+        return new TreeNode
+        {
+            val = list[mid],
+            left = balance(list, start, mid - 1),
+            right = balance(list, mid + 1, end)
+        };
+    }
+}
