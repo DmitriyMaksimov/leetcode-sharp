26a0ed7c0a866507b46c8edd970d4a3d7204ccaa
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-24 12:31:10 +1000

    1305. All Elements in Two Binary Search Trees

---
+++ UnitTests/1300/S01305Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01305Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01305();
+        sut.GetAllElements(TreeNode.Parse("[2,1,4]"), TreeNode.Parse("[1,0,3]")).Should().Equal(0, 1, 1, 2, 3, 4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01305();
+        sut.GetAllElements(TreeNode.Parse("[1,null,8]"), TreeNode.Parse("[8,1]")).Should().Equal(1, 1, 8, 8);
+    }
+}
---
+++ leetcode-sharp/1300/S01305.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 1305. All Elements in Two Binary Search Trees
+// https://leetcode.com/problems/all-elements-in-two-binary-search-trees
+public class S01305
+{
+    private readonly List<int> _result = new();
+
+    private void dfs(TreeNode? root)
+    {
+        if (root == null)
+            return;
+
+        _result.Add(root.val);
+        
+        dfs(root.left);
+        dfs(root.right);
+    }
+
+    public IList<int> GetAllElements(TreeNode? root1, TreeNode? root2)
+    {
+        dfs(root1);
+        dfs(root2);
+
+        _result.Sort();
+        
+        return _result;
+    }
+}
