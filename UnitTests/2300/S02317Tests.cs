c5f09dc4cfdc6d16e7d29dcd2d7ac9e59c91b7ac
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-24 12:11:00 +1000

    2317. Maximum XOR After Operations

---
+++ UnitTests/2300/S02317Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02317Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02317();
+        sut.MaximumXOR(new[] {3, 2, 4, 6}).Should().Be(7);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02317();
+        sut.MaximumXOR(new[] {1, 2, 3, 9, 2}).Should().Be(11);
+    }
+}
---
+++ leetcode-sharp/2300/S02317.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 2317. Maximum XOR After Operations
+// https://leetcode.com/problems/maximum-xor-after-operations
+public class S02317
+{
+    public int MaximumXOR(int[] nums)
+    {
+        return nums.Aggregate(0, (current, num) => current | num);
+    }
+}
