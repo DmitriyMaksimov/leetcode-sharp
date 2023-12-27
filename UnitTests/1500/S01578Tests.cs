2927adabea4ccb19f710f07bbe33810342d639e6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-27 11:57:13 +1000

    1578. Minimum Time to Make Rope Colorful

---
+++ UnitTests/1500/S01578Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01578Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01578();
+        sut.MinCost("abaac", new[] {1, 2, 3, 4, 5}).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01578();
+        sut.MinCost("abc", new[] {1, 2, 3}).Should().Be(0);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01578();
+        sut.MinCost("aabaa", new[] {1, 2, 3, 4, 1}).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/1500/S01578.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 1578. Minimum Time to Make Rope Colorful
+// https://leetcode.com/problems/minimum-time-to-make-rope-colorful
+public class S01578
+{
+    public int MinCost(string colors, int[] neededTime)
+    {
+        var result = 0;
+        var maxCost = 0;
+        var sumCost = 0;
+        var n = colors.Length;
+
+        for (var i = 0; i < n; ++i)
+        {
+            if (i > 0 && colors[i] != colors[i - 1])
+            {
+                result += sumCost - maxCost;
+                sumCost = 0;
+                maxCost = 0;
+            }
+
+            sumCost += neededTime[i];
+            maxCost = Math.Max(maxCost, neededTime[i]);
+        }
+
+        result += sumCost - maxCost;
+
+        return result;
+    }
+}
