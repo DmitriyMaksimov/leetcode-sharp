46d3560b7763f6c4a8cf46cdf415b50f7ff4b8d2
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-28 10:46:01 +1000

    1531. String Compression II

---
+++ UnitTests/1500/S01531Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01531Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01531();
+        sut.GetLengthOfOptimalCompression("aaabcccd", 2).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01531();
+        sut.GetLengthOfOptimalCompression("aabbaa", 2).Should().Be(2);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01531();
+        sut.GetLengthOfOptimalCompression("aaaaaaaaaaa", 0).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/1500/S01531.cs
@@ -0,0 +1,55 @@
+﻿namespace leetcode_sharp;
+
+// 1531. String Compression II
+// https://leetcode.com/problems/string-compression-ii
+public class S01531
+{
+    private const int N = 127;
+    private readonly int[,] _dp = new int[N, N];
+
+    private int solve(string str, int left, int k)
+    {
+        if (k < 0)
+        {
+            return N;
+        }
+
+        var n = str.Length;
+
+        if (left >= n || n - left <= k)
+        {
+            return 0;
+        }
+
+        var res = _dp[left, k];
+        if (res != -1)
+        {
+            return res;
+        }
+
+        res = N;
+
+        var cnt = new int[26];
+
+        for (int j = left, most = 0; j < n; j++)
+        {
+            most = Math.Max(most, ++cnt[str[j] - 'a']);
+            res = Math.Min(res, 1 + (most == 1 ? 0 : most < 10 ? 1 : most < 100 ? 2 : 3) + solve(str, j + 1, k - (j - left + 1 - most)));
+        }
+
+        return _dp[left, k] = res;
+    }
+
+    public int GetLengthOfOptimalCompression(string s, int k)
+    {
+        for (var i = 0; i < N; i++)
+        {
+            for (var j = 0; j < N; j++)
+            {
+                _dp[i, j] = -1;
+            }
+        }
+
+        return solve(s, 0, k);
+    }
+}
