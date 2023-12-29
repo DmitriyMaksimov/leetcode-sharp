32ee8a6569dbfd6d4b35c73cd9f732b50e2d0203
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-29 12:03:48 +1000

    1335. Minimum Difficulty of a Job Schedule

---
+++ UnitTests/1300/S01335Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01335Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01335();
+        sut.MinDifficulty(new[] {6, 5, 4, 3, 2, 1}, 2).Should().Be(7);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01335();
+        sut.MinDifficulty(new[] {9, 9, 9}, 4).Should().Be(-1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01335();
+        sut.MinDifficulty(new[] {1, 1, 1}, 3).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/1300/S01335.cs
@@ -0,0 +1,53 @@
+﻿namespace leetcode_sharp;
+
+// 1335. Minimum Difficulty of a Job Schedule
+// https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule
+public class S01335
+{
+    public int MinDifficulty(int[] jobDifficulty, int d)
+    {
+        var n = jobDifficulty.Length;
+
+        if (n < d)
+        {
+            return -1;
+        }
+
+        if (n == d)
+        {
+            var totalDifficulty = jobDifficulty.Sum();
+            return totalDifficulty;
+        }
+
+        var dp = new int[n];
+        dp[0] = jobDifficulty[0];
+
+        for (var i = 1; i < n; i++)
+        {
+            dp[i] = Math.Max(dp[i - 1], jobDifficulty[i]);
+        }
+
+        var dpPrev = new int[n];
+
+        for (var i = 1; i < d; i++)
+        {
+            (dpPrev, dp) = (dp, dpPrev);
+
+            for (var j = i; j < n; j++)
+            {
+                var lastDayDifficulty = jobDifficulty[j];
+                var tmpMin = lastDayDifficulty + dpPrev[j - 1];
+
+                for (var t = j - 1; t >= i; t--)
+                {
+                    lastDayDifficulty = Math.Max(lastDayDifficulty, jobDifficulty[t]);
+                    tmpMin = Math.Min(tmpMin, lastDayDifficulty + dpPrev[t - 1]);
+                }
+
+                dp[j] = tmpMin;
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
