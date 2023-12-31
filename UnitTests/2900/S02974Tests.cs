5c4a094d4f3e155f6304ad43e947293dfe4045ab
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2023-12-31 13:14:34 +1000

    2974. Minimum Number Game

---
+++ UnitTests/2900/S02974Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02974Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02974();
+        sut.NumberGame(new[] {5, 4, 2, 3}).Should().Equal(3, 2, 5, 4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02974();
+        sut.NumberGame(new[] {2, 5}).Should().Equal(5, 2);
+    }
+}
---
+++ leetcode-sharp/2900/S02974.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode_sharp;
+
+// 2974. Minimum Number Game
+// https://leetcode.com/problems/minimum-number-game
+public class S02974
+{
+    public int[] NumberGame(int[] nums)
+    {
+        Array.Sort(nums);
+        
+        for (var i = 0; i < nums.Length - 1; i += 2)
+        {
+            (nums[i + 1], nums[i]) = (nums[i], nums[i + 1]);
+        }
+
+        return nums;
+    }
+}
