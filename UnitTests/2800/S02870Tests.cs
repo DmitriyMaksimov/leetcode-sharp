d00271d73f83490d3f452a91eb7184830a2ec285
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-04 10:08:19 +1000

    2870. Minimum Number of Operations to Make Array Empty

---
+++ UnitTests/2800/S02870Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02870Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02870();
+        sut.MinOperations(new[] {2, 3, 3, 2, 2, 4, 2, 3, 4}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02870();
+        sut.MinOperations(new[] {2, 1, 2, 2, 3, 3}).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/2800/S02870.cs
@@ -0,0 +1,39 @@
+﻿namespace leetcode_sharp;
+
+// 2870. Minimum Number of Operations to Make Array Empty
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty
+public class S02870
+{
+    public int MinOperations(int[] nums)
+    {
+        var dictionary = createMinOperationsDictionary(nums);
+        var result = 0;
+
+        foreach (var item in dictionary)
+        {
+            if (item.Value == 1) return -1;
+
+            if (item.Value % 3 == 0)
+            {
+                result += item.Value / 3;
+            }
+            else
+            {
+                result += item.Value / 3 + 1;
+            }
+        }
+
+        return result;
+    }
+
+    private static Dictionary<int, int> createMinOperationsDictionary(IReadOnlyList<int> nums)
+    {
+        var dictionary = new Dictionary<int, int>();
+        foreach (var n in nums)
+        {
+            dictionary[n] = dictionary.GetValueOrDefault(n) + 1;
+        }
+
+        return dictionary;
+    }
+}
