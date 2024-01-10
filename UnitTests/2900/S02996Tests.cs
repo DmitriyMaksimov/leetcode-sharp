4f3e880126a08e5c77d7d23a7d1ef797b363a425
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-11 07:08:40 +1000

    2996. Smallest Missing Integer Greater Than Sequential Prefix Sum

---
+++ UnitTests/2900/S02996Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02996Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02996();
+        sut.MissingInteger(new[] {1, 2, 3, 2, 5}).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02996();
+        sut.MissingInteger(new[] {3, 4, 5, 1, 12, 14, 13}).Should().Be(15);
+    }
+}
