1ba333ca1cfc416dc0a64388e48600e7a50b701c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-11 07:08:40 +1000

    2996. Smallest Missing Integer Greater Than Sequential Prefix Sum

---
+++ leetcode-sharp/2900/S02996.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum
+// https://leetcode.com/problems/smallest-missing-integer-greater-than-sequential-prefix-sum
+public class S02996
+{
+    public int MissingInteger(int[] nums)
+    {
+        var countSum = nums[0];
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i - 1] + 1 != nums[i])
+            {
+                break;
+            }
+
+            countSum += nums[i];
+        }
+
+        foreach (var num in nums.OrderBy(x => x))
+        {
+            if (countSum == num)
+            {
+                countSum++;
+            }
+        }
+
+        return countSum;
+    }
+}
