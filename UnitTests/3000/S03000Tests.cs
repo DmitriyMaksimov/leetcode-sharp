18a7bfbb6643fc1dd1c0b5d159b84007c7832b30
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-11 07:01:11 +1000

    3000. Maximum Area of Longest Diagonal Rectangle

---
+++ UnitTests/3000/S03000Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S03000Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S03000();
+        sut.AreaOfMaxDiagonal(new[] {new[] {9, 3}, new[] {8, 6}}).Should().Be(48);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S03000();
+        sut.AreaOfMaxDiagonal(new[] {new[] {3, 4}, new[] {4, 3}}).Should().Be(12);
+    }
+}
---
+++ leetcode-sharp/3000/S03000.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 3000. Maximum Area of Longest Diagonal Rectangle
+// https://leetcode.com/problems/maximum-area-of-longest-diagonal-rectangle
+public class S03000
+{
+    public int AreaOfMaxDiagonal(int[][] dimensions)
+    {
+        var maxArea = -1;
+        var maxDiagonal = -1L;
+
+        foreach (var dimension in dimensions)
+        {
+            var length = dimension[0];
+            var width = dimension[1];
+            long diagonal = length * length + width * width;
+
+            if (diagonal > maxDiagonal)
+            {
+                maxDiagonal = diagonal;
+                maxArea = length * width;
+            }
+            else if (diagonal == maxDiagonal)
+            {
+                maxArea = Math.Max(maxArea, length * width);
+            }
+        }
+
+        return maxArea;
+    }
+}
