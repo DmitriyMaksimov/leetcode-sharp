052bcf90ab164d5bf5876dea8730b1bc5e0c2116
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-11 18:50:21 +1000

    2980. Check if Bitwise OR Has Trailing Zeros

---
+++ UnitTests/2900/S02980Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02980Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02980();
+        sut.HasTrailingZeros(new[] {1, 2, 3, 4, 5}).Should().BeTrue();
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02980();
+        sut.HasTrailingZeros(new[] {1, 3, 5, 7, 9}).Should().BeFalse();
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02980();
+        sut.HasTrailingZeros(new[] {2, 4, 8, 16}).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/2900/S02980.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 2980. Check if Bitwise OR Has Trailing Zeros
+// https://leetcode.com/problems/check-if-bitwise-or-has-trailing-zeros
+public class S02980
+{
+    public bool HasTrailingZeros(int[] nums)
+    {
+        var countOfEvenNumbers = 0;
+        
+        foreach (var num in nums)
+        {
+            if (num % 2 != 0) continue;
+
+            if (++countOfEvenNumbers == 2)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
