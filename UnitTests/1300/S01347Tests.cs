e125001959b54369a3aad32ab712d277bee507ad
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-13 10:32:18 +1000

    1347. Minimum Number of Steps to Make Two Strings Anagram

---
+++ UnitTests/1300/S01347Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01347Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01347();
+        sut.MinSteps("bab", "aba").Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01347();
+        sut.MinSteps("leetcode", "practice").Should().Be(5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01347();
+        sut.MinSteps("anagram", "mangaar").Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1300/S01347.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 1347. Minimum Number of Steps to Make Two Strings Anagram
+// https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram
+public class S01347
+{
+    public int MinSteps(string s, string t)
+    {
+        const int maxLetters = 26;
+
+        var arrS = new int[maxLetters];
+        var arrT = new int[maxLetters];
+
+        foreach (var c in s)
+        {
+            arrS[c - 'a']++;
+        }
+
+        foreach (var c in t)
+        {
+            arrT[c - 'a']++;
+        }
+
+        var count = 0;
+
+        for (var i = 0; i < maxLetters; i++)
+        {
+            count += Math.Max(arrS[i] - arrT[i], 0);
+        }
+
+        return count;
+    }
+}
