69cfebe773acddd328cfc84684a45303a1cb51b1
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-13 10:07:19 +1000

    2657. Find the Prefix Common Array of Two Arrays

---
+++ UnitTests/2600/S02657Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02657Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02657();
+        sut.FindThePrefixCommonArray(new[] {1, 3, 2, 4}, new[] {3, 1, 2, 4}).Should().Equal(0, 2, 3, 4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02657();
+        sut.FindThePrefixCommonArray(new[] {2, 3, 1}, new[] {3, 1, 2}).Should().Equal(0, 1, 3);
+    }
+}
---
+++ leetcode-sharp/2600/S02657.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 2657. Find the Prefix Common Array of Two Arrays
+// https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays
+public class S02657
+{
+    public int[] FindThePrefixCommonArray(int[] A, int[] B)
+    {
+        HashSet<int> setA = new();
+        HashSet<int> setB = new();
+        var result = new int[A.Length];
+        var count = 0;
+
+        for (var i = 0; i < B.Length; i++)
+        {
+            if (A[i] == B[i])
+            {
+                ++count;
+            }
+            else
+            {
+                setA.Add(A[i]);
+                setB.Add(B[i]);
+            }
+
+            if (setA.Contains(B[i]))
+            {
+                ++count;
+            }
+
+            if (setB.Contains(A[i]))
+            {
+                ++count;
+            }
+
+            result[i] = count;
+        }
+
+        return result;
+    }
+}
