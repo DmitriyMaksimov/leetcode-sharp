b3de4e6d15373068a95647876c81cc93ec691f52
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-13 10:13:39 +1000

    2079. Watering Plants

---
+++ UnitTests/2000/S02079Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02079Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02079();
+        sut.WateringPlants(new[] {2, 2, 3, 3}, 5).Should().Be(14);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02079();
+        sut.WateringPlants(new[] {1, 1, 1, 4, 2, 3}, 4).Should().Be(30);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02079();
+        sut.WateringPlants(new[] {7, 7, 7, 7, 7, 7, 7}, 8).Should().Be(49);
+    }
+}
---
+++ leetcode-sharp/2000/S02079.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 2079. Watering Plants
+// https://leetcode.com/problems/watering-plants
+public class S02079
+{
+    public int WateringPlants(int[] plants, int capacity)
+    {
+        var res = 0;
+        var can = capacity;
+
+        for (var i = 0; i < plants.Length; ++i)
+        {
+            if (can < plants[i])
+            {
+                res += i * 2;
+                can = capacity;
+            }
+
+            can -= plants[i];
+        }
+
+        return res + plants.Length;
+    }
+}
