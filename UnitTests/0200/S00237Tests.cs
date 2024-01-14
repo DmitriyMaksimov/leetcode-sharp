4e981f8e046422730091098ede3ce570a20b5f0f
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-14 19:27:34 +1000

    237. Delete Node in a Linked List

---
+++ UnitTests/0200/S00237Tests.cs
@@ -0,0 +1,25 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00237Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00237();
+        var list = ListNode.Parse("[4,5,1,9]");
+        sut.DeleteNode(list!.next!);
+        list.Should().BeEquivalentTo(ListNode.Parse("[4,1,9]"));
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00237();
+        var list = ListNode.Parse("[4,5,1,9]");
+        sut.DeleteNode(list!.next!.next!);
+        list.Should().BeEquivalentTo(ListNode.Parse("[4,5,9]"));
+    }
+}
---
+++ leetcode-sharp/0200/S00237.cs
@@ -0,0 +1,12 @@
+﻿namespace leetcode_sharp;
+
+// 237. Delete Node in a Linked List
+// https://leetcode.com/problems/delete-node-in-a-linked-list
+public class S00237
+{
+    public void DeleteNode(ListNode node)
+    {
+        node.val = node.next!.val;
+        node.next = node.next.next;
+    }
+}
