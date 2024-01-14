9d96e1648ee9d28547d7fd2fe5e8d9fa8133954f
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-14 19:19:57 +1000

    1823. Find the Winner of the Circular Game

---
+++ UnitTests/1800/S01823Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01823Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01823();
+        sut.FindTheWinner(5, 2).Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01823();
+        sut.FindTheWinner(6, 5).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1800/S01823.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 1823. Find the Winner of the Circular Game
+// https://leetcode.com/problems/find-the-winner-of-the-circular-game
+public class S01823
+{
+    public int FindTheWinner(int n, int k)
+    {
+        var queue = new Queue<int>(Enumerable.Range(1, n));
+
+        var count = k;
+
+        while (queue.Count > 1)
+        {
+            var player = queue.Dequeue();
+
+            if (--count > 0)
+            {
+                queue.Enqueue(player);
+            }
+            else
+            {
+                count = k;
+            }
+        }
+
+        return queue.Dequeue();
+    }
+}
