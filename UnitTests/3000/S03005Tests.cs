57de43561625962521f709796ede1d7d7406e255
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-14 19:15:00 +1000

    3005. Count Elements With Maximum Frequency

---
+++ UnitTests/3000/S03005Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S03005Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S03005();
+        sut.MaxFrequencyElements(new[] {1, 2, 2, 3, 1, 4}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S03005();
+        sut.MaxFrequencyElements(new[] {1, 2, 3, 4, 5}).Should().Be(5);
+    }
+}
---
+++ leetcode-sharp/3000/S03005.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 3005. Count Elements With Maximum Frequency
+// https://leetcode.com/problems/count-elements-with-maximum-frequency
+public class S03005
+{
+    public int MaxFrequencyElements(int[] nums)
+    {
+        const int maxElements = 100;
+        var frequencies = new int[maxElements + 1];
+        var maxFrequency = 0;
+        
+        foreach (var num in nums)
+        {
+            if (++frequencies[num] > maxFrequency)
+            {
+                maxFrequency = frequencies[num];
+            }
+        }
+        
+        return frequencies.Count(x => x== maxFrequency) * maxFrequency;
+    }
+}
