17ceb861779281935a0b0009a36cfe3c28992f48
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-18 14:16:01 +1000

    1641. Count Sorted Vowel Strings

---
+++ UnitTests/1600/S01641Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01641Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01641();
+        sut.CountVowelStrings(1).Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01641();
+        sut.CountVowelStrings(2).Should().Be(15);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01641();
+        sut.CountVowelStrings(33).Should().Be(66045);
+    }
+}
---
+++ leetcode-sharp/1600/S01641.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode_sharp;
+
+// 1641. Count Sorted Vowel Strings
+// https://leetcode.com/problems/count-sorted-vowel-strings
+public class S01641
+{
+    public int CountVowelStrings(int n)
+    {
+        return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
+    }
+}
