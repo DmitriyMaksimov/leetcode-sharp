05d20e3ddf750e4c051fbfa96ac0fc4cabb87c84
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-18 16:41:04 +1000

    950. Reveal Cards In Increasing Order

---
+++ UnitTests/0900/S00950Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00950Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00950();
+        sut.DeckRevealedIncreasing(new[] {17, 13, 11, 2, 3, 5, 7}).Should().Equal(2, 13, 3, 11, 5, 17, 7);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00950();
+        sut.DeckRevealedIncreasing(new[] {1, 1000}).Should().Equal(1, 1000);
+    }
+}
---
+++ leetcode-sharp/0900/S00950.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 950. Reveal Cards In Increasing Order
+// https://leetcode.com/problems/reveal-cards-in-increasing-order
+public class S00950
+{
+    public int[] DeckRevealedIncreasing(int[] deck)
+    {
+        Array.Sort(deck);
+        var n = deck.Length;
+        var ans = new int[n];
+        var q = new Queue<int>(Enumerable.Range(0, n));
+
+        for (var i = 0; i < n; i++)
+        {
+            ans[q.Dequeue()] = deck[i];
+            if (q.Count > 0)
+            {
+                q.Enqueue(q.Dequeue());
+            }
+        }
+
+        return ans;
+    }
+}
