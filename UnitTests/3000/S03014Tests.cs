858d6b897de23cac2fb53b137d3d5bed5c3849f7
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-23 19:39:42 +1000

    3014. Minimum Number of Pushes to Type Word I

---
+++ UnitTests/3000/S03014Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S03014Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S03014();
+        sut.MinimumPushes("abcde").Should().Be(5);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S03014();
+        sut.MinimumPushes("xycdefghij").Should().Be(12);
+    }
+}
---
+++ leetcode-sharp/3000/S03014.cs
@@ -0,0 +1,16 @@
+﻿namespace leetcode_sharp;
+
+// 3014. Minimum Number of Pushes to Type Word I
+// https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i
+public class S03014
+{
+    public int MinimumPushes(string word)
+    {
+        var n = word.Length;
+
+        var quotient = n / 8;
+        var remainder = n % 8;
+
+        return quotient * (quotient + 1) / 2 * 8 + remainder * (quotient + 1);
+    }
+}
