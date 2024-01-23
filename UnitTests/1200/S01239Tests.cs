b5df43a3195bcd5873143044562ebbd04b182a81
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-23 11:23:48 +1000

    1239. Maximum Length of a Concatenated String with Unique Characters

---
+++ UnitTests/1200/S01239Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01239Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01239();
+        sut.MaxLength(new[] { "un","iq","ue"}).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01239();
+        sut.MaxLength(new[] {"cha","r","act","ers" }).Should().Be(6);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01239();
+        sut.MaxLength(new[] { "abcdefghijklmnopqrstuvwxyz"}).Should().Be(26);
+    }
+}
---
+++ leetcode-sharp/1200/S01239.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 1239. Maximum Length of a Concatenated String with Unique Characters
+// https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters
+public class S01239
+{
+    public int MaxLength(IList<string> arr)
+    {
+        return maxLength(arr, 0, "");
+    }
+
+    private static int maxLength(IList<string> arr, int n, string s)
+    {
+        if (s.Distinct().Count() < s.Length)
+        {
+            return 0;
+        }
+
+        return arr.Count == n ? s.Length : Math.Max(maxLength(arr, n + 1, s), maxLength(arr, n + 1, s + arr[n]));
+    }
+}
