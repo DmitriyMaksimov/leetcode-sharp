956b6ae7c232d1c3851870de45a876d66eafe5dd
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-23 19:27:45 +1000

    3010. Divide an Array Into Subarrays With Minimum Cost I

---
+++ UnitTests/3000/S03010Tests.cs
@@ -0,0 +1,35 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S03010Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S03010();
+        sut.MinimumCost([1, 2, 3, 12]).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S03010();
+        sut.MinimumCost([5, 4, 3]).Should().Be(12);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S03010();
+        sut.MinimumCost([10, 3, 1, 1]).Should().Be(12);
+    }
+
+    [Test]
+    public void T4()
+    {
+        var sut = new S03010();
+        sut.MinimumCost([1, 6, 1, 5]).Should().Be(7);
+    }
+}
---
+++ leetcode-sharp/3000/S03010.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 3010. Divide an Array Into Subarrays With Minimum Cost I
+// https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i
+public class S03010
+{
+    public int MinimumCost(int[] nums)
+    {
+        var n1 = nums[1];
+        var n2 = nums[2];
+
+        for (var i = 3; i < nums.Length; i++)
+        {
+            
+            if (nums[i] <= n1 || nums[i] <= n2)
+            {
+                n1 = Math.Min(n1, n2);
+                n2 = nums[i];
+            }
+        }
+
+        return nums[0] + n1 + n2;
+    }
+}
