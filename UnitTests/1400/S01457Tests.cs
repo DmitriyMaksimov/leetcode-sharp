3684f398813fc01fe3900fdf8ede340d0ff46326
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-24 14:47:02 +1000

    1457. Pseudo-Palindromic Paths in a Binary Tree

---
+++ UnitTests/1400/S01457Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01457Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01457();
+        sut.PseudoPalindromicPaths(TreeNode.Parse("[2,3,1,3,1,null,1]")).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01457();
+        sut.PseudoPalindromicPaths(TreeNode.Parse("[2,1,1,1,3,null,null,null,null,null,1]")).Should().Be(1);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01457();
+        sut.PseudoPalindromicPaths(TreeNode.Parse("[9]")).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1400/S01457.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 1457. Pseudo-Palindromic Paths in a Binary Tree
+// https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree
+public class S01457
+{
+    public int PseudoPalindromicPaths(TreeNode? root)
+    {
+        return CountPseudoPalindromicPaths(root, 0);
+    }
+
+    private int CountPseudoPalindromicPaths(TreeNode? node, int count)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+
+        count ^= 1 << (node.val - 1);
+
+        if (node.left == null && node.right == null)
+        {
+            // Check if the current path is pseudo-palindromic
+            return (count & (count - 1)) == 0 ? 1 : 0;
+        }
+
+        var leftPaths = CountPseudoPalindromicPaths(node.left, count);
+        var rightPaths = CountPseudoPalindromicPaths(node.right, count);
+
+        return leftPaths + rightPaths;
+    }
+}
