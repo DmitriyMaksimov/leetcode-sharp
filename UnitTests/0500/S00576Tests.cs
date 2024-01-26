8710744bede0a407c6bd4eb0db5d3e56d2fd393a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-26 17:46:29 +1000

    576. Out of Boundary Paths

---
+++ UnitTests/0500/S00576Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00576Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00576();
+        sut.FindPaths(2, 2, 2, 0, 0).Should().Be(6);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00576();
+        sut.FindPaths(1, 3, 3, 0, 1).Should().Be(12);
+    }
+}
---
+++ leetcode-sharp/0500/S00576.cs
@@ -0,0 +1,48 @@
+﻿namespace leetcode_sharp;
+
+// 576. Out of Boundary Paths
+// https://leetcode.com/problems/out-of-boundary-paths
+public class S00576
+{
+    private int _m;
+    private int _n;
+    private const int Mod = 1_000_000_007;
+    
+    public int FindPaths(int m, int n, int maxMove, int startRow, int startColumn)
+    {
+        var dp = new int[m, n, maxMove + 1];
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                for (var k = 0; k <= maxMove; k++)
+                {
+                    dp[i, j, k] = -1;
+                }
+            }
+        }
+
+        this._m = m;
+        this._n = n;
+        return helper(maxMove, startRow, startColumn, dp);
+    }
+
+    private int helper(int maxMove, int x, int y, int[,,] dp)
+    {
+        if (x < 0 || x >= _m || y < 0 || y >= _n) return 1;
+        if (maxMove <= 0) return 0;
+        if (dp[x, y, maxMove] != -1)
+        {
+            return dp[x, y, maxMove];
+        }
+
+        var res = helper(maxMove - 1, x + 1, y, dp) % Mod;
+        res = (res + helper(maxMove - 1, x, y - 1, dp)) % Mod;
+        res = (res + helper(maxMove - 1, x - 1, y, dp)) % Mod;
+        res = (res + helper(maxMove - 1, x, y + 1, dp)) % Mod;
+
+        dp[x, y, maxMove] = res;
+
+        return res;
+    }
+}
