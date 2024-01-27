bc8418f2fccc56298cdd08a1cb42bd56b566c3eb
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-27 10:21:37 +1000

    629. K Inverse Pairs Array

---
+++ UnitTests/0600/S00629Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00629Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00629();
+        sut.KInversePairs(3, 0).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00629();
+        sut.KInversePairs(3, 1).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/0600/S00629.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 629. K Inverse Pairs Array
+// https://leetcode.com/problems/k-inverse-pairs-array
+public class S00629
+{
+    private const int Mod = 1000000007;
+    private readonly int[,] _dp = new int[1001, 1001];
+
+    public int KInversePairs(int n, int k)
+    {
+        _dp[0, 0] = 1;
+
+        if (_dp[n, k] != 0)
+        {
+            return _dp[n, k];
+        }
+
+        for (var i = 1; i <= n; ++i)
+        {
+            for (var j = 0; j <= k; ++j)
+            {
+                _dp[i, j] = (_dp[i - 1, j] + (j > 0 ? _dp[i, j - 1] : 0)) % Mod;
+                if (j >= i)
+                {
+                    _dp[i, j] = (Mod + _dp[i, j] - _dp[i - 1, j - i]) % Mod;
+                }
+            }
+        }
+
+        return _dp[n, k];
+    }
+}
