f6afee9ace5f8d1c37661ae62135857e9f5f3b2d
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-28 12:28:58 +1000

    1074. Number of Submatrices That Sum to Target

---
+++ UnitTests/1000/S01074Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01074Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01074();
+        sut.NumSubmatrixSumTarget([[0, 1, 0], [1, 1, 1], [0, 1, 0]], 0).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01074();
+        sut.NumSubmatrixSumTarget([[1, -1], [-1, 1]], 0).Should().Be(5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01074();
+        sut.NumSubmatrixSumTarget([[904]], 0).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/1000/S01074.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcode_sharp;
+
+// 1074. Number of Submatrices That Sum to Target
+// https://leetcode.com/problems/number-of-submatrices-that-sum-to-target
+public class S01074
+{
+    public int NumSubmatrixSumTarget(int[][] matrix, int target)
+    {
+        var result = 0;
+        var m = matrix.Length;
+        var n = matrix[0].Length;
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 1; j < n; j++)
+            {
+                matrix[i][j] += matrix[i][j - 1];
+            }
+        }
+
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = i; j < n; j++)
+            {
+                var counter = new Dictionary<int, int> {[0] = 1};
+                var current = 0;
+
+                for (var k = 0; k < m; k++)
+                {
+                    current += matrix[k][j] - (i > 0 ? matrix[k][i - 1] : 0);
+                    result += counter.GetValueOrDefault(current - target);
+                    counter[current] = counter.GetValueOrDefault(current) + 1;
+                }
+            }
+        }
+
+        return result;
+    }
+}
