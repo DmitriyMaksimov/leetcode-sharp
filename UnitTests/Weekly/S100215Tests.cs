ee38736e7f11a2103fc54f722def6d42e55db1d6
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-01-28 15:46:27 +1000

    Number of Changing Keys

---
+++ UnitTests/Weekly/S100215Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests.Weekly;
+
+[TestFixture]
+public class S100215Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S100215();
+        sut.CountKeyChanges("aAbBcC").Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S100215();
+        sut.CountKeyChanges("AaAaAaaA").Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/Weekly/S100215.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// Number of Changing Keys
+public class S100215
+{
+    public int CountKeyChanges(string s)
+    {
+        s = s.ToLower();
+        var count = 0;
+        var lastKey = s[0];
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (s[i] != lastKey)
+            {
+                lastKey = s[i];
+                ++count;
+            }
+        }
+
+        return count;
+    }
+}
