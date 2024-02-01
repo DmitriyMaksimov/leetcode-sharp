9f2ae8f818c6d202edcca09d20c582ddf5b7d560
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-01 18:52:08 +1000

    2966. Divide Array Into Arrays With Max Difference

---
+++ UnitTests/2900/S02966Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02966Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02966();
+        sut.DivideArray([1, 3, 4, 8, 7, 9, 3, 5, 1], 2).Should().BeEquivalentTo([(int[]) [1, 1, 3], [3, 4, 5], [7, 8, 9]]);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02966();
+        sut.DivideArray([1, 3, 3, 2, 7, 3], 3).Should().BeEmpty();
+    }
+}
---
+++ leetcode-sharp/2900/S02966.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 2966. Divide Array Into Arrays With Max Difference
+// https://leetcode.com/problems/divide-array-into-arrays-with-max-difference
+public class S02966
+{
+    public int[][] DivideArray(int[] nums, int k)
+    {
+        var size = nums.Length;
+        if (size % 3 != 0)
+        {
+            return [];
+        }
+
+        Array.Sort(nums);
+
+        var result = new List<int[]>();
+
+        for (var i = 0; i < size; i += 3)
+        {
+            if (i + 2 < size && nums[i + 2] - nums[i] <= k)
+            {
+                result.Add([nums[i], nums[i + 1], nums[i + 2]]);
+            }
+            else
+            {
+                return [];
+            }
+        }
+
+        return result.ToArray();
+    }
+}
