f98cfa189363a682903cb6ff35bf90473bab8487
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-04 09:51:01 +1000

    1910. Remove All Occurrences of a Substring

---
+++ UnitTests/1900/S01910Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01910Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01910();
+        sut.RemoveOccurrences("daabcbaabcbc", "abc").Should().Be("dab");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01910();
+        sut.RemoveOccurrences("axxxxyyyyb", "xy").Should().Be("ab");
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01910();
+        sut.RemoveOccurrences("aabababa", "aba").Should().Be("ba");
+    }
+}
---
+++ leetcode-sharp/1900/S01910.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace leetcode_sharp;
+
+// 1910. Remove All Occurrences of a Substring
+// https://leetcode.com/problems/remove-all-occurrences-of-a-substring
+public class S01910
+{
+    public string RemoveOccurrences(string s, string part)
+    {
+        var st = new Stack<char>();
+        var n = s.Length;
+        var m = part.Length;
+        for (var i = 0; i < n; i++)
+        {
+            st.Push(s[i]);
+            
+            if (st.Count < m) continue;
+            
+            var x = "";
+            
+            for (var j = m - 1; j >= 0; j--)
+            {
+                var ch = part[j];
+                if (ch != st.Peek())
+                {
+                    foreach (var c in x)
+                    {
+                        st.Push(c);
+                    }
+
+                    break;
+                }
+
+                x = st.Peek() + x;
+                st.Pop();
+            }
+        }
+
+        return new string(st.Reverse().ToArray());
+    }
+}
