67f4114ffbd78d266c4c48173f9ee940f0078616
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-03 11:22:39 +1000

    1291. Sequential Digits

---
+++ UnitTests/1200/S01291Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01291Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01291();
+        sut.SequentialDigits(100, 300).Should().Equal(123, 234);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01291();
+        sut.SequentialDigits(1000, 13000).Should().Equal(1234, 2345, 3456, 4567, 5678, 6789, 12345);
+    }
+}
---
+++ leetcode-sharp/1200/S01291.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 1291. Sequential Digits
+// https://leetcode.com/problems/sequential-digits
+public class S01291
+{
+    public IList<int> SequentialDigits(int low, int high)
+    {
+        const string digits = "123456789";
+        List<int> result = [];
+        var lowLength = low.ToString().Length;
+        var highLength = high.ToString().Length;
+
+        for (var currentLength = lowLength; currentLength <= highLength; currentLength++)
+        {
+            for (var startIndex = 0; startIndex < 10 - currentLength; startIndex++)
+            {
+                var num = int.Parse(digits.Substring(startIndex, currentLength));
+
+                if (low <= num && num <= high)
+                {
+                    result.Add(num);
+                }
+            }
+        }
+
+        return result;
+    }
+}
