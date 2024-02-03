452d9e90ee084a7398936eb77aa085eda12e570e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-03 11:29:27 +1000

    1043. Partition Array for Maximum Sum

---
+++ UnitTests/1000/S01043Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01043Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01043();
+        sut.MaxSumAfterPartitioning([1, 15, 7, 9, 2, 5, 10], 3).Should().Be(84);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01043();
+        sut.MaxSumAfterPartitioning([1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3], 4).Should().Be(83);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01043();
+        sut.MaxSumAfterPartitioning([1], 1).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1000/S01043.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 1043. Partition Array for Maximum Sum
+// https://leetcode.com/problems/partition-array-for-maximum-sum
+public class S01043
+{
+    public int MaxSumAfterPartitioning(int[] arr, int k)
+    {
+        var dp = new int[k + 1];
+
+        for (var start = arr.Length - 1; start >= 0; start--)
+        {
+            var currMax = 0;
+            int end = Math.Min(arr.Length, start + k);
+
+            for (var i = start; i < end; i++)
+            {
+                currMax = Math.Max(currMax, arr[i]);
+                dp[start % (k + 1)] = Math.Max(dp[start % (k + 1)], dp[(i + 1) % (k + 1)] + currMax * (i - start + 1));
+            }
+        }
+
+        return dp[0];
+    }
+}
