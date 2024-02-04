bd50feab153a8ec74daddd391182a4c1415e605a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-04 12:50:50 +1000

    1605. Find Valid Matrix Given Row and Column Sums

---
+++ UnitTests/1600/S01605Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01605Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01605();
+        sut.RestoreMatrix([3, 8], [4, 7]).Should().BeEquivalentTo([(int[]) [3, 0], [1, 7]]);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01605();
+        sut.RestoreMatrix([5, 7, 10], [8, 6, 8]).Should().BeEquivalentTo([(int[]) [0, 5, 0], [3, 4, 0], [2, 0, 8]]);
+    }
+}
---
+++ leetcode-sharp/1600/S01605.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_sharp;
+
+// 1605. Find Valid Matrix Given Row and Column Sums
+// https://leetcode.com/problems/find-valid-matrix-given-row-and-column-sums
+public class S01605
+{
+    public int[][] RestoreMatrix(int[] rowSum, int[] colSum)
+    {
+        var m = rowSum.Length;
+        var n = colSum.Length;
+        var matrix = new int[m][];
+
+        for (var i = 0; i < m; ++i)
+        {
+            matrix[i] = new int[n];
+            for (var j = 0; j < n; ++j)
+            {
+                matrix[i][j] = Math.Min(rowSum[i], colSum[j]);
+                rowSum[i] -= matrix[i][j];
+                colSum[j] -= matrix[i][j];
+            }
+        }
+
+        return matrix;
+    }
+}
