98e2905a3dec9cf22b8370d7ee39cde836475235
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-04 12:36:02 +1000

    100214. Ant on the Boundary

---
+++ UnitTests/Weekly/S100214Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S100214Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S100214();
+        sut.ReturnToBoundaryCount([2, 3, -5]).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S100214();
+        sut.ReturnToBoundaryCount([3, 2, -3, -4]).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/Weekly/S100214.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_sharp;
+
+// 100214. Ant on the Boundary
+// https://leetcode.com/contest/weekly-contest-383/problems/ant-on-the-boundary
+public class S100214
+{
+    public int ReturnToBoundaryCount(int[] nums)
+    {
+        var result = 0;
+        var currentPosition = 0;
+        
+        foreach (var num in nums)
+        {
+            currentPosition += num;
+            if (currentPosition == 0)
+            {
+                ++result;
+            }
+        }
+        
+        return result;
+    }
+}
