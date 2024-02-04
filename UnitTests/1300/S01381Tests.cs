53e416a01227e314f503dc1bbefae6cf06706bde
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-04 11:54:26 +1000

    1381. Design a Stack With Increment Operation

---
+++ UnitTests/1300/S01381Tests.cs
@@ -0,0 +1,25 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01381Tests
+{
+    [Test]
+    public void T1()
+    {
+        var stk = new S01381.CustomStack(3);
+        stk.Push(1); // stack becomes [1]
+        stk.Push(2); // stack becomes [1, 2]
+        stk.Pop().Should().Be(2); // return 2 --> Return top of the stack 2, stack becomes [1]
+        stk.Push(2); // stack becomes [1, 2]
+        stk.Push(3); // stack becomes [1, 2, 3]
+        stk.Push(4); // stack still [1, 2, 3], Do not add another elements as size is 4
+        stk.Increment(5, 100); // stack becomes [101, 102, 103]
+        stk.Increment(2, 100); // stack becomes [201, 202, 103]
+        stk.Pop().Should().Be(103); // return 103 --> Return top of the stack 103, stack becomes [201, 202]
+        stk.Pop().Should().Be(202); // return 202 --> Return top of the stack 202, stack becomes [201]
+        stk.Pop().Should().Be(201); // return 201 --> Return top of the stack 201, stack becomes []
+        stk.Pop().Should().Be(-1); // return -1 --> Stack is empty return -1.
+    }
+}
---
+++ leetcode-sharp/1300/S01381.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode_sharp;
+
+// 1381. Design a Stack With Increment Operation
+// https://leetcode.com/problems/design-a-stack-with-increment-operation
+public class S01381
+{
+    public class CustomStack
+    {
+        private readonly int _maxSize;
+        private readonly int[] _stack;
+        private int _numberOfElements = 0;
+
+        public CustomStack(int maxSize)
+        {
+            _maxSize = maxSize;
+            _stack = new int[maxSize];
+        }
+
+        public void Push(int x)
+        {
+            if (_numberOfElements < _maxSize)
+            {
+                _stack[_numberOfElements++] = x;
+            }
+        }
+
+        public int Pop()
+        {
+            return _numberOfElements == 0 ? -1 : _stack[--_numberOfElements];
+        }
+
+        public void Increment(int k, int val)
+        {
+            var n = Math.Min(_numberOfElements, k);
+            for (var i = 0; i < n; i++)
+            {
+                _stack[i] += val;
+            }
+        }
+    }
+}
