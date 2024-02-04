db25aebea61be8a8363e138ba971990fc41ca6f7
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-04 12:24:04 +1000

    1829. Maximum XOR for Each Query

---
+++ UnitTests/1800/S01829Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01829Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01829();
+        sut.GetMaximumXor([0, 1, 1, 3], 2).Should().Equal(0, 3, 2, 3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01829();
+        sut.GetMaximumXor([2, 3, 4, 7], 3).Should().Equal(5, 2, 6, 5);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01829();
+        sut.GetMaximumXor([0, 1, 2, 2, 5, 7], 3).Should().Equal(4, 3, 6, 4, 6, 7);
+    }
+}
---
+++ leetcode-sharp/1800/S01829.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_sharp;
+
+// 1829. Maximum XOR for Each Query
+// https://leetcode.com/problems/maximum-xor-for-each-query
+public class S01829
+{
+    public int[] GetMaximumXor(int[] nums, int maximumBit)
+    {
+        var n = nums.Length;
+        var result = new int[n];
+        var xor = nums.Aggregate(0, (current, num) => current ^ num);
+
+        result[0] = xor ^= (1 << maximumBit) - 1;
+
+        for (var i = 1; i < n; i++)
+        {
+            result[i] = xor ^= nums[n - i];
+        }
+
+        return result;
+    }
+}
