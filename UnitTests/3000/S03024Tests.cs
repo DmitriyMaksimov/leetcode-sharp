56e2c0253cf89598a0f65ee1c6071ef99d67af05
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-05 20:57:19 +1000

    3024. Type of Triangle II

---
+++ UnitTests/3000/S03024Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S03024Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S03024();
+        sut.TriangleType([3, 3, 3]).Should().Be("equilateral");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S03024();
+        sut.TriangleType([3, 4, 5]).Should().Be("scalene");
+    }
+}
---
+++ leetcode-sharp/3000/S03024.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 
+// https://leetcode.com/problems/type-of-triangle-ii
+public class S03024
+{
+    public string TriangleType(int[] nums)
+    {
+        if (nums[0] + nums[1] <= nums[2] ||
+            nums[0] + nums[2] <= nums[1] ||
+            nums[1] + nums[2] <= nums[0])
+        {
+            return "none";
+        }
+
+        if (nums[0] == nums[1] && nums[1] == nums[2])
+        {
+            return "equilateral";
+        }
+
+        if (nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2])
+        {
+            return "scalene";
+        }
+        
+        if (nums[0] == nums[1] || nums[0] == nums[2] || nums[1] == nums[2])
+        {
+            return "isosceles";
+        }
+
+        return "none";
+    }
+}
