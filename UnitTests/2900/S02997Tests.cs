19b7e78800573e9c47ab61dd74286476597cd3ae
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-06 19:39:43 +1000

    2997. Minimum Number of Operations to Make Array XOR Equal to K

---
+++ UnitTests/2900/S02997Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02997Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02997();
+        sut.MinOperations([2, 1, 3, 4], 1).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02997();
+        sut.MinOperations([2, 0, 2, 0], 0).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/2900/S02997.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode_sharp;
+
+// 2997. Minimum Number of Operations to Make Array XOR Equal to K
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-array-xor-equal-to-k
+public class S02997
+{
+    public int MinOperations(int[] nums, int k)
+    {
+        k = nums.Aggregate(k, (current, num) => current ^ num);
+
+        var answer = 0;
+
+        while (k != 0)
+        {
+            answer += k & 1;
+            k >>= 1;
+        }
+
+        return answer;
+    }
+}
