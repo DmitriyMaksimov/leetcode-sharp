54698cbd1c6d7d2018f67143450c9e9c365b79fd
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-06 19:42:23 +1000

    Use BitOperations

--- leetcode-sharp/2900/S02997.cs
+++ leetcode-sharp/2900/S02997.cs
@@ -1,4 +1,6 @@
-﻿namespace leetcode_sharp;
+﻿using System.Numerics;
+
+namespace leetcode_sharp;
 
 // 2997. Minimum Number of Operations to Make Array XOR Equal to K
 // https://leetcode.com/problems/minimum-number-of-operations-to-make-array-xor-equal-to-k
@@ -6,16 +8,6 @@ public class S02997
 {
     public int MinOperations(int[] nums, int k)
     {
-        k = nums.Aggregate(k, (current, num) => current ^ num);
-
-        var answer = 0;
-
-        while (k != 0)
-        {
-            answer += k & 1;
-            k >>= 1;
-        }
-
-        return answer;
+        return BitOperations.PopCount((uint) nums.Aggregate(k, (current, num) => current ^ num));
     }
 }
