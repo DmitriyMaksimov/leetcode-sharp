f4ec57b117f310f6d9c47b5a1455712ef6e74a70
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-07 21:46:38 +1000

    890. Find and Replace Pattern

---
+++ UnitTests/0800/S00890Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00890Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00890();
+        sut.FindAndReplacePattern(["abc", "deq", "mee", "aqq", "dkd", "ccc"], "abb").Should().Equal("mee", "aqq");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00890();
+        sut.FindAndReplacePattern(["a", "b", "c"], "a").Should().Equal("a", "b", "c");
+    }
+}
---
+++ leetcode-sharp/0800/S00890.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_sharp;
+
+// 890. Find and Replace Pattern
+// https://leetcode.com/problems/find-and-replace-pattern
+public class S00890
+{
+    public IList<string> FindAndReplacePattern(string[] words, string pattern)
+    {
+        var p = buildPattern(pattern);
+
+        return words.Where(word => buildPattern(word).SequenceEqual(p)).ToList();
+    }
+
+    private static IEnumerable<int> buildPattern(string w)
+    {
+        var m = new Dictionary<char, int>();
+        var n = w.Length;
+        var res = new int[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            m.TryAdd(w[i], m.Count);
+            res[i] = m[w[i]];
+        }
+
+        return res;
+    }
+}
