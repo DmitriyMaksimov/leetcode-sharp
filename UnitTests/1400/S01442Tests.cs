5c143e38a117cbf57d902b84b233fc7c81425b61
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-07 20:26:24 +1000

    1442. Count Triplets That Can Form Two Arrays of Equal XOR

---
+++ UnitTests/1400/S01442Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01442Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01442();
+        sut.CountTriplets([2, 3, 1, 6, 7]).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01442();
+        sut.CountTriplets([1, 1, 1, 1, 1]).Should().Be(10);
+    }
+}
---
+++ leetcode-sharp/1400/S01442.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 1442. Count Triplets That Can Form Two Arrays of Equal XOR
+// https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor
+public class S01442
+{
+    public int CountTriplets(int[] arr)
+    {
+        var n = arr.Length + 1;
+        var result = 0;
+        var prefix = new int[n];
+
+        for (var i = 1; i < n; ++i)
+        {
+            prefix[i] = arr[i - 1] ^ prefix[i - 1];
+        }
+
+        for (var i = 0; i < n; ++i)
+        {
+            for (var j = i + 1; j < n; ++j)
+            {
+                if (prefix[i] == prefix[j])
+                {
+                    result += j - i - 1;
+                }
+            }
+        }
+
+        return result;
+    }
+}
