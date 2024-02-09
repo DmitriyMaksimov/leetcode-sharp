69aade6d63e58788431f9294dbe1fd503f35f255
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-09 11:17:28 +1000

    368. Largest Divisible Subset

---
+++ UnitTests/0300/S00368Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00368Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00368();
+        sut.LargestDivisibleSubset([1, 2, 3]).Should().Equal(2, 1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00368();
+        sut.LargestDivisibleSubset([1, 2, 4, 8]).Should().Equal(8, 4, 2, 1);
+    }
+}
---
+++ leetcode-sharp/0300/S00368.cs
@@ -0,0 +1,47 @@
+﻿namespace leetcode_sharp;
+
+// 368. Largest Divisible Subset
+// https://leetcode.com/problems/largest-divisible-subset
+public class S00368
+{
+    public IList<int> LargestDivisibleSubset(int[] nums)
+    {
+        var n = nums.Length;
+        var dp = new int[n];
+        Array.Fill(dp, 1);
+        Array.Sort(nums);
+
+        var maxSize = 1;
+        var maxIndex = 0;
+
+        for (var i = 1; i < n; i++)
+        {
+            for (var j = 0; j < i; j++)
+            {
+                if (nums[i] % nums[j] == 0)
+                {
+                    dp[i] = Math.Max(dp[i], dp[j] + 1);
+                    if (dp[i] > maxSize)
+                    {
+                        maxSize = dp[i];
+                        maxIndex = i;
+                    }
+                }
+            }
+        }
+
+        var result = new List<int>();
+        var num = nums[maxIndex];
+        for (var i = maxIndex; i >= 0; i--)
+        {
+            if (num % nums[i] == 0 && dp[i] == maxSize)
+            {
+                result.Add(nums[i]);
+                num = nums[i];
+                maxSize--;
+            }
+        }
+
+        return result;
+    }
+}
