ce33c9c00caa737d20a9ffbddda8de9acedbdf88
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-09 11:09:10 +1000

    811. Subdomain Visit Count

---
+++ UnitTests/0800/S00811Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00811Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00811();
+        sut.SubdomainVisits(["9001 discuss.leetcode.com"]).Should().BeEquivalentTo("9001 leetcode.com","9001 discuss.leetcode.com","9001 com");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00811();
+        sut.SubdomainVisits(["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]).Should().BeEquivalentTo("901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com");
+    }
+}
---
+++ leetcode-sharp/0800/S00811.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 811. Subdomain Visit Count
+// https://leetcode.com/problems/subdomain-visit-count
+public class S00811
+{
+    public IList<string> SubdomainVisits(string[] cpdomains)
+    {
+        var counts = new Dictionary<string, int>();
+
+        foreach (var domain in cpdomains)
+        {
+            var strings = domain.Split(' ');
+            var n = int.Parse(strings[0]);
+            var s = strings[1];
+
+            for (var i = 0; i < s.Length; ++i)
+            {
+                if (s[i] == '.')
+                {
+                    var d = s[(i + 1)..];
+                    counts[d] = counts.GetValueOrDefault(d) + n;
+                }
+            }
+
+            counts[s] = counts.GetValueOrDefault(s) + n;
+        }
+
+        return counts.Select(x => $"{x.Value} {x.Key}").ToList();
+    }
+}
