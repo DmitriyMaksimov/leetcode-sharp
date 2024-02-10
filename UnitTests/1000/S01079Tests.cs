85b79bfbab6ba660ac7a18651deab9b9e81ebd98
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-10 16:38:51 +1000

    1079. Letter Tile Possibilities

---
+++ UnitTests/1000/S01079Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01079Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01079();
+        sut.NumTilePossibilities("AAB").Should().Be(8);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01079();
+        sut.NumTilePossibilities("AAABBC").Should().Be(188);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01079();
+        sut.NumTilePossibilities("V").Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/1000/S01079.cs
@@ -0,0 +1,36 @@
+﻿namespace leetcode_sharp;
+
+// 1079. Letter Tile Possibilities
+// https://leetcode.com/problems/letter-tile-possibilities
+public class S01079
+{
+    private const int MaxChars = 26;
+
+    public int NumTilePossibilities(string tiles)
+    {
+        var count = new int[MaxChars];
+
+        foreach (var tile in tiles)
+        {
+            count[tile - 'A']++;
+        }
+
+        return dfs(count);
+    }
+
+    private static int dfs(int[] arr)
+    {
+        var sum = 0;
+
+        for (var i = 0; i < MaxChars; i++)
+        {
+            if (arr[i] == 0) continue;
+
+            arr[i]--;
+            sum += dfs(arr) + 1;
+            arr[i]++;
+        }
+
+        return sum;
+    }
+}
