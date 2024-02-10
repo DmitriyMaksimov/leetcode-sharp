21147d4a649870db9aebca2d3821c0b6b82b992e
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-11 09:52:04 +1000

    419. Battleships in a Board

---
+++ UnitTests/0400/S00419Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00419Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00419();
+        sut.CountBattleships([['X', '.', '.', 'X'], ['.', '.', '.', 'X'], ['.', '.', '.', 'X']]).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00419();
+        sut.CountBattleships([['.']]).Should().Be(0);
+    }
+}
---
+++ leetcode-sharp/0400/S00419.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_sharp;
+
+// 419. Battleships in a Board
+// https://leetcode.com/problems/battleships-in-a-board
+public class S00419
+{
+    public int CountBattleships(char[][] board)
+    {
+        var m = board.Length;
+        var n = board[0].Length;
+        
+        if (m == 0) return 0;
+
+        var count = 0;
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                if (board[i][j] == '.') continue;
+                if (i > 0 && board[i - 1][j] == 'X') continue;
+                if (j > 0 && board[i][j - 1] == 'X') continue;
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
