de2444807ac02fe1aa465a4537940e5df7a27c1a
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-11 09:46:21 +1000

    2375. Construct Smallest Number From DI String

---
+++ UnitTests/2300/S02375Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02375Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02375();
+        sut.SmallestNumber("IIIDIDDD").Should().Be("123549876");
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02375();
+        sut.SmallestNumber("DDD").Should().Be("4321");
+    }
+}
---
+++ leetcode-sharp/2300/S02375.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_sharp;
+
+// 2375. Construct Smallest Number From DI String
+// https://leetcode.com/problems/construct-smallest-number-from-di-string
+public class S02375
+{
+    public string SmallestNumber(string pattern)
+    {
+        var list = new List<char>(pattern.Length);
+        var stack = new Stack<char>();
+
+        for (var i = 0; i <= pattern.Length; i++)
+        {
+            stack.Push((char) ('1' + i));
+            if (i == pattern.Length || pattern[i] == 'I')
+            {
+                list.AddRange(stack);
+                stack.Clear();
+            }
+        }
+
+        return new string(list.ToArray());
+    }
+}
