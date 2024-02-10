43d507cf5bb84608b8cbe4f61f6e5fe769a7b0bf
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-10 10:20:16 +1000

    647. Palindromic Substrings

---
+++ UnitTests/0600/S00647Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00647Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00647();
+        sut.CountSubstrings("abc").Should().Be(3);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00647();
+        sut.CountSubstrings("aaa").Should().Be(6);
+    }
+}
---
+++ leetcode-sharp/0600/S00647.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_sharp;
+
+// 647. Palindromic Substrings
+// https://leetcode.com/problems/palindromic-substrings
+public class S00647
+{
+    public int CountSubstrings(string s)
+    {
+        var cnt = 0;
+        var n = s.Length;
+        for (var i = 0; i < n; i++)
+        {
+            cnt += countPalindromic(s, i, i);
+            cnt += countPalindromic(s, i, i + 1);
+        }
+
+        return cnt;
+    }
+
+    private static int countPalindromic(string s, int i, int j)
+    {
+        var cnt = 0;
+
+        while (i >= 0 && j < s.Length && s[i] == s[j])
+        {
+            cnt++;
+            i--;
+            j++;
+        }
+
+        return cnt;
+    }
+}
