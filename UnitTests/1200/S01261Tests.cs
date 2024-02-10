53cee77e83a468ac75024e0d57ab74a2ea59519b
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-10 14:01:24 +1000

    1261. Find Elements in a Contaminated Binary Tree

---
+++ UnitTests/1200/S01261Tests.cs
@@ -0,0 +1,32 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01261Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01261.FindElements(TreeNode.Parse("[-1,null,-1]"));
+        sut.Find(1).Should().BeFalse();
+        sut.Find(2).Should().BeTrue();
+    }
+    [Test]
+    public void T2()
+    {
+        var sut = new S01261.FindElements(TreeNode.Parse("[-1,-1,-1,-1,-1]"));
+        sut.Find(1).Should().BeTrue();
+        sut.Find(3).Should().BeTrue();
+        sut.Find(5).Should().BeFalse();
+    }
+    [Test]
+    public void T3()
+    {
+        var sut = new S01261.FindElements(TreeNode.Parse("[-1,null,-1,-1,null,-1]"));
+        sut.Find(2).Should().BeTrue();
+        sut.Find(3).Should().BeFalse();
+        sut.Find(4).Should().BeFalse();
+        sut.Find(5).Should().BeTrue();
+    }
+}
---
+++ leetcode-sharp/1200/S01261.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_sharp;
+
+// 1261. Find Elements in a Contaminated Binary Tree
+// https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree
+public static class S01261
+{
+    public class FindElements
+    {
+        private readonly HashSet<int> _hashSet = [];
+
+        public FindElements(TreeNode root)
+        {
+            dfs(root, 0);
+        }
+
+        private void dfs(TreeNode? node, int val)
+        {
+            if (node == null) return;
+
+            _hashSet.Add(val);
+            node.val = val;
+
+            dfs(node.left, 2 * val + 1);
+            dfs(node.right, 2 * val + 2);
+        }
+
+        public bool Find(int target)
+        {
+            return _hashSet.Contains(target);
+        }
+    }
+}
