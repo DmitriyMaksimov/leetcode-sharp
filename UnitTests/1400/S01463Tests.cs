78737dc98b9f73b8ae880c7f7e2a4b4a098c71d4
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-11 10:18:17 +1000

    1463. Cherry Pickup II

---
+++ UnitTests/1400/S01463Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01463Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01463();
+        sut.CherryPickup([[3, 1, 1], [2, 5, 1], [1, 5, 5], [2, 1, 1]]).Should().Be(24);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01463();
+        sut.CherryPickup([[1, 0, 0, 0, 0, 0, 1], [2, 0, 0, 0, 0, 3, 0], [2, 0, 9, 0, 0, 0, 0], [0, 3, 0, 5, 4, 0, 0], [1, 0, 2, 3, 0, 0, 6]]).Should().Be(28);
+    }
+}
---
+++ leetcode-sharp/1400/S01463.cs
@@ -0,0 +1,53 @@
+﻿namespace leetcode_sharp;
+
+// 1463. Cherry Pickup II
+// https://leetcode.com/problems/cherry-pickup-ii
+public class S01463
+{
+    private readonly Dictionary<(int row, int col1, int col2), int> _dp = new();
+    private int _m;
+    private int _n;
+    private int[][] _grid;
+
+    public int CherryPickup(int[][] grid)
+    {
+        _grid = grid;
+        _m = grid.Length;
+        _n = grid[0].Length;
+
+        return dfs(0, 0, _n - 1);
+    }
+
+    private int dfs(int row, int col1, int col2)
+    {
+        if (row == _m)
+        {
+            return 0;
+        }
+
+        if (_dp.TryGetValue((row, col1, col2), out var dpValue))
+        {
+            return dpValue;
+        }
+
+        var result = 0;
+
+        for (var i = -1; i <= 1; i++)
+        {
+            for (var j = -1; j <= 1; j++)
+            {
+                var newCol1 = col1 + i;
+                var newCol2 = col2 + j;
+
+                if (newCol1 >= 0 && newCol1 < _n && newCol2 >= 0 && newCol2 < _n)
+                {
+                    result = Math.Max(result, dfs(row + 1, newCol1, newCol2));
+                }
+            }
+        }
+
+        var cherries = col1 == col2 ? _grid[row][col1] : _grid[row][col1] + _grid[row][col2];
+
+        return _dp[(row, col1, col2)] = result + cherries;
+    }
+}
