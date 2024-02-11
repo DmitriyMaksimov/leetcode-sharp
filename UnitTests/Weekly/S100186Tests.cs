409eb2a82e23d75fee9867794fa0a4fc6719b445
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-11 12:57:00 +1000

    100186. Number of Subarrays That Match a Pattern I

---
+++ UnitTests/Weekly/S100186Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S100186Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S100186();
+        sut.CountMatchingSubarrays([1, 2, 3, 4, 5, 6], [1, 1]).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S100186();
+        sut.CountMatchingSubarrays([1,4,4,1,3,5,5,3], [1,0,-1]).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/Weekly/S100186.cs
@@ -0,0 +1,45 @@
+﻿namespace leetcode_sharp;
+
+// 100186. Number of Subarrays That Match a Pattern I
+public class S100186
+{
+    public int CountMatchingSubarrays(int[] nums, int[] pattern)
+    {
+        var count = 0;
+        var n = nums.Length;
+        var m = pattern.Length;
+
+        for (var i = 0; i < n - m; i++)
+        {
+            var isMatching = true;
+
+            for (var j = 0; j < m; j++)
+            {
+                if (pattern[j] == 1 && nums[i + j + 1] <= nums[i + j])
+                {
+                    isMatching = false;
+                    break;
+                }
+
+                if (pattern[j] == 0 && nums[i + j + 1] != nums[i + j])
+                {
+                    isMatching = false;
+                    break;
+                }
+
+                if (pattern[j] == -1 && nums[i + j + 1] >= nums[i + j])
+                {
+                    isMatching = false;
+                    break;
+                }
+            }
+
+            if (isMatching)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
