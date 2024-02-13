ec854fb5f3ace50773becfc9544160df79e52a1c
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-13 10:23:28 +1000

    1104. Path In Zigzag Labelled Binary Tree

---
+++ UnitTests/1100/S01104Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01104Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01104();
+        sut.PathInZigZagTree(14).Should().Equal(1,3,4,14);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01104();
+        sut.PathInZigZagTree(26).Should().Equal(1,2,6,10,26);
+    }
+}
---
+++ leetcode-sharp/1100/S01104.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_sharp;
+
+// 1104. Path In Zigzag Labelled Binary Tree
+// https://leetcode.com/problems/path-in-zigzag-labelled-binary-tree
+public class S01104
+{
+    public IList<int> PathInZigZagTree(int label)
+    {
+        var result = new LinkedList<int>();
+        var parent = label;
+
+        result.AddFirst(parent);
+
+        while (parent != 1)
+        {
+            var d = (int) (Math.Log(parent) / Math.Log(2));
+            var offset = (int) Math.Pow(2, d + 1) - 1 - parent;
+            parent = ((int) Math.Pow(2, d) + offset) / 2;
+
+            result.AddFirst(parent);
+        }
+
+        return result.ToList();
+    }
+}
