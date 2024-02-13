a97320c2bd372aa4bb4e53254641e1d07ecf4ae9
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-13 10:29:53 +1000

    921. Minimum Add to Make Parentheses Valid

---
+++ UnitTests/0900/S00921Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00921Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S00921();
+        sut.MinAddToMakeValid("())").Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S00921();
+        sut.MinAddToMakeValid("(((").Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/0900/S00921.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_sharp;
+
+// 921. Minimum Add to Make Parentheses Valid
+// https://leetcode.com/problems/minimum-add-to-make-parentheses-valid
+public class S00921
+{
+    public int MinAddToMakeValid(string s)
+    {
+        var left = 0;
+        var right = 0;
+
+        for (var i = 0; i < s.Length; ++i)
+        {
+            if (s[i] == '(')
+            {
+                right++;
+            }
+            else if (right > 0)
+            {
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+
+        return left + right;
+    }
+}
