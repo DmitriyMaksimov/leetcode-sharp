076f26209bafef5fa3ccee76d121bace0686500f
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-15 12:06:54 +1000

    2971. Find Polygon With the Largest Perimeter

---
+++ UnitTests/2900/S02971Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02971Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02971();
+        sut.LargestPerimeter([5, 5, 5]).Should().Be(15);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02971();
+        sut.LargestPerimeter([1, 12, 1, 2, 5, 50, 3]).Should().Be(12);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S02971();
+        sut.LargestPerimeter([5, 5, 50]).Should().Be(-1);
+    }
+}
---
+++ leetcode-sharp/2900/S02971.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode_sharp;
+
+// 2971. Find Polygon With the Largest Perimeter
+// https://leetcode.com/problems/find-polygon-with-the-largest-perimeter
+public class S02971
+{
+    public long LargestPerimeter(int[] nums)
+    {
+        Array.Sort(nums);
+        var len = nums.Length;
+        var arr = new long[len];
+        arr[0] = nums[0];
+
+        for (var i = 1; i < len; i++)
+        {
+            arr[i] += arr[i - 1] + nums[i];
+        }
+
+        long ans = int.MinValue;
+
+        for (var i = 2; i < len; i++)
+        {
+            if (arr[i] - arr[i - 1] < arr[i - 1])
+            {
+                ans = Math.Max(ans, arr[i]);
+            }
+        }
+
+        return ans == int.MinValue ? -1 : ans;
+    }
+}
