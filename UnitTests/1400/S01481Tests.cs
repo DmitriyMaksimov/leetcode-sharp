22c4fe314fe6e20f03fd25457376175449be1be3
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-16 15:47:34 +1000

    1481. Least Number of Unique Integers after K Removals

---
+++ UnitTests/1400/S01481Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01481Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01481();
+        sut.FindLeastNumOfUniqueInts([5, 5, 4], 1).Should().Be(1);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01481();
+        sut.FindLeastNumOfUniqueInts([4, 3, 1, 1, 3, 3, 2], 3).Should().Be(2);
+    }
+}
---
+++ leetcode-sharp/1400/S01481.cs
@@ -0,0 +1,40 @@
+﻿namespace leetcode_sharp;
+
+// 1481. Least Number of Unique Integers after K Removals
+// https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals
+public class S01481
+{
+    public int FindLeastNumOfUniqueInts(int[] arr, int k)
+    {
+        var count = new Dictionary<int, int>();
+
+        foreach (var n in arr)
+        {
+            count[n] = count.GetValueOrDefault(n) + 1;
+        }
+
+        var remaining = count.Count;
+        var occur = 1;
+        var occurrenceCount = new int[arr.Length + 1];
+
+        foreach (var value in count.Values)
+        {
+            ++occurrenceCount[value];
+        }
+
+        while (k > 0)
+        {
+            if (k - occur * occurrenceCount[occur] >= 0)
+            {
+                k -= occur * occurrenceCount[occur];
+                remaining -= occurrenceCount[occur++];
+            }
+            else
+            {
+                return remaining - k / occur;
+            }
+        }
+
+        return remaining;
+    }
+}
