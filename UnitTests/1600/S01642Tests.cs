0c1b25db69666efca4537485850fffc898ef2b0f
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-17 12:13:11 +1000

    1642. Furthest Building You Can Reach

---
+++ UnitTests/1600/S01642Tests.cs
@@ -0,0 +1,28 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01642Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01642();
+        sut.FurthestBuilding([4, 2, 7, 6, 9, 14, 12], 5, 1).Should().Be(4);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01642();
+        sut.FurthestBuilding([4, 12, 2, 7, 3, 18, 20, 3, 19], 10, 2).Should().Be(7);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01642();
+        sut.FurthestBuilding([14, 3, 19, 3], 17, 0).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/1600/S01642.cs
@@ -0,0 +1,39 @@
+﻿namespace leetcode_sharp;
+
+// 1642. Furthest Building You Can Reach
+// https://leetcode.com/problems/furthest-building-you-can-reach
+public class S01642
+{
+    public int FurthestBuilding(int[] heights, int bricks, int ladders)
+    {
+        var p = new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => y - x));
+
+        int i;
+
+        for (i = 0; i < heights.Length - 1; i++)
+        {
+            var diff = heights[i + 1] - heights[i];
+
+            if (diff <= 0)
+            {
+                continue;
+            }
+
+            bricks -= diff;
+            p.Enqueue(diff, diff);
+
+            if (bricks < 0)
+            {
+                bricks += p.Dequeue();
+                ladders--;
+            }
+
+            if (ladders < 0)
+            {
+                break;
+            }
+        }
+
+        return i;
+    }
+}
