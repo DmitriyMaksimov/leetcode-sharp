4437d6b52fa7ae1a071a2b388248ce136c6b2ee9
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-02-18 12:30:00 +1000

    2402. Meeting Rooms III

---
+++ UnitTests/2400/S02402Tests.cs
@@ -0,0 +1,21 @@
+﻿using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02402Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02402();
+        sut.MostBooked(2, [[0, 10], [1, 5], [2, 7], [3, 4]]).Should().Be(0);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02402();
+        sut.MostBooked(3, [[1, 20], [2, 10], [3, 5], [4, 9], [6, 8]]).Should().Be(1);
+    }
+}
---
+++ leetcode-sharp/2400/S02402.cs
@@ -0,0 +1,48 @@
+﻿namespace leetcode_sharp;
+
+// 2402. Meeting Rooms III
+// https://leetcode.com/problems/meeting-rooms-iii
+public class S02402
+{
+    public int MostBooked(int n, int[][] meetings)
+    {
+        var rooms = new long[n];
+        var count = new int[n];
+        
+        foreach (var meeting in meetings.OrderBy(x => x[0]))
+        {
+            var meetingStart = meeting[0];
+            var meetingEnd = meeting[1];
+            var bestIndex = long.MinValue;
+            var bestEnd = long.MaxValue;
+
+            var flag = false;
+            
+            for (var i = 0; i < rooms.Length; i++)
+            {
+                var end = rooms[i];
+                if (meetingStart >= end)
+                {
+                    rooms[i] = meetingEnd;
+                    count[i] += 1;
+                    flag = true;
+                    break;
+                }
+
+                if (end < bestEnd)
+                {
+                    bestIndex = i;
+                    bestEnd = end;
+                }
+            }
+
+            if (!flag)
+            {
+                rooms[bestIndex] = meetingEnd - meetingStart + rooms[bestIndex];
+                count[bestIndex] += 1;
+            }
+        }
+
+        return Array.IndexOf(count, count.Max());
+    }
+}
