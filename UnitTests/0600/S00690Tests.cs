173e2aa5a1f487fc4a6bbb4a144754e16de5b194
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-09-15 18:42:48 +1000

    690. Employee Importance

---
+++ UnitTests/0600/S00690Tests.cs
@@ -0,0 +1,28 @@
+using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S00690Tests
+{
+    [Test]
+    public void T1()
+    {
+        var e1 = new S00690.Employee { id = 1, importance = 5, subordinates = [2, 3] };
+        var e2 = new S00690.Employee { id = 2, importance = 3, subordinates = [] };
+        var e3 = new S00690.Employee { id = 3, importance = 3, subordinates = [] };
+
+        var sut = new S00690();
+        sut.GetImportance([e1, e2, e3], 1).Should().Be(11);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var e1 = new S00690.Employee { id = 1, importance = 2, subordinates = [5] };
+        var e2 = new S00690.Employee { id = 5, importance = -3, subordinates = [] };
+
+        var sut = new S00690();
+        sut.GetImportance([e1, e2], 5).Should().Be(-3);
+    }
+}
---
+++ leetcode-sharp/0600/S00690.cs
@@ -0,0 +1,35 @@
+namespace leetcode_sharp;
+
+// 690. Employee Importance
+// https://leetcode.com/problems/employee-importance
+public class S00690
+{
+    public class Employee
+    {
+        public int id;
+        public int importance;
+        public IList<int> subordinates;
+    }
+
+    public int GetImportance(IList<Employee> employees, int id)
+    {
+        var map = employees.ToDictionary(employee => employee.id);
+
+        var total = 0;
+        var queue = new Queue<Employee>();
+        queue.Enqueue(map[id]);
+
+        while (queue.Count != 0)
+        {
+            var current = queue.Dequeue();
+            total += current.importance;
+
+            foreach (var subordinate in current.subordinates)
+            {
+                queue.Enqueue(map[subordinate]);
+            }
+        }
+
+        return total;
+    }
+}
