cd29b26d77167db96da893cef83867cbd4b765e9
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2024-12-21 13:46:44 +1000

    2872. Maximum Number of K-Divisible Components

---
+++ UnitTests/2800/S02872Tests.cs
@@ -0,0 +1,21 @@
+using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S02872Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S02872();
+        sut.MaxKDivisibleComponents(5, [[0, 2], [1, 2], [1, 3], [2, 4]], [1, 8, 1, 4, 4], 6).Should().Be(2);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S02872();
+        sut.MaxKDivisibleComponents(7, [[0, 1], [0, 2], [1, 3], [1, 4], [2, 5], [2, 6]], [3, 0, 6, 1, 5, 2, 1], 3).Should().Be(3);
+    }
+}
---
+++ leetcode-sharp/2800/S02872.cs
@@ -0,0 +1,52 @@
+namespace leetcode_sharp;
+
+// 2872. Maximum Number of K-Divisible Components
+// https://leetcode.com/problems/maximum-number-of-k-divisible-components
+public class S02872
+{
+    private int[] _dp = null!;
+    private bool[] _visited = null!;
+    private int _result;
+
+    public int MaxKDivisibleComponents(int n, int[][] edges, int[] values, int k)
+    {
+        _dp = values.Select(x => x).ToArray();
+
+        var adj = new List<int>[n];
+        for (var i = 0; i < n; i++)
+        {
+            adj[i] = [];
+        }
+
+        foreach (var edge in edges)
+        {
+            adj[edge[0]].Add(edge[1]);
+            adj[edge[1]].Add(edge[0]);
+        }
+
+        _visited = new bool[n];
+
+        Dfs(adj, 0, k);
+
+        return _result;
+    }
+
+    private int Dfs(List<int>[] adj, int s, int k)
+    {
+        _visited[s] = true;
+
+        foreach (var nbr in adj[s].Where(nbr => !_visited[nbr]))
+        {
+            _dp[s] += Dfs(adj, nbr, k);
+        }
+
+        if (_dp[s] % k != 0)
+        {
+            return _dp[s];
+        }
+
+        _result++;
+
+        return 0;
+    }
+}
