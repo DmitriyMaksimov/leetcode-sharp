7c39a1bfc407ce1754feb10d0ae4bfd7f9601f91
Dmitriy Maksimov <Dmitriy.Maksimov@gmail.com>
2025-01-27 12:21:51 +1000

    1462. Course Schedule IV

---
+++ UnitTests/1400/S01462Tests.cs
@@ -0,0 +1,28 @@
+using leetcode_sharp;
+
+namespace UnitTests;
+
+[TestFixture]
+public class S01462Tests
+{
+    [Test]
+    public void T1()
+    {
+        var sut = new S01462();
+        sut.CheckIfPrerequisite(2, [[1, 0]], [[0, 1], [1, 0]]).Should().Equal(false, true);
+    }
+
+    [Test]
+    public void T2()
+    {
+        var sut = new S01462();
+        sut.CheckIfPrerequisite(2, [], [[1, 0], [0, 1]]).Should().Equal(false, false);
+    }
+
+    [Test]
+    public void T3()
+    {
+        var sut = new S01462();
+        sut.CheckIfPrerequisite(3, [[1, 2], [1, 0], [2, 0]], [[1, 0], [1, 2]]).Should().Equal(true, true);
+    }
+}
---
+++ leetcode-sharp/1400/S01462.cs
@@ -0,0 +1,64 @@
+namespace leetcode_sharp;
+
+// 1462. Course Schedule IV
+// https://leetcode.com/problems/course-schedule-iv
+public class S01462
+{
+    public IList<bool> CheckIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries)
+    {
+        var indegree = new int[numCourses];
+        var adj = new Dictionary<int, HashSet<int>>();
+        var prerequisitesMap = new Dictionary<int, HashSet<int>>();
+
+        for (var i = 0; i < numCourses; i++)
+        {
+            prerequisitesMap[i] = [];
+            adj[i] = [];
+        }
+
+        foreach (var pre in prerequisites)
+        {
+            indegree[pre[1]]++;
+            adj[pre[0]].Add(pre[1]);
+        }
+
+        var queue = new Queue<int>();
+        for (var i = 0; i < numCourses; i++)
+        {
+            if (indegree[i] == 0)
+            {
+                queue.Enqueue(i);
+            }
+        }
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            foreach (var next in adj[node])
+            {
+                prerequisitesMap[next].Add(node);
+                prerequisitesMap[next].UnionWith(prerequisitesMap[node]);
+                indegree[next]--;
+                if (indegree[next] == 0)
+                {
+                    queue.Enqueue(next);
+                }
+            }
+        }
+
+        var res = new List<bool>();
+        foreach (var pair in queries)
+        {
+            if (prerequisitesMap[pair[1]].Contains(pair[0]))
+            {
+                res.Add(true);
+            }
+            else
+            {
+                res.Add(false);
+            }
+        }
+
+        return res;
+    }
+}
