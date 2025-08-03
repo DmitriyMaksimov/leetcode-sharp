c5a2e7379e643a2a28bfe0d64b614c2bc22b44d7
Dmitriy Maksimov <dmitriy.maksimov@gmail.com>
2025-08-03 17:33:34 +1000

    Fix warnings

--- leetcode-sharp/0400/S00491.cs
+++ leetcode-sharp/0400/S00491.cs
@@ -4,30 +4,35 @@
 // https://leetcode.com/problems/non-decreasing-subsequences/
 public class S00491
 {
-    private class SequenceComparer<T>:IEqualityComparer<IEnumerable<T>>
+    private class SequenceComparer<T> : IEqualityComparer<IEnumerable<T>>
     {
-        public bool Equals(IEnumerable<T> seq1,IEnumerable<T> seq2)
+        public bool Equals(IEnumerable<T>? seq1, IEnumerable<T>? seq2)
         {
+            if (seq1 == null || seq2 == null)
+            {
+                return false;
+            }
+
             return seq1.SequenceEqual(seq2);
         }
-    
+
         public int GetHashCode(IEnumerable<T> seq)
         {
             return seq.Aggregate(1234567, (current, elem) => unchecked(current * 37 + elem!.GetHashCode()));
         }
     }
-    
+
     public IList<IList<int>> FindSubsequences(int[] nums)
     {
         var result = new HashSet<IList<int>>(new SequenceComparer<int>());
-        
+
         var numberOfVariants = (int)Math.Pow(2, nums.Length);
-        
+
         for (var bitMask = 1; bitMask < numberOfVariants; bitMask++)
         {
             var mask = numberOfVariants / 2;
             var list = new List<int>();
-            
+
             for (var bit = 0; bit < nums.Length; bit++)
             {
                 if ((bitMask & mask) != 0)
@@ -40,6 +45,7 @@ public class S00491
                             break;
                         }
                     }
+
                     list.Add(nums[bit]);
                 }
 
@@ -48,7 +54,7 @@ public class S00491
 
             if (list.Count > 1)
             {
-                result.Add(list);                        
+                result.Add(list);
             }
         }
 
